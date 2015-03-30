d84c8dbcec7024ffa82b87a2326fc80d705a1d0b
Miroslav Batchkarov <mbatchkarov@gmail.com>
2015-03-30 09:27:09 +0100

    oops

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(name='Keras',
       version='0.0.1',
