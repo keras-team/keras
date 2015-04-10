8b2849bd314fc8f8f42f65fda95916b5f1cddafc
fchollet <francois.chollet@gmail.com>
2015-04-09 17:43:03 -0700

    Add frequency sampling to skipgram. Warning: math.

--- keras/preprocessing/sequence.py
+++ keras/preprocessing/sequence.py
@@ -22,8 +22,30 @@ def pad_sequences(seqs, maxlen=None, dtype='int32'):
     return x
 
 
+def make_sampling_table(size, sampling_factor=1e-5):
+    '''
+        This generates an array where the ith element
+        is the probability that a word of rank i would be sampled,
+        according to the sampling distribution used in word2vec.
+        
+        The word2vec formula is:
+            p(word) = min(1, sqrt(word.frequency/sampling_factor) / (word.frequency/sampling_factor))
+
+        We assume that the word frequencies follow Zipf's law (s=1) to derive 
+        a numerical approximation of frequency(rank):
+           frequency(rank) ~ 1/(rank * (log(rank) + gamma) + 1/2 - 1/(12*rank))
+        where gamma is the Euler–Mascheroni constant.
+    '''
+    gamma = 0.577
+    rank = np.array(range(size))
+    rank[0] = 1
+    inv_fq = rank * (np.log(rank) + gamma) + 0.5 - 1./(12.*rank)
+    f = sampling_factor * inv_fq
+    return np.minimum(1., f / np.sqrt(f))
+
+
 def skipgrams(sequence, vocabulary_size, 
-    window_size=4, negative_samples=1., shuffle=True, categorical=False, seed=None):
+    window_size=4, negative_samples=1., shuffle=True, categorical=False, seed=None, sampling_table=None):
     ''' 
         Take a sequence (list of indexes of words), 
         returns couples of [word_index, other_word index] and labels (1s or 0s),
@@ -42,14 +64,18 @@ def skipgrams(sequence, vocabulary_size,
     labels = []
     for i, wi in enumerate(sequence):
         if not wi:
-            pass
+            continue
+        if sampling_table is not None:
+            if sampling_table[i] < random.random():
+                continue
+
         window_start = max(0, i-window_size)
         window_end = min(len(sequence), i+window_size+1)
         for j in range(window_start, window_end):
             if j != i:
                 wj = sequence[j]
                 if not wj:
-                    pass
+                    continue
                 couples.append([wi, wj])
                 if categorical:
                     labels.append([0,1])
@@ -60,7 +86,7 @@ def skipgrams(sequence, vocabulary_size,
         nb_negative_samples = int(len(labels) * negative_samples)
         words = [c[0] for c in couples]
         random.shuffle(words)
-        
+
         couples += [[words[i%len(words)], random.randint(1, vocabulary_size-1)] for i in range(nb_negative_samples)]
         if categorical:
             labels += [[1,0]]*nb_negative_samples
