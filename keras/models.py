616537f3085aa00c41871a2226c4fdd4d6e8c669
fchollet <francois.chollet@gmail.com>
2015-04-15 13:50:18 -0700

    Add accuracy monitoring, verbose eval/predict

--- keras/models.py
+++ keras/models.py
@@ -13,6 +13,10 @@ def standardize_y(y):
         y = np.reshape(y, (len(y), 1))
     return y
 
+def make_batches(size, batch_size):
+    nb_batch = int(np.ceil(size/float(batch_size)))
+    return [(i*batch_size, min(size, (i+1)*batch_size)) for i in range(0, nb_batch)]
+
 class Sequential(object):
     def __init__(self):
         self.layers = []
@@ -24,7 +28,7 @@ class Sequential(object):
             self.layers[-1].connect(self.layers[-2])
         self.params += [p for p in layer.params]
 
-    def compile(self, optimizer, loss):
+    def compile(self, optimizer, loss, class_mode="categorical"):
         self.optimizer = optimizers.get(optimizer)
         self.loss = objectives.get(loss)
 
@@ -34,44 +38,76 @@ class Sequential(object):
         self.y_test = self.layers[-1].output(train=False)
 
         # output of model
-        self.Y = T.matrix() # TODO: support for custom output shapes
+        self.y = T.matrix() # TODO: support for custom output shapes
 
-        train_loss = self.loss(self.Y, self.y_train)
-        test_score = self.loss(self.Y, self.y_test)
+        train_loss = self.loss(self.y, self.y_train)
+        test_score = self.loss(self.y, self.y_test)
+
+        if class_mode == "categorical":
+            train_accuracy = T.mean(T.eq(T.argmax(self.y, axis=-1), T.argmax(self.y_train, axis=-1)))
+            test_accuracy = T.mean(T.eq(T.argmax(self.y, axis=-1), T.argmax(self.y_test, axis=-1)))
+
+        elif class_mode == "binary":
+            train_accuracy = T.mean(T.eq(self.y, T.round(self.y_train)))
+            test_accuracy = T.mean(T.eq(self.y, T.round(self.y_test)))
+        else:
+            raise Exception("Invalid class mode:" + str(class_mode))
+        self.class_mode = class_mode
+
         updates = self.optimizer.get_updates(self.params, train_loss)
 
-        self._train = theano.function([self.X, self.Y], train_loss, 
+        self._train = theano.function([self.X, self.y], train_loss, 
+            updates=updates, allow_input_downcast=True)
+        self._train_with_acc = theano.function([self.X, self.y], [train_loss, train_accuracy], 
             updates=updates, allow_input_downcast=True)
         self._predict = theano.function([self.X], self.y_test, 
             allow_input_downcast=True)
-        self._test = theano.function([self.X, self.Y], test_score, 
+        self._test = theano.function([self.X, self.y], test_score, 
+            allow_input_downcast=True)
+        self._test_with_acc = theano.function([self.X, self.y], [test_score, test_accuracy], 
             allow_input_downcast=True)
 
-    def train(self, X, y):
+    def train(self, X, y, accuracy=False):
         y = standardize_y(y)
-        loss = self._train(X, y)
-        return loss
+        if accuracy:
+            return self._train_with_acc(X, y)
+        else:
+            return self._train(X, y)
+        
 
-    def test(self, X, y):
+    def test(self, X, y, accuracy=False):
         y = standardize_y(y)
-        score = self._test(X, y)
-        return score
+        if accuracy:
+            return self._test_with_acc(X, y)
+        else:
+            return self._test(X, y)
+
 
     def fit(self, X, y, batch_size=128, nb_epoch=100, verbose=1,
-            validation_split=0., shuffle=True):
-        # If a validation split size is given (e.g. validation_split=0.2)
-        # then split X into smaller X and X_val,
-        # and split y into smaller y and y_val.
+            validation_split=0., validation_data=None, shuffle=True, show_accuracy=False):
         y = standardize_y(y)
 
         do_validation = False
-        if validation_split > 0 and validation_split < 1:
+        if validation_data:
+            try:
+                X_val, y_val = validation_data
+            except:
+                raise Exception("Invalid format for validation data; provide a tuple (X_val, y_val).")
             do_validation = True
-            split_at = int(len(X) * (1 - validation_split))
-            (X, X_val) = (X[0:split_at], X[split_at:])
-            (y, y_val) = (y[0:split_at], y[split_at:])
+            y_val = standardize_y(y_val)
             if verbose:
                 print "Train on %d samples, validate on %d samples" % (len(y), len(y_val))
+        else:
+            if 0 < validation_split < 1:
+                # If a validation split size is given (e.g. validation_split=0.2)
+                # then split X into smaller X and X_val,
+                # and split y into smaller y and y_val.
+                do_validation = True
+                split_at = int(len(X) * (1 - validation_split))
+                (X, X_val) = (X[0:split_at], X[split_at:])
+                (y, y_val) = (y[0:split_at], y[split_at:])
+                if verbose:
+                    print "Train on %d samples, validate on %d samples" % (len(y), len(y_val))
         
         index_array = np.arange(len(X))
         for epoch in range(nb_epoch):
@@ -80,57 +116,92 @@ class Sequential(object):
             if shuffle:
                 np.random.shuffle(index_array)
 
-            nb_batch = int(np.ceil(len(X)/float(batch_size)))
+            batches = make_batches(len(X), batch_size)
             progbar = Progbar(target=len(X))
-            for batch_index in range(0, nb_batch):
-                batch_start = batch_index*batch_size
-                batch_end = min(len(X), (batch_index+1)*batch_size)
+            for batch_index, (batch_start, batch_end) in enumerate(batches):
                 batch_ids = index_array[batch_start:batch_end]
-
                 X_batch = X[batch_ids]
                 y_batch = y[batch_ids]
-                loss = self._train(X_batch, y_batch)
+
+                if show_accuracy:
+                    loss, acc = self._train_with_acc(X_batch, y_batch)
+                else:
+                    loss = self._train(X_batch, y_batch)
                 
+                # logging
                 if verbose:
-                    is_last_batch = (batch_index == nb_batch - 1)
+                    is_last_batch = (batch_index == len(batches) - 1)
                     if not is_last_batch or not do_validation:
-                        progbar.update(batch_end, [('loss', loss)])
+                        if show_accuracy:
+                            progbar.update(batch_end, [('loss', loss), ('acc.', acc)])
+                        else:
+                            progbar.update(batch_end, [('loss', loss)])
                     else:
-                        progbar.update(batch_end, [('loss', loss), ('val. loss', self.test(X_val, y_val))])
+                        if show_accuracy:
+                            val_loss, val_acc = self.test(X_val, y_val, accuracy=True)
+                            progbar.update(batch_end, [('loss', loss), ('acc.', acc), ('val. loss', val_loss), ('val. acc.', val_acc)])
+                        else:
+                            val_loss = self.test(X_val, y_val, accuracy=False)
+                            progbar.update(batch_end, [('loss', loss), ('val. loss', val_loss)])
+
             
-    def predict_proba(self, X, batch_size=128):
-        for batch_index in range(0, len(X)/batch_size+1):
-            batch = range(batch_index*batch_size, min(len(X), (batch_index+1)*batch_size))
-            if not batch:
-                break
-            batch_preds = self._predict(X[batch])
+    def predict_proba(self, X, batch_size=128, verbose=1):
+        batches = make_batches(len(X), batch_size)
+        if verbose:
+            progbar = Progbar(target=len(X))
+        for batch_index, (batch_start, batch_end) in enumerate(batches):
+            X_batch = X[batch_start:batch_end]
+            batch_preds = self._predict(X_batch)
 
             if batch_index == 0:
                 shape = (len(X),) + batch_preds.shape[1:]
                 preds = np.zeros(shape)
-            preds[batch] = batch_preds
+            preds[batch_start:batch_end] = batch_preds
+
+            if verbose:
+                progbar.update(batch_end)
         return preds
 
-    def predict_classes(self, X, batch_size=128):
-        proba = self.predict_proba(X, batch_size=batch_size)
-        # The last dimension is the one containing the class probas
-        classes_dim = proba.ndim - 1
-        if proba.shape[classes_dim] > 1:
-            return proba.argmax(axis=classes_dim)
+
+    def predict_classes(self, X, batch_size=128, verbose=1):
+        proba = self.predict_proba(X, batch_size=batch_size, verbose=verbose)
+        if self.class_mode == "categorical":
+            return proba.argmax(axis=-1)
         else:
             return (proba>0.5).astype('int32')
 
-    def evaluate(self, X, y, batch_size=128):
+
+    def evaluate(self, X, y, batch_size=128, show_accuracy=False, verbose=1):
         y = standardize_y(y)
-        av_score = 0.
-        samples = 0
-        for batch_index in range(0, len(X)/batch_size+1):
-            batch = range(batch_index*batch_size, min(len(X), (batch_index+1)*batch_size))
-            if not batch:
-                break
-            score = self._test(X[batch], y[batch])
-            av_score += len(batch)*score
-            samples += len(batch)
-        return av_score/samples
+
+        if show_accuracy:
+            tot_acc = 0.
+        tot_score = 0.
+
+        batches = make_batches(len(X), batch_size)
+        progbar = Progbar(target=len(X))
+        for batch_index, (batch_start, batch_end) in enumerate(batches):
+            X_batch = X[batch_start:batch_end]
+            y_batch = y[batch_start:batch_end]
+
+            if show_accuracy:
+                loss, acc = self._test_with_acc(X_batch, y_batch)
+                tot_acc += acc
+            else:
+                loss = self._test(X_batch, y_batch)
+            tot_score += loss
+
+            if verbose:
+                if show_accuracy:
+                    progbar.update(batch_end, [('loss', loss), ('acc.', acc)])
+                else:
+                    progbar.update(batch_end, [('loss', loss)])
+
+        if show_accuracy:
+            return tot_score/len(batches), tot_acc/len(batches)
+        else:
+            return tot_score/len(batches)
+                
+
 
 
