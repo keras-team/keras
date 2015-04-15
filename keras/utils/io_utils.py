2635c32d4bf4e3e47d8120a4cf48ceb0b67c48c6
Joao Felipe Santos <joao.eel@gmail.com>
2015-04-15 17:26:41 -0400

    Added helper class to use data in HDF5 datasets

---
+++ keras/utils/io_utils.py
@@ -0,0 +1,52 @@
+import h5py, numpy
+from collections import defaultdict
+
+class HDF5Matrix:
+    
+    refs = defaultdict(int)
+
+    def __init__(self, datapath, dataset, start, end, normalizer=None):
+        if datapath not in self.refs.keys():
+            f = h5py.File(datapath)
+            self.refs[datapath] = f
+        else:
+            f = self.refs[datapath]
+        self.start = start
+        self.end = end
+        self.data = f[dataset]
+        self.normalizer = normalizer
+    
+    def __len__(self):
+        return self.end - self.start
+
+    def __getitem__(self, key):
+        if isinstance(key, slice):
+            if key.stop + self.start <= self.end:
+                idx = slice(key.start+self.start, key.stop + self.start)
+            else:
+                raise IndexError
+        elif isinstance(key, int):
+            if key + self.start < self.end:
+                idx = key+self.start
+            else:
+                raise IndexError
+        elif isinstance(key, numpy.ndarray):
+            if numpy.max(key) + self.start < self.end:
+                idx = (self.start + key).tolist()
+            else:
+                raise IndexError
+        elif isinstance(key, list):
+            if max(key) + self.start < self.end:
+                idx = map(lambda x: x + self.start, key)
+            else:
+                raise IndexError
+        if self.normalizer is not None:
+            return self.normalizer(self.data[idx])
+        else:
+            return self.data[idx]
+
+    @property
+    def shape(self):
+        return tuple([self.end - self.start, self.data.shape[1]])
+
+
