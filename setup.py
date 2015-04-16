03abbafe746917fd825039b70686284a2ae13cb7
João Felipe Santos <joao.eel@gmail.com>
2015-04-16 10:09:30 -0400

    Updated setup.py to use setuptools if it is available

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(name='Keras',
       version='0.0.1',
