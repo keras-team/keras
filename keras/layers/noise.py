38baf81243ef0b1930d44d909437c69350804a56
Michael Oliver <michael.d.oliver@gmail.com>
2015-05-04 12:06:21 -0700

    add Layer module dependency

--- keras/layers/noise.py
+++ keras/layers/noise.py
@@ -8,6 +8,7 @@ from .. import activations, initializations
 from ..regularizers import identity
 from ..utils.theano_utils import shared_zeros, floatX
 from ..utils.generic_utils import make_tuple
+from ..layers.core import Layer
 
 from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
 from six.moves import zip
