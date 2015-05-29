b2dc29213408b6dae3c3abf8c5a262211fd137cd
fchollet <francois.chollet@gmail.com>
2015-05-29 16:53:37 -0700

    Small doc fix

--- examples/mnist_nn.py
+++ examples/mnist_nn.py
@@ -22,8 +22,8 @@ np.random.seed(1337) # for reproducibility
 # the data, shuffled and split between tran and test sets
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-X_train=X_train.reshape(60000,784)
-X_test=X_test.reshape(10000,784)
+X_train = X_train.reshape(60000,784)
+X_test = X_test.reshape(10000,784)
 X_train = X_train.astype("float32")
 X_test = X_test.astype("float32")
 X_train /= 255
