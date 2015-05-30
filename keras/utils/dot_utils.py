912ab043238516773c804dbdf09fcd2fdde694b5
Victor Zhong <victor@victorzhong.com>
2015-05-29 23:49:22 -0700

    add comments, documentation

--- keras/utils/dot_utils.py
+++ keras/utils/dot_utils.py
@@ -10,6 +10,10 @@ class Grapher(object):
         self.class_counts = Counter()
 
     def get_name(self, model):
+        """
+        returns the name of the model instance. If model does not have a `name` attribute, then it will be assigned
+        a generic (and unique) identifier based on its class
+        """
         if hasattr(model, 'name'):
             return model.name
         clz = model.__class__.__name__
@@ -23,6 +27,9 @@ class Grapher(object):
         return t
 
     def add_model(self, model, graph, parent=None):
+        """
+        Recursively adds `model` and its components to the pydot graph
+        """
         this = self.get_name(model)
         if isinstance(model, Model):
             parent = self.add_edge(parent, this, graph)
@@ -36,6 +43,9 @@ class Grapher(object):
             return self.add_edge(parent, this, graph)
 
     def plot(self, model, to_file):
+        """
+        creates a graph visualizing the structure of `model` and writes it to `to_file`
+        """
         graph = pydot.Dot(graph_type='graph')
         self.add_model(model, graph)
         graph.write_png(to_file)
