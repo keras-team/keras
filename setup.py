b82ab804daf24d6091c449148a938083425155d3
fchollet <francois.chollet@gmail.com>
2015-06-13 17:26:18 -0700

    Update setup.py and setup.cfg for pypi release

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup
 from setuptools import find_packages
 
-setup(name='Keras',
-      version='0.0.1',
-      description='Theano-based Deep Learning',
-      author='Francois Chollet',
-      author_email='francois.chollet@gmail.com',
-      url='https://github.com/fchollet/keras',
-      license='MIT',
-      install_requires=['theano'],
-      packages=find_packages(),
+setup(name = 'Keras',
+      version = '0.1.0',
+      description = 'Theano-based Deep Learning library',
+      long_description = open('README.md').read(),
+      author = 'Francois Chollet',
+      author_email = 'francois.chollet@gmail.com',
+      url = 'https://github.com/fchollet/keras',
+      download_url = 'https://github.com/fchollet/keras/tarball/0.1.0',
+      license = 'MIT',
+      install_requires = ['theano'],
+      packages = find_packages(),
 )
