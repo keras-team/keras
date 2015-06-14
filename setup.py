ce209553792dcf5c640690269accbf1512d8c80f
fchollet <francois.chollet@gmail.com>
2015-06-13 17:54:33 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name = 'Keras',
-      version = '0.1.0',
+      version = '0.1.1',
       description = 'Theano-based Deep Learning library',
       author = 'Francois Chollet',
       author_email = 'francois.chollet@gmail.com',
       url = 'https://github.com/fchollet/keras',
-      download_url = 'https://github.com/fchollet/keras/tarball/0.1.0',
+      download_url = 'https://github.com/fchollet/keras/tarball/0.1.1',
       license = 'MIT',
       install_requires = ['theano'],
       packages = find_packages(),
