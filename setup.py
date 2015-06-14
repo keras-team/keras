fabfdb868eb5d6b032dcaa9f71844743e57f69ca
fchollet <francois.chollet@gmail.com>
2015-06-13 17:51:51 -0700

    Remove long_description from setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ from setuptools import find_packages
 setup(name = 'Keras',
       version = '0.1.0',
       description = 'Theano-based Deep Learning library',
-      long_description = open('README.md').read(),
       author = 'Francois Chollet',
       author_email = 'francois.chollet@gmail.com',
       url = 'https://github.com/fchollet/keras',
