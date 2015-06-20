1d586440a8649e0e0e0b19220040c4b210bcea1b
fchollet <francois.chollet@gmail.com>
2015-06-20 15:30:18 -0700

    Rewrite class_weight and sample_weight tests

--- keras/models.py
+++ keras/models.py
@@ -381,5 +381,3 @@ class Sequential(Model, containers.Sequential):
             weights = [g['param_{}'.format(p)] for p in range(g.attrs['nb_params'])]
             self.layers[k].set_weights(weights)
         f.close()
-
-
