97f23268c2567721b1aa858a286c30d739170eef
fchollet <francois.chollet@gmail.com>
2015-06-20 17:09:08 -0700

    Refactor test_lossweights into unit test

--- test/test_lossweights.py
+++ test/test_lossweights.py
@@ -5,6 +5,7 @@ from keras.models import Sequential
 from keras.layers.core import Dense, Activation
 from keras.utils import np_utils
 import numpy as np
+import unittest
 
 nb_classes = 10
 batch_size = 128
@@ -12,15 +13,15 @@ nb_epoch = 5
 weighted_class = 9
 standard_weight = 1
 high_weight = 5
-max_train_samples = 10000
-max_test_samples = 10000
+max_train_samples = 5000
+max_test_samples = 1000
 
 np.random.seed(1337) # for reproducibility
 
 # the data, shuffled and split between tran and test sets
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
-X_train = X_train.reshape(60000,784)[:max_train_samples]
-X_test = X_test.reshape(10000,784)[:max_test_samples]
+X_train = X_train.reshape(60000, 784)[:max_train_samples]
+X_test = X_test.reshape(10000, 784)[:max_test_samples]
 X_train = X_train.astype("float32") / 255
 X_test = X_test.astype("float32") / 255
 
@@ -45,27 +46,34 @@ def test_weights(model, class_weight=None, sample_weight=None):
     score = model.evaluate(X_test[test_ids, :], Y_test[test_ids, :], verbose=0)
     return score
 
-class_weight = dict([(i, standard_weight) for i in range(nb_classes)])
-class_weight[weighted_class] = high_weight
+class TestConcatenation(unittest.TestCase):
 
-sample_weight = np.ones((y_train.shape[0])) * standard_weight
-sample_weight[y_train == weighted_class] = high_weight
+    def test_loss_weighting(self):
+        class_weight = dict([(i, standard_weight) for i in range(nb_classes)])
+        class_weight[weighted_class] = high_weight
 
-for loss in ['mae', 'mse', 'categorical_crossentropy']:
-    print('loss:', loss)
-    # no weights: reference point
-    model = create_model()
-    model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
-    standard_score = test_weights(model)
-    # test class_weight
-    model = create_model()
-    model.compile(loss=loss, optimizer='rmsprop')
-    score = test_weights(model, class_weight=class_weight)
-    print('score:', score, ' vs.', standard_score)
-    assert(score < standard_score)
-    # test sample_weight
-    model = create_model()
-    model.compile(loss=loss, optimizer='rmsprop')
-    score = test_weights(model, sample_weight=sample_weight)
-    print('score:', score, ' vs.', standard_score)
-    assert(score < standard_score)
+        sample_weight = np.ones((y_train.shape[0])) * standard_weight
+        sample_weight[y_train == weighted_class] = high_weight
+
+        for loss in ['mae', 'mse', 'categorical_crossentropy']:
+            print('loss:', loss)
+            # no weights: reference point
+            model = create_model()
+            model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+            standard_score = test_weights(model)
+            # test class_weight
+            model = create_model()
+            model.compile(loss=loss, optimizer='rmsprop')
+            score = test_weights(model, class_weight=class_weight)
+            print('score:', score, ' vs.', standard_score)
+            self.assertTrue(score < standard_score)
+            # test sample_weight
+            model = create_model()
+            model.compile(loss=loss, optimizer='rmsprop')
+            score = test_weights(model, sample_weight=sample_weight)
+            print('score:', score, ' vs.', standard_score)
+            self.assertTrue(score < standard_score)
+
+if __name__ == '__main__':
+    print('Test class_weight and sample_weight')
+    unittest.main()
