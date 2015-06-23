88947c7fbe2539abd4e6592ec70f3196f91e4c34
sergsb <serg.sosnin@gmail.com>
2015-06-23 11:23:45 +0300

    Fixed spaces, names

--- keras/layers/convolutional.py
+++ keras/layers/convolutional.py
@@ -135,21 +135,21 @@ class Convolution2D(Layer):
 
 
 class MaxPooling2D(Layer):
-    def __init__(self, poolsize=(2, 2), ignore_border=True,subsample=None):
+    def __init__(self, poolsize=(2, 2), ignore_border=True, st=None):
         super(MaxPooling2D,self).__init__()
         self.input = T.tensor4()
         self.poolsize = poolsize
         self.ignore_border = ignore_border
-        self.subsample = subsample
+        self.st = st
     def get_output(self, train):
         X = self.get_input(train)
-        output = downsample.max_pool_2d(X, self.poolsize, ignore_border=self.ignore_border,st=self.subsample)
+        output = downsample.max_pool_2d(X, self.poolsize, ignore_border=self.ignore_border, st=self.st)
         return output
 
     def get_config(self):
         return {"name":self.__class__.__name__,
             "poolsize":self.poolsize,
-            "subsample":self.subsample,
+            "st":self.st,
             "ignore_border":self.ignore_border}
 
 
@@ -159,4 +159,3 @@ class MaxPooling2D(Layer):
 # class Convolution3D: TODO
 
 # class MaxPooling3D: TODO
-
