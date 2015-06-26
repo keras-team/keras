ba3be348daf414331326a43e8a97bbd9c43b9000
Pranav Shyam <pranavshyam13@gmail.com>
2015-06-26 12:29:26 +0530

    advanced activations now supports that

--- keras/layers/advanced_activations.py
+++ keras/layers/advanced_activations.py
@@ -2,13 +2,16 @@ from ..layers.core import Layer
 from ..utils.theano_utils import shared_zeros
 
 class LeakyReLU(Layer):
-    def __init__(self, alpha=0.3):
-        super(LeakyReLU,self).__init__()
+    def __init__(self, alpha=0.3, name=None, prev=None, input_dim=(None,)):
+        super(LeakyReLU,self).__init__(name, prev, input_dim)
         self.alpha = alpha
 
     def get_output(self, train):
         X = self.get_input(train)
         return ((X + abs(X)) / 2.0) + self.alpha * ((X - abs(X)) / 2.0)
+
+    def get_output_dim(self, input_dim):
+        return input_dim
 
     def get_config(self):
         return {"name":self.__class__.__name__,
@@ -17,15 +20,23 @@ class LeakyReLU(Layer):
 
 class PReLU(Layer):
     '''
-        Reference: 
+        Reference:
             Delving Deep into Rectifiers: Surpassing Human-Level Performance on ImageNet Classification
                 http://arxiv.org/pdf/1502.01852v1.pdf
     '''
-    def __init__(self, input_shape):
-        super(PReLU,self).__init__()
-        self.alphas = shared_zeros(input_shape)
-        self.params = [self.alphas]
+    def __init__(self, input_shape=None, name=None, prev=None, input_dim=(None,)):
+        super(PReLU,self).__init__(name, prev, input_dim)
         self.input_shape = input_shape
+
+    def setup(self):
+        if self.input_shape is None:
+            if self.input_dim == (None, ):
+                raise RuntimeError
+            else:
+                self.input_shape = self.input_dim
+        self.alphas = shared_zeros(self.input_shape)
+        self.params = [self.alphas]
+
 
     def get_output(self, train):
         X = self.get_input(train)
@@ -33,6 +44,9 @@ class PReLU(Layer):
         neg = self.alphas * ((X - abs(X)) / 2.0)
         return pos + neg
 
+    def get_output_dim(self, input_dim):
+        return input_dim
+
     def get_config(self):
         return {"name":self.__class__.__name__,
-        "input_shape":self.input_shape}
+        "input_dim":self.input_dim}
