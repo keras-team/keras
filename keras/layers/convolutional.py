ae40dba223ee46e07af8d3aee2560bfbcd8f6e24
Pranav Shyam <pranavshyam13@gmail.com>
2015-06-26 11:02:52 +0530

    Fixed Error(typo)
    MaxPooling 1D and 2D working fine

--- keras/layers/convolutional.py
+++ keras/layers/convolutional.py
@@ -150,7 +150,7 @@ class MaxPooling2D(Layer):
 
     def get_output(self, train):
         X = self.get_input(train)
-        output = downsample.max_pool_2d(X, ds=self.poolsize, ,st=self.stride, ignore_border=self.ignore_border)
+        output = downsample.max_pool_2d(X, ds=self.poolsize, st=self.stride, ignore_border=self.ignore_border)
         return output
 
     def get_config(self):
