15055483ff7046970fd9ca6cf6db108585a11fc2
Tennessee Leeuwenburg <tleeuwenburg@gmail.com>
2015-06-26 17:01:09 +1000

    Added a docstring to the linear activation function about input checking.
    Added some more unit tests

--- tests/auto/keras/test_activations.py
+++ tests/auto/keras/test_activations.py
@@ -1,3 +1,5 @@
+import math
+
 import keras
 import theano
 import theano.tensor as T
@@ -5,10 +7,21 @@ import theano.tensor as T
 import numpy
 
 def list_assert_equal(a, b, round_to=7):
+    '''
+    This will do a pairwise, rounded equality test across two lists of
+    numbers.
+    '''
     pairs = zip(a, b)
     for i, j in pairs:
         assert round(i, round_to) == round(j, round_to)
 
+def get_standard_values():
+    '''
+    These are just a set of floats used for testing the activation
+    functions, and are useful in multiple tests.
+    '''
+
+    return [0,0.1,0.5,0.9,1.0]
 
 def test_softmax():
 
@@ -26,7 +39,7 @@ def test_softmax():
     x = T.vector()
     exp = s(x)
     f = theano.function([x], exp)
-    test_values=[0,0.1,0.5,0.9,1.0]
+    test_values=get_standard_values()
 
     result = f(test_values)
     expected = softmax(test_values)
@@ -36,7 +49,52 @@ def test_softmax():
 
     list_assert_equal(result, expected)
 
+def test_relu():
+    '''
+    Relu implementation doesn't depend on the value being
+    a theano variable. Testing ints, floats and theano tensors.
+    '''
+
+    from keras.activations import relu as r
+
+    assert r(5) == 5
+    assert r(-5) == 0
+    assert r(-0.1) == 0
+    assert r(0.1) == 0.1
+
+    x = T.vector()
+    exp = r(x)
+    f = theano.function([x], exp)
+
+    test_values = get_standard_values()
+    result = f(test_values)
+
+    list_assert_equal(result, test_values) # because no negatives in test values
+
+
+def test_tanh():
+
+    from keras.activations import tanh as t
+    test_values = get_standard_values()
+
+    x = T.vector()
+    exp = t(x)
+    f = theano.function([x], exp)
+
+    result = f(test_values)
+    expected = [math.tanh(v) for v in test_values]
+
+    print(result)
+    print(expected)
+
+    list_assert_equal(result, expected)
+
+
 def test_linear():
+    '''
+    This function does no input validation, it just returns the thing
+    that was passed in.
+    '''
 
     from keras.activations import linear as l
 
