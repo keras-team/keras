d4cb76174f442709f56803b83e1d85614eb8f19a
Pranav Shyam <pranavshyam13@gmail.com>
2015-06-27 23:39:10 +0530

    Removed dimension inference in advanced activations

--- keras/layers/advanced_activations.py
+++ keras/layers/advanced_activations.py
@@ -2,16 +2,13 @@ from ..layers.core import Layer
 from ..utils.theano_utils import shared_zeros
 
 class LeakyReLU(Layer):
-    def __init__(self, alpha=0.3, name=None, prev=None, input_dim=(None,)):
+    def __init__(self, alpha=0.3, name=None, prev=None):
         super(LeakyReLU,self).__init__(name, prev, input_dim)
         self.alpha = alpha
 
     def get_output(self, train):
         X = self.get_input(train)
         return ((X + abs(X)) / 2.0) + self.alpha * ((X - abs(X)) / 2.0)
-
-    def get_output_dim(self, input_dim):
-        return input_dim
 
     def get_config(self):
         return {"name":self.__class__.__name__,
@@ -24,16 +21,10 @@ class PReLU(Layer):
             Delving Deep into Rectifiers: Surpassing Human-Level Performance on ImageNet Classification
                 http://arxiv.org/pdf/1502.01852v1.pdf
     '''
-    def __init__(self, input_shape=None, name=None, prev=None, input_dim=(None,)):
+    def __init__(self, input_shape, name=None, prev=None):
         super(PReLU,self).__init__(name, prev, input_dim)
         self.input_shape = input_shape
 
-    def setup(self):
-        if self.input_shape is None:
-            if self.input_dim == (None, ):
-                raise RuntimeError
-            else:
-                self.input_shape = self.input_dim
         self.alphas = shared_zeros(self.input_shape)
         self.params = [self.alphas]
 
@@ -43,9 +34,6 @@ class PReLU(Layer):
         pos = ((X + abs(X)) / 2.0)
         neg = self.alphas * ((X - abs(X)) / 2.0)
         return pos + neg
-
-    def get_output_dim(self, input_dim):
-        return input_dim
 
     def get_config(self):
         return {"name":self.__class__.__name__,
