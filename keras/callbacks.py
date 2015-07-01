560cb94519ad5a5eed3adc323a9e1ad3cf0690e9
fchollet <francois.chollet@gmail.com>
2015-06-30 17:59:34 -0700

    Callback refactor

--- keras/callbacks.py
+++ keras/callbacks.py
@@ -109,128 +109,113 @@ class BaseLogger(Callback):
             print('Epoch %d' % epoch)
             self.progbar = Progbar(target=self.params['nb_sample'], \
                 verbose=self.verbose)
-        self.current = 0
-        self.tot_loss = 0.
-        self.tot_acc = 0.
+        self.seen = 0
+        self.totals = {}
 
     def on_batch_begin(self, batch, logs={}):
-        if self.current < self.params['nb_sample']:
+        if self.seen < self.params['nb_sample']:
             self.log_values = []
 
     def on_batch_end(self, batch, logs={}):
         batch_size = logs.get('size', 0)
-        self.current += batch_size
+        self.seen += batch_size
 
-        loss = logs.get('loss')
-        self.log_values.append(('loss', loss))
-        self.tot_loss += loss * batch_size
-        if self.params['show_accuracy']:
-            accuracy = logs.get('accuracy')
-            self.log_values.append(('acc.', accuracy))
-            self.tot_acc += accuracy * batch_size
+        for k, v in logs.items():
+            self.log_values.append((k, v))
+            if k in self.totals:
+                self.totals[k] += v * batch_size
+            else:
+                self.totals[k] = v * batch_size
+
         # skip progbar update for the last batch; will be handled by on_epoch_end
-        if self.verbose and self.current < self.params['nb_sample']:
-            self.progbar.update(self.current, self.log_values)
+        if self.verbose and self.seen < self.params['nb_sample']:
+            self.progbar.update(self.seen, self.log_values)
 
     def on_epoch_end(self, epoch, logs={}):
-        self.log_values.append(('loss', self.tot_loss / self.current))
-        if self.params['show_accuracy']:
-            self.log_values.append(('acc.', self.tot_acc / self.current))
-        if self.params['do_validation']:
-            val_loss = logs.get('val_loss')
-            self.log_values.append(('val. loss', val_loss))
-            if self.params['show_accuracy']:
-                val_acc = logs.get('val_accuracy')
-                self.log_values.append(('val. acc.', val_acc))
-        self.progbar.update(self.current, self.log_values)
+        for k, v in self.totals.items():
+            self.log_values.append((k, self.totals[k] / self.seen))
+                
+        for k, v in logs.items():
+            self.log_values.append((k, v))
+        self.progbar.update(self.seen, self.log_values)
 
 
 class History(Callback):
 
     def on_train_begin(self, logs={}):
         self.epoch = []
-        self.loss = []
-        if self.params['show_accuracy']:
-            self.accuracy = []
-        if self.params['do_validation']:
-            self.validation_loss = []
-            if self.params['show_accuracy']:
-                self.validation_accuracy = []
+        self.history = {}
 
     def on_epoch_begin(self, epoch, logs={}):
         self.seen = 0
-        self.tot_loss = 0.
-        self.tot_accuracy = 0.
+        self.totals = {}
 
     def on_batch_end(self, batch, logs={}):
         batch_size = logs.get('size', 0)
         self.seen += batch_size
-        self.tot_loss += logs.get('loss', 0.) * batch_size
-        if self.params['show_accuracy']:
-            self.tot_accuracy += logs.get('accuracy', 0.) * batch_size
+        for k, v in logs.items():
+            if k in self.totals:
+                self.totals[k] += v * batch_size
+            else:
+                self.totals[k] = v * batch_size
 
     def on_epoch_end(self, epoch, logs={}):
-        val_loss = logs.get('val_loss')
-        val_acc = logs.get('val_accuracy')
         self.epoch.append(epoch)
-        self.loss.append(self.tot_loss / self.seen)
-        if self.params['show_accuracy']:
-            self.accuracy.append(self.tot_accuracy / self.seen)
-        if self.params['do_validation']:
-            self.validation_loss.append(val_loss)
-            if self.params['show_accuracy']:
-                self.validation_accuracy.append(val_acc)
+        for k, v in self.totals.items():
+            self.history[k] = v / self.seen
+
+        for k, v in logs.items():
+            self.history[k] = v
 
 
 class ModelCheckpoint(Callback):
-    def __init__(self, filepath, verbose=0, save_best_only=False):
+    def __init__(self, filepath, monitor='val_loss', verbose=0, save_best_only=False):
         super(Callback, self).__init__()
         
+        self.monitor
         self.verbose = verbose
         self.filepath = filepath
         self.save_best_only = save_best_only
-        self.loss = []
-        self.best_loss = np.Inf
-        self.val_loss = []
-        self.best_val_loss = np.Inf
+        self.best = np.Inf
 
     def on_epoch_end(self, epoch, logs={}):
-        if self.save_best_only and self.params['do_validation']:
-            cur_val_loss = logs.get('val_loss')
-            self.val_loss.append(cur_val_loss)
-            if cur_val_loss < self.best_val_loss:
-                if self.verbose > 0:
-                    print("Epoch %05d: validation loss improved from %0.5f to %0.5f, saving model to %s"
-                        % (epoch, self.best_val_loss, cur_val_loss, self.filepath))
-                self.best_val_loss = cur_val_loss
-                self.model.save_weights(self.filepath, overwrite=True)
+        if self.save_best_only:
+            current = logs.get(self.monitor)
+            if current is None:
+                warnings.warn("Can save best model only with %s available, skipping." % (self.monitor), RuntimeWarning)
             else:
-                if self.verbose > 0:
-                    print("Epoch %05d: validation loss did not improve" % (epoch))
-        elif self.save_best_only and not self.params['do_validation']:
-            warnings.warn("Can save best model only with validation data, skipping", RuntimeWarning)
-        elif not self.save_best_only:
+                if current < self.best:
+                    if self.verbose > 0:
+                        print("Epoch %05d: %s improved from %0.5f to %0.5f, saving model to %s"
+                            % (epoch, self.monitor, self.best, current, self.filepath))
+                    self.best = current
+                    self.model.save_weights(self.filepath, overwrite=True)
+                else:
+                    if self.verbose > 0:
+                        print("Epoch %05d: %s did not improve" % (epoch, self.monitor))
+        else:
             if self.verbose > 0:
                 print("Epoch %05d: saving model to %s" % (epoch, self.filepath))
             self.model.save_weights(self.filepath, overwrite=True)
 
 
 class EarlyStopping(Callback):
-    def __init__(self, patience=0, verbose=0):
+    def __init__(self, monitor='val_loss', patience=0, verbose=0):
         super(Callback, self).__init__()
 
+        self.monitor = monitor
         self.patience = patience
         self.verbose = verbose
-        self.best_val_loss = np.Inf
+        self.best = np.Inf
         self.wait = 0
 
     def on_epoch_end(self, epoch, logs={}):
-        if not self.params['do_validation']:
-            warnings.warn("Early stopping requires validation data!", RuntimeWarning)
-
-        cur_val_loss = logs.get('val_loss')
-        if cur_val_loss < self.best_val_loss:
-            self.best_val_loss = cur_val_loss
+        current = logs.get(self.monitor)
+        if current is None:
+            warnings.warn("Early stopping requires %s available!" % (self.monitor), RuntimeWarning)
+ 
+        if current < self.best:
+            self.best = current
             self.wait = 0
         else:
             if self.wait >= self.patience:
@@ -243,24 +228,29 @@ class EarlyStopping(Callback):
 class RemoteMonitor(Callback):
     def __init__(self, root='http://localhost:9000'):
         self.root = root
-        self.seen = 0
-        self.tot_loss = 0.
-        self.tot_accuracy = 0.
 
     def on_epoch_begin(self, epoch, logs={}):
         self.seen = 0
-        self.tot_loss = 0.
-        self.tot_accuracy = 0.
+        self.totals = {}
+        
 
     def on_batch_end(self, batch, logs={}):
         batch_size = logs.get('size', 0)
         self.seen += batch_size
-        self.tot_loss += logs.get('loss', 0.) * batch_size
-        if self.params['show_accuracy']:
-            self.tot_accuracy += logs.get('accuracy', 0.) * batch_size
+        for k, v in logs.items():
+            if k in self.totals:
+                self.totals[k] += v * batch_size
+            else:
+                self.totals[k] = v * batch_size
 
     def on_epoch_end(self, epoch, logs={}):
         import requests
-        logs['epoch'] = epoch
-        logs['loss'] = self.tot_loss / self.seen
-        r = requests.post(self.root + '/publish/epoch/end/', {'data':json.dumps(logs)})
+        send = {}
+        send['epoch'] = epoch
+
+        for k, v in self.totals.items():
+            send[k] = v / self.seen
+        for k, v in self.logs:
+            send[k] = v
+
+        r = requests.post(self.root + '/publish/epoch/end/', {'data':json.dumps(send)})
