47fd945cf1bf9435ae2009df3338723b438a49b0
Kheir Eddine FARFAR <kfarfar@gmail.com>
2015-07-04 15:16:52 +0100

    Add MAPE objective

--- keras/objectives.py
+++ keras/objectives.py
@@ -12,6 +12,9 @@ def mean_squared_error(y_true, y_pred):
 def mean_absolute_error(y_true, y_pred):
     return T.abs_(y_pred - y_true).mean(axis=-1)
 
+def mean_absolute_percentage_error(y_true, y_pred):
+    return T.abs_((y_true - y_pred) / y_true).mean() * 100
+
 def mean_squared_logarithmic_error(y_true, y_pred):
     return T.sqr(T.log(T.clip(y_pred, epsilon, np.inf) + 1.) - T.log(T.clip(y_true, epsilon, np.inf) + 1.)).mean(axis=-1)
 
@@ -38,6 +41,7 @@ def binary_crossentropy(y_true, y_pred):
 # aliases
 mse = MSE = mean_squared_error
 mae = MAE = mean_absolute_error
+mape = MAPE = mean_absolute_percentage_error
 msle = MSLE = mean_squared_logarithmic_error
 
 from .utils.generic_utils import get_from_module
