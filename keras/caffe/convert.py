9cb40c728ca44b8f6cc6f73aeafd71678ba96222
Pranav Shyam <pranavshyam13@gmail.com>
2015-07-07 00:12:56 +0530

    re-architected the interface and structuring

--- keras/caffe/convert.py
+++ keras/caffe/convert.py
@@ -1,9 +1,9 @@
 import caffe_pb2 as caffe
 import google.protobuf
-from converters import ConvertModel, ConvertWeights, ConvertSolver, ConvertMeanFile
+from converters import model_from_config, model_from_param, convert_weights, convert_solver, convert_meanfile
 
 class CaffeToKeras(object):
-	def __init__(self, prototext, caffemodel=None, solver=None, mean=None, phase='train'):
+	def __init__(self, prototext=None, caffemodel=None, solver=None, mean=None, phase='train'):
 		'''
 			prototext: model description file in caffe
 			caffemodel: stored weights file
@@ -13,7 +13,7 @@ class CaffeToKeras(object):
 
 			Usage:
 				model = CaffeToKeras(prototext='VGG16.prototxt', caffemodel='VGG16_700iter.caffemodel')
-				graph = model('network')
+				graph = model('network') # loaded with with weights is caffemodel is provided, else randomly initialized
 				weights = model('weights') # useful for embedding networks
 		'''
 		if phase == 'train':
@@ -27,26 +27,27 @@ class CaffeToKeras(object):
 
 			self.input_dim = config.input_dim
 			self.layers = config.layers[:]
-			self.network = ConvertModel(self.layers, self.phase, self.input_dim)
+			self.network = model_from_config(self.layers, self.phase, self.input_dim[1:])
 
 		if caffemodel is not None:
 			param = caffe.NetParameter()
 			param.MergeFromString(open(caffemodel,'rb').read())
 
+			self.weights = convert_weights(param.layers)
+
 			if prototext is None:
 				self.layers = param.layers[:]
-				self.network = ConvertModel(self.layers, self.phase, self.input_dim)
-
-			self.weights = ConvertWeights(param.layers)
-			self.network.copy_weights(self.weights)
+				self.network = model_from_param(self.layers)
+			else:
+				self.network.copy_weights(self.weights)
 
 		if solver is not None:
 			# parse and save
-			self.solver = ConvertSolver(solver)
+			self.solver = convert_solver(solver)
 
 		if mean is not None:
-			#parse and save
-			self.mean = ConvertMeanFile(mean)
+			# parse and save
+			self.mean = convert_meanfile(mean)
 
 	def __call__(self, item):
 		if item == 'network':
