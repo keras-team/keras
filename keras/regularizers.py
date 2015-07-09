ca22bb7db527bc72110c27cd5ccc2c3ac95ed598
Max Pumperla <max.pumperla@numberfour.eu>
2015-07-09 10:42:38 +0200

    Getter for regularizers

--- keras/regularizers.py
+++ keras/regularizers.py
@@ -69,3 +69,7 @@ def activity_l1l2(l1=0.01, l2=0.01):
     return ActivityRegularizer(l1=l1, l2=l2)
 
 identity = Regularizer
+
+from .utils.generic_utils import get_from_module
+def get(identifier):
+    return get_from_module(identifier, globals(), 'regularizer', instantiate=True)
