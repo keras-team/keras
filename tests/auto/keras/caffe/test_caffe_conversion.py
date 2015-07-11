525a329d72b434cf9079282384308745fe443783
Pranav Shyam <pranavshyam13@gmail.com>
2015-07-12 00:26:02 +0530

    added a param conversion test

--- tests/auto/keras/caffe/test_caffe_conversion.py
+++ tests/auto/keras/caffe/test_caffe_conversion.py
@@ -1,45 +1,52 @@
 from __future__ import print_function
 import unittest
 import numpy as np
-np.random.seed(1337)
-from keras.models import Graph, Sequential
-from keras.layers import containers
-from keras.layers.core import Dense, Activation
-from keras.utils.test_utils import get_test_data
+
+import os
 
 from keras.caffe.convert import CaffeToKeras
 
-X = np.random.random((100, 32))
-X2 = np.random.random((100, 32))
-y = np.random.random((100, 4))
-y2 = np.random.random((100, 4))
-
-(X_train, y_train), (X_test, y_test) = get_test_data(nb_train=1000, nb_test=200, input_shape=(32,),
-    classification=False, output_shape=(4,))
-(X2_train, y2_train), (X2_test, y2_test) = get_test_data(nb_train=1000, nb_test=200, input_shape=(32,),
-    classification=False, output_shape=(4,))
-
 class TestCaffeIntegration(unittest.TestCase):
-    def test_proto_non_deploy(self):
-        print('test a minimal inception module for correct topology')
+    def test_proto(self):
+        print('test a minimal inception module for correct topology construction from prototext')
         
+        #add fetching option here
         model = CaffeToKeras(prototext='./minimal_inception.prototxt')
-        assert(len(model('inputs')) == 1)
-        assert(len(model('outputs')) == 3)
-        assert(model.outputs == ['loss1', 'loss2', 'loss3'])
-        assert(model.inputs == ['data'])
+        assert(model('outputs') == ['loss1', 'loss2', 'loss3'])
+        assert(model('inputs') == ['data'])
 
         network = model('network')
         network.compile('rmsprop', {'loss1': 'mse', 'loss2': 'mse', 'loss3': 'mse'})
-
         datam = np.random.random((1, 3, 224, 224))
         outputs = network.predict({'data': datam})
+
         assert(len(outputs) == 3)
         assert(outputs['loss1'].shape == (1, 64, 112, 112))
         assert(outputs['loss2'].shape == (1, 192, 27, 27))
         assert(outputs['loss3'].shape == (1, 256, 27, 27))
 
+        network.get_config(verbose=1)
+
+
+    def test_param(self):
+        print('test a complete parameterized model for correct param conversion, grouped convolutions')
+        
+        #add fetching option here
+        model = CaffeToKeras(caffemodel='./hybridCNN_iter_700000_upgraded.caffemodel')
+        assert(model('outputs') == ['prob'])
+        assert(model('inputs') == ['conv1'])
+
+        network = model('network')
+        network.compile('rmsprop', {'prob': 'mse'})
+        datam = np.random.random((1, 3, 227, 227))
+        outputs = network.predict({'conv1': datam})
+
+        assert(len(outputs) == 1)
+        assert(outputs['prob'].shape == (1, 1183))
+
+        network.get_config(verbose=1)
+
 
 if __name__ == '__main__':
-    print('Test graph model')
+    print('Test caffe model conversion')
     unittest.main()
