200a0062620800d955f89c78ac95c143497bdc09
Max Pumperla <max.pumperla@numberfour.eu>
2015-07-15 17:05:09 +0200

    get_from_module available with additional dictionary arguments to initialize objects

--- keras/utils/generic_utils.py
+++ keras/utils/generic_utils.py
@@ -4,13 +4,15 @@ import time
 import sys
 import six
 
-def get_from_module(identifier, module_params, module_name, instantiate=False):
+def get_from_module(identifier, module_params, module_name, instantiate=False, kwargs=None):
     if isinstance(identifier, six.string_types):
         res = module_params.get(identifier)
         if not res:
             raise Exception('Invalid ' + str(module_name) + ': ' + str(identifier))
-        if instantiate:
+        if instantiate and not kwargs:
             return res()
+        elif instantiate and kwargs:
+            return res(**kwargs)
         else:
             return res
     return identifier
