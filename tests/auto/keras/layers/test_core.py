571448f5cd4ab2c30e2e18f75136f4b3f47cd80b
Thomas McColgan <thomas.mccolgan@gmail.com>
2015-07-17 12:38:17 +0200

    test connecting base layers

--- tests/auto/keras/layers/test_core.py
+++ tests/auto/keras/layers/test_core.py
@@ -25,6 +25,27 @@ class TestLayerBase(unittest.TestCase):
             assert_allclose(layer.get_input(train).eval(), input)
             assert_allclose(layer.get_output(train).eval(), input)
 
+    def test_connections(self):
+        nb_samples = 10
+        input_dim = 5
+        layer1 = core.Layer()
+        layer2 = core.Layer()
+
+        input = np.ones((nb_samples, input_dim))
+        layer1.input = theano.shared(value=input)
+
+        # As long as there is no previous layer, an error should be raised.
+        for train in [True, False]:
+            self.assertRaises(AttributeError, layer2.get_input, train)
+
+        # After connecting, input of layer1 should be passed through
+        layer2.set_previous(layer1)
+        for train in [True, False]:
+            assert_allclose(layer2.get_input(train).eval(), input)
+            assert_allclose(layer2.get_output(train).eval(), input)
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
