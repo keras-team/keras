67426a6fa3b7a078a8560d519dea777ba94e59be
Thomas McColgan <thomas.mccolgan@gmail.com>
2015-07-17 13:30:27 +0200

    Test the constructor, config and params functions of all core layers.

--- tests/auto/keras/layers/test_core.py
+++ tests/auto/keras/layers/test_core.py
@@ -45,6 +45,75 @@ class TestLayerBase(unittest.TestCase):
             assert_allclose(layer2.get_output(train).eval(), input)
 
 
+class TestConfigParams(unittest.TestCase):
+    """
+    Test the constructor, config and params functions of all layers in core.
+    """
+
+    def _runner(self, layer):
+        conf = layer.get_config()
+        assert (type(conf) == dict)
+
+        param = layer.get_params()
+        # Typically a list or a tuple, but may be any iterable
+        assert hasattr(param, '__iter__')
+
+    def test_base(self):
+        layer = core.Layer()
+        self._runner(layer)
+
+    def test_masked(self):
+        layer = core.MaskedLayer()
+        self._runner(layer)
+
+    def test_merge(self):
+        layer_1 = core.Layer()
+        layer_2 = core.Layer()
+        layer = core.Merge([layer_1, layer_2])
+        self._runner(layer)
+
+    def test_dropout(self):
+        layer = core.Dropout(0.5)
+        self._runner(layer)
+
+    def test_activation(self):
+        layer = core.Activation('linear')
+        self._runner(layer)
+
+    def test_reshape(self):
+        layer = core.Reshape(10, 10)
+        self._runner(layer)
+
+    def test_flatten(self):
+        layer = core.Flatten()
+        self._runner(layer)
+
+    def test_repeat_vector(self):
+        layer = core.RepeatVector(10)
+        self._runner(layer)
+
+    def test_dense(self):
+        layer = core.Dense(10, 10)
+        self._runner(layer)
+
+    def test_act_reg(self):
+        layer = core.ActivityRegularization(0.5, 0.5)
+        self._runner(layer)
+
+    def test_time_dist_dense(self):
+        layer = core.TimeDistributedDense(10, 10)
+        self._runner(layer)
+
+    def test_autoencoder(self):
+        layer_1 = core.Layer()
+        layer_2 = core.Layer()
+
+        layer = core.AutoEncoder(layer_1, layer_2)
+        self._runner(layer)
+
+    def test_maxout_dense(self):
+        layer = core.MaxoutDense(10, 10)
+        self._runner(layer)
 
 
 if __name__ == '__main__':
