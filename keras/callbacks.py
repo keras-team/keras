9b6ca59edc614e0dee5565a36a2b696dbdde56ef
Pablo Jadzinsky <pjadzinsky@gmail.com>
2015-07-20 10:20:34 -0700

    Added current training batch to model as a parameter in keras/models and created a callback to take snapshots of predictions as learning progresses

--- keras/callbacks.py
+++ keras/callbacks.py
@@ -261,3 +261,75 @@ class RemoteMonitor(Callback):
             send[k] = v
 
         r = requests.post(self.root + '/publish/epoch/end/', {'data':json.dumps(send)})
+
+class SnapshotPrediction(Callback):
+    def __init__(self, filepath, verbose=0):
+        super(Callback, self).__init__()
+        
+        self.verbose = verbose
+        self.filepath = filepath
+
+    def on_epoch_begin(self, epoch, logs={}):
+        # open hdf5 file to save predictions. On first epoch, if file is open it will be empty ('w').
+        # On any other epoch file is opened mode 'a'
+        import h5py
+        
+        filepath = self.filepath
+
+        try:
+            self.epoch=epoch    # will be used as dictionary key when saving predictions
+            if epoch==0:
+                # Open HDF5 for saving
+                import os.path
+
+                # if file exists and should not be overwritten
+                overwrite = False
+                if not overwrite and os.path.isfile(filepath):
+                    import sys
+                    get_input = input
+                    if sys.version_info[:2] <= (2, 7):
+                        get_input = raw_input
+                    overwrite = get_input('[WARNING] %s already exists - overwrite? [y/n]' % (filepath))
+                    while overwrite not in ['y', 'n']:
+                        overwrite = get_input('Enter "y" (overwrite) or "n" (cancel).')
+                    if overwrite == 'n':
+                        return
+                    print('[TIP] Next time specify overwrite=True in save_weights!')
+
+                    self.f = h5py.File(filepath, 'w')
+                else:
+                    self.f = h5py.File(filepath, 'w')
+            else:
+                self.f = h5py.File(filepath, 'a')
+
+        except:
+            self.f.close()
+
+    def on_batch_begin(self, batch, logs={}):
+        from keras.models import Sequential, Graph
+        try:
+            if batch==0:
+                X = self.model.batch
+                if type(self.model)==Graph:
+                    X = {name:value for (name, value) in zip(self.model.input_order, X)}
+                elif type(self.model)==Sequential:
+                    X = X[0]
+
+                predictions = self.model.predict(X)
+
+                # save to file
+                f = self.f
+                g = f.create_group(str(self.epoch))
+                if type(self.model)==Graph:
+                    for name in predictions:
+                        dset = g.create_dataset(name, predictions[name].shape, dtype=predictions[name].dtype)
+                        dset[:] = predictions[name]
+                elif type(self.model)==Sequential:
+                    # Check this works as expected
+                    dset = g.create_dataset('output', predictions.shape, dtype=predictions.dtype)
+                    dset[:] = predictions
+
+                f.flush()
+                f.close()
+        except:
+            self.f.close()
