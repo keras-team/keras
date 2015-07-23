1ad453f6d01d4007662fa63d59508d27bac029d5
Julien Rebetez <julien.rebetez@heig-vd.ch>
2015-07-23 16:46:35 +0200

    Add check to print_layer_shapes to fail explicitely on model used connected to other models.

--- keras/utils/model_utils.py
+++ keras/utils/model_utils.py
@@ -11,6 +11,16 @@ def print_layer_shapes(model, input_shape):
         model: An instance of models.Model
         input_shape: The shape of the input you will provide to the model.
     """
+    # This is to handle the case where a model has been connected to a previous
+    # layer (and therefore get_input would recurse into previous layer's
+    # output).
+    if hasattr(model.layers[0], 'previous'):
+        # TODO: If the model is used as a part of another model, get_input will
+        # return the input of the whole model and this won't work. So this is
+        # not handled yet
+        raise Exception("This function doesn't work on model used as subparts "
+                        " for other models")
+
     input_var = model.get_input(train=False)
     input_tmp = np.zeros(input_shape, dtype=np.float32)
     print("input shape : ", input_shape)
