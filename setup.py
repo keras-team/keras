dd3615ef89f0e075cec09422eb786a382ce10265
Pranav Shyam <pranavshyam13@gmail.com>
2015-07-24 15:07:50 +0530

    removed caffe_pb2.py dependancy

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 from setuptools import setup
 from setuptools import find_packages
 
+# for caffe support
+import os
+from six.moves.urllib.request import urlretrieve
+
+
 setup(name = 'Keras',
       version = '0.1.1',
       description = 'Theano-based Deep Learning library',
@@ -12,3 +17,15 @@ setup(name = 'Keras',
       install_requires = ['theano'],
       packages = find_packages(),
 )
+
+
+print 'setting up for caffe interfacing...' 
+# First, compile Caffe protobuf Python file
+datadir = os.path.expanduser(os.path.join('~', '.keras', 'data'))
+if not os.path.exists(datadir):
+    os.makedirs(datadir)
+
+caffe_source = os.path.join(datadir, 'caffe.proto')
+caffe_destination = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'keras', 'caffe')
+urlretrieve('https://raw.githubusercontent.com/BVLC/caffe/master/src/caffe/proto/caffe.proto', caffe_source)
+os.system('protoc --proto_path="' + datadir + '" --python_out="' + caffe_destination + '" "' + caffe_source + '"')
