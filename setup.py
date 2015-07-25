79eac6ac3b6eb9c73285b57772fee0e36922f7bc
fchollet <francois.chollet@gmail.com>
2015-07-25 13:08:10 +0900

    Add caffe compilation to setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,18 @@
 from setuptools import setup
 from setuptools import find_packages
 
+import os
+from six.moves.urllib.request import urlretrieve
+
+# First, compile Caffe protobuf Python file
+datadir = os.path.expanduser(os.path.join('~', '.keras', 'data'))
+if not os.path.exists(datadir):
+    os.makedirs(datadir)
+
+caffe_source = os.path.join(datadir, 'caffe.proto')
+caffe_destination = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'keras', 'caffe')
+urlretrieve('https://raw.githubusercontent.com/BVLC/caffe/master/src/caffe/proto/caffe.proto', caffe_source)
+os.system('protoc --proto_path="' + datadir + '" --python_out="' + caffe_destination + '" "' + caffe_source + '"')
 
 setup(name='Keras',
       version='0.1.2',
