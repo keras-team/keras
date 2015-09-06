84909a49c2c58d7cda893897c183067acc0794ef
Thomas McColgan <thomas.mccolgan@gmail.com>
2015-09-06 12:55:50 +0200

    add upsampling layer tests

--- tests/auto/keras/layers/test_convolutional.py
+++ tests/auto/keras/layers/test_convolutional.py
@@ -25,10 +25,12 @@ class TestConvolutions(unittest.TestCase):
                 for subsample_length in [1, 3]:
                     for W_regularizer in [None, 'l2']:
                         for b_regularizer in [None, 'l2']:
-                            layer = convolutional.Convolution1D(
-                                input_dim, nb_filter, filter_length, weights=weight,
-                                border_mode=border_mode, W_regularizer=W_regularizer,
-                                b_regularizer=b_regularizer, subsample_length=subsample_length)
+                            for act_regularizer in [None, 'l2']:
+                                layer = convolutional.Convolution1D(
+                                    input_dim, nb_filter, filter_length, weights=weight,
+                                    border_mode=border_mode, W_regularizer=W_regularizer,
+                                    b_regularizer=b_regularizer, activity_regularizer=act_regularizer,
+                                    subsample_length=subsample_length)
 
                             layer.input = theano.shared(value=input)
                             for train in [True, False]:
@@ -74,18 +76,20 @@ class TestConvolutions(unittest.TestCase):
                 for subsample in [(1, 1), (2, 3)]:
                     for W_regularizer in [None, 'l2']:
                         for b_regularizer in [None, 'l2']:
-                            layer = convolutional.Convolution2D(
-                                nb_filter, stack_size, nb_row, nb_col, weights=weight,
-                                border_mode=border_mode, W_regularizer=W_regularizer,
-                                b_regularizer=b_regularizer, subsample=subsample)
+                            for act_regularizer in [None, 'l2']:
+                                layer = convolutional.Convolution2D(
+                                    nb_filter, stack_size, nb_row, nb_col, weights=weight,
+                                    border_mode=border_mode, W_regularizer=W_regularizer,
+                                    b_regularizer=b_regularizer, activity_regularizer=act_regularizer,
+                                    subsample=subsample)
 
-                            layer.input = theano.shared(value=input)
-                            for train in [True, False]:
-                                out = layer.get_output(train).eval()
-                                if border_mode == 'same' and subsample == (1, 1):
-                                    assert out.shape[2:] == input.shape[2:]
+                                layer.input = theano.shared(value=input)
+                                for train in [True, False]:
+                                    out = layer.get_output(train).eval()
+                                    if border_mode == 'same' and subsample == (1, 1):
+                                        assert out.shape[2:] == input.shape[2:]
 
-                            config = layer.get_config()
+                                config = layer.get_config()
 
     def test_maxpooling_2d(self):
         nb_samples = 9
@@ -123,6 +127,42 @@ class TestConvolutions(unittest.TestCase):
             assert_allclose(out[:, :, 2:-2, 2:-2], 1.)
 
         config = layer.get_config()
+
+    def test_upsample_1d(self):
+        nb_samples = 9
+
+        nb_steps = 7
+        input_dim = 10
+
+        input = np.ones((nb_samples, nb_steps, input_dim))
+        for length in [2,3,9]:
+            layer = convolutional.UpSample1D(length=length)
+            layer.input = theano.shared(value=input)
+            for train in [True, False]:
+                out = layer.get_output(train).eval()
+                assert out.shape[1] == length*nb_steps
+
+            config = layer.get_config()
+
+    def test_upsample_2d(self):
+        nb_samples = 9
+
+        stack_size = 7
+        input_nb_row = 11
+        input_nb_col = 12
+
+        input = np.ones((nb_samples, stack_size, input_nb_row, input_nb_col))
+
+        for length_row in [2,3,9]:
+            for length_col in [2,3,9]:
+                layer = convolutional.UpSample2D(size=(length_row,length_col))
+                layer.input = theano.shared(value=input)
+                for train in [True, False]:
+                    out = layer.get_output(train).eval()
+                    assert out.shape[2] == length_row*input_nb_row
+                    assert out.shape[3] == length_col*input_nb_col
+
+            config = layer.get_config()
 
 if __name__ == '__main__':
     unittest.main()
