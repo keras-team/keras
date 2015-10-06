1df4a3a5b098a111cea8c648795e215517ce8cd7
Marco Unternaehrer <marco.unter@gmail.com>
2015-10-06 14:10:28 +0200

    draft of SpatialBatchNormalization

--- examples/cifar10_cnn.py
+++ examples/cifar10_cnn.py
@@ -26,7 +26,7 @@ from six.moves import range
 batch_size = 32
 nb_classes = 10
 nb_epoch = 200
-data_augmentation = True
+data_augmentation = False
 
 # input image dimensions
 img_rows, img_cols = 32, 32
@@ -34,6 +34,7 @@ img_rows, img_cols = 32, 32
 img_channels = 3
 
 # the data, shuffled and split between tran and test sets
+print("loading data")
 (X_train, y_train), (X_test, y_test) = cifar10.load_data()
 print('X_train shape:', X_train.shape)
 print(X_train.shape[0], 'train samples')
@@ -43,6 +44,7 @@ print(X_test.shape[0], 'test samples')
 Y_train = np_utils.to_categorical(y_train, nb_classes)
 Y_test = np_utils.to_categorical(y_test, nb_classes)
 
+print("creating model")
 model = Sequential()
 
 model.add(Convolution2D(32, 3, 3, border_mode='full',
@@ -67,6 +69,7 @@ model.add(Dropout(0.5))
 model.add(Dense(nb_classes))
 model.add(Activation('softmax'))
 
+print("compiling model")
 # let's train the model using SGD + momentum (how original).
 sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd)
