74b590eff8f111e38704353ca97781ce0db40e57
Francois Chollet <francois.chollet@gmail.com>
2015-10-24 20:39:06 -0700

    Whitespace / style fixes only

--- examples/imdb_cnn_lstm.py
+++ examples/imdb_cnn_lstm.py
@@ -18,35 +18,32 @@ from keras.datasets import imdb
 
     GPU command:
         THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32 python imdb_lstm.py
-        
+
     Get to 0.8498 test accuracy after 2 epochs. 41s/epoch on K520 GPU.
 '''
 
-
-#Parameters
-
-#Embedding
+# Embedding
 max_features = 20000
-maxlen = 100 
+maxlen = 100
 embedding_size = 128
 
-#Convolution
+# Convolution
 filter_length = 3
 nb_filter = 64
 pool_length = 2
 
-#LSTM
+# LSTM
 lstm_output_size = 70
 
-#Training
+# Training
 batch_size = 30
 nb_epoch = 2
+
 '''
 Note:
 batch_size is highly sensitive.
-Only 2 epoches are done as the dataset is too small.
+Only 2 epochs are needed as the dataset is very small.
 '''
-
 
 print("Loading data...")
 (X_train, y_train), (X_test, y_test) = imdb.load_data(nb_words=max_features, test_split=0.2)
@@ -64,23 +61,24 @@ print('Build model...')
 model = Sequential()
 model.add(Embedding(max_features, embedding_size, input_length=maxlen))
 model.add(Dropout(0.25))
-
 model.add(Convolution1D(nb_filter=nb_filter,
                         filter_length=filter_length,
                         border_mode="valid",
                         activation="relu",
                         subsample_length=1))
-
 model.add(MaxPooling1D(pool_length=pool_length))
-
-model.add(LSTM(lstm_output_size)) 
+model.add(LSTM(lstm_output_size))
 model.add(Dense(1))
 model.add(Activation('sigmoid'))
 
-model.compile(loss='binary_crossentropy', optimizer='adam', class_mode="binary")
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              class_mode="binary")
 
 print("Train...")
-model.fit(X_train, y_train, batch_size=batch_size, nb_epoch=nb_epoch, validation_data=(X_test, y_test), show_accuracy=True)
-score, acc = model.evaluate(X_test, y_test, batch_size=batch_size, show_accuracy=True)
+model.fit(X_train, y_train, batch_size=batch_size, nb_epoch=nb_epoch,
+          validation_data=(X_test, y_test), show_accuracy=True)
+score, acc = model.evaluate(X_test, y_test, batch_size=batch_size,
+                            show_accuracy=True)
 print('Test score:', score)
 print('Test accuracy:', acc)
