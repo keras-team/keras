7432034ada203e42b9725086e662789c9b9b0a5d
Stephen Roller <roller@cs.utexas.edu>
2015-11-02 16:35:31 -0600

    TimeDistributedMerge needed to be imported into layer_utils, so it could be serialized to to_json

--- keras/utils/layer_utils.py
+++ keras/utils/layer_utils.py
@@ -6,7 +6,7 @@ import copy
 
 from ..layers.advanced_activations import LeakyReLU, PReLU
 from ..layers.core import Dense, Merge, Dropout, Activation, Reshape, Flatten, RepeatVector, Layer, AutoEncoder, Masking, Permute
-from ..layers.core import ActivityRegularization, TimeDistributedDense, AutoEncoder, MaxoutDense
+from ..layers.core import ActivityRegularization, TimeDistributedDense, TimeDistributedMerge, AutoEncoder, MaxoutDense
 from ..layers.convolutional import Convolution1D, Convolution2D, MaxPooling1D, MaxPooling2D, ZeroPadding2D
 from ..layers.embeddings import Embedding, WordContextProduct
 from ..layers.noise import GaussianNoise, GaussianDropout
