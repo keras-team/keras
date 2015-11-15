5ed913da1108b63c69d48d30b395ae35e576dc9f
Francois Chollet <francois.chollet@gmail.com>
2015-11-15 15:01:58 -0800

    Convert constraints, initialization, activations

--- keras/activations.py
+++ keras/activations.py
@@ -1,33 +1,29 @@
 from __future__ import absolute_import
-import theano.tensor as T
+from . import backend as K
 
 
 def softmax(x):
-    return T.nnet.softmax(x.reshape((-1, x.shape[-1]))).reshape(x.shape)
-
-
-def time_distributed_softmax(x):
-    return softmax(x)
+    return K.softmax(x)
 
 
 def softplus(x):
-    return T.nnet.softplus(x)
+    return K.softplus(x)
 
 
-def relu(x):
-    return T.nnet.relu(x)
+def relu(x, alpha=0., max_value=None):
+    return K.relu(x, alpha=alpha, max_value=max_value)
 
 
 def tanh(x):
-    return T.tanh(x)
+    return K.tanh(x)
 
 
 def sigmoid(x):
-    return T.nnet.sigmoid(x)
+    return K.sigmoid(x)
 
 
 def hard_sigmoid(x):
-    return T.nnet.hard_sigmoid(x)
+    return K.hard_sigmoid(x)
 
 
 def linear(x):
