c600a4450c9bbc716a0cfacff6b61e78853dbc52
Max Pumperla <max.pumperla@googlemail.com>
2015-12-05 18:36:28 +0100

    Removed GPL test part

--- keras/layers/convolutional.py
+++ keras/layers/convolutional.py
@@ -591,29 +591,3 @@ class ZeroPadding2D(Layer):
                   "padding": self.padding}
         base_config = super(ZeroPadding2D, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
-
-
-
-class GlobalPooling2D(Layer):
-    """
-    By @entron
-    Borrowed and modified from here: https://github.com/fchollet/keras/pull/522
-    """
-    def __init__(self, pooling_function='average'):
-        super(GlobalPooling2D, self).__init__()
-        if pooling_function not in {'average', 'max'}:
-            raise Exception('Invalid pooling function for GlobalPooling2D:', pooling_function)
-        if pooling_function == 'average':
-            self.pooling_function = K.mean
-        else:
-            self.pooling_function = K.max
-        self.input = K.placeholder(ndim=4)
-
-    def get_output(self, train):
-        X = self.get_input(train)
-        return self.pooling_function(self.pooling_function(X, axis=-1), axis=-1)
-
-    def get_config(self):
-        return {"name": self.__class__.__name__,
-                "pooling_function": self.pooling_function.__name__}
-
