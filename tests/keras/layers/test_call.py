90e9da4093e357dc49d5631d2792221092516854
EderSantana <eder@macbook.com>
2015-12-04 20:30:36 -0500

    Fix tensorflow test

--- tests/keras/layers/test_call.py
+++ tests/keras/layers/test_call.py
@@ -5,12 +5,11 @@ from __future__ import print_function
 
 import unittest
 import numpy as np
-import theano
 
 from numpy.testing import assert_allclose
 
 from keras import backend as K
-from keras.layers.core import Layer, Dense
+from keras.layers.core import Dense
 from keras.models import Sequential
 floatX = K.common._FLOATX
 
@@ -20,15 +19,16 @@ class TestCall(unittest.TestCase):
 
     def test_layer_call(self):
         """Test keras.layers.core.Layer.__call__"""
-        nb_samples, input_dim = 3, 10
-        layer = Layer()
+        nb_samples, input_dim, output_dim = 3, 10, 5
+        layer = Dense(output_dim, input_dim=input_dim)
+        W = np.asarray(K.eval(layer.W))
         X = K.placeholder(ndim=2)
         Y = layer(X)
-        F = K.function([X], Y)
+        F = K.function([X], [Y])
 
         x = np.random.randn(nb_samples, input_dim).astype(floatX)
-        y = F([x, ])
-        assert_allclose(x, y)
+        y = F([x])[0]
+        assert_allclose(np.dot(x, W), y)
 
     def test_sequential_call(self):
         """Test keras.models.Sequential.__call__"""
@@ -39,15 +39,14 @@ class TestCall(unittest.TestCase):
 
         X = K.placeholder(ndim=2)
         Y = model(X)
-        F = K.function([X], Y)
+        F = K.function([X], [Y])
 
         x = np.random.randn(nb_samples, input_dim).astype(floatX)
-        y1 = F([x, ])
+        y1 = F([x])[0]
         y2 = model.predict(x)
         # results of __call__ should match model.predict
         assert_allclose(y1, y2)
 
 
 if __name__ == '__main__':
-    theano.config.exception_verbosity = 'high'
     unittest.main(verbosity=2)
