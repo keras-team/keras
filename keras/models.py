6a38cc2e27e7217c110523b2e9b4ff0dee1b4cbe
isaacgerg <isaacgerg@users.noreply.github.com>
2015-12-09 15:14:59 -0500

    added print method

--- keras/models.py
+++ keras/models.py
@@ -585,6 +585,11 @@ class Sequential(Model, containers.Sequential):
             weights = [g['param_{}'.format(p)] for p in range(g.attrs['nb_params'])]
             self.layers[k].set_weights(weights)
         f.close()
+        
+    def print_layer_shapes(self):
+        print('{:>30}{:>30}'.format('Input','Output'))
+        for k in self.layers:
+            print('{:>30}{:>30}'.format(str(k.input_shape[1:]), str(k.output_shape[1:])))            
 
 
 class Graph(Model, containers.Graph):
