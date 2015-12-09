02b40ccfe3cc237e54cb6ce386ca8823cfd58195
Max Pumperla <max.pumperla@googlemail.com>
2015-12-09 14:49:17 +0100

    activations unit to pytest

--- tests/keras/test_activations.py
+++ tests/keras/test_activations.py
@@ -1,4 +1,4 @@
-import unittest
+import pytest
 from keras import backend as K
 import numpy as np
 from numpy.testing import assert_allclose
@@ -12,85 +12,81 @@ def get_standard_values():
     return np.array([[0, 0.1, 0.5, 0.9, 1.0]], dtype=K.floatx())
 
 
-class TestActivations(unittest.TestCase):
+# Reference sigmoid, numerically stable
+def ref_sigmoid(x):
+    if x >= 0:
+        return 1 / (1 + np.exp(-x))
+    else:
+        z = np.exp(x)
+        return z / (1 + z)
+vec_sigmoid = np.vectorize(ref_sigmoid)
 
-    def test_softmax(self):
-        from keras.activations import softmax as s
+def test_softmax():
+    from keras.activations import softmax as s
 
-        # Test using a reference implementation of softmax
-        def softmax(values):
-            m = np.max(values)
-            e = np.exp(values - m)
-            return e / np.sum(e)
+    # Test using a reference implementation of softmax
+    def softmax(values):
+        m = np.max(values)
+        e = np.exp(values - m)
+        return e / np.sum(e)
 
-        x = K.placeholder(ndim=2)
-        exp = s(x)
-        f = K.function([x], [exp])
-        test_values = get_standard_values()
+    x = K.placeholder(ndim=2)
+    exp = s(x)
+    f = K.function([x], [exp])
+    test_values = get_standard_values()
 
-        result = f([test_values])[0]
-        expected = softmax(test_values)
-        assert_allclose(result, expected, rtol=1e-05)
+    result = f([test_values])[0]
+    expected = softmax(test_values)
+    assert_allclose(result, expected, rtol=1e-05)
 
-    def test_sigmoid(self):
-        from keras.activations import sigmoid
+def test_sigmoid():
+    from keras.activations import sigmoid
 
-        # Reference sigmoid
-        def ref_sigmoid(x):
-            if x >= 0:
-                return 1 / (1 + np.exp(-x))
-            else:
-                return z / (1 + np.exp(x))
-        vec_sigmoid = np.vectorize(ref_sigmoid)
+    x = K.placeholder(ndim=2)
+    sig_out = sigmoid(x)
+    f = K.function([x], [sig_out])
+    test_values = get_standard_values()
 
-        x = K.placeholder(ndim=2)
-        sig_out = sigmoid(x)
-        f = K.function([x], [sig_out])
-        test_values = get_standard_values()
+    result = f([test_values])[0]
+    expected = vec_sigmoid(test_values)
+    assert_allclose(result, expected, rtol=1e-05)
 
-        result = f([test_values])[0]
-        expected = vec_sigmoid(test_values)
-        assert_allclose(result, expected, rtol=1e-05)
+def test_relu():
+    '''
+    Relu implementation doesn't depend on the value being
+    a theano variable. Testing ints, floats and theano tensors.
+    '''
+    from keras.activations import relu as r
 
-    def test_relu(self):
-        '''
-        Relu implementation doesn't depend on the value being
-        a theano variable. Testing ints, floats and theano tensors.
-        '''
-        from keras.activations import relu as r
+    x = K.placeholder(ndim=2)
+    exp = r(x)
+    f = K.function([x], [exp])
 
-        x = K.placeholder(ndim=2)
-        exp = r(x)
-        f = K.function([x], [exp])
+    test_values = get_standard_values()
+    result = f([test_values])[0]
 
-        test_values = get_standard_values()
-        result = f([test_values])[0]
+    # because no negatives in test values
+    assert_allclose(result, test_values, rtol=1e-05)
 
-        # because no negatives in test values
-        assert_allclose(result, test_values, rtol=1e-05)
+def test_tanh():
+    from keras.activations import tanh as t
+    test_values = get_standard_values()
 
-    def test_tanh(self):
-        from keras.activations import tanh as t
-        test_values = get_standard_values()
+    x = K.placeholder(ndim=2)
+    exp = t(x)
+    f = K.function([x], [exp])
 
-        x = K.placeholder(ndim=2)
-        exp = t(x)
-        f = K.function([x], [exp])
+    result = f([test_values])[0]
+    expected = np.tanh(test_values)
+    assert_allclose(result, expected, rtol=1e-05)
 
-        result = f([test_values])[0]
-        expected = np.tanh(test_values)
-        assert_allclose(result, expected, rtol=1e-05)
+def test_linear():
+    '''
+    This function does no input validation, it just returns the thing
+    that was passed in.
+    '''
+    from keras.activations import linear as l
 
-    def test_linear(self):
-        '''
-        This function does no input validation, it just returns the thing
-        that was passed in.
-        '''
-        from keras.activations import linear as l
-
-        xs = [1, 5, True, None, 'foo']
-        for x in xs:
-            assert x == l(x)
-
-if __name__ == '__main__':
-    unittest.main()
+    xs = [1, 5, True, None, 'foo']
+    for x in xs:
+        assert x == l(x)
