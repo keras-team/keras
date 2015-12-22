9a5b934f669b96345b74198d6f79b967bb774622
Thomas McColgan <thomas.mccolgan@gmail.com>
2015-12-22 13:42:04 +0100

    split out theano only part

--- tests/keras/layers/test_core.py
+++ tests/keras/layers/test_core.py
@@ -180,7 +180,7 @@ def _runner(layer):
     layer.trainable = True
     layer.trainable = False
 
-def test_siamese():
+def test_siamese_all():
     right_input_layer = core.Dense(7, input_dim=3)
     left_input_layer = core.Dense(7, input_dim=3)
 
@@ -190,14 +190,19 @@ def test_siamese():
         siamese_layer.output_shape
         siamese_layer.get_output()
 
-    # Merge modes 'dot' and 'cos' requires a different call signature
+@pytest.mark.skipif(K._BACKEND == 'tensorflow',
+                    reason='currently not working with TensorFlow')
+def test_siamese_theano_only():
+    right_input_layer = core.Dense(7, input_dim=3)
+    left_input_layer = core.Dense(7, input_dim=3)
+
+    shared_layer = core.Dense(5,input_dim=7)
+
     for mode in ['dot', 'cos']:
         siamese_layer = core.Siamese(shared_layer, [left_input_layer, right_input_layer], merge_mode=mode,
                                      dot_axes=([1], [1]))
         siamese_layer.output_shape
         siamese_layer.get_output()
-
-
 
 if __name__ == '__main__':
     pytest.main([__file__])
