d8e83cc773a0f7bd7ed550c784bacda4fa4da53b
cyc <chenchiapply@gmail.com>
2015-12-21 22:33:35 -0500

    Fix the wrong link
    Fix the wrong link for "Towards AI-Complete Question Answering: A Set of Prerequisite Toy Tasks"

--- examples/babi_memnn.py
+++ examples/babi_memnn.py
@@ -3,7 +3,7 @@
 References:
 - Jason Weston, Antoine Bordes, Sumit Chopra, Tomas Mikolov, Alexander M. Rush,
   "Towards AI-Complete Question Answering: A Set of Prerequisite Toy Tasks",
-  http://arxiv.org/abs/1503.08895
+  http://arxiv.org/abs/1502.05698
 
 - Sainbayar Sukhbaatar, Arthur Szlam, Jason Weston, Rob Fergus,
   "End-To-End Memory Networks",
