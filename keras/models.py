fc39fa490aa9d9745aab87eba83c94c1e3416313
Steven Xu <xxu@student.unimelb.edu.au>
2016-01-07 03:00:40 +1100

    support model saving/loading

--- keras/models.py
+++ keras/models.py
@@ -158,11 +158,16 @@ def model_from_config(config, custom_objects={}):
         optimizer_name = optimizer_params.pop('name')
         optimizer = optimizers.get(optimizer_name, optimizer_params)
 
+        output_ndim = config.get('output_ndim')
+        output_dtype = config.get('output_dtype')
+
         if model_name == 'Sequential':
             model.compile(loss=loss, optimizer=optimizer,
-                          class_mode=class_mode)
+                          class_mode=class_mode,
+                          output_ndim=output_ndim, output_dtype=output_dtype)
         elif model_name == 'Graph':
-            model.compile(loss=loss, optimizer=optimizer)
+            model.compile(loss=loss, optimizer=optimizer,
+                          output_ndim=output_ndim, output_dtype=output_dtype)
     return model
 
 
@@ -339,6 +344,10 @@ class Model(object):
                 config['loss'] = dict([(k, get_function_name(v)) for k, v in self.loss.items()])
             else:
                 config['loss'] = get_function_name(self.loss)
+        if hasattr(self, 'output_ndim'):
+            config['output_ndim'] = self.output_ndim
+        if hasattr(self, 'output_dtype'):
+            config['output_dtype'] = self.output_dtype
 
         if verbose:
             pp = pprint.PrettyPrinter(indent=4)
@@ -411,6 +420,8 @@ class Sequential(Model, containers.Sequential):
         self.y_test = self.get_output(train=False)
 
         # target of model
+        self.output_ndim = output_ndim
+        self.output_dtype = output_dtype
         if output_ndim is None:
             output_ndim = K.ndim(self.y_train)
         if output_dtype == "float":
@@ -1068,6 +1079,9 @@ class Graph(Model, containers.Graph):
                                              train_loss)
         updates += self.updates
         self.loss = loss
+
+        self.output_ndim = output_ndim
+        self.output_dtype = output_dtype
 
         self._train = K.function(train_ins, [train_loss], updates=updates)
         self._test = K.function(test_ins, [test_loss])
