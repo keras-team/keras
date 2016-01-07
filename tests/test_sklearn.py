8477f892c13a224fb0018307a8864cb63d0b4fac
Laia <joao.laia@siemens.com>
2016-01-07 16:09:56 +0100

    Removed tests that require sklearn package.

--- tests/test_sklearn.py
+++ tests/test_sklearn.py
@@ -4,10 +4,6 @@ from keras.models import Sequential
 from keras.optimizers import SGD
 from keras.wrappers.scikit_learn import KerasClassifier
 
-import sklearn
-from sklearn.ensemble import BaggingClassifier
-from sklearn.grid_search import GridSearchCV
-
 import numpy as np
 
 # Data generation
@@ -41,12 +37,6 @@ def test_fit():
     clf = get_wrapped_model(outputs=2, loss='categorical_crossentropy', epochs=2)
     clf.fit(X, y)
 
-def test_clone():
-    clf = get_wrapped_model(outputs=2, loss='categorical_crossentropy', epochs=1)
-    clf.fit(X, y)
-    cloned = sklearn.base.clone(clf)
-    assert cmp(clf.get_params().keys(), cloned.get_params().keys()) == 0
-    assert cloned._compiled_model is None
 
 def test_score_binary():
     # We will test the score function, and make sure the classes_ are not being changed
@@ -57,21 +47,10 @@ def test_score_binary():
     score = clf.score(X, np.ones(X.shape[0])*y[0])
     assert len(clf.classes_) == 2
     
+
 def test_score_categorical():
     # We will test the score function, and make sure the classes_ are not being changed
     clf = get_wrapped_model(outputs=2, loss='categorical_crossentropy', epochs=2)
     clf.fit(X, y)
     score = clf.score(X, y)
     assert len(clf.classes_) == 2
-    
-def test_bagging():
-    clf = get_wrapped_model(outputs=2, loss='categorical_crossentropy', epochs=2)
-    nnbag_clf = BaggingClassifier(base_estimator=clf, n_estimators=2)
-    nnbag_clf.fit(X, y)
-    nnbag_clf.predict_proba(X)
-
-def test_GridSearchCV():
-    clf = get_wrapped_model(outputs=2, loss='categorical_crossentropy', epochs=2)
-    grid_params = {'nb_epoch': [1,2]}
-    clf_gs = GridSearchCV(clf, param_grid=grid_params, cv=2, verbose=10)
-    
