Fix masking in RNNs

keras/backend/tensorflow_backend.py
@@ -57,11 +57,11 @@ def ones(shape, dtype=_FLOATX, name=None):
 
 
 def ones_like(x, name=None):
-    return tf.ones_like(x)
+    return tf.ones_like(x, name=name)
 
 
 def zeros_like(x, name=None):
-    return tf.zeros_like(x)
+    return tf.zeros_like(x, name=name)
 
 
 def count_params(x):
@@ -213,6 +213,10 @@ def clip(x, min_value, max_value):
 
 def equal(x, y):
     return tf.equal(x, y)
+
+
+def not_equal(x, y):
+    return tf.not_equal(x, y)
 
 
 def maximum(x, y):
@@ -384,8 +388,8 @@ def gradients(loss, variables):
 
 # CONTROL FLOW
 
-def rnn(step_function, inputs, initial_states,
-        go_backwards=False, masking=True):
+def rnn(step_function, inputs, output_dim, initial_states,
+        go_backwards=False, mask=None):
     '''Iterates over the time dimension of a tensor.
 
     Parameters
@@ -402,15 +406,16 @@ def rnn(step_function, inputs, initial_states,
             output: tensor with shape (samples, ...) (no time dimension),
             new_states: list of tensors, same length and shapes
                 as 'states'.
+    output_dim:
+        Number of output dimensions (for tensorflow back-end can safely be set
+        to None, it will be inferred automatically)
     initial_states: tensor with shape (samples, ...) (no time dimension),
         containing the initial values for the states used in
         the step function.
     go_backwards: boolean. If True, do the iteration over
         the time dimension in reverse order.
-    masking: boolean. If true, any input timestep inputs[s, i]
-        that is all-zeros will be skipped (states will be passed to
-        the next step unchanged) and the corresponding output will
-        be all zeros.
+    mask: binary tensor with shape (samples, time, 1), with a zero for every element
+        that is masked.
 
     Returns
     -------
@@ -424,32 +429,52 @@ def rnn(step_function, inputs, initial_states,
     '''
     inputs = tf.transpose(inputs, (1, 0, 2))
     input_list = tf.unpack(inputs)
+    if mask is None:
+        mask = ones_like(tf.slice(inputs, [0, 0, 0], [-1, -1, 1]))
+        inputs_shape = inputs.get_shape()
+
+        # TODO: the mask's shape should be automatically inferred, by
+        # tensorflow yet for some reason it fails to in some test-cases. This
+        # fixes the issue, but should be removed in future.
+        mask.set_shape([inputs_shape[0].value, inputs_shape[1].value, 1])
+        mask = tf.cast(mask, tf.bool)
+    else:
+        # Transpose not supported by bool tensor types, hence round-trip to uint8.
+        mask = tf.cast(tf.transpose(tf.cast(mask, tf.uint8), (1, 0, 2)), tf.bool)
+
+    mask_list = tf.unpack(mask)
 
     states = initial_states
     successive_states = []
     successive_outputs = []
     if go_backwards:
         input_list.reverse()
-    for input in input_list:
-        output, new_states = step_function(input, states)
-        if masking:
-            # for now we raise an exception because tf.reduce_any will not work
-            raise Exception("Masking is Theano-only for the time being.")
 
-            # if all-zero input timestep, return
-            # all-zero output and unchanged states
-            switch = tf.reduce_any(input)
-            output = tf.python.control_flow_ops.cond(switch,
-                                                     lambda: output,
-                                                     lambda: 0. * output)
-            return_states = []
-            for state, new_state in zip(states, new_states):
-                return_states.append(tf.python.control_flow_ops.cond(switch,
-                                                                     lambda: new_state,
-                                                                     lambda: state))
-            states = return_states
+    for input, mask_t in zip(input_list, mask_list):
+        output, new_states = step_function(input, states)
+
+        # tf.select needs its condition tensor to be the same shape as its two
+        # result tensors, but in our case the condition (mask) tensor is
+        # (nsamples, 1), and A and B are (nsamples, ndimensions). So we need to
+        # broadcast the mask to match the shape of A and B. That's what the
+        # tile call does, is just repeat the mask along its second dimension
+        # ndimensions times.
+        tiled_mask_t = tf.tile(mask_t, tf.pack([1, tf.shape(output)[1]]))
+
+        if len(successive_outputs) == 0:
+            prev_output = zeros_like(output)
         else:
-            states = new_states
+            prev_output = successive_outputs[-1]
+
+        output = tf.select(tiled_mask_t, output, prev_output)
+
+        return_states = []
+        for state, new_state in zip(states, new_states):
+            # (see earlier comment for tile explanation)
+            tiled_mask_t = tf.tile(mask_t, tf.pack([1, tf.shape(new_state)[1]]))
+            return_states.append(tf.select(tiled_mask_t, new_state, state))
+
+        states = return_states
         successive_outputs.append(output)
         successive_states.append(states)
 
@@ -467,6 +492,7 @@ def switch(condition, then_expression, else_expression):
     return tf.python.control_flow_ops.cond(condition,
                                            lambda: then_expression,
                                            lambda: else_expression)
+
 
 
 # NN OPERATIONS