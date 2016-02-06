7e6fc2b4dd055f3600b15a973253a3be876f4ac5
Fariz Rahman <farizrahman4u@gmail.com>
2016-02-07 03:07:07 +0530

    Update core.py

--- keras/layers/core.py
+++ keras/layers/core.py
@@ -1784,7 +1784,7 @@ def add_shared_layer(layer, inputs):
     for m in inputs:
         if len(m.layers) == 0:
             from keras.layers.embeddings import Embedding
-            if hasattr(layer, 'input') and str(layer.input.type) == 'TensorType(int32, matrix)':
+            if issubclass(layer.__class__, Embedding):
 
                 class IntInput(Layer):
 
