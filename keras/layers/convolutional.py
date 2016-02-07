c450089de92b7bcc909e94e9d8cd79a960c4d303
Francois Chollet <francois.chollet@gmail.com>
2016-02-07 15:42:18 -0800

    Documentation improvements

--- keras/layers/convolutional.py
+++ keras/layers/convolutional.py
@@ -422,7 +422,7 @@ class MaxPooling1D(_Pooling1D):
 class AveragePooling1D(_Pooling1D):
     '''Average pooling for temporal data.
 
-        # Input shape
+    # Input shape
         3D tensor with shape: `(samples, steps, features)`.
 
     # Output shape
