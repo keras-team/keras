6279544dc306f9eff6debb2b17b9e278a69b8ced
Francois Chollet <francois.chollet@gmail.com>
2016-02-09 13:57:16 -0800

    0.3.2 PyPI release

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import find_packages
 
 
 setup(name='Keras',
-      version='0.3.1',
-      description='Theano-based Deep Learning library',
+      version='0.3.2',
+      description='Deep Learning for Python',
       author='Francois Chollet',
       author_email='francois.chollet@gmail.com',
       url='https://github.com/fchollet/keras',
-      download_url='https://github.com/fchollet/keras/tarball/0.3.1',
+      download_url='https://github.com/fchollet/keras/tarball/0.3.2',
       license='MIT',
       install_requires=['theano', 'pyyaml', 'six'],
       extras_require={
