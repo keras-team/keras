0b209a12dcf1cf595d65b1c3593ba6f984110bfa
Fariz Rahman <farizrahman4u@gmail.com>
2016-02-24 17:04:13 +0530

    Update tensorflow_backend.py

--- keras/backend/tensorflow_backend.py
+++ keras/backend/tensorflow_backend.py
@@ -33,6 +33,8 @@ def placeholder(shape=None, ndim=None, dtype=_FLOATX, name=None):
             shape = [None for _ in range(ndim)]
     return tf.placeholder(dtype, shape=shape, name=name)
 
+def dtype(x):
+    return x.dtype.name
 
 def shape(x):
     return x.get_shape()
