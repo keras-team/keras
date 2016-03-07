342696eff584bcbeca32e5ed1341244990a74614
Florian <fgolemo@gmail.com>
2016-03-07 12:48:56 +0100

    added tiny part of function doc

--- keras/models.py
+++ keras/models.py
@@ -468,7 +468,7 @@ class Model(object):
         model_summary(self)
 
     def reset(self):
-        ''' Reset all weights and biases to random values
+        ''' Reset all weights and biases to random values, then recompiles
 
         Returns: Model
         '''
