bdd70d06d3590828deeaa6e41c08bba1e6c74abc
Francois Chollet <francois.chollet@gmail.com>
2016-03-31 11:15:44 -0700

    Prepare 0.3.3 release (last release before 1.0).

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import find_packages
 
 
 setup(name='Keras',
-      version='0.3.2',
+      version='0.3.3',
       description='Deep Learning for Python',
       author='Francois Chollet',
       author_email='francois.chollet@gmail.com',
       url='https://github.com/fchollet/keras',
-      download_url='https://github.com/fchollet/keras/tarball/0.3.2',
+      download_url='https://github.com/fchollet/keras/tarball/0.3.3',
       license='MIT',
       install_requires=['theano', 'pyyaml', 'six'],
       extras_require={
