f85f74769bae1f1a8ac5760c923d9dea52fef78c
EderSantana <eder@macbook.com>
2016-04-03 21:59:47 -0400

    Fix backend batch_dot tests

--- tests/keras/backend/test_backends.py
+++ tests/keras/backend/test_backends.py
@@ -43,7 +43,7 @@ class TestBackend(object):
         check_two_tensor_operation('dot', (4, 2), (5, 2, 3))
 
         check_two_tensor_operation('batch_dot', (4, 2, 3), (4, 5, 3),
-                                   axes=((2,), (2,)))
+                                   axes=(2, 2))
         check_single_tensor_operation('transpose', (4, 2))
 
     def test_shape_operations(self):
