57ea065db795d8451ad5fbdf84e8b262276d8690
Thomas Boquet <thomas.boquet@hec.ca>
2016-04-13 21:00:49 -0400

    Added learning phase to callbacks (#2297) (#2303)
    * added learning phase to callbacks (#2297)
    
    * cleaned imports
    
    * replaced tabs by spaces
    
    * added case where uses_learning_phase is False
    
    * fixed pep8 blank line bug

--- keras/callbacks.py
+++ keras/callbacks.py
@@ -467,8 +467,14 @@ class TensorBoard(Callback):
             if epoch % self.histogram_freq == 0:
                 # TODO: implement batched calls to sess.run
                 # (current call will likely go OOM on GPU)
-                feed_dict = dict(zip(self.model.inputs,
-                                     self.model.validation_data))
+                if self.model.uses_learning_phase:
+                    cut_v_data = len(self.model.inputs)
+                    val_data = self.model.validation_data[:cut_v_data] + [0]
+                    tensors = self.model.inputs + [K.learning_phase()]
+                else:
+                    val_data = self.model.validation_data
+                    tensors = self.model.inputs
+                feed_dict = dict(zip(tensors, val_data))
                 result = self.sess.run([self.merged], feed_dict=feed_dict)
                 summary_str = result[0]
                 self.writer.add_summary(summary_str, epoch)
