5a3e196b0e3235d26b38002c6b398d31d12f4902
Brian McMahan <braingineer@users.noreply.github.com>
2016-04-18 21:50:16 -0400

    Update theano_backend.py
    moved the constants None check to outside the mask check (per the automated build failure)

--- keras/backend/theano_backend.py
+++ keras/backend/theano_backend.py
@@ -557,15 +557,16 @@ def rnn(step_function, inputs, initial_states,
 
     axes = [1, 0] + list(range(2, ndim))
     inputs = inputs.dimshuffle(axes)
-
+    if constants is None:
+        constants = []
+        
     if mask is not None:
         if mask.ndim == ndim-1:
             mask = expand_dims(mask)
         assert mask.ndim == ndim
         mask = mask.dimshuffle(axes)
 
-        if constants is None:
-            constants = []
+
 
         if unroll:
             indices = list(range(input_length))
