43bf70d51ad1bf2858d2f6783ace58ef46c6ce9e
Fariz Rahman <farizrahman4u@gmail.com>
2016-04-20 14:44:18 +0530

    Create test_ops.py

---
+++ tests/keras/test_ops.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import
+from __future__ import print_function
+import pytest
+import os
+import numpy as np
+np.random.seed(1337)
+
+from keras import backend as K
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation, Merge, Lambda
+
+
+
+
+
+if __name__ == '__main__':
+    pytest.main([__file__])
