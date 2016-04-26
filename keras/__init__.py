bfbdbb05bc349257e0ac0eef186f7bdb91a89e17
Francois Chollet <francois.chollet@gmail.com>
2016-04-26 13:02:11 -0700

    Add root imports

--- keras/__init__.py
+++ keras/__init__.py
@@ -1,1 +1,17 @@
+from __future__ import absolute_import
 __version__ = '1.0.1'
+from . import backend
+from . import datasets
+from . import engine
+from . import layers
+from . import preprocessing
+from . import utils
+from . import wrappers
+from . import callbacks
+from . import constraints
+from . import initializations
+from . import metrics
+from . import models
+from . import objectives
+from . import optimizers
+from . import regularizers
