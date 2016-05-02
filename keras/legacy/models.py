c478f9b03225c5cff350c0be95d842b584aeaaba
Brian McMahan <braingineer@users.noreply.github.com>
2016-05-02 17:29:26 -0400

    pep fix
    I think that's going to be the only pep complain..

--- keras/legacy/models.py
+++ keras/legacy/models.py
@@ -473,7 +473,7 @@ class Graph(Model):
         x = self._get_x(data)
         output_list = super(Graph, self).predict(x, batch_size=batch_size,
                                                  verbose=verbose)
-        if not isinstance(output_list,list):
+        if not isinstance(output_list, list):
             output_list = [output_list]
         return dict(zip(self._graph_outputs, output_list))
 
@@ -530,7 +530,7 @@ class Graph(Model):
 
     def predict_on_batch(self, data):
         output_list = super(Graph, self).predict_on_batch(data)
-        if not isinstance(output_list,list):
+        if not isinstance(output_list, list):
             output_list = [output_list]
         return dict(zip(self._graph_outputs, output_list))
 
