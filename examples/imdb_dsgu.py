6933889e5e02ca5c1e3623c7e0640f4f37ef8972
Yuan Gao <gaoyuankidult@gmail.com>
2016-05-08 22:41:46 +0300

    Fixed typos imdb_dsgu.py file.

--- examples/imdb_dsgu.py
+++ examples/imdb_dsgu.py
@@ -1,16 +1,4 @@
-'''Trains a LSTM on the IMDB sentiment classification task.
-
-The dataset is actually too small for LSTM to be of any advantage
-compared to simpler, much faster methods such as TF-IDF+LogReg.
-
-Notes:
-
-- RNNs are tricky. Choice of batch size is important,
-choice of loss and optimizer is critical, etc.
-Some configurations won't converge.
-
-- LSTM loss decrease patterns during training can be quite different
-from what you see with CNNs/MLPs/etc.
+'''Trains a DSGU on the IMDB sentiment classification task.
 '''
 from __future__ import print_function
 import numpy as np
