0abb8e88d7f4cc78636ecd817c833d4dc13d1e5b
dieuwkehupkes <dieuwkehupkes@gmail.com>
2016-05-11 12:02:04 +0200

    Update documentation docstring Embedding
    From the documentation it is not entirely clear that if mask_zero is set
    to True, the input_dim argument should be equal to the size of the
    vocabulary + 2, as index 0 cannot be used anymore.
    
    (This behaviour seems a bit strange, as it has as a consequence that the
    first column of the weights of the embeddings will never be used or
    updated. The resulting network thus has a redundant set of parameters).

--- keras/layers/embeddings.py
+++ keras/layers/embeddings.py
@@ -17,7 +17,7 @@ class Embedding(Layer):
       model = Sequential()
       model.add(Embedding(1000, 64, input_length=10))
       # the model will take as input an integer matrix of size (batch, input_length).
-      # the largest integer (i.e. word index) in the input should be no larger than 1000 (vocabulary size).
+      # the largest integer (i.e. word index) in the input should be no larger than 999 (vocabulary size).
       # now model.output_shape == (None, 10, 64), where None is the batch dimension.
 
       input_array = np.random.randint(1000, size=(32, 10))
@@ -28,7 +28,7 @@ class Embedding(Layer):
     ```
 
     # Arguments
-      input_dim: int >= 0. Size of the vocabulary, ie.
+      input_dim: int > 0. Size of the vocabulary, ie.
           1 + maximum integer index occurring in the input data.
       output_dim: int >= 0. Dimension of the dense embedding.
       init: name of initialization function for the weights
@@ -46,6 +46,8 @@ class Embedding(Layer):
           This is useful for [recurrent layers](recurrent.md) which may take
           variable length input. If this is `True` then all subsequent layers
           in the model need to support masking or an exception will be raised.
+          If mask_zero is set to True, as a consequence, index 0 cannot be
+          used in the vocabulary (input_dim should equal |vocabulary| + 2).
       input_length: Length of input sequences, when it is constant.
           This argument is required if you are going to connect
           `Flatten` then `Dense` layers upstream
