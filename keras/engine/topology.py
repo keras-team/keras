2f8acfe4bf340ccae58ade4cbfddb0513fa1d4dc
Xingdi (Eric) Yuan <eric.yuan@maluuba.com>
2016-05-19 15:40:06 -0400

    changeable print_summary (#2761)
    * use changeable print_summary
    
    * minor

--- keras/engine/topology.py
+++ keras/engine/topology.py
@@ -2400,7 +2400,7 @@ class Container(Layer):
         import yaml
         return yaml.dump(self._updated_config(), **kwargs)
 
-    def summary(self):
+    def summary(self, line_length=100, positions=[.33, .55, .67, 1.]):
         from keras.utils.layer_utils import print_summary
 
         if hasattr(self, 'flattened_layers'):
@@ -2409,7 +2409,7 @@ class Container(Layer):
         else:
             flattened_layers = self.layers
 
-        print_summary(flattened_layers, getattr(self, 'container_nodes', None))
+        print_summary(flattened_layers, getattr(self, 'container_nodes', None), line_length=line_length, positions=positions)
 
 
 def get_source_inputs(tensor, layer=None, node_index=None):
