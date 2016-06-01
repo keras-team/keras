cefd06bcf4142a340ad45ec3aa17d91686abc348
sallamander <ssall@alumni.nd.edu>
2016-05-31 21:00:41 -0500

    Update ImageDataGenerator to allow channelwise standardization

--- tests/keras/preprocessing/test_image.py
+++ tests/keras/preprocessing/test_image.py
@@ -41,6 +41,7 @@ class TestImage:
                 samplewise_center=True,
                 featurewise_std_normalization=True,
                 samplewise_std_normalization=True,
+                channelwise=True, 
                 zca_whitening=True,
                 rotation_range=90.,
                 width_shift_range=0.1,
@@ -90,6 +91,69 @@ class TestImage:
                 assert ((potentially_flipped_x == x).all() or
                         (potentially_flipped_x == flip_axis(x, col_index)).all())
 
+    def test_standardize(self): 
+        eps = 1e-5
+
+        for test_images in self.all_test_images:
+            img_list = []
+            for im in test_images:
+                img_list.append(img_to_array(im)[None, ...])
+
+            imgs = np.vstack(img_list)
+            for dim_ordering in ('tf', 'th'):
+                if dim_ordering == 'th': 
+                    imgs = imgs.swapaxes(1, 3)
+                generator = ImageDataGenerator(
+                    featurewise_center=True,
+                    samplewise_center=False,
+                    featurewise_std_normalization=True,
+                    samplewise_std_normalization=False,
+                    channelwise=False, 
+                    zca_whitening=False,
+                    rotation_range=0,
+                    width_shift_range=0,
+                    height_shift_range=0,
+                    shear_range=0,
+                    zoom_range=0,
+                    channel_shift_range=0,
+                    horizontal_flip=False,
+                    vertical_flip=False,
+                    dim_ordering=dim_ordering)
+                generator.fit(imgs)
+
+                eps = 1e-5
+
+                standardized_imgs = np.array([generator.standardize(img) for img in imgs])
+                assert np.all(np.abs(standardized_imgs.mean(axis=0)) < eps)
+                assert np.all(np.abs(standardized_imgs.std(axis=0) - 1) < eps)
+
+                generator.channelwise = True 
+                generator.fit(imgs)
+                collapse_axis = (0, generator.row_index, generator.col_index)
+
+                standardized_imgs = np.array([generator.standardize(img) for img in imgs])
+                assert np.all(np.abs(standardized_imgs.mean(axis=collapse_axis)) < eps)
+                assert np.all(np.abs(standardized_imgs.std(axis=collapse_axis) - 1) < eps)
+
+                generator.featurewise_center = False
+                generator.featurewise_std_normalization = False
+                generator.samplewise_center = True
+                generator.samplewise_std_normalization = True
+                generator.channelwise = False
+                generator.fit(imgs)
+
+                collapse_axis = generator.channel_index - 1
+                standardized_img = generator.standardize(imgs[0])
+                assert np.all(np.abs(standardized_img.mean(axis=collapse_axis)) < eps)
+                assert np.all(np.abs(standardized_img.std(axis=collapse_axis) - 1) < eps)
+
+                generator.channelwise = True 
+                generator.fit(imgs)
+
+                collapse_axis = (generator.row_index - 1, generator.col_index - 1)
+                standardized_img = generator.standardize(imgs[0])
+                assert np.all(np.abs(standardized_img.mean(axis=collapse_axis)) < eps)
+                assert np.all(np.abs(standardized_img.std(axis=collapse_axis) - 1) < eps)
 
 if __name__ == '__main__':
     pytest.main([__file__])
