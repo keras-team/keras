1f735a5ce7c63256ecd685ede40ee623168fadde
talpay <tay.alpay@gmail.com>
2016-06-02 11:13:34 +0200

    KLD: Clip at 1

--- keras/objectives.py
+++ keras/objectives.py
@@ -49,8 +49,8 @@ def binary_crossentropy(y_true, y_pred):
 
 
 def kullback_leibler_divergence(y_true, y_pred):
-    y_true = K.clip(y_true, K.epsilon(), 1-K.epsilon())
-    y_pred = K.clip(y_pred, K.epsilon(), 1-K.epsilon())
+    y_true = K.clip(y_true, K.epsilon(), 1)
+    y_pred = K.clip(y_pred, K.epsilon(), 1)
     return K.sum(y_true * K.log(y_true / y_pred), axis=-1)
 
 
