8f6951c54063f0c153da58243351490b3d68b528
Jack Hessel <jmhessel@gmail.com>
2016-07-04 00:14:55 -0400

    added configuration function, comments, did a bit of cleaning

--- keras/layers/advanced_activations.py
+++ keras/layers/advanced_activations.py
@@ -266,17 +266,44 @@ class SReLU(Layer):
         return dict(list(base_config.items()) + list(config.items()))
 
 class HierarchicalSoftmax(Layer):
+    '''Two-layer Hierarchical Softmax layer. Provides an approximate
+    softmax that is much faster to compute in cases where there are a
+    large number (~10K+) of classes.
 
-    def __init__(self, per_top, total_class, **kwargs):
+    # Input shape
+        A list of two tensors:
+           - The first tensor should have shape (nb_samples, dim) and represents the input feature vector
+           - The second tensor should have shape (nb_samples,), have integer type, and represent the
+           labels of each training example.
+
+    # Output shape
+        1D Tensor with shape (nb_samples,) representing the negative log probability of the correct class
+
+    # Arguments
+        total_class: How many classes the hierarchical softmax is over
+        per_top: How many classes per top level code (should be on the order of the square root of the total number of classes)
+
+    # References
+    - [Classes for Fast Maximum Entropy Training](http://arxiv.org/pdf/cs/0108006.pdf)
+    - [Hierarchical Probabilistic Neural Network Language Model](http://www.iro.umontreal.ca/~lisa/pointeurs/hierarchical-nnlm-aistats05.pdf)
+    - [Strategies for Training Large Vocabulary Neural Language Models](http://arxiv.org/pdf/1512.04906)
+
+    '''
+    def __init__(self, total_class, per_top = None, **kwargs):
+        if per_top is None:
+            per_top = int(np.ceil(np.sqrt(total_class)))
         self.per_top = per_top
-        self.n_top_level = int(np.ceil(total_class * 1. / self.per_top))
+        self.total_cass = total_class
+
+        self.n_top_level = int(np.ceil(self.total_class * 1. / self.per_top))
         self.n_second_level = self.n_top_level * self.per_top
         
         self.class_to_all_top = np.reshape(np.array([item for sublist in
-                                                     [[i for _ in range(per_top)]
+                                                     [[i for _ in range(self.per_top)]
                                                       for i in range(self.n_top_level)]
                                                      for item in sublist]),
                                            (self.n_second_level, 1))
+
         self.top_to_all_second = np.reshape(np.arange(self.n_second_level),
                                             (self.n_top_level, per_top))
         
@@ -288,21 +315,22 @@ class HierarchicalSoftmax(Layer):
         super(HierarchicalSoftmax, self).__init__(**kwargs)
 
     def build(self, input_shape):
+
         input_dim = input_shape[0][1]
 
+        #Some matrices that provide required mappings into the weight matrices
         self.class_to_top = K.variable(self.class_to_all_top, dtype = np.int32)
         self.top_to_second = K.variable(self.top_to_all_second, dtype = np.int32)
         self.class_to_second = K.variable(self.class_to_all_second, dtype = np.int32)
 
+        #A few identity matrices we need in memory for indexing purposes
         self.identity_constant_top = K.eye(self.n_top_level)
         self.identity_constant_second = K.eye(self.per_top)
-        
-        self.top_level_weights = K.variable(np.random.random((self.n_top_level,
-                                                              input_dim)))
+
+        self.top_level_weights = K.variable(np.random.random((self.n_top_level,input_dim)))
         self.top_level_biases = K.zeros((self.n_top_level,))
 
-        self.second_level_weights = K.variable(np.random.random((self.n_second_level,
-                                                                 input_dim)))
+        self.second_level_weights = K.variable(np.random.random((self.n_second_level,input_dim)))
         self.second_level_biases = K.zeros((self.n_second_level,))
 
         
@@ -320,8 +348,7 @@ class HierarchicalSoftmax(Layer):
         
     def call(self, inputs, mask=None):
         if type(inputs) is not list or len(inputs) != 2:
-            raise Exception('HierarchicalSoftmax must be called on a list')
-                            #'ofGot: ' + str(inputs))
+            raise Exception('HierarchicalSoftmax must be called on a list of two tensors, got: ' + str(inputs))
         input_vecs, labels = inputs
 
         #first: get the prob of the top level class for all inputs
@@ -331,11 +358,10 @@ class HierarchicalSoftmax(Layer):
 
         #second: for each label, record the probability of the correct top class
         batch_tops = K.gather(self.class_to_top, labels)
-        top_index_mat = K.squeeze(K.squeeze(K.gather(self.identity_constant_top,
-                                           K.gather(self.class_to_top, labels)),1),1)
+        top_index_mat = K.squeeze(K.squeeze(K.gather(self.identity_constant_top, batch_tops),1),1)
         batch_top_probs = K.max(K.minimum(top_probs, top_index_mat), 1)
 
-        #third: get the (batch, per_second_level) matrix of indices
+        #third: get the (batch, per_second_level) matrix of indices, so we know which weights to use
         second_level_indices = K.squeeze(K.squeeze(K.gather(self.top_to_second, batch_tops),1),1)
 
         #fourth: compute the second level probabilities
@@ -344,8 +370,8 @@ class HierarchicalSoftmax(Layer):
         second_level_selected_biases = K.gather(self.second_level_biases,
                                        second_level_indices)
 
-        input_vecs = K.expand_dims(input_vecs, 1)
-        second_logprobs = K.squeeze(K.batch_dot(input_vecs, second_level_selected_weights),1)
+        second_logprobs = K.squeeze(K.batch_dot(K.expand_dims(input_vecs,1),
+                                                second_level_selected_weights),1)
         second_logprobs += second_level_selected_biases
         second_probs = K.softmax(second_logprobs)
 
@@ -354,11 +380,16 @@ class HierarchicalSoftmax(Layer):
                                               K.gather(self.class_to_second, labels)),1)
         batch_second_probs = K.max(K.minimum(second_probs, second_index_mat), 1)
 
-        #now we can finally output the log probability of each class
-        batch_top_probs = K.log(batch_top_probs)
-        batch_second_probs = K.log(batch_second_probs)
-        ret_tensor = - batch_top_probs - batch_second_probs 
-        return ret_tensor
+        #now we can output the negative log probability of the correct class for each input
+        batch_top_probs = -K.log(batch_top_probs)
+        batch_second_probs = -K.log(batch_second_probs)
+        return batch_top_probs + batch_second_probs
         
     def get_output_shape_for(self, input_shape):
         return (input_shape[0][0],1)
+
+    def get_config(self):
+        config = {'total_class' = self.total_class
+                  'per_top' = self.per_top}
+        base_config = super(HierarchicalSoftmax, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
