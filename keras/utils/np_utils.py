2ad81d68a37e2e1cb5c14c373245203fc0106537
EderSantana <eder@macbook.com>
2016-07-14 14:10:58 -0700

    stop gradients

--- keras/utils/np_utils.py
+++ keras/utils/np_utils.py
@@ -110,16 +110,13 @@ def convert_kernel(kernel, dim_ordering='th'):
     return new_kernel
 
 
-def conv_output_length(input_length, filter_size, border_mode, stride, dilation=1, transpose=False):
+def conv_output_length(input_length, filter_size, border_mode, stride, dilation=1):
     if input_length is None:
         return None
     assert border_mode in {'same', 'valid'}
-    dilated_filter_size = filter_size + (filter_size -1) * (dilation - 1)
+    dilated_filter_size = filter_size + (filter_size - 1) * (dilation - 1)
     if border_mode == 'same':
         output_length = input_length
     elif border_mode == 'valid':
         output_length = input_length - dilated_filter_size + 1
-    if transpose:
-      return (output_length + stride - 1) * stride
-    else:
-      return (output_length + stride - 1) // stride
+    return (output_length + stride - 1) // stride
