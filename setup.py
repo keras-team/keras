892d9fae846b2e15e73de075e7290a8bfcfeb145
Francois Chollet <francois.chollet@gmail.com>
2016-07-16 12:25:03 -0700

    Prepare 1.0.6 release

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import find_packages
 
 
 setup(name='Keras',
-      version='1.0.5',
+      version='1.0.6',
       description='Deep Learning for Python',
       author='Francois Chollet',
       author_email='francois.chollet@gmail.com',
       url='https://github.com/fchollet/keras',
-      download_url='https://github.com/fchollet/keras/tarball/1.0.5',
+      download_url='https://github.com/fchollet/keras/tarball/1.0.6',
       license='MIT',
       install_requires=['theano', 'pyyaml', 'six'],
       extras_require={
