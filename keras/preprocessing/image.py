4302d8060d06b8a19f2d16b5565ffdabf82277dd
Francois Chollet <francois.chollet@gmail.com>
2016-07-19 14:30:43 -0700

    Fix image resizing in preprocessing/image

--- keras/preprocessing/image.py
+++ keras/preprocessing/image.py
@@ -162,7 +162,7 @@ def load_img(path, grayscale=False, target_size=None):
     else:  # Ensure 3 channel even when loaded image is grayscale
         img = img.convert('RGB')
     if target_size:
-        img = img.resize(target_size)
+        img = img.resize((target_size[1], target_size[0]))
     return img
 
 
