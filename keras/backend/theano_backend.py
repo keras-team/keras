94da62cbaef7bae48f03c0ca2212e00f52d738be
Fariz Rahman <farizrahman4u@gmail.com>
2016-08-01 00:59:31 +0530

    Update theano_backend.py

--- keras/backend/theano_backend.py
+++ keras/backend/theano_backend.py
@@ -600,7 +600,11 @@ def one_hot(indices, nb_classes):
     Input: Integer tensor of shape (batch_size, input_length)
     Output: One hot representation of the input with shape (batch_size, input_length, nb_classes)
     '''
-    return T.extra_ops.to_one_hot(indices, nb_classes)
+    input_length = indices.shape[1]
+    indices = T.flatten(indices)
+    oh = T.extra_ops.to_one_hot(indices, nb_classes)
+    oh = T.reshape(oh, (-1, input_length, nb_classes))
+    return oh
 
 
 # VALUE MANIPULATION
