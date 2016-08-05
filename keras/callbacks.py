27526b4d3681ba3aa75075973f23e811a3cdb346
Moussa Taifi <mtaifi@appnexus.com>
2016-08-05 14:06:39 -0400

    Fix documentation for ModelCheckpoint callback params

--- keras/callbacks.py
+++ keras/callbacks.py
@@ -231,12 +231,12 @@ class ModelCheckpoint(Callback):
         verbose: verbosity mode, 0 or 1.
         save_best_only: if `save_best_only=True`,
             the latest best model according to
-            the validation loss will not be overwritten.
+            the quantity monitored will not be overwritten.
         mode: one of {auto, min, max}.
             If `save_best_only=True`, the decision
             to overwrite the current save file is made
             based on either the maximization or the
-            minization of the monitored. For `val_acc`,
+            minimization of the monitored quantity. For `val_acc`,
             this should be `max`, for `val_loss` this should
             be `min`, etc. In `auto` mode, the direction is
             automatically inferred from the name of the monitored quantity.
