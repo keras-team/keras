853a8ed487b79db4fde6ca1acf6ff70f08594d28
Fariz Rahman <farizrahman4u@gmail.com>
2016-08-17 03:35:32 +0530

    Fix example

--- keras/layers/wrappers.py
+++ keras/layers/wrappers.py
@@ -145,7 +145,9 @@ class Bidirectional(Wrapper):
     # Examples:
     ```python
     model = Sequential()
-    model.add(Bidirectional(LSTM(10), input_shape=(5, 10)))
+    model.add(Bidirectional(LSTM(10, return_sequences=True), input_shape=(5, 10)))
+    model.add(Bidirectional(LSTM(10)))
+    model.add(Dense(5))
     model.add(Activation('softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
     ```
