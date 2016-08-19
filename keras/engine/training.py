acc5c45feba49de09ee6f46dfb5fad64f4eeb28f
Francois Chollet <francois.chollet@gmail.com>
2016-08-19 11:21:47 -0700

    Remove lock in fit_generator

--- keras/engine/training.py
+++ keras/engine/training.py
@@ -413,20 +413,16 @@ def generator_queue(generator, max_q_size=10,
     if pickle_safe:
         q = multiprocessing.Queue(maxsize=max_q_size)
         _stop = multiprocessing.Event()
-        lock = multiprocessing.Lock()
     else:
         q = queue.Queue()
         _stop = threading.Event()
-        lock = threading.Lock()
 
     try:
         def data_generator_task():
             while not _stop.is_set():
                 try:
                     if pickle_safe or q.qsize() < max_q_size:
-                        lock.acquire()
                         generator_output = next(generator)
-                        lock.release()
                         q.put(generator_output)
                     else:
                         time.sleep(wait_time)
