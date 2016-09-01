ec98a47a32c8644f6a2ff414a1f4e7e6fd3b2e04
Matt Dering <matthew.dering@gmail.com>
2016-09-01 11:52:46 -0400

    Fix Batch Norm compatibility with 3D inputs
    the theano backend now uses dnn_batch_normalization which only supports
    up to 4-dimensional input. This breaks any 5-d layers such as 3D
    convolutions.

--- keras/backend/theano_backend.py
+++ keras/backend/theano_backend.py
@@ -386,7 +386,7 @@ def normalize_batch_in_training(x, gamma, beta,
 def batch_normalization(x, mean, var, beta, gamma, epsilon=0.0001):
     '''Apply batch normalization on x given mean, var, beta and gamma.
     '''
-    if theano.config.device.startswith('cuda') or theano.config.device.startswith('gpu'):
+    if x.ndim < 5 and (theano.config.device.startswith('cuda') or theano.config.device.startswith('gpu')):
         try:
             return theano.sandbox.cuda.dnn.dnn_batch_normalization_test(x, gamma, beta, mean, var,
                                                                         'spatial', epsilon)
