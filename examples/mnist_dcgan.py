eadd4a6461e43cb5bc5c2cca65fb61e8eaab2edc
Jeremy McGibbon <mcgibbon@atmos.washington.edu>
2016-08-31 19:19:24 -0700

    Added training loop for DCGAN

--- examples/mnist_dcgan.py
+++ examples/mnist_dcgan.py
@@ -1,20 +1,16 @@
-'''Trains a simple convnet on the MNIST dataset.
+"""
+Trains a DCGAN on the MNIST dataset.
+"""
 
-Gets to 99.25% test accuracy after 12 epochs
-(there is still a lot of margin for parameter tuning).
-16 seconds per epoch on a GRID K520 GPU.
-'''
-
-from __future__ import print_function
+from __future__ import print_function, division
 import numpy as np
-np.random.seed(1337)  # for reproducibility
-
 from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten, BatchNormalization, LeakyReLU, MinibatchDiscrimination
 from keras.layers import Convolution2D, MaxPooling2D, ZeroPadding2D, UpSampling2D, Reshape
 from keras.optimizers import Adam
-from keras.utils import np_utils
+
+np.random.seed(1337)  # for reproducibility
 
 
 def set_trainable(net, boolean):
@@ -23,8 +19,8 @@ def set_trainable(net, boolean):
         layer.trainable = boolean
 
 batch_size = 128
-nb_classes = 10
 nb_epoch = 12
+nb_zdim = 100
 
 # input image dimensions
 img_rows, img_cols = 28, 28
@@ -48,18 +44,17 @@ print(X_train.shape[0], 'train samples')
 optimizer = Adam(lr=2e-4, beta_1=0.5)
 discrim_optimizer = optimizer
 
-n_base_filters = 128
 discriminator = Sequential()
 discriminator.add(ZeroPadding2D(padding=(4, 4), input_shape=(1, 28, 28)))  # pad to 32x32 to get power of 2
-discriminator.add(Convolution2D(n_base_filters, kernel_size[0], kernel_size[1],
+discriminator.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1],
                                 subsample=(2, 2), border_mode='same'))
 discriminator.add(BatchNormalization(mode=2))
 discriminator.add(LeakyReLU(0.2))
-discriminator.add(Convolution2D(n_base_filters * 2, kernel_size[0], kernel_size[1],
+discriminator.add(Convolution2D(nb_filters * 2, kernel_size[0], kernel_size[1],
                                 subsample=(nb_pool, nb_pool), border_mode='same'))
 discriminator.add(BatchNormalization(mode=2))
 discriminator.add(LeakyReLU(0.2))
-discriminator.add(Convolution2D(n_base_filters * 4, kernel_size[0], kernel_size[1],
+discriminator.add(Convolution2D(nb_filters * 4, kernel_size[0], kernel_size[1],
                                 subsample=(nb_pool, nb_pool), border_mode='same'))
 discriminator.add(BatchNormalization(mode=2))
 discriminator.add(LeakyReLU(0.2))
@@ -72,16 +67,16 @@ discriminator.compile(loss='binary_crossentropy', optimizer=discrim_optimizer)
 
 print('Setting up generator')
 generator = Sequential()
-generator.add(Dense(output_dim=(n_base_filters * 4) * 4 * 4, input_dim=100))
+generator.add(Dense(output_dim=(nb_filters * 4) * 4 * 4, input_dim=nb_zdim))
 generator.add(BatchNormalization(mode=2))
 generator.add(Activation('relu'))
-generator.add(Reshape(target_shape=(n_base_filters * 4, 4, 4)))
+generator.add(Reshape(target_shape=(nb_filters * 4, 4, 4)))
 generator.add(UpSampling2D(size=(2, 2)))
-generator.add(Convolution2D(n_base_filters * 2, kernel_size[0], kernel_size[1], border_mode='same'))
+generator.add(Convolution2D(nb_filters * 2, kernel_size[0], kernel_size[1], border_mode='same'))
 generator.add(BatchNormalization(mode=2))
 generator.add(Activation('relu'))
 generator.add(UpSampling2D(size=(2, 2)))
-generator.add(Convolution2D(n_base_filters, kernel_size[0], kernel_size[1], border_mode='same'))
+generator.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1], border_mode='same'))
 generator.add(BatchNormalization(mode=2))
 generator.add(Activation('relu'))
 generator.add(UpSampling2D(size=(2, 2)))
@@ -99,3 +94,28 @@ gan.add(discriminator)
 gan.compile(loss='binary_crossentropy', optimizer=optimizer)
 
 losses = {'discriminator': [], 'generator': []}
+zeros = np.zeros(batch_size)
+ones = np.ones(batch_size)
+nb_batches = int((X_train.shape[0]-batch_size)/batch_size)
+
+for i_epoch in range(nb_epoch):
+    print('\nEpoch {}/{}'.format(i_epoch, nb_epoch))
+    for i_start in range(0, X_train.shape[0]-batch_size, batch_size):
+        set_trainable(discriminator, True)
+
+        noise_gen = np.random.uniform(-1, 1, size=(batch_size, nb_zdim))
+        X_generated_batch = generator.predict(noise_gen)
+        X_train_batch = X_train[i_start:i_start+batch_size, :, :, :]
+
+        X_batch = np.concatenate([X_generated_batch, X_train_batch], axis=0)
+        y = np.concatenate([zeros, ones], axis=0)
+        discrim_loss = discriminator.train_on_batch(X_batch, y)
+        losses['discriminator'].append(discrim_loss)
+
+        set_trainable(discriminator, False)
+        noise_train = np.random.uniform(-1, 1, size=(batch_size, nb_zdim))
+        gen_loss = gan.train_on_batch(noise_train, zeros)
+        losses['generator'].append(gen_loss)
+
+        print('\rbatch {} of {}... losses - D: {:.4f}, G: {:.4f}'.format(
+            int(i_start/batch_size), nb_batches, discrim_loss, gen_loss), end='')
