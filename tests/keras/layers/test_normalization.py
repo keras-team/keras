f90cbcd1e3e4fd21c2e91bd89933d689514a557c
Pedro S <phpsavarese@gmail.com>
2016-09-02 12:15:51 -0300

    Added regularization option to BatchNormalization layer (#3671)
    * Added regularization option to BatchNormalization layer
    
    * Update normalization.py
    
    * Added regularization to BN test
    
    * Fixed identation
    
    * Removed trailing whitespace and refixed identation

--- tests/keras/layers/test_normalization.py
+++ tests/keras/layers/test_normalization.py
@@ -16,8 +16,11 @@ input_shapes = [np.ones((10, 10)), np.ones((10, 10, 10))]
 
 @keras_test
 def basic_batchnorm_test():
+    from keras import regularizers
     layer_test(normalization.BatchNormalization,
-               kwargs={'mode': 1},
+               kwargs={'mode': 1,
+                       'gamma_regularizer': regularizers.l2(0.01),
+                       'beta_regularizer': regularizers.l2(0.01)},
                input_shape=(3, 4, 2))
     layer_test(normalization.BatchNormalization,
                kwargs={'mode': 0},
