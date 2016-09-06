82b9ced1e14306fd586cc862108b396bccb4c5e2
kuza55 <kuza55@gmail.com>
2016-09-06 15:18:19 -0400

    Fixed issue with _keras_shape for sparse Inputs

--- keras/backend/tensorflow_backend.py
+++ keras/backend/tensorflow_backend.py
@@ -163,8 +163,8 @@ def placeholder(shape=None, ndim=None, dtype=_FLOATX, sparse=None, name=None):
         if ndim:
             shape = tuple([None for _ in range(ndim)])
     if sparse:
-        shape = list([0 for _ in range(len(shape))])
-        x = tf.sparse_placeholder(dtype, shape=tf.constant(np.array(shape, dtype=np.int64)), name=name)
+        tf_shape = tf.constant(np.array(list([0 for _ in range(len(shape))]), dtype=np.int64))
+        x = tf.sparse_placeholder(dtype, shape=tf_shape, name=name)
     else:
         x = tf.placeholder(dtype, shape=shape, name=name)
     x._keras_shape = shape
