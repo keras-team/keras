0c47185140ba31c858bf9929083d19b4470ebc26
dolaameng <dolaameng@gmail.com>
2016-09-07 23:26:00 +0800

    fix bug in neural_style_transfer example for image_dim_ordering=tf

--- examples/neural_style_transfer.py
+++ examples/neural_style_transfer.py
@@ -128,7 +128,10 @@ outputs_dict = dict([(layer.name, layer.output) for layer in model.layers])
 # the gram matrix of an image tensor (feature-wise outer product)
 def gram_matrix(x):
     assert K.ndim(x) == 3
-    features = K.batch_flatten(x)
+    if K.image_dim_ordering() == 'th':
+    	features = K.batch_flatten(x)
+    else:
+    	features = K.batch_flatten(K.permute_dimensions(x, (2, 0, 1)))
     gram = K.dot(features, K.transpose(features))
     return gram
 
