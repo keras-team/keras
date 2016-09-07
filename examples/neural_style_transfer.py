4eb80cdd5926db5bff5b3c1a09a1ad5c379af511
dolaameng <dolaameng@gmail.com>
2016-09-08 00:05:30 +0800

    fix PEP8 mixed space and tab

--- examples/neural_style_transfer.py
+++ examples/neural_style_transfer.py
@@ -129,9 +129,9 @@ outputs_dict = dict([(layer.name, layer.output) for layer in model.layers])
 def gram_matrix(x):
     assert K.ndim(x) == 3
     if K.image_dim_ordering() == 'th':
-    	features = K.batch_flatten(x)
+        features = K.batch_flatten(x)
     else:
-    	features = K.batch_flatten(K.permute_dimensions(x, (2, 0, 1)))
+        features = K.batch_flatten(K.permute_dimensions(x, (2, 0, 1)))
     gram = K.dot(features, K.transpose(features))
     return gram
 
