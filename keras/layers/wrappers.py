ee2d08ff798ba8aa02e14db0aeec0cca613f428a
Francois Chollet <francois.chollet@gmail.com>
2016-09-14 15:02:05 -0700

    Fix activity regularization for wrapper layers

--- keras/layers/wrappers.py
+++ keras/layers/wrappers.py
@@ -20,6 +20,13 @@ class Wrapper(Layer):
         self.regularizers = getattr(self.layer, 'regularizers', [])
         self.constraints = getattr(self.layer, 'constraints', {})
 
+        # properly attribute the current layer to
+        # regularizers that need access to it
+        # (e.g. ActivityRegularizer).
+        for regularizer in self.regularizers:
+            if hasattr(regularizer, 'set_layer'):
+                regularizer.set_layer(self)
+
     def get_weights(self):
         weights = self.layer.get_weights()
         return weights
