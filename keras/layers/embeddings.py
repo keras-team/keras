532b4cc2829e8bc14fb13ec18d27edd83db55e33
Vu Nhan <nhanitvn@gmail.com>
2016-09-22 22:20:30 +0700

    Add OneHot layer for generating one hot embedding for zero-based integer features

--- keras/layers/embeddings.py
+++ keras/layers/embeddings.py
@@ -149,3 +149,49 @@ class Embedding(Layer):
                   'dropout': self.dropout}
         base_config = super(Embedding, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
+
+
+class OneHot(Layer):
+    '''Turn positive integers (indexes) into dense dummy vectors of fixed size
+    eg. [[0], [1], [2]] -> [[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]]
+
+    This layer can only be used as the first layer in a model.
+
+    # Example
+
+    ```python
+        model = Sequential()
+        model.add(OneHot(3))
+        # now: model.output_shape == (None, 1, 3)
+
+    # Arguments
+        input_dim: int > 0. Size of the vocabulary, ie.
+
+    # Input shape
+        2D tensor with shape: `(nb_samples, sequence_length)`.
+
+    # Output shape
+        3D tensor with shape: `(nb_samples, sequence_length, input_dim)`.
+    '''
+    def __init__(self, input_dim, input_length=None, **kwargs):
+        self.input_dim = input_dim
+        self.input_length = input_length
+        kwargs['input_shape'] = (self.input_length,)
+        kwargs['input_dtype'] = 'int32'
+        super(OneHot, self).__init__(**kwargs)
+
+    def get_output_shape_for(self, input_shape):
+        if not self.input_length:
+            input_length = input_shape[1]
+        else:
+            input_length = self.input_length
+        return (input_shape[0], input_length, self.input_dim)
+
+    def call(self, x, mask=None):
+        return K.one_hot(x, self.input_dim)
+
+    def get_config(self):
+        config = {'input_dim': self.input_dim,
+                  'input_length': self.input_length}
+        base_config = super(OneHot, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
