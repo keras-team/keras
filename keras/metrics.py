58b163172bce24ccab0e4583c623ccd3e78812f8
Carl Thomé <carlthome@gmail.com>
2016-10-05 13:45:19 +0200

    Update docstring

--- keras/metrics.py
+++ keras/metrics.py
@@ -92,11 +92,26 @@ def matthews_correlation(y_true, y_pred):
     return numerator / (denominator + K.epsilon())
 
 
-def fmeasure(y_true, y_pred, beta=1):
-    '''Approximate F-score as an overall loss for all the samples at once.
-    '''
+def fbetascore(y_true, y_pred, beta=1):
+    '''Compute F score, the weighted harmonic mean of precision and recall.
     
-    # Count non-zero elements.    
+    This is useful for multi-label classification where input samples can be 
+    tagged with a set of labels. By only using accuracy (precision) a model 
+    would achieve a perfect score by simply assigning every class to every 
+    input. In order to avoid this, a metric should penalize incorrect class 
+    assignments as well (recall). The F-beta score (ranged from 0.0 to 1.0) 
+    computes this, as a weighted mean of the proportion of correct class 
+    assignments vs. the proportion of incorrect class assignments.
+    
+    With beta = 1, this is equivalent to a F-measure. With beta < 1, assigning 
+    correct classes becomes more important, and with beta > 1 the metric is 
+    instead weighted towards penalizing incorrect class assignments.
+    
+    '''
+    if beta < 0:
+        raise ValueError('The lowest choosable beta is zero (only precision).')
+    
+    # Count positive samples.
     c1 = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
     c2 = K.sum(K.round(K.clip(y_pred, 0, 1)))
     c3 = K.sum(K.round(K.clip(y_true, 0, 1)))
@@ -111,7 +126,7 @@ def fmeasure(y_true, y_pred, beta=1):
     # How many relevant items are selected?
     recall = c1 / c3
     
-    # The harmonic mean of precision and recall.
+    # Weight precision and recall together as a single scalar.
     beta2 = beta ** 2
     f_score = (1 + beta2) * (precision * recall) / (beta2 * precision + recall)
     return f_score
