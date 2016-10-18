a3b81cb500524cff334600c12cccbdb13274dbf3
zhourunlai <u201316277@hust.edu.cn>
2016-10-18 13:48:25 +0800

    fixed

--- keras/utils/visualize_util.py
+++ keras/utils/visualize_util.py
@@ -74,42 +74,41 @@ def plot(model, to_file='model.png', show_shapes=False, show_layer_names=True):
     dot.write(to_file, format=format)
 
 
-def figures(history,figure_name="plots"):
-    from keras.callbacks import History  
+def figures(history, figure_name="plots"):
     import matplotlib.pyplot as plt
-  
-    hist = history.history   
-    epoch = history.epoch  
-    acc = hist['acc']  
-    loss = hist['loss']  
-    val_loss = hist['val_loss']  
-    val_acc  = hist['val_acc']
-  
-    plt.figure(1)  
-  
-    plt.subplot(221)  
-    plt.plot(epoch,acc)  
-    plt.title("Training accuracy vs Epoch")  
-    plt.xlabel("Epoch")  
-    plt.ylabel("Accuracy")       
-  
-    plt.subplot(222)  
-    plt.plot(epoch,loss)  
-    plt.title("Training loss vs Epoch")  
-    plt.xlabel("Epoch")  
-    plt.ylabel("Loss")    
-  
-    plt.subplot(223)  
-    plt.plot(epoch,val_acc)  
-    plt.title("Validation Acc vs Epoch")  
-    plt.xlabel("Epoch")  
-    plt.ylabel("Validation Accuracy")    
-  
-    plt.subplot(224)  
-    plt.plot(epoch,val_loss)  
-    plt.title("Validation loss vs Epoch")  
-    plt.xlabel("Epoch")  
+
+    hist = history.history
+    epoch = history.epoch
+    acc = hist['acc']
+    loss = hist['loss']
+    val_loss = hist['val_loss']
+    val_acc = hist['val_acc']
+
+    plt.figure(1)
+
+    plt.subplot(221)
+    plt.plot(epoch, acc)
+    plt.title("Training accuracy vs Epoch")
+    plt.xlabel("Epoch")
+    plt.ylabel("Accuracy")
+
+    plt.subplot(222)
+    plt.plot(epoch, loss)
+    plt.title("Training loss vs Epoch")
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+
+    plt.subplot(223)
+    plt.plot(epoch, val_acc)
+    plt.title("Validation Acc vs Epoch")
+    plt.xlabel("Epoch")
+    plt.ylabel("Validation Accuracy")
+
+    plt.subplot(224)
+    plt.plot(epoch, val_loss)
+    plt.title("Validation loss vs Epoch")
+    plt.xlabel("Epoch")
     plt.ylabel("Validation Loss")
-    
-    plt.tight_layout()  
+
+    plt.tight_layout()
     plt.savefig(figure_name)
