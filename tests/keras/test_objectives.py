77010d01d185da96cfd361f1961a136f0873e5cc
Sachinthaka Abeywardana <sachin.abeywardana@gmail.com>
2016-10-24 17:03:40 +1100

    Added quantile regression loss function for some of the more popular quantiles

--- tests/keras/test_objectives.py
+++ tests/keras/test_objectives.py
@@ -14,7 +14,14 @@ allobj = [objectives.mean_squared_error,
           objectives.binary_crossentropy,
           objectives.kullback_leibler_divergence,
           objectives.poisson,
-          objectives.cosine_proximity]
+          objectives.cosine_proximity,
+          objectives.q1,
+          objectives.q5,
+          objectives.q10,
+          objectives.q50,
+          objectives.q90,
+          objectives.q95,
+          objectives.q99]
 
 
 def test_objective_shapes_3d():
@@ -42,6 +49,18 @@ def test_cce_one_hot():
     y_a = K.variable(np.random.randint(0, 7, (6,)))
     y_b = K.variable(np.random.random((6, 7)))
     assert K.eval(objectives.sparse_categorical_crossentropy(y_a, y_b)).shape == (6,)
+
+def test_q50_abs():
+    """
+    This function tests if the output of the mean absolute and the 50th quantile
+    are the same.
+    """
+    y_a = K.variable((6, 1))
+    y_b = K.variable((6, 1))
+    q50_out = objectives.q50(y_a, y_b)
+    abs_out = objectives.mean_absolute_error(y_a, y_b)
+
+    assert np.abs(K.eval(q50_out - abs_out))<1e-06
 
 
 if __name__ == "__main__":
