88178356730f497f056751a6d013f21635b4fc2c
Gijs van Tulder <gvtulder@gmail.com>
2016-11-04 15:36:02 +0100

    Implement K.tensordot.

--- tests/keras/backend/test_backends.py
+++ tests/keras/backend/test_backends.py
@@ -68,6 +68,11 @@ class TestBackend(object):
         check_two_tensor_operation('dot', (4, 2), (2, 4))
         check_two_tensor_operation('dot', (4, 2), (5, 2, 3))
 
+        check_two_tensor_operation('tensordot', (4, 2), (2, 4), axes=1)
+        check_two_tensor_operation('tensordot', (4, 2), (2, 4, 5), axes=1)
+        check_two_tensor_operation('tensordot', (4, 2, 4), (2, 4, 5), axes=2)
+        check_two_tensor_operation('tensordot', (4, 2, 4), (2, 4, 3), axes=2)
+
         check_two_tensor_operation('batch_dot', (4, 2, 3), (4, 5, 3),
                                    axes=(2, 2))
         check_single_tensor_operation('transpose', (4, 2))
