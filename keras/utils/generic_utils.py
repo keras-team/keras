99a4a320a65d2bafe70df5eae2b791164833f560
wlike <xwf1788@gmail.com>
2016-11-10 21:24:29 +0800

    modify get_from_module function to support overriding default parameter value in initializations/activations/regularizers

--- keras/utils/generic_utils.py
+++ keras/utils/generic_utils.py
@@ -5,15 +5,26 @@ import sys
 import six
 import marshal
 import types as python_types
+from functools import partial
 
 
 def get_from_module(identifier, module_params, module_name,
                     instantiate=False, kwargs=None):
     if isinstance(identifier, six.string_types):
-        res = module_params.get(identifier)
+        idx = identifier.find('(')
+        if -1 == idx:
+            res = module_params.get(identifier)
+        else:
+            func_name = identifier[:idx]
+            aux_kwargs = eval('dict' + identifier[idx:])
+            if not kwargs: kwargs = {}
+            kwargs.update(aux_kwargs)
+            res = partial(module_params.get(func_name), **kwargs)
+
         if not res:
             raise Exception('Invalid ' + str(module_name) + ': ' +
                             str(identifier))
+
         if instantiate and not kwargs:
             return res()
         elif instantiate and kwargs:
@@ -21,13 +32,17 @@ def get_from_module(identifier, module_params, module_name,
         else:
             return res
     elif type(identifier) is dict:
-        name = identifier.pop('name')
-        res = module_params.get(name)
-        if res:
-            return res(**identifier)
-        else:
+        func_name = identifier.pop('func_name')
+        res = module_params.get(func_name)
+
+        if not res:
             raise Exception('Invalid ' + str(module_name) + ': ' +
                             str(identifier))
+
+        if instantiate:
+            return res(**identifier)
+        else:
+            return partial(res, **identifier)
     return identifier
 
 
