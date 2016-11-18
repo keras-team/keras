4dd358319612bda2c87940dde8c7097c2a66b95a
Ken Chatfield <ken.chatfield@gmail.com>
2016-11-18 18:25:34 +0000

    Added unit test

--- tests/keras/engine/test_training.py
+++ tests/keras/engine/test_training.py
@@ -8,6 +8,7 @@ from keras.engine.training import Model
 from keras.models import Sequential
 from keras import backend as K
 from keras.utils.test_utils import keras_test
+from keras.callbacks import LambdaCallback
 
 
 @keras_test
@@ -145,6 +146,25 @@ def test_model_methods():
     out = model.test_on_batch([input_a_np, input_b_np],
                               [output_a_np, output_b_np])
     assert len(out) == 4
+
+    # test starting from non-zero initial epoch
+    trained_epochs = []
+    def on_epoch_begin(epoch, logs):
+        trained_epochs.append(epoch)
+    tracker_cb = LambdaCallback(on_epoch_begin=on_epoch_begin)
+    out = model.fit([input_a_np, input_b_np],
+                    [output_a_np, output_b_np], nb_epoch=5, batch_size=4,
+                    initial_epoch=2, callbacks=[tracker_cb])
+    assert trained_epochs == [2, 3, 4]
+
+    trained_epochs = []
+    def gen_data(batch_sz):
+        while True:
+            yield ([np.random.random((batch_sz, 3)), np.random.random((batch_sz, 3))],
+                   [np.random.random((batch_sz, 4)), np.random.random((batch_sz, 3))])
+    out = model.fit_generator(gen_data(4), samples_per_epoch=10, nb_epoch=5,
+                    initial_epoch=2, callbacks=[tracker_cb])
+    assert trained_epochs == [2, 3, 4]
 
     # test with a custom metric function
     mse = lambda y_true, y_pred: K.mean(K.pow(y_true - y_pred, 2))
