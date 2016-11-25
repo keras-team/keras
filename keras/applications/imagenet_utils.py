de256cb5d5f5a939f916b2827d2a974bc9eec60b
fchollet <francois.chollet@gmail.com>
2016-11-24 23:30:00 -0800

    Make sure ImageNet predictions are sorted

--- keras/applications/imagenet_utils.py
+++ keras/applications/imagenet_utils.py
@@ -46,5 +46,6 @@ def decode_predictions(preds, top=5):
     for pred in preds:
         top_indices = pred.argsort()[-top:][::-1]
         result = [tuple(CLASS_INDEX[str(i)]) + (pred[i],) for i in top_indices]
+        result.sort(key=lambda x: x[2], reverse=True)
         results.append(result)
     return results
