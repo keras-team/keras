91024e8107dee0488759b4f098aca100e6de27b9
amarshah <amarshah@users.noreply.github.com>
2016-11-29 11:52:32 +0000

    correction to kl_term
    kl_term involves a sum over the latent_dim, not a mean

--- examples/variational_autoencoder_deconv.py
+++ examples/variational_autoencoder_deconv.py
@@ -106,7 +106,7 @@ def vae_loss(x, x_decoded_mean):
     x = K.flatten(x)
     x_decoded_mean = K.flatten(x_decoded_mean)
     xent_loss = img_rows * img_cols * objectives.binary_crossentropy(x, x_decoded_mean)
-    kl_loss = - 0.5 * K.mean(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)
+    kl_loss = - 0.5 * K.sum(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)
     return xent_loss + kl_loss
 
 vae = Model(x, x_decoded_mean_squash)
