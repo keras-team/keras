5ec41d741a61b0864cf73bc382cefec57c35957d
mbhenry <mike.henry@isosemi.com>
2016-12-21 18:41:18 -0800

    Removed TimeDistributed usage and fixed quote inconsistencies

--- examples/image_ocr.py
+++ examples/image_ocr.py
@@ -52,7 +52,7 @@ from keras.utils.data_utils import get_file
 from keras.preprocessing import image
 import keras.callbacks
 
-OUTPUT_DIR = "image_ocr"
+OUTPUT_DIR = 'image_ocr'
 
 np.random.seed(55)
 
@@ -244,6 +244,8 @@ class TextImageGenerator(keras.callbacks.Callback):
     # each time an image is requested from train/val/test, a new random
     # painting of the text is performed
     def get_batch(self, index, size, train):
+        #width and height are backwards from typical Keras convention
+        #because width is the time dimension when it gets fed into the RNN
         if K.image_dim_ordering() == 'th':
             X_data = np.ones([size, 1, self.img_w, self.img_h])
         else:
@@ -434,29 +436,29 @@ def train(num_epochs, img_w, start_epoch, run_name):
     act = 'relu'
     input_data = Input(name='the_input', shape=input_shape, dtype='float32')
     inner = Convolution2D(conv_num_filters, filter_size, filter_size, border_mode='same',
-                          activation=act, init="he_normal", name='conv1')(input_data)
+                          activation=act, init='he_normal', name='conv1')(input_data)
     inner = MaxPooling2D(pool_size=(pool_size, pool_size), name='max1')(inner)
     inner = Convolution2D(conv_num_filters, filter_size, filter_size, border_mode='same',
-                          activation=act, init="he_normal", name='conv2')(inner)
+                          activation=act, init='he_normal', name='conv2')(inner)
     inner = MaxPooling2D(pool_size=(pool_size, pool_size), name='max2')(inner)
 
     conv_to_rnn_dims = (img_w // (pool_size ** 2), (img_h // (pool_size ** 2)) * conv_num_filters)
     inner = Reshape(target_shape=conv_to_rnn_dims, name='reshape')(inner)
 
     # cuts down input size going into RNN:
-    inner = TimeDistributed(Dense(time_dense_size, activation=act, name='dense1'))(inner)
+    inner = Dense(time_dense_size, activation=act, name='dense1')(inner)
 
     # Two layers of bidirecitonal GRUs
     # GRU seems to work as well, if not better than LSTM:
-    gru_1 = GRU(rnn_size, return_sequences=True, init="he_normal", name='gru1')(inner)
-    gru_1b = GRU(rnn_size, return_sequences=True, go_backwards=True, init="he_normal", name='gru1_b')(inner)
+    gru_1 = GRU(rnn_size, return_sequences=True, init='he_normal', name='gru1')(inner)
+    gru_1b = GRU(rnn_size, return_sequences=True, go_backwards=True, init='he_normal', name='gru1_b')(inner)
     gru1_merged = merge([gru_1, gru_1b], mode='sum')
-    gru_2 = GRU(rnn_size, return_sequences=True, init="he_normal", name='gru2')(gru1_merged)
-    gru_2b = GRU(rnn_size, return_sequences=True, go_backwards=True, init="he_normal", name='gru2_b')(gru1_merged)
+    gru_2 = GRU(rnn_size, return_sequences=True, init='he_normal', name='gru2')(gru1_merged)
+    gru_2b = GRU(rnn_size, return_sequences=True, go_backwards=True, init='he_normal', name='gru2_b')(gru1_merged)
 
     # transforms RNN output to character activations:
-    inner = TimeDistributed(Dense(img_gen.get_output_size(), init="he_normal",
-                                  name='dense2'))(merge([gru_2, gru_2b], mode='concat'))
+    inner = Dense(img_gen.get_output_size(), init='he_normal',
+                                  name='dense2')(merge([gru_2, gru_2b], mode='concat'))
     y_pred = Activation('softmax', name='softmax')(inner)
     Model(input=[input_data], output=y_pred).summary()
 
@@ -465,7 +467,7 @@ def train(num_epochs, img_w, start_epoch, run_name):
     label_length = Input(name='label_length', shape=[1], dtype='int64')
     # Keras doesn't currently support loss funcs with extra parameters
     # so CTC loss is implemented in a lambda layer
-    loss_out = Lambda(ctc_lambda_func, output_shape=(1,), name="ctc")([y_pred, labels, input_length, label_length])
+    loss_out = Lambda(ctc_lambda_func, output_shape=(1,), name='ctc')([y_pred, labels, input_length, label_length])
 
     # clipnorm seems to speeds up convergence
     sgd = SGD(lr=0.02, decay=1e-6, momentum=0.9, nesterov=True, clipnorm=5)
@@ -475,7 +477,7 @@ def train(num_epochs, img_w, start_epoch, run_name):
     # the loss calc occurs elsewhere, so use a dummy lambda func for the loss
     model.compile(loss={'ctc': lambda y_true, y_pred: y_pred}, optimizer=sgd)
     if start_epoch > 0:
-        weight_file = os.path.join(OUTPUT_DIR, os.path.join(run_name, "weights%0d.h5" % (start_epoch - 1)))
+        weight_file = os.path.join(OUTPUT_DIR, os.path.join(run_name, 'weights%0d.h5' % (start_epoch - 1)))
         model.load_weights(weight_file)
     # captures output of softmax so we can decode the output during visualization
     test_func = K.function([input_data], [y_pred])
@@ -487,7 +489,7 @@ def train(num_epochs, img_w, start_epoch, run_name):
                         callbacks=[viz_cb, img_gen])
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     run_name = datetime.datetime.now().strftime('%Y:%m:%d:%H:%M:%S')
     train(20, 128, 0, run_name)
     # increase to wider images and start at epoch 20. The learned weights are reloaded
