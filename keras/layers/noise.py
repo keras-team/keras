01304e3e7d6a5c78d3a4e9d17c85168ad3b3e57a
Partha <parthaghosh.iitbbs@gmail.com>
2016-12-25 15:30:34 +0100

    Noise layer sigma modification capabality in runtime

--- keras/layers/noise.py
+++ keras/layers/noise.py
@@ -36,6 +36,13 @@ class GaussianNoise(Layer):
                                       std=self.sigma)
         return K.in_train_phase(noise_x, x)
 
+    # Often we want to progressively add random noise to teh input of an
+    # NN in order to train it for unseen data and yet keep the prediction 
+    # in the allowed data manifold. Having to set the noise sigma in the 
+    # very beginnin and only once is thus not desirable.
+    def set_covariance(self, covar):
+	self.sigma = covar
+
     def get_config(self):
         config = {'sigma': self.sigma}
         base_config = super(GaussianNoise, self).get_config()
