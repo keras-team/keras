keras-team/keras

commit f6b804263a6e1d3ce6d2131cb9d758e1d7e57888

callbacks: Maximize fmeasure as well when mode==auto (#4902)

keras/callbacks.py
@@ -274,7 +274,7 @@ class ModelCheckpoint(Callback):
             self.monitor_op = np.greater
             self.best = -np.Inf
         else:
-            if 'acc' in self.monitor:
+            if self.monitor.startswith(('acc', 'fmeasure')):
                 self.monitor_op = np.greater
                 self.best = -np.Inf
             else:
@@ -357,7 +357,7 @@ class EarlyStopping(Callback):
         elif mode == 'max':
             self.monitor_op = np.greater
         else:
-            if 'acc' in self.monitor:
+            if self.monitor.startswith(('acc', 'fmeasure')):
                 self.monitor_op = np.greater
             else:
                 self.monitor_op = np.less

commit 8722d82de3ee96f8dc6a3cb5d06956a2ba5f0086

Fix bug introduced in commit: https://github.com/fchollet/keras/commit/f6b804263a6e1d3ce6d2131cb9d758e1d7e57888.

keras/callbacks.py
@@ -299,7 +299,7 @@ class ModelCheckpoint(Callback):
             self.monitor_op = np.greater
             self.best = -np.Inf
         else:
-            if self.monitor.startswith(('acc', 'fmeasure')):
+            if 'acc' in self.monitor or self.monitor.startswith('fmeasure'):
                 self.monitor_op = np.greater
                 self.best = -np.Inf
             else:
@@ -385,7 +385,7 @@ class EarlyStopping(Callback):
         elif mode == 'max':
             self.monitor_op = np.greater
         else:
-            if self.monitor.startswith(('acc', 'fmeasure')):
+            if 'acc' in self.monitor or self.monitor.startswith('fmeasure'):
                 self.monitor_op = np.greater
             else:
                 self.monitor_op = np.less