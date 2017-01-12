c6bf7558b2121c3f2f280a0d15ee4354994e0c86
Jack Hessel <jhessel@cs.cornell.edu>
2017-01-12 02:10:21 -0500

    Potential deconv model saving fix? (#4999)
    Adding this cast to a tuple seems to fix this issue.

--- keras/backend/tensorflow_backend.py
+++ keras/backend/tensorflow_backend.py
@@ -2468,7 +2468,7 @@ def _preprocess_deconv_output_shape(x, shape, dim_ordering):
         shape = (shape[0], shape[2], shape[3], shape[1])
 
     if shape[0] is None:
-        shape = (tf.shape(x)[0], ) + shape[1:]
+        shape = (tf.shape(x)[0], ) + tuple(shape[1:])
     return shape
 
 
