Correct calculation of metrics with masking (#2260)
* Reshape y_pred and y_true from (samples, timesteps, ... ) to (samples * timesteps, ... )

* Filter out masked timesteps from y_pred and y_true

* Added K.where() and extended functionality of K.flatten()

* Added/changed corresponding tests

tests/test_loss_masking.py
@@ -2,8 +2,9 @@ import numpy as np
 import pytest
 
 from keras.models import Sequential
-from keras.engine.training import weighted_objective
-from keras.layers.core import TimeDistributedDense, Masking
+from keras.engine.training import weighted_objective, masked_tensor
+from keras.layers.core import Dense, Masking
+from keras.layers.wrappers import TimeDistributed
 from keras.utils.test_utils import keras_test
 from keras import objectives
 from keras import backend as K
@@ -16,12 +17,14 @@ def test_masking():
                   [[0], [0]]])
     model = Sequential()
     model.add(Masking(mask_value=0, input_shape=(2, 1)))
-    model.add(TimeDistributedDense(1, init='one'))
-    model.compile(loss='mse', optimizer='sgd')
+    model.add(TimeDistributed(Dense(1, init='one')))
+    model.compile(loss='mse', optimizer='sgd', metrics=['accuracy'])
     y = np.array([[[1], [1]],
                   [[1], [1]]])
-    loss = model.train_on_batch(X, y)
+    (loss, acc) = model.train_on_batch(X, y)
+
     assert loss == 0
+    assert acc == 1
 
 
 @keras_test
@@ -40,6 +43,18 @@ def test_loss_masking():
                                K.variable(Y),
                                K.variable(weights),
                                K.variable(mask)))
+
+
+@keras_test
+def test_masked_tensor():
+    x = np.random.randint(0, 10, size=(5, 10, 5))
+    mask = np.random.randint(0, 2, size=(5, 10))
+    i = np.nonzero(mask)
+    exp_out = x[i[0], i[1], :]
+
+    k_out = K.eval(masked_tensor(K.variable(x, dtype='int32'), K.variable(mask, dtype='int32')))
+
+    assert np.all(k_out == exp_out)
 
 
 if __name__ == '__main__':