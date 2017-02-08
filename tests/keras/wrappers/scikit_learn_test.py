22cb9c432fa5ab1e8555a899c3fff97adf07f696
reiinakano <reii_nakano@yahoo.com>
2017-02-08 17:13:01 +0800

    Add ability for KerasClassifier to process string classes. Included unit tests.

--- tests/keras/wrappers/scikit_learn_test.py
+++ tests/keras/wrappers/scikit_learn_test.py
@@ -45,6 +45,7 @@ def test_clasify_build_fn():
         batch_size=batch_size, epochs=epochs)
 
     assert_classification_works(clf)
+    assert_string_classification_works(clf)
 
 
 def test_clasify_class_build_fn():
@@ -58,6 +59,7 @@ def test_clasify_class_build_fn():
         batch_size=batch_size, epochs=epochs)
 
     assert_classification_works(clf)
+    assert_string_classification_works(clf)
 
 
 def test_clasify_inherit_class_build_fn():
@@ -71,6 +73,7 @@ def test_clasify_inherit_class_build_fn():
         batch_size=batch_size, epochs=epochs)
 
     assert_classification_works(clf)
+    assert_string_classification_works(clf)
 
 
 def assert_classification_works(clf):
@@ -83,6 +86,24 @@ def assert_classification_works(clf):
     assert preds.shape == (num_test, )
     for prediction in np.unique(preds):
         assert prediction in range(num_class)
+
+    proba = clf.predict_proba(X_test, batch_size=batch_size)
+    assert proba.shape == (num_test, num_class)
+    assert np.allclose(np.sum(proba, axis=1), np.ones(num_test))
+
+
+def assert_string_classification_works(clf):
+    str_y_train = np.array(['cls1', 'cls2', 'cls3'])[y_train]
+
+    clf.fit(X_train, str_y_train, batch_size=batch_size, nb_epoch=epochs)
+
+    score = clf.score(X_train, str_y_train, batch_size=batch_size)
+    assert np.isscalar(score) and np.isfinite(score)
+
+    preds = clf.predict(X_test, batch_size=batch_size)
+    assert preds.shape == (num_test, )
+    for prediction in np.unique(preds):
+        assert prediction in ['cls1', 'cls2', 'cls3']
 
     proba = clf.predict_proba(X_test, batch_size=batch_size)
     assert proba.shape == (num_test, num_class)
