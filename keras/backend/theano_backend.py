b30aea6282bfa1d9faf9091e776f572596ff8b9d
Achal <achalshah20@gmail.com>
2017-02-23 19:45:14 -0500

    Updated num_classes to nb_class

--- keras/backend/theano_backend.py
+++ keras/backend/theano_backend.py
@@ -1287,7 +1287,7 @@ def categorical_crossentropy(output, target, from_logits=False):
 
 def sparse_categorical_crossentropy(output, target, from_logits=False):
     target = T.cast(T.flatten(target), 'int32')
-    target = T.extra_ops.to_one_hot(target, num_classes=output.shape[-1])
+    target = T.extra_ops.to_one_hot(target, nb_class=output.shape[-1])
     target = reshape(target, shape(output))
     return categorical_crossentropy(output, target, from_logits)
 
