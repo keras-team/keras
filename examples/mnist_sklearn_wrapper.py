3cb5fe4dec13d192802f507e6b1203cc5a7da6c1
abhai <abhai.dilip@gmail.com>
2017-02-27 23:34:56 +0530

    Convert dim_ordering to data_format

--- examples/mnist_sklearn_wrapper.py
+++ examples/mnist_sklearn_wrapper.py
@@ -25,7 +25,7 @@ img_rows, img_cols = 28, 28
 # load training data and do basic data normalization
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-if K.image_dim_ordering() == 'th':
+if K.image_data_format() == 'channels_first':
     X_train = X_train.reshape(X_train.shape[0], 1, img_rows, img_cols)
     X_test = X_test.reshape(X_test.shape[0], 1, img_rows, img_cols)
     input_shape = (1, img_rows, img_cols)
