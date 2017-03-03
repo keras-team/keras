5b0967a08ff5a277d318506a9c28a31acd04794a
Francois Chollet <francois.chollet@gmail.com>
2017-03-03 15:05:42 -0800

    Add resnet50 test.

--- tests/keras/applications/applications_test.py
+++ tests/keras/applications/applications_test.py
@@ -5,6 +5,26 @@ from keras import backend as K
 
 
 @keras_test
+def test_resnet50():
+    model = applications.ResNet50(weights=None)
+    assert model.output_shape == (None, 1000)
+
+
+@keras_test
+def test_resnet50_notop():
+    model = applications.ResNet50(weights=None, include_top=False)
+    assert model.output_shape == (None, None, None, 2048)
+
+
+@keras_test
+def test_resnet50_pooling():
+    model = applications.ResNet50(weights=None,
+                                  include_top=False,
+                                  pooling='avg')
+    assert model.output_shape == (None, 2048)
+
+
+@keras_test
 def test_vgg16():
     model = applications.VGG16(weights=None)
     assert model.output_shape == (None, 1000)
