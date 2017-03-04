c84e08367d93855cac96bf4ba672e6561b4b4012
Kevin Mader <kmader@users.noreply.github.com>
2017-03-04 11:48:47 +0100

    Update deep_dream.py
    the model weights are never used and can be removed, moved the comments into the header of the Evaluator class

--- examples/deep_dream.py
+++ examples/deep_dream.py
@@ -40,9 +40,6 @@ result_prefix = args.result_prefix
 img_height = 600
 img_width = 600
 
-# path to the model weights file.
-weights_path = 'vgg16_weights.h5'
-
 # some settings we found interesting
 saved_settings = {
     'bad_trip': {'features': {'block4_conv1': 0.05,
@@ -164,16 +161,16 @@ def eval_loss_and_grads(x):
         grad_values = np.array(outs[1:]).flatten().astype('float64')
     return loss_value, grad_values
 
-# this Evaluator class makes it possible
-# to compute loss and gradients in one pass
-# while retrieving them via two separate functions,
-# "loss" and "grads". This is done because scipy.optimize
-# requires separate functions for loss and gradients,
-# but computing them separately would be inefficient.
-
 
 class Evaluator(object):
-
+    """
+    this Evaluator class makes it possible
+    to compute loss and gradients in one pass
+    while retrieving them via two separate functions,
+    "loss" and "grads". This is done because scipy.optimize
+    requires separate functions for loss and gradients,
+    but computing them separately would be inefficient.
+    """
     def __init__(self):
         self.loss_value = None
         self.grad_values = None
