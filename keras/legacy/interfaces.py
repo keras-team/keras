6ff31b4f98880a1d4a4c9b427dc04e0f512c6fc9
Rizky Luthfianto <mrluthfianto@gmail.com>
2017-03-08 06:50:07 +0700

    Add API conversion interface for Embedding layer.

--- keras/legacy/interfaces.py
+++ keras/legacy/interfaces.py
@@ -74,3 +74,70 @@ def legacy_dense_support(func):
 
         return func(*args, **kwargs)
     return wrapper
+
+
+def legacy_embedding_support(func):
+    """Function wrapper to convert the `Embedding` constructor from Keras 1 to 2.
+
+    # Arguments
+        func: `__init__` method of `Embedding`.
+
+    # Returns
+        A constructor conversion wrapper.
+    """
+    @six.wraps(func)
+    def wrapper(*args, **kwargs):
+        if len(args) > 3:
+            # The first entry in `args` is `self`.
+            raise TypeError('The `Embedding` layer can have at most '
+                            'two positional argument (`input_dim` and `output_dim`).')
+
+        # input_dim and output_dim
+        input_dim = kwargs.pop('input_dim', None)
+        if input_dim is None:
+            input_dim = args[1]
+
+        output_dim = kwargs.pop('output_dim', None)
+        if output_dim is None:
+            output_dim = args[2]
+
+        if 'dropout' in kwargs:
+            del kwargs['dropout']
+            warnings.warn('The `dropout` argument is no longer exists. '
+                          'Please use keras.layers.SpatialDropout1D layer '
+                          'right after the Embedding layer to get the same behavior.')
+
+        args = (args[0], input_dim, output_dim)
+
+        converted = []
+
+        # Remaining kwargs.
+        conversions = [
+            ('init', 'embeddings_initializer'),
+            ('W_regularizer', 'embeddings_regularizer'),
+            ('W_constraint', 'embeddings_constraint'),
+        ]
+
+        for old_arg, new_arg in conversions:
+            if old_arg in kwargs:
+                if new_arg in kwargs:
+                    raise_duplicate_arg_error(old_arg, new_arg)
+                arg_value = kwargs.pop(old_arg)
+                kwargs[new_arg] = arg_value
+                converted.append((new_arg, arg_value))
+
+        if converted:
+            signature = '`Embedding(' + str(input_dim) + ', ' + str(output_dim)
+
+            for name, value in converted:
+                signature += ', ' + name + '='
+                if isinstance(value, six.string_types):
+                    signature += ('"' + value + '"')
+                else:
+                    signature += str(value)
+            signature += ')`'
+            warnings.warn('Update your `Embedding` layer call '
+                          'to the Keras 2 API: ' + signature)
+
+        return func(*args, **kwargs)
+    return wrapper
