be14277d833c70b5aa1ae08544ab8eaad5a31b81
88k3ys <antonmbk@gmail.com>
2017-03-11 00:37:21 -0800

    Changed the convolution kernels in the residual pathway to size 1x1, otherwise receptive fields are huge (much bigger than input image due to border_mode='same').

--- examples/mnist_swwae.py
+++ examples/mnist_swwae.py
@@ -64,7 +64,7 @@ def convresblock(x, nfeats=8, ksize=3, nskipped=2):
     for i in range(nskipped):
         y = BatchNormalization(mode=0, axis=1)(y)
         y = Activation('relu')(y)
-        y = Convolution2D(nfeats, ksize, ksize, border_mode='same')(y)
+        y = Convolution2D(nfeats, 1, 1, border_mode='same')(y)
     return merge([y0, y], mode='sum')
 
 
