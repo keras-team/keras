acaf56ad1f139f35b0ff0015edfe3f818da40d78
abhai <abhai.dilip@gmail.com>
2017-03-12 03:41:46 +0530

    Abstract legacy method interface generator

--- keras/legacy/interfaces.py
+++ keras/legacy/interfaces.py
@@ -65,6 +65,69 @@ def generate_legacy_interface(allowed_positional_args=None,
         return wrapper
     return legacy_support
 
+def generate_legacy_method_interface(allowed_positional_args=None,
+                                     conversions=None,
+                                     preprocessor=None,
+                                     value_conversions=None):
+    allowed_positional_args = allowed_positional_args or []
+    conversions = conversions or []
+    value_conversions = value_conversions or []
+
+    def legacy_support(func):
+        @six.wraps(func)
+        def wrapper(*args, **kwargs):
+            method_name = func.__name__
+            if preprocessor:
+                args, kwargs, converted = preprocessor(args, kwargs)
+            else:
+                converted = []
+            if len(args) > len(allowed_positional_args) + 1:
+                raise TypeError('The function `'+ method_name +
+                                '` can accept only ' +
+                                str(len(allowed_positional_args)) +
+                                ' positional arguments (' +
+                                str(allowed_positional_args) + '), but '
+                                'you passed the following '
+                                'positional arguments: ' +
+                                str(args[1:]))
+            for key in value_conversions:
+                if key in kwargs:
+                    old_value = kwargs[key]
+                    if old_value in value_conversions[key]:
+                        kwargs[key] = value_conversions[key][old_value]
+            for old_name, new_name in conversions:
+                if old_name in kwargs:
+                    value = kwargs.pop(old_name)
+                    if new_name in kwargs:
+                        raise_duplicate_arg_error(old_name, new_name)
+                    kwargs[new_name] = value
+                    converted.append((new_name, old_name))
+            if converted:
+                signature = '`' + method_name + '('
+                for value in args[1:]:
+                    if isinstance(value, six.string_types):
+                        signature += '"' + value + '"'
+                    elif hasattr(value, '__name__'):
+                        signature += value.__name__ + '()'
+                    else:
+                        signature += str(value)
+                    signature += ', '
+                for i, (name, value) in enumerate(kwargs.items()):
+                    signature += name + '='
+                    if isinstance(value, six.string_types):
+                        signature += '"' + value + '"'
+                    elif hasattr(value, '__name__'):
+                        signature += value.__name__ + '()'                        
+                    else:
+                        signature += str(value)
+                    if i < len(kwargs) - 1:
+                        signature += ', '
+                signature += ')`'
+                warnings.warn('Update your `' + method_name +
+                              '` function call to the Keras 2 API: ' + signature)
+            return func(*args, **kwargs)
+        return wrapper
+    return legacy_support
 
 def raise_duplicate_arg_error(old_arg, new_arg):
     raise TypeError('For the `' + new_arg + '` argument, '
@@ -494,6 +557,7 @@ legacy_cropping3d_support = generate_legacy_interface(
                                         'default': None}})
 
 
+<<<<<<< HEAD
 # For fit_generator, evaluate_generator, predict_generator methods
 def legacy_generator_methods_support(func):
     @six.wraps(func)
