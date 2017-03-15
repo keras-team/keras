1a69cb98fedda8c093e90881c42aaa0337fbb467
88k3ys <antonmbk@gmail.com>
2017-03-14 22:35:13 -0700

    Added a more informative docstring decsribing elu argument and its two behaviors.

--- examples/mnist_swwae.py
+++ examples/mnist_swwae.py
@@ -58,7 +58,29 @@ from keras import layers
 
 
 def convresblock(x, nfeats=8, ksize=3, nskipped=2, elu=True):
-    ''' The proposed residual block from [4]'''
+    """The proposed residual block from [4].
+    
+    Running with elu=True will use ELU nonlinearity and running with
+    elu=False will use BatchNorm + RELU nonlinearity.  While ELU's are fast
+    due to the fact they do not suffer from BatchNorm overhead, they may
+    overfit because they do not offer the stochastic element of the batch
+    formation process of BatchNorm, which acts as a good regularizer.
+
+    # Arguments
+        x: 4D tensor, the tensor to feed through the block
+        nfeats: Integer, number of feature maps for conv layers.
+        ksize: Integer, width and height of conv kernels in first convolution.
+        nskipped: Integer, number of conv layers for the residual function.
+        elu: Boolean, whether to use ELU or BN+RELU.
+
+    # Input shape
+        4D tensor with shape:
+        `(batch, channels, rows, cols)`
+
+    # Output shape
+        4D tensor with shape:
+        `(batch, filters, rows, cols)`
+    """                                            
     y0 = Conv2D(nfeats, ksize, padding='same')(x)
     y = y0
     for i in range(nskipped):
