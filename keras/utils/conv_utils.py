819f3e2ba5878983b3b09d5ab7fbf629b205c2ec
Francois Chollet <fchollet@google.com>
2017-03-15 15:55:09 -0700

    style fix

--- keras/utils/conv_utils.py
+++ keras/utils/conv_utils.py
@@ -146,10 +146,9 @@ def conv_input_length(output_length, filter_size, padding, stride):
 def deconv_length(dim_size, stride_size, kernel_size, padding):
     if dim_size is None:
         return None
+    dim_size *= stride_size
     if padding == 'valid':
-        dim_size = dim_size * stride_size + max(kernel_size - stride_size, 0)
+        dim_size += max(kernel_size - stride_size, 0)
     elif padding == 'full':
-        dim_size = dim_size * stride_size - (stride_size + kernel_size - 2)
-    elif padding == 'same':
-        dim_size = dim_size * stride_size
+        dim_size -= (stride_size + kernel_size - 2)
     return dim_size
