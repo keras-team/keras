10399242459eb222afece5edc5645ad458ea43c7
Kosuke Kusano <cocuh@users.noreply.github.com>
2017-03-16 23:03:45 +0900

    fix typo in some layers' `get_config()` (#5810)
    * fix typo
    
    * fix typo
    
    * fix typo

--- keras/layers/core.py
+++ keras/layers/core.py
@@ -857,7 +857,7 @@ class Dense(Layer):
             'activation': activations.serialize(self.activation),
             'use_bias': self.use_bias,
             'kernel_initializer': initializers.serialize(self.kernel_initializer),
-            'bias_initializer': initializers.serialize(self.kernel_initializer),
+            'bias_initializer': initializers.serialize(self.bias_initializer),
             'kernel_regularizer': regularizers.serialize(self.kernel_regularizer),
             'bias_regularizer': regularizers.serialize(self.bias_regularizer),
             'activity_regularizer': regularizers.serialize(self.activity_regularizer),
