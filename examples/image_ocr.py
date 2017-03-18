92e7ede90612a96a1bfa5d08d4199d3a0e1f3117
Kashif Rasul <kashif.rasul@gmail.com>
2017-03-18 09:33:15 +0100

    pep8

--- examples/image_ocr.py
+++ examples/image_ocr.py
@@ -429,12 +429,12 @@ def train(run_name, start_epoch, stop_epoch, img_w):
     act = 'relu'
     input_data = Input(name='the_input', shape=input_shape, dtype='float32')
     inner = Conv2D(conv_filters, filter_size, padding='same',
-                          activation=act, kernel_initializer='he_normal',
-                          name='conv1')(input_data)
+                   activation=act, kernel_initializer='he_normal',
+                   name='conv1')(input_data)
     inner = MaxPooling2D(pool_size=(pool_size, pool_size), name='max1')(inner)
     inner = Conv2D(conv_filters, filter_size, padding='same',
-                          activation=act, kernel_initializer='he_normal',
-                          name='conv2')(inner)
+                   activation=act, kernel_initializer='he_normal',
+                   name='conv2')(inner)
     inner = MaxPooling2D(pool_size=(pool_size, pool_size), name='max2')(inner)
 
     conv_to_rnn_dims = (img_w // (pool_size ** 2), (img_h // (pool_size ** 2)) * conv_filters)
