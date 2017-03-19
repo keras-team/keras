90529b222e7d10ee1a987197071aa60a0ef3ac46
Abhai Kollara Dilip <abhai.dilip@gmail.com>
2017-03-19 09:08:42 +0530

    Docstring fix for Conv, Local, Pooling layers (#5854)
    * Docstring fix
    
    * Doc string fix for local, pooling

--- keras/layers/local.py
+++ keras/layers/local.py
@@ -226,9 +226,9 @@ class LocallyConnected2D(Layer):
             one of `channels_last` (default) or `channels_first`.
             The ordering of the dimensions in the inputs.
             `channels_last` corresponds to inputs with shape
-            `(batch, width, height, channels)` while `channels_first`
+            `(batch, height, width, channels)` while `channels_first`
             corresponds to inputs with shape
-            `(batch, channels, width, height)`.
+            `(batch, channels, height, width)`.
             It defaults to the `image_data_format` value found in your
             Keras config file at `~/.keras/keras.json`.
             If you never set it, then it will be "channels_last".
