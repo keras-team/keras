7ef521ff587d695190bebc88355f34cf609df2dc
Cheng Wang <chengwang0511@gmail.com>
2017-04-02 14:59:10 +0200

    remove unused variables in cifar10_cnn

--- examples/cifar10_cnn.py
+++ examples/cifar10_cnn.py
@@ -20,11 +20,6 @@ num_classes = 10
 epochs = 200
 data_augmentation = True
 
-# input image dimensions
-img_rows, img_cols = 32, 32
-# The CIFAR10 images are RGB.
-img_channels = 3
-
 # The data, shuffled and split between train and test sets:
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 print('x_train shape:', x_train.shape)
