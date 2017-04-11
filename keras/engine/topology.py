f059345d7504fa03e5e97c525992c77de4252ea2
rb16 <russjbates@gmail.com>
2017-04-11 09:50:55 +0100

    Recursive implementation of preprocess_weights_for_loading to accomodate Bidirectional and TimeDistributed wrappers.

--- keras/engine/topology.py
+++ keras/engine/topology.py
@@ -2750,120 +2750,116 @@ def preprocess_weights_for_loading(layer, weights,
     if layer.__class__.__name__ == 'Bidirectional':
         num_weights_per_layer = len(weights) // 2
 
-        forward_weights = _preprocess_weights(layer.forward_layer, weights[:num_weights_per_layer],
-                                              original_keras_version,
-                                              original_backend)
-        backward_weights = _preprocess_weights(layer.backward_layer, weights[num_weights_per_layer:],
-                                               original_keras_version,
-                                               original_backend)
+        forward_weights = preprocess_weights_for_loading(layer.forward_layer,
+                                                         weights[:num_weights_per_layer],
+                                                         original_keras_version,
+                                                         original_backend)
+        backward_weights = preprocess_weights_for_loading(layer.backward_layer,
+                                                          weights[num_weights_per_layer:],
+                                                          original_keras_version,
+                                                          original_backend)
         weights = forward_weights + backward_weights
     elif layer.__class__.__name__ == 'TimeDistributed':
         layer = layer.layer
-        weights = _preprocess_weights(layer, weights, original_keras_version, original_backend)
+        weights = preprocess_weights_for_loading(layer, weights, original_keras_version, original_backend)
     else:
-        weights = _preprocess_weights(layer, weights, original_keras_version, original_backend)
+        if original_keras_version == '1':
+            if layer.__class__.__name__ == 'Conv1D':
+                shape = weights[0].shape
+                # Handle Keras 1.1 format
+                if shape[:2] != (layer.kernel_size[0], 1) or shape[3] != layer.filters:
+                    # Legacy shape:
+                    # (filters, input_dim, filter_length, 1)
+                    assert shape[0] == layer.filters and shape[2:] == (layer.kernel_size[0], 1)
+                    weights[0] = np.transpose(weights[0], (2, 3, 1, 0))
+                weights[0] = weights[0][:, 0, :, :]
 
-    return weights
-
-
-def _preprocess_weights(layer, weights, original_keras_version=None, original_backend=None):
-
-    if original_keras_version == '1':
-        if layer.__class__.__name__ == 'Conv1D':
-            shape = weights[0].shape
-            # Handle Keras 1.1 format
-            if shape[:2] != (layer.kernel_size[0], 1) or shape[3] != layer.filters:
-                # Legacy shape:
-                # (filters, input_dim, filter_length, 1)
-                assert shape[0] == layer.filters and shape[2:] == (layer.kernel_size[0], 1)
-                weights[0] = np.transpose(weights[0], (2, 3, 1, 0))
-            weights[0] = weights[0][:, 0, :, :]
-
-        if layer.__class__.__name__ == 'Conv2D':
-            if layer.data_format == 'channels_first':
-                # old: (filters, stack_size, kernel_rows, kernel_cols)
-                # new: (kernel_rows, kernel_cols, stack_size, filters)
-                weights[0] = np.transpose(weights[0], (2, 3, 1, 0))
-
-        if layer.__class__.__name__ == 'Conv2DTranspose':
-            if layer.data_format == 'channels_last':
-                # old: (kernel_rows, kernel_cols, stack_size, filters)
-                # new: (kernel_rows, kernel_cols, filters, stack_size)
-                weights[0] = np.transpose(weights[0], (0, 1, 3, 2))
-            if layer.data_format == 'channels_first':
-                # old: (filters, stack_size, kernel_rows, kernel_cols)
-                # new: (kernel_rows, kernel_cols, filters, stack_size)
-                weights[0] = np.transpose(weights[0], (2, 3, 0, 1))
-
-        if layer.__class__.__name__ == 'Conv3D':
-            if layer.data_format == 'channels_first':
-                # old: (filters, stack_size, ...)
-                # new: (..., stack_size, filters)
-                weights[0] = np.transpose(weights[0], (2, 3, 4, 1, 0))
-
-        if layer.__class__.__name__ == 'GRU':
-            if len(weights) == 9:
-                kernel = np.concatenate([weights[0],
-                                         weights[3],
-                                         weights[6]], axis=-1)
-                recurrent_kernel = np.concatenate([weights[1],
-                                                   weights[4],
-                                                   weights[7]], axis=-1)
-                bias = np.concatenate([weights[2],
-                                       weights[5],
-                                       weights[8]], axis=-1)
-                weights = [kernel, recurrent_kernel, bias]
-
-        if layer.__class__.__name__ == 'LSTM':
-            if len(weights) == 12:
-                # old: i, c, f, o
-                # new: i, f, c, o
-                kernel = np.concatenate([weights[0],
-                                         weights[6],
-                                         weights[3],
-                                         weights[9]], axis=-1)
-                recurrent_kernel = np.concatenate([weights[1],
-                                                   weights[7],
-                                                   weights[4],
-                                                   weights[10]], axis=-1)
-                bias = np.concatenate([weights[2],
-                                       weights[8],
-                                       weights[5],
-                                       weights[11]], axis=-1)
-                weights = [kernel, recurrent_kernel, bias]
-
-        if layer.__class__.__name__ == 'ConvLSTM2D':
-            if len(weights) == 12:
-                kernel = np.concatenate([weights[0],
-                                         weights[6],
-                                         weights[3],
-                                         weights[9]], axis=-1)
-                recurrent_kernel = np.concatenate([weights[1],
-                                                   weights[7],
-                                                   weights[4],
-                                                   weights[10]], axis=-1)
-                bias = np.concatenate([weights[2],
-                                       weights[8],
-                                       weights[5],
-                                       weights[11]], axis=-1)
+            if layer.__class__.__name__ == 'Conv2D':
                 if layer.data_format == 'channels_first':
                     # old: (filters, stack_size, kernel_rows, kernel_cols)
                     # new: (kernel_rows, kernel_cols, stack_size, filters)
-                    kernel = np.transpose(kernel, (2, 3, 1, 0))
-                    recurrent_kernel = np.transpose(recurrent_kernel,
-                                                    (2, 3, 1, 0))
-                weights = [kernel, recurrent_kernel, bias]
+                    weights[0] = np.transpose(weights[0], (2, 3, 1, 0))
 
-    if original_backend and K.backend() != original_backend:
-        conv_layers = ['Conv1D',
-                       'Conv2D',
-                       'Conv3D',
-                       'Conv2DTranspose']
-        if layer.__class__.__name__ in conv_layers:
-            weights[0] = conv_utils.convert_kernel(weights[0])
-        if layer.__class__.__name__ == 'ConvLSTM2D':
-            weights[0] = conv_utils.convert_kernel(weights[0])
-            weights[1] = conv_utils.convert_kernel(weights[1])
+            if layer.__class__.__name__ == 'Conv2DTranspose':
+                if layer.data_format == 'channels_last':
+                    # old: (kernel_rows, kernel_cols, stack_size, filters)
+                    # new: (kernel_rows, kernel_cols, filters, stack_size)
+                    weights[0] = np.transpose(weights[0], (0, 1, 3, 2))
+                if layer.data_format == 'channels_first':
+                    # old: (filters, stack_size, kernel_rows, kernel_cols)
+                    # new: (kernel_rows, kernel_cols, filters, stack_size)
+                    weights[0] = np.transpose(weights[0], (2, 3, 0, 1))
+
+            if layer.__class__.__name__ == 'Conv3D':
+                if layer.data_format == 'channels_first':
+                    # old: (filters, stack_size, ...)
+                    # new: (..., stack_size, filters)
+                    weights[0] = np.transpose(weights[0], (2, 3, 4, 1, 0))
+
+            if layer.__class__.__name__ == 'GRU':
+                if len(weights) == 9:
+                    kernel = np.concatenate([weights[0],
+                                             weights[3],
+                                             weights[6]], axis=-1)
+                    recurrent_kernel = np.concatenate([weights[1],
+                                                       weights[4],
+                                                       weights[7]], axis=-1)
+                    bias = np.concatenate([weights[2],
+                                           weights[5],
+                                           weights[8]], axis=-1)
+                    weights = [kernel, recurrent_kernel, bias]
+
+            if layer.__class__.__name__ == 'LSTM':
+                if len(weights) == 12:
+                    # old: i, c, f, o
+                    # new: i, f, c, o
+                    kernel = np.concatenate([weights[0],
+                                             weights[6],
+                                             weights[3],
+                                             weights[9]], axis=-1)
+                    recurrent_kernel = np.concatenate([weights[1],
+                                                       weights[7],
+                                                       weights[4],
+                                                       weights[10]], axis=-1)
+                    bias = np.concatenate([weights[2],
+                                           weights[8],
+                                           weights[5],
+                                           weights[11]], axis=-1)
+                    weights = [kernel, recurrent_kernel, bias]
+
+            if layer.__class__.__name__ == 'ConvLSTM2D':
+                if len(weights) == 12:
+                    kernel = np.concatenate([weights[0],
+                                             weights[6],
+                                             weights[3],
+                                             weights[9]], axis=-1)
+                    recurrent_kernel = np.concatenate([weights[1],
+                                                       weights[7],
+                                                       weights[4],
+                                                       weights[10]], axis=-1)
+                    bias = np.concatenate([weights[2],
+                                           weights[8],
+                                           weights[5],
+                                           weights[11]], axis=-1)
+                    if layer.data_format == 'channels_first':
+                        # old: (filters, stack_size, kernel_rows, kernel_cols)
+                        # new: (kernel_rows, kernel_cols, stack_size, filters)
+                        kernel = np.transpose(kernel, (2, 3, 1, 0))
+                        recurrent_kernel = np.transpose(recurrent_kernel,
+                                                        (2, 3, 1, 0))
+                    weights = [kernel, recurrent_kernel, bias]
+
+        if original_backend and K.backend() != original_backend:
+            conv_layers = ['Conv1D',
+                           'Conv2D',
+                           'Conv3D',
+                           'Conv2DTranspose']
+            if layer.__class__.__name__ in conv_layers:
+                weights[0] = conv_utils.convert_kernel(weights[0])
+            if layer.__class__.__name__ == 'ConvLSTM2D':
+                weights[0] = conv_utils.convert_kernel(weights[0])
+                weights[1] = conv_utils.convert_kernel(weights[1])
+
     return weights
 
 
@@ -2981,29 +2977,11 @@ def load_weights_from_hdf5_group_by_name(f, layers):
         for layer in index.get(name, []):
             symbolic_weights = layer.weights
 
-            if layer.__class__.__name__ == 'Bidirectional':
-                nb_weights = len(weight_values) // 2
-                forward_weights = weight_values[:nb_weights]
-                backward_weights = weight_values[nb_weights:]
-                forward_weights = preprocess_weights_for_loading(layer.forward_layer,
-                                                                 forward_weights,
-                                                                 original_keras_version,
-                                                                 original_backend)
-                backward_weights = preprocess_weights_for_loading(layer.backward_layer,
-                                                                  backward_weights,
-                                                                  original_keras_version,
-                                                                  original_backend)
-                weight_values = forward_weights + backward_weights
-            elif layer.__class__.__name__ == 'TimeDistributed':
-                weight_values = preprocess_weights_for_loading(layer.layer,
-                                                               weight_values,
-                                                               original_keras_version,
-                                                               original_backend)
-            else:
-                weight_values = preprocess_weights_for_loading(layer,
-                                                               weight_values,
-                                                               original_keras_version,
-                                                               original_backend)
+            weight_values = preprocess_weights_for_loading(layer,
+                                                           weight_values,
+                                                           original_keras_version,
+                                                           original_backend)
+
             if len(weight_values) != len(symbolic_weights):
                 raise ValueError('Layer #' + str(k) +
                                  ' (named "' + layer.name +
