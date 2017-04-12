e9b73601dd10ac23a1b35cab2434e94758d3b170
John B Nelson <jbn@abreka.com>
2017-04-12 06:32:46 -0700

    Update fit_generator docstr for new API

--- keras/models.py
+++ keras/models.py
@@ -1033,8 +1033,8 @@ class Sequential(Model):
                 - a tuple (inputs, targets, sample_weights).
                 All arrays should contain the same number of samples.
                 The generator is expected to loop over its data
-                indefinitely. An epoch finishes when `samples_per_epoch`
-                samples have been seen by the model.
+                indefinitely. An epoch finishes when `steps_per_epoch`
+                batches have been seen by the model.
             steps_per_epoch: Total number of steps (batches of samples)
                 to yield from `generator` before declaring one epoch
                 finished and starting the next epoch. It should typically
@@ -1087,7 +1087,7 @@ class Sequential(Model):
                         f.close()
 
             model.fit_generator(generate_arrays_from_file('/my_file.txt'),
-                                samples_per_epoch=10000, epochs=10)
+                                steps_per_epoch=1000, epochs=10)
         ```
         """
         if self.model is None:
