47dddaa7fd6947800f5f091336cf822b6db72a51
Taehoon Lee <taehoonlee@snu.ac.kr>
2017-05-05 03:49:37 +0900

    Fix valid condition for TF <-> TH conversion of Conv1D (#6497)

--- keras/utils/conv_utils.py
+++ keras/utils/conv_utils.py
@@ -70,7 +70,7 @@ def convert_kernel(kernel):
     Also works reciprocally, since the transformation is its own inverse.
 
     # Arguments
-        kernel: Numpy array (4D or 5D).
+        kernel: Numpy array (3D, 4D or 5D).
 
     # Returns
         The converted kernel.
@@ -79,7 +79,7 @@ def convert_kernel(kernel):
         ValueError: in case of invalid kernel shape or invalid data_format.
     """
     kernel = np.asarray(kernel)
-    if not 4 <= kernel.ndim <= 5:
+    if not 3 <= kernel.ndim <= 5:
         raise ValueError('Invalid kernel shape:', kernel.shape)
     slices = [slice(None, None, -1) for _ in range(kernel.ndim)]
     no_flip = (slice(None, None), slice(None, None))
