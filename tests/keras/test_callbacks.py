e391bc625d7c2b8385c9f60ce3f2f0544b45d245
Gökcen Eraslan <goekcen.eraslan@helmholtz-muenchen.de>
2017-05-04 21:08:31 +0200

    Tensorboard: Enable grad vis in tb convnet test.

--- tests/keras/test_callbacks.py
+++ tests/keras/test_callbacks.py
@@ -381,7 +381,7 @@ def test_TensorBoard_convnet():
                   optimizer='rmsprop',
                   metrics=['accuracy'])
     tsb = callbacks.TensorBoard(log_dir=filepath, histogram_freq=1,
-                                write_images=True)
+                                write_images=True, write_grads=True)
     cbks = [tsb]
     model.summary()
     history = model.fit(x_train, y_train, epochs=2, batch_size=16,
