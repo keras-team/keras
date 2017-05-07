bb4142919235b80512244d00f258b0bab09e2e8a
Han Lin <hotdogee@gmail.com>
2017-05-08 06:14:33 +0800

    Don't rate limit final update

--- keras/utils/generic_utils.py
+++ keras/utils/generic_utils.py
@@ -246,7 +246,7 @@ class Progbar(object):
 
         now = time.time()
         if self.verbose == 1:
-            if not force and (now - self.last_update) < self.interval:
+            if not force and (now - self.last_update) < self.interval and current < self.target:
                 return
 
             prev_total_width = self.total_width
