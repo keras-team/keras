a1dd556622e137a6f3d01e8244b5aa92b6cbde5c
Fariz Rahman <farizrahman4u@gmail.com>
2017-05-10 02:18:46 +0530

    Update convolutional_recurrent.py

--- keras/layers/convolutional_recurrent.py
+++ keras/layers/convolutional_recurrent.py
@@ -415,7 +415,7 @@ class ConvLSTM2D(ConvRecurrent2D):
     def reset_states(self):
         if not self.stateful:
             raise RuntimeError('Layer must be stateful.')
-        input_shape = self.input_spec.shape
+        input_shape = self.input_spec[0].shape
         output_shape = self.compute_output_shape(input_shape)
         if not input_shape[0]:
             raise ValueError('If a RNN is stateful, a complete '
