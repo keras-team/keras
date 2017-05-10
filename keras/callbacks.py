504bded884bbfa33d2c563154389752e6aef1676
Clara Eng <cengh@users.noreply.github.com>
2017-05-10 13:44:57 -0700

    Add callback to terminate training if NaN loss encountered. (Update to #4849) (#6456)
    * Added callback TerminateOnNaN.
    
    * Added fixes.

--- keras/callbacks.py
+++ keras/callbacks.py
@@ -227,6 +227,21 @@ class BaseLogger(Callback):
                     logs[k] = self.totals[k] / self.seen
 
 
+class TerminateOnNaN(Callback):
+    """Callback that terminates training when a NaN loss is encountered."""
+
+    def __init__(self):
+        super(TerminateOnNaN, self).__init__()
+
+    def on_batch_end(self, batch, logs=None):
+        logs = logs or {}
+        loss = logs.get('loss')
+        if loss is not None:
+            if np.isnan(loss) or np.isinf(loss):
+                print('Batch %d: Invalid loss, terminating training' % (batch))
+                self.model.stop_training = True
+
+
 class ProgbarLogger(Callback):
     """Callback that prints metrics to stdout.
 
