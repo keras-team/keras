714f865022ddd9a5b7ea948153c0cd64fa337e6b
Kevin Mader <kmader@4quant.com>
2017-05-14 01:10:36 +0100

    incorporating pep8 and feedback from @fchollet

--- tests/keras/utils/io_utils_test.py
+++ tests/keras/utils/io_utils_test.py
@@ -1,5 +1,5 @@
-"""Tests for functions in io_utils.py.
-"""
+'''Tests for functions in io_utils.py.
+'''
 import os
 import pytest
 from keras.models import Sequential
@@ -9,8 +9,8 @@ import numpy as np
 import warnings
 import h5py
 
-def create_dataset(h5_path = 'test.h5'):
 
+def create_dataset(h5_path='test.h5'):
     X = np.random.randn(200, 10).astype('float32')
     y = np.random.randint(0, 2, size=(200, 1))
     f = h5py.File(h5_path, 'w')
@@ -22,10 +22,11 @@ def create_dataset(h5_path = 'test.h5'):
     y_dset[:] = y
     f.close()
 
+
 def test_io_utils():
-    """Tests the HDF5Matrix code using the sample from @jfsantos at
+    '''Tests the HDF5Matrix code using the sample from @jfsantos at
     https://gist.github.com/jfsantos/e2ef822c744357a4ed16ec0c885100a3
-    """
+    '''
     h5_path = 'test.h5'
     create_dataset(h5_path)
 
@@ -38,7 +39,7 @@ def test_io_utils():
     y_test = HDF5Matrix(h5_path, 'my_labels', start=150, end=200)
 
     # HDF5Matrix behave more or less like Numpy matrices with regards to indexing
-    print(y_train[10])
+    assert y_train.shape == (150, 1), 'HDF5Matrix shape should match input array'
     # But they do not support negative indices, so don't try print(X_train[-1])
 
     model = Sequential()
@@ -48,20 +49,17 @@ def test_io_utils():
     model.compile(loss='binary_crossentropy', optimizer='sgd')
 
     # Note: you have to use shuffle='batch' or False with HDF5Matrix
-    model.fit(X_train, y_train, batch_size=32, shuffle='batch', verbose = False)
+    model.fit(X_train, y_train, batch_size=32, shuffle='batch', verbose=False)
     # test that evalutation and prediction don't crash and return reasonable results
-    out_pred = model.predict(X_test, batch_size=32, verbose = False)
-    out_eval = model.evaluate(X_test, y_test, batch_size=32, verbose = False)
+    out_pred = model.predict(X_test, batch_size=32, verbose=False)
+    out_eval = model.evaluate(X_test, y_test, batch_size=32, verbose=False)
 
-    assert out_pred.shape == (50,1), "Prediction shape does not match"
-    assert out_eval.shape == (), "Shape of evaluation does not match"
-    assert out_eval>0, "Evaluation value does not meet criteria: {}".format(out_eval)
-    del X_train, y_train, X_test, y_test
+    assert out_pred.shape == (50, 1), 'Prediction shape does not match'
+    assert out_eval.shape == (), 'Shape of evaluation does not match'
+    assert out_eval > 0, 'Evaluation value does not meet criteria: {}'.format(out_eval)
 
-    try:
-        os.remove(h5_path)
-    except:
-        warnings.warn("HDF5 File could not be deleted: {}".format(h5_path), RuntimeWarning)
+    os.remove(h5_path)
+
 
 if __name__ == '__main__':
     pytest.main([__file__])
