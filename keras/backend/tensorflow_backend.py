c6ff9ca578b55df43110ebc82363462ed08282be
ynqa <un.pensiero.vano@gmail.com>
2017-05-28 14:38:41 +0900

    Fix to drop duplicate in one_hot Returns

--- keras/backend/tensorflow_backend.py
+++ keras/backend/tensorflow_backend.py
@@ -2087,11 +2087,8 @@ def one_hot(indices, num_classes):
         num_classes: Integer, number of classes to consider.
 
     # Returns
-        (n + 1)D one hot representation of the input
+        A tensor has (n + 1)D one hot representation of the input
         with shape `(batch_size, dim1, dim2, ... dim(n-1), num_classes)`
-
-    # Returns
-        The one-hot tensor.
     """
     return tf.one_hot(indices, depth=num_classes, axis=-1)
 
