23911b0de2295f4c8d45c5168c942319fdc0eab9
Rizky Luthfianto <mrluthfianto@gmail.com>
2017-06-10 13:36:40 +0700

    add Scaled Exponential Linear Unit activation

--- keras/activations.py
+++ keras/activations.py
@@ -34,6 +34,20 @@ def elu(x, alpha=1.0):
     return K.elu(x, alpha)
 
 
+def selu(x, alpha=1.6732632423543772848170429916717, scale=1.0507009873554804934193349852946):
+    """Scaled Exponential Linear Unit. (Klambauer et al., 2017)
+
+    # Arguments
+        x: A tensor or variable to compute the activation function for.
+        alpha: A scalar, slope of positive section.
+        scale: A scalar, to ensure the slope larger than 1.0 for positive inputs.
+
+    # References
+        - [Self-Normalizing Neural Networks](https://arxiv.org/abs/1706.02515)
+    """
+    return scale * K.elu(x, alpha)
+
+
 def softplus(x):
     return K.softplus(x)
 
