f1878e612286666b9790cf16a3019543519ca65d
Rizky Luthfianto <mrluthfianto@gmail.com>
2017-06-11 12:37:14 +0700

    selu: hardcode alpha and scale variable

--- keras/activations.py
+++ keras/activations.py
@@ -34,7 +34,7 @@ def elu(x, alpha=1.0):
     return K.elu(x, alpha)
 
 
-def selu(x, alpha=1.6732632423543772848170429916717, scale=1.0507009873554804934193349852946):
+def selu(x):
     """Scaled Exponential Linear Unit. (Klambauer et al., 2017)
 
     # Arguments
@@ -45,6 +45,8 @@ def selu(x, alpha=1.6732632423543772848170429916717, scale=1.0507009873554804934
     # References
         - [Self-Normalizing Neural Networks](https://arxiv.org/abs/1706.02515)
     """
+    alpha = 1.6732632423543772848170429916717
+    scale = 1.0507009873554804934193349852946
     return scale * K.elu(x, alpha)
 
 
