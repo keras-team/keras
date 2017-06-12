d4b618bf23d83b1a7306bc6816bd509c23bba15d
Francois Chollet <francois.chollet@gmail.com>
2017-06-12 11:46:21 -0700

    Prepare new PyPI release.

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import find_packages
 
 
 setup(name='Keras',
-      version='2.0.4',
+      version='2.0.5',
       description='Deep Learning for Python',
       author='Francois Chollet',
       author_email='francois.chollet@gmail.com',
       url='https://github.com/fchollet/keras',
-      download_url='https://github.com/fchollet/keras/tarball/2.0.4',
+      download_url='https://github.com/fchollet/keras/tarball/2.0.5',
       license='MIT',
       install_requires=['theano', 'pyyaml', 'six'],
       extras_require={
