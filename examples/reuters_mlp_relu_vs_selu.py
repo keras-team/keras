898a5ed6a1ea6266da8ebe2dbc5aab4de798c502
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2017-06-16 13:27:50 -0400

    Docstring clean-up

--- examples/reuters_mlp_relu_vs_selu.py
+++ examples/reuters_mlp_relu_vs_selu.py
@@ -1,9 +1,13 @@
-'''
+'''Compare self-normalizing MLPs with regular MLPs.
 
 Compare the performance of a simple MLP using two
 different activation functions: RELU and SELU
 on the Reuters newswire topic classification task.
 
+# Reference:
+  Klambauer, G., Unterthiner, T., Mayr, A., & Hochreiter, S. (2017). 
+  Self-Normalizing Neural Networks. arXiv preprint arXiv:1706.02515.
+  https://arxiv.org/abs/1706.02515
 '''
 from __future__ import print_function
 
@@ -31,17 +35,19 @@ def create_network(n_dense=6,
                    optimizer='adam',
                    num_classes=1,
                    max_words=max_words):
-    """Generic function to create a fully connect neural network
+    """Generic function to create a fully-connected neural network.
 
     # Arguments
-        n_dense: int > 0. Number of dense layers
-        dense_units: int > 0. Number of dense units per layer
-        dropout: keras.layers.Layer. a dropout layer to apply
-        dropout_rate: 0 <= float <= 1. the rate of dropout
-        kernel_initializer: str. the initializer for the weights
-        optimizer: str/keras.optimizers.Optimizer. the optimizer to use
-        num_classes: int > 0. the number of classes to predict
-        max_words: int > 0. the maximum number of words per data point
+        n_dense: int > 0. Number of dense layers.
+        dense_units: int > 0. Number of dense units per layer.
+        dropout: keras.layers.Layer. A dropout layer to apply.
+        dropout_rate: 0 <= float <= 1. The rate of dropout.
+        kernel_initializer: str. The initializer for the weights.
+        optimizer: str/keras.optimizers.Optimizer. The optimizer to use.
+        num_classes: int > 0. The number of classes to predict.
+        max_words: int > 0. The maximum number of words per data point.
+    # Returns
+        A Keras model instance.
     """
     model = Sequential()
     model.add(Dense(dense_units, input_shape=(max_words,),
@@ -69,7 +75,7 @@ network1 = {
     'dropout': Dropout,
     'dropout_rate': 0.5,
     'kernel_initializer': 'glorot_uniform',
-    'optimizer': 'adam'
+    'optimizer': 'sgd'
 }
 
 network2 = {
@@ -108,27 +114,33 @@ print('y_test shape:', y_test.shape)
 print('\nBuilding network 1...')
 
 model1 = create_network(num_classes=num_classes, **network1)
-history_model1 = model1.fit(x_train, y_train,
+history_model1 = model1.fit(x_train,
+                            y_train,
                             batch_size=batch_size,
                             epochs=epochs,
                             verbose=1,
                             validation_split=0.1)
 
-
-score_model1 = model1.evaluate(x_test, y_test,
-                               batch_size=batch_size, verbose=1)
+score_model1 = model1.evaluate(x_test,
+                               y_test,
+                               batch_size=batch_size,
+                               verbose=1)
 
 
 print('\nBuilding network 2...')
 model2 = create_network(num_classes=num_classes, **network2)
 
-history_model2 = model2.fit(x_train, y_train,
+history_model2 = model2.fit(x_train,
+                            y_train,
                             batch_size=batch_size,
                             epochs=epochs,
                             verbose=1,
                             validation_split=0.1)
-score_model2 = model2.evaluate(x_test, y_test,
-                               batch_size=batch_size, verbose=1)
+
+score_model2 = model2.evaluate(x_test,
+                               y_test,
+                               batch_size=batch_size,
+                               verbose=1)
 
 print('\nNetwork 1 results')
 print('Hyperparameters:', network1)
@@ -139,14 +151,22 @@ print('Hyperparameters:', network2)
 print('Test score:', score_model2[0])
 print('Test accuracy:', score_model2[1])
 
-plt.plot(range(epochs), history_model1.history[
-         'val_loss'], 'g-', label='Network 1 Val Loss')
-plt.plot(range(epochs), history_model2.history[
-         'val_loss'], 'r-', label='Network 2 Val Loss')
-plt.plot(range(epochs), history_model1.history[
-         'loss'], 'g--', label='Network 1 Loss')
-plt.plot(range(epochs), history_model2.history[
-         'loss'], 'r--', label='Network 2 Loss')
+plt.plot(range(epochs),
+         history_model1.history['val_loss'],
+         'g-',
+         label='Network 1 Val Loss')
+plt.plot(range(epochs),
+         history_model2.history['val_loss'],
+         'r-',
+         label='Network 2 Val Loss')
+plt.plot(range(epochs),
+         history_model1.history['loss'],
+         'g--',
+         label='Network 1 Loss')
+plt.plot(range(epochs),
+         history_model2.history['loss'],
+         'r--',
+         label='Network 2 Loss')
 plt.xlabel('Epochs')
 plt.ylabel('Loss')
 plt.legend()
