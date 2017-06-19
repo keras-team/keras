2ddcd2352243c1fa26ee8004d1a9164c759214d0
Andrew Hundt <ATHundt@gmail.com>
2017-06-19 16:26:51 -0400

    test_multiprocessing.py fixed incorrect usage of np.random.randint() https://docs.scipy.org/doc/numpy-1.11.0/reference/generated/numpy.random.randint.html

--- tests/test_multiprocessing.py
+++ tests/test_multiprocessing.py
@@ -270,27 +270,26 @@ def test_multiprocessing_evaluate_error():
 
 @keras_test
 def test_multiprocessing_predict_error():
-    batch_size = 10
     good_batches = 3
 
     def custom_generator():
         """Raises an exception after a few good batches"""
         for i in range(good_batches):
-            yield (np.random.randint(batch_size, 256, (50, 2)),
-                   np.random.randint(batch_size, 2, 50))
+            yield (np.random.randint(1, 256, (2, 50)),
+                   np.random.randint(1, 256, (2, 50)))
         raise RuntimeError
 
     model = Sequential()
     model.add(Dense(1, input_shape=(2, )))
     model.compile(loss='mse', optimizer='adadelta')
 
-    with pytest.raises((ValueError, Exception)):
+    with pytest.raises((ValueError)):
         model.predict_generator(
             custom_generator(), good_batches + 1, 1,
             workers=4, pickle_safe=True,
         )
 
-    with pytest.raises((ValueError, Exception)):
+    with pytest.raises((ValueError)):
         model.predict_generator(
             custom_generator(), good_batches + 1, 1,
             pickle_safe=False,
