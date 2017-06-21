b6fe3f70d2fcbb3af2fb38d4a4458b484f0e763d
Andrew Hundt <ATHundt@gmail.com>
2017-06-21 00:09:51 -0400

    training.py _check_loss_and_target_compatibility() doesn't crash if y is None

--- keras/engine/training.py
+++ keras/engine/training.py
@@ -259,7 +259,7 @@ def _check_loss_and_target_compatibility(targets, loss_fns, output_shapes):
                   'binary_crossentropy',
                   'categorical_crossentropy'}
     for y, loss, shape in zip(targets, loss_fns, output_shapes):
-        if loss is None:
+        if y is None or loss is None:
             continue
         if loss.__name__ == 'categorical_crossentropy':
             if y.shape[-1] == 1:
