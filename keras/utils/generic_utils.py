04a20177cf332004a291cee626c20a0262427796
Chris <chris.kerr@athion.de>
2017-06-21 18:58:38 +0200

    Avoid DeprecationWarning from inspect.getargspec (rebased) (#7035)
    * Utility function to check if a callable has a given keyword argument
    
    * Added unit tests for the has_arg function
    
    * Replace uses of getargspec with the new has_arg function
    
    Not changing keras.backend, because that gives ImportErrors due to
    a circular import (conv_utils uses the backend, and is imported
    before generic_utils in utils/__init__.py)
    
    Not changing keras.utils.test_utils, because that change exposes
    (what looks to me like) a latent bug
    
    * Replace incorrect use of getargspec in test_utils.py
    
    The previous code would always fail to detect the 'weights' argument.
    Simply replacing getargspec would cause the tests for some of the legacy
    layers to fail because the passed 'weights' argument is bad.
    
    Instead, I have added a check for whether the passed `weights` array
    is empty, this avoids tripping the bug.
    
    * Replacing getargspec with has_arg in the backend modules
    
    This requires reordering imports to avoid errors caused by
    conv_utils trying to import the backend, the backend wanting to
    import generic_utils, and utils/__init__.py listing conv_utils
    before generic_utils.
    
    * Removed getargspec from legacy wrapping function
    
    Instead save the wrapped function in an attribute and call
    getargspec on this attribute during documentation generation.

--- keras/utils/generic_utils.py
+++ keras/utils/generic_utils.py
@@ -132,9 +132,8 @@ def deserialize_keras_object(identifier, module_objects=None,
                 raise ValueError('Unknown ' + printable_module_name +
                                  ': ' + class_name)
         if hasattr(cls, 'from_config'):
-            arg_spec = inspect.getargspec(cls.from_config)
             custom_objects = custom_objects or {}
-            if 'custom_objects' in arg_spec.args:
+            if has_arg(cls.from_config, 'custom_objects'):
                 return cls.from_config(config['config'],
                                        custom_objects=dict(list(_GLOBAL_CUSTOM_OBJECTS.items()) +
                                                            list(custom_objects.items())))
@@ -205,6 +204,48 @@ def func_load(code, defaults=None, closure=None, globs=None):
                                      name=code.co_name,
                                      argdefs=defaults,
                                      closure=closure)
+
+
+def has_arg(fn, name, accept_all=False):
+    """Checks if a callable accepts a given keyword argument.
+
+    For Python 2, checks if there is an argument with the given name.
+
+    For Python 3, checks if there is an argument with the given name, and
+    also whether this argument can be called with a keyword (i.e. if it is
+    not a positional-only argument).
+
+    # Arguments
+        fn: Callable to inspect.
+        name: Check if `fn` can be called with `name` as a keyword argument.
+        accept_all: What to return if there is no parameter called `name`
+                    but the function accepts a `**kwargs` argument.
+
+    # Returns
+        bool, whether `fn` accepts a `name` keyword argument.
+    """
+    if sys.version_info < (3,):
+        arg_spec = inspect.getargspec(fn)
+        if accept_all and arg_spec.keywords is not None:
+            return True
+        return (name in arg_spec.args)
+    elif sys.version_info < (3, 3):
+        arg_spec = inspect.getfullargspec(fn)
+        if accept_all and arg_spec.varkw is not None:
+            return True
+        return (name in arg_spec.args or
+                name in arg_spec.kwonlyargs)
+    else:
+        signature = inspect.signature(fn)
+        parameter = signature.parameters.get(name)
+        if parameter is None:
+            if accept_all:
+                for param in signature.parameters.values():
+                    if param.kind == inspect.Parameter.VAR_KEYWORD:
+                        return True
+            return False
+        return (parameter.kind in (inspect.Parameter.POSITIONAL_OR_KEYWORD,
+                                   inspect.Parameter.KEYWORD_ONLY))
 
 
 class Progbar(object):
