fb97b6e0fa4d7338c6bf6cd722f3d67b117e76e4
Andrew Hundt <ATHundt@gmail.com>
2017-06-22 00:29:33 -0400

    _check_array_lengths properly handles corner cases with None (#7063)
    * _check_array_lengths properly handles corner cases with None
    
    * test_training.py _check_array_lengths() unit test
    
    * set_of_lengths if/else + whitespace
    
    * training.py pep8 whitespace
    
    * training.py restate set more cleanly

--- tests/keras/engine/test_training.py
+++ tests/keras/engine/test_training.py
@@ -7,8 +7,10 @@ import scipy.sparse as sparse
 from keras.layers import Dense, Dropout
 from keras.engine.topology import Input
 from keras.engine.training import Model
+from keras.engine.training import Model
 from keras.engine.training import _check_loss_and_target_compatibility
 from keras.engine.training import _weighted_masked_objective
+from keras.engine.training import _check_array_lengths
 from keras.engine.training import _slice_arrays
 from keras.models import Sequential
 from keras import backend as K
@@ -28,6 +30,27 @@ class RandomSequence(Sequence):
         return [np.random.random((self.batch_size, 3)), np.random.random((self.batch_size, 3))], [
             np.random.random((self.batch_size, 4)),
             np.random.random((self.batch_size, 3))]
+
+
+@keras_test
+def test_check_array_lengths():
+    _check_array_lengths(None, None, None)
+    a_np = np.random.random((4, 3, 3))
+    _check_array_lengths(a_np, a_np, a_np)
+    _check_array_lengths([a_np, a_np], [a_np, a_np], [a_np, a_np])
+    _check_array_lengths([None], [None], [None])
+
+    b_np = np.random.random((3, 4))
+    with pytest.raises(ValueError):
+        _check_array_lengths(a_np, None, None)
+    with pytest.raises(ValueError):
+        _check_array_lengths(a_np, a_np, None)
+    with pytest.raises(ValueError):
+        _check_array_lengths([a_np], [None], None)
+    with pytest.raises(ValueError):
+        _check_array_lengths([a_np], [b_np], None)
+    with pytest.raises(ValueError):
+        _check_array_lengths([a_np], None, [b_np])
 
 
 @keras_test
