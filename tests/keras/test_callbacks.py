2e6091550c068ec97b8c240f0fc2b7d98c0c38f9
arodiss <gleb.sidora@gmail.com>
2017-06-27 20:53:32 +0300

    Added Dead ReLU detector callback

--- tests/keras/test_callbacks.py
+++ tests/keras/test_callbacks.py
@@ -5,6 +5,7 @@ import numpy as np
 import pytest
 from csv import Sniffer
 import shutil
+import warnings
 from keras import optimizers
 from keras import initializers
 from keras import callbacks
@@ -476,6 +477,37 @@ def test_CallbackValData():
     assert cbk.validation_data[0] is cbk2.validation_data[0]
     assert cbk.validation_data[1] is cbk2.validation_data[1]
     assert cbk.validation_data[2].shape == cbk2.validation_data[2].shape
+
+
+@keras_test
+def test_DeadDeadReluDetector():
+    def do_test(weights, expected_warnings, verbose):
+        with warnings.catch_warnings(record=True) as w:
+            dataset = np.ones((1, 1, 1))    # data to be fed as training
+            model = Sequential()
+            model.add(Dense(10, activation='relu', input_shape=(1, 1), use_bias=False, weights=[weights]))
+            model.compile(optimizer='sgd', loss='categorical_crossentropy')
+            model.fit(
+                dataset,
+                np.ones((1, 1, 10)),
+                epochs=1,
+                callbacks=[callbacks.DeadReluDetector(dataset, verbose=verbose)],
+                verbose=False
+            )
+            assert len(w) == expected_warnings
+            for warn_item in w:
+                assert issubclass(warn_item.category, RuntimeWarning)
+                assert "dead neurons" in str(warn_item.message)
+
+    weights_1_dead = np.ones((1, 10))      # weights that correspond to NN with 1/10 neurons dead
+    weights_1_dead[:, 0] = 0
+    weights_2_dead = np.ones((1, 10))      # weights that correspond to NN with 2/10 neurons dead
+    weights_2_dead[:, 0] = 0
+    weights_2_dead[:, 1] = 0
+
+    do_test(weights_1_dead, verbose=True, expected_warnings=1)
+    do_test(weights_1_dead, verbose=False, expected_warnings=0)
+    do_test(weights_2_dead, verbose=True, expected_warnings=1)
 
 
 @keras_test
