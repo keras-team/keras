cad3d1e7d999d083ce31687e6576963cbd79dc28
Frédéric Branchaud-Charron <frederic.branchaud-charron@usherbrooke.ca>
2017-07-06 16:20:33 -0400

    [Performance] Add a manager to share the sequence (#7256)
    * Add a manager to share the sequence
    
    * Add doc
    
    * Style fixes

--- keras/utils/data_utils.py
+++ keras/utils/data_utils.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 
 import hashlib
 import multiprocessing
+import multiprocessing.managers
 import os
 import random
 import shutil
@@ -352,11 +353,37 @@ class Sequence(object):
         raise NotImplementedError
 
 
+class HolderManager(multiprocessing.managers.BaseManager):
+    """Custom manager to share a Holder object."""
+    pass
+
+
+class Holder(object):
+    """Object to encapsulate a Sequence.
+
+    This allows the Sequence to be shared across multiple workers.
+
+    # Arguments
+        seq: Sequence object to be shared.
+    """
+    def __init__(self, seq):
+        self.seq = seq
+
+    def __getitem__(self, idx):
+        return self.seq[idx]
+
+    def __len__(self):
+        return len(self.seq)
+
+# Register the Holder class using the ListProxy (allows __len__ and __getitem__)
+HolderManager.register('Holder', Holder, multiprocessing.managers.ListProxy)
+
+
 def get_index(ds, i):
     """Quick fix for Python2, otherwise, it cannot be pickled.
 
     # Arguments
-        ds: a Sequence object
+        ds: a Holder or Sequence object
         i: index
 
     # Returns
@@ -440,7 +467,9 @@ class OrderedEnqueuer(SequenceEnqueuer):
     def __init__(self, sequence,
                  use_multiprocessing=False,
                  scheduling='sequential'):
-        self.sequence = sequence
+        self.manager = HolderManager()
+        self.manager.start()
+        self.sequence = self.manager.Holder(sequence)
         self.use_multiprocessing = use_multiprocessing
         self.scheduling = scheduling
         self.workers = 0
@@ -517,6 +546,7 @@ class OrderedEnqueuer(SequenceEnqueuer):
         self.executor.close()
         self.executor.join()
         self.run_thread.join(timeout)
+        self.manager.shutdown()
 
 
 class GeneratorEnqueuer(SequenceEnqueuer):
