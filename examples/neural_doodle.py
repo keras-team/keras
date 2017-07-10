99ba51555f1bac68742ee2b85f3d452c0531e343
cclauss <cclauss@bluewin.ch>
2017-07-10 23:43:31 +0200

    Redefine xrange for Python 3 (#7295)
    * xrange = range  # Python 3
    
    * raw_input = input  # Python 3
    
    * Update io_utils.py
    
    * Removing raw_input redefinition
    
    Somehow this breaks the test for this file :-(
    
    ```python
    try:
        raw_input          # Python 2
    except NameError:
        raw_input = input  # Python 3
    ```
    
    * Replace xrange() with range()

--- examples/neural_doodle.py
+++ examples/neural_doodle.py
@@ -154,9 +154,9 @@ def load_mask_labels():
                                img_ncols:].reshape((img_nrows, img_ncols))
 
     stack_axis = 0 if K.image_data_format() == 'channels_first' else -1
-    style_mask = np.stack([style_mask_label == r for r in xrange(num_labels)],
+    style_mask = np.stack([style_mask_label == r for r in range(num_labels)],
                           axis=stack_axis)
-    target_mask = np.stack([target_mask_label == r for r in xrange(num_labels)],
+    target_mask = np.stack([target_mask_label == r for r in range(num_labels)],
                            axis=stack_axis)
 
     return (np.expand_dims(style_mask, axis=0),
@@ -251,7 +251,7 @@ def style_loss(style_image, target_image, style_masks, target_masks):
     assert 3 == K.ndim(style_image) == K.ndim(target_image)
     assert 3 == K.ndim(style_masks) == K.ndim(target_masks)
     loss = K.variable(0)
-    for i in xrange(num_labels):
+    for i in range(num_labels):
         if K.image_data_format() == 'channels_first':
             style_mask = style_masks[i, :, :]
             target_mask = target_masks[i, :, :]
