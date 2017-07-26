cafa2860a311171fbebde938d14d11bcbb2a5755
Eric Wu <ericwu09@gmail.com>
2017-07-26 15:35:20 +0800

    Support return_state parameter in ConvRecurrent2D (#7407)
    * Support return_state parameter in ConvRecurrent2D
    
    Added support for the return_state=True parameter in the ConvRecurrent2D abstract class.
    
    * Fix under-indent error
    
    * Support for return_state parameter for ConvLSTM2D
    
    * return_state unit test
    
    * Fix PEP8 errors
    
    * Fix PEP8 errors
    
    * Update convolutional_recurrent.py
    
    * Fix TF functionality
    
    * Fix Python3.5 error
    
    * Formatting and comment changes
    
    * Adding channels_first
    
    * runtime error
    
    * remove batch size check

--- tests/keras/layers/convolutional_recurrent_test.py
+++ tests/keras/layers/convolutional_recurrent_test.py
@@ -30,6 +30,25 @@ def test_convolutional_recurrent():
                                     input_channel)
 
         for return_sequences in [True, False]:
+
+            # test for return state:
+            input = Input(batch_shape=inputs.shape)
+            kwargs = {'data_format': data_format,
+                      'return_sequences': return_sequences,
+                      'return_state': True,
+                      'stateful': True,
+                      'filters': filters,
+                      'kernel_size': (num_row, num_col),
+                      'padding': 'valid'}
+            layer = convolutional_recurrent.ConvLSTM2D(**kwargs)
+            layer.build(inputs.shape)
+            outputs = layer(input)
+            output, states = outputs[0], outputs[1:]
+            assert len(states) == 2
+            model = Model(input, states[0])
+            state = model.predict(inputs)
+            np.testing.assert_allclose(K.eval(layer.states[0]), state, atol=1e-4)
+
             # test for output shape:
             output = layer_test(convolutional_recurrent.ConvLSTM2D,
                                 kwargs={'data_format': data_format,
