1cb81f2f9fe20074a3df13716ee3c8eb7bdec109
Ehud Ben-Reuven <udi@benreuven.com>
2017-08-03 12:36:15 -0400

    fix issue #7512 (#7513)

--- keras/backend/theano_backend.py
+++ keras/backend/theano_backend.py
@@ -1536,7 +1536,7 @@ def sparse_categorical_crossentropy(target, output, from_logits=False):
     target = T.cast(T.flatten(target), 'int32')
     target = T.extra_ops.to_one_hot(target, nb_class=output.shape[-1])
     target = reshape(target, shape(output))
-    return categorical_crossentropy(output, target, from_logits)
+    return categorical_crossentropy(target, output, from_logits)
 
 
 def binary_crossentropy(target, output, from_logits=False):
