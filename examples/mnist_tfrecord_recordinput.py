mnist_tfrecord_recordinput.py fix accuracy calculation

examples/mnist_tfrecord_recordinput.py
@@ -231,9 +231,9 @@ test_model.compile(optimizer='rmsprop',
                    metrics=['accuracy'])
 test_model.load_weights('saved_wt.h5')
 test_model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
-test_model.summary()
 
 # Take steps for each element of validation data.
-evaluate_steps = y_batch_shape[0]/batch_size
+validation_samples = 10000
+evaluate_steps = validation_samples/batch_size
 loss, acc = test_model.evaluate(y=y_test_in, steps=evaluate_steps)
-print('\nTest accuracy: {0}'.format(acc), ' loss: {0}'.format(loss))
+print('\nTest accuracy: {0}'.format(np.mean(acc)))