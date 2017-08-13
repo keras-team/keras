33a03fdb7908f84adaf4827adfc308365fb73ff2
Mike Clark <wassname@users.noreply.github.com>
2017-08-14 05:28:27 +1200

    Changed exception to warning for NumpyIterator (#7623)
    * Changed exception to warning for NumpyIterator
    
    Right now NumpyIterator wont let us use multichannel data but it's common in satellite datasets such as [Kaggle's Dstl Satellite Imagery Feature Detection](https://www.kaggle.com/c/dstl-satellite-imagery-feature-detection) or [spacenet](https://spacenetchallenge.github.io/). This changes the exception to a warning.
    
    * Removed raise
    
    * fix PEP8 E127

--- keras/preprocessing/image.py
+++ keras/preprocessing/image.py
@@ -779,12 +779,12 @@ class NumpyArrayIterator(Iterator):
                              'with shape', self.x.shape)
         channels_axis = 3 if data_format == 'channels_last' else 1
         if self.x.shape[channels_axis] not in {1, 3, 4}:
-            raise ValueError('NumpyArrayIterator is set to use the '
-                             'data format convention "' + data_format + '" '
-                             '(channels on axis ' + str(channels_axis) + '), i.e. expected '
-                             'either 1, 3 or 4 channels on axis ' + str(channels_axis) + '. '
-                             'However, it was passed an array with shape ' + str(self.x.shape) +
-                             ' (' + str(self.x.shape[channels_axis]) + ' channels).')
+            warnings.warn('NumpyArrayIterator is set to use the '
+                          'data format convention "' + data_format + '" '
+                          '(channels on axis ' + str(channels_axis) + '), i.e. expected '
+                          'either 1, 3 or 4 channels on axis ' + str(channels_axis) + '. '
+                          'However, it was passed an array with shape ' + str(self.x.shape) +
+                          ' (' + str(self.x.shape[channels_axis]) + ' channels).')
         if y is not None:
             self.y = np.asarray(y)
         else:
