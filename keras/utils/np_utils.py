e732a7eb69b7f895a4067535f40f77c3c119ac23
Froskekongen <erlend.aune.1983@gmail.com>
2017-08-24 15:38:45 +0200

    Creating generators for model.fit_generator for np.memmap and np.array

--- keras/utils/np_utils.py
+++ keras/utils/np_utils.py
@@ -40,3 +40,81 @@ def normalize(x, axis=-1, order=2):
     l2 = np.atleast_1d(np.linalg.norm(x, order, axis))
     l2[l2 == 0] = 1
     return x / np.expand_dims(l2, axis)
+
+
+def batchyield_shuffle(x, y, shufflearr=None, batchsize=256, stopiter=None):
+    """Creates a generator over numpy arrays with
+    shuffling that can iterate over memmaped arrays
+
+    # Arguments
+        x: Feature data (numpy array or memmap)
+            to iterate over.
+        y: Target data (numpy array or memmap)
+            to iterate over.
+        shufflearr: (optional) if only you want to
+            iterate over a subset of indices
+        batchsize: size of returned batchsize
+        stopiter: total number of batches
+
+    # Returns
+        generator that returns batches of data
+    """
+    if shufflearr is not None:
+        if isinstance(x, np.ndarray):
+            n_samp = x.shape[0]
+        else:
+            n_samp = x[0].shape[0]
+        shufflearr = np.arange(n_samp)
+    np.random.shuffle(shufflearr)
+    dataind = 0
+    iterations = 0
+    while True:
+        if stopiter:
+            if iterations >= stopiter:
+                break
+        endind = dataind + batchsize
+        if endind > (shufflearr.shape[0] - 1):
+            np.random.shuffle(shufflearr)
+            dataind = 0
+            endind = batchsize
+        batchinds = shufflearr[dataind:endind]
+        dataind += batchsize
+        if isinstance(x, np.ndarray) and isinstance(y, np.ndarray):
+            yield (x[batchinds], y[batchinds])
+        elif isinstance(x, list) and isinstance(y, list):
+            yield ([kk[batchinds] for kk in x], [ss[batchinds] for ss in y])
+        iterations += 1
+
+
+def batchyield_choice(x, y, batchsize=256, stopiter=None):
+    """Creates a generator over numpy arrays a
+    random subset of indices that can iterate
+    over memmaped arrays
+
+    # Arguments
+        x: Feature data (numpy array or memmap)
+            to iterate over.
+        y: Target data (numpy array or memmap)
+            to iterate over.
+        batchsize: size of returned batchsize
+        stopiter: total number of batches
+
+    # Returns
+        generator that returns batches of data
+    """
+    dataind = 0
+    iterations = 0
+    if isinstance(x, np.ndarray):
+        n_samp = x.shape[0]
+    else:
+        n_samp = x[0].shape[0]
+    while True:
+        if stopiter:
+            if iterations >= stopiter:
+                break
+        batchinds = np.random.choice(n_samp, batchsize, replace=False)
+        if isinstance(x, np.ndarray) and isinstance(y, np.ndarray):
+            yield (x[batchinds], y[batchinds])
+        elif isinstance(x, list) and isinstance(y, list):
+            yield ([kk[batchinds] for kk in x], [ss[batchinds] for ss in y])
+        iterations += 1
