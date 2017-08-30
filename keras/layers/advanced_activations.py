0b9b2185236987ee641d50011eaeaafbda00836c
Adam Menges <adam@adammenges.com>
2017-08-30 15:56:49 -0700

    first shot at fixing #7107

--- keras/layers/advanced_activations.py
+++ keras/layers/advanced_activations.py
@@ -35,7 +35,12 @@ class LeakyReLU(Layer):
     def __init__(self, alpha=0.3, **kwargs):
         super(LeakyReLU, self).__init__(**kwargs)
         self.supports_masking = True
-        self.alpha = K.cast_to_floatx(alpha)
+        # self.alpha = K.cast_to_floatx(alpha)
+        # TODO: Undo this with something better
+        try:
+            self.alpha = K.cast_to_floatx(alpha)
+        except TypeError:
+            self.alpha = K.cast_to_floatx(alpha['value'])
 
     def call(self, inputs):
         return K.relu(inputs, alpha=self.alpha)
