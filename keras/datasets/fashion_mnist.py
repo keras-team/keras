219c9a5ea0f0c55ebcba80acd94943282b38dd68
Kashif Rasul <kashif.rasul@gmail.com>
2017-09-03 17:52:46 +0200

    added fashion-mnist dataset

---
+++ keras/datasets/fashion_mnist.py
@@ -0,0 +1,42 @@
+import gzip
+import os
+import struct
+
+from ..utils.data_utils import get_file
+import numpy as np
+
+
+def load_data():
+    """Loads Fashion-MNIST dataset.
+
+    # Returns
+        Tuple of Numpy arrays: `(x_train, y_train), (x_test, y_test)`.
+    """
+    dirname = os.path.join('datasets', 'fashion-mnist')
+    base = 'http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/'
+    files = ['train-labels-idx1-ubyte.gz', 'train-images-idx3-ubyte.gz',
+             't10k-labels-idx1-ubyte.gz', 't10k-images-idx3-ubyte.gz']
+
+    paths = []
+    for file in files:
+        paths.append(get_file(file, origin=base+file, cache_subdir=dirname))
+
+    with gzip.open(paths[0], 'rb') as lbpath:
+        struct.unpack('>II', lbpath.read(8))
+        y_train = np.frombuffer(lbpath.read(), dtype=np.uint8)
+
+    with gzip.open(paths[1], 'rb') as imgpath:
+        struct.unpack(">IIII", imgpath.read(16))
+        x_train = np.frombuffer(imgpath.read(),
+                                dtype=np.uint8).reshape(len(y_train), 784)
+
+    with gzip.open(paths[2], 'rb') as lbpath:
+        struct.unpack('>II', lbpath.read(8))
+        y_test = np.frombuffer(lbpath.read(), dtype=np.uint8)
+
+    with gzip.open(paths[3], 'rb') as imgpath:
+        struct.unpack(">IIII", imgpath.read(16))
+        x_test = np.frombuffer(imgpath.read(),
+                               dtype=np.uint8).reshape(len(y_test), 784)
+
+    return (x_train, y_train), (x_test, y_test)
