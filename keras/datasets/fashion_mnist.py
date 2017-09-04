55b0654c944dfecffced50d790240558fc271a96
Kashif Rasul <kashif.rasul@zalando.de>
2017-09-04 11:11:40 +0200

    reshape as in docs

--- keras/datasets/fashion_mnist.py
+++ keras/datasets/fashion_mnist.py
@@ -25,13 +25,13 @@ def load_data():
 
     with gzip.open(paths[1], 'rb') as imgpath:
         x_train = np.frombuffer(imgpath.read(), np.uint8,
-                                offset=16).reshape(len(y_train), 784)
+                                offset=16).reshape(len(y_train), 28, 28)
 
     with gzip.open(paths[2], 'rb') as lbpath:
         y_test = np.frombuffer(lbpath.read(), np.uint8, offset=8)
 
     with gzip.open(paths[3], 'rb') as imgpath:
         x_test = np.frombuffer(imgpath.read(), np.uint8,
-                               offset=16).reshape(len(y_test), 784)
+                               offset=16).reshape(len(y_test), 28, 28)
 
     return (x_train, y_train), (x_test, y_test)
