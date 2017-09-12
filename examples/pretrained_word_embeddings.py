89032bbda459bbe778080cb12d60bf6bb9c77e83
Robert Grzesik <rob.grzesik@gmail.com>
2017-09-12 10:16:13 -0400

    Reverted to previous commit

--- examples/pretrained_word_embeddings.py
+++ examples/pretrained_word_embeddings.py
@@ -113,7 +113,7 @@ for word, i in word_index.items():
     embedding_vector = embeddings_index.get(word)
     if embedding_vector is not None:
         # words not found in embedding index will be all-zeros.
-        embedding_matrix[i - 1] = embedding_vector
+        embedding_matrix[i] = embedding_vector
 
 # load pre-trained word embeddings into an Embedding layer
 # note that we set trainable = False so as to keep the embeddings fixed
