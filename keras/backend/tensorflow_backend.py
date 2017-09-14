7a715593911db2f7dd2b02a0a6cb383478fbb8f0
Francois Chollet <francois.chollet@gmail.com>
2017-09-13 17:24:06 -0700

    Fix pep8

--- keras/backend/tensorflow_backend.py
+++ keras/backend/tensorflow_backend.py
@@ -2182,17 +2182,16 @@ def get_variable_shape(x):
 
 def print_tensor(x, message=''):
     """Prints `message` and the tensor value when evaluated.
- 
+
      Note that `print_tensor` returns a new tensor identical to `x`
      which should be used in the following code. Otherwise the
      print operation is not taken into account during evaluation.
- 
+
      # Example
      ```python
          >>> x = K.print_tensor(x, message="x is: ")
-         >>> y = K.foo_bar(x)
      ```
-     
+
     # Arguments
         x: Tensor to print.
         message: Message to print jointly with the tensor.
