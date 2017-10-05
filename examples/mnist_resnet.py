51f3ae34e2a5c0a2dbcb05a51ee513d9bef34813
Rowel Atienza <rowel.atienza@gmail.com>
2017-10-05 10:00:35 +0800

    Style fixes

--- examples/mnist_resnet.py
+++ examples/mnist_resnet.py
@@ -32,8 +32,8 @@ else:
     x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
     input_shape = (img_rows, img_cols, 1)
 
-x_train = x_train.astype('float32')/255
-x_test = x_test.astype('float32')/255
+x_train = x_train.astype('float32') / 255
+x_test = x_test.astype('float32') / 255
 print('x_train shape:', x_train.shape)
 print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
@@ -58,14 +58,13 @@ for i in range(3):
 y = keras.layers.Flatten()(x)
 y = Dropout(0.4)(y)
 yout = Dense(num_classes, activation='softmax')(y)
-        
 model = Model(inputs=[xin], outputs=[yout])
-model.compile(loss='categorical_crossentropy', optimizer=SGD(lr=1e-1),\
-        metrics=['accuracy']) 
+model.compile(loss='categorical_crossentropy', optimizer=SGD(lr=1e-1),
+              metrics=['accuracy'])
 model.summary()
 
-model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size, \
-        shuffle=True, validation_data=(x_test, y_test))
+model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size,
+          shuffle=True, validation_data=(x_test, y_test))
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
