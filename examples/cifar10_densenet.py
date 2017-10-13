f3d7968db40f4fe7d3e9406c42051b2bd1d37c92
Rowel Atienza <rowel.atienza@gmail.com>
2017-10-13 14:22:19 +0800

    adding densenet on cifar10 implementation

---
+++ examples/cifar10_densenet.py
@@ -0,0 +1,203 @@
+"""Trains a 100-Layer DenseNet on the CIFAR10 dataset.
+
+With data augmentation:
+Greater than 93.5% test accuracy in 100 epochs
+10min per epoch on GTX 1080Ti
+
+Densely Connected Convolutional Networks
+https://arxiv.org/pdf/1608.06993.pdf
+http://openaccess.thecvf.com/content_cvpr_2017/papers/
+    Huang_Densely_Connected_Convolutional_CVPR_2017_paper.pdf
+Network below is similar to 100-Layer DenseNet-BC (k=12)
+"""
+
+from __future__ import print_function
+import keras
+from keras.layers import Dense, Conv2D, BatchNormalization, Activation
+from keras.layers import MaxPooling2D, AveragePooling2D, Input, Flatten, Dropout
+from keras.optimizers import RMSprop
+from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
+from keras.preprocessing.image import ImageDataGenerator
+from keras import backend as K
+from keras.models import Model
+from keras.datasets import cifar10
+import numpy as np
+import os
+
+# Training params.
+batch_size = 32
+epochs = 100
+data_augmentation = True
+
+# Network architecture params.
+num_classes = 10
+num_dense_blocks = 3
+num_bottleneck_layers = 32
+use_max_pool = False
+growth_rate = 12
+num_filters_bef_dense_block = 2 * growth_rate
+compression_factor = 0.5
+
+# Load the CIFAR10 data.
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+# Input image dimensions.
+# We assume data format "channels_last".
+img_rows = x_train.shape[1]
+img_cols = x_train.shape[2]
+channels = x_train.shape[3]
+
+if K.image_data_format() == 'channels_first':
+    img_rows = x_train.shape[2]
+    img_cols = x_train.shape[3]
+    channels = x_train.shape[1]
+    x_train = x_train.reshape(x_train.shape[0], channels, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], channels, img_rows, img_cols)
+    input_shape = (channels, img_rows, img_cols)
+else:
+    img_rows = x_train.shape[1]
+    img_cols = x_train.shape[2]
+    channels = x_train.shape[3]
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, channels)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, channels)
+    input_shape = (img_rows, img_cols, channels)
+
+# Normalize data.
+x_train = x_train.astype('float32') / 255
+x_test = x_test.astype('float32') / 255
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+print('y_train shape:', y_train.shape)
+
+# Convert class vectors to binary class matrices.
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+# Start model definition.
+# Densenet CNNs (composite function) are made of BN-ReLU-Conv2D
+# For ImageNet, DenseNet uses strides = 2. Cifar10 uses strides = 1.
+inputs = Input(shape=input_shape)
+x = BatchNormalization()(inputs)
+x = Activation('relu')(x)
+x = Conv2D(num_filters_bef_dense_block,
+           kernel_size=7,
+           padding='same',
+           kernel_initializer='he_normal',
+           strides=1)(x)
+
+# For ImageNet, DenseNet uses max pool after 1st conv.
+# For Cifar10, no maxpooling. So, we skip.
+if use_max_pool:
+    x = MaxPooling2D(pool_size=3, strides=2, padding='same')(x)
+
+# Stack of Dense Blocks bridged by Transition Layers.
+for i in range(num_dense_blocks):
+    # A Dense Block is a stack of Bottleneck Layers
+    for j in range(num_bottleneck_layers):
+        y = BatchNormalization()(x)
+        y = Activation('relu')(y)
+        y = Conv2D(4 * growth_rate,
+                   kernel_size=1,
+                   padding='same',
+                   kernel_initializer='he_normal')(y)
+        if not data_augmentation:
+            y = Dropout(0.2)(y)
+        y = BatchNormalization()(y)
+        y = Activation('relu')(y)
+        y = Conv2D(growth_rate,
+                   kernel_size=3,
+                   padding='same',
+                   kernel_initializer='he_normal')(y)
+        if not data_augmentation:
+            y = Dropout(0.2)(y)
+        x = keras.layers.concatenate([x, y])
+
+    # No transition layer after the last Dense Block
+    if i == num_dense_blocks - 1:
+        continue
+
+    # Transition Layer compresses the number of feature maps and reduces the size by 2.
+    num_filters_bef_dense_block += num_bottleneck_layers * growth_rate
+    num_filters_bef_dense_block = int(num_filters_bef_dense_block * compression_factor)
+    y = BatchNormalization()(x)
+    y = Conv2D(num_filters_bef_dense_block,
+               kernel_size=1,
+               padding='same',
+               kernel_initializer='he_normal')(y)
+    if not data_augmentation:
+        y = Dropout(0.2)(y)
+    x = MaxPooling2D()(y)
+
+
+# Add classifier on top.
+# After average pooling, size of feature map is 1 x 1.
+x = AveragePooling2D(pool_size=7)(x)
+y = Flatten()(x)
+outputs = Dense(num_classes, kernel_initializer='he_normal',
+                activation='softmax')(y)
+
+# Instantiate and compile model.
+# Orig paper uses SGD but RMSprop works better for DenseNet
+model = Model(inputs=inputs, outputs=outputs)
+model.compile(loss='categorical_crossentropy',
+              optimizer=RMSprop(1e-3),
+              metrics=['accuracy'])
+model.summary()
+
+# Prepare model model saving directory.
+save_dir = os.path.join(os.getcwd(), 'saved_models')
+model_name = 'cifar10_densenet_model.{epoch:02d}.h5'
+if not os.path.isdir(save_dir):
+    os.makedirs(save_dir)
+filepath = os.path.join(save_dir, model_name)
+
+# Prepare callbacks for model saving and for learning rate reducer.
+checkpoint = ModelCheckpoint(filepath=filepath,
+                             verbose=1,
+                             save_best_only=False)
+lr_reducer = ReduceLROnPlateau(factor=np.sqrt(0.1),
+                               cooldown=0,
+                               patience=5,
+                               min_lr=0.5e-6)
+callbacks = [checkpoint, lr_reducer]
+
+# Run training, with or without data augmentation.
+if not data_augmentation:
+    print('Not using data augmentation.')
+    model.fit(x_train, y_train,
+              batch_size=batch_size,
+              epochs=epochs,
+              validation_data=(x_test, y_test),
+              shuffle=True,
+              callbacks=callbacks)
+else:
+    print('Using real-time data augmentation.')
+    # This will do preprocessing and realtime data augmentation:
+    datagen = ImageDataGenerator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
+        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip=True,  # randomly flip images
+        vertical_flip=False)  # randomly flip images
+
+    # Compute quantities required for featurewise normalization
+    # (std, mean, and principal components if ZCA whitening is applied).
+    datagen.fit(x_train)
+
+    # Fit the model on the batches generated by datagen.flow().
+    model.fit_generator(datagen.flow(x_train, y_train, batch_size=batch_size),
+                        steps_per_epoch=x_train.shape[0] // batch_size,
+                        validation_data=(x_test, y_test),
+                        epochs=epochs, verbose=1, workers=4,
+                        callbacks=callbacks)
+
+# Score trained model.
+scores = model.evaluate(x_test, y_test, verbose=1)
+print('Test loss:', scores[0])
+print('Test accuracy:', scores[1])
