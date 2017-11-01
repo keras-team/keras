02ce95b047fcb126c931fdb60540172372231da2
Oleg Zabluda <ozabluda@gmail.com>
2017-11-01 08:16:54 -0700

    pep8

--- examples/mnist_tfrecord.py
+++ examples/mnist_tfrecord.py
@@ -130,7 +130,7 @@ coord = tf.train.Coordinator()
 threads = tf.train.start_queue_runners(sess, coord)
 
 train_model.fit(epochs=epochs,
-    steps_per_epoch=int(np.ceil(data.train.num_examples / float(batch_size))))
+                steps_per_epoch=int(np.ceil(data.train.num_examples / float(batch_size))))
 
 # Save the model weights.
 train_model.save_weights('saved_wt.h5')
