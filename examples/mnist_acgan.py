1c5de050137ed3050d607239eb49d7482696b497
Oleg Zabluda <ozabluda@gmail.com>
2017-11-02 11:46:19 -0700

    generator.compile() as has no effect. Remove.

--- examples/mnist_acgan.py
+++ examples/mnist_acgan.py
@@ -149,8 +149,6 @@ if __name__ == '__main__':
 
     # build the generator
     generator = build_generator(latent_size)
-    generator.compile(optimizer=Adam(lr=adam_lr, beta_1=adam_beta_1),
-                      loss='binary_crossentropy')
 
     latent = Input(shape=(latent_size, ))
     image_class = Input(shape=(1,), dtype='int32')
