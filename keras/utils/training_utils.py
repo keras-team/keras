925ca148de9f5367c6fa148554ba552435df6ab5
Hans Gaiser <hansg91@gmail.com>
2017-11-17 20:46:17 +0100

    Copy name of inputs/outputs in multi_gpu_model. (#8505)

--- keras/utils/training_utils.py
+++ keras/utils/training_utils.py
@@ -171,7 +171,7 @@ def multi_gpu_model(model, gpus):
     # Merge outputs on CPU.
     with tf.device('/cpu:0'):
         merged = []
-        for outputs in all_outputs:
+        for name, outputs in zip(model.output_names, all_outputs):
             merged.append(concatenate(outputs,
-                                      axis=0))
+                                      axis=0, name=name))
         return Model(model.inputs, merged)
