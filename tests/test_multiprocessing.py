6db71202af1e973d06ab3c72fe012469835d138d
Maarten de Vries <maarten@de-vri.es>
2017-11-22 11:37:39 +0100

    Fix randint usage in test_multiprocessing

--- tests/test_multiprocessing.py
+++ tests/test_multiprocessing.py
@@ -232,7 +232,7 @@ def test_multiprocessing_fit_error():
         """Raises an exception after a few good batches"""
         for i in range(good_batches):
             yield (np.random.randint(batch_size, 256, (50, 2)),
-                   np.random.randint(batch_size, 2, 50))
+                   np.random.randint(batch_size, 12, 50))
         raise RuntimeError
 
     model = Sequential()
@@ -258,12 +258,13 @@ def test_multiprocessing_fit_error():
 def test_multiprocessing_evaluate_error():
     batch_size = 10
     good_batches = 3
+    workers = 4
 
     def custom_generator():
         """Raises an exception after a few good batches"""
         for i in range(good_batches):
             yield (np.random.randint(batch_size, 256, (50, 2)),
-                   np.random.randint(batch_size, 2, 50))
+                   np.random.randint(batch_size, 12, 50))
         raise RuntimeError
 
     model = Sequential()
@@ -272,8 +273,8 @@ def test_multiprocessing_evaluate_error():
 
     with pytest.raises(StopIteration):
         model.evaluate_generator(
-            custom_generator(), good_batches + 1, 1,
-            workers=4, use_multiprocessing=True,
+            custom_generator(), good_batches * workers + 1, 1,
+            workers=workers, use_multiprocessing=True,
         )
 
     with pytest.raises(StopIteration):
