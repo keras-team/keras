0267ac4cf03d8f7d0f48a4c353c5c0656cc844fb
Evan Hubinger <evanjhub@gmail.com>
2017-11-23 02:17:45 -0800

    Fix function serialization

--- keras/utils/generic_utils.py
+++ keras/utils/generic_utils.py
@@ -9,6 +9,7 @@ import six
 import marshal
 import types as python_types
 import inspect
+import codecs
 
 _GLOBAL_CUSTOM_OBJECTS = {}
 
@@ -172,7 +173,8 @@ def func_dump(func):
     # Returns
         A tuple `(code, defaults, closure)`.
     """
-    code = marshal.dumps(func.__code__).decode('raw_unicode_escape')
+    raw_code = marshal.dumps(func.__code__)
+    code = codecs.encode(raw_code, "base64")
     defaults = func.__defaults__
     if func.__closure__:
         closure = tuple(c.cell_contents for c in func.__closure__)
@@ -197,7 +199,7 @@ def func_load(code, defaults=None, closure=None, globs=None):
         code, defaults, closure = code
         if isinstance(defaults, list):
             defaults = tuple(defaults)
-    code = marshal.loads(code.encode('raw_unicode_escape'))
+    code = marshal.loads(codecs.decode(six.binary_type(code), "base64"))
     if globs is None:
         globs = globals()
     return python_types.FunctionType(code, globs,
