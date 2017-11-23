9482d03328a983e8da20064e053eee78b0665db5
罗诚 <luoch@users.noreply.github.com>
2017-11-23 17:52:23 +0800

    fix list_devices() function is not available issue
    When using tensorflow as backend and the version of tensorflow is under r1.3, the list_devices() function is not available of Session instance. This may cause some issue like keras.utils.multi_gpu_model(model, gpus) function can not work under r1.3 of tensorflow. This change is a hack to fix that issue.

--- keras/backend/tensorflow_backend.py
+++ keras/backend/tensorflow_backend.py
@@ -5,6 +5,7 @@ from tensorflow.python.ops import control_flow_ops
 from tensorflow.python.ops import functional_ops
 from tensorflow.python.ops import ctc_ops as ctc
 from tensorflow.python.ops import variables as tf_variables
+from tensorflow.python.client import device_lib
 
 from collections import defaultdict
 
@@ -186,6 +187,10 @@ def get_session():
                     v._keras_initialized = True
                 if uninitialized_vars:
                     session.run(tf.variables_initializer(uninitialized_vars))
+    # hotfix for list_devices() function. 
+    # list_devices() function is not available under tensorflow r1.3
+    if hasattr(session, 'list_devices') is False:
+        session.list_devices = lambda: device_lib.list_local_devices()
     return session
 
 
