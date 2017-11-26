62577379c2c6a3f560f969c9b0f753193325bc9b
WarpTime <amey.desai07@gmail.com>
2017-11-26 18:38:49 +0530

    Add Hamming metric for multilabel classification

--- keras/metrics.py
+++ keras/metrics.py
@@ -40,6 +40,11 @@ def top_k_categorical_accuracy(y_true, y_pred, k=5):
 def sparse_top_k_categorical_accuracy(y_true, y_pred, k=5):
     return K.mean(K.in_top_k(y_pred, K.cast(K.max(y_true, axis=-1), 'int32'), k), axis=-1)
 
+def binary_hamming(y_true, y_pred):
+    y_true = y_true - 0.5 + K.epsilon()
+    y_pred = y_pred - 0.5 + K.epsilon()
+    hamming_dist = (K.prod((y_true, y_pred), axis=0) > 0)
+    return K.mean(hamming_dist)
 
 # Aliases
 
@@ -48,7 +53,7 @@ mae = MAE = mean_absolute_error
 mape = MAPE = mean_absolute_percentage_error
 msle = MSLE = mean_squared_logarithmic_error
 cosine = cosine_proximity
-
+hamming = binary_hamming
 
 def serialize(metric):
     return metric.__name__
