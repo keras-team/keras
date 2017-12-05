a2549c2eda0a8536b6801f0c4a9595579e93f580
Kashif Rasul <kashif.rasul@zalando.de>
2017-12-05 16:49:52 +0100

    initial AMSgrad implementation

--- tests/keras/optimizers_test.py
+++ tests/keras/optimizers_test.py
@@ -106,6 +106,12 @@ def test_nadam():
 
 
 @keras_test
+def test_amsgrad():
+    _test_optimizer(optimizers.AMSgrad())
+    _test_optimizer(optimizers.AMSgrad(decay=1e-3))
+
+
+@keras_test
 def test_clipnorm():
     sgd = optimizers.SGD(lr=0.01, momentum=0.9, clipnorm=0.5)
     _test_optimizer(sgd)
