77a52279317ec4dd452f33e858cf4bcc824f6bd4
Kashif Rasul <kashif.rasul@gmail.com>
2017-12-10 20:20:34 +0100

    pep8

--- keras/optimizers.py
+++ keras/optimizers.py
@@ -460,7 +460,7 @@ class Adam(Optimizer):
                   'beta_2': float(K.get_value(self.beta_2)),
                   'decay': float(K.get_value(self.decay)),
                   'epsilon': self.epsilon,
-                  'amsgrad': self.amsgrad,}
+                  'amsgrad': self.amsgrad}
         base_config = super(Adam, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
 
