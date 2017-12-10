fb36e95fdaf490f595b7a4bae08598246f98ea61
Oleg Zabluda <ozabluda@gmail.com>
2017-12-10 11:35:34 -0800

    Sync API of Sequential.evaluate() with Model.evaluate() (feeding from framework-native tensors) (#8694)
    * Sync API of Sequential.evaluate() with Model.evaluate() (feeding from framework-native tensors)
    
    * Add steps parameter

--- keras/models.py
+++ keras/models.py
@@ -971,17 +971,28 @@ class Sequential(Model):
                               steps_per_epoch=steps_per_epoch,
                               validation_steps=validation_steps)
 
-    def evaluate(self, x, y, batch_size=None, verbose=1,
-                 sample_weight=None):
+    def evaluate(self, x=None, y=None,
+                 batch_size=None,
+                 verbose=1,
+                 sample_weight=None,
+                 steps=None):
         """Computes the loss on some input data, batch by batch.
 
         # Arguments
             x: input data, as a Numpy array or list of Numpy arrays
                 (if the model has multiple inputs).
+                `x` can be `None` (default) if feeding from
+                framework-native tensors (e.g. TensorFlow data tensors).
             y: labels, as a Numpy array.
+                `y` can be `None` (default) if feeding from
+                framework-native tensors (e.g. TensorFlow data tensors).
             batch_size: Integer. If unspecified, it will default to 32.
             verbose: verbosity mode, 0 or 1.
             sample_weight: sample weights, as a Numpy array.
+            steps: Integer or `None`.
+                Total number of steps (batches of samples)
+                before declaring the evaluation round finished.
+                Ignored with the default value of `None`.
 
         # Returns
             Scalar test loss (if the model has no metrics)
@@ -998,7 +1009,8 @@ class Sequential(Model):
         return self.model.evaluate(x, y,
                                    batch_size=batch_size,
                                    verbose=verbose,
-                                   sample_weight=sample_weight)
+                                   sample_weight=sample_weight,
+                                   steps=steps)
 
     def predict(self, x, batch_size=None, verbose=0):
         """Generates output predictions for the input samples.
