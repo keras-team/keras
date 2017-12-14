5ad7e5c33e511c2c7a4252131c707b2fc3dbd9c2
Jiajie Chen <jiajiechen0319@gmail.com>
2017-12-14 12:05:37 -0800

    fix backend for mlp

--- keras/engine/topology.py
+++ keras/engine/topology.py
@@ -408,7 +408,6 @@ class Layer(object):
         initializer = initializers.get(initializer)
         if dtype is None:
             dtype = K.floatx()
-        print("Initialized result", initializer(shape))
         weight = K.variable(initializer(shape),
                             dtype=dtype,
                             name=name,
