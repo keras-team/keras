106b715b3662293b4be3fc12dd1f0b31a2e37316
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2017-12-31 00:16:07 +0900

    Blank spaces this time.

--- keras/engine/training.py
+++ keras/engine/training.py
@@ -30,12 +30,12 @@ def _standardize_input_data(data, names, shapes=None,
                             check_batch_axis=True,
                             exception_prefix=''):
     """Normalizes inputs and targets provided by users.
-	
+
     Users may pass data as a list of arrays, dictionary of arrays,
     or as a single array. We normalize this to an ordered list of
     arrays (same order as `names`), while checking that the provided
     arrays have shapes that match the network's expectations.
-	
+
     # Arguments
         data: User-provided input data (polymorphic).
         names: List of expected array names.
@@ -44,10 +44,10 @@ def _standardize_input_data(data, names, shapes=None,
             the batch axis of the arrays matches the expected
             value found in `shapes`.
         exception_prefix: String prefix used for exception formatting.
-		
+
     # Returns
         List of standardized input arrays (one array per model input).
-		
+
     # Raises
         ValueError: in case of improperly formatted user-provided data.
     """
