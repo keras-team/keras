2082d2b383755c3baab3ca1f686ad3264c7b70b7
gabrieldemarmiesse <gabrieldemarmiesse@hotmail.fr>
2018-01-02 13:21:52 +0900

    Changed exception type.

--- keras/utils/data_utils.py
+++ keras/utils/data_utils.py
@@ -187,7 +187,7 @@ def get_file(fname,
 
     try:
         return get_file_from(datadir_base, *list_args)
-    except PermissionError:
+    except OSError:
         datadir_base = os.path.join('/tmp', '.keras')
         return get_file_from(datadir_base, *list_args)
 
