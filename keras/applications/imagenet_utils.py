4778fa0cec5e35be043fd157ce70b99421b621e1
Francois Chollet <francois.chollet@gmail.com>
2018-01-09 15:17:21 -0800

    Further formatting fixes

--- keras/applications/imagenet_utils.py
+++ keras/applications/imagenet_utils.py
@@ -1,3 +1,5 @@
+"""Utilities for ImageNet data preprocessing & prediction decoding.
+"""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
