6f09be91e695d74f19aac084808b998ecf6eb24e
Francois Chollet <francois.chollet@gmail.com>
2018-01-12 15:24:06 -0800

    Remove unused variable

--- keras/preprocessing/image.py
+++ keras/preprocessing/image.py
@@ -749,8 +749,9 @@ class ImageDataGenerator(object):
         if self.zca_whitening:
             flat_x = np.reshape(x, (x.shape[0], x.shape[1] * x.shape[2] * x.shape[3]))
             num_examples = flat_x.shape[0]
-            u, s, vt = linalg.svd(flat_x / np.sqrt(num_examples))
-            s_expand = np.hstack((s, np.zeros(vt.shape[0] - num_examples, dtype=flat_x.dtype)))
+            _, s, vt = linalg.svd(flat_x / np.sqrt(num_examples))
+            s_expand = np.hstack((s, np.zeros(vt.shape[0] - num_examples,
+                                              dtype=flat_x.dtype)))
             self.principal_components = (vt.T / np.sqrt(s_expand ** 2 + self.zca_epsilon)).dot(vt)
 
 
