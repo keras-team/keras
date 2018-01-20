480ea9594e2d6a1a3b506dff8b249237aed3933e
Andrew Hundt <athundt@gmail.com>
2018-01-19 21:49:40 -0500

    EvaluateInputTensor Callback moved to mnist_tfrecord.py

--- examples/mnist_tfrecord.py
+++ examples/mnist_tfrecord.py
@@ -42,7 +42,7 @@ import tensorflow as tf
 import keras
 from keras import backend as K
 from keras import layers
-from keras.callbacks import EvaluateInputTensor
+from keras.callbacks import Callback
 
 from tensorflow.contrib.learn.python.learn.datasets import mnist
 
@@ -51,6 +51,53 @@ if K.backend() != 'tensorflow':
                        'TensorFlow backend, '
                        'because it requires TFRecords, which '
                        'are not supported on other platforms.')
+
+
+class EvaluateInputTensor(Callback):
+    """ Validate a model which does not expect external numpy data during training.
+
+    Keras does not expect external numpy data at training time, and thus cannot
+    accept numpy arrays for validation when all of a Keras Model's
+    `Input(input_tensor)` layers are provided an  `input_tensor` parameter,
+    and the call to `Model.compile(target_tensors)` defines all `target_tensors`.
+    Instead, create a second model for validation which is also configured
+    with input tensors and add it to the `EvaluateInputTensor` callback
+    to perform validation.
+
+    It is recommended that this callback be the first in the list of callbacks
+    because it defines the validation variables required by many other callbacks,
+    and Callbacks are made in order.
+
+    # Arguments
+        model: Keras model on which to call model.evaluate().
+        steps: Integer or `None`.
+            Total number of steps (batches of samples)
+            before declaring the evaluation round finished.
+            Ignored with the default value of `None`.
+    """
+
+    def __init__(self, model, steps, metrics_prefix='val', verbose=1):
+        # parameter of callbacks passed during initialization
+        # pass evalation mode directly
+        super(EvaluateInputTensor, self).__init__()
+        self.val_model = model
+        self.num_steps = steps
+        self.verbose = verbose
+        self.metrics_prefix = metrics_prefix
+
+    def on_epoch_end(self, epoch, logs={}):
+        self.val_model.set_weights(self.model.get_weights())
+        results = self.val_model.evaluate(None, None, steps=int(self.num_steps),
+                                          verbose=self.verbose)
+        metrics_str = '\n'
+        for result, name in zip(results, self.val_model.metrics_names):
+            metric_name = self.metrics_prefix + '_' + name
+            logs[metric_name] = result
+            if self.verbose > 0:
+                metrics_str = metrics_str + metric_name + ': ' + str(result) + ' '
+
+        if self.verbose > 0:
+            print(metrics_str)
 
 
 def cnn_layers(x_train_input):
--- tests/keras/test_callbacks.py
+++ tests/keras/test_callbacks.py
@@ -550,52 +550,6 @@ def test_TensorBoard_histogram_freq_must_have_validation_data(tmpdir):
     assert 'validation_data must be provided' in str(raised_exception.value)
 
 
-@pytest.mark.skipif(K.backend() != 'tensorflow', reason='Requires TensorFlow backend')
-@keras_test
-def test_EvaluateInputTensor():
-    """We test building a model with a TF variable as input.
-    We should be able to call fit with the EvaluateInputTensor callback.
-    """
-    import tensorflow as tf
-
-    input_a = K.variable(np.random.random((10, 3)))
-    input_b = K.variable(np.random.random((10, 3)))
-
-    output_a = K.variable(np.random.random((10, 4)))
-    output_b = K.variable(np.random.random((10, 3)))
-
-    a = Input(tensor=input_a)
-    b = Input(tensor=input_b, name='input_b')
-
-    a_2 = Dense(4, name='dense_1')(a)
-    dp = Dropout(0.5, name='dropout')
-    b_2 = dp(b)
-
-    model = Model([a, b], [a_2, b_2])
-    model.summary()
-
-    optimizer = 'rmsprop'
-    loss = 'mse'
-    loss_weights = [1., 0.5]
-    model.compile(optimizer, loss, metrics=['mean_squared_error'],
-                  loss_weights=loss_weights,
-                  sample_weight_mode=None,
-                  target_tensors=[output_a, output_b])
-
-    eval_model = Model([a, b], [a_2, b_2])
-    eval_model.compile(optimizer, loss, metrics=['mean_squared_error'],
-                       loss_weights=loss_weights,
-                       sample_weight_mode=None,
-                       target_tensors=[output_a, output_b])
-
-    eval_callback = callbacks.EvaluateInputTensor(model=eval_model, steps=1)
-
-    # test fit
-    out = model.fit(None, None, epochs=1,
-                    callbacks=[eval_callback],
-                    steps_per_epoch=1)
-
-
 @keras_test
 @pytest.mark.skipif((K.backend() != 'tensorflow'),
                     reason='Requires TensorFlow backend')
