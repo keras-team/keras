10b46cbdc47f7164cd92209077939a7f7882b3c0
Francois Chollet <francois.chollet@gmail.com>
2018-01-24 13:13:26 -0800

    Simplify VAE examples

--- examples/variational_autoencoder_deconv.py
+++ examples/variational_autoencoder_deconv.py
@@ -6,11 +6,13 @@ with Keras and deconvolution layers.
 - Auto-Encoding Variational Bayes
   https://arxiv.org/abs/1312.6114
 '''
+from __future__ import print_function
+
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import norm
 
-from keras.layers import Input, Dense, Lambda, Flatten, Reshape, Layer
+from keras.layers import Input, Dense, Lambda, Flatten, Reshape
 from keras.layers import Conv2D, Conv2DTranspose
 from keras.models import Model
 from keras import backend as K
@@ -109,32 +111,18 @@ deconv_2_decoded = decoder_deconv_2(deconv_1_decoded)
 x_decoded_relu = decoder_deconv_3_upsamp(deconv_2_decoded)
 x_decoded_mean_squash = decoder_mean_squash(x_decoded_relu)
 
+# instantiate VAE model
+vae = Model(x, x_decoded_mean_squash)
 
-# Custom loss layer
-class CustomVariationalLayer(Layer):
-    def __init__(self, **kwargs):
-        self.is_placeholder = True
-        super(CustomVariationalLayer, self).__init__(**kwargs)
+# Compute VAE loss
+xent_loss = img_rows * img_cols * metrics.binary_crossentropy(
+    K.flatten(x),
+    K.flatten(x_decoded_mean_squash))
+kl_loss = - 0.5 * K.sum(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)
+vae_loss = K.mean(xent_loss + kl_loss)
+vae.add_loss(vae_loss)
 
-    def vae_loss(self, x, x_decoded_mean_squash):
-        x = K.flatten(x)
-        x_decoded_mean_squash = K.flatten(x_decoded_mean_squash)
-        xent_loss = img_rows * img_cols * metrics.binary_crossentropy(x, x_decoded_mean_squash)
-        kl_loss = - 0.5 * K.mean(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)
-        return K.mean(xent_loss + kl_loss)
-
-    def call(self, inputs):
-        x = inputs[0]
-        x_decoded_mean_squash = inputs[1]
-        loss = self.vae_loss(x, x_decoded_mean_squash)
-        self.add_loss(loss, inputs=inputs)
-        # We don't use this output.
-        return x
-
-
-y = CustomVariationalLayer()([x, x_decoded_mean_squash])
-vae = Model(x, y)
-vae.compile(optimizer='rmsprop', loss=None)
+vae.compile(optimizer='rmsprop')
 vae.summary()
 
 # train the VAE on MNIST digits
