def319da7bec1fb41824a634c3dc88e328bba728
Oleg Zabluda <ozabluda@gmail.com>
2018-01-27 21:03:10 -0800

    Add docstrings

--- keras/preprocessing/image.py
+++ keras/preprocessing/image.py
@@ -382,8 +382,16 @@ class ImageDataGenerator(object):
         zca_whitening: apply ZCA whitening.
         zca_epsilon: epsilon for ZCA whitening. Default is 1e-6.
         rotation_range: degrees (0 to 180).
-        width_shift_range: fraction of total width, if < 1, or pixels if >= 1.
-        height_shift_range: fraction of total height, if < 1, or pixels if >= 1.
+        width_shift_range: float, 1-D array-like or int
+            float: fraction of total width, if < 1, or pixels if >= 1.
+            1-D array-like: random elements from the array.
+            int: integer number of pixels from interval
+                `(-width_shift_range, +width_shift_range)`
+            With `width_shift_range=2` possible values are integers [-1, 0, +1],
+            same as with `width_shift_range=[-1, 0, +1]`,
+            while with `width_shift_range=1.0` possible values are floats in
+            the interval [-1.0, +1.0).
+        height_shift_range: float, 1-D array-like or int. See `width_shift_range`
         shear_range: shear intensity (shear angle in radians).
         zoom_range: amount of zoom. if scalar z, zoom will be randomly picked
             in the range [1-z, 1+z]. A sequence of two can be passed instead
