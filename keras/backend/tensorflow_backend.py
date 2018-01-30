533a8acf0da8f93adc76dd753a664e39163e22d2
Andrew Hundt <ATHundt@gmail.com>
2018-01-30 14:07:57 -0500

    keras.backend.arange doc default start is 0 (#9250)
    * keras.backend.arange doc default start is 0
    
    * tensorflow_backend.py arange start -> "start"
    
    * tensorflow_backend.py remove comma

--- keras/backend/tensorflow_backend.py
+++ keras/backend/tensorflow_backend.py
@@ -2046,7 +2046,7 @@ def arange(start, stop=None, step=1, dtype='int32'):
 
     The function arguments use the same convention as
     Theano's arange: if only one argument is provided,
-    it is in fact the "stop" argument.
+    it is in fact the "stop" argument and "start" is 0.
 
     The default type of the returned tensor is `'int32'` to
     match TensorFlow's default.
