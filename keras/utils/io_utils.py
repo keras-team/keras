814af92ee45f8ed01524804ec039523b01d04365
cclauss <cclauss@bluewin.ch>
2018-01-30 14:06:43 +0100

    get_input() in the script

--- keras/utils/io_utils.py
+++ keras/utils/io_utils.py
@@ -6,7 +6,7 @@ from __future__ import print_function
 import numpy as np
 from collections import defaultdict
 
-from six.moves import input
+from six.moves import input as get_input
 try:
     import h5py
 except ImportError:
@@ -141,10 +141,10 @@ def ask_to_proceed_with_overwrite(filepath):
     # Returns
         True if we can proceed with overwrite, False otherwise.
     """
-    overwrite = input('[WARNING] %s already exists - overwrite? '
-                      '[y/n]' % (filepath)).strip().lower()
+    overwrite = get_input('[WARNING] %s already exists - overwrite? '
+                          '[y/n]' % (filepath)).strip().lower()
     while overwrite not in ('y', 'n'):
-        overwrite = input('Enter "y" (overwrite) or "n" (cancel).').lower()
+        overwrite = get_input('Enter "y" (overwrite) or "n" (cancel).').lower()
     if overwrite == 'n':
         return False
     print('[TIP] Next time specify overwrite=True!')
