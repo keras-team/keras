ad00676b80556a6354180a1bfa3009d4db316d3e
Oleg Zabluda <ozabluda@gmail.com>
2018-02-02 14:04:59 -0800

    Pass current learning rate to schedule() in LearningRateScheduler (#8865)
    * Pass current learning rate to schedule() in LearningRateScheduler
    
    * pep8
    
    * Use keyword arguments
    
    * make first argument epoch positional

--- keras/callbacks.py
+++ keras/callbacks.py
@@ -564,8 +564,8 @@ class LearningRateScheduler(Callback):
 
     # Arguments
         schedule: a function that takes an epoch index as input
-            (integer, indexed from 0) and returns a new
-            learning rate as output (float).
+            (integer, indexed from 0) and current learning rate
+            and returns a new learning rate as output (float).
         verbose: int. 0: quiet, 1: update messages.
     """
 
@@ -577,7 +577,11 @@ class LearningRateScheduler(Callback):
     def on_epoch_begin(self, epoch, logs=None):
         if not hasattr(self.model.optimizer, 'lr'):
             raise ValueError('Optimizer must have a "lr" attribute.')
-        lr = self.schedule(epoch)
+        lr = float(K.get_value(self.model.optimizer.lr))
+        try:  # new API
+            lr = self.schedule(epoch, lr=lr)
+        except TypeError:  # old API for backward compatibility
+            lr = self.schedule(epoch)
         if not isinstance(lr, (float, np.float32, np.float64)):
             raise ValueError('The output of the "schedule" function '
                              'should be float.')
