d9f26a92f4fdc1f1e170a4203a7c13abf3af86e8
GalaxyDream <xinsongdu@ufl.edu>
2018-02-08 00:47:18 -0500

    Enable us to use sklearn to do cv for functional api (#9320)
    * enable us to use sklearn to do cv for functional api
    
    * adjust code for multiclass
    
    * remove tailing space
    
    * remove irrelevant comment

--- keras/wrappers/scikit_learn.py
+++ keras/wrappers/scikit_learn.py
@@ -224,7 +224,12 @@ class KerasClassifier(BaseWrapper):
                 Class predictions.
         """
         kwargs = self.filter_sk_params(Sequential.predict_classes, kwargs)
-        classes = self.model.predict_classes(x, **kwargs)
+
+        proba = self.model.predict(x, **kwargs)
+        if proba.shape[-1] > 1:
+            classes = proba.argmax(axis=-1)
+        else:
+            classes = (proba > 0.5).astype('int32')
         return self.classes_[classes]
 
     def predict_proba(self, x, **kwargs):
@@ -247,7 +252,7 @@ class KerasClassifier(BaseWrapper):
                 (instead of `(n_sample, 1)` as in Keras).
         """
         kwargs = self.filter_sk_params(Sequential.predict_proba, kwargs)
-        probs = self.model.predict_proba(x, **kwargs)
+        probs = self.model.predict(x, **kwargs)
 
         # check if binary classification
         if probs.shape[1] == 1:
