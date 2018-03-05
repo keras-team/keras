344898540e303cb6af1b0ebd3667b9d0d29797e4
Francois Chollet <francois.chollet@gmail.com>
2018-03-04 17:18:32 -0800

    Improve preprocessing.sequence docstrings

--- keras/preprocessing/sequence.py
+++ keras/preprocessing/sequence.py
@@ -24,19 +24,21 @@ def pad_sequences(sequences, maxlen=None, dtype='int32',
     Supports post-padding and pre-padding (default).
 
     # Arguments
-        sequences: list of lists where each element is a sequence
-        maxlen: int, maximum length
-        dtype: type to cast the resulting sequence.
-        padding: 'pre' or 'post', pad either before or after each sequence.
-        truncating: 'pre' or 'post', remove values from sequences larger than
-            maxlen either in the beginning or in the end of the sequence
-        value: float, value to pad the sequences to the desired value.
+        sequences: List of lists, where each element is a sequence.
+        maxlen: Int, maximum length of all sequences.
+        dtype: Type of the output sequences.
+        padding: String, 'pre' or 'post':
+            pad either before or after each sequence.
+        truncating: String, 'pre' or 'post':
+            remove values from sequences larger than
+            `maxlen`, either at the beginning or at the end of the sequences.
+        value: Float, padding value.
 
     # Returns
-        x: numpy array with dimensions (number_of_sequences, maxlen)
+        x: Numpy array with shape `(len(sequences), maxlen)`
 
     # Raises
-        ValueError: in case of invalid values for `truncating` or `padding`,
+        ValueError: In case of invalid values for `truncating` or `padding`,
             or in case of invalid shape for a `sequences` entry.
     """
     if not hasattr(sequences, '__len__'):
@@ -69,12 +71,14 @@ def pad_sequences(sequences, maxlen=None, dtype='int32',
         elif truncating == 'post':
             trunc = s[:maxlen]
         else:
-            raise ValueError('Truncating type "%s" not understood' % truncating)
+            raise ValueError('Truncating type "%s" '
+                             'not understood' % truncating)
 
         # check `trunc` has expected shape
         trunc = np.asarray(trunc, dtype=dtype)
         if trunc.shape[1:] != sample_shape:
-            raise ValueError('Shape of sample %s of sequence at position %s is different from expected shape %s' %
+            raise ValueError('Shape of sample %s of sequence at position %s '
+                             'is different from expected shape %s' %
                              (trunc.shape[1:], idx, sample_shape))
 
         if padding == 'post':
@@ -94,16 +98,16 @@ def make_sampling_table(size, sampling_factor=1e-5):
     according to the sampling distribution used in word2vec.
 
     The word2vec formula is:
-        p(word) = min(1, sqrt(word.frequency/sampling_factor) / (word.frequency/sampling_factor))
+        `p(word) = min(1, sqrt(word.frequency/sampling_factor) / (word.frequency/sampling_factor))`
 
     We assume that the word frequencies follow Zipf's law (s=1) to derive
     a numerical approximation of frequency(rank):
-       frequency(rank) ~ 1/(rank * (log(rank) + gamma) + 1/2 - 1/(12*rank))
-        where gamma is the Euler-Mascheroni constant.
+       `frequency(rank) ~ 1/(rank * (log(rank) + gamma) + 1/2 - 1/(12*rank))`
+        where `gamma` is the Euler-Mascheroni constant.
 
     # Arguments
-        size: int, number of possible words to sample.
-        sampling_factor: the sampling factor in the word2vec formula.
+        size: Int, number of possible words to sample.
+        sampling_factor: The sampling factor in the word2vec formula.
 
     # Returns
         A 1D Numpy array of length `size` where the ith entry
@@ -124,29 +128,32 @@ def skipgrams(sequence, vocabulary_size,
     """Generates skipgram word pairs.
 
     Takes a sequence (list of indexes of words),
-    returns couples of [word_index, other_word index] and labels (1s or 0s),
-    where label = 1 if 'other_word' belongs to the context of 'word',
-    and label=0 if 'other_word' is randomly sampled
+    returns couples of samples `[word_index, other_word index]`
+    and labels (1s or 0s),
+    where `label == 1` if 'other_word' belongs to the context of 'word',
+    and `label == 0` if 'other_word' is randomly sampled.
 
     # Arguments
-        sequence: a word sequence (sentence), encoded as a list
+        sequence: A word sequence (sentence), encoded as a list
             of word indices (integers). If using a `sampling_table`,
             word indices are expected to match the rank
             of the words in a reference dataset (e.g. 10 would encode
             the 10-th most frequently occurring token).
             Note that index 0 is expected to be a non-word and will be skipped.
-        vocabulary_size: int. maximum possible word index + 1
-        window_size: int. actually half-window.
-            The window of a word wi will be [i-window_size, i+window_size+1]
-        negative_samples: float >= 0. 0 for no negative (=random) samples.
-            1 for same number as positive samples. etc.
-        shuffle: whether to shuffle the word couples before returning them.
+        vocabulary_size: Int, maximum possible word index + 1
+        window_size: Int, size of sampling windows (technically half-window).
+            The window of a word `w_i` will be
+            `[i - window_size, i + window_size+1]`.
+        negative_samples: Float >= 0. 0 for no negative (i.e. random) samples.
+            1 for same number as positive samples.
+        shuffle: Whether to shuffle the word couples before returning them.
         categorical: bool. if False, labels will be
-            integers (eg. [0, 1, 1 .. ]),
-            if True labels will be categorical eg. [[1,0],[0,1],[0,1] .. ]
+            integers (eg. `[0, 1, 1 .. ]`),
+            if `True`, labels will be categorical, e.g.
+            `[[1,0],[0,1],[0,1] .. ]`.
         sampling_table: 1D array of size `vocabulary_size` where the entry i
             encodes the probability to sample a word of rank i.
-        seed: random seed.
+        seed: Random seed.
 
     # Returns
         couples, labels: where `couples` are int pairs and
@@ -184,7 +191,8 @@ def skipgrams(sequence, vocabulary_size,
         random.shuffle(words)
 
         couples += [[words[i % len(words)],
-                    random.randint(1, vocabulary_size - 1)] for i in range(num_negative_samples)]
+                    random.randint(1, vocabulary_size - 1)]
+                    for i in range(num_negative_samples)]
         if categorical:
             labels += [[1, 0]] * num_negative_samples
         else:
@@ -205,9 +213,9 @@ def _remove_long_seq(maxlen, seq, label):
     """Removes sequences that exceed the maximum length.
 
     # Arguments
-        maxlen: int, maximum length
-        seq: list of lists where each sublist is a sequence
-        label: list where each element is an integer
+        maxlen: Int, maximum length of the output sequences.
+        seq: List of lists, where each sublist is a sequence.
+        label: List where each element is an integer.
 
     # Returns
         new_seq, new_label: shortened lists for `seq` and `label`.
@@ -225,54 +233,62 @@ class TimeseriesGenerator(Sequence):
 
     This class takes in a sequence of data-points gathered at
     equal intervals, along with time series parameters such as
-    stride, length of history etc., to produce batches for
+    stride, length of history, etc., to produce batches for
     training/validation.
 
     # Arguments
-        data: indexable generator (such as list or Numpy array)
-            containing consecutive data points.
-        targets: targets corresponding to instances in `data`.
-            Should have same length as `data`.
-        length: length of history to consider for prediction.
-            `data[i-length : i+1]` is used to prepare the input.
-        sampling_rate: period between successive individual data
-            points. For rate `r`, points
+        data: Indexable generator (such as list or Numpy array)
+            containing consecutive data points (timesteps).
+            The data should be at 2D, and axis 0 is expected
+            to be the time dimension.
+        targets: Targets corresponding to timesteps in `data`.
+            It should have same length as `data`.
+        length: Length of the output sequences (in number of timesteps).
+        sampling_rate: Period between successive individual timesteps
+            within sequences. For rate `r`, timesteps
             `data[i]`, `data[i-r]`, ... `data[i - length]`
-            are used as input.
-        stride: period between successive sub-sequences used for
-            training. For stride `s`, consecutive training samples would
-            be centered around `data[i]`, `data[i+s]`, `data[i+2*s]`, ...
-        start_index, end_index: delimiters for sub-sets of `data` & `targets`
-            to be used for batch generation. Useful to reserve part of
-            data for validation.
-        shuffle: whether to shuffle training samples, or draw them in
-            chronological order.
-        reverse: if `true`, input points are provided in reverse order.
-        batch_size: number of timeseries samples in each batch
-            (except maybe last one)
+            are used for create a sample sequence.
+        stride: Period between successive output sequences.
+            For stride `s`, consecutive output samples would
+            be centered around `data[i]`, `data[i+s]`, `data[i+2*s]`, etc.
+        start_index, end_index: Data points earlier than `start_index`
+            or later than `end_index` will not be used in the output sequences.
+            This is seful to reserve part of the data for test or validation.
+        shuffle: Whether to shuffle output samples,
+            or instead draw them in chronological order.
+        reverse: Boolean: if `true`, timesteps in each output sample will be
+            in reverse chronological order.
+        batch_size: Number of timeseries samples in each batch
+            (except maybe the last one).
 
     # Returns
         A `Sequence` instance.
 
     # Examples
 
-    ```data = np.array([[i] for i in range(50)])
-       targets = np.array([[i] for i in range(50)])
+    ```python
+    from keras.preprocessing.sequence import TimeseriesGenerator
+    import numpy as np
 
-       data_gen = TimeseriesGenerator(data, targets,
-                                      length=10, sampling_rate=2,
-                                      batch_size=2)
-       assert len(data_gen) == 20
-       assert (np.array_equal(data_gen[0][0],
-                              np.array([[[0], [2], [4], [6], [8]],
-                                        [[1], [3], [5], [7], [9]]])))
-       assert (np.array_equal(data_gen[0][1],
-                              np.array([[10], [11]])))
+    data = np.array([[i] for i in range(50)])
+    targets = np.array([[i] for i in range(50)])
+
+    data_gen = TimeseriesGenerator(data, targets,
+                                   length=10, sampling_rate=2,
+                                   batch_size=2)
+    assert len(data_gen) == 20
+
+    batch_0 = data_gen[0]
+    x, y = batch_0
+    assert np.array_equal(x,
+                          np.array([[[0], [2], [4], [6], [8]],
+                                    [[1], [3], [5], [7], [9]]]))
+    assert np.array_equal(y,
+                          np.array([[10], [11]]))
     ```
     """
 
-    def __init__(self, data, targets,
-                 length,
+    def __init__(self, data, targets, length,
                  sampling_rate=1,
                  stride=1,
                  start_index=0,
