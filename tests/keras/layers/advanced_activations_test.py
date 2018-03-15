8f7c3c7ce4bcdd1f0ce0f9dde5bd7ef53936483c
Wilder Rodrigues <wilder.rodrigues@ekholabs.ai>
2018-03-15 22:13:55 +0100

    Add the ReLUs activation function as an advanced layer.

--- tests/keras/layers/advanced_activations_test.py
+++ tests/keras/layers/advanced_activations_test.py
@@ -43,5 +43,12 @@ def test_softmax():
                    input_shape=(2, 3, 4))
 
 
+@keras_test
+def test_relus():
+    for epsilon in [0.0025, 0.0035, 0.0045]:
+        layer_test(layers.ReLUs, kwargs={'epsilon': epsilon},
+                   input_shape=(2, 3, 4))
+
+
 if __name__ == '__main__':
     pytest.main([__file__])
