b818116f50e69ea3ac6f085381e8264aed54c905
Lucas David <lucasolivdavid@gmail.com>
2018-04-14 16:13:23 -0300

    fix TimeSeriesGenerator glitch (#9899)

--- tests/keras/preprocessing/sequence_test.py
+++ tests/keras/preprocessing/sequence_test.py
@@ -1,5 +1,7 @@
+from math import ceil
+
 import numpy as np
-from numpy.testing import assert_allclose
+from numpy.testing import assert_allclose, assert_raises
 
 import pytest
 
@@ -152,7 +154,7 @@ def test_TimeseriesGenerator():
                                    length=10, sampling_rate=2,
                                    start_index=10, end_index=30,
                                    batch_size=2)
-    assert len(data_gen) == 5
+    assert len(data_gen) == 6
     assert (np.allclose(data_gen[0][0],
                         np.array([[[10], [12], [14], [16], [18]],
                                   [[11], [13], [15], [17], [19]]])))
@@ -165,12 +167,75 @@ def test_TimeseriesGenerator():
                                    length=10, sampling_rate=2,
                                    start_index=10, end_index=30,
                                    batch_size=2)
-
-    assert len(data_gen) == 5
+    assert len(data_gen) == 6
     assert np.allclose(data_gen[0][0], np.array(
         [np.array(data[10:19:2]), np.array(data[11:20:2])]))
     assert (np.allclose(data_gen[0][1],
                         np.array([targets[20], targets[21]])))
+
+    with assert_raises(ValueError) as context:
+        TimeseriesGenerator(data, targets, length=50)
+    error = str(context.exception)
+    assert '`start_index+length=50 > end_index=49` is disallowed' in error
+
+
+def test_TimeSeriesGenerator_doesnt_miss_any_sample():
+    x = np.array([[i] for i in range(10)])
+
+    for length in range(3, 10):
+        g = TimeseriesGenerator(x, x,
+                                length=length,
+                                batch_size=1)
+        expected = max(0, len(x) - length)
+        actual = len(g)
+
+        assert expected == actual
+
+        if len(g) > 0:
+            # All elements in range(length, 10) should be used as current step
+            expected = np.arange(length, 10).reshape(-1, 1)
+
+            y = np.concatenate([g[ix][1] for ix in range(len(g))], axis=0)
+            assert_allclose(y, expected)
+
+    x = np.array([[i] for i in range(23)])
+
+    strides = (1, 1, 5, 7, 3, 5, 3)
+    lengths = (3, 3, 4, 3, 1, 3, 7)
+    batch_sizes = (6, 6, 6, 5, 6, 6, 6)
+    shuffles = (False, True, True, False, False, False, False)
+
+    for stride, length, batch_size, shuffle in zip(strides,
+                                                   lengths,
+                                                   batch_sizes,
+                                                   shuffles):
+        g = TimeseriesGenerator(x, x,
+                                length=length,
+                                sampling_rate=1,
+                                stride=stride,
+                                start_index=0,
+                                end_index=None,
+                                shuffle=shuffle,
+                                reverse=False,
+                                batch_size=batch_size)
+        if shuffle:
+            # all batches have the same size when shuffle is True.
+            expected_sequences = ceil(
+                (23 - length) / float(batch_size * stride)) * batch_size
+        else:
+            # last batch will be different if `(samples - length) / stride`
+            # is not a multiple of `batch_size`.
+            expected_sequences = ceil((23 - length) / float(stride))
+
+        expected_batches = ceil(expected_sequences / float(batch_size))
+
+        y = [g[ix][1] for ix in range(len(g))]
+
+        actual_sequences = sum(len(_y) for _y in y)
+        actual_batches = len(y)
+
+        assert expected_sequences == actual_sequences
+        assert expected_batches == actual_batches
 
 
 if __name__ == '__main__':
