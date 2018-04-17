517871bc1feca346ab103df69e581459bd929bbc
mrTsjolder <mrtsjolder@gmail.com>
2018-04-17 21:17:55 +0200

    Fix sloppiness in correction

--- tests/keras/initializers_test.py
+++ tests/keras/initializers_test.py
@@ -53,49 +53,49 @@ def test_constant(tensor_shape):
 @pytest.mark.parametrize('tensor_shape', [FC_SHAPE, CONV_SHAPE], ids=['FC', 'CONV'])
 def test_lecun_uniform(tensor_shape):
     fan_in, _ = initializers._compute_fans(tensor_shape)
-    scale = np.sqrt(3. / fan_in)
+    std = np.sqrt(1. / fan_in)
     _runner(initializers.lecun_uniform(), tensor_shape,
-            target_mean=0., target_std=scale)
+            target_mean=0., target_std=std)
 
 
 @pytest.mark.parametrize('tensor_shape', [FC_SHAPE, CONV_SHAPE], ids=['FC', 'CONV'])
 def test_glorot_uniform(tensor_shape):
     fan_in, fan_out = initializers._compute_fans(tensor_shape)
-    scale = np.sqrt(6. / (fan_in + fan_out))
+    std = np.sqrt(2. / (fan_in + fan_out))
     _runner(initializers.glorot_uniform(), tensor_shape,
-            target_mean=0., target_std=scale)
+            target_mean=0., target_std=std)
 
 
 @pytest.mark.parametrize('tensor_shape', [FC_SHAPE, CONV_SHAPE], ids=['FC', 'CONV'])
 def test_he_uniform(tensor_shape):
     fan_in, _ = initializers._compute_fans(tensor_shape)
-    scale = np.sqrt(6. / fan_in)
+    std = np.sqrt(2. / fan_in)
     _runner(initializers.he_uniform(), tensor_shape,
-            target_mean=0., target_std=scale)
+            target_mean=0., target_std=std)
 
 
 @pytest.mark.parametrize('tensor_shape', [FC_SHAPE, CONV_SHAPE], ids=['FC', 'CONV'])
 def test_lecun_normal(tensor_shape):
     fan_in, _ = initializers._compute_fans(tensor_shape)
-    scale = np.sqrt(1. / fan_in)
+    std = np.sqrt(1. / fan_in)
     _runner(initializers.lecun_normal(), tensor_shape,
-            target_mean=0., target_std=scale)
+            target_mean=0., target_std=std)
 
 
 @pytest.mark.parametrize('tensor_shape', [FC_SHAPE, CONV_SHAPE], ids=['FC', 'CONV'])
 def test_glorot_normal(tensor_shape):
     fan_in, fan_out = initializers._compute_fans(tensor_shape)
-    scale = np.sqrt(2. / (fan_in + fan_out))
+    std = np.sqrt(2. / (fan_in + fan_out))
     _runner(initializers.glorot_normal(), tensor_shape,
-            target_mean=0., target_std=scale)
+            target_mean=0., target_std=std)
 
 
 @pytest.mark.parametrize('tensor_shape', [FC_SHAPE, CONV_SHAPE], ids=['FC', 'CONV'])
 def test_he_normal(tensor_shape):
     fan_in, _ = initializers._compute_fans(tensor_shape)
-    scale = np.sqrt(2. / fan_in)
+    std = np.sqrt(2. / fan_in)
     _runner(initializers.he_normal(), tensor_shape,
-            target_mean=0., target_std=scale)
+            target_mean=0., target_std=std)
 
 
 @pytest.mark.parametrize('tensor_shape', [FC_SHAPE, CONV_SHAPE], ids=['FC', 'CONV'])
