65d31ee93fb2c56314413eb24535f898ecccb70f
mrTsjolder <mrtsjolder@gmail.com>
2018-04-17 20:21:45 +0200

    Use correct theano function for truncated_normal

--- keras/backend/theano_backend.py
+++ keras/backend/theano_backend.py
@@ -2336,9 +2336,13 @@ def truncated_normal(shape, mean=0.0, stddev=1.0, dtype=None, seed=None):
     if seed is None:
         seed = np.random.randint(1, 10e6)
     rng = RandomStreams(seed=seed)
-    normal_tensor = rng.normal(size=shape, avg=mean, std=stddev, dtype=dtype)
-    # Poor man's truncated normal: we literally clip the tensor
-    return T.clip(normal_tensor, mean - 2 * stddev, mean + 2 * stddev)
+
+    try:
+        return rng.normal(size=shape, avg=mean, std=stddev, dtype=dtype, truncate=True)
+    except TypeError:
+        normal_tensor = rng.normal(size=shape, avg=mean, std=stddev, dtype=dtype)
+        # Poor man's truncated normal: we literally clip the tensor
+        return T.clip(normal_tensor, mean - 2 * stddev, mean + 2 * stddev)
 
 
 # Theano implementation of CTC
