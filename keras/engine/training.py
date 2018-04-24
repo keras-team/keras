d0498c67d51c70fedd726be040e47b205aba6e2e
Francois Chollet <francois.chollet@gmail.com>
2018-04-24 10:15:26 -0700

    Fix merge mixup.

--- keras/engine/training.py
+++ keras/engine/training.py
@@ -16,11 +16,11 @@ from .. import backend as K
 from .. import optimizers
 from .. import losses
 from .. import metrics as metrics_module
-from .. import callbacks as cbks
 from ..utils.data_utils import Sequence
 from ..utils.data_utils import GeneratorEnqueuer
 from ..utils.data_utils import OrderedEnqueuer
 from ..utils.generic_utils import Progbar
+from .. import callbacks as cbks
 from ..legacy import interfaces
 
 
@@ -853,6 +853,7 @@ class Model(Network):
         nested_weighted_metrics = _collect_metrics(weighted_metrics, self.output_names)
         self.metrics_updates = []
         self.stateful_metric_names = []
+        self.stateful_metric_functions = []
         with K.name_scope('metrics'):
             for i in range(len(self.outputs)):
                 if i in skip_target_indices:
@@ -927,8 +928,9 @@ class Model(Network):
 
                         # Keep track of state updates created by
                         # stateful metrics (i.e. metrics layers).
-                        if isinstance(metric_fn, Layer):
+                        if isinstance(metric_fn, Layer) and metric_fn.stateful:
                             self.stateful_metric_names.append(metric_name)
+                            self.stateful_metric_functions.append(metric_fn)
                             self.metrics_updates += metric_fn.updates
 
                 handle_metrics(output_metrics)
@@ -1174,9 +1176,8 @@ class Model(Network):
 
         for epoch in range(initial_epoch, epochs):
             # Reset stateful metrics
-            for m in self.metrics:
-                if isinstance(m, Layer):
-                    m.reset_states()
+            for m in self.stateful_metric_functions:
+                m.reset_states()
             callbacks.on_epoch_begin(epoch)
             epoch_logs = {}
             if steps_per_epoch is not None:
@@ -1363,9 +1364,8 @@ class Model(Network):
         """
 
         if hasattr(self, 'metrics'):
-            for m in self.metrics:
-                if isinstance(m, Layer):
-                    m.reset_states()
+            for m in self.stateful_metric_functions:
+                m.reset_states()
             stateful_metric_indices = [
                 i for i, name in enumerate(self.metrics_names)
                 if str(name) in self.stateful_metric_names]
@@ -1398,7 +1398,7 @@ class Model(Network):
                             outs.append(0.)
                     for i, batch_out in enumerate(batch_outs):
                         if i in stateful_metric_indices:
-                            outs[i] = batch_out
+                            outs[i] = float(batch_out)
                         else:
                             outs[i] += batch_out
                 else:
@@ -1962,7 +1962,7 @@ class Model(Network):
                       use_multiprocessing=False,
                       shuffle=True,
                       initial_epoch=0):
-        """Trains the model on data yielded batch-by-batch by a Python generator.
+        """Trains the model on data generated batch-by-batch by a Python generator or an instance of `Sequence`.
 
         The generator is run in parallel to the model, for efficiency.
         For instance, this allows you to do real-time data augmentation
@@ -1981,10 +1981,10 @@ class Model(Network):
                 - a tuple `(inputs, targets, sample_weights)`.
                 This tuple (a single output of the generator) makes a single
                 batch. Therefore, all arrays in this tuple must have the same
-                length (equal to the size of this batch). Different batches
-                may have different sizes. For example, the last batch of the
-                epoch is commonly smaller than the others, if the size of the
-                dataset is not divisible by the batch size.
+                length (equal to the size of this batch). Different batches may
+                have different sizes. For example, the last batch of the epoch
+                is commonly smaller than the others, if the size of the dataset
+                is not divisible by the batch size.
                 The generator is expected to loop over its data
                 indefinitely. An epoch finishes when `steps_per_epoch`
                 batches have been seen by the model.
@@ -2018,32 +2018,32 @@ class Model(Network):
                 The model will not be trained on this data.
             validation_steps: Only relevant if `validation_data`
                 is a generator. Total number of steps (batches of samples)
-                to yield from `validation_data` generator before stopping.
+                to yield from `validation_data` generator before stopping
+                at the end of every epoch. It should typically
+                be equal to the number of samples of your
+                validation dataset divided by the batch size.
                 Optional for `Sequence`: if unspecified, will use
                 the `len(validation_data)` as a number of steps.
             class_weight: Optional dictionary mapping class indices (integers)
                 to a weight (float) value, used for weighting the loss function
-                (during training only).
-                This can be useful to tell the model to
-                "pay more attention" to samples from
-                an under-represented class.
+                (during training only). This can be useful to tell the model to
+                "pay more attention" to samples from an under-represented class.
             max_queue_size: Integer. Maximum size for the generator queue.
                 If unspecified, `max_queue_size` will default to 10.
             workers: Integer. Maximum number of processes to spin up
-                when using process based threading.
+                when using process-based threading.
                 If unspecified, `workers` will default to 1. If 0, will
                 execute the generator on the main thread.
-            use_multiprocessing: Boolean. If True, use process based threading.
-                If unspecified, `use_multiprocessing` will default to False.
-                Note that because
-                this implementation relies on multiprocessing,
-                you should not pass
-                non picklable arguments to the generator
-                as they can't be passed
-                easily to children processes.
-            shuffle: Boolean. Whether to shuffle the training data
-                in batch-sized chunks before each epoch.
-                Only used with instances of `Sequence` (`keras.utils.Sequence`).
+            use_multiprocessing: Boolean.
+                If `True`, use process-based threading.
+                If unspecified, `use_multiprocessing` will default to `False`.
+                Note that because this implementation relies on multiprocessing,
+                you should not pass non-picklable arguments to the generator
+                as they can't be passed easily to children processes.
+            shuffle: Boolean. Whether to shuffle the order of the batches at
+                the beginning of each epoch. Only used with instances
+                of `Sequence` (`keras.utils.Sequence`).
+                Has no effect when `steps_per_epoch` is not `None`.
             initial_epoch: Integer.
                 Epoch at which to start training
                 (useful for resuming a previous training run).
@@ -2054,11 +2054,14 @@ class Model(Network):
             at successive epochs, as well as validation loss values
             and validation metrics values (if applicable).
 
+        # Raises
+            ValueError: In case the generator yields data in an invalid format.
+
         # Example
 
         ```python
             def generate_arrays_from_file(path):
-                while 1:
+                while True:
                     with open(path) as f:
                         for line in f:
                             # create numpy arrays of input data
@@ -2069,10 +2072,6 @@ class Model(Network):
             model.fit_generator(generate_arrays_from_file('/my_file.txt'),
                                 steps_per_epoch=10000, epochs=10)
         ```
-
-        # Raises
-            ValueError: In case the generator yields
-                data in an invalid format.
         """
         wait_time = 0.01  # in seconds
         epoch = initial_epoch
@@ -2145,45 +2144,25 @@ class Model(Network):
         val_enqueuer = None
 
         try:
-            if do_validation:
-                if val_gen:
-                    if workers > 0:
-                        if isinstance(validation_data, Sequence):
-                            val_enqueuer = OrderedEnqueuer(
-                                validation_data,
-                                use_multiprocessing=use_multiprocessing)
-                            if validation_steps is None:
-                                validation_steps = len(validation_data)
-                        else:
-                            val_enqueuer = GeneratorEnqueuer(
-                                validation_data,
-                                use_multiprocessing=use_multiprocessing,
-                                wait_time=wait_time)
-                        val_enqueuer.start(workers=workers, max_queue_size=max_queue_size)
-                        validation_generator = val_enqueuer.get()
-                    else:
-                        if isinstance(validation_data, Sequence):
-                            validation_generator = iter(validation_data)
-                        else:
-                            validation_generator = validation_data
+            if do_validation and not val_gen:
+                # Prepare data for validation
+                if len(validation_data) == 2:
+                    val_x, val_y = validation_data
+                    val_sample_weight = None
+                elif len(validation_data) == 3:
+                    val_x, val_y, val_sample_weight = validation_data
                 else:
-                    if len(validation_data) == 2:
-                        val_x, val_y = validation_data
-                        val_sample_weight = None
-                    elif len(validation_data) == 3:
-                        val_x, val_y, val_sample_weight = validation_data
-                    else:
-                        raise ValueError('`validation_data` should be a tuple '
-                                         '`(val_x, val_y, val_sample_weight)` '
-                                         'or `(val_x, val_y)`. Found: ' +
-                                         str(validation_data))
-                    val_x, val_y, val_sample_weights = self._standardize_user_data(
-                        val_x, val_y, val_sample_weight)
-                    val_data = val_x + val_y + val_sample_weights
-                    if self.uses_learning_phase and not isinstance(K.learning_phase(), int):
-                        val_data += [0.]
-                    for cbk in callbacks:
-                        cbk.validation_data = val_data
+                    raise ValueError('`validation_data` should be a tuple '
+                                     '`(val_x, val_y, val_sample_weight)` '
+                                     'or `(val_x, val_y)`. Found: ' +
+                                     str(validation_data))
+                val_x, val_y, val_sample_weights = self._standardize_user_data(
+                    val_x, val_y, val_sample_weight)
+                val_data = val_x + val_y + val_sample_weights
+                if self.uses_learning_phase and not isinstance(K.learning_phase(), int):
+                    val_data += [0.]
+                for cbk in callbacks:
+                    cbk.validation_data = val_data
 
             if workers > 0:
                 if is_sequence:
@@ -2206,6 +2185,8 @@ class Model(Network):
             # Construct epoch logs.
             epoch_logs = {}
             while epoch < epochs:
+                for m in self.stateful_metric_functions:
+                    m.reset_states()
                 callbacks.on_epoch_begin(epoch)
                 steps_done = 0
                 batch_index = 0
@@ -2230,7 +2211,11 @@ class Model(Network):
                                          str(generator_output))
                     # build batch logs
                     batch_logs = {}
-                    if isinstance(x, list):
+                    if x is None or len(x) == 0:
+                        # Handle data tensors support when no input given
+                        # step-size = 1 for data tensors
+                        batch_size = 1
+                    elif isinstance(x, list):
                         batch_size = x[0].shape[0]
                     elif isinstance(x, dict):
                         batch_size = list(x.values())[0].shape[0]
@@ -2258,9 +2243,11 @@ class Model(Network):
                     if steps_done >= steps_per_epoch and do_validation:
                         if val_gen:
                             val_outs = self.evaluate_generator(
-                                validation_generator,
+                                validation_data,
                                 validation_steps,
-                                workers=0)
+                                workers=workers,
+                                use_multiprocessing=use_multiprocessing,
+                                max_queue_size=max_queue_size)
                         else:
                             # No need for try/except because
                             # data has already been validated.
@@ -2298,7 +2285,8 @@ class Model(Network):
     def evaluate_generator(self, generator, steps=None,
                            max_queue_size=10,
                            workers=1,
-                           use_multiprocessing=False):
+                           use_multiprocessing=False,
+                           verbose=0):
         """Evaluates the model on a data generator.
 
         The generator should return the same kind of data
@@ -2326,6 +2314,7 @@ class Model(Network):
                 non picklable arguments to the generator
                 as they can't be passed
                 easily to children processes.
+            verbose: verbosity mode, 0 or 1.
 
         # Returns
             Scalar test loss (if the model has a single output and no metrics)
@@ -2339,9 +2328,19 @@ class Model(Network):
         """
         self._make_test_function()
 
+        stateful_metric_indices = []
+        if hasattr(self, 'metrics'):
+            for m in self.stateful_metric_functions:
+                m.reset_states()
+            stateful_metric_indices = [
+                i for i, name in enumerate(self.metrics_names)
+                if str(name) in self.stateful_metric_names]
+        else:
+            stateful_metric_indices = []
+
         steps_done = 0
         wait_time = 0.01
-        all_outs = []
+        outs_per_batch = []
         batch_sizes = []
         is_sequence = isinstance(generator, Sequence)
         if not is_sequence and use_multiprocessing and workers > 1:
@@ -2377,6 +2376,9 @@ class Model(Network):
                 else:
                     output_generator = generator
 
+            if verbose == 1:
+                progbar = Progbar(target=steps)
+
             while steps_done < steps:
                 generator_output = next(output_generator)
                 if not hasattr(generator_output, '__len__'):
@@ -2395,8 +2397,15 @@ class Model(Network):
                                      'or (x, y). Found: ' +
                                      str(generator_output))
                 outs = self.test_on_batch(x, y, sample_weight=sample_weight)
+                if not isinstance(outs, list):
+                    outs = [outs]
+                outs_per_batch.append(outs)
 
-                if isinstance(x, list):
+                if x is None or len(x) == 0:
+                    # Handle data tensors support when no input given
+                    # step-size = 1 for data tensors
+                    batch_size = 1
+                elif isinstance(x, list):
                     batch_size = x[0].shape[0]
                 elif isinstance(x, dict):
                     batch_size = list(x.values())[0].shape[0]
@@ -2405,24 +2414,26 @@ class Model(Network):
                 if batch_size == 0:
                     raise ValueError('Received an empty batch. '
                                      'Batches should at least contain one item.')
-                all_outs.append(outs)
 
                 steps_done += 1
                 batch_sizes.append(batch_size)
+                if verbose == 1:
+                    progbar.update(steps_done)
 
         finally:
             if enqueuer is not None:
                 enqueuer.stop()
 
-        if not isinstance(outs, list):
-            return np.average(np.asarray(all_outs),
-                              weights=batch_sizes)
-        else:
-            averages = []
-            for i in range(len(outs)):
-                averages.append(np.average([out[i] for out in all_outs],
+        averages = []
+        for i in range(len(outs)):
+            if i not in stateful_metric_indices:
+                averages.append(np.average([out[i] for out in outs_per_batch],
                                            weights=batch_sizes))
-            return averages
+            else:
+                averages.append(float(outs_per_batch[-1][i]))
+        if len(averages) == 1:
+            return averages[0]
+        return averages
 
     @interfaces.legacy_generator_methods_support
     def predict_generator(self, generator, steps=None,
