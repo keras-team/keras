893f576e5787291e224477ab6f967d0414ae8efa
Francois Chollet <francois.chollet@gmail.com>
2018-04-30 16:17:52 -0700

    Simplify Sequential implementation.
    RELNOTES: This breaks weight loading and model loading/saving for models from Keras 0.* that used Merge layers. The Merge layer has been deprecated for 2 years.

--- tests/keras/test_sequential_model.py
+++ tests/keras/test_sequential_model.py
@@ -382,7 +382,6 @@ def test_sequential_update_disabling():
 
     model.compile('sgd', 'mse')
     assert not model.updates
-    assert not model.model.updates
 
     x1 = model.predict(val_a)
     model.train_on_batch(val_a, val_out)
@@ -392,7 +391,6 @@ def test_sequential_update_disabling():
     model.trainable = True
     model.compile('sgd', 'mse')
     assert model.updates
-    assert model.model.updates
 
     model.train_on_batch(val_a, val_out)
     x2 = model.predict(val_a)
