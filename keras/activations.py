5777d59da6d1c2e3a849ab0e7ef2765dfc757d10
vkk800 <vkk800@users.noreply.github.com>
2018-05-01 19:18:25 +0200

    Add documentation to several activation functions (#10066)
    * Add documentation to several activation functions
    
    * Fix style issues.

--- keras/activations.py
+++ keras/activations.py
@@ -15,7 +15,7 @@ def softmax(x, axis=-1):
     """Softmax activation function.
 
     # Arguments
-        x : Tensor.
+        x: Input tensor.
         axis: Integer, axis along which the softmax normalization is applied.
 
     # Returns
@@ -36,17 +36,38 @@ def softmax(x, axis=-1):
 
 
 def elu(x, alpha=1.0):
+    """Exponential linear unit.
+
+    # Arguments
+        x: Input tensor.
+        alpha: A scalar, slope of negative section.
+
+    # Returns
+        The exponential linear activation: `x` if `x > 0` and
+        `alpha * (exp(x)-1)` if `x < 0`.
+
+    # References
+        - [Fast and Accurate Deep Network Learning by Exponential
+        Linear Units (ELUs)](https://arxiv.org/abs/1511.07289)
+    """
     return K.elu(x, alpha)
 
 
 def selu(x):
-    """Scaled Exponential Linear Unit. (Klambauer et al., 2017).
+    """Scaled Exponential Linear Unit (SELU).
+
+    SELU is equal to: `scale * elu(x, alpha)`, where alpha and scale
+    are pre-defined constants. The values of `alpha` and `scale` are
+    chosen so that the mean and variance of the inputs are preserved
+    between two consecutive layers as long as the weights are initialized
+    correctly (see `lecun_normal` initialization) and the number of inputs
+    is "large enough" (see references for more information).
 
     # Arguments
         x: A tensor or variable to compute the activation function for.
 
     # Returns
-        Tensor with the same shape and dtype as `x`.
+       The scaled exponential unit activation: `scale * elu(x, alpha)`.
 
     # Note
         - To be used together with the initialization "lecun_normal".
@@ -61,30 +82,78 @@ def selu(x):
 
 
 def softplus(x):
+    """Softplus activation function.
+
+    # Arguments
+        x: Input tensor.
+
+    # Returns
+        The softplus activation: `log(exp(x) + 1`.
+    """
     return K.softplus(x)
 
 
 def softsign(x):
+    """Softsign activation function.
+
+    # Arguments
+        x: Input tensor.
+
+    # Returns
+        The softplus activation: `x / (abs(x) + 1)`.
+    """
     return K.softsign(x)
 
 
 def relu(x, alpha=0., max_value=None):
+    """(Leaky) Rectified Linear Unit.
+
+    # Arguments
+        x: Input tensor.
+        alpha: Slope of the negative part. Defaults to zero.
+        max_value: Maximum value for the output.
+
+    # Returns
+        The (leaky) rectified linear unit activation: `x` if `x > 0`,
+        `alpha * x` if `x < 0`. If `max_value` is defined, the result
+        is truncated to this value.
+    """
     return K.relu(x, alpha=alpha, max_value=max_value)
 
 
 def tanh(x):
+    """Hyperbolic tangent activation function.
+    """
     return K.tanh(x)
 
 
 def sigmoid(x):
+    """Sigmoid activation function.
+    """
     return K.sigmoid(x)
 
 
 def hard_sigmoid(x):
+    """Hard sigmoid activation function.
+
+    Faster to compute than sigmoid activation.
+
+    # Arguments
+        x: Input tensor.
+
+    # Returns
+        Hard sigmoid activation:
+
+        - `0` if `x < -2.5`
+        - `1` if `x > 2.5`
+        - `0.2 * x + 0.5` if `-2.5 <= x <= 2.5`.
+    """
     return K.hard_sigmoid(x)
 
 
 def linear(x):
+    """Linear (e.g. unit) activation function.
+    """
     return x
 
 
