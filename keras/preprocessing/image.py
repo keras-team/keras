945e0353ef32802e3ac3ee21d2f89366a204c482
vkk800 <vkk800@users.noreply.github.com>
2018-05-06 22:22:58 +0200

    Fix documentation style issue

--- keras/preprocessing/image.py
+++ keras/preprocessing/image.py
@@ -242,8 +242,10 @@ def random_brightness(x, brightness_range):
         x: Input tensor. Must be 3D.
         brightness_range: Tuple of floats; brightness range.
         channel_axis: Index of axis for channels in the input tensor.
+
     # Returns
         Numpy image tensor.
+
     # Raises
         ValueError if `brightness_range` isn't a tuple.
     """
