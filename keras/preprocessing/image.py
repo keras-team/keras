794f814343b655e01de73578d71e8b8a53523687
Botty Dimanov <bottydim@users.noreply.github.com>
2018-05-25 22:21:41 +0100

    Handle capitalised extensions in list_pictures (#10220)
    #10219

--- keras/preprocessing/image.py
+++ keras/preprocessing/image.py
@@ -486,7 +486,7 @@ def load_img(path, grayscale=False, target_size=None,
 def list_pictures(directory, ext='jpg|jpeg|bmp|png|ppm'):
     return [os.path.join(root, f)
             for root, _, files in os.walk(directory) for f in files
-            if re.match(r'([\w]+\.(?:' + ext + '))', f)]
+            if re.match(r'([\w]+\.(?:' + ext + '))', f.lower())]
 
 
 class ImageDataGenerator(object):
