11eb92967057600be5921d91e287d447955b893e
François Chollet <francois.chollet@gmail.com>
2018-06-01 23:53:49 -0700

    Split `applications` and `preprocessing` modules. (#10339)
    * Split `applications` and `preprocessing` modules.
    
    * Fix dependencies.

--- keras/preprocessing/__init__.py
+++ keras/preprocessing/__init__.py
@@ -1,3 +1,14 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from .. import backend
+from .. import utils
+
+import keras_preprocessing
+
+keras_preprocessing.set_keras_submodules(backend=backend, utils=utils)
+
 from . import image
 from . import sequence
 from . import text
