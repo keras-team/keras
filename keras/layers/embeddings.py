e759f6acb5c669f8b66303e9b7dcf4b49159fad6
Dan Stine <dstine@users.noreply.github.com>
2018-06-18 14:35:55 -0400

    Fix docstring of layer.Embedding to properly render arguments (#10461)

--- keras/layers/embeddings.py
+++ keras/layers/embeddings.py
@@ -35,30 +35,30 @@ class Embedding(Layer):
     ```
 
     # Arguments
-      input_dim: int > 0. Size of the vocabulary,
-          i.e. maximum integer index + 1.
-      output_dim: int >= 0. Dimension of the dense embedding.
-      embeddings_initializer: Initializer for the `embeddings` matrix
-          (see [initializers](../initializers.md)).
-      embeddings_regularizer: Regularizer function applied to
-          the `embeddings` matrix
-          (see [regularizer](../regularizers.md)).
-      embeddings_constraint: Constraint function applied to
-          the `embeddings` matrix
-          (see [constraints](../constraints.md)).
-      mask_zero: Whether or not the input value 0 is a special "padding"
-          value that should be masked out.
-          This is useful when using [recurrent layers](recurrent.md)
-          which may take variable length input.
-          If this is `True` then all subsequent layers
-          in the model need to support masking or an exception will be raised.
-          If mask_zero is set to True, as a consequence, index 0 cannot be
-          used in the vocabulary (input_dim should equal size of
-          vocabulary + 1).
-      input_length: Length of input sequences, when it is constant.
-          This argument is required if you are going to connect
-          `Flatten` then `Dense` layers upstream
-          (without it, the shape of the dense outputs cannot be computed).
+        input_dim: int > 0. Size of the vocabulary,
+            i.e. maximum integer index + 1.
+        output_dim: int >= 0. Dimension of the dense embedding.
+        embeddings_initializer: Initializer for the `embeddings` matrix
+            (see [initializers](../initializers.md)).
+        embeddings_regularizer: Regularizer function applied to
+            the `embeddings` matrix
+            (see [regularizer](../regularizers.md)).
+        embeddings_constraint: Constraint function applied to
+            the `embeddings` matrix
+            (see [constraints](../constraints.md)).
+        mask_zero: Whether or not the input value 0 is a special "padding"
+            value that should be masked out.
+            This is useful when using [recurrent layers](recurrent.md)
+            which may take variable length input.
+            If this is `True` then all subsequent layers
+            in the model need to support masking or an exception will be raised.
+            If mask_zero is set to True, as a consequence, index 0 cannot be
+            used in the vocabulary (input_dim should equal size of
+            vocabulary + 1).
+        input_length: Length of input sequences, when it is constant.
+            This argument is required if you are going to connect
+            `Flatten` then `Dense` layers upstream
+            (without it, the shape of the dense outputs cannot be computed).
 
     # Input shape
         2D tensor with shape: `(batch_size, sequence_length)`.
