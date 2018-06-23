6902e76cf83a74c45d0ddea8033996b4afe8471f
fuzzythecat <fuzzy0427@gmail.com>
2018-06-24 07:58:07 +0900

    Add relu6

--- keras/applications/mobilenet.py
+++ keras/applications/mobilenet.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 
 from keras_applications import mobilenet
 
+relu6 = mobilenet.relu6
 MobileNet = mobilenet.MobileNet
 decode_predictions = mobilenet.decode_predictions
 preprocess_input = mobilenet.preprocess_input
