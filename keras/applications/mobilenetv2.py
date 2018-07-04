e9560d48186e21e7cbb1ff930ba4ec3844a19882
fuzzythecat <fuzzy0427@gmail.com>
2018-07-04 13:21:51 +0900

    Replace relu6 with ReLU layers (#10597)
    * Remove unused relu6
    
    * Remove doc for loading relu6

--- keras/applications/mobilenetv2.py
+++ keras/applications/mobilenetv2.py
@@ -4,7 +4,6 @@ from __future__ import print_function
 
 from keras_applications import mobilenet_v2
 
-relu6 = mobilenet_v2.relu6
 MobileNetV2 = mobilenet_v2.MobileNetV2
 decode_predictions = mobilenet_v2.decode_predictions
 preprocess_input = mobilenet_v2.preprocess_input
