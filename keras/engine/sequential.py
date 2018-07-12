d06383231ca8838042716bd2ca82eb1cf5bf4928
Yu-Yang Huang <myutwo150@gmail.com>
2018-07-12 23:18:26 +0800

    Fix nested sequential deferred build (#10655)
    * Fix nested sequential deferred build
    
    * Add test
    
    * Unify style

--- keras/engine/sequential.py
+++ keras/engine/sequential.py
@@ -149,8 +149,6 @@ class Sequential(Model):
                     first_layer = layer.layers[0]
                     while isinstance(first_layer, (Model, Sequential)):
                         first_layer = first_layer.layers[0]
-                    batch_shape = first_layer.batch_input_shape
-                    dtype = first_layer.dtype
 
                 if hasattr(first_layer, 'batch_input_shape'):
                     batch_shape = first_layer.batch_input_shape
