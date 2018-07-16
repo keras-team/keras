1fcc791ee1c94c2a3efa5072aa2f10f1aab51a2a
Chanran Kim <bassilver@naver.com>
2018-07-17 01:29:24 +0900

    (docs) Update `Conv2D` call to the Keras 2 API (#10692)
    Update `Conv2D` call to the Keras 2 API

--- keras/layers/core.py
+++ keras/layers/core.py
@@ -485,9 +485,8 @@ class Flatten(Layer):
 
     ```python
         model = Sequential()
-        model.add(Conv2D(64, 3, 3,
-                         padding='same',
-                         input_shape=(3, 32, 32)))
+        model.add(Conv2D(64, (3, 3),
+                         input_shape=(3, 32, 32), padding='same',))
         # now: model.output_shape == (None, 64, 32, 32)
 
         model.add(Flatten())
