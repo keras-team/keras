d2803c0fb7d0ba9361dcba8eb9bcebbf2f774958
Yong Zheng Xin <zhengxin.yong@minerva.kgi.edu>
2018-07-18 19:08:38 -0400

    PEP8 fixed line length examples/ (#10724)
    * PEP8 fixed line length examples/
    
    * revert addition_rnn
    
    * Fix PEP8 in babi_memnn

--- examples/cifar10_cnn.py
+++ examples/cifar10_cnn.py
@@ -85,19 +85,26 @@ else:
         zca_whitening=False,  # apply ZCA whitening
         zca_epsilon=1e-06,  # epsilon for ZCA whitening
         rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
-        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
-        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+        # randomly shift images horizontally (fraction of total width)
+        width_shift_range=0.1,
+        # randomly shift images vertically (fraction of total height)
+        height_shift_range=0.1,
         shear_range=0.,  # set range for random shear
         zoom_range=0.,  # set range for random zoom
         channel_shift_range=0.,  # set range for random channel shifts
-        fill_mode='nearest',  # set mode for filling points outside the input boundaries
+        # set mode for filling points outside the input boundaries
+        fill_mode='nearest',
         cval=0.,  # value used for fill_mode = "constant"
         horizontal_flip=True,  # randomly flip images
         vertical_flip=False,  # randomly flip images
-        rescale=None,  # set rescaling factor (applied before any other transformation)
-        preprocessing_function=None,  # set function that will be applied on each input
-        data_format=None,  # image data format, either "channels_first" or "channels_last"
-        validation_split=0.0)  # fraction of images reserved for validation (strictly between 0 and 1)
+        # set rescaling factor (applied before any other transformation)
+        rescale=None,
+        # set function that will be applied on each input
+        preprocessing_function=None,
+        # image data format, either "channels_first" or "channels_last"
+        data_format=None,
+        # fraction of images reserved for validation (strictly between 0 and 1)
+        validation_split=0.0)
 
     # Compute quantities required for feature-wise normalization
     # (std, mean, and principal components if ZCA whitening is applied).
