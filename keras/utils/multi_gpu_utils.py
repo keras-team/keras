c698f7236b601cf112cdafa4de4c2272dfff6cb7
Vasilis Vryniotis <datumbox@users.noreply.github.com>
2018-08-06 16:47:19 +0100

    Fix multi-output Siamese networks in multi_gpu_model

--- keras/utils/multi_gpu_utils.py
+++ keras/utils/multi_gpu_utils.py
@@ -4,6 +4,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from collections import Counter
+
 from ..layers.merge import concatenate
 from .. import backend as K
 from ..layers.core import Lambda
@@ -231,10 +233,20 @@ def multi_gpu_model(model, gpus=None, cpu_merge=True, cpu_relocation=False):
                 for o in range(len(outputs)):
                     all_outputs[o].append(outputs[o])
 
+    # Deduplicate output names to handle Siamese networks.
+    occurrences = Counter(model.output_names)
+    conflict_counter = {n: 0 for n, count in occurrences.items() if count > 1}
+    output_names = []
+    for n in model.output_names:
+        if n in conflict_counter:
+            conflict_counter[n] += 1
+            n += '_%d' % conflict_counter[n]
+        output_names.append(n)
+
     # Merge outputs under expected scope.
     with tf.device('/cpu:0' if cpu_merge else '/gpu:%d' % target_gpu_ids[0]):
         merged = []
-        for name, outputs in zip(model.output_names, all_outputs):
+        for name, outputs in zip(output_names, all_outputs):
             merged.append(concatenate(outputs,
                                       axis=0, name=name))
         return Model(model.inputs, merged)
