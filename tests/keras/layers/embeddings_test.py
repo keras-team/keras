9400be98783135a1d42dd238f4e6c3aa048eceea
Yanbo Liang <ybliang8@gmail.com>
2018-09-06 14:49:22 -0700

    Add missing raise for ValueError at Embedding compute_output_shape (#11091)
    * Add missing raise for ValueError at Embedding compute_output_shape
    
    * Add unit tests.

--- tests/keras/layers/embeddings_test.py
+++ tests/keras/layers/embeddings_test.py
@@ -1,6 +1,7 @@
 import pytest
 from keras.utils.test_utils import layer_test, keras_test
 from keras.layers.embeddings import Embedding
+from keras.models import Sequential
 import keras.backend as K
 
 
@@ -27,6 +28,26 @@ def test_embedding():
                input_shape=(3, 2, 5),
                input_dtype='int32',
                expected_output_dtype=K.floatx())
+
+
+@keras_test
+def test_embedding_invalid():
+
+    # len(input_length) should be equal to len(input_shape) - 1
+    with pytest.raises(ValueError):
+        model = Sequential([Embedding(
+            input_dim=10,
+            output_dim=4,
+            input_length=2,
+            input_shape=(3, 4, 5))])
+
+    # input_length should be equal to input_shape[1:]
+    with pytest.raises(ValueError):
+        model = Sequential([Embedding(
+            input_dim=10,
+            output_dim=4,
+            input_length=2,
+            input_shape=(3, 5))])
 
 
 if __name__ == '__main__':
