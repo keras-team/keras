e435be77db69ce625a0749389cee3ac676611cff
Lai Wei <royweilai@gmail.com>
2018-09-11 10:32:43 -0700

    fix sparse categorical acc (#11100)
    * fix sparse categorical acc
    
    * fix style
    
    * flatten y_true for comparision
    
    * fix style
    
    * add comment about flatten y_true

--- keras/metrics.py
+++ keras/metrics.py
@@ -34,7 +34,8 @@ def categorical_accuracy(y_true, y_pred):
 
 
 def sparse_categorical_accuracy(y_true, y_pred):
-    return K.cast(K.equal(K.max(y_true, axis=-1),
+    # flatten y_true in case it's in shape (num_samples, 1) instead of (num_samples,)
+    return K.cast(K.equal(K.flatten(y_true),
                           K.cast(K.argmax(y_pred, axis=-1), K.floatx())),
                   K.floatx())
 
