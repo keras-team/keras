5a6af4bc6d44e9adbc2a21804bfcd18c4ce849ef
Lai Wei <royweilai@gmail.com>
2018-09-11 10:32:43 -0700

    fix sparse categorical acc (#11100)
    * fix sparse categorical acc
    
    * fix style
    
    * flatten y_true for comparision
    
    * fix style
    
    * add comment about flatten y_true

--- tests/keras/metrics_test.py
+++ tests/keras/metrics_test.py
@@ -47,6 +47,18 @@ def test_sparse_metrics():
         assert K.eval(metric(y_a, y_b)).shape == (6,)
 
 
+@keras_test
+def test_sparse_categorical_accuracy_correctness():
+    y_a = K.variable(np.random.randint(0, 7, (6,)), dtype=K.floatx())
+    y_b = K.variable(np.random.random((6, 7)), dtype=K.floatx())
+    # use one_hot embedding to convert sparse labels to equivalent dense labels
+    y_a_dense_labels = K.cast(K.one_hot(K.cast(y_a, dtype='int32'), num_classes=7),
+                              dtype=K.floatx())
+    sparse_categorical_acc = metrics.sparse_categorical_accuracy(y_a, y_b)
+    categorical_acc = metrics.categorical_accuracy(y_a_dense_labels, y_b)
+    assert np.allclose(K.eval(sparse_categorical_acc), K.eval(categorical_acc))
+
+
 def test_serialize():
     '''This is a mock 'round trip' of serialize and deserialize.
     '''
