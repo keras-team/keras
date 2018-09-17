256ebd071304b19486aef2e3cb53ced05c84a082
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2018-09-17 23:55:33 +0200

    Removed print statement.

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,6 @@ from keras import backend as K
 
 @pytest.fixture(autouse=True)
 def clear_session_after_test():
-    print("dododod")
     yield
     if K.backend() == 'tensorflow' or K.backend() == 'cntk':
         K.clear_session()
