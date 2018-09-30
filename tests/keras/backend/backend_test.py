5a3a9dc8105e04a352bb9bb9a9c2862cb015761e
danErez <dan.erez.sd@gmail.com>
2018-09-30 20:50:18 +0300

    spelling error

--- tests/keras/backend/backend_test.py
+++ tests/keras/backend/backend_test.py
@@ -201,7 +201,7 @@ class TestBackend(object):
         check_single_tensor_operation('eye', 3, WITH_NP, shape_or_val=False)
 
     def test_ones(self):
-        check_single_tensor_operation('one', (3, 5, 10, 8), WITH_NP, shape_or_val=False)
+        check_single_tensor_operation('ones', (3, 5, 10, 8), WITH_NP, shape_or_val=False)
 
     def test_zeros(self):
         check_single_tensor_operation('zeros', (3, 5, 10, 8), WITH_NP, shape_or_val=False)
