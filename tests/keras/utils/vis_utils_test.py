3f47a137bdfddc201220cbe7ff6f36c95cac884a
ivan <inyokwetrust@gmail.com>
2018-10-18 14:04:08 -0700

    Documentation and test for vis_utils.py

--- tests/keras/utils/vis_utils_test.py
+++ tests/keras/utils/vis_utils_test.py
@@ -2,7 +2,9 @@ import pytest
 import os
 import sys
 import numpy as np
-from keras.layers import Conv2D
+from keras import Input, Model
+
+from keras.layers import Conv2D, Bidirectional
 from keras.layers import Dense
 from keras.layers import Flatten
 from keras.layers import LSTM
@@ -24,6 +26,17 @@ def test_plot_model():
     model.add(TimeDistributed(Dense(5, name='dense2')))
     vis_utils.plot_model(model, to_file='model2.png', show_shapes=True)
     os.remove('model2.png')
+
+    sentence_input = Input(shape=(2, 3), dtype='float32', name="input2")
+    l_lstm = Bidirectional(LSTM(16))(sentence_input)
+    sent_encoder = Model(sentence_input, l_lstm)
+    review_input = Input(shape=(5, 2, 3), dtype='float32')
+    review_encoder = TimeDistributed(sent_encoder)(review_input)
+    l_lstm_sent = LSTM(16)(review_encoder)
+    preds = Dense(5, activation='softmax')(l_lstm_sent)
+    model = Model(review_input, preds)
+    vis_utils.plot_model(model, to_file='model3.png', show_shapes=True, expand_nested=True, dpi=300)
+    os.remove('model3.png')
 
 
 if __name__ == '__main__':
