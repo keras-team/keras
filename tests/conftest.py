c999668eaa888c5d7e7fbabb52daef6e0381063a
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2018-11-18 16:53:29 +0100

    Using a filter to narrow down which warning should not be takend into account

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,10 @@
+import warnings
 import pytest
 from keras import backend as K
+
+warning_msg = r'your data is of type "float64", but your input ' \
+              r'variable (.*) expects (.*). Please convert your ' \
+              r'data beforehand to speed up training.'
 
 
 @pytest.fixture(autouse=True)
@@ -8,6 +13,9 @@ def clear_session_after_test():
 
     This wrapper runs for all the tests in the keras test suite.
     """
-    yield
+    with warnings.catch_warnings():
+        warnings.filterwarnings('ignore', message=warning_msg, category=UserWarning)
+        yield
+
     if K.backend() == 'tensorflow' or K.backend() == 'cntk':
         K.clear_session()
