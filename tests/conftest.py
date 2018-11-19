d57498aad308e9b7c14157b0de0a5a77f80853a6
gabrieldemarmiesse <gabreil.de-marmiesse@valeo.com>
2018-11-19 16:03:08 +0100

    Trying to fix the python2 bug.

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,16 +3,16 @@ import pytest
 from keras import backend as K
 
 
-warnings.simplefilter('once')
-
-
 @pytest.fixture(autouse=True)
 def clear_session_after_test():
     """Test wrapper to clean up after TensorFlow and CNTK tests.
 
     This wrapper runs for all the tests in the keras test suite.
     """
-    yield
+
+    with warnings.catch_warnings():
+        warnings.simplefilter('once')
+        yield
 
     if K.backend() == 'tensorflow' or K.backend() == 'cntk':
         K.clear_session()
