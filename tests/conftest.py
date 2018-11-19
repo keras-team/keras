1d35220c358dacfe36f542d9a2562582468f96f1
gabrieldemarmiesse <gabreil.de-marmiesse@valeo.com>
2018-11-19 15:49:11 +0100

    Display each warning only once.

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,9 +2,8 @@ import warnings
 import pytest
 from keras import backend as K
 
-warning_msg = r'your data is of type "float64", but your input ' \
-              r'variable (.*) expects (.*). Please convert your ' \
-              r'data beforehand to speed up training.'
+
+warnings.simplefilter('once')
 
 
 @pytest.fixture(autouse=True)
@@ -13,9 +12,7 @@ def clear_session_after_test():
 
     This wrapper runs for all the tests in the keras test suite.
     """
-    with warnings.catch_warnings():
-        warnings.filterwarnings('ignore', message=warning_msg, category=UserWarning)
-        yield
+    yield
 
     if K.backend() == 'tensorflow' or K.backend() == 'cntk':
         K.clear_session()
