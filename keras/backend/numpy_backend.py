4b59b4d66d01c4ceeb2b4ca2dc15cf249c9083f8
danErez <dan.erez.sd@gmail.com>
2018-11-25 09:02:31 +0200

    rest travis ci - also add Execption() around ctc decode.

--- keras/backend/numpy_backend.py
+++ keras/backend/numpy_backend.py
@@ -704,7 +704,7 @@ def ctc_decode(y_pred, input_length, greedy=True, beam_width=100, top_paths=1):
             decoded_dense[i, :len(decoded)] = decoded
         return decoded_dense[:, :np.max(decoded_length)], log_prob
     else:
-        raise "not supported yet"
+        raise Exception("not supported yet")
 
 
 def _remove_repeats(inds):
