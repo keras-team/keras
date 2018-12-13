351e9a07323a0375de7ee8ef3eca9d5ff7774ee0
Tarun S Paparaju <tarunp@greenwoodhigh.edu.in>
2018-12-13 23:18:29 +0530

    Update callbacks.py

--- keras/callbacks.py
+++ keras/callbacks.py
@@ -245,26 +245,7 @@ class BaseLogger(Callback):
                     if k in self.stateful_metrics:
                         logs[k] = self.totals[k]
                     else:
-                        logs[k] = self.totals[k] / self.seen
-                        
-class BinaryClassificationMetrics(Callback):
-    def on_train_begin(self, logs=None):
-        self.val_precisions = []
-        self.val_f1s = []
-        self.val_recalls = []
-        
-    def on_epoch_end(self, epoch, logs=None):
-        logs = logs or {}
-        val_predict = (np.asarray(self.model.predict(self.model.validation_data[0]))).round()
-        val_targ = self.model.validation_data[1]
-        _val_precision = np.dot(val_targ, val_predict)/sum(val_predict)
-        _val_recall = np.dot(val_targ, val_predict)/sum(val_targ)
-        _val_f1 = (2*_val_precision*_val_recall)/(_val_precision + _val_recall)
-        self.val_precisions.append(_val_precision)
-        self.val_recalls.append(_val_recall)
-        self.val_f1s.append(_val_f1)
-        print(" — val_f1: %f — val_precision: %f — val_recall %f" %(_val_f1, _val_precision, _val_recall))
-        return  
+                        logs[k] = self.totals[k] / self.seen           
 
 class TerminateOnNaN(Callback):
     """Callback that terminates training when a NaN loss is encountered.
