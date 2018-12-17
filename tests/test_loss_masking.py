4b54657ab4806b0aaef8f8eeb973edb83c3d3483
Zhao HG <CyberZHG@gmail.com>
2018-12-17 08:13:16 +0800

    [P] Fixed NaN loss when given a mask with all zeros (#11643)

--- tests/test_loss_masking.py
+++ tests/test_loss_masking.py
@@ -8,16 +8,28 @@ from keras import losses
 from keras import backend as K
 
 
+def create_masking_model():
+    model = Sequential()
+    model.add(Masking(mask_value=0, input_shape=(None, 1)))
+    model.add(TimeDistributed(Dense(1, kernel_initializer='one')))
+    model.compile(loss='mse', optimizer='sgd')
+    return model
+
+
 def test_masking():
     np.random.seed(1337)
     x = np.array([[[1], [1]],
                   [[0], [0]]])
-    model = Sequential()
-    model.add(Masking(mask_value=0, input_shape=(2, 1)))
-    model.add(TimeDistributed(Dense(1, kernel_initializer='one')))
-    model.compile(loss='mse', optimizer='sgd')
+    model = create_masking_model()
     y = np.array([[[1], [1]],
                   [[1], [1]]])
+    loss = model.train_on_batch(x, y)
+    assert loss == 0
+
+
+def test_masking_is_all_zeros():
+    x = y = np.array([[[0], [0]]])
+    model = create_masking_model()
     loss = model.train_on_batch(x, y)
     assert loss == 0
 
