a67fb917ad403ebc392ce2cbb2acff592cb4fbc2
Dref360 <frederic.branchaud-charron@usherbrooke.ca>
2018-12-18 11:59:49 -0500

    use_spawn check for windows

--- tests/test_multiprocessing.py
+++ tests/test_multiprocessing.py
@@ -23,11 +23,13 @@ skip_generators = pytest.mark.skipif(K.backend() in {'tensorflow', 'cntk'} and
 
 
 def use_spawn(func):
-    """Decorator to test both Unix (fork) and Windows (spawn)"""
+    """Decorator which uses `spawn` when possible.
+    This is useful on Travis to avoid memory issues.
+    """
 
     @six.wraps(func)
     def wrapper(*args, **kwargs):
-        if sys.version_info > (3, 4):
+        if sys.version_info > (3, 4) and os.name != 'nt':
             mp.set_start_method('spawn', force=True)
             out = func(*args, **kwargs)
             mp.set_start_method('fork', force=True)
