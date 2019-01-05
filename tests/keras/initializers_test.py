1d81a20292ca6926e595d06a6cd725dbb104a146
Raphael Meudec <raphael.meudec@gmail.com>
2019-01-05 15:22:04 +0100

    [P] Identity initializer with zero padding (#11887)
    * modifying identity initializer behaviour
    
    * impact tests on initializers
    
    * remove limitation on shape which doesn't need to be multiple from each other anymore. Update docstring accordingl
    
    * review comments
    
    * fix docstring
    
    * use recurrent identity for RNN layers
    
    * shape[0] can't be larger than shape[1] for RNN layers
    
    * fix behaviour in python2.7
    
    * switch type checking to isinstance

--- tests/keras/initializers_test.py
+++ tests/keras/initializers_test.py
@@ -108,13 +108,14 @@ def test_orthogonal(tensor_shape):
                          [(100, 100), (10, 20), (30, 80), (1, 2, 3, 4)],
                          ids=['FC', 'RNN', 'RNN_INVALID', 'CONV'])
 def test_identity(tensor_shape):
-    if len(tensor_shape) > 2 or max(tensor_shape) % min(tensor_shape) != 0:
+    target_mean = (1. * min(tensor_shape)) / (tensor_shape[0] * tensor_shape[1])
+    if len(tensor_shape) > 2:
         with pytest.raises(ValueError):
             _runner(initializers.identity(), tensor_shape,
-                    target_mean=1. / tensor_shape[0], target_max=1.)
+                    target_mean=target_mean, target_max=1.)
     else:
         _runner(initializers.identity(), tensor_shape,
-                target_mean=1. / tensor_shape[0], target_max=1.)
+                target_mean=target_mean, target_max=1.)
 
 
 @pytest.mark.parametrize('tensor_shape', [FC_SHAPE, CONV_SHAPE], ids=['FC', 'CONV'])
