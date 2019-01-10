e94093cb47373697599533b7d1ff085bb5fa84a3
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2019-01-10 20:40:27 +0100

    printing the memory usage.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,17 @@
+from __future__ import print_function
+
 import pytest
 from keras import backend as K
+import os
+from psutil import Process
+
+_proc = Process(os.getpid())
+
+history_of_memory_used = []
+
+
+def get_consumed_ram():
+    return _proc.memory_info().rss / (1024 * 1024)
 
 
 @pytest.fixture(autouse=True)
@@ -11,3 +23,8 @@ def clear_session_after_test():
     yield
     if K.backend() == 'tensorflow' or K.backend() == 'cntk':
         K.clear_session()
+
+    mem_used = get_consumed_ram()
+    history_of_memory_used.append(mem_used)
+    print('Memory used by ' + str(os.getpid()) + ' is ' + mem_used)
+    print('History of ' + str(os.getpid()) + ' is ' + str(history_of_memory_used))
