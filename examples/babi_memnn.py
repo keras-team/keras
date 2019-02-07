1126e69e5f9b36eb2dbd24caf0bdb20ef8a9997c
joosephook <joosephook@gmail.com>
2019-02-07 16:43:45 +0000

    Paper titles now link to the corresponding papers toexamples/babi_memnn.py

--- examples/babi_memnn.py
+++ examples/babi_memnn.py
@@ -4,12 +4,10 @@
 References:
 
 - Jason Weston, Antoine Bordes, Sumit Chopra, Tomas Mikolov, Alexander M. Rush,
-  "Towards AI-Complete Question Answering: A Set of Prerequisite Toy Tasks",
-  <http://arxiv.org/abs/1502.05698>
+  ["Towards AI-Complete Question Answering: A Set of Prerequisite Toy Tasks"](http://arxiv.org/abs/1502.05698)
 
 - Sainbayar Sukhbaatar, Arthur Szlam, Jason Weston, Rob Fergus,
-  "End-To-End Memory Networks",
-  <http://arxiv.org/abs/1503.08895>
+  ["End-To-End Memory Networks"](http://arxiv.org/abs/1503.08895)
 
 Reaches 98.6% accuracy on task 'single_supporting_fact_10k' after 120 epochs.
 Time per epoch: 3s on CPU (core i7).
