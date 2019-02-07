8814c4324416913b6191d2b09689a6cb5935782d
Moritz Kirschte <moritz.kirschte@gmail.com>
2019-02-07 12:32:56 +0100

    applied @fchollet style-suggestions and added the ability to manually specify a filter_range

--- examples/conv_filter_visualization.py
+++ examples/conv_filter_visualization.py
@@ -1,41 +1,41 @@
-'''Visualization of the filters of VGG16, via gradient ascent in input space.
+"""Visualization of the filters of VGG16, via gradient ascent in input space.
 
 This script can run on CPU in a few minutes.
 
 Results example: http://i.imgur.com/4nj4KjN.jpg
-'''
+"""
 from __future__ import print_function
 
 import time
 import numpy as np
 from PIL import Image as pil_image
 from keras.preprocessing.image import save_img
-from keras.layers.convolutional import Conv2D
+from keras import layers
 from keras.applications import vgg16
 from keras import backend as K
 
 
-def _normalize(x):
-    '''utility function to normalize a tensor.
+def normalize(x):
+    """utility function to normalize a tensor.
 
     # Arguments
         x: An input tensor.
 
     # Returns
         The normalized input tensor.
-    '''
+    """
     return x / (K.sqrt(K.mean(K.square(x))) + K.epsilon())
 
 
-def _deprocess_image(x):
-    '''utility function to convert a float array into a valid uint8 image.
+def deprocess_image(x):
+    """utility function to convert a float array into a valid uint8 image.
 
     # Arguments
         x: A numpy-array representing the generated image.
 
     # Returns
         A processed numpy-array, which could be used in e.g. imshow.
-    '''
+    """
     # normalize tensor: center on 0., ensure std is 0.25
     x -= x.mean()
     x /= (x.std() + K.epsilon())
@@ -53,9 +53,9 @@ def _deprocess_image(x):
     return x
 
 
-def _process_image(x, former):
-    '''utility function to convert a valid uint8 image back into a float array.
-       Reverses `_deprocess_image`.
+def process_image(x, former):
+    """utility function to convert a valid uint8 image back into a float array.
+       Reverses `deprocess_image`.
 
     # Arguments
         x: A numpy-array, which could be used in e.g. imshow.
@@ -64,7 +64,7 @@ def _process_image(x, former):
 
     # Returns
         A processed numpy-array representing the generated image.
-    '''
+    """
     if K.image_data_format() == 'channels_first':
         x = x.transpose((2, 0, 1))
     return (x / 255 - 0.5) * 4 * former.std() + former.mean()
@@ -73,11 +73,12 @@ def _process_image(x, former):
 def visualize_layer(model,
                     layer_name,
                     step=1.,
-                    epochs=10,
+                    epochs=15,
                     upscaling_steps=9,
                     upscaling_factor=1.2,
-                    output_dim=(412, 412)):
-    '''Visualizes the most relevant filters of one conv-layer in a certain model.
+                    output_dim=(412, 412),
+                    filter_range=(0, None)):
+    """Visualizes the most relevant filters of one conv-layer in a certain model.
 
     # Arguments
         model: The model containing layer_name.
@@ -90,22 +91,27 @@ def visualize_layer(model,
         upscaling_factor: Factor to which to slowly upgrade
                           the image towards output_dim.
         output_dim: [img_width, img_height] The output image dimensions.
-    '''
+        filter_range: Tupel[lower, upper]
+                      Determines the to be computed filter numbers.
+                      If the second value is `None`,
+                      the last filter will be inferred as the upper boundary.
+    """
 
     def _generate_filter_image(input_img,
                                layer_output,
                                filter_index):
-        '''Generates image for one particular filter.
+        """Generates image for one particular filter.
 
         # Arguments
             input_img: The input-image Tensor.
             layer_output: The output-image Tensor.
-            filter_index: The to be processed filter number: assumed to be valid.
+            filter_index: The to be processed filter number.
+                          Assumed to be valid.
 
         #Returns
             Either None if no image could be generated.
             or a tuple of the image (array) itself and the last loss.
-        '''
+        """
         s_time = time.time()
 
         # we build a loss function that maximizes the activation
@@ -119,7 +125,7 @@ def visualize_layer(model,
         grads = K.gradients(loss, input_img)[0]
 
         # normalization trick: we normalize the gradient
-        grads = _normalize(grads)
+        grads = normalize(grads)
 
         # this function returns the loss and grads given the input picture
         iterate = K.function([input_img], [loss, grads])
@@ -154,13 +160,13 @@ def visualize_layer(model,
             intermediate_dim = tuple(
                 int(x / (upscaling_factor ** up)) for x in output_dim)
             # Upscale
-            img = _deprocess_image(input_img_data[0])
+            img = deprocess_image(input_img_data[0])
             img = np.array(pil_image.fromarray(img).resize(intermediate_dim,
                                                            pil_image.BICUBIC))
-            input_img_data = [_process_image(img, input_img_data[0])]
+            input_img_data = [process_image(img, input_img_data[0])]
 
         # decode the resulting input image
-        img = _deprocess_image(input_img_data[0])
+        img = deprocess_image(input_img_data[0])
         e_time = time.time()
         print('Costs of filter {:3}: {:5.0f} ( {:4.2f}s )'.format(filter_index,
                                                                   loss_value,
@@ -168,14 +174,14 @@ def visualize_layer(model,
         return img, loss_value
 
     def _draw_filters(filters, n=None):
-        '''Draw the best filters in a nxn grid.
+        """Draw the best filters in a nxn grid.
 
         # Arguments
             filters: A List of generated images and their corresponding losses
                      for each processed filter.
             n: dimension of the grid.
                If none, the largest possible square will be used
-        '''
+        """
         if n is None:
             n = int(np.floor(np.sqrt(len(filters))))
 
@@ -186,17 +192,17 @@ def visualize_layer(model,
 
         # build a black picture with enough space for
         # e.g. our 8 x 8 filters of size 412 x 412, with a 5px margin in between
-        margin = 5
-        width = n * output_dim[0] + (n - 1) * margin
-        height = n * output_dim[1] + (n - 1) * margin
+        MARGIN = 5
+        width = n * output_dim[0] + (n - 1) * MARGIN
+        height = n * output_dim[1] + (n - 1) * MARGIN
         stitched_filters = np.zeros((width, height, 3), dtype='uint8')
 
         # fill the picture with our saved filters
         for i in range(n):
             for j in range(n):
                 img, _ = filters[i * n + j]
-                width_margin = (output_dim[0] + margin) * i
-                height_margin = (output_dim[1] + margin) * j
+                width_margin = (output_dim[0] + MARGIN) * i
+                height_margin = (output_dim[1] + MARGIN) * j
                 stitched_filters[
                     width_margin: width_margin + output_dim[0],
                     height_margin: height_margin + output_dim[1], :] = img
@@ -212,11 +218,21 @@ def visualize_layer(model,
     layer_dict = dict([(layer.name, layer) for layer in model.layers[1:]])
 
     output_layer = layer_dict[layer_name]
-    assert isinstance(output_layer, Conv2D)
+    assert isinstance(output_layer, layers.Conv2D)
+
+    # Compute to be processed filter range
+    filter_lower = filter_range[0]
+    filter_upper = (filter_range[1]
+                    if filter_range[1] is not None
+                    else len(output_layer.get_weights()[1]))
+    assert(filter_lower >= 0
+           and filter_upper <= len(output_layer.get_weights()[1])
+           and filter_upper > filter_lower)
+    print('Compute filters {:} to {:}'.format(filter_lower, filter_upper))
 
     # iterate through each filter and generate its corresponding image
     processed_filters = []
-    for f in range(len(output_layer.get_weights()[1])):
+    for f in range(filter_lower, filter_upper):
         img_loss = _generate_filter_image(input_img, output_layer.output, f)
 
         if img_loss is not None:
@@ -235,7 +251,7 @@ if __name__ == '__main__':
     # build the VGG16 network with ImageNet weights
     vgg = vgg16.VGG16(weights='imagenet', include_top=False)
     print('Model loaded.')
-    print(vgg.summary())
+    vgg.summary()
 
     # example function call
     visualize_layer(vgg, LAYER_NAME)
