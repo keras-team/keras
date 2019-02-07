c0b476af98144112a6990ee57a792fd6d6963899
joosephook <joosephook@gmail.com>
2019-02-07 16:59:12 +0000

    Broke up long paper title for PEP8 in toexamples/babi_memnn.py

--- examples/babi_memnn.py
+++ examples/babi_memnn.py
@@ -4,7 +4,8 @@
 References:
 
 - Jason Weston, Antoine Bordes, Sumit Chopra, Tomas Mikolov, Alexander M. Rush,
-  ["Towards AI-Complete Question Answering: A Set of Prerequisite Toy Tasks"](http://arxiv.org/abs/1502.05698)
+  ["Towards AI-Complete Question Answering:
+  A Set of Prerequisite Toy Tasks"](http://arxiv.org/abs/1502.05698)
 
 - Sainbayar Sukhbaatar, Arthur Szlam, Jason Weston, Rob Fergus,
   ["End-To-End Memory Networks"](http://arxiv.org/abs/1503.08895)
