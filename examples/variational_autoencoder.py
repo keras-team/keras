a0c66202a536d1745845559c00545c85e1c602e8
Sklan <Sklan@users.noreply.github.com>
2019-02-11 21:49:05 +0530

    Update variational_autoencoder.py

--- examples/variational_autoencoder.py
+++ examples/variational_autoencoder.py
@@ -33,7 +33,6 @@ import os
 # reparameterization trick
 # instead of sampling from Q(z|X), sample epsilon = N(0,I)
 # z = z_mean + sqrt(var) * epsilon
-
 def sampling(args):
     """Reparameterization trick by sampling from an isotropic unit Gaussian.
 
@@ -50,6 +49,7 @@ def sampling(args):
     # by default, random_normal has mean = 0 and std = 1.0
     epsilon = K.random_normal(shape=(batch, dim))
     return z_mean + K.exp(0.5 * z_log_var) * epsilon
+
 
 def plot_results(models,
                  data,
