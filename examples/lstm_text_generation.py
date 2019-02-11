77874666a24fae40ba19b773fff3bf0fd4a0cf63
dudeperf3ct <dudeperf3ct@users.noreply.github.com>
2019-02-11 08:33:49 +0530

    add verbose=0
    If `verbose=0` is not added, we get the following error.
    
    `AttributeError: 'ProgbarLogger' object has no attribute 'log_values'`

--- examples/lstm_text_generation.py
+++ examples/lstm_text_generation.py
@@ -109,4 +109,5 @@ print_callback = LambdaCallback(on_epoch_end=on_epoch_end)
 model.fit(x, y,
           batch_size=128,
           epochs=60,
-          callbacks=[print_callback])
+          callbacks=[print_callback],
+          verbose=0)
