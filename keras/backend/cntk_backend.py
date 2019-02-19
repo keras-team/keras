eca52f0862cd0819e2574a9b06cf1e2da7f9fc14
Abhai Kollara Dilip <abhai.dilip@gmail.com>
2019-02-19 21:02:01 +0530

    Cumulative sum for CNTK

--- keras/backend/cntk_backend.py
+++ keras/backend/cntk_backend.py
@@ -2743,7 +2743,14 @@ def to_dense(tensor):
 
 
 def cumsum(x, axis=0):
-    raise NotImplementedError
+    dim = x.shape[axis]
+    U = C.constant(np.triu(np.ones((dim, dim))).astype(x.dtype))
+    if axis != -1:
+        x = C.swapaxes(x, -1, axis)
+    out = C.times(x, U)
+    if axis != -1:
+        out = C.swapaxes(out, -1, axis)
+    return out
 
 
 def cumprod(x, axis=0):
