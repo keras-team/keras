11f161d4e4a2104899ad7cb407baee6ab6487fa3
Milos <34348494+mkondela@users.noreply.github.com>
2019-02-25 00:22:04 +0100

    Fix typo in siamese example (#12339)

--- examples/mnist_siamese.py
+++ examples/mnist_siamese.py
@@ -43,9 +43,9 @@ def contrastive_loss(y_true, y_pred):
     http://yann.lecun.com/exdb/publis/pdf/hadsell-chopra-lecun-06.pdf
     '''
     margin = 1
-    sqaure_pred = K.square(y_pred)
+    square_pred = K.square(y_pred)
     margin_square = K.square(K.maximum(margin - y_pred, 0))
-    return K.mean(y_true * sqaure_pred + (1 - y_true) * margin_square)
+    return K.mean(y_true * square_pred + (1 - y_true) * margin_square)
 
 
 def create_pairs(x, digit_indices):
