663aed04596bcd723cfabef282635a64ae8b2de2
Bita Hasheminezhad <bita.hn90@gmail.com>
2019-02-25 16:56:00 -0600

    An initial implementation of eval by Rod

--- keras/backend/phylanx_backend.py
+++ keras/backend/phylanx_backend.py
@@ -4,13 +4,35 @@ from __future__ import division
 from __future__ import print_function
 
 import numpy as np
+import phylanx
+from phylanx import Phylanx, PhylanxSession
 from .common import floatx
+
+PhylanxSession.init(1)
+
+et = phylanx.execution_tree
+cs = phylanx.compiler_state()
 
 def variable(value, dtype=None, name=None, constraint=None):
     if constraint is not None:
         raise TypeError("Constraint must be None when "
                         "using the NumPy backend.")
-    return np.array(value, dtype)
+    return phylanx.execution_tree.var(np.array(value, dtype))
 
-def eye(size, dtype=None, name=None):
-    return np.eye(size, dtype=dtype)
+
+class eye:
+    def __init__(self, *args):
+        @Phylanx
+        def eye_impl(size, dtype=None, name=None):
+            return np.eye(size, dtype='float')
+
+        self.src = eye_impl.__src__
+        self.args = args
+
+    def eval(self):
+        return et.eval(self.src, cs, *self.args)
+
+def eval(func):
+    return func.eval()
+
+
