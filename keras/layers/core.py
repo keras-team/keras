68bcc3ba99be2b4bbece0cdfd6eb1ab9f44be837
Marc Uecker <Pyrestone@users.noreply.github.com>
2019-03-30 12:40:29 +0100

    Add drop_at_test_time parameter to Dropout.

--- keras/layers/core.py
+++ keras/layers/core.py
@@ -94,18 +94,20 @@ class Dropout(Layer):
             you want the dropout mask to be the same for all timesteps,
             you can use `noise_shape=(batch_size, 1, features)`.
         seed: A Python integer to use as random seed.
+        drop_at_test_time: wether to use
 
     # References
         - [Dropout: A Simple Way to Prevent Neural Networks from Overfitting](
            http://www.jmlr.org/papers/volume15/srivastava14a/srivastava14a.pdf)
     """
     @interfaces.legacy_dropout_support
-    def __init__(self, rate, noise_shape=None, seed=None, **kwargs):
+    def __init__(self, rate, noise_shape=None, seed=None, drop_at_test_time=False, **kwargs):
         super(Dropout, self).__init__(**kwargs)
         self.rate = min(1., max(0., rate))
         self.noise_shape = noise_shape
         self.seed = seed
         self.supports_masking = True
+        self.drop_at_test_time = drop_at_test_time
 
     def _get_noise_shape(self, inputs):
         if self.noise_shape is None:
@@ -124,13 +126,15 @@ class Dropout(Layer):
                 return K.dropout(inputs, self.rate, noise_shape,
                                  seed=self.seed)
             return K.in_train_phase(dropped_inputs, inputs,
-                                    training=training)
+                                    training=(training or drop_at_test_time))
         return inputs
 
     def get_config(self):
         config = {'rate': self.rate,
                   'noise_shape': self.noise_shape,
-                  'seed': self.seed}
+                  'seed': self.seed,
+                  'drop_at_test_time': self.drop_at_test_time
+                 }
         base_config = super(Dropout, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
 
