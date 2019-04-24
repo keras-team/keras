5c2af3586018fb41156dc86cdff14654b8888f90
Marcela Morales Quispe <marcela.morales.quispe@gmail.com>
2019-04-23 20:45:27 -0500

    Add MarkDown formatting to examples/mnist_irnn.py
    Signed-off-by: Marcela Morales Quispe <marcela.morales.quispe@gmail.com>

--- examples/mnist_irnn.py
+++ examples/mnist_irnn.py
@@ -1,16 +1,15 @@
-'''This is a reproduction of the IRNN experiment
-with pixel-by-pixel sequential MNIST in
+'''
+# A reproduction of the IRNN experiment
+Using pixel-by-pixel sequential MNIST from
 "A Simple Way to Initialize Recurrent Networks of Rectified Linear Units"
 by Quoc V. Le, Navdeep Jaitly, Geoffrey E. Hinton
-
-arxiv:1504.00941v2 [cs.NE] 7 Apr 2015
-http://arxiv.org/pdf/1504.00941v2.pdf
+([arxiv:1504.00941v2 [cs.NE] 7 Apr 2015](http://arxiv.org/pdf/1504.00941v2.pdf))
 
 Optimizer is replaced with RMSprop which yields more stable and steady
 improvement.
 
 Reaches 0.93 train/test accuracy after 900 epochs
-(which roughly corresponds to 1687500 steps in the original paper.)
+(which roughly corresponds to 1687500 steps in the original paper).
 '''
 
 from __future__ import print_function
