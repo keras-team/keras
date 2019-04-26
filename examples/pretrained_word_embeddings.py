9d7ea898c3b8edc7774728d13d560bc29ddbaa64
Marcela Morales Quispe <marcela.morales.quispe@gmail.com>
2019-04-25 22:52:25 -0500

    Add MarkDown formatting to examples/pretrained_word_embeddings.py
    Signed-off-by: Marcela Morales Quispe <marcela.morales.quispe@gmail.com>

--- examples/pretrained_word_embeddings.py
+++ examples/pretrained_word_embeddings.py
@@ -1,14 +1,16 @@
-'''This script loads pre-trained word embeddings (GloVe embeddings)
+'''
+# Pretrained Word Embeddings
+This script loads pre-trained word embeddings (GloVe embeddings)
 into a frozen Keras Embedding layer, and uses it to
 train a text classification model on the 20 Newsgroup dataset
 (classification of newsgroup messages into 20 different categories).
 
 GloVe embedding data can be found at:
-http://nlp.stanford.edu/data/glove.6B.zip
-(source page: http://nlp.stanford.edu/projects/glove/)
+<http://nlp.stanford.edu/data/glove.6B.zip>
+(source page: <http://nlp.stanford.edu/projects/glove/>)
 
 20 Newsgroup data can be found at:
-http://www.cs.cmu.edu/afs/cs.cmu.edu/project/theo-20/www/data/news20.html
+<http://www.cs.cmu.edu/afs/cs.cmu.edu/project/theo-20/www/data/news20.html>
 '''
 
 from __future__ import print_function
