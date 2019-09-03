f7949282704e9943a98b03dc047995ba0b57e032
Pavithra Vijay <psv@google.com>
2019-09-03 15:14:28 -0700

    Adding TruePositives, TrueNegatives, FalseNegatives metric classes.

--- keras/metrics.py
+++ keras/metrics.py
@@ -1041,12 +1041,11 @@ class TruePositives(_ConfusionMatrixConditionCount):
     [0, 0, 1, 0] then the true positives value would be 1.
 
     If `sample_weight` is given, calculates the sum of the weights of
-    true positives. This metric creates one local variable, `true_positives`
+    true positives. This metric creates one local variable, `accumulator`
     that is used to keep track of the number of true positives.
 
     If `sample_weight` is `None`, weights default to 1.
     Use `sample_weight` of 0 to mask values.
-
 
     Usage with the compile API:
 
@@ -1068,6 +1067,84 @@ class TruePositives(_ConfusionMatrixConditionCount):
     def __init__(self, thresholds=None, name=None, dtype=None):
         super(TruePositives, self).__init__(
             confusion_matrix_cond=metrics_utils.ConfusionMatrix.TRUE_POSITIVES,
+            thresholds=thresholds,
+            name=name,
+            dtype=dtype)
+
+
+class TrueNegatives(_ConfusionMatrixConditionCount):
+    """Calculates the number of true negatives.
+
+    For example, if `y_true` is [0, 1, 0, 0] and `y_pred` is [1, 1, 0, 0]
+    then the true negatives value is 2.  If the weights were specified as
+    [0, 0, 1, 0] then the true negatives value would be 1.
+
+    If `sample_weight` is given, calculates the sum of the weights of
+    true negatives. This metric creates one local variable, `accumulator`
+    that is used to keep track of the number of true negatives.
+
+    If `sample_weight` is `None`, weights default to 1.
+    Use `sample_weight` of 0 to mask values.
+
+    Usage with the compile API:
+
+    ```python
+    model = keras.Model(inputs, outputs)
+    model.compile('sgd', loss='mse', metrics=[keras.metrics.TrueNegatives()])
+    ```
+
+    # Arguments
+        thresholds: (Optional) Defaults to 0.5. A float value or a python
+            list/tuple of float threshold values in [0, 1]. A threshold is compared
+            with prediction values to determine the truth value of predictions
+            (i.e., above the threshold is `true`, below is `false`). One metric
+            value is generated for each threshold value.
+        name: (Optional) string name of the metric instance.
+        dtype: (Optional) data type of the metric result.
+    """
+
+    def __init__(self, thresholds=None, name=None, dtype=None):
+        super(TrueNegatives, self).__init__(
+            confusion_matrix_cond=metrics_utils.ConfusionMatrix.TRUE_NEGATIVES,
+            thresholds=thresholds,
+            name=name,
+            dtype=dtype)
+
+
+class FalseNegatives(_ConfusionMatrixConditionCount):
+    """Calculates the number of false negatives.
+
+    For example, if `y_true` is [0, 1, 1, 1] and `y_pred` is [0, 1, 0, 0]
+    then the false negatives value is 2.  If the weights were specified as
+    [0, 0, 1, 0] then the false negatives value would be 1.
+
+    If `sample_weight` is given, calculates the sum of the weights of
+    false negatives. This metric creates one local variable, `accumulator`
+    that is used to keep track of the number of false negatives.
+
+    If `sample_weight` is `None`, weights default to 1.
+    Use `sample_weight` of 0 to mask values.
+
+    Usage with the compile API:
+
+    ```python
+    model = keras.Model(inputs, outputs)
+    model.compile('sgd', loss='mse', metrics=[keras.metrics.FalseNegatives()])
+    ```
+
+    # Arguments
+        thresholds: (Optional) Defaults to 0.5. A float value or a python
+            list/tuple of float threshold values in [0, 1]. A threshold is compared
+            with prediction values to determine the truth value of predictions
+            (i.e., above the threshold is `true`, below is `false`). One metric
+            value is generated for each threshold value.
+        name: (Optional) string name of the metric instance.
+        dtype: (Optional) data type of the metric result.
+    """
+
+    def __init__(self, thresholds=None, name=None, dtype=None):
+        super(FalseNegatives, self).__init__(
+            confusion_matrix_cond=metrics_utils.ConfusionMatrix.FALSE_NEGATIVES,
             thresholds=thresholds,
             name=name,
             dtype=dtype)
--- tests/keras/metrics_confusion_matrix_test.py
+++ tests/keras/metrics_confusion_matrix_test.py
@@ -69,3 +69,175 @@ class TestFalsePositives(object):
 
         with pytest.raises(Exception):
             metrics.FalsePositives(thresholds=[None])
+
+
+class TestTruePositives(object):
+
+    def test_config(self):
+        tp_obj = metrics.TruePositives(name='my_tp', thresholds=[0.4, 0.9])
+        assert tp_obj.name == 'my_tp'
+        assert len(tp_obj.weights) == 1
+        assert tp_obj.thresholds == [0.4, 0.9]
+
+        # Check save and restore config
+        tp_obj2 = metrics.TruePositives.from_config(tp_obj.get_config())
+        assert tp_obj2.name == 'my_tp'
+        assert len(tp_obj2.weights) == 1
+        assert tp_obj2.thresholds == [0.4, 0.9]
+
+    def test_unweighted(self):
+        tp_obj = metrics.TruePositives()
+
+        y_true = ((0, 1, 0, 1, 0), (0, 0, 1, 1, 1),
+                  (1, 1, 1, 1, 0), (0, 0, 0, 0, 1))
+        y_pred = ((0, 0, 1, 1, 0), (1, 1, 1, 1, 1),
+                  (0, 1, 0, 1, 0), (1, 1, 1, 1, 1))
+
+        result = tp_obj(y_true, y_pred)
+        assert np.allclose(7., K.eval(result))
+
+    def test_weighted(self):
+        tp_obj = metrics.TruePositives()
+        y_true = ((0, 1, 0, 1, 0), (0, 0, 1, 1, 1),
+                  (1, 1, 1, 1, 0), (0, 0, 0, 0, 1))
+        y_pred = ((0, 0, 1, 1, 0), (1, 1, 1, 1, 1),
+                  (0, 1, 0, 1, 0), (1, 1, 1, 1, 1))
+        sample_weight = (1., 1.5, 2., 2.5)
+        result = tp_obj(y_true, y_pred, sample_weight=sample_weight)
+        assert np.allclose(12., K.eval(result))
+
+    def test_unweighted_with_thresholds(self):
+        tp_obj = metrics.TruePositives(thresholds=[0.15, 0.5, 0.85])
+
+        y_pred = ((0.9, 0.2, 0.8, 0.1), (0.2, 0.9, 0.7, 0.6),
+                  (0.1, 0.2, 0.4, 0.3), (0, 1, 0.7, 0.3))
+        y_true = ((0, 1, 1, 0), (1, 0, 0, 0), (0, 0, 0, 0),
+                  (1, 1, 1, 1))
+
+        result = tp_obj(y_true, y_pred)
+        assert np.allclose([6., 3., 1.], K.eval(result))
+
+    def test_weighted_with_thresholds(self):
+        tp_obj = metrics.TruePositives(thresholds=[0.15, 0.5, 0.85])
+
+        y_pred = ((0.9, 0.2, 0.8, 0.1), (0.2, 0.9, 0.7, 0.6),
+                  (0.1, 0.2, 0.4, 0.3), (0, 1, 0.7, 0.3))
+        y_true = ((0, 1, 1, 0), (1, 0, 0, 0), (0, 0, 0, 0),
+                  (1, 1, 1, 1))
+
+        result = tp_obj(y_true, y_pred, sample_weight=37.)
+        assert np.allclose([222., 111., 37.], K.eval(result))
+
+
+class TestTrueNegatives(object):
+
+    def test_config(self):
+        tn_obj = metrics.TrueNegatives(name='my_tn', thresholds=[0.4, 0.9])
+        assert tn_obj.name == 'my_tn'
+        assert len(tn_obj.weights) == 1
+        assert tn_obj.thresholds == [0.4, 0.9]
+
+        # Check save and restore config
+        tn_obj2 = metrics.TrueNegatives.from_config(tn_obj.get_config())
+        assert tn_obj2.name == 'my_tn'
+        assert len(tn_obj2.weights) == 1
+        assert tn_obj2.thresholds == [0.4, 0.9]
+
+    def test_unweighted(self):
+        tn_obj = metrics.TrueNegatives()
+
+        y_true = ((0, 1, 0, 1, 0), (0, 0, 1, 1, 1),
+                  (1, 1, 1, 1, 0), (0, 0, 0, 0, 1))
+        y_pred = ((0, 0, 1, 1, 0), (1, 1, 1, 1, 1),
+                  (0, 1, 0, 1, 0), (1, 1, 1, 1, 1))
+
+        result = tn_obj(y_true, y_pred)
+        assert np.allclose(3., K.eval(result))
+
+    def test_weighted(self):
+        tn_obj = metrics.TrueNegatives()
+        y_true = ((0, 1, 0, 1, 0), (0, 0, 1, 1, 1),
+                  (1, 1, 1, 1, 0), (0, 0, 0, 0, 1))
+        y_pred = ((0, 0, 1, 1, 0), (1, 1, 1, 1, 1),
+                  (0, 1, 0, 1, 0), (1, 1, 1, 1, 1))
+        sample_weight = (1., 1.5, 2., 2.5)
+        result = tn_obj(y_true, y_pred, sample_weight=sample_weight)
+        assert np.allclose(4., K.eval(result))
+
+    def test_unweighted_with_thresholds(self):
+        tn_obj = metrics.TrueNegatives(thresholds=[0.15, 0.5, 0.85])
+
+        y_pred = ((0.9, 0.2, 0.8, 0.1), (0.2, 0.9, 0.7, 0.6),
+                  (0.1, 0.2, 0.4, 0.3), (0, 1, 0.7, 0.3))
+        y_true = ((0, 1, 1, 0), (1, 0, 0, 0), (0, 0, 0, 0), (1, 1, 1, 1))
+
+        result = tn_obj(y_true, y_pred)
+        assert np.allclose([2., 5., 7.], K.eval(result))
+
+    def test_weighted_with_thresholds(self):
+        tn_obj = metrics.TrueNegatives(thresholds=[0.15, 0.5, 0.85])
+
+        y_pred = ((0.9, 0.2, 0.8, 0.1), (0.2, 0.9, 0.7, 0.6),
+                  (0.1, 0.2, 0.4, 0.3), (0, 1, 0.7, 0.3))
+        y_true = ((0, 1, 1, 0), (1, 0, 0, 0), (0, 0, 0, 0), (1, 1, 1, 1))
+        sample_weight = ((0.0, 2.0, 3.0, 5.0),)
+
+        result = tn_obj(y_true, y_pred, sample_weight=sample_weight)
+        assert np.allclose([5., 15., 23.], K.eval(result))
+
+
+class TestFalseNegatives(object):
+
+    def test_config(self):
+        fn_obj = metrics.FalseNegatives(name='my_fn', thresholds=[0.4, 0.9])
+        assert fn_obj.name == 'my_fn'
+        assert len(fn_obj.weights) == 1
+        assert fn_obj.thresholds == [0.4, 0.9]
+
+        # Check save and restore config
+        fn_obj2 = metrics.FalseNegatives.from_config(fn_obj.get_config())
+        assert fn_obj2.name == 'my_fn'
+        assert len(fn_obj2.weights) == 1
+        assert fn_obj2.thresholds == [0.4, 0.9]
+
+    def test_unweighted(self):
+        fn_obj = metrics.FalseNegatives()
+
+        y_true = ((0, 1, 0, 1, 0), (0, 0, 1, 1, 1),
+                  (1, 1, 1, 1, 0), (0, 0, 0, 0, 1))
+        y_pred = ((0, 0, 1, 1, 0), (1, 1, 1, 1, 1),
+                  (0, 1, 0, 1, 0), (1, 1, 1, 1, 1))
+
+        result = fn_obj(y_true, y_pred)
+        assert np.allclose(3., K.eval(result))
+
+    def test_weighted(self):
+        fn_obj = metrics.FalseNegatives()
+        y_true = ((0, 1, 0, 1, 0), (0, 0, 1, 1, 1),
+                  (1, 1, 1, 1, 0), (0, 0, 0, 0, 1))
+        y_pred = ((0, 0, 1, 1, 0), (1, 1, 1, 1, 1),
+                  (0, 1, 0, 1, 0), (1, 1, 1, 1, 1))
+        sample_weight = (1., 1.5, 2., 2.5)
+        result = fn_obj(y_true, y_pred, sample_weight=sample_weight)
+        assert np.allclose(5., K.eval(result))
+
+    def test_unweighted_with_thresholds(self):
+        fn_obj = metrics.FalseNegatives(thresholds=[0.15, 0.5, 0.85])
+
+        y_pred = ((0.9, 0.2, 0.8, 0.1), (0.2, 0.9, 0.7, 0.6),
+                  (0.1, 0.2, 0.4, 0.3), (0, 1, 0.7, 0.3))
+        y_true = ((0, 1, 1, 0), (1, 0, 0, 0), (0, 0, 0, 0), (1, 1, 1, 1))
+
+        result = fn_obj(y_true, y_pred)
+        assert np.allclose([1., 4., 6.], K.eval(result))
+
+    def test_weighted_with_thresholds(self):
+        fn_obj = metrics.FalseNegatives(thresholds=[0.15, 0.5, 0.85])
+
+        y_pred = ((0.9, 0.2, 0.8, 0.1), (0.2, 0.9, 0.7, 0.6),
+                  (0.1, 0.2, 0.4, 0.3), (0, 1, 0.7, 0.3))
+        y_true = ((0, 1, 1, 0), (1, 0, 0, 0), (0, 0, 0, 0), (1, 1, 1, 1))
+        sample_weight = ((3.0,), (5.0,), (7.0,), (4.0,))
+
+        result = fn_obj(y_true, y_pred, sample_weight=sample_weight)
+        assert np.allclose([4., 16., 23.], K.eval(result))
