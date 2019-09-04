0081bb26d7fa79421e9c8b2a794b899f8a302ec7
Pavithra Vijay <psv@google.com>
2019-09-04 14:59:21 -0700

    Adding AUC, SensitivityAtSpecificity metrics.

--- keras/utils/metrics_utils.py
+++ keras/utils/metrics_utils.py
@@ -9,6 +9,9 @@ from .. import backend as K
 from . import losses_utils
 
 
+NEG_INF = -1e10
+
+
 class Reduction(object):
     """Types of metrics reduction.
 
@@ -94,7 +97,53 @@ class ConfusionMatrix(Enum):
     FALSE_NEGATIVES = 'fn'
 
 
+class AUCCurve(Enum):
+    """Type of AUC Curve (ROC or PR)."""
+    ROC = 'ROC'
+    PR = 'PR'
+
+    @staticmethod
+    def from_str(key):
+        if key in ('pr', 'PR'):
+            return AUCCurve.PR
+        elif key in ('roc', 'ROC'):
+            return AUCCurve.ROC
+        else:
+            raise ValueError('Invalid AUC curve value "%s".' % key)
+
+
+class AUCSummationMethod(Enum):
+    """Type of AUC summation method.
+
+    https://en.wikipedia.org/wiki/Riemann_sum)
+
+    Contains the following values:
+    * 'interpolation': Applies mid-point summation scheme for `ROC` curve. For
+    `PR` curve, interpolates (true/false) positives but not the ratio that is
+    precision (see Davis & Goadrich 2006 for details).
+    * 'minoring': Applies left summation for increasing intervals and right
+    summation for decreasing intervals.
+    * 'majoring': Applies right summation for increasing intervals and left
+    summation for decreasing intervals.
+    """
+    INTERPOLATION = 'interpolation'
+    MAJORING = 'majoring'
+    MINORING = 'minoring'
+
+    @staticmethod
+    def from_str(key):
+        if key in ('interpolation', 'Interpolation'):
+            return AUCSummationMethod.INTERPOLATION
+        elif key in ('majoring', 'Majoring'):
+            return AUCSummationMethod.MAJORING
+        elif key in ('minoring', 'Minoring'):
+            return AUCSummationMethod.MINORING
+        else:
+            raise ValueError('Invalid AUC summation method value "%s".' % key)
+
+
 def weighted_assign_add(label, pred, weights, var):
+    # Logical and
     label = K.expand_dims(label, 0)
     pred = K.expand_dims(pred, 0)
     are_different = K.concatenate([label, pred], axis=0)
