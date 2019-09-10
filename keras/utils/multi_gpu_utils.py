b3927f823be4ec11063746b37bd2e4ac9d24d0cb
François Chollet <francois.chollet@gmail.com>
2019-09-10 10:13:03 -0700

    Add back CPU to multi_gpu_utils available devices

--- keras/utils/multi_gpu_utils.py
+++ keras/utils/multi_gpu_utils.py
@@ -13,7 +13,7 @@ from ..utils.generic_utils import to_list
 
 
 def _get_available_devices():
-    return K.tensorflow_backend._get_available_gpus()
+    return K.tensorflow_backend._get_available_gpus() + ['/cpu:0']
 
 
 def _normalize_device_name(name):
