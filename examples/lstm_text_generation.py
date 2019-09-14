8ea7d2e5f18bc9f62dd083ca8dce3d3f638a0013
jamesrichter <jamesaugustrichter@gmail.com>
2019-09-14 12:58:59 -0700

    changed "learning_rate" to "lr"
    In colab at least, "learning_rate" is not an argument for optimizers.  Sorry if this is unnecessary change

--- examples/lstm_text_generation.py
+++ examples/lstm_text_generation.py
@@ -60,7 +60,7 @@ model = Sequential()
 model.add(LSTM(128, input_shape=(maxlen, len(chars))))
 model.add(Dense(len(chars), activation='softmax'))
 
-optimizer = RMSprop(learning_rate=0.01)
+optimizer = RMSprop(lr=0.01)
 model.compile(loss='categorical_crossentropy', optimizer=optimizer)
 
 
