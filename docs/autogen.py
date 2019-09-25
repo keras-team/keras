eaa651f21eea020077b068f073a554fd5160f7d2
fuzzythecat <fuzzy0427@gmail.com>
2019-09-25 14:06:27 +0900

    Fix PEP8 error

--- docs/autogen.py
+++ docs/autogen.py
@@ -348,7 +348,7 @@ def copy_examples(examples_dir, destination_dir):
         docstring, starting_line = get_module_docstring(module_path)
         destination_file = os.path.join(destination_dir, file[:-2] + 'md')
         with open(destination_file, 'w+', encoding='utf-8') as f_out, \
-                open(os.path.join(examples_dir, file), 
+                open(os.path.join(examples_dir, file),
                      'r+', encoding='utf-8') as f_in:
 
             f_out.write(docstring + '\n\n')
