ec400034e59c2da982746cd1da820ab990029a32
Taehoon Lee <me@taehoonlee.com>
2019-10-14 19:15:21 +0900

    Fix PEP8

--- keras/applications/efficientnet.py
+++ keras/applications/efficientnet.py
@@ -3,7 +3,7 @@ from __future__ import division
 from __future__ import print_function
 
 try:
-   from keras_applications import efficientnet
+    from keras_applications import efficientnet
 except:
     efficientnet = None
 from . import keras_modules_injection
