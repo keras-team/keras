95365b202ffb8596825b8fb3168ca1a0076db366
Taehoon Lee <me@taehoonlee.com>
2019-10-14 17:14:59 +0900

    Add module imports

--- keras/applications/__init__.py
+++ keras/applications/__init__.py
@@ -34,3 +34,6 @@ from .densenet import DenseNet121, DenseNet169, DenseNet201
 from .nasnet import NASNetMobile, NASNetLarge
 from .resnet import ResNet101, ResNet152
 from .resnet_v2 import ResNet50V2, ResNet101V2, ResNet152V2
+from .efficientnet import EfficientNetB0, EfficientNetB1, EfficientNetB2
+from .efficientnet import EfficientNetB3, EfficientNetB4, EfficientNetB5
+from .efficientnet import EfficientNetB6, EfficientNetB7
