2beb5cdffc503d6f10a688087e0c2dc5575c41c5
Sunil Goda <sunilgoda@users.noreply.github.com>
2019-10-19 12:43:46 +0530

    Fixed #13441
    Updated the Embeddings layer code block documentation to reflect the correct type of input

--- keras/layers/embeddings.py
+++ keras/layers/embeddings.py
@@ -14,7 +14,7 @@ from ..utils.generic_utils import to_list
 
 
 class Embedding(Layer):
-    """Turns positive integers (indexes) into dense vectors of fixed size.
+    """Turns non-negative integers (indexes) into dense vectors of fixed size.
     eg. [[4], [20]] -> [[0.25, 0.1], [0.6, -0.2]]
 
     This layer can only be used as the first layer in a model.
