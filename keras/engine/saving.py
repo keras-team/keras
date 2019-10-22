4c2969b87913419e550f9011bb90527f7e5d878a
Thibault Buhet <38053590+Tbuhet@users.noreply.github.com>
2019-10-22 16:14:39 +0200

    Fix h5py group naming while model saving (#13477)

--- keras/engine/saving.py
+++ keras/engine/saving.py
@@ -744,7 +744,9 @@ def save_weights_to_hdf5_group(group, layers):
     group.attrs['backend'] = K.backend().encode('utf8')
     group.attrs['keras_version'] = str(keras_version).encode('utf8')
 
-    for layer in layers:
+    # Sort model layers by layer name to ensure that group names are strictly
+    # growing to avoid prefix issues.
+    for layer in sorted(layers, key=lambda x: x.name):
         g = group.create_group(layer.name)
         symbolic_weights = layer.weights
         weight_values = K.batch_get_value(symbolic_weights)
