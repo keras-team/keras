ba8c27ca41c1510ab6afc485f2156484fd25d857
Anup Kumar <anup.rulez@gmail.com>
2019-11-29 16:24:37 +0100

    Fix contrastive_loss equation to fit according to the paper

--- examples/mnist_siamese.py
+++ examples/mnist_siamese.py
@@ -45,7 +45,7 @@ def contrastive_loss(y_true, y_pred):
     margin = 1
     square_pred = K.square(y_pred)
     margin_square = K.square(K.maximum(margin - y_pred, 0))
-    return K.mean(y_true * square_pred + (1 - y_true) * margin_square)
+    return K.mean((1 - y_true) * square_pred + y_true * margin_square)
 
 
 def create_pairs(x, digit_indices):
