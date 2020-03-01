8286f57a8df4ed4b6d0bf1e7eed1e8ac14cb7589
Noah <noahhuetter@gmail.com>
2020-03-01 11:21:11 +0100

    Variable not used
    `num_predictions` is not used in this example

--- examples/cifar10_cnn.py
+++ examples/cifar10_cnn.py
@@ -18,7 +18,6 @@ batch_size = 32
 num_classes = 10
 epochs = 100
 data_augmentation = True
-num_predictions = 20
 save_dir = os.path.join(os.getcwd(), 'saved_models')
 model_name = 'keras_cifar10_trained_model.h5'
 
