dbc6a66639be9ca40841397ebeac49cde3601bd8
Michael Kang <pkang29@gmail.com>
2020-03-01 20:03:41 -0600

    deleted everything

--- docs/theme/main.html
+++
@@ -1,1 +0,0 @@
-{% extends "base.html" %}
--- keras/engine/topology.py
+++
@@ -1,5 +0,0 @@
-"""This module is deprecated, but kept around for backwards compatibility.
-"""
-from .base_layer import Layer, Node, InputSpec
-from .input_layer import Input, InputLayer
-from .network import Network, get_source_inputs
