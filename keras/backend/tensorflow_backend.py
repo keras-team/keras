63e5155423891aac601fddc9be4a2b9b45705658
Pei Jia <jiapei@longervision.com>
2020-03-18 15:28:22 -0700

    Compatible with Tensorflow 2.1

--- keras/backend/tensorflow_backend.py
+++ keras/backend/tensorflow_backend.py
@@ -501,10 +501,10 @@ def _get_available_gpus():
     if _LOCAL_DEVICES is None:
         if _is_tf_1():
             devices = get_session().list_devices()
-            _LOCAL_DEVICES = [x.name for x in devices]
         else:
-            _LOCAL_DEVICES = tf.config.experimental_list_devices()
-    return [x for x in _LOCAL_DEVICES if 'device:gpu' in x.lower()]
+        	devices = tf.config.list_logical_devices()
+        _LOCAL_DEVICES = [x.name for x in devices]
+    return [x for x in _LOCAL_DEVICES if 'device:gpu' in x.name.lower()]
 
 
 def _has_nchw_support():
