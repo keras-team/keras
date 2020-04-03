3f3b982fc98f51a5b357f0559fa103f0db67d86c
Afshin Khadangi <39042320+akhadangi@users.noreply.github.com>
2020-04-03 13:08:05 +1100

    Added TLU (Trainable Linear Units)
    Added TLU class as part of advanced activation functions in Keras from the paper [EM-net: Deep learning for electron microscopy image segmentation] (https://www.biorxiv.org/content/10.1101/2020.02.03.933127v1)

--- keras/layers/advanced_activations.py
+++ keras/layers/advanced_activations.py
@@ -150,6 +150,117 @@ class PReLU(Layer):
         return input_shape
 
 
+class TLU(Layer):
+    """Trainable Rectified Linear Unit.
+
+    It follows:
+    `f(x) = alpha * x for x < 0`,
+    `f(x) =  beta * x for x >= 0`,
+    where `alpha` and `beta` are learned array with the same shape as x.
+
+    # Input shape
+        Arbitrary. Use the keyword argument `input_shape`
+        (tuple of integers, does not include the samples axis)
+        when using this layer as the first layer in a model.
+
+    # Output shape
+        Same shape as the input.
+
+    # Arguments
+        alpha_initializer: initializer function for the weights.
+        alpha_regularizer: regularizer for the weights.
+        alpha_constraint: constraint for the weights.
+        beta_initializer: initializer function for the weights.
+        beta_regularizer: regularizer for the weights.
+        beta_constraint: constraint for the weights.
+        shared_axes: the axes along which to share learnable
+            parameters for the activation function.
+            For example, if the incoming feature maps
+            are from a 2D convolution
+            with output shape `(batch, height, width, channels)`,
+            and you wish to share parameters across space
+            so that each filter only has one set of parameters,
+            set `shared_axes=[1, 2]`.
+
+    # References
+        - [EM-net: Deep learning for electron microscopy 
+        image segmentation](https://www.biorxiv.org/content/10.1101/2020.02.03.933127v1)
+    """
+
+    @interfaces.legacy_prelu_support
+    def __init__(self, alpha_initializer='zeros',
+                 beta_initializer='ones',
+                 alpha_regularizer=None,
+                 alpha_constraint=None,
+                 beta_regularizer=None,
+                 beta_constraint=None,
+                 shared_axes=None,
+                 **kwargs):
+        super(TLU, self).__init__(**kwargs)
+        self.supports_masking = True
+        self.alpha_initializer = initializers.get(alpha_initializer)
+        self.alpha_regularizer = regularizers.get(alpha_regularizer)
+        self.alpha_constraint = constraints.get(alpha_constraint)
+        self.beta_initializer = initializers.get(beta_initializer)
+        self.beta_regularizer = regularizers.get(beta_regularizer)
+        self.beta_constraint = constraints.get(beta_constraint)
+        if shared_axes is None:
+            self.shared_axes = None
+        else:
+            self.shared_axes = to_list(shared_axes, allow_tuple=True)
+
+    def build(self, input_shape):
+        param_shape = list(input_shape[1:])
+        self.param_broadcast = [False] * len(param_shape)
+        if self.shared_axes is not None:
+            for i in self.shared_axes:
+                param_shape[i - 1] = 1
+                self.param_broadcast[i - 1] = True
+        self.alpha = self.add_weight(shape=param_shape,
+                                     name='alpha',
+                                     initializer=self.alpha_initializer,
+                                     regularizer=self.alpha_regularizer,
+                                     constraint=self.alpha_constraint)
+        self.beta = self.add_weight(shape=param_shape,
+                                     name='beta',
+                                     initializer=self.beta_initializer,
+                                     regularizer=self.beta_regularizer,
+                                     constraint=self.beta_constraint)
+        # Set input spec
+        axes = {}
+        if self.shared_axes:
+            for i in range(1, len(input_shape)):
+                if i not in self.shared_axes:
+                    axes[i] = input_shape[i]
+        self.input_spec = InputSpec(ndim=len(input_shape), axes=axes)
+        self.built = True
+
+    def call(self, inputs, mask=None):
+        pos = self.beta * K.relu(inputs)
+        if K.backend() == 'theano':
+            neg = (K.pattern_broadcast(self.alpha, self.param_broadcast) *
+                   (inputs - K.abs(inputs)) * 0.5)
+        else:
+            neg = -self.alpha * K.relu(-inputs)
+        return pos + neg
+
+    def get_config(self):
+        config = {
+            'alpha_initializer': initializers.serialize(self.alpha_initializer),
+            'alpha_regularizer': regularizers.serialize(self.alpha_regularizer),
+            'alpha_constraint': constraints.serialize(self.alpha_constraint),
+            'beta_initializer': initializers.serialize(self.beta_initializer),
+            'beta_regularizer': regularizers.serialize(self.beta_regularizer),
+            'beta_constraint': constraints.serialize(self.beta_constraint),
+            'shared_axes': self.shared_axes
+        }
+        base_config = super(TLU, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
+
+
 class ELU(Layer):
     """Exponential Linear Unit.
 
