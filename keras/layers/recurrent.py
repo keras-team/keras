6098dab408bfdeb5354d11f3f41bad5d297a5aec
Christian Tate <ctate1996@gmail.com>
2020-04-30 10:31:37 -0700

    Update recurrent.py

--- keras/layers/recurrent.py
+++ keras/layers/recurrent.py
@@ -3287,94 +3287,94 @@ def _standardize_args(inputs, initial_state, constants, num_constants):
         return dict(list(base_config.items()) + list(config.items))
 
 
-        class DGLSTM(RNN):
-            """Long Short-Term Memory layer - Hochreiter 1997.
-             # Arguments
-             units: Positive integer, dimensionality of the output space.
-             activation: Activation function to use
-                (see [activations](../activations.md)).
-                Default: hyperbolic tangent (`tanh`).
-                If you pass `None`, no activation is applied
-                (ie. "linear" activation: `a(x) = x`).
-             recurrent_activation: Activation function to use
-                for the recurrent step
-                (see [activations](../activations.md)).
-                Default: sigmoid (`sigmoid`).
-                If you pass `None`, no activation is applied
-                (ie. "linear" activation: `a(x) = x`).
-             use_bias: Boolean, whether the layer uses a bias vector.
-             kernel_initializer: Initializer for the `kernel` weights matrix,
-                used for the linear transformation of the inputs.
-                (see [initializers](../initializers.md)).
-             recurrent_initializer: Initializer for the `recurrent_kernel`
-                weights matrix,
-                used for the linear transformation of the recurrent state.
-                (see [initializers](../initializers.md)).
-             bias_initializer: Initializer for the bias vector
-                (see [initializers](../initializers.md)).
-             unit_forget_bias: Boolean.
-                If True, add 1 to the bias of the forget gate at initialization.
-                Setting it to true will also force `bias_initializer="zeros"`.
-                This is recommended in [Jozefowicz et al. (2015)](
-                http://www.jmlr.org/proceedings/papers/v37/jozefowicz15.pdf).
-             kernel_regularizer: Regularizer function applied to
-                the `kernel` weights matrix
-                (see [regularizer](../regularizers.md)).
-             recurrent_regularizer: Regularizer function applied to
-                the `recurrent_kernel` weights matrix
-                (see [regularizer](../regularizers.md)).
-             bias_regularizer: Regularizer function applied to the bias vector
-                (see [regularizer](../regularizers.md)).
-             activity_regularizer: Regularizer function applied to
-                the output of the layer (its "activation").
-                (see [regularizer](../regularizers.md)).
-             kernel_constraint: Constraint function applied to
-                the `kernel` weights matrix
-                (see [constraints](../constraints.md)).
-            recurrent_constraint: Constraint function applied to
-                the `recurrent_kernel` weights matrix
-                (see [constraints](../constraints.md)).
-            bias_constraint: Constraint function applied to the bias vector
-                (see [constraints](../constraints.md)).
-            dropout: Float between 0 and 1.
-                Fraction of the units to drop for
-                the linear transformation of the inputs.
-            recurrent_dropout: Float between 0 and 1.
-                Fraction of the units to drop for
-                the linear transformation of the recurrent state.
-            implementation: Implementation mode, either 1 or 2.
-                Mode 1 will structure its operations as a larger number of
-                smaller dot products and additions, whereas mode 2 will
-                batch them into fewer, larger operations. These modes will
-                have different performance profiles on different hardware and
-                for different applications.
-            return_sequences: Boolean. Whether to return the last output
-                in the output sequence, or the full sequence.
-            return_state: Boolean. Whether to return the last state
-                in addition to the output. The returned elements of the
-                states list are the hidden state and the cell state, respectively.
-            go_backwards: Boolean (default False).
-                If True, process the input sequence backwards and return the
-                reversed sequence.
-            stateful: Boolean (default False). If True, the last state
-                for each sample at index i in a batch will be used as initial
-                state for the sample of index i in the following batch.
-            unroll: Boolean (default False).
-                If True, the network will be unrolled,
-                else a symbolic loop will be used.
-                Unrolling can speed-up a RNN,
-                although it tends to be more memory-intensive.
-                Unrolling is only suitable for short sequences.
-        # References
-            - [Depth-Gated Recurrent Neural Networks](
-              https://arxiv.org/pdf/1508.03790v2.pdf)
-            - [Learning to forget: Continual prediction with LSTM](
-              http://www.mitpressjournals.org/doi/pdf/10.1162/089976600300015015)
-            - [Supervised sequence labeling with recurrent neural networks](
-              http://www.cs.toronto.edu/~graves/preprint.pdf)
-            - [A Theoretically Grounded Application of Dropout in
-               Recurrent Neural Networks](https://arxiv.org/abs/1512.05287)
-               """
+class DGLSTM(RNN):
+"""Depth-Gated LSTM - Yao et al. 2015.
+ # Arguments
+ units: Positive integer, dimensionality of the output space.
+ activation: Activation function to use
+    (see [activations](../activations.md)).
+    Default: hyperbolic tangent (`tanh`).
+    If you pass `None`, no activation is applied
+    (ie. "linear" activation: `a(x) = x`).
+ recurrent_activation: Activation function to use
+    for the recurrent step
+    (see [activations](../activations.md)).
+    Default: sigmoid (`sigmoid`).
+    If you pass `None`, no activation is applied
+    (ie. "linear" activation: `a(x) = x`).
+ use_bias: Boolean, whether the layer uses a bias vector.
+ kernel_initializer: Initializer for the `kernel` weights matrix,
+    used for the linear transformation of the inputs.
+    (see [initializers](../initializers.md)).
+ recurrent_initializer: Initializer for the `recurrent_kernel`
+    weights matrix,
+    used for the linear transformation of the recurrent state.
+    (see [initializers](../initializers.md)).
+ bias_initializer: Initializer for the bias vector
+    (see [initializers](../initializers.md)).
+ unit_forget_bias: Boolean.
+    If True, add 1 to the bias of the forget gate at initialization.
+    Setting it to true will also force `bias_initializer="zeros"`.
+    This is recommended in [Jozefowicz et al. (2015)](
+    http://www.jmlr.org/proceedings/papers/v37/jozefowicz15.pdf).
+ kernel_regularizer: Regularizer function applied to
+    the `kernel` weights matrix
+    (see [regularizer](../regularizers.md)).
+ recurrent_regularizer: Regularizer function applied to
+    the `recurrent_kernel` weights matrix
+    (see [regularizer](../regularizers.md)).
+ bias_regularizer: Regularizer function applied to the bias vector
+    (see [regularizer](../regularizers.md)).
+ activity_regularizer: Regularizer function applied to
+    the output of the layer (its "activation").
+    (see [regularizer](../regularizers.md)).
+ kernel_constraint: Constraint function applied to
+    the `kernel` weights matrix
+    (see [constraints](../constraints.md)).
+recurrent_constraint: Constraint function applied to
+    the `recurrent_kernel` weights matrix
+    (see [constraints](../constraints.md)).
+bias_constraint: Constraint function applied to the bias vector
+    (see [constraints](../constraints.md)).
+dropout: Float between 0 and 1.
+    Fraction of the units to drop for
+    the linear transformation of the inputs.
+recurrent_dropout: Float between 0 and 1.
+    Fraction of the units to drop for
+    the linear transformation of the recurrent state.
+implementation: Implementation mode, either 1 or 2.
+    Mode 1 will structure its operations as a larger number of
+    smaller dot products and additions, whereas mode 2 will
+    batch them into fewer, larger operations. These modes will
+    have different performance profiles on different hardware and
+    for different applications.
+return_sequences: Boolean. Whether to return the last output
+    in the output sequence, or the full sequence.
+return_state: Boolean. Whether to return the last state
+    in addition to the output. The returned elements of the
+    states list are the hidden state and the cell state, respectively.
+go_backwards: Boolean (default False).
+    If True, process the input sequence backwards and return the
+    reversed sequence.
+stateful: Boolean (default False). If True, the last state
+    for each sample at index i in a batch will be used as initial
+    state for the sample of index i in the following batch.
+unroll: Boolean (default False).
+    If True, the network will be unrolled,
+    else a symbolic loop will be used.
+    Unrolling can speed-up a RNN,
+    although it tends to be more memory-intensive.
+    Unrolling is only suitable for short sequences.
+# References
+- [Depth-Gated Recurrent Neural Networks](
+  https://arxiv.org/pdf/1508.03790v2.pdf)
+- [Learning to forget: Continual prediction with LSTM](
+  http://www.mitpressjournals.org/doi/pdf/10.1162/089976600300015015)
+- [Supervised sequence labeling with recurrent neural networks](
+  http://www.cs.toronto.edu/~graves/preprint.pdf)
+- [A Theoretically Grounded Application of Dropout in
+   Recurrent Neural Networks](https://arxiv.org/abs/1512.05287)
+   """
 
     @interfaces.legacy_recurrent_support
     def __init__(self, units,
