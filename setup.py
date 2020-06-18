69d638d4befd668a9f81d1183305ed6355289c65
François Chollet <francois.chollet@gmail.com>
2020-06-18 14:38:49 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,37 +2,24 @@ from setuptools import setup
 from setuptools import find_packages
 
 long_description = '''
-Keras is a high-level neural networks API,
-written in Python and capable of running on top of
-TensorFlow, CNTK, or Theano.
-
-Use Keras if you need a deep learning library that:
-
-- Allows for easy and fast prototyping
-  (through user friendliness, modularity, and extensibility).
-- Supports both convolutional networks and recurrent networks,
-  as well as combinations of the two.
-- Runs seamlessly on CPU and GPU.
+Keras is a high-level neural networks API for Python.
 
 Read the documentation at: https://keras.io/
 
-For a detailed overview of what makes Keras special, see:
-https://keras.io/why-use-keras/
-
-Keras is compatible with Python 2.7-3.6
+Keras is compatible with Python 3.6+
 and is distributed under the MIT license.
 '''
 
 setup(name='Keras',
-      version='2.4.0',
+      version='2.4.1',
       description='Deep Learning for humans',
       long_description=long_description,
       author='Francois Chollet',
       author_email='francois.chollet@gmail.com',
       url='https://github.com/keras-team/keras',
-      download_url='https://github.com/keras-team/keras/tarball/2.3.0',
+      download_url='https://github.com/keras-team/keras/tarball/2.4.1',
       license='MIT',
-      install_requires=['tensorflow>=2.2.0',
+      install_requires=['tensorflow',
                         'numpy>=1.9.1',
                         'scipy>=0.14',
                         'pyyaml',
@@ -54,8 +41,6 @@ setup(name='Keras',
           'Intended Audience :: Education',
           'Intended Audience :: Science/Research',
           'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3',
           'Programming Language :: Python :: 3.6',
           'Topic :: Software Development :: Libraries',
