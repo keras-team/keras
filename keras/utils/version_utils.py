cc6d3f2c279ed017c9f749522d4591de02b1241f
A. Unique TensorFlower <gardener@tensorflow.org>
2021-01-06 18:20:50 -0800

    Optimize `version_utils.swap_class` method for better performance.
    PiperOrigin-RevId: 350465713

--- keras/utils/version_utils.py
+++ keras/utils/version_utils.py
@@ -92,21 +92,27 @@ def should_use_v2():
         graph.name.startswith("wrapped_function")):
       return False
     return True
+  else:
+    return False
 
 
 def swap_class(cls, v2_cls, v1_cls, use_v2):
   """Swaps in v2_cls or v1_cls depending on graph mode."""
   if cls == object:
     return cls
-
   if cls in (v2_cls, v1_cls):
-    if use_v2:
-      return v2_cls
-    return v1_cls
+    return v2_cls if use_v2 else v1_cls
 
   # Recursively search superclasses to swap in the right Keras class.
-  cls.__bases__ = tuple(
-      swap_class(base, v2_cls, v1_cls, use_v2) for base in cls.__bases__)
+  new_bases = []
+  for base in cls.__bases__:
+    if ((use_v2 and issubclass(base, v1_cls)
+         or (not use_v2 and issubclass(base, v2_cls)))):
+      new_base = swap_class(base, v2_cls, v1_cls, use_v2)
+    else:
+      new_base = base
+    new_bases.append(new_base)
+  cls.__bases__ = tuple(new_bases)
   return cls
 
 
