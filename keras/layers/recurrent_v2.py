da39848ecdca190445b6786cb16f93fc921f3be8
Scott Zhu <scottzhu@google.com>
2021-02-22 11:54:44 -0800

    Sync OSS keras to head.
    PiperOrigin-RevId: 358870927

--- keras/layers/recurrent_v2.py
+++ keras/layers/recurrent_v2.py
@@ -21,7 +21,6 @@ from __future__ import print_function
 import tensorflow.compat.v2 as tf
 
 import uuid
-from tensorflow.python.eager import function
 from tensorflow.python.eager.context import get_device_name
 from keras import activations
 from keras import backend as K
@@ -1779,9 +1778,6 @@ def _function_register(func, *args, **kwargs):
   Raises:
     ValueError: When the input function is not a defun wrapped python function.
   """
-  if not isinstance(func, function.Function):
-    raise ValueError('Only defun function is allowed to be registered. '
-                     'Got type: %s' % type(func))
   concrete_func = func.get_concrete_function(*args, **kwargs)
   concrete_func.add_to_graph()
   concrete_func.add_gradient_functions_to_graph()
