44937e880d68ad32cff6ce74fed28e2c5fe5212b
A. Unique TensorFlower <gardener@tensorflow.org>
2021-02-23 15:24:33 -0800

    Enable GPU complex sqrt ops.
    Support for std::complex has been expanded, with most of the standard operators now specialized to work on device. Eigen has also now implemented sqrt for complex numbers on device, allowing us to remove the original restrictions.
    
    PiperOrigin-RevId: 359147785

--- keras/optimizer_v2/rmsprop_test.py
+++ keras/optimizer_v2/rmsprop_test.py
@@ -26,16 +26,12 @@ import math
 
 from absl.testing import parameterized
 import numpy as np
-from tensorflow.python.framework import test_util
 from keras import combinations
 from keras import testing_utils
 from keras.optimizer_v2 import learning_rate_schedule
 from keras.optimizer_v2 import rmsprop
 
-_DATA_TYPES = [tf.half, tf.float32, tf.float64]
-# TODO(b/143684500): Eigen to support complex sqrt
-if not test_util.IsBuiltWithNvcc():
-  _DATA_TYPES += [tf.complex64, tf.complex128]
+_DATA_TYPES = [tf.half, tf.float32, tf.float64, tf.complex64, tf.complex128]
 
 _TEST_PARAM_VALUES = [
     # learning_rate, rho, momentum, epsilon, centered
