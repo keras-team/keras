4ae9001ef615983a405d38213b516bcaa3b39aaf
Scott Zhu <scottzhu@google.com>
2021-02-26 11:02:33 -0800

    Fix OSS keras save model test.
    PiperOrigin-RevId: 359794785

--- keras/saving/saved_model/saved_model_test.py
+++ keras/saving/saved_model/saved_model_test.py
@@ -1183,11 +1183,15 @@ class MetricTest(tf.test.TestCase, parameterized.TestCase):
     class CustomMetric(keras.metrics.MeanSquaredError):
       pass
 
-    model = testing_utils.get_small_mlp(1, 4, input_dim=3)
-    model.compile(loss='mse', optimizer='rmsprop', metrics=[CustomMetric()])
+    with self.cached_session():
+      metric = CustomMetric()
+      model = testing_utils.get_small_mlp(1, 4, input_dim=3)
+      model.compile(loss='mse', optimizer='rmsprop', metrics=[metric])
+      self.evaluate(tf.compat.v1.global_variables_initializer())
+      self.evaluate([v.initializer for v in metric.variables])
 
-    saved_model_dir = self._save_model_dir()
-    tf.saved_model.save(model, saved_model_dir)
+      saved_model_dir = self._save_model_dir()
+      tf.saved_model.save(model, saved_model_dir)
     with self.assertRaisesRegex(ValueError, 'custom_objects'):
       keras_load.load(saved_model_dir)
 
