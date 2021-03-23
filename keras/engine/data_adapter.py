00135a70697dc9b4611971e5833e2d84501c3da7
Scott Zhu <scottzhu@google.com>
2021-03-23 14:27:46 -0700

    Sync OSS keras to head.
    PiperOrigin-RevId: 364644711

--- keras/engine/data_adapter.py
+++ keras/engine/data_adapter.py
@@ -42,15 +42,6 @@ from tensorflow.python.util.tf_export import keras_export
 keras_data_adapter_gauge = tf.__internal__.monitoring.BoolGauge(
     "/tensorflow/api/oss-keras/data_adapters", "keras data adapter usage", "method")
 
-try:
-  from scipy import sparse as scipy_sparse  # pylint: disable=g-import-not-at-top
-except ImportError:
-  scipy_sparse = None
-try:
-  import pandas as pd  # pylint: disable=g-import-not-at-top
-except ImportError:
-  pd = None
-
 
 @six.add_metaclass(abc.ABCMeta)
 class DataAdapter(object):
@@ -223,9 +214,7 @@ class TensorLikeDataAdapter(DataAdapter):
     if y is not None:
       flat_inputs += tf.nest.flatten(y)
 
-    tensor_types = (tf.Tensor, np.ndarray)
-    if pd:
-      tensor_types = (tf.Tensor, np.ndarray, pd.Series, pd.DataFrame)
+    tensor_types = _get_tensor_types()
 
     def _is_tensor(v):
       if isinstance(v, tensor_types):
@@ -558,9 +547,7 @@ class CompositeTensorDataAdapter(DataAdapter):
           not _is_distributed_dataset(v)):
         return True
       # Support Scipy sparse tensors if scipy is installed
-      if scipy_sparse is not None and scipy_sparse.issparse(v):
-        return True
-      return False
+      return _is_scipy_sparse(v)
 
     def _is_tensor_or_composite(v):
       if isinstance(v, (tf.Tensor, np.ndarray)):
@@ -1036,7 +1023,7 @@ def _process_tensorlike(inputs):
       if issubclass(x.dtype.type, np.floating):
         dtype = backend.floatx()
       return tf.convert_to_tensor(x, dtype=dtype)
-    elif scipy_sparse and scipy_sparse.issparse(x):
+    elif _is_scipy_sparse(x):
       return _scipy_sparse_to_sparse_tensor(x)
     return x
 
@@ -1452,9 +1439,7 @@ def train_validation_split(arrays, validation_split):
   """
 
   def _can_split(t):
-    tensor_types = (tf.Tensor, np.ndarray)
-    if pd:
-      tensor_types = (tf.Tensor, np.ndarray, pd.Series, pd.DataFrame)
+    tensor_types = _get_tensor_types()
     return isinstance(t, tensor_types) or t is None
 
   flat_arrays = tf.nest.flatten(arrays)
@@ -1633,6 +1618,24 @@ def _check_data_cardinality(data):
           label, ", ".join(str(i.shape[0]) for i in tf.nest.flatten(single_data)))
     msg += "Make sure all arrays contain the same number of samples."
     raise ValueError(msg)
+
+
+def _get_tensor_types():
+  try:
+    import pandas as pd  # pylint: disable=g-import-not-at-top
+
+    return (tf.Tensor, np.ndarray, pd.Series, pd.DataFrame)
+  except ImportError:
+    return (tf.Tensor, np.ndarray)
+
+
+def _is_scipy_sparse(x):
+  try:
+    from scipy.sparse import issparse  # pylint: disable=g-import-not-at-top
+
+    return issparse(x)
+  except ImportError:
+    return False
 
 
 def _scipy_sparse_to_sparse_tensor(t):
