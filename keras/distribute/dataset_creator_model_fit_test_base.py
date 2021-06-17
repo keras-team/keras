f5d698cd2c9c0378af4d87cd82f8a4a7ad6d522a
Scott Zhu <scottzhu@google.com>
2021-06-17 14:58:58 -0700

    Allow steps_per_epoch=-1 for PSS to run indefinitely.
    PiperOrigin-RevId: 380053049

--- keras/distribute/dataset_creator_model_fit_test_base.py
+++ keras/distribute/dataset_creator_model_fit_test_base.py
@@ -89,11 +89,6 @@ class DatasetCreatorModelFitTestBase(tf.test.TestCase, parameterized.TestCase):
         if not is_loss_float:
           raise RuntimeError("loss is supposed to be in the logs and float.")
 
-      def on_train_end(self, logs=None):
-        if self._prev_epoch != 9:
-          raise RuntimeError("Unexpected last epoch: {}".format(
-              self._prev_epoch))
-
     with strategy.scope():
       model = sequential.Sequential([core_layers.Dense(10)])
       if with_normalization_layer:
