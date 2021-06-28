f1b1939490f440c2d5e6a712476db7a164efe813
Matt Watson <mattdangerw@google.com>
2021-06-28 16:02:57 -0700

    Remove CombinerPreprocessingLayer
    There is no more use of this class, all adaptable layers are ported to
    PreprocessingLayer.
    
    PiperOrigin-RevId: 381965239

--- keras/engine/base_preprocessing_layer_test.py
+++ keras/engine/base_preprocessing_layer_test.py
@@ -14,95 +14,42 @@
 # ==============================================================================
 """Tests for Keras' base preprocessing layer."""
 
-import tensorflow.compat.v2 as tf
-
-import json
 import os
-
 from absl.testing import parameterized
-import numpy as np
+
 
 import keras
 from keras import keras_parameterized
 from keras import testing_utils
 from keras.engine import base_preprocessing_layer
+import numpy as np
+import tensorflow.compat.v2 as tf
 
 
-# Define a test-only implementation of CombinerPreprocessingLayer to validate
+# Define a test-only implementation of BasePreprocessingLayer to validate
 # its correctness directly.
-class AddingPreprocessingLayer(
-    base_preprocessing_layer.CombinerPreprocessingLayer):
-  _SUM_NAME = "sum"
-
-  def __init__(self, **kwargs):
-    super(AddingPreprocessingLayer, self).__init__(
-        combiner=self.AddingCombiner(), **kwargs)
+class AddingPreprocessingLayer(base_preprocessing_layer.PreprocessingLayer):
 
   def build(self, input_shape):
     super(AddingPreprocessingLayer, self).build(input_shape)
-    self._sum = self._add_state_variable(
-        name=self._SUM_NAME,
-        shape=(1,),
-        dtype=tf.float32,
-        initializer=tf.compat.v1.zeros_initializer)
+    self.sum = tf.Variable(0., dtype=tf.float32)
+
+  def update_state(self, data):
+    self.sum.assign_add(tf.reduce_sum(tf.cast(data, tf.float32)))
 
   def reset_state(self):  # pylint: disable=method-hidden
-    self._sum.assign([0.])
+    self.sum.assign(0.)
 
   def set_total(self, sum_value):
     """This is an example of how a subclass would implement a direct setter.
 
-    These methods should generally just create a dict mapping the correct names
-    to the relevant passed values, and call self._set_state_variables() with the
-    dict of data.
-
     Args:
       sum_value: The total to set.
     """
-    self._set_state_variables({self._SUM_NAME: [sum_value]})
+    self.sum.assign(sum_value)
 
   def call(self, inputs):
-    return inputs + self._sum
-
-  # Define a Combiner for this layer class.
-  class AddingCombiner(base_preprocessing_layer.Combiner):
-
-    def compute(self, batch_values, accumulator=None):
-      """Compute a step in this computation, returning a new accumulator."""
-      new_accumulator = 0 if batch_values is None else np.sum(batch_values)
-      if accumulator is None:
-        return new_accumulator
-      else:
-        return self.merge([accumulator, new_accumulator])
-
-    def merge(self, accumulators):
-      """Merge several accumulators to a single accumulator."""
-      # Combine accumulators and return the result.
-      result = accumulators[0]
-      for accumulator in accumulators[1:]:
-        result = np.sum([np.sum(result), np.sum(accumulator)])
-      return result
-
-    def extract(self, accumulator):
-      """Convert an accumulator into a dict of output values."""
-      # We have to add an additional dimension here because the weight shape
-      # is (1,) not None.
-      return {AddingPreprocessingLayer._SUM_NAME: [accumulator]}
-
-    def restore(self, output):
-      """Create an accumulator based on 'output'."""
-      # There is no special internal state here, so we just return the relevant
-      # internal value. We take the [0] value here because the weight itself
-      # is of the shape (1,) and we want the scalar contained inside it.
-      return output[AddingPreprocessingLayer._SUM_NAME][0]
-
-    def serialize(self, accumulator):
-      """Serialize an accumulator for a remote call."""
-      return tf.compat.as_bytes(json.dumps(accumulator))
-
-    def deserialize(self, encoded_accumulator):
-      """Deserialize an accumulator received from 'serialize()'."""
-      return json.loads(tf.compat.as_text(encoded_accumulator))
+    return inputs + self.sum
 
 
 @keras_parameterized.run_all_keras_modes(always_skip_v1=True)
@@ -133,17 +80,6 @@ class PreprocessingLayerTest(keras_parameterized.TestCase):
       with self.assertRaisesRegex(ValueError,
                                   ".*infinite number of elements.*"):
         layer.adapt(input_dataset)
-
-  def test_pre_build_injected_update_with_no_build_fails(self):
-    """Test external update injection before build() is called fails."""
-    input_dataset = np.array([1, 2, 3, 4, 5])
-
-    layer = AddingPreprocessingLayer()
-    combiner = layer._combiner
-    updates = combiner.extract(combiner.compute(input_dataset))
-
-    with self.assertRaisesRegex(RuntimeError, ".*called after build.*"):
-      layer._set_state_variables(updates)
 
   def test_setter_update(self):
     """Test the prototyped setter method."""
@@ -182,39 +118,6 @@ class PreprocessingLayerTest(keras_parameterized.TestCase):
     model._run_eagerly = testing_utils.should_run_eagerly()
 
     layer.adapt(input_dataset)
-
-    self.assertAllEqual([[16], [17], [18]], model.predict([1., 2., 3.]))
-
-  def test_pre_build_injected_update(self):
-    """Test external update injection before build() is called."""
-    input_dataset = np.array([1, 2, 3, 4, 5])
-
-    layer = AddingPreprocessingLayer()
-    combiner = layer._combiner
-    updates = combiner.extract(combiner.compute(input_dataset))
-
-    layer.build((1,))
-    layer._set_state_variables(updates)
-
-    input_data = keras.Input(shape=(1,))
-    output = layer(input_data)
-    model = keras.Model(input_data, output)
-    model._run_eagerly = testing_utils.should_run_eagerly()
-
-    self.assertAllEqual([[16], [17], [18]], model.predict([1., 2., 3.]))
-
-  def test_post_build_injected_update(self):
-    """Test external update injection after build() is called."""
-    input_dataset = np.array([1, 2, 3, 4, 5])
-    input_data = keras.Input(shape=(1,))
-    layer = AddingPreprocessingLayer()
-    output = layer(input_data)
-    model = keras.Model(input_data, output)
-    model._run_eagerly = testing_utils.should_run_eagerly()
-
-    combiner = layer._combiner
-    updates = combiner.extract(combiner.compute(input_dataset))
-    layer._set_state_variables(updates)
 
     self.assertAllEqual([[16], [17], [18]], model.predict([1., 2., 3.]))
 
@@ -291,15 +194,14 @@ class PreprocessingLayerTest(keras_parameterized.TestCase):
   def test_adapt_sets_input_shape_rank(self):
     """Check that `.adapt()` sets the `input_shape`'s rank."""
     # Shape: (3,1,2)
-    adapt_dataset = np.array([[[1., 2.]],
-                              [[3., 4.]],
-                              [[5., 6.]]], dtype=np.float32)
+    adapt_dataset = np.array([[[1., 2.]], [[3., 4.]], [[5., 6.]]],
+                             dtype=np.float32)
 
     layer = AddingPreprocessingLayer()
     layer.adapt(adapt_dataset)
 
-    input_dataset = np.array([[[1., 2.], [3., 4.]],
-                              [[3., 4.], [5., 6.]]], dtype=np.float32)
+    input_dataset = np.array([[[1., 2.], [3., 4.]], [[3., 4.], [5., 6.]]],
+                             dtype=np.float32)
     layer(input_dataset)
 
     model = keras.Sequential([layer])
@@ -309,9 +211,8 @@ class PreprocessingLayerTest(keras_parameterized.TestCase):
   def test_adapt_doesnt_overwrite_input_shape(self):
     """Check that `.adapt()` doesn't change the `input_shape`."""
     # Shape: (3, 1, 2)
-    adapt_dataset = np.array([[[1., 2.]],
-                              [[3., 4.]],
-                              [[5., 6.]]], dtype=np.float32)
+    adapt_dataset = np.array([[[1., 2.]], [[3., 4.]], [[5., 6.]]],
+                             dtype=np.float32)
 
     layer = AddingPreprocessingLayer(input_shape=[1, 2])
     layer.adapt(adapt_dataset)
@@ -355,10 +256,8 @@ class ConvertToListTest(keras_parameterized.TestCase):
           "inputs": lambda: tf.constant([[1, 2, 3], [4, 5, 6]]),
           "expected": [[1, 2, 3], [4, 5, 6]]
       }, {
-          "testcase_name":
-              "ragged_tensor",
-          "inputs":
-              lambda: tf.ragged.constant([[1, 2, 3, 4], [4, 5, 6]]),
+          "testcase_name": "ragged_tensor",
+          "inputs": lambda: tf.ragged.constant([[1, 2, 3, 4], [4, 5, 6]]),
           "expected": [[1, 2, 3, 4], [4, 5, 6]]
       }, {
           "testcase_name": "sparse_tensor",
