5a66d50c4f090bad4f94d60dfc9a6f71159a3ac7
Hongkun Yu <hongkuny@google.com>
2021-06-28 11:28:46 -0700

    Keras docstring: Py3 style subclass
    PiperOrigin-RevId: 381908829

--- keras/engine/training.py
+++ keras/engine/training.py
@@ -142,7 +142,7 @@ class Model(base_layer.Layer, version_utils.ModelVersionSelector):
   class MyModel(tf.keras.Model):
 
     def __init__(self):
-      super(MyModel, self).__init__()
+      super().__init__()
       self.dense1 = tf.keras.layers.Dense(4, activation=tf.nn.relu)
       self.dense2 = tf.keras.layers.Dense(5, activation=tf.nn.softmax)
 
@@ -163,7 +163,7 @@ class Model(base_layer.Layer, version_utils.ModelVersionSelector):
   class MyModel(tf.keras.Model):
 
     def __init__(self):
-      super(MyModel, self).__init__()
+      super().__init__()
       self.dense1 = tf.keras.layers.Dense(4, activation=tf.nn.relu)
       self.dense2 = tf.keras.layers.Dense(5, activation=tf.nn.softmax)
       self.dropout = tf.keras.layers.Dropout(0.5)
