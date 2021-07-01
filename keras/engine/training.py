af061fd3e81ed8a4ba696c65d72be10c63a261d9
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2021-07-01 16:11:55 -0700

    Support only build models; mitigate https://github.com/keras-team/keras/issues/14808

--- keras/engine/training.py
+++ keras/engine/training.py
@@ -323,27 +323,11 @@ class Model(base_layer.Layer, version_utils.ModelVersionSelector):
     super(Model, self).__setattr__(name, value)
 
   def __reduce__(self):
-    if self.built:
-      return (deserialize_model_from_bytecode, serialize_model_as_bytecode(self))
-    else:
-      # SavedModel (and hence serialize_model_as_bytecode) only support built models
-      # but if the model is not built, it can be serialized as a plain Python object
-      # so call up the MRO to get an implementation of __reduce__,
-      # possibly landing at object.__reduce__
-      return super(Model, self).__reduce__()
+    return (deserialize_model_from_bytecode, serialize_model_as_bytecode(self))
 
   def __deepcopy__(self, memo):
-    if self.built:
-      new = deserialize_model_from_bytecode(*serialize_model_as_bytecode(self))
-      memo[id(self)] = new
-    else:
-      # See comment in __reduce__ for explanation 
-      deserializer, serialized, *rest = super(Model, self).__reduce__()
-      new = deserializer(*serialized)
-      memo[id(self)] = new
-      if rest:
-        state = copy.deepcopy(rest[0], memo=memo)
-        new.__setstate__(state)
+    new = deserialize_model_from_bytecode(*serialize_model_as_bytecode(self))
+    memo[id(self)] = new
     return new
 
   def __copy__(self):
