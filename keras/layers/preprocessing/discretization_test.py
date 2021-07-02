75dab47139306fbf5da5171aabe6fc2537144f44
Matt Watson <mattdangerw@google.com>
2021-07-02 11:20:30 -0700

    Output int64 by default from Discretization
    All other "int" output from KPLs (Hashing, StringLookup, IntegerLookup and TextVectorization)
    is int64, we should be consistent.
    
    PiperOrigin-RevId: 382778202

--- keras/layers/preprocessing/discretization_test.py
+++ keras/layers/preprocessing/discretization_test.py
@@ -128,6 +128,12 @@ class DiscretizationTest(keras_parameterized.TestCase,
     output = layer(input_data)
     self.assertAllEqual(output.shape.as_list(), [16, 4])
 
+  def test_output_dtype(self):
+    input_data = keras.Input(batch_size=16, shape=(4,), dtype=tf.int64)
+    layer = discretization.Discretization(bin_boundaries=[-.5, 0.5, 1.5])
+    output = layer(input_data)
+    self.assertAllEqual(output.dtype, tf.int64)
+
   def test_num_bins_negative_fails(self):
     with self.assertRaisesRegex(ValueError, "`num_bins` must be.*num_bins=-7"):
       _ = discretization.Discretization(num_bins=-7)
