f8d3f180e2383078624c6c27288f8833880c909c
Yuefeng Zhou <yuefengz@google.com>
2021-07-14 11:50:05 -0700

    Support dataset for model.fit + PSStrategy.
    PiperOrigin-RevId: 384749842

--- keras/distribute/dataset_creator_model_fit_ps_only_test.py
+++ keras/distribute/dataset_creator_model_fit_ps_only_test.py
@@ -15,47 +15,44 @@
 # ==============================================================================
 """Tests for `DatasetCreator` with `Model.fit` across usages and strategies."""
 
-import tensorflow.compat.v2 as tf
 from keras import callbacks as callbacks_lib
 from keras.distribute import dataset_creator_model_fit_test_base as test_base
 from keras.distribute import strategy_combinations
+import tensorflow.compat.v2 as tf
 
 
 @tf.__internal__.distribute.combinations.generate(
     tf.__internal__.test.combinations.combine(
         strategy=strategy_combinations.parameter_server_strategies_multi_worker,
+        use_dataset_creator=[True, False],
         mode="eager"))
 class DatasetCreatorModelFitParameterServerStrategyOnlyTest(
     test_base.DatasetCreatorModelFitTestBase):
 
-  def testModelFitWithRunEagerly(self, strategy):
+  def testModelFitWithRunEagerly(self, strategy, use_dataset_creator):
     with self.assertRaisesRegex(
         ValueError, "When using `Model` with `ParameterServerStrategy`, "
         "`run_eagerly` is not supported."):
-      self._model_fit(strategy, run_eagerly=True)
-
-  def testModelFitWithDatasetInstance(self, strategy):
-    with self.assertRaisesRegex(
-        NotImplementedError,
-        "Only `tf.keras.utils.experimental.DatasetCreator`, `tf.Tensor`, "
-        "numpy arrays and pandas dataframes are supported types at this "
-        "time."):
       self._model_fit(
-          strategy, x=tf.data.Dataset.from_tensor_slices([1, 1]))
+          strategy, run_eagerly=True, use_dataset_creator=use_dataset_creator)
 
-  def testModelPredict(self, strategy):
+  def testModelPredict(self, strategy, use_dataset_creator):
+    if use_dataset_creator:
+      self.skipTest("Unused option.")
     model, _ = self._model_compile(strategy)
     test_data = tf.data.Dataset.from_tensor_slices(
         [1., 2., 3., 1., 5., 1.]).repeat().batch(2)
     model.predict(x=test_data, steps=3)
 
-  def testClusterCoordinatorSingleInstance(self, strategy):
-    model = self._model_fit(strategy)
+  def testClusterCoordinatorSingleInstance(self, strategy, use_dataset_creator):
+    model = self._model_fit(strategy, use_dataset_creator=use_dataset_creator)
     strategy = model.distribute_strategy
-    self.assertIs(strategy._cluster_coordinator,
-                  tf.distribute.experimental.coordinator.ClusterCoordinator(strategy))
+    self.assertIs(
+        strategy._cluster_coordinator,
+        tf.distribute.experimental.coordinator.ClusterCoordinator(strategy))
 
-  def testModelFitErrorOnBatchLevelCallbacks(self, strategy):
+  def testModelFitErrorOnBatchLevelCallbacks(self, strategy,
+                                             use_dataset_creator):
 
     class BatchLevelCallback(callbacks_lib.Callback):
 
@@ -65,9 +62,12 @@ class DatasetCreatorModelFitParameterServerStrategyOnlyTest(
     with self.assertRaisesRegex(ValueError,
                                 "Batch-level `Callback`s are not supported"):
       callbacks = [BatchLevelCallback()]
-      self._model_fit(strategy, callbacks=callbacks)
+      self._model_fit(
+          strategy,
+          callbacks=callbacks,
+          use_dataset_creator=use_dataset_creator)
 
-  def testModelFitCallbackSupportsTFLogs(self, strategy):
+  def testModelFitCallbackSupportsTFLogs(self, strategy, use_dataset_creator):
 
     class MyCallback(callbacks_lib.Callback):
 
@@ -81,37 +81,32 @@ class DatasetCreatorModelFitParameterServerStrategyOnlyTest(
 
     my_callback = MyCallback()
     callbacks = [my_callback]
-    self._model_fit(strategy, callbacks=callbacks)
+    self._model_fit(
+        strategy, callbacks=callbacks, use_dataset_creator=use_dataset_creator)
 
-  def testModelFitVerbosity(self, strategy):
+  def testModelFitVerbosity(self, strategy, use_dataset_creator):
 
     class MyCallback(callbacks_lib.Callback):
       pass
 
     my_callback = MyCallback()
     callbacks = [my_callback]
-    self._model_fit(strategy, callbacks=callbacks)
+    self._model_fit(
+        strategy, callbacks=callbacks, use_dataset_creator=use_dataset_creator)
     # PSStrategy should default to epoch-level logging.
     self.assertEqual(my_callback.params["verbose"], 2)
 
-  def testModelFitTensorBoardEpochLevel(self, strategy):
+  def testModelFitTensorBoardEpochLevel(self, strategy, use_dataset_creator):
     log_dir = self.get_temp_dir()
     callbacks = [callbacks_lib.TensorBoard(log_dir)]
-    self._model_fit(strategy, callbacks=callbacks)
+    self._model_fit(
+        strategy, callbacks=callbacks, use_dataset_creator=use_dataset_creator)
     self.assertTrue(tf.compat.v1.gfile.Exists(log_dir))
     files = tf.compat.v1.gfile.ListDirectory(log_dir)
     self.assertGreaterEqual(len(files), 1)
 
-  def testModelEvaluateWithDatasetInstance(self, strategy):
-    with self.assertRaisesRegex(
-        NotImplementedError,
-        "Only `tf.keras.utils.experimental.DatasetCreator`, `tf.Tensor`, "
-        "numpy arrays and pandas dataframes are supported types at this "
-        "time."):
-      self._model_evaluate(
-          strategy, x=tf.data.Dataset.from_tensor_slices([1, 1]))
-
-  def testModelEvaluateErrorOnBatchLevelCallbacks(self, strategy):
+  def testModelEvaluateErrorOnBatchLevelCallbacks(self, strategy,
+                                                  use_dataset_creator):
 
     class BatchLevelCallback(callbacks_lib.Callback):
 
@@ -121,7 +116,10 @@ class DatasetCreatorModelFitParameterServerStrategyOnlyTest(
     with self.assertRaisesRegex(ValueError,
                                 "Batch-level `Callback`s are not supported"):
       callbacks = [BatchLevelCallback()]
-      self._model_evaluate(strategy, callbacks=callbacks)
+      self._model_evaluate(
+          strategy,
+          callbacks=callbacks,
+          use_dataset_creator=use_dataset_creator)
 
 
 if __name__ == "__main__":
