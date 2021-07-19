ed6dae62551510787265a821f4b3d969ab4075d2
Tomer Kaftan <kaftan@google.com>
2021-07-19 12:53:31 -0700

    Expand on the variable scope shim documentation a little bit
    PiperOrigin-RevId: 385620989

--- keras/legacy_tf_layers/variable_scope_shim.py
+++ keras/legacy_tf_layers/variable_scope_shim.py
@@ -581,26 +581,136 @@ class VariableAndLossTracker(tf.Module):
 class VariableScopeModule(tf.Module):
   """Wrapper Module to capture `compat.v1.get_variable` and `compat.v1.layers`.
 
-  See go/tf2-migration-model-bookkeeping for background.
-
-  This shim layer allows using large sets of TF1 model-forward-pass code as a
+  This shim module allows using large sets of TF1 model-forward-pass code as a
   tf.Module that works in TF2 with TF2 behaviors enabled. To use it,
   override this class and put your TF1 model's forward pass inside your
-  implementation for `forward_pass`.
+  implementation for `forward_pass`. You can then call this module directly
+  to run your TF1 code with working variable reuse.
 
-  Limitations:
+  Caution: Unless `tf.compat.v1.VariableScopeLayer` does not work for your code
+  because of Keras layers' call signature semantics around the first argument,
+  and you need a lower-level solution, we recommend using
+  `tf.compat.v1.VariableScopeLayer` instead. `VariableScopeModule` can be used
+  if you cannot adopt Keras layers to your use case.
+
+  Example of capturing tf.compat.v1.layer-based modeling code as a tf.Module:
+
+  ```python
+  class WrappedDoubleDenseModule(variable_scope_shim.VariableScopeModule):
+
+    def __init__(self, units, *args, **kwargs):
+      super().__init__(*args, **kwargs)
+      self.units = units
+
+    def forward_pass(self, inputs):
+      with variable_scope.variable_scope("double_dense_layer"):
+        out = tf.compat.v1.layers.dense(
+            inputs, self.units, name="dense_one",
+            kernel_initializer=tf.compat.v1.random_normal_initializer,
+            kernel_regularizer="l2")
+        out = tf.compat.v1.layers.dense(
+            out, self.units, name="dense_two",
+            kernel_initializer=tf.compat.v1.random_normal_initializer(),
+            kernel_regularizer="l2")
+      return out
+
+  # Create a layer that can be used as a callable Module
+  layer = WrappedDoubleDenseModule(10)
+
+  # call the layer on inputs
+  layer(...)
+
+  # Variables created/used within `forward_pass` will be tracked
+  layer.variables
+  layer.trainable_variables
+
+  # Regularization losses created by `tf.compat.v1.get_variable` or by
+  # `tf.compat.v1.layers` will be tracked as well. Note: all other
+  # regularization losses must be tracked manually if using modules. If you
+  # would like fully-automated regularization loss tracking, use
+  # `tf.compat.v1.VariableScopeLayer` instead.
+  reg_losses = layer.get_compat_v1_regularization_losses()
+  ```
+
+  Example of capturing tf.compat.v1.get_variable-based modeling code as
+  a tf.Module:
+
+  ```python
+  class WrappedDoubleDenseModule(variable_scope_shim.VariableScopeModule):
+
+    def __init__(self, units, *args, **kwargs):
+      super().__init__(*args, **kwargs)
+      self.units = units
+
+    def forward_pass(self, inputs):
+      out = inputs
+      with variable_scope.variable_scope("double_dense_layer"):
+        with tf.compat.v1.variable_scope("dense_one"):
+          # The weights are created with a `regularizer`,
+          # so the layer should track their regularization losses
+          kernel = tf.compat.v1.get_variable(
+              shape=[out.shape[-1], self.units],
+              regularizer=regularizers.L2(),
+              initializer=init_ops.ones_initializer(),
+              name="kernel")
+          bias = tf.compat.v1.get_variable(
+              shape=[self.units,],
+              initializer=init_ops.zeros_initializer(),
+              name="bias")
+          out = tf.compat.v1.math.matmul(out, kernel)
+          out = tf.compat.v1.nn.bias_add(out, bias)
+        with tf.compat.v1.variable_scope("dense_two"):
+          kernel = tf.compat.v1.get_variable(
+              shape=[out.shape[-1], self.units],
+              regularizer=regularizers.L2(),
+              initializer=init_ops.ones_initializer(),
+              name="kernel")
+          bias = tf.compat.v1.get_variable(
+              shape=[self.units,],
+              initializer=init_ops.zeros_initializer(),
+              name="bias")
+          out = tf.compat.v1.math.matmul(out, kernel)
+          out = tf.compat.v1.nn.bias_add(out, bias)
+      return out
+
+  # Create a module that can be used as a callable Module
+  layer = WrappedDoubleDenseModule(10)
+
+  # call the module on inputs
+  layer(...)
+
+  # Variables created/used within the scope will be tracked by the module
+  layer.variables
+  layer.trainable_variables
+
+  # Regularization losses created by `tf.compat.v1.get_variable` or by
+  # `tf.compat.v1.layers` will be tracked as well. Note: all other
+  # regularization losses must be tracked manually if using modules. If you
+  # would like fully-automated regularization loss tracking, use
+  # `tf.compat.v1.VariableScopeLayer` instead.
+  reg_losses = layer.get_compat_v1_regularization_losses()
+  ```
+
+  Caveats:
     * TF2 will not prune unused variable updates (or unused outputs). You may
       need to adjust your forward pass code to avoid computations or variable
       updates that you don't intend to use. (E.g. by adding a flag to the
       `forward_pass` call signature and branching on it).
-    * Avoid Nesting variable creation in `tf.function` inside of `forward_pass`
+    * Avoid Nesting variable creation in tf.function inside of `forward_pass`
       While the layer may safely be used from inside a `tf.function`, using
       a function inside of `forward_pass` will break the variable scoping.
-    * TBD: Nesting keras layers/models or other `VariableScopeModule`s
-      directly in `forward_pass` may not work correctly just yet.
-      Support for this/instructions for how to do this is still being worked on.
-
-  Coming soon: A better guide, testing/verification guide.
+    * If you would like to nest modules or other
+      `VariableScopeModules`s directly in `forward_pass`, you need to
+      assign them as properties of your VariableScopeModule so that
+      object-oriented weights will activate in for them.
+      See the intro to modules, layers, and models
+      [guide](https://www.tensorflow.org/guide/intro_to_modules) for more info
+      on this explicit recursive weight tracking.
+      Only weights made by `tf.compat.v1.get_variable` calls and
+      `tf.compat.v1.layers` will get implicitly tracked by your
+      VariableScopeModule without being assigned as properties. Note: if these
+      modules have their own regularization losses you must track them
+      manually.
   """
 
   def __init__(self, name=None):
@@ -608,6 +718,7 @@ class VariableScopeModule(tf.Module):
     self._var_tracker = VariableAndLossTracker()
 
   def forward_pass(self, *args, **kwargs):
+    """Implement this method. It should include your model forward pass."""
     raise NotImplementedError
 
   def __call__(self, *args, **kwargs):
@@ -616,18 +727,34 @@ class VariableScopeModule(tf.Module):
         return self.forward_pass(*args, **kwargs)
 
   def get_compat_v1_regularization_losses(self):
+    """Dict w/ regularization losses from `get_variable` and `compat.v1.layers`.
+
+    VariableScopeModule is able to track regularization losses from
+    `tf.compat.v1.get_variable` calls and `compat.v1.layers` that are used
+    within the `forward_pass`. All other regularization losses (such as
+    those belonging to Keras layers or nested VariableScopeModules)
+    must be tracked via your own manual mechanism as is standard for tf.Modules.
+    If you would like more comprehensive regularization loss tracking, use
+    `VariableScopeModule` instead.
+
+    Returns:
+      A dict of the form { variable_name : regularization_loss } for any
+      regularization losses specified for variables created and used by
+      `tf.compat.v1.get_variable` (with the regularizer argument passed)
+      or `tf.compat.v1.layers` within `forward_pass`.
+    """
     return self._var_tracker.get_regularization_losses()
 
 
 class VariableScopeLayer(base_layer.Layer):
   """Wrapper Layer to capture `compat.v1.get_variable` and `compat.v1.layers`.
 
-  See go/tf2-migration-model-bookkeeping for background.
-
   This shim layer allows using large sets of TF1 model-forward-pass code as a
-  Keras layer that works in TF2 with TF2 behaviors enabled. To use it,
+  Keras layer that works in TF2 with TF2 behaviors enabled. It will capture
+  both weights and regularization losses of your forward-pass code. To use it,
   override this class and put your TF1 model's forward pass inside your
-  implementation for `forward_pass`.
+  implementation for `forward_pass`. (Unlike standard custom Keras layers,
+  do not override `call`.)
 
   Below are some examples, and then more details on the functionality of this
   shim layer to wrap TF1 model forward passes.
@@ -641,15 +768,15 @@ class VariableScopeLayer(base_layer.Layer):
       super().__init__(*args, **kwargs)
       self.units = units
 
-    def forward_pass(self, inputs, training=None):
-      out = tf.compat.v1.layers.dense(
-          inputs, self.units, name="dense_one",
-          kernel_initializer=init_ops.ones_initializer(),
-          kernel_regularizer="l2")
-      with variable_scope.variable_scope("nested_scope"):
+    def forward_pass(self, inputs):
+      with variable_scope.variable_scope("double_dense_layer"):
+        out = tf.compat.v1.layers.dense(
+            inputs, self.units, name="dense_one",
+            kernel_initializer=tf.compat.v1.random_normal_initializer,
+            kernel_regularizer="l2")
         out = tf.compat.v1.layers.dense(
             out, self.units, name="dense_two",
-            kernel_initializer=init_ops.ones_initializer(),
+            kernel_initializer=tf.compat.v1.random_normal_initializer(),
             kernel_regularizer="l2")
       return out
 
@@ -668,8 +795,8 @@ class VariableScopeLayer(base_layer.Layer):
   reg_losses = layer.losses
   ```
 
-  The solution is to wrap the model construction and execution in a keras-style
-  scope:
+  Example of capturing tf.compat.v1.get_variable-based modeling code as
+  a Keras layer:
 
   ```python
   class WrappedDoubleDenseLayer(variable_scope_shim.VariableScopeLayer):
@@ -678,23 +805,23 @@ class VariableScopeLayer(base_layer.Layer):
       super().__init__(*args, **kwargs)
       self.units = units
 
-    def forward_pass(self, inputs, training=None):
+    def forward_pass(self, inputs):
       out = inputs
-      with tf.compat.v1.variable_scope("dense_one"):
-        # The weights are created with a `regularizer`,
-        # so the layer should track their regularization losses
-        kernel = tf.compat.v1.get_variable(
-            shape=[out.shape[-1], self.units],
-            regularizer=regularizers.L2(),
-            initializer=init_ops.ones_initializer(),
-            name="kernel")
-        bias = tf.compat.v1.get_variable(
-            shape=[self.units,],
-            initializer=init_ops.zeros_initializer(),
-            name="bias")
-        out = tf.compat.v1.math.matmul(out, kernel)
-        out = tf.compat.v1.nn.bias_add(out, bias)
-      with tf.compat.v1.variable_scope("nested_scope"):
+      with tf.compat.v1.variable_scope("double_dense_layer"):
+        with tf.compat.v1.variable_scope("dense_one"):
+          # The weights are created with a `regularizer`,
+          # so the layer should track their regularization losses
+          kernel = tf.compat.v1.get_variable(
+              shape=[out.shape[-1], self.units],
+              regularizer=regularizers.L2(),
+              initializer=init_ops.ones_initializer(),
+              name="kernel")
+          bias = tf.compat.v1.get_variable(
+              shape=[self.units,],
+              initializer=init_ops.zeros_initializer(),
+              name="bias")
+          out = tf.compat.v1.math.matmul(out, kernel)
+          out = tf.compat.v1.nn.bias_add(out, bias)
         with tf.compat.v1.variable_scope("dense_two"):
           kernel = tf.compat.v1.get_variable(
               shape=[out.shape[-1], self.units],
@@ -729,15 +856,19 @@ class VariableScopeLayer(base_layer.Layer):
     creations will get captured by this wrapper layer. Regularization losses
     are accessible in `layer.losses` after a call just like in a standard
     Keras layer, and will be captured by any model that includes this layer.
+    Regularization losses attached to Keras layers/models set as attributes
+    of your layer will also get captured in the standard Keras regularization
+    loss tracking.
 
   Variable scope / variable reuse:
     variable-scope based reuse in the `forward_pass` will be respected,
     and work like variable-scope based reuse in TF1.
 
   Variable Names/Pre-trained checkpoint loading:
-    variable naming from get_variable and `compat.v1.layer` layers will match
+    Variable naming from get_variable and `compat.v1.layer` layers will match
     the TF1 names, so you should be able to re-use your old name-based
-    checkpoints.
+    checkpoints. Variable naming for Keras layers/models or for variables
+    created by `tf.Variable` may change when going to eager execution.
 
   Training Arg in `forward_pass`:
     Keras will pass a `training` arg to this layer if `forward_pass` contains
@@ -756,19 +887,20 @@ class VariableScopeLayer(base_layer.Layer):
     to pass in a `training` arg., and the semantics applied to
     the first positional arg in the call signature.
 
-  Limitations:
+  Caveats:
     * TF2 will not prune unused variable updates (or unused outputs). You may
       need to adjust your forward pass code to avoid computations or variable
       updates that you don't intend to use. (E.g. by adding a flag to the
       `forward_pass` call signature and branching on it).
     * Avoid Nesting variable creation in tf.function inside of `forward_pass`
-      While the layer may safetely be used from inside a `tf.function`, using
+      While the layer may safely be used from inside a `tf.function`, using
       a function inside of `forward_pass` will break the variable scoping.
-    * TBD: Nesting keras layers/models or other `VariableScopeLayer`s
-      directly in `forward_pass` may not work correctly just yet.
-      Support for this/instructions for how to do this is sill being worked on.
-
-  Coming soon: A better guide, testing/verification guide.
+    * If you would like to nest Keras layers/models or other
+      `VariableScopeLayer`s directly in `forward_pass`, you need to
+      assign them as attributes of your layer so that Keras's standard
+      object-oriented weights and loss tracking will kick in.
+      See the intro to modules, layers, and models
+      [guide](https://www.tensorflow.org/guide/intro_to_modules) for more info
   """
 
   def __init__(self, **kwargs):
@@ -785,6 +917,7 @@ class VariableScopeLayer(base_layer.Layer):
     return tf_inspect.getfullargspec(self.forward_pass)
 
   def forward_pass(self, *args, **kwargs):
+    """Implement this method. It should include your model forward pass."""
     raise NotImplementedError
 
   def call(self, *args, **kwargs):
