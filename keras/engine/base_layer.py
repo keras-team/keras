d47f9e525370ef1a28292fc12f8a61218ef2a48f
Haifeng Jin <haifengj@google.com>
2021-07-26 20:53:44 -0700

    check the trainable type for tf.keras.layers.Layer.__init__ to be boolean.
    PiperOrigin-RevId: 387021364

--- keras/engine/base_layer.py
+++ keras/engine/base_layer.py
@@ -330,6 +330,11 @@ class Layer(tf.Module, version_utils.LayerVersionSelector):
     # Mutable properties
     # Indicates whether the layer's weights are updated during training
     # and whether the layer's updates are run during training.
+    if not (isinstance(trainable, bool) or
+            (isinstance(trainable, (tf.Tensor, tf.Variable)) and
+             trainable.dtype is tf.bool)):
+      raise TypeError(
+          f'Expected trainable argument to be a boolean, but got: {trainable}')
     self._trainable = trainable
     # A stateful layer is a layer whose updates are run during inference too,
     # for instance stateful RNNs.
