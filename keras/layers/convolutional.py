714475891857fb966bb13e2feaa26f82784361cb
rubiales <arubiales11@gmail.com>
2021-08-12 20:13:37 +0200

    fix message error to the keras standards #14970

--- keras/layers/convolutional.py
+++ keras/layers/convolutional.py
@@ -3410,11 +3410,9 @@ class Cropping1D(Layer):
 
   def call(self, inputs):
     if tf.not_equal(tf.size(inputs), 0) and sum(self.cropping) >= inputs.shape[1]:
-      raise ValueError(
-        'cropping parameter of Cropping layer is too high,' +
-        'the result of crop' + str(inputs.shape) + ' with cropping ' +
-        str(self.cropping) + ' is an empty tensor'
-      )
+      raise ValueError('cropping parameter of Cropping layer must be '
+                       'greater than the input shape. Recieved: inputs.shape='
+                       f'{inputs.shape}, and cropping={self.cropping}')
     if self.cropping[1] == 0:
       return inputs[:, self.cropping[0]:, :]
     else:
