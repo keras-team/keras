facdbc4574999fa301d5cd3717f5810f10dd9719
Harish S.G <harishsg99@gmail.com>
2021-08-23 12:44:24 +0530

    Added Function to print memory usage of model!!

--- keras/utils/layer_utils.py
+++ keras/utils/layer_utils.py
@@ -173,7 +173,51 @@ def print_summary(model, line_length=None, positions=None, print_fn=None):
     relevant_nodes = []
     for v in model._nodes_by_depth.values():
       relevant_nodes += v
+  
+  def get_model_memory_usage(model,batch_size):
+    """Prints memory usage of a model.
+     model: Keras model instance.
+     batch_size : batch size
+    """
+    import numpy as np
+    try:
+        from keras import backend as K
+    except:
+        from tensorflow.keras import backend as K
 
+    shapes_mem_count = 0
+    internal_model_mem_count = 0
+    for l in model.layers:
+        layer_type = l.__class__.__name__
+        if layer_type == 'Model':
+            internal_model_mem_count += get_model_memory_usage(batch_size, l)
+        single_layer_mem = 1
+        out_shape = l.output_shape
+        if type(out_shape) is list:
+            out_shape = out_shape[0]
+        for s in out_shape:
+            if s is None:
+                continue
+            single_layer_mem *= s
+        
+        single_layer_mem_MB = single_layer_mem/(1024**2)  
+        print("Memory for", out_shape, " layer in MB is:", single_layer_mem_MB)
+
+        shapes_mem_count += single_layer_mem
+
+    trainable_count = np.sum([K.count_params(p) for p in model.trainable_weights])
+    non_trainable_count = np.sum([K.count_params(p) for p in model.non_trainable_weights])
+
+    number_size = 4.0
+    if K.floatx() == 'float16':
+        number_size = 2.0
+    if K.floatx() == 'float64':
+        number_size = 8.0
+
+    total_memory = number_size * (batch_size * shapes_mem_count + trainable_count + non_trainable_count)
+    gbytes = np.round(total_memory / (1024.0 ** 3), 3) + internal_model_mem_count
+    return gbytes
+  
   def print_row(fields, positions):
     left_to_print = [str(x) for x in fields]
     while any(left_to_print):
