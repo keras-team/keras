90c2c0ff5b1223e5d9e495bb8f92945d44cf38b2
George Karpenkov <cheshire@google.com>
2021-08-30 15:14:31 -0700

    Implement compiled MWMS with XLA on GPU.
    Required a new runner with _noshare combination, and a test in Keras for full
    coverage.
    
    PiperOrigin-RevId: 393864055

--- keras/distribute/ctl_correctness_test.py
+++ keras/distribute/ctl_correctness_test.py
@@ -245,7 +245,10 @@ class TestDistributionStrategyDnnCorrectness(tf.test.TestCase,
       tf.__internal__.test.combinations.combine(
           distribution=[
               tf.__internal__.distribute.combinations.one_device_strategy_gpu,
-              tf.__internal__.distribute.combinations.mirrored_strategy_with_two_gpus,
+              tf.__internal__.distribute.combinations
+              .mirrored_strategy_with_two_gpus,
+              tf.__internal__.distribute.combinations
+              .multi_worker_mirrored_2x1_gpu_noshare,
           ],
           optimizer_fn=[
               optimizer_combinations.gradient_descent_optimizer_keras_v2_fn,
