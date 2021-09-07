f31256bca86bf551d9beeb4bd899234cc2f725d4
rubiales <arubiales11@gmail.com>
2021-09-07 20:33:36 +0200

    Fix cropping2D empty list #15325

--- keras/layers/convolutional.py
+++ keras/layers/convolutional.py
@@ -3522,6 +3522,12 @@ class Cropping2D(Layer):
   def call(self, inputs):
     # pylint: disable=invalid-unary-operand-type
     if self.data_format == 'channels_first':
+      if (inputs.shape[2] is not None and inputs.shape[3] is not None and
+        (sum(self.cropping[0]) >= inputs.shape[2] or
+          sum(self.cropping[1]) >= inputs.shape[3])):
+          raise ValueError('cropping parameter of Cropping layer must be '
+                        'greater than the input shape. Received: inputs.shape='
+                        f'{inputs.shape}, and cropping={self.cropping}')  
       if self.cropping[0][1] == self.cropping[1][1] == 0:
         return inputs[:, :, self.cropping[0][0]:, self.cropping[1][0]:]
       elif self.cropping[0][1] == 0:
@@ -3533,6 +3539,12 @@ class Cropping2D(Layer):
       return inputs[:, :, self.cropping[0][0]:-self.cropping[0][1],
                     self.cropping[1][0]:-self.cropping[1][1]]
     else:
+      if (inputs.shape[1] is not None and inputs.shape[2] is not None and
+        (sum(self.cropping[0]) >= inputs.shape[1] or
+          sum(self.cropping[1]) >= inputs.shape[2])):
+          raise ValueError('cropping parameter of Cropping layer must be '
+                        'greater than the input shape. Received: inputs.shape='
+                        f'{inputs.shape}, and cropping={self.cropping}')   
       if self.cropping[0][1] == self.cropping[1][1] == 0:
         return inputs[:, self.cropping[0][0]:, self.cropping[1][0]:, :]
       elif self.cropping[0][1] == 0:
