80bf4e0a1be063ea43ea410e8cbd6d3d77d51f34
rubiales <arubiales11@gmail.com>
2021-09-10 12:03:02 +0200

    fix if condition

--- keras/layers/convolutional.py
+++ keras/layers/convolutional.py
@@ -3522,9 +3522,8 @@ class Cropping2D(Layer):
   def call(self, inputs):
     # pylint: disable=invalid-unary-operand-type
     if self.data_format == 'channels_first':
-      if (inputs.shape[2] is not None and inputs.shape[3] is not None and
-        (sum(self.cropping[0]) >= inputs.shape[2] or
-          sum(self.cropping[1]) >= inputs.shape[3])):
+      if ((inputs.shape[2] is not None and sum(self.cropping[0]) >= inputs.shape[2]) or
+          (inputs.shape[3] is not None and sum(self.cropping[1]) >= inputs.shape[3])):
           raise ValueError('cropping parameter of Cropping layer must be '
                         'greater than the input shape. Received: inputs.shape='
                         f'{inputs.shape}, and cropping={self.cropping}')  
@@ -3539,9 +3538,8 @@ class Cropping2D(Layer):
       return inputs[:, :, self.cropping[0][0]:-self.cropping[0][1],
                     self.cropping[1][0]:-self.cropping[1][1]]
     else:
-      if (inputs.shape[1] is not None and inputs.shape[2] is not None and
-        (sum(self.cropping[0]) >= inputs.shape[1] or
-          sum(self.cropping[1]) >= inputs.shape[2])):
+      if ((inputs.shape[1] is not None and sum(self.cropping[0]) >= inputs.shape[1]) or
+          (inputs.shape[2] is not None and sum(self.cropping[1]) >= inputs.shape[2])):
           raise ValueError('cropping parameter of Cropping layer must be '
                         'greater than the input shape. Received: inputs.shape='
                         f'{inputs.shape}, and cropping={self.cropping}')   
