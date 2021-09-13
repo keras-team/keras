9bc0bc308b86bcd3ad8da1154f1c4807d9288ae6
rubiales <arubiales11@gmail.com>
2021-09-13 11:07:44 +0200

    fix indent and improve error message

--- keras/layers/convolutional.py
+++ keras/layers/convolutional.py
@@ -3516,9 +3516,9 @@ class Cropping2D(Layer):
     if self.data_format == 'channels_first':
       if ((inputs.shape[2] is not None and sum(self.cropping[0]) >= inputs.shape[2]) or
           (inputs.shape[3] is not None and sum(self.cropping[1]) >= inputs.shape[3])):
-          raise ValueError('cropping parameter of Cropping layer must be '
-                        'greater than the input shape. Received: inputs.shape='
-                        f'{inputs.shape}, and cropping={self.cropping}')  
+          raise ValueError('Argument `cropping` must be '
+                           'greater than the input shape. Received: inputs.shape='
+                           f'{inputs.shape}, and cropping={self.cropping}')  
       if self.cropping[0][1] == self.cropping[1][1] == 0:
         return inputs[:, :, self.cropping[0][0]:, self.cropping[1][0]:]
       elif self.cropping[0][1] == 0:
@@ -3532,9 +3532,9 @@ class Cropping2D(Layer):
     else:
       if ((inputs.shape[1] is not None and sum(self.cropping[0]) >= inputs.shape[1]) or
           (inputs.shape[2] is not None and sum(self.cropping[1]) >= inputs.shape[2])):
-          raise ValueError('cropping parameter of Cropping layer must be '
-                        'greater than the input shape. Received: inputs.shape='
-                        f'{inputs.shape}, and cropping={self.cropping}')   
+          raise ValueError('Argument `cropping` must be '
+                           'greater than the input shape. Received: inputs.shape='
+                           f'{inputs.shape}, and cropping={self.cropping}')   
       if self.cropping[0][1] == self.cropping[1][1] == 0:
         return inputs[:, self.cropping[0][0]:, self.cropping[1][0]:, :]
       elif self.cropping[0][1] == 0:
