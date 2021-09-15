7c10f1dc2dd93d281efce05c11bc21423be2f30f
Aditya Kane <adityakane1@gmail.com>
2021-09-15 10:44:49 +0530

    Committing all changes

--- keras/layers/convolutional.py
+++ keras/layers/convolutional.py
@@ -284,15 +284,24 @@ class Conv(Layer):
   def compute_output_shape(self, input_shape):
     input_shape = tf.TensorShape(input_shape).as_list()
     batch_rank = len(input_shape) - self.rank - 1
-    if self.data_format == 'channels_last':
-      return tf.TensorShape(
-          input_shape[:batch_rank]
-          + self._spatial_output_shape(input_shape[batch_rank:-1])
-          + [self.filters])
-    else:
-      return tf.TensorShape(
-          input_shape[:batch_rank] + [self.filters] +
-          self._spatial_output_shape(input_shape[batch_rank + 1:]))
+    try:
+      if self.data_format == 'channels_last':
+        return tf.TensorShape(
+            input_shape[:batch_rank]
+            + self._spatial_output_shape(input_shape[batch_rank:-1])
+            + [self.filters])
+      else:
+        return tf.TensorShape(
+            input_shape[:batch_rank] + [self.filters] +
+            self._spatial_output_shape(input_shape[batch_rank + 1:]))
+
+    except ValueError:
+        raise ValueError(f'One of the dimensions in the output is <= 0 '
+                         f'due to downsampling in {self.name}. Consider '
+                         f'increasing the input size. '
+                         f'Received input shape {input_shape} which would produce '
+                         f'output shape with a zero or negative value in a '
+                         f'dimension.')
 
   def _recreate_conv_op(self, inputs):  # pylint: disable=unused-argument
     return False
