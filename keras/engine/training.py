4cebcff8213033c9e8cd5e5e748077c34c98c2dd
Vishnuvardhan Janapati <46058173+jvishnuvardhan@users.noreply.github.com>
2021-09-22 12:46:44 -0700

    Update training.py
    Updated the PR based on @fchollet suggestion.

--- keras/engine/training.py
+++ keras/engine/training.py
@@ -1631,9 +1631,9 @@ class Model(base_layer.Layer, version_utils.ModelVersionSelector):
     inference. Also, note the fact that test loss is not affected by
     regularization layers like noise and dropout.
     
-    Note: `model.predict(x)` returns a numpy array whereas `model(x)` returns a
-    `tf.Tensor`. However, numpy array can be accessed through
-    `model(x).numpy()`. 
+    Note: Check [FAQs](https://keras.io/getting_started/faq/#whats-the-difference-between-model-methods-predict-and-call) 
+    for more details about the differences between `Model` methods `predict()`
+    and `__call__()`. 
 
     Args:
         x: Input samples. It could be:
