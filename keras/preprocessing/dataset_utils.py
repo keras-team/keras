3c4f0e24cda970c70f80bcbc380c82b7749aee33
Francois Chollet <fchollet@google.com>
2021-10-07 12:01:43 -0700

    Make sure to parallelize map() operations in built-in dataset creation utilities.
    PiperOrigin-RevId: 401570546

--- keras/preprocessing/dataset_utils.py
+++ keras/preprocessing/dataset_utils.py
@@ -204,13 +204,18 @@ def labels_to_dataset(labels, label_mode, num_classes):
     - 'categorical' means that the labels are mapped into a categorical vector.
       (e.g. for `categorical_crossentropy` loss).
     num_classes: number of classes of labels.
+
+  Returns:
+    A `Dataset` instance.
   """
   label_ds = tf.data.Dataset.from_tensor_slices(labels)
   if label_mode == 'binary':
     label_ds = label_ds.map(
-        lambda x: tf.expand_dims(tf.cast(x, 'float32'), axis=-1))
+        lambda x: tf.expand_dims(tf.cast(x, 'float32'), axis=-1),
+        num_parallel_calls=tf.data.AUTOTUNE)
   elif label_mode == 'categorical':
-    label_ds = label_ds.map(lambda x: tf.one_hot(x, num_classes))
+    label_ds = label_ds.map(lambda x: tf.one_hot(x, num_classes),
+                            num_parallel_calls=tf.data.AUTOTUNE)
   return label_ds
 
 
@@ -218,12 +223,12 @@ def check_validation_split_arg(validation_split, subset, shuffle, seed):
   """Raise errors in case of invalid argument values.
 
   Args:
-    shuffle: Whether to shuffle the data. Either True or False.
-    seed: random seed for shuffling and transformations.
     validation_split: float between 0 and 1, fraction of data to reserve for
       validation.
     subset: One of "training" or "validation". Only used if `validation_split`
       is set.
+    shuffle: Whether to shuffle the data. Either True or False.
+    seed: random seed for shuffling and transformations.
   """
   if validation_split and not 0 < validation_split < 1:
     raise ValueError(
