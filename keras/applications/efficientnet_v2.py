734b3cbf965d010faef64e8b168ef8bc43d430e3
Sebastian <mocart15@gmail.com>
2021-10-14 11:10:20 +0200

    Fixed more indents.

--- keras/applications/efficientnet_v2.py
+++ keras/applications/efficientnet_v2.py
@@ -777,50 +777,50 @@ def FusedMBConvBlock(
       se = layers.Reshape(se_shape, name=name + "se_reshape")(se)
 
       se = layers.Conv2D(
-          filters_se,
-          1,
-          padding="same",
-          activation=activation,
-          kernel_initializer=CONV_KERNEL_INITIALIZER,
-          name=name + "se_reduce",
+        filters_se,
+        1,
+        padding="same",
+        activation=activation,
+        kernel_initializer=CONV_KERNEL_INITIALIZER,
+        name=name + "se_reduce",
       )(se)
       se = layers.Conv2D(
-          filters,
-          1,
-          padding="same",
-          activation="sigmoid",
-          kernel_initializer=CONV_KERNEL_INITIALIZER,
-          name=name + "se_expand",
+        filters,
+        1,
+        padding="same",
+        activation="sigmoid",
+        kernel_initializer=CONV_KERNEL_INITIALIZER,
+        name=name + "se_expand",
       )(se)
 
       x = layers.multiply([x, se], name=name + "se_excite")
 
     # Output phase:
     x = layers.Conv2D(
-        output_filters,
-        kernel_size=1 if expand_ratio != 1 else kernel_size,
-        strides=1 if expand_ratio != 1 else strides,
-        kernel_initializer=CONV_KERNEL_INITIALIZER,
-        padding="same",
-        use_bias=False,
-        name=name + "project_conv",
+      output_filters,
+      kernel_size=1 if expand_ratio != 1 else kernel_size,
+      strides=1 if expand_ratio != 1 else strides,
+      kernel_initializer=CONV_KERNEL_INITIALIZER,
+      padding="same",
+      use_bias=False,
+      name=name + "project_conv",
     )(x)
     x = layers.BatchNormalization(
-        axis=bn_axis, momentum=bn_momentum, name=name + "project_bn"
+      axis=bn_axis, momentum=bn_momentum, name=name + "project_bn"
     )(x)
     if expand_ratio == 1:
       x = layers.Activation(
-          activation=activation, name=name + "project_activation"
+        activation=activation, name=name + "project_activation"
       )(x)
 
     # Residual:
     if strides == 1 and input_filters == output_filters:
       if survival_probability:
         x = layers.Dropout(
-              survival_probability,
-              noise_shape=(None, 1, 1, 1),
-              name=name + "drop"
-            )(x)
+          survival_probability,
+          noise_shape=(None, 1, 1, 1),
+          name=name + "drop"
+        )(x)
       x = layers.add([x, inputs], name=name + "add")
     return x
   return apply
