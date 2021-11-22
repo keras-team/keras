3660e0df94d802a05002ada986cfd3afda965e61
Matt Watson <mattdangerw@google.com>
2021-11-22 09:57:08 -0800

    Call convert_to_tensor on all Discretization inputs
    PiperOrigin-RevId: 411584963

--- keras/layers/preprocessing/preprocessing_utils.py
+++ keras/layers/preprocessing/preprocessing_utils.py
@@ -14,7 +14,6 @@
 # ==============================================================================
 """Utils for preprocessing layers."""
 
-from keras import backend
 from keras.utils import tf_utils
 import numpy as np
 import tensorflow.compat.v2 as tf
@@ -38,7 +37,7 @@ def ensure_tensor(inputs, dtype=None):
 def listify_tensors(x):
   """Convert any tensors or numpy arrays to lists for config serialization."""
   if tf.is_tensor(x):
-    x = backend.get_value(x)
+    x = x.numpy()
   if isinstance(x, np.ndarray):
     x = x.tolist()
   return x
