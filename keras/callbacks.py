69a439d3794fb5cdd0bd1ceb2bee9b45c5d629ba
Surya Prakash Mishra <mishrasp393@gmail.com>
2021-12-02 11:16:21 +0530

    fix: add space before = sign

--- keras/callbacks.py
+++ keras/callbacks.py
@@ -1282,7 +1282,7 @@ class ModelCheckpoint(Callback):
     self.epochs_since_last_save = 0
     self._batches_seen_since_last_saving = 0
     self._last_batch_seen = 0
-    self.best= initial_value_threshold
+    self.best = initial_value_threshold
 
     if save_weights_only:
       if options is None or isinstance(
