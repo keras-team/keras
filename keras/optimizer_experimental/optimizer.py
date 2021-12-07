b057eff257dd49d8d6f931cbf72600ca2df06e2a
Scott Zhu <scottzhu@google.com>
2021-12-07 10:22:33 -0800

    Extract optimizer iteration variable logic for subclass to override.
    PiperOrigin-RevId: 414763454

--- keras/optimizer_experimental/optimizer.py
+++ keras/optimizer_experimental/optimizer.py
@@ -62,11 +62,14 @@ class _BaseOptimizer(tf.Module):
     self._ema_option = ema_option
     self._jit_compile = jit_compile
 
+    self._create_iteration_variable()
+    self._process_kwargs(kwargs)
+
+  def _create_iteration_variable(self):
+    """Create the iterations counter variable."""
     with tf.init_scope():
       # Lift the variable creation to init scope to avoid environment issue.
       self._iterations = tf.Variable(0, name="iteration", dtype=tf.int64)
-
-    self._process_kwargs(kwargs)
 
   def _process_kwargs(self, kwargs):
     legacy_gradients_clip_kwargs = {"clipnorm", "clipvalue", "global_clipnorm"}
