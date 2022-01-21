e6db323c9abb54d43d722c8b7635a4f5545605f4
Fabien Hertschuh <fhertschuh@google.com>
2022-01-20 21:35:57 -0800

    Refactor `pooling.py` into smaller logically organized files hosted under a `pooling` directory.
    PiperOrigin-RevId: 423231364

---
+++ keras/layers/pooling/base_global_pooling1d.py
@@ -0,0 +1,53 @@
+# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Private base class for global pooling 1D layers."""
+# pylint: disable=g-classes-have-attributes
+
+from keras.engine.base_layer import Layer
+from keras.engine.input_spec import InputSpec
+from keras.utils import conv_utils
+import tensorflow.compat.v2 as tf
+
+
+class GlobalPooling1D(Layer):
+  """Abstract class for different global pooling 1D layers."""
+
+  def __init__(self, data_format='channels_last', keepdims=False, **kwargs):
+    super(GlobalPooling1D, self).__init__(**kwargs)
+    self.input_spec = InputSpec(ndim=3)
+    self.data_format = conv_utils.normalize_data_format(data_format)
+    self.keepdims = keepdims
+
+  def compute_output_shape(self, input_shape):
+    input_shape = tf.TensorShape(input_shape).as_list()
+    if self.data_format == 'channels_first':
+      if self.keepdims:
+        return tf.TensorShape([input_shape[0], input_shape[1], 1])
+      else:
+        return tf.TensorShape([input_shape[0], input_shape[1]])
+    else:
+      if self.keepdims:
+        return tf.TensorShape([input_shape[0], 1, input_shape[2]])
+      else:
+        return tf.TensorShape([input_shape[0], input_shape[2]])
+
+  def call(self, inputs):
+    raise NotImplementedError
+
+  def get_config(self):
+    config = {'data_format': self.data_format, 'keepdims': self.keepdims}
+    base_config = super(GlobalPooling1D, self).get_config()
+    return dict(list(base_config.items()) + list(config.items()))
+
---
+++ keras/layers/pooling/global_max_pooling_test.py
@@ -0,0 +1,111 @@
+# Copyright 2016 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Tests for global max pooling layers."""
+
+from absl.testing import parameterized
+import keras
+from keras import combinations
+from keras import testing_utils
+import tensorflow.compat.v2 as tf
+
+
+@combinations.generate(combinations.combine(mode=['graph', 'eager']))
+class GlobalMaxPoolingTest(tf.test.TestCase, parameterized.TestCase):
+
+  def test_global_max_pooling_1d(self):
+    testing_utils.layer_test(
+        keras.layers.GlobalMaxPooling1D, input_shape=(3, 4, 5))
+    testing_utils.layer_test(
+        keras.layers.GlobalMaxPooling1D,
+        kwargs={'data_format': 'channels_first'},
+        input_shape=(3, 4, 5))
+
+  def test_global_max_pooling_2d_with_ragged(self):
+    ragged_data = tf.ragged.constant(
+        [[[[1.0], [1.0]], [[2.0], [2.0]], [[3.0], [3.0]]],
+         [[[1.0], [1.0]], [[2.0], [2.0]]]],
+        ragged_rank=1)
+    dense_data = ragged_data.to_tensor()
+
+    inputs = keras.Input(shape=(None, 2, 1), dtype='float32', ragged=True)
+    out = keras.layers.GlobalMaxPooling2D()(inputs)
+    model = keras.models.Model(inputs=inputs, outputs=out)
+    output_ragged = model.predict(ragged_data, steps=1)
+
+    inputs = keras.Input(shape=(None, 2, 1), dtype='float32')
+    out = keras.layers.GlobalMaxPooling2D()(inputs)
+    model = keras.models.Model(inputs=inputs, outputs=out)
+    output_dense = model.predict(dense_data, steps=1)
+
+    self.assertAllEqual(output_ragged, output_dense)
+
+  def test_global_max_pooling_2d(self):
+    testing_utils.layer_test(
+        keras.layers.GlobalMaxPooling2D,
+        kwargs={'data_format': 'channels_first'},
+        input_shape=(3, 4, 5, 6))
+    testing_utils.layer_test(
+        keras.layers.GlobalMaxPooling2D,
+        kwargs={'data_format': 'channels_last'},
+        input_shape=(3, 5, 6, 4))
+
+  def test_global_maxpooling_3d(self):
+    testing_utils.layer_test(
+        keras.layers.GlobalMaxPooling3D,
+        kwargs={'data_format': 'channels_first'},
+        input_shape=(3, 4, 3, 4, 3))
+    testing_utils.layer_test(
+        keras.layers.GlobalMaxPooling3D,
+        kwargs={'data_format': 'channels_last'},
+        input_shape=(3, 4, 3, 4, 3))
+
+  def test_global_max_pooling_1d_keepdims(self):
+    testing_utils.layer_test(
+        keras.layers.GlobalMaxPooling1D,
+        kwargs={'keepdims': True},
+        input_shape=(3, 4, 5),
+        expected_output_shape=(None, 1, 5))
+    testing_utils.layer_test(
+        keras.layers.GlobalMaxPooling1D,
+        kwargs={'data_format': 'channels_first', 'keepdims': True},
+        input_shape=(3, 4, 5),
+        expected_output_shape=(None, 4, 1))
+
+  def test_global_max_pooling_2d_keepdims(self):
+    testing_utils.layer_test(
+        keras.layers.GlobalMaxPooling2D,
+        kwargs={'data_format': 'channels_first', 'keepdims': True},
+        input_shape=(3, 4, 5, 6),
+        expected_output_shape=(None, 4, 1, 1))
+    testing_utils.layer_test(
+        keras.layers.GlobalMaxPooling2D,
+        kwargs={'data_format': 'channels_last', 'keepdims': True},
+        input_shape=(3, 4, 5, 6),
+        expected_output_shape=(None, 1, 1, 6))
+
+  def test_global_max_pooling_3d_keepdims(self):
+    testing_utils.layer_test(
+        keras.layers.GlobalMaxPooling3D,
+        kwargs={'data_format': 'channels_first', 'keepdims': True},
+        input_shape=(3, 4, 3, 4, 3),
+        expected_output_shape=(None, 4, 1, 1, 1))
+    testing_utils.layer_test(
+        keras.layers.GlobalMaxPooling3D,
+        kwargs={'data_format': 'channels_last', 'keepdims': True},
+        input_shape=(3, 4, 3, 4, 3),
+        expected_output_shape=(None, 1, 1, 1, 3))
+
+if __name__ == '__main__':
+  tf.test.main()
