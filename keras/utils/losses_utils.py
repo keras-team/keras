2e0c9c39d59f80626ac0128f44dc4c0b1385562d
Jun Xu <xjun@google.com>
2022-02-07 14:54:29 -0800

    Allow Keras remove_squeezable_dimensions to accept all composite tensors.
    PiperOrigin-RevId: 427024646

--- keras/utils/losses_utils.py
+++ keras/utils/losses_utils.py
@@ -111,9 +111,10 @@ def remove_squeezable_dimensions(
     Tuple of `labels` and `predictions`, possibly with last dim squeezed.
   """
   with backend.name_scope(name or 'remove_squeezable_dimensions'):
-    if not isinstance(predictions, tf.RaggedTensor):
+    # tf.is_tensor returns True if predictions is a tensor or composite tensor.
+    if not tf.is_tensor(predictions):
       predictions = tf.convert_to_tensor(predictions)
-    if not isinstance(labels, tf.RaggedTensor):
+    if not tf.is_tensor(labels):
       labels = tf.convert_to_tensor(labels)
     predictions_shape = predictions.shape
     predictions_rank = predictions_shape.ndims
