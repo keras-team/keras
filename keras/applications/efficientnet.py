Fix the Efficientnet input normalization issue.
Fix https://github.com/tensorflow/tensorflow/issues/49930

We were incorrectly normalize the input (with wrong variance).

The validation acc after this fix with imagenet is:

b0 0.7699
b1 0.7875
b2 0.7979
b3 0.8141
b4 0.8294

PiperOrigin-RevId: 427277729

keras/applications/efficientnet.py
@@ -21,8 +21,6 @@ Reference:
       https://arxiv.org/abs/1905.11946) (ICML 2019)
 """
 
-import tensorflow.compat.v2 as tf
-
 import copy
 import math
 
@@ -32,7 +30,10 @@ from keras.engine import training
 from keras.layers import VersionAwareLayers
 from keras.utils import data_utils
 from keras.utils import layer_utils
-from tensorflow.python.util.tf_export import keras_export
+
+import tensorflow.compat.v2 as tf
+
+from tensorflow.python.util.tf_export import keras_export  # pylint: disable=g-direct-tensorflow-import
 
 
 BASE_WEIGHTS_PATH = 'https://storage.googleapis.com/keras-applications/'
@@ -204,6 +205,10 @@ BASE_DOCSTRING = """Instantiates the {name} architecture.
 """
 
 
+IMAGENET_MEAN_RGB = [0.485, 0.456, 0.406]
+IMAGENET_STDDEV_RGB = [0.229, 0.224, 0.225]
+
+
 def EfficientNet(
     width_coefficient,
     depth_coefficient,
@@ -319,7 +324,14 @@ def EfficientNet(
   # Build stem
   x = img_input
   x = layers.Rescaling(1. / 255.)(x)
-  x = layers.Normalization(axis=bn_axis)(x)
+  if weights == 'imagenet':
+    # Note that we use square value of STDDEV as the variance for the layer,
+    # since normalization is computed as: result = (input - mean) / sqrt(var)
+    x = layers.Normalization(axis=bn_axis,
+                             mean=IMAGENET_MEAN_RGB,
+                             variance=tf.math.square(IMAGENET_STDDEV_RGB))(x)
+  else:
+    x = layers.Normalization(axis=bn_axis)(x)
 
   x = layers.ZeroPadding2D(
       padding=imagenet_utils.correct_pad(x, 3),