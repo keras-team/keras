1007d5b7b23ed2383b335cf5bff4b9a7f0981572
Reed Wanderman-Milne <reedwm@google.com>
2022-02-08 19:38:24 -0800

    Remove experimental Keras mixed precision API.
    The non-experimental mixed precision API was added in TensorFlow 2.4, and since then the experimental API has been deprecated. This change removes the experimental API.
    
    Deserializing the now-removed PolicyV1 and LossScaleOptimizerV1 classes is still supported, if they were serialized with get_config() prior to this change. These classes are deserialized into the non-experimental Policy and LossScaleOptimizer classes, which has been the case since TensorFlow 2.4. Eventually, support for deserializing these classes may be removed.
    
    PiperOrigin-RevId: 427357317

--- keras/engine/training.py
+++ keras/engine/training.py
@@ -30,7 +30,6 @@ from keras.engine import compile_utils
 from keras.engine import data_adapter
 from keras.engine import training_utils
 from keras.mixed_precision import loss_scale_optimizer as lso
-from keras.mixed_precision import policy
 from keras.optimizers import optimizer_v1
 from keras.optimizers.optimizer_experimental import optimizer as optimizer_experimental
 from keras.saving import hdf5_format
@@ -623,25 +622,13 @@ class Model(base_layer.Layer, version_utils.ModelVersionSelector):
 
   def _get_optimizer(self, optimizer):
     """Wraps `optimizer` in `LossScaleOptimizer` if necessary."""
-    # The deprecated PolicyV1 has a loss_scale, which we use for backwards
-    # compatibility to match TF 2.3 behavior. The new Policy does not have a
-    # loss_scale, so we use dynamic loss scaling if the mixed_float16 policy is
-    # used.
-    if isinstance(self._dtype_policy, policy.PolicyV1):
-      loss_scale = self._dtype_policy.loss_scale
-    elif self._dtype_policy.name == 'mixed_float16':
-      loss_scale = 'dynamic'
-    else:
-      loss_scale = None
-
     def _get_single_optimizer(opt):
       opt = optimizers.get(opt)
-      if (loss_scale is not None and
+      if (self.dtype_policy.name == 'mixed_float16' and
           not isinstance(opt, lso.LossScaleOptimizer)):
-        if loss_scale == 'dynamic':
-          opt = lso.LossScaleOptimizer(opt)
-        else:
-          opt = lso.LossScaleOptimizerV1(opt, loss_scale)
+        # Loss scaling is necessary with mixed_float16 for models to converge to
+        # the same accuracy as with float32.
+        opt = lso.LossScaleOptimizer(opt)
       return opt
 
     return tf.nest.map_structure(_get_single_optimizer, optimizer)
