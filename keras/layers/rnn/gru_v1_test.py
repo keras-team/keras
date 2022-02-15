5fda404f455b8ee22744b3e6e56ad4d955f68f07
Fabien Hertschuh <fhertschuh@google.com>
2022-02-15 12:39:35 -0800

    Refactor RNN classes such that V2 cells and layers no longer depend on V1 counterparts.
    V2 GRU and LSTM cells no longer extend their V1 counterpart; instead the inheritance is the other way around.
    V2 GRU and LSTM layers no longer extend their V1 counterpart; instead the common code was duplicated.
    V2 cell wrappers and legacy cell wrappers no longer have a complex hierarchy with multiple inheritance to share code; instead, the common code was duplicated.
    Unit tests for GRU and LSTM layers were reorganized so that all generic tests that work for both V1 and V2 are in `gru_test.py` and `lstm_test.py`. The only tests in `gru_v1_test.py` and `lstm_v1_test.py` are the ones that compare V1 and V2 for accuracy or performance, and V1 specific tests.
    Also made cell wrappers API more consistent, all wrappers now expose a `wrapped_cell` property, not just `DropoutWrapper`.
    
    PiperOrigin-RevId: 428850325

--- keras/layers/rnn/gru_v1_test.py
+++ keras/layers/rnn/gru_v1_test.py
@@ -13,269 +13,147 @@
 # limitations under the License.
 # ==============================================================================
 """Tests for GRU V1 layer."""
-
-import copy
+# pylint: disable=g-direct-tensorflow-import
 
 from absl.testing import parameterized
 import keras
+from keras.layers.rnn import gru
+from keras.layers.rnn import gru_v1
 from keras.testing_infra import test_combinations
 from keras.testing_infra import test_utils
 from keras.utils import np_utils
 import numpy as np
 import tensorflow.compat.v2 as tf
 
+from tensorflow.core.protobuf import rewriter_config_pb2
 
-@test_combinations.run_all_keras_modes
-class GRULayerTest(test_combinations.TestCase):
 
-  def test_return_sequences_gru(self):
-    num_samples = 2
-    timesteps = 3
-    embedding_dim = 4
-    units = 2
-    test_utils.layer_test(
-        keras.layers.GRU,
-        kwargs={'units': units,
-                'return_sequences': True},
-        input_shape=(num_samples, timesteps, embedding_dim))
+# Global config for grappler setting that is used for graph mode test.
+_rewrites = rewriter_config_pb2.RewriterConfig()
+_rewrites.implementation_selector = rewriter_config_pb2.RewriterConfig.ON
+_rewrites.min_graph_nodes = -1
+_graph_options = tf.compat.v1.GraphOptions(rewrite_options=_rewrites)
+_config = tf.compat.v1.ConfigProto(graph_options=_graph_options)
+
+
+@test_utils.run_all_without_tensor_float_32('RNN GRU can use TF32 on GPU')
+@test_combinations.run_all_keras_modes(config=_config)
+class GRUGraphRewriteTest(test_combinations.TestCase):
 
   @tf.test.disable_with_predicate(
       pred=tf.test.is_built_with_rocm,
-      skip_message='Double type is not yet supported in ROCm')
+      skip_message='Skipping as ROCm MIOpen does not support padded input yet.')
   @test_utils.run_v2_only
-  def test_float64_gru(self):
-    num_samples = 2
-    timesteps = 3
-    embedding_dim = 4
-    units = 2
-    test_utils.layer_test(
-        keras.layers.GRU,
-        kwargs={'units': units,
-                'return_sequences': True,
-                'dtype': 'float64'},
-        input_shape=(num_samples, timesteps, embedding_dim),
-        input_dtype='float64')
-
-  def test_dynamic_behavior_gru(self):
-    num_samples = 2
-    timesteps = 3
-    embedding_dim = 4
-    units = 2
-    layer = keras.layers.GRU(units, input_shape=(None, embedding_dim))
-    model = keras.models.Sequential()
-    model.add(layer)
-    model.compile(
-        'rmsprop',
-        'mse',
-        run_eagerly=test_utils.should_run_eagerly())
-    x = np.random.random((num_samples, timesteps, embedding_dim))
-    y = np.random.random((num_samples, units))
-    model.train_on_batch(x, y)
-
-  def test_dropout_gru(self):
-    num_samples = 2
-    timesteps = 3
-    embedding_dim = 4
-    units = 2
-    test_utils.layer_test(
-        keras.layers.GRU,
-        kwargs={'units': units,
-                'dropout': 0.1,
-                'recurrent_dropout': 0.1},
-        input_shape=(num_samples, timesteps, embedding_dim))
-
-  def test_recurrent_dropout_with_implementation_restriction(self):
-    layer = keras.layers.GRU(2, recurrent_dropout=0.1, implementation=2)
-    # The implementation is force to 1 due to the limit of recurrent_dropout.
-    self.assertEqual(layer.implementation, 1)
-
-  @parameterized.parameters([0, 1, 2])
-  def test_implementation_mode_gru(self, implementation_mode):
-    num_samples = 2
-    timesteps = 3
-    embedding_dim = 4
-    units = 2
-    test_utils.layer_test(
-        keras.layers.GRU,
-        kwargs={'units': units,
-                'implementation': implementation_mode},
-        input_shape=(num_samples, timesteps, embedding_dim))
-
-  def test_reset_after_gru(self):
-    num_samples = 2
-    timesteps = 3
-    embedding_dim = 4
-    units = 2
+  def test_gru_feature_parity_v1_v2(self):
+    input_shape = 10
+    rnn_state_size = 8
+    timestep = 4
+    batch = 20
 
     (x_train, y_train), _ = test_utils.get_test_data(
-        train_samples=num_samples,
+        train_samples=batch,
         test_samples=0,
-        input_shape=(timesteps, embedding_dim),
-        num_classes=units)
-    y_train = np_utils.to_categorical(y_train, units)
+        input_shape=(timestep, input_shape),
+        num_classes=rnn_state_size,
+        random_seed=87654321)
+    y_train = np_utils.to_categorical(y_train, rnn_state_size)
+    # For the last batch item of the test data, we filter out the last
+    # timestep to simulate the variable length sequence and masking test.
+    x_train[-2:, -1, :] = 0.0
+    y_train[-2:] = 0
 
-    inputs = keras.layers.Input(shape=[timesteps, embedding_dim])
-    gru_layer = keras.layers.GRU(units,
-                                 reset_after=True)
-    output = gru_layer(inputs)
+    inputs = keras.layers.Input(
+        shape=[timestep, input_shape], dtype=tf.float32)
+    masked_input = keras.layers.Masking()(inputs)
+    gru_layer = gru_v1.GRU(rnn_state_size,
+                           recurrent_activation='sigmoid',
+                           reset_after=True)
+    output = gru_layer(masked_input)
     gru_model = keras.models.Model(inputs, output)
-    gru_model.compile(
-        'rmsprop',
-        'mse',
-        run_eagerly=test_utils.should_run_eagerly())
+    weights = gru_model.get_weights()
+    y_1 = gru_model.predict(x_train)
+    gru_model.compile('rmsprop', 'mse')
     gru_model.fit(x_train, y_train)
-    gru_model.predict(x_train)
+    y_2 = gru_model.predict(x_train)
+
+    with test_utils.device(should_use_gpu=True):
+      cudnn_layer = gru.GRU(rnn_state_size,
+                            recurrent_activation='sigmoid',
+                            reset_after=True)
+      cudnn_model = keras.models.Model(inputs, cudnn_layer(masked_input))
+    cudnn_model.set_weights(weights)
+    y_3 = cudnn_model.predict(x_train)
+    cudnn_model.compile('rmsprop', 'mse')
+    cudnn_model.fit(x_train, y_train)
+    y_4 = cudnn_model.predict(x_train)
+
+    self.assertAllClose(y_1, y_3, rtol=2e-5, atol=2e-5)
+    self.assertAllClose(y_2, y_4, rtol=2e-5, atol=2e-5)
+
+  @parameterized.named_parameters(
+      # test_name, time_major, go_backwards
+      ('normal', False, False),
+      ('time_major', True, False),
+      ('go_backwards', False, True),
+      ('both', True, True),
+  )
+  def test_time_major_and_go_backward_v1_v2(self, time_major, go_backwards):
+    input_shape = 10
+    rnn_state_size = 8
+    timestep = 4
+    batch = 100
+
+    x_train = np.random.random((batch, timestep, input_shape))
+
+    def build_model(layer_cls):
+      inputs = keras.layers.Input(
+          shape=[timestep, input_shape], dtype=tf.float32)
+      layer = layer_cls(rnn_state_size,
+                        recurrent_activation='sigmoid',
+                        time_major=time_major,
+                        return_sequences=True,
+                        go_backwards=go_backwards,
+                        reset_after=True)
+      if time_major:
+        converted_input = keras.layers.Lambda(
+            lambda t: tf.transpose(t, [1, 0, 2]))(inputs)
+        outputs = layer(converted_input)
+        outputs = keras.layers.Lambda(
+            lambda t: tf.transpose(t, [1, 0, 2]))(outputs)
+      else:
+        outputs = layer(inputs)
+      return keras.models.Model(inputs, outputs)
+
+    gru_model = build_model(gru_v1.GRU)
+    y_ref = gru_model.predict(x_train)
+    weights = gru_model.get_weights()
+
+    gru_v2_model = build_model(gru.GRU)
+    gru_v2_model.set_weights(weights)
+    y = gru_v2_model.predict(x_train)
+
+    self.assertAllClose(y, y_ref)
 
   @tf.test.disable_with_predicate(
       pred=tf.test.is_built_with_rocm,
-      skip_message='MIOpen only supports packed input output')
-  def test_with_masking_layer_gru(self):
-    layer_class = keras.layers.GRU
-    inputs = np.random.random((2, 3, 4))
-    targets = np.abs(np.random.random((2, 3, 5)))
-    targets /= targets.sum(axis=-1, keepdims=True)
-    model = keras.models.Sequential()
-    model.add(keras.layers.Masking(input_shape=(3, 4)))
-    model.add(layer_class(units=5, return_sequences=True, unroll=False))
-    model.compile(
-        loss='categorical_crossentropy',
-        optimizer='rmsprop',
-        run_eagerly=test_utils.should_run_eagerly())
-    model.fit(inputs, targets, epochs=1, batch_size=2, verbose=1)
+      skip_message='Skipping as ROCm MIOpen does not support padded input yet.')
+  @test_utils.run_v2_only
+  def test_explicit_device_with_go_backward_and_mask_v1(self):
+    batch_size = 8
+    timestep = 7
+    masksteps = 5
+    units = 4
 
-  @tf.test.disable_with_predicate(
-      pred=tf.test.is_built_with_rocm,
-      skip_message='MIOpen only supports packed input output')
-  def test_statefulness_gru(self):
-    num_samples = 2
-    timesteps = 3
-    embedding_dim = 4
-    units = 2
-    layer_class = keras.layers.GRU
+    inputs = np.random.randn(batch_size, timestep, units).astype(np.float32)
+    mask = np.ones((batch_size, timestep)).astype(np.bool)
+    mask[:, masksteps:] = 0
 
-    model = keras.models.Sequential()
-    model.add(
-        keras.layers.Embedding(
-            4,
-            embedding_dim,
-            mask_zero=True,
-            input_length=timesteps,
-            batch_input_shape=(num_samples, timesteps)))
-    layer = layer_class(
-        units, return_sequences=False, stateful=True, weights=None)
-    model.add(layer)
-    model.compile(
-        optimizer='sgd',
-        loss='mse',
-        run_eagerly=test_utils.should_run_eagerly())
-    out1 = model.predict(np.ones((num_samples, timesteps)))
-    self.assertEqual(out1.shape, (num_samples, units))
-
-    # train once so that the states change
-    model.train_on_batch(
-        np.ones((num_samples, timesteps)), np.ones((num_samples, units)))
-    out2 = model.predict(np.ones((num_samples, timesteps)))
-
-    # if the state is not reset, output should be different
-    self.assertNotEqual(out1.max(), out2.max())
-
-    # check that output changes after states are reset
-    # (even though the model itself didn't change)
-    layer.reset_states()
-    out3 = model.predict(np.ones((num_samples, timesteps)))
-    self.assertNotEqual(out2.max(), out3.max())
-
-    # check that container-level reset_states() works
-    model.reset_states()
-    out4 = model.predict(np.ones((num_samples, timesteps)))
-    np.testing.assert_allclose(out3, out4, atol=1e-5)
-
-    # check that the call to `predict` updated the states
-    out5 = model.predict(np.ones((num_samples, timesteps)))
-    self.assertNotEqual(out4.max(), out5.max())
-
-    # Check masking
-    layer.reset_states()
-
-    left_padded_input = np.ones((num_samples, timesteps))
-    left_padded_input[0, :1] = 0
-    left_padded_input[1, :2] = 0
-    out6 = model.predict(left_padded_input)
-
-    layer.reset_states()
-
-    right_padded_input = np.ones((num_samples, timesteps))
-    right_padded_input[0, -1:] = 0
-    right_padded_input[1, -2:] = 0
-    out7 = model.predict(right_padded_input)
-
-    np.testing.assert_allclose(out7, out6, atol=1e-5)
-
-  def test_get_initial_states(self):
-    batch_size = 4
-    cell = keras.layers.GRUCell(20)
-    initial_state = cell.get_initial_state(
-        batch_size=batch_size, dtype=tf.float32)
-    _, state = cell(np.ones((batch_size, 20), dtype=np.float32), initial_state)
-    self.assertEqual(state.shape, initial_state.shape)
-
-
-@test_combinations.generate(test_combinations.combine(mode=['graph', 'eager']))
-class GRULayerGenericTest(tf.test.TestCase):
-
-  def test_constraints_gru(self):
-    embedding_dim = 4
-    layer_class = keras.layers.GRU
-    k_constraint = keras.constraints.max_norm(0.01)
-    r_constraint = keras.constraints.max_norm(0.01)
-    b_constraint = keras.constraints.max_norm(0.01)
-    layer = layer_class(
-        5,
-        return_sequences=False,
-        weights=None,
-        input_shape=(None, embedding_dim),
-        kernel_constraint=k_constraint,
-        recurrent_constraint=r_constraint,
-        bias_constraint=b_constraint)
-    layer.build((None, None, embedding_dim))
-    self.assertEqual(layer.cell.kernel.constraint, k_constraint)
-    self.assertEqual(layer.cell.recurrent_kernel.constraint, r_constraint)
-    self.assertEqual(layer.cell.bias.constraint, b_constraint)
-
-  def test_from_config_gru(self):
-    layer_class = keras.layers.GRU
-    for stateful in (False, True):
-      l1 = layer_class(units=1, stateful=stateful)
-      l2 = layer_class.from_config(l1.get_config())
-      assert l1.get_config() == l2.get_config()
-
-  def test_deep_copy_gru(self):
-    cell = keras.layers.GRUCell(5)
-    copied_cell = copy.deepcopy(cell)
-    self.assertEqual(copied_cell.units, 5)
-    self.assertEqual(cell.get_config(), copied_cell.get_config())
-
-  def test_regularizers_gru(self):
-    embedding_dim = 4
-    layer_class = keras.layers.GRU
-    layer = layer_class(
-        5,
-        return_sequences=False,
-        weights=None,
-        input_shape=(None, embedding_dim),
-        kernel_regularizer=keras.regularizers.l1(0.01),
-        recurrent_regularizer=keras.regularizers.l1(0.01),
-        bias_regularizer='l2',
-        activity_regularizer='l1')
-    layer.build((None, None, 2))
-    self.assertLen(layer.losses, 3)
-
-    x = keras.backend.variable(np.ones((2, 3, 2)))
-    layer(x)
-    if tf.executing_eagerly():
-      self.assertLen(layer.losses, 4)
-    else:
-      self.assertLen(layer.get_losses_for(x), 1)
+    gru_layer = gru_v1.GRU(
+        units, return_sequences=True, go_backwards=True)
+    with test_utils.device(should_use_gpu=True):
+      outputs_masked = gru_layer(inputs, mask=tf.constant(mask))
+      outputs_trimmed = gru_layer(inputs[:, :masksteps])
+    self.assertAllClose(outputs_masked[:, -masksteps:], outputs_trimmed)
 
 
 if __name__ == '__main__':
