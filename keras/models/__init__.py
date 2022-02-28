300529582e3865db8aee421e349bf0d6f5c43b15
Chen Qian <chenmoney@google.com>
2022-02-28 12:35:52 -0800

    Implement a Sharpness-Aware Minimization (SAM) API in Keras.
    To use this SAM api, the task should be compatible with `model.fit()`, which means tasks requiring CTL cannot use SAM now, e.g., neural style transfer.
    
    We also move models.py into models/ folder, as we are expecting to see more customized models being added into models/ folder.
    
    PiperOrigin-RevId: 431500786

---
+++ keras/models/__init__.py
@@ -0,0 +1,36 @@
+# Copyright 2022 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Keras models API."""
+# pylint: disable=g-bad-import-order
+
+from keras.engine.functional import Functional
+from keras.engine.sequential import Sequential
+from keras.engine.training import Model
+from keras.models.cloning import clone_and_build_model
+from keras.models.cloning import clone_model
+from keras.models.cloning import share_weights
+from keras.models.sharpness_aware_minimization import SharpnessAwareMinimization
+from keras.saving.model_config import model_from_config
+from keras.saving.model_config import model_from_json
+from keras.saving.model_config import model_from_yaml
+from keras.saving.save import load_model
+from keras.saving.save import save_model
+
+# Private symbols that are used in tests.
+# TODO(b/221261361): Clean up private symbols usage and remove these imports.
+from keras.models.cloning import _clone_functional_model
+from keras.models.cloning import _clone_layer
+from keras.models.cloning import _clone_layers_and_model_config
+from keras.models.cloning import _clone_sequential_model
