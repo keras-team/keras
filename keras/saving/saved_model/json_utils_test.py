851cc442128d33d865e6bf73187cf198dd4b08c9
Haifeng Jin <haifengj@google.com>
2022-03-04 11:46:09 -0800

    Support saving model for models with extra constant `CompositeTensor` input. Now saves the `CompositeTensor` just like how constant `tf.Tensor` input was saved by serializing it.
    PiperOrigin-RevId: 432497765

--- keras/saving/saved_model/json_utils_test.py
+++ keras/saving/saved_model/json_utils_test.py
@@ -19,9 +19,11 @@ import tensorflow.compat.v2 as tf
 
 import enum
 from keras.saving.saved_model import json_utils
+from keras.testing_infra import test_combinations
+from keras.testing_infra import test_utils
 
 
-class JsonUtilsTest(tf.test.TestCase):
+class JsonUtilsTest(test_combinations.TestCase):
 
   def test_encode_decode_tensor_shape(self):
     metadata = {
@@ -67,6 +69,26 @@ class JsonUtilsTest(tf.test.TestCase):
     string = json_utils.Encoder().encode(config)
     loaded = json_utils.decode(string)
     self.assertAllEqual({'key': 'a', 'key2': 'b'}, loaded)
+
+  @test_utils.run_v2_only
+  def test_encode_decode_ragged_tensor(self):
+    x = tf.ragged.constant([[1., 2.], [3.]])
+    string = json_utils.Encoder().encode(x)
+    loaded = json_utils.decode(string)
+    self.assertAllEqual(loaded, x)
+
+  @test_utils.run_v2_only
+  def test_encode_decode_extension_type_tensor(self):
+    class MaskedTensor(tf.experimental.ExtensionType):
+      __name__ = 'MaskedTensor'
+      values: tf.Tensor
+      mask: tf.Tensor
+    x = MaskedTensor(values=[[1, 2, 3], [4, 5, 6]],
+                     mask=[[True, True, False], [True, False, True]])
+    string = json_utils.Encoder().encode(x)
+    loaded = json_utils.decode(string)
+    self.assertAllEqual(loaded, x)
+
 
 if __name__ == '__main__':
   tf.test.main()
