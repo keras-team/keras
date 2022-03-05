62aab556c6252e54b9f3ee3fa65243aecd6aea52
Fabien Hertschuh <fhertschuh@google.com>
2022-03-04 16:14:46 -0800

    Refactor RNN classes such that V2 cells and layers no longer depend on V1 counterparts.
    V2 GRU and LSTM cells no longer extend their V1 counterpart; instead the inheritance is the other way around.
    V2 GRU and LSTM layers no longer extend their V1 counterpart; instead the common code was duplicated.
    V2 cell wrappers and legacy cell wrappers no longer have a complex hierarchy with multiple inheritance to share code; instead, the common code was duplicated.
    Unit tests for GRU and LSTM layers were reorganized so that all generic tests that work for both V1 and V2 are in `gru_test.py` and `lstm_test.py`. The only tests in `gru_v1_test.py` and `lstm_v1_test.py` are the ones that compare V1 and V2 for accuracy or performance, and V1 specific tests.
    Also made cell wrappers API more consistent, all wrappers now expose a `wrapped_cell` property, not just `DropoutWrapper`.
    
    PiperOrigin-RevId: 432554966

--- keras/layers/rnn/lstm.py
+++ keras/layers/rnn/lstm.py
@@ -19,19 +19,29 @@ import uuid
 
 from keras import activations
 from keras import backend
+from keras import constraints
+from keras import initializers
+from keras import regularizers
 from keras.engine import base_layer
 from keras.engine.input_spec import InputSpec
 from keras.layers.rnn import gru_lstm_utils
-from keras.layers.rnn import lstm_v1
+from keras.layers.rnn import rnn_utils
+from keras.layers.rnn.base_rnn import RNN
 from keras.layers.rnn.dropout_rnn_cell_mixin import DropoutRNNCellMixin
+from keras.utils import tf_utils
 import tensorflow.compat.v2 as tf
 
 from tensorflow.python.platform import tf_logging as logging
 from tensorflow.python.util.tf_export import keras_export
 
 
+RECURRENT_DROPOUT_WARNING_MSG = (
+    'RNN `implementation=2` is not supported when `recurrent_dropout` is set. '
+    'Using `implementation=1`.')
+
+
 @keras_export('keras.layers.LSTMCell', v1=[])
-class LSTMCell(lstm_v1.LSTMCell):
+class LSTMCell(DropoutRNNCellMixin, base_layer.BaseRandomLayer):
   """Cell class for the LSTM layer.
 
   See [the Keras RNN API guide](https://www.tensorflow.org/guide/keras/rnn)
@@ -125,29 +135,216 @@ class LSTMCell(lstm_v1.LSTMCell):
                dropout=0.,
                recurrent_dropout=0.,
                **kwargs):
-    super(LSTMCell, self).__init__(
-        units,
-        activation=activation,
-        recurrent_activation=recurrent_activation,
-        use_bias=use_bias,
-        kernel_initializer=kernel_initializer,
-        recurrent_initializer=recurrent_initializer,
-        bias_initializer=bias_initializer,
-        unit_forget_bias=unit_forget_bias,
-        kernel_regularizer=kernel_regularizer,
-        recurrent_regularizer=recurrent_regularizer,
-        bias_regularizer=bias_regularizer,
-        kernel_constraint=kernel_constraint,
-        recurrent_constraint=recurrent_constraint,
-        bias_constraint=bias_constraint,
-        dropout=dropout,
-        recurrent_dropout=recurrent_dropout,
-        implementation=kwargs.pop('implementation', 2),
-        **kwargs)
+    if units < 0:
+      raise ValueError(f'Received an invalid value for argument `units`, '
+                       f'expected a positive integer, got {units}.')
+    # By default use cached variable under v2 mode, see b/143699808.
+    if tf.compat.v1.executing_eagerly_outside_functions():
+      self._enable_caching_device = kwargs.pop('enable_caching_device', True)
+    else:
+      self._enable_caching_device = kwargs.pop('enable_caching_device', False)
+    super(LSTMCell, self).__init__(**kwargs)
+    self.units = units
+    self.activation = activations.get(activation)
+    self.recurrent_activation = activations.get(recurrent_activation)
+    self.use_bias = use_bias
+
+    self.kernel_initializer = initializers.get(kernel_initializer)
+    self.recurrent_initializer = initializers.get(recurrent_initializer)
+    self.bias_initializer = initializers.get(bias_initializer)
+    self.unit_forget_bias = unit_forget_bias
+
+    self.kernel_regularizer = regularizers.get(kernel_regularizer)
+    self.recurrent_regularizer = regularizers.get(recurrent_regularizer)
+    self.bias_regularizer = regularizers.get(bias_regularizer)
+
+    self.kernel_constraint = constraints.get(kernel_constraint)
+    self.recurrent_constraint = constraints.get(recurrent_constraint)
+    self.bias_constraint = constraints.get(bias_constraint)
+
+    self.dropout = min(1., max(0., dropout))
+    self.recurrent_dropout = min(1., max(0., recurrent_dropout))
+    implementation = kwargs.pop('implementation', 2)
+    if self.recurrent_dropout != 0 and implementation != 1:
+      logging.debug(RECURRENT_DROPOUT_WARNING_MSG)
+      self.implementation = 1
+    else:
+      self.implementation = implementation
+    self.state_size = [self.units, self.units]
+    self.output_size = self.units
+
+  @tf_utils.shape_type_conversion
+  def build(self, input_shape):
+    default_caching_device = rnn_utils.caching_device(self)
+    input_dim = input_shape[-1]
+    self.kernel = self.add_weight(
+        shape=(input_dim, self.units * 4),
+        name='kernel',
+        initializer=self.kernel_initializer,
+        regularizer=self.kernel_regularizer,
+        constraint=self.kernel_constraint,
+        caching_device=default_caching_device)
+    self.recurrent_kernel = self.add_weight(
+        shape=(self.units, self.units * 4),
+        name='recurrent_kernel',
+        initializer=self.recurrent_initializer,
+        regularizer=self.recurrent_regularizer,
+        constraint=self.recurrent_constraint,
+        caching_device=default_caching_device)
+
+    if self.use_bias:
+      if self.unit_forget_bias:
+
+        def bias_initializer(_, *args, **kwargs):
+          return backend.concatenate([
+              self.bias_initializer((self.units,), *args, **kwargs),
+              initializers.get('ones')((self.units,), *args, **kwargs),
+              self.bias_initializer((self.units * 2,), *args, **kwargs),
+          ])
+      else:
+        bias_initializer = self.bias_initializer
+      self.bias = self.add_weight(
+          shape=(self.units * 4,),
+          name='bias',
+          initializer=bias_initializer,
+          regularizer=self.bias_regularizer,
+          constraint=self.bias_constraint,
+          caching_device=default_caching_device)
+    else:
+      self.bias = None
+    self.built = True
+
+  def _compute_carry_and_output(self, x, h_tm1, c_tm1):
+    """Computes carry and output using split kernels."""
+    x_i, x_f, x_c, x_o = x
+    h_tm1_i, h_tm1_f, h_tm1_c, h_tm1_o = h_tm1
+    i = self.recurrent_activation(
+        x_i + backend.dot(h_tm1_i, self.recurrent_kernel[:, :self.units]))
+    f = self.recurrent_activation(x_f + backend.dot(
+        h_tm1_f, self.recurrent_kernel[:, self.units:self.units * 2]))
+    c = f * c_tm1 + i * self.activation(x_c + backend.dot(
+        h_tm1_c, self.recurrent_kernel[:, self.units * 2:self.units * 3]))
+    o = self.recurrent_activation(
+        x_o + backend.dot(h_tm1_o, self.recurrent_kernel[:, self.units * 3:]))
+    return c, o
+
+  def _compute_carry_and_output_fused(self, z, c_tm1):
+    """Computes carry and output using fused kernels."""
+    z0, z1, z2, z3 = z
+    i = self.recurrent_activation(z0)
+    f = self.recurrent_activation(z1)
+    c = f * c_tm1 + i * self.activation(z2)
+    o = self.recurrent_activation(z3)
+    return c, o
+
+  def call(self, inputs, states, training=None):
+    h_tm1 = states[0]  # previous memory state
+    c_tm1 = states[1]  # previous carry state
+
+    dp_mask = self.get_dropout_mask_for_cell(inputs, training, count=4)
+    rec_dp_mask = self.get_recurrent_dropout_mask_for_cell(
+        h_tm1, training, count=4)
+
+    if self.implementation == 1:
+      if 0 < self.dropout < 1.:
+        inputs_i = inputs * dp_mask[0]
+        inputs_f = inputs * dp_mask[1]
+        inputs_c = inputs * dp_mask[2]
+        inputs_o = inputs * dp_mask[3]
+      else:
+        inputs_i = inputs
+        inputs_f = inputs
+        inputs_c = inputs
+        inputs_o = inputs
+      k_i, k_f, k_c, k_o = tf.split(
+          self.kernel, num_or_size_splits=4, axis=1)
+      x_i = backend.dot(inputs_i, k_i)
+      x_f = backend.dot(inputs_f, k_f)
+      x_c = backend.dot(inputs_c, k_c)
+      x_o = backend.dot(inputs_o, k_o)
+      if self.use_bias:
+        b_i, b_f, b_c, b_o = tf.split(
+            self.bias, num_or_size_splits=4, axis=0)
+        x_i = backend.bias_add(x_i, b_i)
+        x_f = backend.bias_add(x_f, b_f)
+        x_c = backend.bias_add(x_c, b_c)
+        x_o = backend.bias_add(x_o, b_o)
+
+      if 0 < self.recurrent_dropout < 1.:
+        h_tm1_i = h_tm1 * rec_dp_mask[0]
+        h_tm1_f = h_tm1 * rec_dp_mask[1]
+        h_tm1_c = h_tm1 * rec_dp_mask[2]
+        h_tm1_o = h_tm1 * rec_dp_mask[3]
+      else:
+        h_tm1_i = h_tm1
+        h_tm1_f = h_tm1
+        h_tm1_c = h_tm1
+        h_tm1_o = h_tm1
+      x = (x_i, x_f, x_c, x_o)
+      h_tm1 = (h_tm1_i, h_tm1_f, h_tm1_c, h_tm1_o)
+      c, o = self._compute_carry_and_output(x, h_tm1, c_tm1)
+    else:
+      if 0. < self.dropout < 1.:
+        inputs = inputs * dp_mask[0]
+      z = backend.dot(inputs, self.kernel)
+      z += backend.dot(h_tm1, self.recurrent_kernel)
+      if self.use_bias:
+        z = backend.bias_add(z, self.bias)
+
+      z = tf.split(z, num_or_size_splits=4, axis=1)
+      c, o = self._compute_carry_and_output_fused(z, c_tm1)
+
+    h = o * self.activation(c)
+    return h, [h, c]
+
+  def get_config(self):
+    config = {
+        'units':
+            self.units,
+        'activation':
+            activations.serialize(self.activation),
+        'recurrent_activation':
+            activations.serialize(self.recurrent_activation),
+        'use_bias':
+            self.use_bias,
+        'kernel_initializer':
+            initializers.serialize(self.kernel_initializer),
+        'recurrent_initializer':
+            initializers.serialize(self.recurrent_initializer),
+        'bias_initializer':
+            initializers.serialize(self.bias_initializer),
+        'unit_forget_bias':
+            self.unit_forget_bias,
+        'kernel_regularizer':
+            regularizers.serialize(self.kernel_regularizer),
+        'recurrent_regularizer':
+            regularizers.serialize(self.recurrent_regularizer),
+        'bias_regularizer':
+            regularizers.serialize(self.bias_regularizer),
+        'kernel_constraint':
+            constraints.serialize(self.kernel_constraint),
+        'recurrent_constraint':
+            constraints.serialize(self.recurrent_constraint),
+        'bias_constraint':
+            constraints.serialize(self.bias_constraint),
+        'dropout':
+            self.dropout,
+        'recurrent_dropout':
+            self.recurrent_dropout,
+        'implementation':
+            self.implementation
+    }
+    config.update(rnn_utils.config_for_enable_caching_device(self))
+    base_config = super(LSTMCell, self).get_config()
+    return dict(list(base_config.items()) + list(config.items()))
+
+  def get_initial_state(self, inputs=None, batch_size=None, dtype=None):
+    return list(rnn_utils.generate_zero_filled_state_for_cell(
+        self, inputs, batch_size, dtype))
 
 
 @keras_export('keras.layers.LSTM', v1=[])
-class LSTM(DropoutRNNCellMixin, lstm_v1.LSTM, base_layer.BaseRandomLayer):
+class LSTM(DropoutRNNCellMixin, RNN, base_layer.BaseRandomLayer):
   """Long Short-Term Memory layer - Hochreiter 1997.
 
   See [the Keras RNN API guide](https://www.tensorflow.org/guide/keras/rnn)
@@ -288,26 +485,39 @@ class LSTM(DropoutRNNCellMixin, lstm_v1.LSTM, base_layer.BaseRandomLayer):
     # return_runtime is a flag for testing, which shows the real backend
     # implementation chosen by grappler in graph mode.
     self.return_runtime = kwargs.pop('return_runtime', False)
-
-    super(LSTM, self).__init__(
+    implementation = kwargs.pop('implementation', 2)
+    if implementation == 0:
+      logging.warning('`implementation=0` has been deprecated, '
+                      'and now defaults to `implementation=1`.'
+                      'Please update your layer call.')
+    if 'enable_caching_device' in kwargs:
+      cell_kwargs = {'enable_caching_device':
+                     kwargs.pop('enable_caching_device')}
+    else:
+      cell_kwargs = {}
+    cell = LSTMCell(
         units,
         activation=activation,
         recurrent_activation=recurrent_activation,
         use_bias=use_bias,
         kernel_initializer=kernel_initializer,
         recurrent_initializer=recurrent_initializer,
-        bias_initializer=bias_initializer,
         unit_forget_bias=unit_forget_bias,
+        bias_initializer=bias_initializer,
         kernel_regularizer=kernel_regularizer,
         recurrent_regularizer=recurrent_regularizer,
         bias_regularizer=bias_regularizer,
-        activity_regularizer=activity_regularizer,
         kernel_constraint=kernel_constraint,
         recurrent_constraint=recurrent_constraint,
         bias_constraint=bias_constraint,
         dropout=dropout,
         recurrent_dropout=recurrent_dropout,
-        implementation=kwargs.pop('implementation', 2),
+        implementation=implementation,
+        dtype=kwargs.get('dtype'),
+        trainable=kwargs.get('trainable', True),
+        **cell_kwargs)
+    super(LSTM, self).__init__(
+        cell,
         return_sequences=return_sequences,
         return_state=return_state,
         go_backwards=go_backwards,
@@ -315,7 +525,8 @@ class LSTM(DropoutRNNCellMixin, lstm_v1.LSTM, base_layer.BaseRandomLayer):
         time_major=time_major,
         unroll=unroll,
         **kwargs)
-
+    self.activity_regularizer = regularizers.get(activity_regularizer)
+    self.input_spec = [InputSpec(ndim=3)]
     self.state_spec = [
         InputSpec(shape=(None, dim)) for dim in (self.units, self.units)
     ]
@@ -479,6 +690,124 @@ class LSTM(DropoutRNNCellMixin, lstm_v1.LSTM, base_layer.BaseRandomLayer):
       return output, runtime
     else:
       return output
+
+  @property
+  def units(self):
+    return self.cell.units
+
+  @property
+  def activation(self):
+    return self.cell.activation
+
+  @property
+  def recurrent_activation(self):
+    return self.cell.recurrent_activation
+
+  @property
+  def use_bias(self):
+    return self.cell.use_bias
+
+  @property
+  def kernel_initializer(self):
+    return self.cell.kernel_initializer
+
+  @property
+  def recurrent_initializer(self):
+    return self.cell.recurrent_initializer
+
+  @property
+  def bias_initializer(self):
+    return self.cell.bias_initializer
+
+  @property
+  def unit_forget_bias(self):
+    return self.cell.unit_forget_bias
+
+  @property
+  def kernel_regularizer(self):
+    return self.cell.kernel_regularizer
+
+  @property
+  def recurrent_regularizer(self):
+    return self.cell.recurrent_regularizer
+
+  @property
+  def bias_regularizer(self):
+    return self.cell.bias_regularizer
+
+  @property
+  def kernel_constraint(self):
+    return self.cell.kernel_constraint
+
+  @property
+  def recurrent_constraint(self):
+    return self.cell.recurrent_constraint
+
+  @property
+  def bias_constraint(self):
+    return self.cell.bias_constraint
+
+  @property
+  def dropout(self):
+    return self.cell.dropout
+
+  @property
+  def recurrent_dropout(self):
+    return self.cell.recurrent_dropout
+
+  @property
+  def implementation(self):
+    return self.cell.implementation
+
+  def get_config(self):
+    config = {
+        'units':
+            self.units,
+        'activation':
+            activations.serialize(self.activation),
+        'recurrent_activation':
+            activations.serialize(self.recurrent_activation),
+        'use_bias':
+            self.use_bias,
+        'kernel_initializer':
+            initializers.serialize(self.kernel_initializer),
+        'recurrent_initializer':
+            initializers.serialize(self.recurrent_initializer),
+        'bias_initializer':
+            initializers.serialize(self.bias_initializer),
+        'unit_forget_bias':
+            self.unit_forget_bias,
+        'kernel_regularizer':
+            regularizers.serialize(self.kernel_regularizer),
+        'recurrent_regularizer':
+            regularizers.serialize(self.recurrent_regularizer),
+        'bias_regularizer':
+            regularizers.serialize(self.bias_regularizer),
+        'activity_regularizer':
+            regularizers.serialize(self.activity_regularizer),
+        'kernel_constraint':
+            constraints.serialize(self.kernel_constraint),
+        'recurrent_constraint':
+            constraints.serialize(self.recurrent_constraint),
+        'bias_constraint':
+            constraints.serialize(self.bias_constraint),
+        'dropout':
+            self.dropout,
+        'recurrent_dropout':
+            self.recurrent_dropout,
+        'implementation':
+            self.implementation
+    }
+    config.update(rnn_utils.config_for_enable_caching_device(self.cell))
+    base_config = super(LSTM, self).get_config()
+    del base_config['cell']
+    return dict(list(base_config.items()) + list(config.items()))
+
+  @classmethod
+  def from_config(cls, config):
+    if 'implementation' in config and config['implementation'] == 0:
+      config['implementation'] = 1
+    return cls(**config)
 
 
 def standard_lstm(inputs, init_h, init_c, kernel, recurrent_kernel, bias,
