9f4dd4d3b7b907577a89711bc2d29587d3661d8e
Juntang Zhuang <44451229+juntang-zhuang@users.noreply.github.com>
2022-03-05 19:37:29 -0500

    Update gsam.py

--- keras/models/gsam.py
+++ keras/models/gsam.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Implementation for GSAM, Surrogate Gap Guided Sharpness Aware Minimization (ICLR 2022)"""
+"""Implementation for GSAM"""
 
 import copy
 
@@ -21,31 +21,39 @@ from keras.layers import deserialize as deserialize_layer
 from keras.models import Model
 from keras.utils import generic_utils
 import tensorflow.compat.v2 as tf
+from keras.models.sharpness_aware_minimization import SharpnessAwareMinimization
 
 # pylint: disable=g-classes-have-attributes
 
 
 @generic_utils.register_keras_serializable()
-class SharpnessAwareMinimization(Model):
-  """Sharpness aware minimization (SAM) training flow.
-  Sharpness-aware minimization (SAM) is a technique that improves the model
-  generalization and provides robustness to label noise.
+class SharpnessAwareMinimization(SharpnessAwareMinimization):
+  """GSAM training flow.
+  Surrogate Gap Guided Sharpness Aware Minimization (GSAM) is an improvement over Sharpness-Aware-Minimization (SAM)
   Args:
     model: `tf.keras.Model` instance. The inner model that does the
       forward-backward pass.
-    rho: float, defaults to 0.05. The gradients scaling factor.
+    rho_max: float, defaults to 0.05. The max gradients scaling factor.
+    rho_min: float, defaults to 0.01. The min gradients scaling factor.
+    lr_max: float, defaults to 0.001. The max learning rate.
+    lr_min: float, defaults to 0.0, the min learning rate.
+    eps: float, defaults to 1e-12, avoid division by 0.
     name: string, defaults to None. The name of the SAM model.
   Reference:
-    [Pierre Foret et al., 2020](https://arxiv.org/abs/2010.01412)
+    [Juntang Zhuang et al., 2022](https://openreview.net/pdf?id=edONMAnhLu-)
   """
 
-  def __init__(self, model, rho=0.05, name=None):
+  def __init__(self, model, rho_max=0.05, rho_min=0.01, lr_max=0.001, lr_min=0.0, eps=1e-12, name=None):
     super().__init__(name=name)
     self.model = model
-    self.rho = rho
+    self.rho_max = rho_max
+    self.rho_min = rho_min
+    self.lr_max = lr_max
+    self.lr_min = lr_min
+    self.eps = eps
 
   def train_step(self, data):
-    """The logic of one SAM training step.
+    """The logic of one GSAM training step.
     Args:
       data: A nested structure of `Tensor`s. It should be of structure
         (x, y, sample_weight) or (x, y).
@@ -59,10 +67,16 @@ class SharpnessAwareMinimization(Model):
     trainable_variables = self.model.trainable_variables
     gradients = tape.gradient(loss, trainable_variables)
 
+    # update rho according to schedule
+    lr = self.optimizer.lr
+    rho = self.rho_min + (self.rho_max - self.rho_min) * (lr - self.lr_min) / (self.lr_max - self.lr_min)
+    
+    # calculate perturbation amplitude
     epsilon_w_cache = []
     gradients_order2_norm = self._gradients_order2_norm(gradients)
-    scale = self.rho / (gradients_order2_norm + 1e-12)
+    scale = rho / (gradients_order2_norm + self.eps)
 
+    # perturb weights, get "perturbed" gradient
     for (gradient, variable) in zip(gradients, trainable_variables):
       epsilon_w = gradient * scale
       variable.assign_add(epsilon_w)
@@ -72,7 +86,9 @@ class SharpnessAwareMinimization(Model):
       pred = self(x)
       loss = self.compiled_loss(y, pred)
 
-    gradients = tape.gradient(loss, trainable_variables)
+    gradients_perturbed = tape.gradient(loss, trainable_variables)
+    
+    # 
     for (variable, epsilon_w) in zip(trainable_variables, epsilon_w_cache):
       # Restore the variable to its original value before `apply_gradients()`.
       variable.assign_sub(epsilon_w)
@@ -82,33 +98,15 @@ class SharpnessAwareMinimization(Model):
     self.compiled_metrics.update_state(y, pred, sample_weight)
     return {m.name: m.result() for m in self.metrics}
 
-  def call(self, inputs):
-    """Forward pass of SAM.
-    SAM delegates the forward pass call to the wrapped model.
-    Args:
-      inputs: Tensor. The model inputs.
-    Returns:
-      A Tensor, the outputs of the wrapped model for given `inputs`.
-    """
-    return self.model(inputs)
-
   def get_config(self):
     config = super().get_config()
     config.update({
         "model": generic_utils.serialize_keras_object(self.model),
-        "rho": self.rho,
+        "rho_max": self.rho_max,
+        "rho_min": self.rho_min,
+        "lr_max": self.lr_max,
+        "lr_min": self.lr_min,
+        "eps": self.eps,
     })
     return config
-
-  @classmethod
-  def from_config(cls, config, custom_objects=None):
-    # Avoid mutating the input dict.
-    config = copy.deepcopy(config)
-    model = deserialize_layer(
-        config.pop("model"), custom_objects=custom_objects)
-    return cls(model, **config)
-
-  def _gradients_order2_norm(self, gradients):
-    norm = tf.norm(
-        tf.stack([tf.norm(grad) for grad in gradients if grad is not None]))
-    return norm
+  
