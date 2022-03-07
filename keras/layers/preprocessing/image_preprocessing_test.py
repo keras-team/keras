dbc4526978865b372e794accbaa7c584f9c86a0f
Luke Wood <lukewood@google.com>
2022-03-07 13:16:31 -0800

    add `auto_vectorize` argument to BaseImageAugmentation layer.
    This will allow subclassing layers to pick if we will auto-vectorize or not.
    
    PiperOrigin-RevId: 433021990

--- keras/layers/preprocessing/image_preprocessing_test.py
+++ keras/layers/preprocessing/image_preprocessing_test.py
@@ -1885,6 +1885,13 @@ class RandomAddLayer(image_preprocessing.BaseImageAugmentationLayer):
     return label + transformation
 
 
+class VectorizeDisabledLayer(image_preprocessing.BaseImageAugmentationLayer):
+
+  def __init__(self, **kwargs):
+    self.auto_vectorize = False
+    super().__init__(**kwargs)
+
+
 @test_combinations.run_all_keras_modes(always_skip_v1=True)
 class BaseImageAugmentationLayerTest(test_combinations.TestCase):
 
@@ -1901,6 +1908,19 @@ class BaseImageAugmentationLayerTest(test_combinations.TestCase):
     output = add_layer({'images': image})
 
     self.assertIsInstance(output, dict)
+
+  def test_auto_vectorize_disabled(self):
+    vectorize_disabled_layer = VectorizeDisabledLayer()
+    self.assertFalse(vectorize_disabled_layer.auto_vectorize)
+    self.assertEqual(vectorize_disabled_layer._map_fn, tf.map_fn)
+
+  @test_utils.run_v2_only
+  def test_augment_casts_dtypes(self):
+    add_layer = RandomAddLayer(fixed_value=2.0)
+    images = tf.ones((2, 8, 8, 3), dtype='uint8')
+    output = add_layer(images)
+
+    self.assertAllClose(tf.ones((2, 8, 8, 3), dtype='float32') * 3.0, output)
 
   def test_augment_batch_images(self):
     add_layer = RandomAddLayer()
