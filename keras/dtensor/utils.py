d44f52435d7e60228aa454265d0c22441703fe64
Scott Zhu <scottzhu@google.com>
2022-03-10 14:05:15 -0800

    Update keras metrics to support DTensor mesh.
    So far all the weights will have fully replicated layout (since they are usually small counter). We inject the kwargs `mesh` to all the metrics, using the similar approach we did for layers.
    
    PiperOrigin-RevId: 433846454

--- keras/dtensor/utils.py
+++ keras/dtensor/utils.py
@@ -103,6 +103,47 @@ def allow_initializer_layout(init_method):
       target=init_method, decorator_func=_wrap_function)
 
 
+def inject_mesh(init_method):
+  """Inject DTensor mesh information to an object.
+
+  This is useful for keras object like `Metric` and `Optimizer` which need
+  DTensor mesh to create the weights, but doesn't want to change the current
+  public API interface.
+
+  This is for temporary usage and eventually the mesh/layout information will be
+  public arguments in the `__init__` method
+
+  Sample usage:
+  ```python
+  class Accuracy(tf.keras.metrics.Metric):
+
+    @inject_mesh
+    def __init__(self, name='accuracy', dtype=None):
+       super().__init__(**kwargs)
+
+    acc = Accuracy(mesh=mesh)
+    assert acc._mesh == mesh
+  ```
+
+  Args:
+    init_method: the `__init__` method of the Keras class to annotate.
+
+  Returns:
+    the annotated __init__ method.
+  """
+  def _wrap_function(instance, *args, **kwargs):
+    mesh = kwargs.pop("mesh", None)
+    # Note that the injection of _mesh need to happen before the invocation of
+    # __init__, since the class might need the mesh to create weights in the
+    # __init__.
+    if mesh is not None:
+      instance._mesh = mesh  # pylint: disable=protected-access
+    init_method(instance, *args, **kwargs)
+
+  return tf.__internal__.decorator.make_decorator(
+      target=init_method, decorator_func=_wrap_function)
+
+
 def call_with_layout(fn, layout, *args, **kwargs):
   """Invoke the function with inputs and relayout the result.
 
