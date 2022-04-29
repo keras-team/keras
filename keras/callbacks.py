bc0cf4692ab396c53e6281c171bac0fb68d3fa58
A. Unique TensorFlower <gardener@tensorflow.org>
2022-04-29 08:56:45 -0700

    Remove unnecessary overriding of default methods in the `LambdaCallback` class.
    The `LambdaCallback` class overrides methods such as `on_batch_end` with a no-op
    lambda if no corresponding argument is provided. This is unnecessary because
    the no-op lambda is identical to the default method implementation on the
    superclass.
    
    The current overriding behaviour prevents `LambdaCallback` from being used with
    parameter-server strategy distributed training. Overriding the default method
    with a no-op lambda means that `_implements_train_batch_hooks` always returns
    true (even if e.g. you provide only an `on_epoch_end` hook), so the
    `_disallow_batch_hooks_in_ps_strategy` check fails.
    
    PiperOrigin-RevId: 445431057

--- keras/callbacks.py
+++ keras/callbacks.py
@@ -2917,25 +2917,13 @@ class LambdaCallback(Callback):
     self.__dict__.update(kwargs)
     if on_epoch_begin is not None:
       self.on_epoch_begin = on_epoch_begin
-    else:
-      self.on_epoch_begin = lambda epoch, logs: None
     if on_epoch_end is not None:
       self.on_epoch_end = on_epoch_end
-    else:
-      self.on_epoch_end = lambda epoch, logs: None
     if on_batch_begin is not None:
       self.on_batch_begin = on_batch_begin
-    else:
-      self.on_batch_begin = lambda batch, logs: None
     if on_batch_end is not None:
       self.on_batch_end = on_batch_end
-    else:
-      self.on_batch_end = lambda batch, logs: None
     if on_train_begin is not None:
       self.on_train_begin = on_train_begin
-    else:
-      self.on_train_begin = lambda logs: None
     if on_train_end is not None:
       self.on_train_end = on_train_end
-    else:
-      self.on_train_end = lambda logs: None
