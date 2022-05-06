e8ee816b2fcf6cdcb48db81c946a8ffe534f125a
Sayak Paul <spsayakpaul@gmail.com>
2022-05-06 09:07:56 +0530

    feat: application of normalization for 3-channel inputs.

--- keras/applications/convnext.py
+++ keras/applications/convnext.py
@@ -402,8 +402,12 @@ def ConvNeXt(depths,
     inputs = img_input
 
   x = inputs
+  
   if include_preprocessing:
-    x = PreStem(name=model_name)(x)
+    channel_axis = 3 if backend.image_data_format() == "channels_last" else 1
+    num_channels = input_shape[channel_axis - 1]
+    if num_channels == 3:
+      x = PreStem(name=model_name)(x)
   
   # Stem block.
   stem = Sequential(
