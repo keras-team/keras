43d8454fb89f15ad41be0a76a2303ce4595a2c12
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2022-07-06 16:10:22 +0200

    removed pylint instruction

--- keras/applications/resnet.py
+++ keras/applications/resnet.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-# pylint: disable=invalid-name
 """ResNet models for Keras.
 
 Reference:
