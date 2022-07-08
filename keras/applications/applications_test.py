511977b19feef311cdce061cad62baf6b45e7f10
cyai <seriesscar@gmail.com>
2022-07-08 11:11:05 +0530

    Applications: Updated f-string format

--- keras/applications/applications_test.py
+++ keras/applications/applications_test.py
@@ -138,12 +138,12 @@ class ApplicationsTest(tf.test.TestCase, parameterized.TestCase):
     def assertShapeEqual(self, shape1, shape2):
         if len(shape1) != len(shape2):
             raise AssertionError(
-                "Shapes are different rank: %s vs %s" % (shape1, shape2)
+                f"Shapes are different rank: {shape1} vs {shape2}"
             )
         for v1, v2 in zip(shape1, shape2):
             if v1 != v2:
                 raise AssertionError(
-                    "Shapes differ: %s vs %s" % (shape1, shape2)
+                    f"Shapes differ: {shape1} vs {shape2}"
                 )
 
     @parameterized.parameters(*MODEL_LIST)
