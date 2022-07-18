bffa98320579106d90c264fb7f1f456b9949eb45
cyai <83634399+cyai@users.noreply.github.com>
2022-07-18 23:26:34 +0530

    Updated f-string method

--- keras/optimizers/optimizer_v1.py
+++ keras/optimizers/optimizer_v1.py
@@ -49,7 +49,7 @@ class Optimizer:
             # checks that clipnorm >= 0 and clipvalue >= 0
             if kwargs[k] < 0:
                 raise ValueError(
-                    "Expected {} >= 0, received: {}".format(k, kwargs[k])
+                    f"Expected {k} >= 0, received: {kwargs[k]}"
                 )
         self.__dict__.update(kwargs)
         self.updates = []
