92cf82d82d8da7927491dabcd8fb27fb526a2368
Aditya Kane <64411306+AdityaKane2001@users.noreply.github.com>
2022-07-19 17:07:50 +0530

    Update depthwise_conv1d.py

--- keras/layers/convolutional/depthwise_conv1d.py
+++ keras/layers/convolutional/depthwise_conv1d.py
@@ -95,22 +95,22 @@ class DepthwiseConv1D(DepthwiseConv):
         `keras.constraints`).
 
     Input shape:
-      4D tensor with shape: `[batch_size, channels, rows, cols]` if
-        data_format='channels_first'
-      or 4D tensor with shape: `[batch_size, rows, cols, channels]` if
+      3D tensor with shape: `[batch_size, channels, input_dims]` if
+        data_format='channels_first'.
+      or 3D tensor with shape: `[batch_size, input_dims, channels]` if
         data_format='channels_last'.
 
     Output shape:
-      4D tensor with shape: `[batch_size, channels * depth_multiplier, new_rows,
-        new_cols]` if `data_format='channels_first'`
-        or 4D tensor with shape: `[batch_size,
-        new_rows, new_cols, channels * depth_multiplier]` if
-        `data_format='channels_last'`. `rows` and `cols` values might have
+      3D tensor with shape: `[batch_size, channels * depth_multiplier, new_dims]`
+        if `data_format='channels_first'`
+        or 3D tensor with shape: `[batch_size,
+        new_dims, channels * depth_multiplier]` if
+        `data_format='channels_last'`. `new_dims` values might have
         changed due to padding.
 
     Returns:
-      A tensor of rank 4 representing
-      `activation(depthwiseconv2d(inputs, kernel) + bias)`.
+      A tensor of rank 3 representing
+      `activation(depthwiseconv1d(inputs, kernel) + bias)`.
 
     Raises:
       ValueError: if `padding` is "causal".
@@ -197,20 +197,20 @@ class DepthwiseConv1D(DepthwiseConv):
     @tf_utils.shape_type_conversion
     def compute_output_shape(self, input_shape):
         if self.data_format == "channels_first":
-            rows = input_shape[2]
+            input_dims = input_shape[2]
             out_filters = input_shape[1] * self.depth_multiplier
         elif self.data_format == "channels_last":
-            rows = input_shape[1]
+            input_dims = input_shape[1]
             out_filters = input_shape[2] * self.depth_multiplier
 
-        rows = conv_utils.conv_output_length(
-            rows,
+        input_dims = conv_utils.conv_output_length(
+            input_dims,
             self.kernel_size[0],
             self.padding,
             self.strides[0],
             self.dilation_rate[0],
         )
         if self.data_format == "channels_first":
-            return (input_shape[0], out_filters, rows)
+            return (input_shape[0], out_filters, input_dims)
         elif self.data_format == "channels_last":
-            return (input_shape[0], rows, out_filters)
+            return (input_shape[0], input_dims, out_filters)
