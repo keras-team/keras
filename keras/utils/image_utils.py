d84aca22a58fce57e37b6ce1654a95ea806f3046
Francois Chollet <fchollet@google.com>
2022-08-15 09:39:53 -0700

    Minor doc fix.
    PiperOrigin-RevId: 467690390

--- keras/utils/image_utils.py
+++ keras/utils/image_utils.py
@@ -374,7 +374,7 @@ def load_img(
 
     Usage:
 
-    ```
+    ```python
     image = tf.keras.utils.load_img(image_path)
     input_arr = tf.keras.utils.img_to_array(image)
     input_arr = np.array([input_arr])  # Convert single image to a batch.
