8ee8945025c3c6611413385126abe637b618fa83
A. Unique TensorFlower <gardener@tensorflow.org>
2022-10-02 01:32:07 -0700

    Added warmstart_embedding_matrix to tf.keras.utils
    PiperOrigin-RevId: 478299677

--- keras/utils/layer_utils.py
+++ keras/utils/layer_utils.py
@@ -23,6 +23,7 @@ import weakref
 import numpy as np
 import tensorflow.compat.v2 as tf
 
+from keras import initializers
 from keras.utils import io_utils
 
 # isort: off
@@ -809,3 +810,141 @@ class CallFunctionSpec:
                 "The first argument to `Layer.call` must always be passed."
             )
         return inputs, args, kwargs
+
+
+@keras_export("keras.utils.warmstart_embedding_matrix")
+def warmstart_embedding_matrix(
+    base_vocabulary,
+    new_vocabulary,
+    base_embeddings,
+    new_embeddings_initializer="uniform",
+):
+    """Warm start embedding matrix with changing vocab.
+
+    This util can be used to warmstart the embedding layer matrix when
+    vocabulary changes between previously saved checkpoint and model.
+    Vocabulary change could mean, the size of the new vocab is different or the
+    vocabulary is reshuffled or new vocabulary has been added to old vocabulary.
+    If the vocabulary size changes, size of the embedding layer matrix also
+    changes. This util remaps the old vocabulary embeddings to the new embedding
+    layer matrix.
+
+    Example:
+    Here is an example that demonstrates how to use the
+    `warmstart_embedding_matrix` util.
+    >>> import keras
+    >>> vocab_base = tf.convert_to_tensor(["unk", "a", "b", "c"])
+    >>> vocab_new = tf.convert_to_tensor(
+    ...        ["unk", "unk", "a", "b", "c", "d", "e"])
+    >>> vectorized_vocab_base = np.random.rand(vocab_base.shape[0], 3)
+    >>> vectorized_vocab_new = np.random.rand(vocab_new.shape[0], 3)
+    >>> warmstarted_embedding_matrix = warmstart_embedding_matrix(
+    ...       base_vocabulary=vocab_base,
+    ...       new_vocabulary=vocab_new,
+    ...       base_embeddings=vectorized_vocab_base,
+    ...       new_embeddings_initializer=keras.initializers.Constant(
+    ...         vectorized_vocab_new))
+
+    Here is an example that demonstrates how to get vocabulary and embedding
+    weights from layers, use the `warmstart_embedding_matrix` util to remap the
+    layer embeddings and continue with model training.
+    ```
+    # get old and new vocabulary by using layer.get_vocabulary()
+    # for example assume TextVectorization layer is used
+    base_vocabulary = old_text_vectorization_layer.get_vocabulary()
+    new_vocabulary = new_text_vectorization_layer.get_vocabulary()
+    # get previous embedding layer weights
+    embedding_weights_base = model.get_layer('embedding').get_weights()[0]
+    warmstarted_embedding = keras.utils.warmstart_embedding_matrix(
+                                  base_vocabulary,
+                                  new_vocabulary,
+                                  base_embeddings=embedding_weights_base,
+                                  new_embeddings_initializer="uniform")
+    updated_embedding_variable = tf.Variable(warmstarted_embedding)
+
+    # update embedding layer weights
+    model.layers[1].embeddings = updated_embedding_variable
+    model.fit(..)
+    # continue with model training
+
+    ```
+
+    Args:
+        base_vocabulary: The list of vocabulary terms that
+          the preexisting embedding matrix `base_embeddings` represents.
+          It can be either a 1D array/tensor or a tuple/list of vocabulary
+          terms (strings), or a path to a vocabulary text file. If passing a
+           file path, the file should contain one line per term in the
+           vocabulary.
+        new_vocabulary: The list of vocabulary terms for the new vocabulary
+           (same format as above).
+        base_embeddings: NumPy array or tensor representing the preexisting
+          embedding matrix.
+        new_embeddings_initializer: Initializer for embedding vectors for
+          previously unseen terms to be added to the new embedding matrix (see
+          `keras.initializers`). Defaults to "uniform". new_embedding matrix
+          needs to be specified with "constant" initializer.
+          matrix. Default value is None.
+
+    Returns:
+      tf.tensor of remapped embedding layer matrix
+
+    """
+    # convert vocab to list
+    base_vocabulary = convert_vocab_to_list(base_vocabulary)
+    new_vocabulary = convert_vocab_to_list(new_vocabulary)
+
+    # Initialize the new embedding layer matrix
+    new_embeddings_initializer = initializers.get(new_embeddings_initializer)
+    new_embedding = new_embeddings_initializer(
+        shape=(len(new_vocabulary), base_embeddings.shape[1]),
+        dtype=base_embeddings.dtype,
+    )
+
+    # create mapping dict {vocab:index}
+    base_vocabulary_dict = dict(
+        zip(base_vocabulary, range(len(base_vocabulary)))
+    )
+
+    indices_base_vocabulary = []
+    indices_new_vocabulary = []
+    for index, key in enumerate(new_vocabulary):
+        if key in base_vocabulary_dict:
+            indices_base_vocabulary.append(base_vocabulary_dict[key])
+            indices_new_vocabulary.append(int(index))
+
+    # update embedding matrix
+    values_to_update = tf.gather(base_embeddings, indices_base_vocabulary)
+    warmstarted_embedding_matrix = tf.tensor_scatter_nd_update(
+        new_embedding,
+        tf.expand_dims(indices_new_vocabulary, axis=1),
+        values_to_update,
+    )
+    return warmstarted_embedding_matrix
+
+
+def convert_vocab_to_list(vocab):
+    """Convert input vacabulary to list."""
+    vocab_list = []
+    if tf.is_tensor(vocab):
+        vocab_list = list(vocab.numpy())
+    elif isinstance(vocab, (np.ndarray, tuple, list)):
+        vocab_list = list(vocab)
+    elif isinstance(vocab, str):
+        if not tf.io.gfile.exists(vocab):
+            raise ValueError(f"Vocabulary file {vocab} does not exist.")
+        with tf.io.gfile.GFile(vocab, "r") as vocabulary_file:
+            vocab_list = vocabulary_file.read().splitlines()
+    else:
+        raise ValueError(
+            "Vocabulary is expected to be either a NumPy array, "
+            "list, 1D tensor or a vocabulary text file. Instead type "
+            f"{type(vocab)} was received."
+        )
+    if len(vocab_list) == 0:
+        raise ValueError(
+            "Vocabulary is expected to be either a NumPy array, "
+            "list, 1D tensor or a vocabulary text file with at least one token."
+            " Received 0 instead."
+        )
+    return vocab_list
