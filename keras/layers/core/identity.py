0cca530a4ab37dd54d30c6f84515e78e21f166b2
Aditya Kane <adityakane1@gmail.com>
2022-12-01 00:14:42 +0530

    updated docstring

--- keras/layers/core/identity.py
+++ keras/layers/core/identity.py
@@ -25,8 +25,8 @@ class Identity(Layer):
     """Identity layer.
 
     This layer should be used as a placeholder when no operation is to be
-    performed. The layer is argument insensitive, and returns only the
-    first positional argument as the output.
+    performed. The layer is argument insensitive, and returns its `inputs`
+    argument as output.
 
     Args:
         name: Optional name for the layer instance.
