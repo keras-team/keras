f47bfc910865543e48dff2492d041bc7d4b3fc78
MirandaTZ <thiago.zafalon.miranda@gmail.com>
2022-12-19 11:36:34 -0300

    Revert "Add verbose parameter to image_dataset_from_directory"
    This reverts commit f6688c950a1d82cd499c65152bc2a191fd8c0128.

--- keras/utils/image_dataset.py
+++ keras/utils/image_dataset.py
@@ -47,7 +47,6 @@ def image_dataset_from_directory(
     interpolation="bilinear",
     follow_links=False,
     crop_to_aspect_ratio=False,
-    verbose=1,
     **kwargs,
 ):
     """Generates a `tf.data.Dataset` from image files in a directory.
@@ -129,8 +128,6 @@ def image_dataset_from_directory(
         largest possible window in the image (of size `image_size`) that matches
         the target aspect ratio. By default (`crop_to_aspect_ratio=False`),
         aspect ratio may not be preserved.
-      verbose: 0 or 1. Verbosity mode.
-          0 = silent, 1 = print how many files and classes were found.
       **kwargs: Legacy keyword arguments.
 
     Returns:
@@ -218,7 +215,6 @@ def image_dataset_from_directory(
         shuffle=shuffle,
         seed=seed,
         follow_links=follow_links,
-        verbose=verbose,
     )
 
     if label_mode == "binary" and len(class_names) != 2:
