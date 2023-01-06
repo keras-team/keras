50542a234850ebf6128f9c61673fc4cae596ff01
Umer Javed <ujaved@google.com>
2023-01-06 01:07:11 -0800

    Develop and Implement _to_tensors for TraceTypes in FuncGraph.
    PiperOrigin-RevId: 500113409

--- keras/mixed_precision/autocast_variable.py
+++ keras/mixed_precision/autocast_variable.py
@@ -59,6 +59,9 @@ class AutoCastVariableSpec(tf.types.experimental.TraceType):
         """Use the AutoCastVariable value itself as a placeholder."""
         return self._value
 
+    def _to_tensors(self, value):
+        return []
+
     def __hash__(self) -> int:
         return hash(id(self._value))
 
