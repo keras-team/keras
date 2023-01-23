2727df09aa284a94ce8234ad1279d9659cdf2064
Francois Chollet <fchollet@google.com>
2023-01-23 09:47:49 -0800

    Add ReloadedLayer class which provides a Layer API wrapper around a SavedModel artifact.
    PiperOrigin-RevId: 504011658

--- keras/saving/export_lib.py
+++ keras/saving/export_lib.py
@@ -362,6 +362,149 @@ def export_model(model, filepath):
     export_archive.write_out(filepath)
 
 
+class ReloadedLayer(base_layer.Layer):
+    """Reload a Keras model/layer that was saved via SavedModel / ExportArchive.
+
+    Arguments:
+        filepath: `str` or `pathlib.Path` object. The path to the SavedModel.
+        call_endpoint: Name of the endpoint to use as the `call()` method
+            of the reloaded layer. If the SavedModel was created
+            via `model.export()`,
+            then the default endpoint name is `'serve'`. In other cases
+            it may be named `'serving_default'`.
+
+    Example:
+
+    ```python
+    model.export("path/to/artifact")
+    reloaded_layer = ReloadedLayer("path/to/artifact")
+    outputs = reloaded_layer(inputs)
+    ```
+
+    The reloaded object can be used like a regular Keras layer, and supports
+    training/fine-tuning of its trainable weights. Note that the reloaded
+    object retains none of the internal structure or custom methods of the
+    original object -- it's a brand new layer created around the saved
+    function.
+
+    **Limitations:**
+
+    * Only call endpoints with a single `inputs` tensor argument
+    (which may optionally be a dict/tuple/list of tensors) are supported.
+    For endpoints with multiple separate input tensor arguments, consider
+    subclassing `ReloadedLayer` and implementing a `call()` method with a
+    custom signature.
+    * If you need training-time behavior to differ from inference-time behavior
+    (i.e. if you need the reloaded object to support a `training=True` argument
+    in `__call__()`), make sure that the training-time call function is
+    saved as a standalone endpoint in the artifact, and provide its name
+    to the `ReloadedLayer` via the `call_training_endpoint` argument.
+    """
+
+    def __init__(
+        self,
+        filepath,
+        call_endpoint="serve",
+        call_training_endpoint=None,
+        trainable=True,
+        name=None,
+        dtype=None,
+    ):
+        # Initialize an empty layer, then add_weight() etc. as needed.
+        super().__init__(trainable=trainable, name=name, dtype=dtype)
+
+        self._reloaded_obj = tf.saved_model.load(filepath)
+
+        self.filepath = filepath
+        self.call_endpoint = call_endpoint
+        self.call_training_endpoint = call_training_endpoint
+
+        # Resolve the call function.
+        if hasattr(self._reloaded_obj, call_endpoint):
+            # Case 1: it's set as an attribute.
+            self.call_endpoint_fn = getattr(self._reloaded_obj, call_endpoint)
+        elif call_endpoint in self._reloaded_obj.signatures:
+            # Case 2: it's listed in the `signatures` field.
+            self.call_endpoint_fn = self._reloaded_obj.signatures[call_endpoint]
+        else:
+            raise ValueError(
+                f"The endpoint '{call_endpoint}' is neither an "
+                "attribute of the reloaded SavedModel, nor an entry "
+                "in the `signatures` field of the reloaded SavedModel. "
+            )
+
+        # Resolving the training function.
+        if call_training_endpoint:
+            if hasattr(self._reloaded_obj, call_training_endpoint):
+                self.call_training_endpoint_fn = getattr(
+                    self._reloaded_obj, call_training_endpoint
+                )
+            elif call_training_endpoint in self._reloaded_obj.signatures:
+                self.call_training_endpoint_fn = self._reloaded_obj.signatures[
+                    call_training_endpoint
+                ]
+            else:
+                raise ValueError(
+                    f"The endpoint '{call_training_endpoint}' is "
+                    "neither an attribute of the reloaded SavedModel, "
+                    "nor an entry in the `signatures` field of "
+                    "the reloaded SavedModel. "
+                )
+
+        # Add trainable and non-trainable weights from the call_endpoint_fn.
+        all_fns = [self.call_endpoint_fn]
+        if call_training_endpoint:
+            all_fns.append(self.call_training_endpoint_fn)
+        trainable_variables_ids = set()
+        non_trainable_variables_ids = set()
+        for fn in all_fns:
+            # The function may or may not be already a concrete function
+            if hasattr(fn, "concrete_functions"):
+                concrete_functions = fn.concrete_functions
+            else:
+                concrete_functions = [fn]
+            for concrete_fn in concrete_functions:
+                for v in concrete_fn.trainable_variables:
+                    if id(v) not in trainable_variables_ids:
+                        self._add_existing_weight(v, trainable=True)
+                        trainable_variables_ids.add(id(v))
+
+                for v in concrete_fn.variables:
+                    if (
+                        id(v) not in trainable_variables_ids
+                        and id(v) not in non_trainable_variables_ids
+                    ):
+                        self._add_existing_weight(v, trainable=False)
+                        non_trainable_variables_ids.add(id(v))
+        self.built = True
+
+    def _add_existing_weight(self, weight, trainable):
+        """Calls add_weight() to register but not create an existing weight."""
+        self.add_weight(
+            name=weight.name,
+            shape=weight.shape,
+            dtype=weight.dtype,
+            trainable=trainable,
+            getter=lambda *_, **__: weight,
+        )
+
+    def call(self, inputs, training=False, **kwargs):
+        if training:
+            if self.call_training_endpoint:
+                return self.call_training_endpoint_fn(inputs, **kwargs)
+        return self.call_endpoint_fn(inputs, **kwargs)
+
+    def get_config(self):
+        base_config = super().get_config()
+        config = {
+            # Note: this is not intended to be portable.
+            "filepath": self.filepath,
+            "call_endpoint": self.call_endpoint,
+            "call_training_endpoint": self.call_training_endpoint,
+        }
+        return {**base_config, **config}
+
+
 def _make_tensor_spec(x):
     return tf.TensorSpec(x.shape, dtype=x.dtype)
 
--- keras/saving/export_lib_test.py
+++ keras/saving/export_lib_test.py
@@ -15,6 +15,7 @@
 """Tests for inference-only model/layer exporting utilities."""
 import os
 
+import numpy as np
 import tensorflow.compat.v2 as tf
 from absl.testing import parameterized
 
@@ -24,21 +25,22 @@ from keras.testing_infra import test_combinations
 from keras.testing_infra import test_utils
 
 
+def get_model():
+    layers = [
+        keras.layers.Dense(10, activation="relu"),
+        keras.layers.BatchNormalization(),
+        keras.layers.Dense(1, activation="sigmoid"),
+    ]
+    model = test_utils.get_model_from_layers(layers, input_shape=(10,))
+    return model
+
+
 @test_utils.run_v2_only
 class ExportArchiveTest(tf.test.TestCase, parameterized.TestCase):
-    def _get_model(self):
-        layers = [
-            keras.layers.Dense(10, activation="relu"),
-            keras.layers.BatchNormalization(),
-            keras.layers.Dense(1, activation="sigmoid"),
-        ]
-        model = test_utils.get_model_from_layers(layers, input_shape=(10,))
-        return model
-
     @test_combinations.run_with_all_model_types
     def test_standard_model_export(self):
         temp_filepath = os.path.join(self.get_temp_dir(), "exported_model")
-        model = self._get_model()
+        model = get_model()
         ref_input = tf.random.normal((3, 10))
         ref_output = model(ref_input).numpy()
 
@@ -52,7 +54,7 @@ class ExportArchiveTest(tf.test.TestCase, parameterized.TestCase):
     def test_low_level_model_export(self):
         temp_filepath = os.path.join(self.get_temp_dir(), "exported_model")
 
-        model = self._get_model()
+        model = get_model()
         ref_input = tf.random.normal((3, 10))
         ref_output = model(ref_input).numpy()
 
@@ -371,7 +373,7 @@ class ExportArchiveTest(tf.test.TestCase, parameterized.TestCase):
     @test_combinations.run_with_all_model_types
     def test_model_export_method(self):
         temp_filepath = os.path.join(self.get_temp_dir(), "exported_model")
-        model = self._get_model()
+        model = get_model()
         ref_input = tf.random.normal((3, 10))
         ref_output = model(ref_input).numpy()
 
@@ -380,6 +382,159 @@ class ExportArchiveTest(tf.test.TestCase, parameterized.TestCase):
         self.assertAllClose(
             ref_output, revived_model.serve(ref_input).numpy(), atol=1e-6
         )
+
+
+@test_utils.run_v2_only
+class TestReloadedLayer(tf.test.TestCase, parameterized.TestCase):
+    @test_combinations.run_with_all_model_types
+    def test_reloading_export_archive(self):
+        temp_filepath = os.path.join(self.get_temp_dir(), "exported_model")
+        model = get_model()
+        ref_input = tf.random.normal((3, 10))
+        ref_output = model(ref_input).numpy()
+
+        export_lib.export_model(model, temp_filepath)
+        reloaded_layer = export_lib.ReloadedLayer(temp_filepath)
+        self.assertAllClose(
+            reloaded_layer(ref_input).numpy(), ref_output, atol=1e-7
+        )
+        self.assertLen(reloaded_layer.weights, len(model.weights))
+        self.assertLen(
+            reloaded_layer.trainable_weights, len(model.trainable_weights)
+        )
+        self.assertLen(
+            reloaded_layer.non_trainable_weights,
+            len(model.non_trainable_weights),
+        )
+
+        # Test fine-tuning
+        new_model = keras.Sequential([reloaded_layer])
+        new_model.compile(optimizer="rmsprop", loss="mse")
+        x = tf.random.normal((32, 10))
+        y = tf.random.normal((32, 1))
+        new_model.train_on_batch(x, y)
+        new_output = reloaded_layer(ref_input).numpy()
+        self.assertNotAllClose(new_output, ref_output, atol=1e-5)
+
+        # Test that trainable can be set to False
+        reloaded_layer.trainable = False
+        new_model.compile(optimizer="rmsprop", loss="mse")
+        x = tf.random.normal((32, 10))
+        y = tf.random.normal((32, 1))
+        new_model.train_on_batch(x, y)
+        # The output must not have changed
+        self.assertAllClose(
+            reloaded_layer(ref_input).numpy(), new_output, atol=1e-7
+        )
+
+    @test_combinations.run_with_all_model_types
+    def test_reloading_default_saved_model(self):
+        temp_filepath = os.path.join(self.get_temp_dir(), "exported_model")
+        model = get_model()
+        ref_input = tf.random.normal((3, 10))
+        ref_output = model(ref_input).numpy()
+
+        tf.saved_model.save(model, temp_filepath)
+        reloaded_layer = export_lib.ReloadedLayer(
+            temp_filepath, call_endpoint="serving_default"
+        )
+        # The output is a dict, due to the nature of SavedModel saving.
+        new_output = reloaded_layer(ref_input)
+        self.assertAllClose(
+            new_output[list(new_output.keys())[0]].numpy(),
+            ref_output,
+            atol=1e-7,
+        )
+        self.assertLen(reloaded_layer.weights, len(model.weights))
+        self.assertLen(
+            reloaded_layer.trainable_weights, len(model.trainable_weights)
+        )
+        self.assertLen(
+            reloaded_layer.non_trainable_weights,
+            len(model.non_trainable_weights),
+        )
+
+    def test_call_training(self):
+        temp_filepath = os.path.join(self.get_temp_dir(), "exported_model")
+        keras.utils.set_random_seed(1337)
+        model = keras.Sequential(
+            [
+                keras.Input((10,)),
+                keras.layers.Dense(10),
+                keras.layers.Dropout(0.99999),
+            ]
+        )
+        export_archive = export_lib.ExportArchive()
+        export_archive.track(model)
+        export_archive.add_endpoint(
+            name="call_inference",
+            fn=lambda x: model(x, training=False),
+            input_signature=[tf.TensorSpec(shape=(None, 10), dtype=tf.float32)],
+        )
+        export_archive.add_endpoint(
+            name="call_training",
+            fn=lambda x: model(x, training=True),
+            input_signature=[tf.TensorSpec(shape=(None, 10), dtype=tf.float32)],
+        )
+        export_archive.write_out(temp_filepath)
+        reloaded_layer = export_lib.ReloadedLayer(
+            temp_filepath,
+            call_endpoint="call_inference",
+            call_training_endpoint="call_training",
+        )
+        inference_output = reloaded_layer(
+            tf.random.normal((1, 10)), training=False
+        )
+        training_output = reloaded_layer(
+            tf.random.normal((1, 10)), training=True
+        )
+        self.assertAllClose(np.mean(training_output), 0.0, atol=1e-7)
+        self.assertNotAllClose(np.mean(inference_output), 0.0, atol=1e-7)
+
+    @test_combinations.run_with_all_model_types
+    def test_serialization(self):
+        temp_filepath = os.path.join(self.get_temp_dir(), "exported_model")
+        model = get_model()
+        ref_input = tf.random.normal((3, 10))
+        ref_output = model(ref_input).numpy()
+
+        export_lib.export_model(model, temp_filepath)
+        reloaded_layer = export_lib.ReloadedLayer(temp_filepath)
+
+        # Test reinstantiation from config
+        config = reloaded_layer.get_config()
+        rereloaded_layer = export_lib.ReloadedLayer.from_config(config)
+        self.assertAllClose(
+            rereloaded_layer(ref_input).numpy(), ref_output, atol=1e-7
+        )
+
+        # Test whole model saving with reloaded layer inside
+        model = keras.Sequential([reloaded_layer])
+        temp_model_filepath = os.path.join(self.get_temp_dir(), "m.keras")
+        model.save(temp_model_filepath, save_format="keras_v3")
+        reloaded_model = keras.models.load_model(
+            temp_model_filepath,
+            custom_objects={"ReloadedLayer": export_lib.ReloadedLayer},
+        )
+        self.assertAllClose(
+            reloaded_model(ref_input).numpy(), ref_output, atol=1e-7
+        )
+
+    def test_errors(self):
+        # Test missing call endpoint
+        temp_filepath = os.path.join(self.get_temp_dir(), "exported_model")
+        model = keras.Sequential([keras.Input((2,)), keras.layers.Dense(3)])
+        export_lib.export_model(model, temp_filepath)
+        with self.assertRaisesRegex(ValueError, "The endpoint 'wrong'"):
+            export_lib.ReloadedLayer(temp_filepath, call_endpoint="wrong")
+
+        # Test missing call training endpoint
+        with self.assertRaisesRegex(ValueError, "The endpoint 'wrong'"):
+            export_lib.ReloadedLayer(
+                temp_filepath,
+                call_endpoint="serve",
+                call_training_endpoint="wrong",
+            )
 
 
 if __name__ == "__main__":
