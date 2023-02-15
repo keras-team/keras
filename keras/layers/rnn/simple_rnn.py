33b9f86bac9676a98265d46988eb36cf063ec329
A. Unique TensorFlower <gardener@tensorflow.org>
2023-02-15 09:45:32 -0800

    PR #17465: keras.models.clone_model creates different weight names for RNN layers
    Imported from GitHub PR https://github.com/keras-team/keras/pull/17465
    
    This pull request fixes: #17433
    
    It's the easiest solution I came up with.
    
    Another solution would be to add `cell_name` argument to all RNN layers, but that changes the API.
    
    I'm not sure if 'cell' should be used as name, or if we should stick to 'simple_rnn_cell', 'lstm_cell', 'gru_cell' instead.
    
    It also works with saving models AND also when loading old saved models. As in this case, the RNN cell's name is stored in the saved model, so it will correctly be loaded.
    Copybara import of the project:
    
    --
    5f64c727037faf8f9d43a16139cc9f869270b018 by Nicolas Weber <nicolas.weber@neclab.eu>:
    
    added name='cell' solution
    
    --
    914c142e2e84b7607c5c46c84b204fadd4cbdf0a by Nicolas Weber <nicolas.weber@neclab.eu>:
    
    added testcases
    
    --
    b7c47868923a8b3a5ed8e7d2aa0110b23ab79203 by Nicolas Weber <nicolas.weber@neclab.eu>:
    
    removed debugging print
    
    --
    41fe49bfaaace3bc482bac7a2598c5db8d4385c0 by Nicolas Weber <nicolas.weber@neclab.eu>:
    
    formatted, renamed 'cell' to '{layer}_cell', changed to self.assertEqual using lists
    
    --
    98c7bede0aa77d51f844d82a08f148f61a98881c by Nicolas Weber <nicolas.weber@neclab.eu>:
    
    fixed conv_lstm_cell name. Added gru_v1 cell name
    
    --
    839ee9414b8de67c791bf48bd424172bdc6234e4 by Nicolas Weber <nicolas.weber@neclab.eu>:
    
    formatting
    
    Merging this change closes #17465
    
    FUTURE_COPYBARA_INTEGRATE_REVIEW=https://github.com/keras-team/keras/pull/17465 from mergian:dev_rnn_cell_names c3e004b886036d8dd78a73eec6e8acfa5e75bdb7
    PiperOrigin-RevId: 509859548

--- keras/layers/rnn/simple_rnn.py
+++ keras/layers/rnn/simple_rnn.py
@@ -392,6 +392,7 @@ class SimpleRNN(RNN):
             recurrent_dropout=recurrent_dropout,
             dtype=kwargs.get("dtype"),
             trainable=kwargs.get("trainable", True),
+            name="simple_rnn_cell",
             **cell_kwargs,
         )
         super().__init__(
