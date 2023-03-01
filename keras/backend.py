0ae0eada96d3ed70f7ee753d8c9c0148fb7154fd
joe <joesho112358@gmail.com>
2023-02-28 21:09:15 -0500

    fixing lint issue

--- keras/backend.py
+++ keras/backend.py
@@ -737,7 +737,9 @@ def _get_session(op_input_list=()):
         # graph, create and cache a new session.
         if getattr(
             _SESSION, "session", None
-        ) is None or _SESSION.session.graph is not _current_graph(op_input_list):
+        ) is None or _SESSION.session.graph is not _current_graph(
+            op_input_list
+        ):
             # If we are creating the Session inside a tf.distribute.Strategy
             # scope, we ask the strategy for the right session options to use.
             if tf.distribute.has_strategy():
