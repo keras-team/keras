29d31783742a7b9539946ddf4c41f54ec7ce0ea7
Bc. Martin Kubovčík <74611856+markub3327@users.noreply.github.com>
2023-03-28 21:44:06 +0200

    Update spectral_normalization.py

--- keras/layers/normalization/spectral_normalization.py
+++ keras/layers/normalization/spectral_normalization.py
@@ -84,7 +84,7 @@ class SpectralNormalization(Wrapper):
         self.kernel_shape = self.kernel.shape.as_list()
 
         self.vector_u = self.add_weight(
-            shape=(1, self.self.kernel_shape[-1]),
+            shape=(1, self.kernel_shape[-1]),
             initializer=TruncatedNormal(stddev=0.02),
             trainable=False,
             name="vector_u",
@@ -110,7 +110,7 @@ class SpectralNormalization(Wrapper):
         spectral normalized value, so that the layer is ready for `call()`.
         """
 
-        weights = tf.reshape(self.kernel, [-1, self.self.kernel_shape[-1]])
+        weights = tf.reshape(self.kernel, [-1, self.kernel_shape[-1]])
         vector_u = self.vector_u
 
         # check for zeroes weights
@@ -128,7 +128,7 @@ class SpectralNormalization(Wrapper):
             self.vector_u.assign(tf.cast(vector_u, self.vector_u.dtype))
             self.kernel.assign(
                 tf.cast(
-                    tf.reshape(self.kernel / sigma, self.self.kernel_shape),
+                    tf.reshape(self.kernel / sigma, self.kernel_shape),
                     self.kernel.dtype,
                 )
             )
