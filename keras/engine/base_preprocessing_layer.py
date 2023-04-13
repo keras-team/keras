9ad7371a082a3df70c3b1e1e999cbb8d749d2417
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2023-04-12 20:36:21 -0400

    [keras/engine/base_layer.py,keras/engine/base_layer_utils.py,keras/engine/base_layer_v1.py,keras/engine/base_preprocessing_layer.py,keras/engine/data_adapter.py,keras/engine/functional.py,keras/engine/input_layer.py,keras/engine/training.py,keras/engine/training_v1.py] Standardise docstring usage of "Default to"

--- keras/engine/base_preprocessing_layer.py
+++ keras/engine/base_preprocessing_layer.py
@@ -140,14 +140,14 @@ class PreprocessingLayer(Layer, metaclass=abc.ABCMeta):
         """Configures the layer for `adapt`.
 
         Arguments:
-          run_eagerly: Bool. Defaults to `False`. If `True`, this `Model`'s
+          run_eagerly: Bool. If `True`, this `Model`'s
             logic will not be wrapped in a `tf.function`. Recommended to leave
             this as `None` unless your `Model` cannot be run inside a
-            `tf.function`.
-          steps_per_execution: Int. Defaults to 1. The number of batches to run
+            `tf.function`. Defaults to `False`.
+          steps_per_execution: Int. The number of batches to run
             during each `tf.function` call. Running multiple batches inside a
             single `tf.function` call can greatly improve performance on TPUs or
-            small models with a large Python overhead.
+            small models with a large Python overhead. Defaults to `1`.
         """
         if steps_per_execution is None:
             steps_per_execution = 1
