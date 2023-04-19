83f5a25a4bb596571388f62f2fd5258fbf4675d2
Francois Chollet <francois.chollet@gmail.com>
2023-04-18 21:47:41 -0700

    Add weight constraints API.

---
+++ keras_core/constraints/__init__.py
@@ -0,0 +1,5 @@
+from keras_core.constraints.constraints import Constraint
+from keras_core.constraints.constraints import MaxNorm
+from keras_core.constraints.constraints import MinMaxNorm
+from keras_core.constraints.constraints import NonNeg
+from keras_core.constraints.constraints import UnitNorm
