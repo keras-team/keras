46449dca70b6cdc164097e073c6b6359a8dba47d
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2023-04-19 22:43:03 -0400

    [keras/layers/activation/relu.py] Use backticks for defaults in docstrings

--- keras/layers/activation/relu.py
+++ keras/layers/activation/relu.py
@@ -65,10 +65,10 @@ class ReLU(Layer):
       Same shape as the input.
 
     Args:
-      max_value: Float >= 0. Maximum activation value. None means unlimited.
+      max_value: Float >= `0.`. Maximum activation value. `None` means unlimited.
         Defaults to `None`.
-      negative_slope: Float >= 0. Negative slope coefficient. Defaults to `0.`.
-      threshold: Float >= 0. Threshold value for thresholded activation.
+      negative_slope: Float >= `0.`. Negative slope coefficient. Defaults to `0.`.
+      threshold: Float >= `0.`. Threshold value for thresholded activation.
         Defaults to `0.`.
     """
 
