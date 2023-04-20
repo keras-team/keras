6d271fd7914cfc92cdcc267aa6e1ee42fce7c3f2
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2023-04-19 22:41:02 -0400

    [keras/datasets/reuters.py] Use backticks for defaults in docstrings

--- keras/datasets/reuters.py
+++ keras/datasets/reuters.py
@@ -68,14 +68,14 @@ def load_data(
           all words are kept. Defaults to `None`.
       skip_top: skip the top N most frequently occurring words
           (which may not be informative). These words will appear as
-          `oov_char` value in the dataset. 0 means no words are
-          skipped. Defaults to 0
+          `oov_char` value in the dataset. `0` means no words are
+          skipped. Defaults to `0`.
       maxlen: int or None. Maximum sequence length.
-          Any longer sequence will be truncated. None means no truncation.
+          Any longer sequence will be truncated. `None` means no truncation.
           Defaults to `None`.
       test_split: Float between 0 and 1. Fraction of the dataset to be used
         as test data. 0.2 means that 20% of the dataset is used as
-        test data. Defaults to 0.2
+        test data. Defaults to `0.2`.
       seed: int. Seed for reproducible data shuffling.
       start_char: int. The start of a sequence will be marked with this
           character. 0 is usually the padding character. Defaults to `1`.
