fe0c8eedf7e9a2bc8ed5994a90c6131f1bbba48f
Francois Chollet <francois.chollet@gmail.com>
2023-04-21 13:59:07 -0700

    Merge branch 'main' of github.com:keras-team/keras-core

--- keras_core/losses/losses.py
+++ keras_core/losses/losses.py
@@ -1,5 +1,6 @@
 from keras_core import backend
 from keras_core import operations as ops
+from keras_core.api_export import keras_core_export
 from keras_core.losses.loss import Loss
 from keras_core.losses.loss import squeeze_to_same_rank
 
@@ -24,6 +25,7 @@ class LossFunctionWrapper(Loss):
         raise NotImplementedError
 
 
+@keras_core_export("keras_core.losses.MeanSquaredError")
 class MeanSquaredError(LossFunctionWrapper):
     """Computes the mean of squares of errors between labels and predictions.
 
@@ -46,6 +48,7 @@ class MeanSquaredError(LossFunctionWrapper):
         super().__init__(mean_squared_error, reduction=reduction, name=name)
 
 
+@keras_core_export("keras_core.losses.MeanAbsoluteError")
 class MeanAbsoluteError(LossFunctionWrapper):
     """Computes the mean of absolute difference between labels and predictions.
 
@@ -68,6 +71,7 @@ class MeanAbsoluteError(LossFunctionWrapper):
         super().__init__(mean_absolute_error, reduction=reduction, name=name)
 
 
+@keras_core_export("keras_core.losses.MeanAbsolutePercentageError")
 class MeanAbsolutePercentageError(LossFunctionWrapper):
     """Computes the mean absolute percentage error between `y_true` & `y_pred`.
 
@@ -94,6 +98,7 @@ class MeanAbsolutePercentageError(LossFunctionWrapper):
         )
 
 
+@keras_core_export("keras_core.losses.MeanSquaredLogarithmicError")
 class MeanSquaredLogarithmicError(LossFunctionWrapper):
     """Computes the mean squared logarithmic error between `y_true` & `y_pred`.
 
@@ -120,6 +125,10 @@ class MeanSquaredLogarithmicError(LossFunctionWrapper):
         )
 
 
+@keras_core_export(
+    "keras_core.metrics.mean_squared_error",
+    "keras_core.losses.mean_squared_error",
+)
 def mean_squared_error(y_true, y_pred):
     """Computes the mean squared error between labels and predictions.
 
@@ -148,6 +157,10 @@ def mean_squared_error(y_true, y_pred):
     return ops.mean(ops.square(y_true - y_pred), axis=-1)
 
 
+@keras_core_export(
+    "keras_core.metrics.mean_absolute_error",
+    "keras_core.losses.mean_absolute_error",
+)
 def mean_absolute_error(y_true, y_pred):
     """Computes the mean absolute error between labels and predictions.
 
@@ -174,6 +187,10 @@ def mean_absolute_error(y_true, y_pred):
     return ops.mean(ops.abs(y_true - y_pred), axis=-1)
 
 
+@keras_core_export(
+    "keras_core.metrics.mean_absolute_percentage_error",
+    "keras_core.losses.mean_absolute_percentage_error",
+)
 def mean_absolute_percentage_error(y_true, y_pred):
     """Computes the mean absolute percentage error between `y_true` & `y_pred`.
 
@@ -205,6 +222,10 @@ def mean_absolute_percentage_error(y_true, y_pred):
     return 100.0 * ops.mean(diff, axis=-1)
 
 
+@keras_core_export(
+    "keras_core.metrics.mean_squared_logarithmic_error",
+    "keras_core.losses.mean_squared_logarithmic_error",
+)
 def mean_squared_logarithmic_error(y_true, y_pred):
     """Computes the mean squared logarithmic error between `y_true` & `y_pred`.
 
--- keras_core/metrics/regression_metrics.py
+++ keras_core/metrics/regression_metrics.py
@@ -1,13 +1,26 @@
 from keras_core import operations as ops
+from keras_core.api_export import keras_core_export
+from keras_core.losses import losses
+from keras_core.losses.losses import mean_squared_error
 from keras_core.metrics import reduction_metrics
+from keras_core.metrics.metric import Metric
 
 
-def mean_squared_error(y_true, y_pred):
-    ndim = len(y_pred.shape)
-    return ops.mean((y_true - y_pred) ** 2, axis=list(range(1, ndim)))
-
-
+@keras_core_export("keras_core.metrics.MeanSquaredError")
 class MeanSquaredError(reduction_metrics.MeanMetricWrapper):
+    """Computes the mean squared error between `y_true` and `y_pred`.
+
+    Args:
+        name: (Optional) string name of the metric instance.
+        dtype: (Optional) data type of the metric result.
+
+    Standalone usage:
+
+    >>> m = keras_core.metrics.MeanSquaredError()
+    >>> m.update_state([[0, 1], [0, 0]], [[1, 1], [0, 0]])
+    >>> m.result()
+    0.25
+    """
     def __init__(self, name="mean_squared_error", dtype=None):
         super().__init__(fn=mean_squared_error, name=name, dtype=dtype)
 
