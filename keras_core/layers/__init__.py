c690e1b5a68d86d3c748cf20261d75084808d86c
Francois Chollet <francois.chollet@gmail.com>
2023-04-22 19:03:24 -0700

    Move activation layer to layers/activations/

--- keras_core/layers/__init__.py
+++ keras_core/layers/__init__.py
@@ -1,4 +1,4 @@
-from keras_core.layers.core.activation import Activation
+from keras_core.layers.activations.activation import Activation
 from keras_core.layers.core.dense import Dense
 from keras_core.layers.core.input_layer import Input
 from keras_core.layers.core.input_layer import InputLayer
