038d7bb200c0722bbffdfe14ea5d72353b63096e
Haifeng Jin <5476582+haifeng-jin@users.noreply.github.com>
2023-04-27 16:47:34 -0700

    Add the EarlyStopping callback (#44)
    * add earlystopping callback
    
    * addressing comments
    
    * address comments
    
    * addressing comments
    
    * remove unused imports

--- keras_core/utils/numerical_utils.py
+++ keras_core/utils/numerical_utils.py
@@ -1,5 +1,8 @@
+import numpy as np
+
 from keras_core import backend
 from keras_core import operations as ops
+from keras_core.api_export import keras_core_export
 
 
 def l2_normalize(x, axis=0):
@@ -7,3 +10,61 @@ def l2_normalize(x, axis=0):
     square_sum = ops.sum(ops.square(x), axis=axis, keepdims=True)
     l2_norm = ops.reciprocal(ops.sqrt(ops.maximum(square_sum, epsilon)))
     return ops.multiply(x, l2_norm)
+
+
+@keras_core_export("keras_core.utils.to_categorical")
+def to_categorical(x, num_classes=None):
+    """Converts a class vector (integers) to binary class matrix.
+
+    E.g. for use with `categorical_crossentropy`.
+
+    Args:
+        x: Array-like with class values to be converted into a matrix
+            (integers from 0 to `num_classes - 1`).
+        num_classes: Total number of classes. If `None`, this would be inferred
+            as `max(x) + 1`. Defaults to `None`.
+
+    Returns:
+        A binary matrix representation of the input as a NumPy array. The class
+        axis is placed last.
+
+    Example:
+
+    >>> a = keras_core.utils.to_categorical([0, 1, 2, 3], num_classes=4)
+    >>> print(a)
+    [[1. 0. 0. 0.]
+     [0. 1. 0. 0.]
+     [0. 0. 1. 0.]
+     [0. 0. 0. 1.]]
+
+    >>> b = np.array([.9, .04, .03, .03,
+    ...               .3, .45, .15, .13,
+    ...               .04, .01, .94, .05,
+    ...               .12, .21, .5, .17],
+    ...               shape=[4, 4])
+    >>> loss = keras_core.backend.categorical_crossentropy(a, b)
+    >>> print(np.around(loss, 5))
+    [0.10536 0.82807 0.1011  1.77196]
+
+    >>> loss = keras_core.backend.categorical_crossentropy(a, a)
+    >>> print(np.around(loss, 5))
+    [0. 0. 0. 0.]
+    """
+    if backend.is_tensor(x):
+        return backend.nn.one_hot(x, num_classes)
+    x = np.array(x, dtype="int64")
+    input_shape = x.shape
+
+    # Shrink the last dimension if the shape is (..., 1).
+    if input_shape and input_shape[-1] == 1 and len(input_shape) > 1:
+        input_shape = tuple(input_shape[:-1])
+
+    x = x.reshape(-1)
+    if not num_classes:
+        num_classes = np.max(x) + 1
+    batch_size = x.shape[0]
+    categorical = np.zeros((batch_size, num_classes))
+    categorical[np.arange(batch_size), x] = 1
+    output_shape = input_shape + (num_classes,)
+    categorical = np.reshape(categorical, output_shape)
+    return categorical
---
+++ keras_core/utils/numerical_utils_test.py
@@ -0,0 +1,54 @@
+import numpy as np
+from absl.testing import parameterized
+
+from keras_core import backend
+from keras_core import testing
+from keras_core.utils import numerical_utils
+
+NUM_CLASSES = 5
+
+
+class TestNumericalUtils(testing.TestCase, parameterized.TestCase):
+    @parameterized.parameters(
+        [
+            ((1,), (1, NUM_CLASSES)),
+            ((3,), (3, NUM_CLASSES)),
+            ((4, 3), (4, 3, NUM_CLASSES)),
+            ((5, 4, 3), (5, 4, 3, NUM_CLASSES)),
+            ((3, 1), (3, NUM_CLASSES)),
+            ((3, 2, 1), (3, 2, NUM_CLASSES)),
+        ]
+    )
+    def test_to_categorical(self, shape, expected_shape):
+        label = np.random.randint(0, NUM_CLASSES, shape)
+        one_hot = numerical_utils.to_categorical(label, NUM_CLASSES)
+        # Check shape
+        self.assertEqual(one_hot.shape, expected_shape)
+        # Make sure there is only one 1 in a row
+        self.assertTrue(np.all(one_hot.sum(axis=-1) == 1))
+        # Get original labels back from one hots
+        self.assertTrue(
+            np.all(np.argmax(one_hot, -1).reshape(label.shape) == label)
+        )
+
+    def test_to_categorial_without_num_classes(self):
+        label = [0, 2, 5]
+        one_hot = numerical_utils.to_categorical(label)
+        self.assertEqual(one_hot.shape, (3, 5 + 1))
+
+    def test_to_categorical_with_backend_tensor(self):
+        label = backend.convert_to_tensor(np.array([0, 2, 1, 3, 4]))
+        expected = backend.convert_to_tensor(
+            np.array(
+                [
+                    [1, 0, 0, 0, 0],
+                    [0, 0, 1, 0, 0],
+                    [0, 1, 0, 0, 0],
+                    [0, 0, 0, 1, 0],
+                    [0, 0, 0, 0, 1],
+                ]
+            )
+        )
+        one_hot = numerical_utils.to_categorical(label, NUM_CLASSES)
+        assert backend.is_tensor(one_hot)
+        self.assertAllClose(one_hot, expected)
