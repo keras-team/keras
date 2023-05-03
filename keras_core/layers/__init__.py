a85647b3ec2841acdc461141c6e2ce0fa94b33ab
Chen Qian <chenmoney@google.com>
2023-05-02 22:40:42 -0700

    Implement global pooling layer (#74)
    * Add max and poolig layer
    
    * fix tests
    
    * handle TF transpose
    
    * renaming
    
    * initial
    
    * add something
    
    * rename tests
    
    * more
    
    * add docstring
    
    * add mask for global average pooling 1d

--- keras_core/layers/__init__.py
+++ keras_core/layers/__init__.py
@@ -19,6 +19,18 @@ from keras_core.layers.merging.subtract import subtract
 from keras_core.layers.pooling.average_pooling1d import AveragePooling1D
 from keras_core.layers.pooling.average_pooling2d import AveragePooling2D
 from keras_core.layers.pooling.average_pooling3d import AveragePooling3D
+from keras_core.layers.pooling.global_average_pooling1d import (
+    GlobalAveragePooling1D,
+)
+from keras_core.layers.pooling.global_average_pooling2d import (
+    GlobalAveragePooling2D,
+)
+from keras_core.layers.pooling.global_average_pooling3d import (
+    GlobalAveragePooling3D,
+)
+from keras_core.layers.pooling.global_max_pooling1d import GlobalMaxPooling1D
+from keras_core.layers.pooling.global_max_pooling2d import GlobalMaxPooling2D
+from keras_core.layers.pooling.global_max_pooling3d import GlobalMaxPooling3D
 from keras_core.layers.pooling.max_pooling1d import MaxPooling1D
 from keras_core.layers.pooling.max_pooling2d import MaxPooling2D
 from keras_core.layers.pooling.max_pooling3d import MaxPooling3D
---
+++ keras_core/layers/pooling/global_max_pooling_test.py
@@ -0,0 +1,151 @@
+import numpy as np
+import tensorflow as tf
+from absl.testing import parameterized
+
+from keras_core import layers
+from keras_core import testing
+
+
+class GlobalMaxPoolingBasicTest(testing.TestCase, parameterized.TestCase):
+    @parameterized.parameters(
+        ("channels_last", False, (3, 5, 4), (3, 4)),
+        ("channels_last", True, (3, 5, 4), (3, 1, 4)),
+        ("channels_first", False, (3, 5, 4), (3, 5)),
+    )
+    def test_global_max_pooling1d(
+        self,
+        data_format,
+        keepdims,
+        input_shape,
+        output_shape,
+    ):
+        self.run_layer_test(
+            layers.GlobalMaxPooling1D,
+            init_kwargs={
+                "data_format": data_format,
+                "keepdims": keepdims,
+            },
+            input_shape=input_shape,
+            expected_output_shape=output_shape,
+            expected_num_trainable_weights=0,
+            expected_num_non_trainable_weights=0,
+            expected_num_losses=0,
+            supports_masking=False,
+        )
+
+    @parameterized.parameters(
+        ("channels_last", False, (3, 5, 6, 4), (3, 4)),
+        ("channels_last", True, (3, 5, 6, 4), (3, 1, 1, 4)),
+        ("channels_first", False, (3, 5, 6, 4), (3, 5)),
+    )
+    def test_global_max_pooling2d(
+        self,
+        data_format,
+        keepdims,
+        input_shape,
+        output_shape,
+    ):
+        self.run_layer_test(
+            layers.GlobalMaxPooling2D,
+            init_kwargs={
+                "data_format": data_format,
+                "keepdims": keepdims,
+            },
+            input_shape=input_shape,
+            expected_output_shape=output_shape,
+            expected_num_trainable_weights=0,
+            expected_num_non_trainable_weights=0,
+            expected_num_losses=0,
+            supports_masking=False,
+        )
+
+    @parameterized.parameters(
+        ("channels_last", False, (3, 5, 6, 5, 4), (3, 4)),
+        ("channels_last", True, (3, 5, 6, 5, 4), (3, 1, 1, 1, 4)),
+        ("channels_first", False, (3, 5, 6, 5, 4), (3, 5)),
+    )
+    def test_global_max_pooling3d(
+        self,
+        data_format,
+        keepdims,
+        input_shape,
+        output_shape,
+    ):
+        self.run_layer_test(
+            layers.GlobalMaxPooling3D,
+            init_kwargs={
+                "data_format": data_format,
+                "keepdims": keepdims,
+            },
+            input_shape=input_shape,
+            expected_output_shape=output_shape,
+            expected_num_trainable_weights=0,
+            expected_num_non_trainable_weights=0,
+            expected_num_losses=0,
+            supports_masking=False,
+        )
+
+
+class GlobalMaxPoolingCorrectnessTest(testing.TestCase, parameterized.TestCase):
+    @parameterized.parameters(
+        ("channels_last", False),
+        ("channels_last", True),
+        ("channels_first", False),
+    )
+    def test_global_max_pooling1d(self, data_format, keepdims):
+        inputs = np.arange(24, dtype=np.float).reshape((2, 3, 4))
+
+        layer = layers.GlobalMaxPooling1D(
+            data_format=data_format,
+            keepdims=keepdims,
+        )
+        tf_keras_layer = tf.keras.layers.GlobalMaxPooling1D(
+            data_format=data_format,
+            keepdims=keepdims,
+        )
+
+        outputs = layer(inputs)
+        expected = tf_keras_layer(inputs)
+        self.assertAllClose(outputs, expected)
+
+    @parameterized.parameters(
+        ("channels_last", False),
+        ("channels_last", True),
+        ("channels_first", False),
+    )
+    def test_global_max_pooling2d(self, data_format, keepdims):
+        inputs = np.arange(96, dtype=np.float).reshape((2, 3, 4, 4))
+
+        layer = layers.GlobalMaxPooling2D(
+            data_format=data_format,
+            keepdims=keepdims,
+        )
+        tf_keras_layer = tf.keras.layers.GlobalMaxPooling2D(
+            data_format=data_format,
+            keepdims=keepdims,
+        )
+
+        outputs = layer(inputs)
+        expected = tf_keras_layer(inputs)
+        self.assertAllClose(outputs, expected)
+
+    @parameterized.parameters(
+        ("channels_last", False),
+        ("channels_last", True),
+        ("channels_first", False),
+    )
+    def test_global_max_pooling3d(self, data_format, keepdims):
+        inputs = np.arange(360, dtype=np.float).reshape((2, 3, 3, 5, 4))
+
+        layer = layers.GlobalMaxPooling3D(
+            data_format=data_format,
+            keepdims=keepdims,
+        )
+        tf_keras_layer = tf.keras.layers.GlobalMaxPooling3D(
+            data_format=data_format,
+            keepdims=keepdims,
+        )
+
+        outputs = layer(inputs)
+        expected = tf_keras_layer(inputs)
+        self.assertAllClose(outputs, expected)
