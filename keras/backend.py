Fix for Keras `Softmax` layer gradient underflow. https://github.com/tensorflow/tensorflow/issues/60314
The `tf.keras.activations.softmax` function, the `tf.keras.backend.softmax` function and the `tf.keras.layers.Softmax` layer now behave consistently and save the logits in `_keras_logits`. Previously, only the activation function had this behavior. This prevents the computation of the gradient of the crossentropy from underflowing.

The same fix was applied to the `tf.keras.backend.sigmoid` function and the `tf.keras.layers.Sigmoid` layer.

One behavior change is that `tf.keras.backend.softmax` and `tf.keras.layers.Softmax` no longer accept inputs of rank 1.

PiperOrigin-RevId: 529219773

keras/backend.py
@@ -5441,7 +5441,22 @@ def softmax(x, axis=-1):
     Returns:
         A tensor.
     """
-    return tf.nn.softmax(x, axis=axis)
+    if x.shape.rank <= 1:
+        raise ValueError(
+            f"Cannot apply softmax to a tensor that is 1D. Received input: {x}"
+        )
+
+    if isinstance(axis, int):
+        output = tf.nn.softmax(x, axis=axis)
+    else:
+        # nn.softmax does not support tuple axis.
+        numerator = tf.exp(x - tf.reduce_max(x, axis=axis, keepdims=True))
+        denominator = tf.reduce_sum(numerator, axis=axis, keepdims=True)
+        output = numerator / denominator
+
+    # Cache the logits to use for crossentropy loss.
+    output._keras_logits = x
+    return output
 
 
 @keras_export("keras.backend.softplus")
@@ -5899,7 +5914,10 @@ def sigmoid(x):
     Returns:
         A tensor.
     """
-    return tf.math.sigmoid(x)
+    output = tf.sigmoid(x)
+    # Cache the logits to use for crossentropy loss.
+    output._keras_logits = x
+    return output
 
 
 @keras_export("keras.backend.hard_sigmoid")