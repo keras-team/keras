3bf3c24aa8cb0a3637377fe8a7a9587104c33e71
hertschuh <1091026+hertschuh@users.noreply.github.com>
2023-05-09 12:55:46 -0700

    Add `keras_core.layers.Permute`. (#120)

---
+++ keras_core/layers/reshaping/permute.py
@@ -0,0 +1,53 @@
+from keras_core import operations as ops
+from keras_core.layers.input_spec import InputSpec
+from keras_core.layers.layer import Layer
+
+
+class Permute(Layer):
+    """Permutes the dimensions of the input according to a given pattern.
+
+    Useful e.g. connecting RNNs and convnets.
+
+    Args:
+        dims: Tuple of integers. Permutation pattern does not include the
+            batch dimension. Indexing starts at 1.
+            For instance, `(2, 1)` permutes the first and second dimensions
+            of the input.
+    Input shape:
+        Arbitrary.
+    Output shape:
+        Same as the input shape, but with the dimensions re-ordered according
+        to the specified pattern.
+
+    Example:
+
+    >>> x = keras_core.Input(shape=(10, 64))
+    >>> y = keras_core.layers.Permute((2, 1))(x)
+    >>> y.shape
+    (None, 64, 10)
+    """
+
+    def __init__(self, dims, name=None, dtype=None):
+        super().__init__(name=name, dtype=dtype)
+        self.dims = tuple(dims)
+        if sorted(dims) != list(range(1, len(dims) + 1)):
+            raise ValueError(
+                "Invalid permutation argument `dims` for Permute Layer. "
+                "The set of indices in `dims` must be consecutive and start "
+                f"from 1. Received dims={dims}"
+            )
+        self.input_spec = InputSpec(ndim=len(self.dims) + 1)
+
+    def compute_output_shape(self, input_shape):
+        output_shape = [input_shape[0]]
+        for dim in self.dims:
+            output_shape.append(input_shape[dim])
+        return tuple(output_shape)
+
+    def call(self, inputs):
+        return ops.transpose(inputs, axes=(0,) + self.dims)
+
+    def get_config(self):
+        config = {"dims": self.dims}
+        base_config = super().get_config()
+        return {**base_config, **config}
---
+++ keras_core/layers/reshaping/permute_test.py
@@ -0,0 +1,50 @@
+import numpy as np
+import pytest
+
+from keras_core import backend
+from keras_core import layers
+from keras_core import operations as ops
+from keras_core import testing
+
+
+class PermuteTest(testing.TestCase):
+    def test_permute(self):
+        inputs = np.random.random((2, 3, 5)).astype("float32")
+        expected_output = ops.convert_to_tensor(
+            np.transpose(inputs, axes=(0, 2, 1))
+        )
+        self.run_layer_test(
+            layers.Permute,
+            init_kwargs={"dims": (2, 1)},
+            input_data=inputs,
+            expected_output=expected_output,
+        )
+
+    @pytest.mark.skipif(
+        not backend.DYNAMIC_SHAPES_OK,
+        reason="Backend does not support dynamic shapes",
+    )
+    def test_permute_with_dynamic_batch_size(self):
+        input_layer = layers.Input(batch_shape=(None, 3, 5))
+        permuted = layers.Permute((2, 1))(input_layer)
+        self.assertEqual(permuted.shape, (None, 5, 3))
+
+    def test_permute_errors_on_invalid_starting_dims_index(self):
+        with self.assertRaisesRegex(
+            ValueError, r"Invalid permutation .*dims.*"
+        ):
+            self.run_layer_test(
+                layers.Permute,
+                init_kwargs={"dims": (0, 1, 2)},
+                input_shape=(3, 2, 4),
+            )
+
+    def test_permute_errors_on_invalid_set_of_dims_indices(self):
+        with self.assertRaisesRegex(
+            ValueError, r"Invalid permutation .*dims.*"
+        ):
+            self.run_layer_test(
+                layers.Permute,
+                init_kwargs={"dims": (1, 4, 2)},
+                input_shape=(3, 2, 4),
+            )
