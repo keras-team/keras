e1f850da825c2565e557644857fbb23f2f0510b2
Francois Chollet <francois.chollet@gmail.com>
2023-05-13 20:17:42 -0700

    Add examples folder

---
+++ examples/demo_functional.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+from keras_core import Model
+from keras_core import layers
+from keras_core import losses
+from keras_core import metrics
+from keras_core import optimizers
+
+inputs = layers.Input((100,), batch_size=32)
+x = layers.Dense(256, activation="relu")(inputs)
+x = layers.Dense(256, activation="relu")(x)
+x = layers.Dense(256, activation="relu")(x)
+outputs = layers.Dense(16)(x)
+model = Model(inputs, outputs)
+
+model.summary()
+
+x = np.random.random((50000, 100))
+y = np.random.random((50000, 16))
+batch_size = 32
+epochs = 5
+
+model.compile(
+    optimizer=optimizers.SGD(learning_rate=0.001),
+    loss=losses.MeanSquaredError(),
+    metrics=[metrics.MeanSquaredError()],
+)
+history = model.fit(
+    x, y, batch_size=batch_size, epochs=epochs, validation_split=0.2
+)
+
+print("History:")
+print(history.history)
