Fix RNN masking

keras_core/backend/jax/rnn.py
@@ -51,7 +51,7 @@ def rnn(
         rank_diff = len(input_t.shape) - len(mask_t.shape)
         for _ in range(rank_diff):
             mask_t = jnp.expand_dims(mask_t, -1)
-        multiples = [1] * fixed_dim + input_t.shape[fixed_dim:]
+        multiples = [1] * fixed_dim + list(input_t.shape[fixed_dim:])
         return jnp.tile(mask_t, multiples)
 
     if unroll:
@@ -147,14 +147,26 @@ def rnn(
 
             def _step(states, current_input):
                 current_input, current_mask = current_input
-                is_masked = jnp.all(jnp.logical_not(current_mask))
+                is_masked = jnp.all(
+                    jnp.logical_not(current_mask), axis=-1, keepdims=True
+                )
 
                 output_t, new_states = step_function(current_input, states)
-                return lax.cond(
-                    is_masked,
-                    lambda: (states, current_input),
-                    lambda: (new_states, output_t),
-                )
+
+                if zero_output_for_mask:
+                    masked_outs = jnp.where(
+                        is_masked, jnp.zeros_like(output_t), output_t
+                    )
+                else:
+                    # Assume the first state is the previous output.
+                    output_tm1 = states[0]
+                    masked_outs = jnp.where(is_masked, output_tm1, output_t)
+
+                new_states = [
+                    jnp.where(is_masked, s, ns)
+                    for s, ns in zip(states, new_states)
+                ]
+                return (new_states, masked_outs)
 
             scan_xs = (inputs, mask)
 
@@ -175,18 +187,6 @@ def rnn(
         if go_backwards:
             outputs = jnp.flip(outputs, axis=0)
         last_output = outputs[-1]
-
-    if zero_output_for_mask:
-        last_output = jnp.where(
-            _expand_mask(mask_list[-1], last_output),
-            last_output,
-            jnp.zeros_like(last_output),
-        )
-        outputs = jnp.where(
-            _expand_mask(mask, outputs, fixed_dim=2),
-            outputs,
-            jnp.zeros_like(outputs),
-        )
 
     if not time_major:
         outputs = nest.map_structure(swap_batch_timestep, outputs)

keras_core/layers/rnn/simple_rnn_test.py
@@ -139,4 +139,142 @@ class SimpleRNNTest(testing.TestCase):
             output,
         )
 
-    # TODO: test masking
+    def test_pass_initial_state(self):
+        sequence = np.arange(24).reshape((2, 4, 3)).astype("float32")
+        initial_state = np.arange(8).reshape((2, 4)).astype("float32")
+        layer = layers.SimpleRNN(
+            4,
+            kernel_initializer=initializers.Constant(0.01),
+            recurrent_initializer=initializers.Constant(0.02),
+            bias_initializer=initializers.Constant(0.03),
+        )
+        output = layer(sequence, initial_state=initial_state)
+        self.assertAllClose(
+            np.array(
+                [
+                    [0.33621645, 0.33621645, 0.33621645, 0.33621645],
+                    [0.6262637, 0.6262637, 0.6262637, 0.6262637],
+                ]
+            ),
+            output,
+        )
+
+        layer = layers.SimpleRNN(
+            4,
+            kernel_initializer=initializers.Constant(0.01),
+            recurrent_initializer=initializers.Constant(0.02),
+            bias_initializer=initializers.Constant(0.03),
+            go_backwards=True,
+        )
+        output = layer(sequence, initial_state=initial_state)
+        self.assertAllClose(
+            np.array(
+                [
+                    [0.07344437, 0.07344437, 0.07344437, 0.07344437],
+                    [0.43043602, 0.43043602, 0.43043602, 0.43043602],
+                ]
+            ),
+            output,
+        )
+
+    def test_masking(self):
+        sequence = np.arange(24).reshape((2, 4, 3)).astype("float32")
+        mask = np.array([[True, True, False, True], [True, False, False, True]])
+        layer = layers.SimpleRNN(
+            4,
+            kernel_initializer=initializers.Constant(0.01),
+            recurrent_initializer=initializers.Constant(0.02),
+            bias_initializer=initializers.Constant(0.03),
+            unroll=True,
+        )
+        output = layer(sequence, mask=mask)
+        self.assertAllClose(
+            np.array(
+                [
+                    [0.32951632, 0.32951632, 0.32951632, 0.32951632],
+                    [0.61799484, 0.61799484, 0.61799484, 0.61799484],
+                ]
+            ),
+            output,
+        )
+
+        layer = layers.SimpleRNN(
+            2,
+            kernel_initializer=initializers.Constant(0.01),
+            recurrent_initializer=initializers.Constant(0.02),
+            bias_initializer=initializers.Constant(0.03),
+            return_sequences=True,
+        )
+        output = layer(sequence, mask=mask)
+        self.assertAllClose(
+            np.array(
+                [
+                    [0.0599281, 0.0599281],
+                    [0.15122814, 0.15122814],
+                    [0.15122814, 0.15122814],
+                    [0.32394567, 0.32394567],
+                ],
+            ),
+            output[0],
+        )
+        self.assertAllClose(
+            np.array(
+                [
+                    [0.3969304, 0.3969304],
+                    [0.3969304, 0.3969304],
+                    [0.3969304, 0.3969304],
+                    [0.608085, 0.608085],
+                ],
+            ),
+            output[1],
+        )
+
+        layer = layers.SimpleRNN(
+            2,
+            kernel_initializer=initializers.Constant(0.01),
+            recurrent_initializer=initializers.Constant(0.02),
+            bias_initializer=initializers.Constant(0.03),
+            return_sequences=True,
+            zero_output_for_mask=True,
+        )
+        output = layer(sequence, mask=mask)
+        self.assertAllClose(
+            np.array(
+                [
+                    [0.0599281, 0.0599281],
+                    [0.15122814, 0.15122814],
+                    [0.0, 0.0],
+                    [0.32394567, 0.32394567],
+                ],
+            ),
+            output[0],
+        )
+        self.assertAllClose(
+            np.array(
+                [
+                    [0.3969304, 0.3969304],
+                    [0.0, 0.0],
+                    [0.0, 0.0],
+                    [0.608085, 0.608085],
+                ],
+            ),
+            output[1],
+        )
+
+        layer = layers.SimpleRNN(
+            4,
+            kernel_initializer=initializers.Constant(0.01),
+            recurrent_initializer=initializers.Constant(0.02),
+            bias_initializer=initializers.Constant(0.03),
+            go_backwards=True,
+        )
+        output = layer(sequence, mask=mask)
+        self.assertAllClose(
+            np.array(
+                [
+                    [0.07376196, 0.07376196, 0.07376196, 0.07376196],
+                    [0.43645123, 0.43645123, 0.43645123, 0.43645123],
+                ]
+            ),
+            output,
+        )