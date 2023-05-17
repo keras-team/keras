b781ef7e531186d575a9df9d6377ed22dc96fa30
Francois Chollet <francois.chollet@gmail.com>
2023-05-17 16:06:25 -0700

    Merge branch 'main' of github.com:keras-team/keras-core

--- keras_core/optimizers/rmsprop.py
+++ keras_core/optimizers/rmsprop.py
@@ -18,8 +18,10 @@ class RMSprop(optimizer.Optimizer):
     gradients, and uses that average to estimate the variance.
 
     Args:
-        learning_rate: Initial value for the learning rate: a floating point
-            value, defaults to 0.001.
+        learning_rate: A float, a
+            `keras_core.optimizers.schedules.LearningRateSchedule` instance, or
+            a callable that takes no arguments and returns the actual value to
+            use. The learning rate. Defaults to 0.001.
         rho: float, defaults to 0.9. Discounting factor for the old gradients.
         momentum: float, defaults to 0.0. If not 0.0., the optimizer tracks the
             momentum value, with a decay rate equals to `1 - momentum`.
