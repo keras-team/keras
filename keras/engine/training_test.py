PR #18105: [Add Feature] - Warn user if softmax usage is wrong
Imported from GitHub PR https://github.com/keras-team/keras/pull/18105

This is a utility function to check if the usage of softmax makes sense (new users make this mistake a lot). Applying softmax on a single neuron will make the model output ones everytime, there are too many Stackoverflow posts about this.

In order to see this in action, please check [the gist](https://colab.research.google.com/gist/Frightera/fdcec020fff6ee9521ae2fd3eaed774d/checksoftmaxlastlayer.ipynb).

This applies for any other layers (Conv2D etc.) where the applied axis (axis=-1 default) of softmax has only one unit.
Copybara import of the project:

--
90c95b1c9b1472d660de362b6da4d5f20673c1ee by Kaan Bıçakcı <46622558+Frightera@users.noreply.github.com>:

Add last layer activation check for softmax

--
1cedb20ee96ac89b9e4067d79d6d5f3fbe12d9ce by Kaan Bıçakcı <46622558+Frightera@users.noreply.github.com>:

Split logic for sequential and functional models

--
529f9685bb8bcb5605c965130538b03d92402032 by Kaan Bıçakcı <46622558+Frightera@users.noreply.github.com>:

Add tests for _check_last_layer_activation

--
d1acddb7592b6e730d0f689efff72b8dacf400f8 by Kaan Bıçakcı <46622558+Frightera@users.noreply.github.com>:

Update sequential check

--
836301676de8620186900a715bf4c250c54b19ad by Kaan Bıçakcı <46622558+Frightera@users.noreply.github.com>:

Update tests, logic and reformatting

--
ebf16c3ce512cd040ee4ae1b96235910d70f2ae0 by Kaan Bıçakcı <46622558+Frightera@users.noreply.github.com>:

Update tests and the logic

--
afc156a09f992ac43b5194f6de8513d9a7c244e1 by Kaan Bıçakcı <46622558+Frightera@users.noreply.github.com>:

Make validate_softmax_activation experimental

Merging this change closes #18105

FUTURE_COPYBARA_INTEGRATE_REVIEW=https://github.com/keras-team/keras/pull/18105 from Frightera:last_layer_softmax_warn afc156a09f992ac43b5194f6de8513d9a7c244e1
PiperOrigin-RevId: 533267082

keras/engine/training_test.py
@@ -14,11 +14,11 @@
 # ==============================================================================
 """Tests for training routines."""
 
-
 import collections
 import io
 import sys
 import tempfile
+import warnings
 
 import numpy as np
 import tensorflow.compat.v2 as tf
@@ -4998,6 +4998,81 @@ class TestVariableObjectPathMapping(test_combinations.TestCase):
         self.assertEqual(
             mapping.keys(), {"d1.kernel", "d1.bias", "d2.kernel", "d2.bias"}
         )
+
+
+class TestCheckLastLayerActivation(test_combinations.TestCase):
+    def test_sequential_model_output(self):
+
+        for activation in ["softmax", tf.nn.softmax, layers_module.Softmax()]:
+            model = sequential.Sequential(
+                [
+                    layers_module.InputLayer(input_shape=(10,)),
+                    layers_module.Dense(1, activation=activation),
+                ]
+            )
+            with warnings.catch_warnings(record=True) as w:
+                warnings.simplefilter("always")
+                model.compile()
+                self.assertIs(w[-1].category, SyntaxWarning)
+                self.assertIn(
+                    "Found a layer with softmax activation and single unit "
+                    "output",
+                    str(w[-1].message),
+                )
+            del model
+
+    def test_functional_model_output(self):
+        inputs = input_layer.Input(shape=(10,))
+        for activation in ["softmax", tf.nn.softmax, layers_module.Softmax()]:
+            x = layers_module.Dense(1, activation=activation)(inputs)
+            model = training_module.Model(inputs, x)
+            with warnings.catch_warnings(record=True) as w:
+                warnings.simplefilter("always")
+                model.compile()
+                self.assertIs(w[-1].category, SyntaxWarning)
+                self.assertIn(
+                    "Found a layer with softmax activation and single unit "
+                    "output",
+                    str(w[-1].message),
+                )
+            del model
+
+    def test_multi_output_model(self):
+        inputs = input_layer.Input(shape=(10,))
+        for activation in ["softmax", tf.nn.softmax, layers_module.Softmax()]:
+            x = layers_module.Dense(1, activation=activation)(inputs)
+            y = layers_module.Dense(1, activation=activation)(inputs)
+            model = training_module.Model(inputs, [x, y])
+            with warnings.catch_warnings(record=True) as w:
+                warnings.simplefilter("always")
+                model.compile()
+                self.assertIs(w[-1].category, SyntaxWarning)
+                self.assertIn(
+                    "Found a layer with softmax activation and single unit "
+                    "output",
+                    str(w[-1].message),
+                )
+            del model
+
+    def test_multi_input_output_model(self):
+        inputs = [
+            input_layer.Input(shape=(10,)),
+            input_layer.Input(shape=(10,)),
+        ]
+        for activation in ["softmax", tf.nn.softmax, layers_module.Softmax()]:
+            x = layers_module.Dense(1, activation=activation)(inputs[0])
+            y = layers_module.Dense(1, activation=activation)(inputs[1])
+            model = training_module.Model(inputs, [x, y])
+            with warnings.catch_warnings(record=True) as w:
+                warnings.simplefilter("always")
+                model.compile()
+                self.assertIs(w[-1].category, SyntaxWarning)
+                self.assertIn(
+                    "Found a layer with softmax activation and single unit "
+                    "output",
+                    str(w[-1].message),
+                )
+            del model
 
 
 def _is_oss():