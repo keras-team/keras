934ca21d941f98dd8f3475e0baa744b950b5331f
Francois Chollet <francois.chollet@gmail.com>
2023-05-22 12:01:53 -0700

    Add get_source_inputs and tests

--- keras_core/operations/operation_utils.py
+++ keras_core/operations/operation_utils.py
@@ -1,6 +1,7 @@
 import math
 
 import numpy as np
+from tensorflow import nest
 
 
 def compute_pooling_output_shape(
@@ -181,3 +182,37 @@ def reduce_shape(shape, axis=None, keepdims=False):
             shape[ax] = -1
         output_shape = list(filter((-1).__ne__, shape))
         return output_shape
+
+
+def get_source_inputs(tensor):
+    """Returns the list of input tensors necessary to compute `tensor`.
+
+    Output will always be a list of tensors
+    (potentially with 1 element).
+
+    Args:
+        tensor: The tensor to start from.
+
+    Returns:
+        List of input tensors.
+    """
+    if not hasattr(tensor, "_keras_history"):
+        return tensor
+
+    operation, node_index, _ = tensor._keras_history
+    if not operation or not operation._inbound_nodes:
+        return [tensor]
+    else:
+        node = operation._inbound_nodes[node_index]
+        if node.is_input:
+            # Reached input node, stop recursion.
+            return nest.flatten(node.input_tensors)
+        else:
+            source_tensors = []
+            for tensor in node.input_tensors:
+                previous_sources = get_source_inputs(tensor)
+                # Avoid input redundancy.
+                for x in previous_sources:
+                    if all(x is not t for t in source_tensors):
+                        source_tensors.append(x)
+            return source_tensors
---
+++ keras_core/operations/operation_utils_test.py
@@ -0,0 +1,14 @@
+from keras_core import backend
+from keras_core import operations as ops
+from keras_core import testing
+from keras_core.operations import operation_utils
+
+
+class OperationUtilsTest(testing.TestCase):
+    def test_get_source_inputs(self):
+        x1 = backend.KerasTensor(shape=(2,))
+        x2 = backend.KerasTensor(shape=(2,))
+        x = x1 + x2
+        x += 2
+        x = ops.square(x)
+        self.assertEqual(operation_utils.get_source_inputs(x), [x1, x2])
