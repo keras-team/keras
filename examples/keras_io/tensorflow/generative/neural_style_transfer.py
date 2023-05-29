16c9a0af224e0bec4e14c89963d86251216a89a0
Francois Chollet <francois.chollet@gmail.com>
2023-05-29 13:04:55 -0700

    Add more examples

---
+++ examples/keras_io/tensorflow/generative/neural_style_transfer.py
@@ -0,0 +1,285 @@
+"""
+Title: Neural style transfer
+Author: [fchollet](https://twitter.com/fchollet)
+Date created: 2016/01/11
+Last modified: 2020/05/02
+Description: Transferring the style of a reference image to target image using gradient descent.
+Accelerator: GPU
+"""
+
+"""
+## Introduction
+
+Style transfer consists in generating an image
+with the same "content" as a base image, but with the
+"style" of a different picture (typically artistic).
+This is achieved through the optimization of a loss function
+that has 3 components: "style loss", "content loss",
+and "total variation loss":
+
+- The total variation loss imposes local spatial continuity between
+the pixels of the combination image, giving it visual coherence.
+- The style loss is where the deep learning keeps in --that one is defined
+using a deep convolutional neural network. Precisely, it consists in a sum of
+L2 distances between the Gram matrices of the representations of
+the base image and the style reference image, extracted from
+different layers of a convnet (trained on ImageNet). The general idea
+is to capture color/texture information at different spatial
+scales (fairly large scales --defined by the depth of the layer considered).
+- The content loss is a L2 distance between the features of the base
+image (extracted from a deep layer) and the features of the combination image,
+keeping the generated image close enough to the original one.
+
+**Reference:** [A Neural Algorithm of Artistic Style](
+  http://arxiv.org/abs/1508.06576)
+"""
+
+"""
+## Setup
+"""
+
+import numpy as np
+import tensorflow as tf
+import keras_core as keras
+from keras_core.applications import vgg19
+
+base_image_path = keras.utils.get_file(
+    "paris.jpg", "https://i.imgur.com/F28w3Ac.jpg"
+)
+style_reference_image_path = keras.utils.get_file(
+    "starry_night.jpg", "https://i.imgur.com/9ooB60I.jpg"
+)
+result_prefix = "paris_generated"
+
+# Weights of the different loss components
+total_variation_weight = 1e-6
+style_weight = 1e-6
+content_weight = 2.5e-8
+
+# Dimensions of the generated picture.
+width, height = keras.utils.load_img(base_image_path).size
+img_nrows = 400
+img_ncols = int(width * img_nrows / height)
+
+"""
+## Let's take a look at our base (content) image and our style reference image
+"""
+
+from IPython.display import Image, display
+
+display(Image(base_image_path))
+display(Image(style_reference_image_path))
+
+"""
+## Image preprocessing / deprocessing utilities
+"""
+
+
+def preprocess_image(image_path):
+    # Util function to open, resize and format pictures into appropriate tensors
+    img = keras.utils.load_img(image_path, target_size=(img_nrows, img_ncols))
+    img = keras.utils.img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+    img = vgg19.preprocess_input(img)
+    return tf.convert_to_tensor(img)
+
+
+def deprocess_image(x):
+    # Util function to convert a tensor into a valid image
+    x = x.reshape((img_nrows, img_ncols, 3))
+    # Remove zero-center by mean pixel
+    x[:, :, 0] += 103.939
+    x[:, :, 1] += 116.779
+    x[:, :, 2] += 123.68
+    # 'BGR'->'RGB'
+    x = x[:, :, ::-1]
+    x = np.clip(x, 0, 255).astype("uint8")
+    return x
+
+
+"""
+## Compute the style transfer loss
+
+First, we need to define 4 utility functions:
+
+- `gram_matrix` (used to compute the style loss)
+- The `style_loss` function, which keeps the generated image close to the local textures
+of the style reference image
+- The `content_loss` function, which keeps the high-level representation of the
+generated image close to that of the base image
+- The `total_variation_loss` function, a regularization loss which keeps the generated
+image locally-coherent
+"""
+
+# The gram matrix of an image tensor (feature-wise outer product)
+
+
+def gram_matrix(x):
+    x = tf.transpose(x, (2, 0, 1))
+    features = tf.reshape(x, (tf.shape(x)[0], -1))
+    gram = tf.matmul(features, tf.transpose(features))
+    return gram
+
+
+# The "style loss" is designed to maintain
+# the style of the reference image in the generated image.
+# It is based on the gram matrices (which capture style) of
+# feature maps from the style reference image
+# and from the generated image
+
+
+def style_loss(style, combination):
+    S = gram_matrix(style)
+    C = gram_matrix(combination)
+    channels = 3
+    size = img_nrows * img_ncols
+    return tf.reduce_sum(tf.square(S - C)) / (
+        4.0 * (channels**2) * (size**2)
+    )
+
+
+# An auxiliary loss function
+# designed to maintain the "content" of the
+# base image in the generated image
+
+
+def content_loss(base, combination):
+    return tf.reduce_sum(tf.square(combination - base))
+
+
+# The 3rd loss function, total variation loss,
+# designed to keep the generated image locally coherent
+
+
+def total_variation_loss(x):
+    a = tf.square(
+        x[:, : img_nrows - 1, : img_ncols - 1, :] - x[:, 1:, : img_ncols - 1, :]
+    )
+    b = tf.square(
+        x[:, : img_nrows - 1, : img_ncols - 1, :] - x[:, : img_nrows - 1, 1:, :]
+    )
+    return tf.reduce_sum(tf.pow(a + b, 1.25))
+
+
+"""
+Next, let's create a feature extraction model that retrieves the intermediate activations
+of VGG19 (as a dict, by name).
+"""
+
+# Build a VGG19 model loaded with pre-trained ImageNet weights
+model = vgg19.VGG19(weights="imagenet", include_top=False)
+
+# Get the symbolic outputs of each "key" layer (we gave them unique names).
+outputs_dict = dict([(layer.name, layer.output) for layer in model.layers])
+
+# Set up a model that returns the activation values for every layer in
+# VGG19 (as a dict).
+feature_extractor = keras.Model(inputs=model.inputs, outputs=outputs_dict)
+
+"""
+Finally, here's the code that computes the style transfer loss.
+"""
+
+# List of layers to use for the style loss.
+style_layer_names = [
+    "block1_conv1",
+    "block2_conv1",
+    "block3_conv1",
+    "block4_conv1",
+    "block5_conv1",
+]
+# The layer to use for the content loss.
+content_layer_name = "block5_conv2"
+
+
+def compute_loss(combination_image, base_image, style_reference_image):
+    input_tensor = tf.concat(
+        [base_image, style_reference_image, combination_image], axis=0
+    )
+    features = feature_extractor(input_tensor)
+
+    # Initialize the loss
+    loss = tf.zeros(shape=())
+
+    # Add content loss
+    layer_features = features[content_layer_name]
+    base_image_features = layer_features[0, :, :, :]
+    combination_features = layer_features[2, :, :, :]
+    loss = loss + content_weight * content_loss(
+        base_image_features, combination_features
+    )
+    # Add style loss
+    for layer_name in style_layer_names:
+        layer_features = features[layer_name]
+        style_reference_features = layer_features[1, :, :, :]
+        combination_features = layer_features[2, :, :, :]
+        sl = style_loss(style_reference_features, combination_features)
+        loss += (style_weight / len(style_layer_names)) * sl
+
+    # Add total variation loss
+    loss += total_variation_weight * total_variation_loss(combination_image)
+    return loss
+
+
+"""
+## Add a tf.function decorator to loss & gradient computation
+
+To compile it, and thus make it fast.
+"""
+
+
+@tf.function
+def compute_loss_and_grads(
+    combination_image, base_image, style_reference_image
+):
+    with tf.GradientTape() as tape:
+        loss = compute_loss(
+            combination_image, base_image, style_reference_image
+        )
+    grads = tape.gradient(loss, combination_image)
+    return loss, grads
+
+
+"""
+## The training loop
+
+Repeatedly run vanilla gradient descent steps to minimize the loss, and save the
+resulting image every 100 iterations.
+
+We decay the learning rate by 0.96 every 100 steps.
+"""
+
+optimizer = keras.optimizers.SGD(
+    keras.optimizers.schedules.ExponentialDecay(
+        initial_learning_rate=100.0, decay_steps=100, decay_rate=0.96
+    )
+)
+
+base_image = preprocess_image(base_image_path)
+style_reference_image = preprocess_image(style_reference_image_path)
+combination_image = tf.Variable(preprocess_image(base_image_path))
+
+iterations = 4000
+for i in range(1, iterations + 1):
+    loss, grads = compute_loss_and_grads(
+        combination_image, base_image, style_reference_image
+    )
+    optimizer.apply_gradients([(grads, combination_image)])
+    if i % 100 == 0:
+        print("Iteration %d: loss=%.2f" % (i, loss))
+        img = deprocess_image(combination_image.numpy())
+        fname = result_prefix + "_at_iteration_%d.png" % i
+        keras.utils.save_img(fname, img)
+
+"""
+After 4000 iterations, you get the following result:
+"""
+
+display(Image(result_prefix + "_at_iteration_4000.png"))
+
+"""
+**Example available on HuggingFace**
+Trained Model | Demo 
+--- | --- 
+[![Generic badge](https://img.shields.io/badge/%F0%9F%A4%97%20Model-Neural%20style%20transfer-black.svg)](https://huggingface.co/keras-io/VGG19) | [![Generic badge](https://img.shields.io/badge/%F0%9F%A4%97%20Spaces-Neural%20style%20transfer-black.svg)](https://huggingface.co/spaces/keras-io/neural-style-transfer)
+"""
---
+++ examples/keras_io/tensorflow/structured_data/structured_data_classification_from_scratch.py
@@ -0,0 +1,351 @@
+"""
+Title: Structured data classification from scratch
+Author: [fchollet](https://twitter.com/fchollet)
+Date created: 2020/06/09
+Last modified: 2020/06/09
+Description: Binary classification of structured data including numerical and categorical features.
+Accelerator: GPU
+"""
+"""
+## Introduction
+
+This example demonstrates how to do structured data classification, starting from a raw
+CSV file. Our data includes both numerical and categorical features. We will use Keras
+preprocessing layers to normalize the numerical features and vectorize the categorical
+ones.
+
+Note that this example should be run with TensorFlow 2.5 or higher.
+
+### The dataset
+
+[Our dataset](https://archive.ics.uci.edu/ml/datasets/heart+Disease) is provided by the
+Cleveland Clinic Foundation for Heart Disease.
+It's a CSV file with 303 rows. Each row contains information about a patient (a
+**sample**), and each column describes an attribute of the patient (a **feature**). We
+use the features to predict whether a patient has a heart disease (**binary
+classification**).
+
+Here's the description of each feature:
+
+Column| Description| Feature Type
+------------|--------------------|----------------------
+Age | Age in years | Numerical
+Sex | (1 = male; 0 = female) | Categorical
+CP | Chest pain type (0, 1, 2, 3, 4) | Categorical
+Trestbpd | Resting blood pressure (in mm Hg on admission) | Numerical
+Chol | Serum cholesterol in mg/dl | Numerical
+FBS | fasting blood sugar in 120 mg/dl (1 = true; 0 = false) | Categorical
+RestECG | Resting electrocardiogram results (0, 1, 2) | Categorical
+Thalach | Maximum heart rate achieved | Numerical
+Exang | Exercise induced angina (1 = yes; 0 = no) | Categorical
+Oldpeak | ST depression induced by exercise relative to rest | Numerical
+Slope | Slope of the peak exercise ST segment | Numerical
+CA | Number of major vessels (0-3) colored by fluoroscopy | Both numerical & categorical
+Thal | 3 = normal; 6 = fixed defect; 7 = reversible defect | Categorical
+Target | Diagnosis of heart disease (1 = true; 0 = false) | Target
+"""
+
+"""
+## Setup
+"""
+
+import tensorflow as tf
+import pandas as pd
+import keras_core as keras
+from keras_core import layers
+
+"""
+## Preparing the data
+
+Let's download the data and load it into a Pandas dataframe:
+"""
+
+file_url = (
+    "http://storage.googleapis.com/download.tensorflow.org/data/heart.csv"
+)
+dataframe = pd.read_csv(file_url)
+
+"""
+The dataset includes 303 samples with 14 columns per sample (13 features, plus the target
+label):
+"""
+
+dataframe.shape
+
+"""
+Here's a preview of a few samples:
+"""
+
+dataframe.head()
+
+"""
+The last column, "target", indicates whether the patient has a heart disease (1) or not
+(0).
+
+Let's split the data into a training and validation set:
+"""
+
+val_dataframe = dataframe.sample(frac=0.2, random_state=1337)
+train_dataframe = dataframe.drop(val_dataframe.index)
+
+print(
+    f"Using {len(train_dataframe)} samples for training "
+    f"and {len(val_dataframe)} for validation"
+)
+
+"""
+Let's generate `tf.data.Dataset` objects for each dataframe:
+"""
+
+
+def dataframe_to_dataset(dataframe):
+    dataframe = dataframe.copy()
+    labels = dataframe.pop("target")
+    ds = tf.data.Dataset.from_tensor_slices((dict(dataframe), labels))
+    ds = ds.shuffle(buffer_size=len(dataframe))
+    return ds
+
+
+train_ds = dataframe_to_dataset(train_dataframe)
+val_ds = dataframe_to_dataset(val_dataframe)
+
+"""
+Each `Dataset` yields a tuple `(input, target)` where `input` is a dictionary of features
+and `target` is the value `0` or `1`:
+"""
+
+for x, y in train_ds.take(1):
+    print("Input:", x)
+    print("Target:", y)
+
+"""
+Let's batch the datasets:
+"""
+
+train_ds = train_ds.batch(32)
+val_ds = val_ds.batch(32)
+
+"""
+## Feature preprocessing with Keras layers
+
+
+The following features are categorical features encoded as integers:
+
+- `sex`
+- `cp`
+- `fbs`
+- `restecg`
+- `exang`
+- `ca`
+
+We will encode these features using **one-hot encoding**. We have two options
+here:
+
+ - Use `CategoryEncoding()`, which requires knowing the range of input values
+ and will error on input outside the range.
+ - Use `IntegerLookup()` which will build a lookup table for inputs and reserve
+ an output index for unkown input values.
+
+For this example, we want a simple solution that will handle out of range inputs
+at inference, so we will use `IntegerLookup()`.
+
+We also have a categorical feature encoded as a string: `thal`. We will create an
+index of all possible features and encode output using the `StringLookup()` layer.
+
+Finally, the following feature are continuous numerical features:
+
+- `age`
+- `trestbps`
+- `chol`
+- `thalach`
+- `oldpeak`
+- `slope`
+
+For each of these features, we will use a `Normalization()` layer to make sure the mean
+of each feature is 0 and its standard deviation is 1.
+
+Below, we define 3 utility functions to do the operations:
+
+- `encode_numerical_feature` to apply featurewise normalization to numerical features.
+- `encode_string_categorical_feature` to first turn string inputs into integer indices,
+then one-hot encode these integer indices.
+- `encode_integer_categorical_feature` to one-hot encode integer categorical features.
+"""
+
+from keras_core.layers import IntegerLookup
+from keras_core.layers import Normalization
+from keras_core.layers import StringLookup
+
+
+def encode_numerical_feature(feature, name, dataset):
+    # Create a Normalization layer for our feature
+    normalizer = Normalization()
+
+    # Prepare a Dataset that only yields our feature
+    feature_ds = dataset.map(lambda x, y: x[name])
+    feature_ds = feature_ds.map(lambda x: tf.expand_dims(x, -1))
+
+    # Learn the statistics of the data
+    normalizer.adapt(feature_ds)
+
+    # Normalize the input feature
+    encoded_feature = normalizer(feature)
+    return encoded_feature
+
+
+def encode_categorical_feature(feature, name, dataset, is_string):
+    lookup_class = StringLookup if is_string else IntegerLookup
+    # Create a lookup layer which will turn strings into integer indices
+    lookup = lookup_class(output_mode="binary")
+
+    # Prepare a Dataset that only yields our feature
+    feature_ds = dataset.map(lambda x, y: x[name])
+    feature_ds = feature_ds.map(lambda x: tf.expand_dims(x, -1))
+
+    # Learn the set of possible string values and assign them a fixed integer index
+    lookup.adapt(feature_ds)
+
+    # Turn the string input into integer indices
+    encoded_feature = lookup(feature)
+    return encoded_feature
+
+
+"""
+## Build a model
+
+With this done, we can create our end-to-end model:
+"""
+
+# Categorical features encoded as integers
+sex = keras.Input(shape=(1,), name="sex", dtype="int64")
+cp = keras.Input(shape=(1,), name="cp", dtype="int64")
+fbs = keras.Input(shape=(1,), name="fbs", dtype="int64")
+restecg = keras.Input(shape=(1,), name="restecg", dtype="int64")
+exang = keras.Input(shape=(1,), name="exang", dtype="int64")
+ca = keras.Input(shape=(1,), name="ca", dtype="int64")
+
+# Categorical feature encoded as string
+thal = keras.Input(shape=(1,), name="thal", dtype="string")
+
+# Numerical features
+age = keras.Input(shape=(1,), name="age")
+trestbps = keras.Input(shape=(1,), name="trestbps")
+chol = keras.Input(shape=(1,), name="chol")
+thalach = keras.Input(shape=(1,), name="thalach")
+oldpeak = keras.Input(shape=(1,), name="oldpeak")
+slope = keras.Input(shape=(1,), name="slope")
+
+all_inputs = [
+    sex,
+    cp,
+    fbs,
+    restecg,
+    exang,
+    ca,
+    thal,
+    age,
+    trestbps,
+    chol,
+    thalach,
+    oldpeak,
+    slope,
+]
+
+# Integer categorical features
+sex_encoded = encode_categorical_feature(sex, "sex", train_ds, False)
+cp_encoded = encode_categorical_feature(cp, "cp", train_ds, False)
+fbs_encoded = encode_categorical_feature(fbs, "fbs", train_ds, False)
+restecg_encoded = encode_categorical_feature(
+    restecg, "restecg", train_ds, False
+)
+exang_encoded = encode_categorical_feature(exang, "exang", train_ds, False)
+ca_encoded = encode_categorical_feature(ca, "ca", train_ds, False)
+
+# String categorical features
+thal_encoded = encode_categorical_feature(thal, "thal", train_ds, True)
+
+# Numerical features
+age_encoded = encode_numerical_feature(age, "age", train_ds)
+trestbps_encoded = encode_numerical_feature(trestbps, "trestbps", train_ds)
+chol_encoded = encode_numerical_feature(chol, "chol", train_ds)
+thalach_encoded = encode_numerical_feature(thalach, "thalach", train_ds)
+oldpeak_encoded = encode_numerical_feature(oldpeak, "oldpeak", train_ds)
+slope_encoded = encode_numerical_feature(slope, "slope", train_ds)
+
+all_features = layers.concatenate(
+    [
+        sex_encoded,
+        cp_encoded,
+        fbs_encoded,
+        restecg_encoded,
+        exang_encoded,
+        slope_encoded,
+        ca_encoded,
+        thal_encoded,
+        age_encoded,
+        trestbps_encoded,
+        chol_encoded,
+        thalach_encoded,
+        oldpeak_encoded,
+    ]
+)
+x = layers.Dense(32, activation="relu")(all_features)
+x = layers.Dropout(0.5)(x)
+output = layers.Dense(1, activation="sigmoid")(x)
+model = keras.Model(all_inputs, output)
+model.compile("adam", "binary_crossentropy", metrics=["accuracy"])
+
+"""
+Let's visualize our connectivity graph:
+"""
+
+# `rankdir='LR'` is to make the graph horizontal.
+keras.utils.plot_model(model, show_shapes=True, rankdir="LR")
+
+"""
+## Train the model
+"""
+
+model.fit(train_ds, epochs=50, validation_data=val_ds)
+
+"""
+We quickly get to 80% validation accuracy.
+"""
+
+"""
+## Inference on new data
+
+To get a prediction for a new sample, you can simply call `model.predict()`. There are
+just two things you need to do:
+
+1. wrap scalars into a list so as to have a batch dimension (models only process batches
+of data, not single samples)
+2. Call `convert_to_tensor` on each feature
+"""
+
+sample = {
+    "age": 60,
+    "sex": 1,
+    "cp": 1,
+    "trestbps": 145,
+    "chol": 233,
+    "fbs": 1,
+    "restecg": 2,
+    "thalach": 150,
+    "exang": 0,
+    "oldpeak": 2.3,
+    "slope": 3,
+    "ca": 0,
+    "thal": "fixed",
+}
+
+input_dict = {
+    name: tf.convert_to_tensor([value]) for name, value in sample.items()
+}
+predictions = model.predict(input_dict)
+
+print(
+    f"This particular patient had a {100 * predictions[0][0]:.1f} "
+    "percent probability of having a heart disease, "
+    "as evaluated by our model."
+)
