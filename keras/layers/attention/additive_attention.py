311b0ac6967bb41ca925fe38c4ad50e9f298239d
Sachin Prasad <sachinprasad@google.com>
2023-06-07 16:00:20 -0700

    Fix markdown rendering issue

--- keras/layers/attention/additive_attention.py
+++ keras/layers/attention/additive_attention.py
@@ -51,8 +51,7 @@ class AdditiveAttention(BaseDenseAttention):
         dropout: Float between 0 and 1. Fraction of the units to drop for the
             attention scores. Defaults to `0.0`.
 
-    Call Args:
-
+    Call arguments:
         inputs: List of the following tensors:
             * query: Query `Tensor` of shape `[batch_size, Tq, dim]`.
             * value: Value `Tensor` of shape `[batch_size, Tv, dim]`.
