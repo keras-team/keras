f36beadbe4da1392963e7d33d88897204c2a3ef6
tilakrayal <81610181+tilakrayal@users.noreply.github.com>
2023-06-20 20:14:05 +0530

    Update data_utils.py

--- keras/utils/data_utils.py
+++ keras/utils/data_utils.py
@@ -326,10 +326,10 @@ def get_file(
                 self.finished = False
 
             def __call__(self, block_num, block_size, total_size):
-                    if total_size == -1:
-                        total_size = None
-                    if not self.progbar:
-                        self.progbar = Progbar(total_size)
+                if total_size == -1:
+                    total_size = None
+                if not self.progbar:
+                    self.progbar = Progbar(total_size)
                 current = block_num * block_size
 
                 if total_size is None:
