7748a6aebba4bbc77a7866c44c1293c4bb9e0808
Matt Watson <1389937+mattdangerw@users.noreply.github.com>
2023-06-26 09:36:56 -0700

    Mark layers with no state as built (#404)

--- keras_core/layers/activations/softmax.py
+++ keras_core/layers/activations/softmax.py
@@ -39,6 +39,7 @@ class Softmax(Layer):
         super().__init__(**kwargs)
         self.supports_masking = True
         self.axis = axis
+        self.built = True
 
     def call(self, inputs, mask=None):
         if mask is not None:
