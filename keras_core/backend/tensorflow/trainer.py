964db435b24a948662ebc96e806735d56a4f1479
Tirth Patel <tirthasheshpatel@gmail.com>
2023-06-26 23:15:28 +0000

    Add support for RaggedTensors for TensorFlow backend (#385)
    * Add support for RaggedTensors for TensorFlow backend
    
    * Add tests for Layer and Functional base classes
    
    * Add a test for the Sequential model
    
    * Move skipif decorator after named_parameters decorator
    
    * Fix the test for the FunctionalModel
    
    * Use a custom layer in Functional test
    
    * Use 4 space indent in docs
    
    * Fix PEP 501: line too long

--- keras_core/backend/tensorflow/trainer.py
+++ keras_core/backend/tensorflow/trainer.py
@@ -487,7 +487,7 @@ class TensorFlowTrainer(base_trainer.Trainer):
                 callbacks.on_predict_batch_end(step, {"outputs": batch_outputs})
         callbacks.on_predict_end()
         return tf.__internal__.nest.map_structure_up_to(
-            batch_outputs, np.concatenate, outputs
+            batch_outputs, potentially_ragged_concat, outputs
         )
 
     def train_on_batch(
@@ -826,3 +826,49 @@ def _is_tpu_strategy_class(clz):
     if is_tpu_strat(clz):
         return True
     return any(map(_is_tpu_strategy_class, clz.__bases__))
+
+
+def potentially_ragged_concat(tensors):
+    """Concats `Tensor`s along their first dimension.
+
+    Args:
+        tensors: List of `Tensor`s.
+
+    Returns:
+        Concatenation of the inputs along the first dimension -- of type
+        `Tensor` if all input shapes are compatible, or `RaggedTensor`
+        if not.
+    """
+    if len(tensors) == 1:
+        return tensors[0]
+    if isinstance(tensors[0], tf.SparseTensor):
+        return tf.sparse.concat(axis=0, sp_inputs=tensors)
+    elif isinstance(tensors[0], tf.RaggedTensor):
+        return tf.concat(tensors, axis=0)
+    elif not tf.__internal__.tf2.enabled():
+        return tf.concat(tensors, axis=0)
+
+    non_batch_shapes = tf.stack([tf.shape(tensor)[1:] for tensor in tensors])
+    constant_dims = tf.math.reduce_all(
+        non_batch_shapes == non_batch_shapes[:1], axis=0
+    )
+    if tf.math.reduce_all(constant_dims).numpy().item():
+        # All non-batch dims are constant
+        if _is_scalar(tensors[0]):
+            return tf.stack(tensors, axis=0)
+        else:
+            return tf.concat(tensors, axis=0)
+
+    # First, identify constant inner dimensions by finding the
+    # rightmost dimension that is not constant
+    constant_inner_dimensions = (
+        constant_dims.numpy().tolist()[::-1].index(False)
+    )
+    # If there are constant inner dimensions, define a constant inner shape
+    if constant_inner_dimensions == 0:
+        constant_inner_shape = None
+    else:
+        constant_inner_shape = tensors[0].shape[-constant_inner_dimensions:]
+    return tf.ragged.constant(
+        [tensor.numpy() for tensor in tensors], inner_shape=constant_inner_shape
+    ).merge_dims(0, 1)
