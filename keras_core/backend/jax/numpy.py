Fix mixed precision issue in JAX backend (#410)
* init

* Add logging

* minor

* port kerasnlp

* fix jax error

* training fix

* wrap up

* rebase main

* fix mixed precision

* fix comments

* fix test failures

* fix type casting

---------

Co-authored-by: chenmoneygithub <chenmoney@chenmoney-gpu-scratch.us-west1-a.c.keras-team-gcp.internal>

keras_core/backend/jax/numpy.py
@@ -1,6 +1,8 @@
 import jax.numpy as jnp
 
+from keras_core.backend.jax.core import cast
 from keras_core.backend.jax.core import convert_to_tensor
+from keras_core.backend import config
 
 
 def add(x1, x2):
@@ -43,8 +45,14 @@ def multiply(x1, x2):
 
 
 def mean(x, axis=None, keepdims=False):
-    return jnp.mean(x, axis=axis, keepdims=keepdims)
-
+    # `jnp.mean` does not handle low precision (e.g., float16) overflow
+    # correctly, so we compute with float32 and cast back to the original type.
+    outputs = jnp.mean(x, axis=axis, keepdims=keepdims, dtype=jnp.float32)
+    dtype = getattr(x, "dtype", None)
+    if hasattr(dtype, "name") and "float" in dtype.name:
+        return cast(outputs, dtype)
+    else:
+        return cast(outputs, config.floatx())
 
 def max(x, axis=None, keepdims=False, initial=None):
     return jnp.max(x, axis=axis, keepdims=keepdims, initial=initial)
@@ -547,7 +555,14 @@ def transpose(x, axes=None):
 
 
 def var(x, axis=None, keepdims=False):
-    return jnp.var(x, axis=axis, keepdims=keepdims)
+    # `jnp.var` does not handle low precision (e.g., float16) overflow
+    # correctly, so we compute with float32 and cast back to the original type.
+    outputs = jnp.var(x, axis=axis, keepdims=keepdims, dtype=jnp.float32)
+    dtype = getattr(x, "dtype", None)
+    if hasattr(dtype, "name") and "float" in dtype.name:
+        return cast(outputs, dtype)
+    else:
+        return cast(outputs, config.floatx())
 
 
 def sum(x, axis=None, keepdims=False):