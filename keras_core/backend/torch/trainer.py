b06d1839e29ece47d2347d4f03325ea8c19ac6d8
hertschuh <1091026+hertschuh@users.noreply.github.com>
2023-07-12 13:47:28 -0700

    Added JAX distributed training guide. (#464)

--- keras_core/backend/torch/trainer.py
+++ keras_core/backend/torch/trainer.py
@@ -43,16 +43,20 @@ class TorchTrainer(base_trainer.Trainer):
 
         # Compute gradients
         if self.trainable_weights:
+            # Backpropagation
+            trainable_weights = [v for v in self.trainable_weights]
+
             # Call torch.Tensor.backward() on the loss to compute gradients
             # for the weights.
             loss.backward()
 
-            trainable_weights = self.trainable_weights[:]
             gradients = [v.value.grad for v in trainable_weights]
 
             # Update weights
             with torch.no_grad():
-                self.optimizer.apply(gradients, trainable_weights)
+                self.optimizer.apply_gradients(
+                    zip(gradients, trainable_weights)
+                )
         else:
             warnings.warn("The model does not have any trainable weights.")
 
