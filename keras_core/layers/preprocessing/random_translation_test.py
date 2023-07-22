5c06f2c17c6b422c31dae20b2afe68158ecc9129
HongYu <20734616+james77777778@users.noreply.github.com>
2023-07-23 05:55:18 +0800

    Convert RandomTranslation to the backend-agnostic implementation (#572)
    * Converting RandomTranslation
    
    * Update docstring
    
    * Address comments

--- keras_core/layers/preprocessing/random_translation_test.py
+++ keras_core/layers/preprocessing/random_translation_test.py
@@ -26,8 +26,38 @@ class RandomTranslationTest(testing.TestCase, parameterized.TestCase):
             run_training_check=False,
         )
 
-    def test_random_translation_up_numeric_reflect(self):
-        input_image = np.reshape(np.arange(0, 25), (1, 5, 5, 1))
+    @parameterized.named_parameters(
+        ("bad_len", [0.1, 0.2, 0.3], 0.0),
+        ("bad_type", {"dummy": 0.3}, 0.0),
+        ("exceed_range_single", -1.1, 0.0),
+        ("exceed_range_tuple", (-1.1, 0.0), 0.0),
+    )
+    def test_random_translation_with_bad_factor(
+        self, height_factor, width_factor
+    ):
+        with self.assertRaises(ValueError):
+            self.run_layer_test(
+                layers.RandomTranslation,
+                init_kwargs={
+                    "height_factor": height_factor,
+                    "width_factor": width_factor,
+                },
+                input_shape=(2, 3, 4),
+                expected_output_shape=(2, 3, 4),
+                supports_masking=False,
+                run_training_check=False,
+            )
+
+    def test_random_translation_with_inference_mode(self):
+        input_data = np.random.random((1, 4, 4, 3))
+        expected_output = input_data
+        layer = layers.RandomTranslation(0.2, 0.1)
+        output = layer(input_data, training=False)
+        self.assertAllClose(output, expected_output)
+
+    @parameterized.parameters(["channels_first", "channels_last"])
+    def test_random_translation_up_numeric_reflect(self, data_format):
+        input_image = np.arange(0, 25)
         expected_output = np.asarray(
             [
                 [5, 6, 7, 8, 9],
@@ -37,14 +67,22 @@ class RandomTranslationTest(testing.TestCase, parameterized.TestCase):
                 [20, 21, 22, 23, 24],
             ]
         )
-        expected_output = backend.convert_to_tensor(
-            np.reshape(expected_output, (1, 5, 5, 1))
-        )
+        if data_format == "channels_last":
+            input_image = np.reshape(input_image, (1, 5, 5, 1))
+            expected_output = backend.convert_to_tensor(
+                np.reshape(expected_output, (1, 5, 5, 1))
+            )
+        else:
+            input_image = np.reshape(input_image, (1, 1, 5, 5))
+            expected_output = backend.convert_to_tensor(
+                np.reshape(expected_output, (1, 1, 5, 5))
+            )
         self.run_layer_test(
             layers.RandomTranslation,
             init_kwargs={
                 "height_factor": (-0.2, -0.2),
                 "width_factor": 0.0,
+                "data_format": data_format,
             },
             input_shape=None,
             input_data=input_image,
@@ -53,10 +91,9 @@ class RandomTranslationTest(testing.TestCase, parameterized.TestCase):
             run_training_check=False,
         )
 
-    def test_random_translation_up_numeric_constant(self):
-        input_image = np.reshape(np.arange(0, 25), (1, 5, 5, 1)).astype(
-            "float32"
-        )
+    @parameterized.parameters(["channels_first", "channels_last"])
+    def test_random_translation_up_numeric_constant(self, data_format):
+        input_image = np.arange(0, 25).astype("float32")
         # Shifting by -.2 * 5 = 1 pixel.
         expected_output = np.asarray(
             [
@@ -67,15 +104,23 @@ class RandomTranslationTest(testing.TestCase, parameterized.TestCase):
                 [0, 0, 0, 0, 0],
             ]
         )
-        expected_output = backend.convert_to_tensor(
-            np.reshape(expected_output, (1, 5, 5, 1)), dtype="float32"
-        )
+        if data_format == "channels_last":
+            input_image = np.reshape(input_image, (1, 5, 5, 1))
+            expected_output = backend.convert_to_tensor(
+                np.reshape(expected_output, (1, 5, 5, 1)), dtype="float32"
+            )
+        else:
+            input_image = np.reshape(input_image, (1, 1, 5, 5))
+            expected_output = backend.convert_to_tensor(
+                np.reshape(expected_output, (1, 1, 5, 5)), dtype="float32"
+            )
         self.run_layer_test(
             layers.RandomTranslation,
             init_kwargs={
                 "height_factor": (-0.2, -0.2),
                 "width_factor": 0.0,
                 "fill_mode": "constant",
+                "data_format": data_format,
             },
             input_shape=None,
             input_data=input_image,
@@ -84,8 +129,9 @@ class RandomTranslationTest(testing.TestCase, parameterized.TestCase):
             run_training_check=False,
         )
 
-    def test_random_translation_down_numeric_reflect(self):
-        input_image = np.reshape(np.arange(0, 25), (1, 5, 5, 1))
+    @parameterized.parameters(["channels_first", "channels_last"])
+    def test_random_translation_down_numeric_reflect(self, data_format):
+        input_image = np.arange(0, 25)
         # Shifting by .2 * 5 = 1 pixel.
         expected_output = np.asarray(
             [
@@ -96,14 +142,22 @@ class RandomTranslationTest(testing.TestCase, parameterized.TestCase):
                 [15, 16, 17, 18, 19],
             ]
         )
-        expected_output = backend.convert_to_tensor(
-            np.reshape(expected_output, (1, 5, 5, 1))
-        )
+        if data_format == "channels_last":
+            input_image = np.reshape(input_image, (1, 5, 5, 1))
+            expected_output = backend.convert_to_tensor(
+                np.reshape(expected_output, (1, 5, 5, 1))
+            )
+        else:
+            input_image = np.reshape(input_image, (1, 1, 5, 5))
+            expected_output = backend.convert_to_tensor(
+                np.reshape(expected_output, (1, 1, 5, 5))
+            )
         self.run_layer_test(
             layers.RandomTranslation,
             init_kwargs={
                 "height_factor": (0.2, 0.2),
                 "width_factor": 0.0,
+                "data_format": data_format,
             },
             input_shape=None,
             input_data=input_image,
@@ -112,8 +166,11 @@ class RandomTranslationTest(testing.TestCase, parameterized.TestCase):
             run_training_check=False,
         )
 
-    def test_random_translation_asymmetric_size_numeric_reflect(self):
-        input_image = np.reshape(np.arange(0, 16), (1, 8, 2, 1))
+    @parameterized.parameters(["channels_first", "channels_last"])
+    def test_random_translation_asymmetric_size_numeric_reflect(
+        self, data_format
+    ):
+        input_image = np.arange(0, 16)
         # Shifting by .2 * 5 = 1 pixel.
         expected_output = np.asarray(
             [
@@ -127,14 +184,22 @@ class RandomTranslationTest(testing.TestCase, parameterized.TestCase):
                 [6, 7],
             ]
         )
-        expected_output = backend.convert_to_tensor(
-            np.reshape(expected_output, (1, 8, 2, 1))
-        )
+        if data_format == "channels_last":
+            input_image = np.reshape(input_image, (1, 8, 2, 1))
+            expected_output = backend.convert_to_tensor(
+                np.reshape(expected_output, (1, 8, 2, 1))
+            )
+        else:
+            input_image = np.reshape(input_image, (1, 1, 8, 2))
+            expected_output = backend.convert_to_tensor(
+                np.reshape(expected_output, (1, 1, 8, 2))
+            )
         self.run_layer_test(
             layers.RandomTranslation,
             init_kwargs={
                 "height_factor": (0.5, 0.5),
                 "width_factor": 0.0,
+                "data_format": data_format,
             },
             input_shape=None,
             input_data=input_image,
@@ -143,8 +208,9 @@ class RandomTranslationTest(testing.TestCase, parameterized.TestCase):
             run_training_check=False,
         )
 
-    def test_random_translation_down_numeric_constant(self):
-        input_image = np.reshape(np.arange(0, 25), (1, 5, 5, 1))
+    @parameterized.parameters(["channels_first", "channels_last"])
+    def test_random_translation_down_numeric_constant(self, data_format):
+        input_image = np.arange(0, 25)
         # Shifting by .2 * 5 = 1 pixel.
         expected_output = np.asarray(
             [
@@ -155,9 +221,16 @@ class RandomTranslationTest(testing.TestCase, parameterized.TestCase):
                 [15, 16, 17, 18, 19],
             ]
         )
-        expected_output = backend.convert_to_tensor(
-            np.reshape(expected_output, (1, 5, 5, 1))
-        )
+        if data_format == "channels_last":
+            input_image = np.reshape(input_image, (1, 5, 5, 1))
+            expected_output = backend.convert_to_tensor(
+                np.reshape(expected_output, (1, 5, 5, 1))
+            )
+        else:
+            input_image = np.reshape(input_image, (1, 1, 5, 5))
+            expected_output = backend.convert_to_tensor(
+                np.reshape(expected_output, (1, 1, 5, 5))
+            )
         self.run_layer_test(
             layers.RandomTranslation,
             init_kwargs={
@@ -165,6 +238,7 @@ class RandomTranslationTest(testing.TestCase, parameterized.TestCase):
                 "width_factor": 0.0,
                 "fill_mode": "constant",
                 "fill_value": 0.0,
+                "data_format": data_format,
             },
             input_shape=None,
             input_data=input_image,
@@ -173,8 +247,9 @@ class RandomTranslationTest(testing.TestCase, parameterized.TestCase):
             run_training_check=False,
         )
 
-    def test_random_translation_left_numeric_reflect(self):
-        input_image = np.reshape(np.arange(0, 25), (1, 5, 5, 1))
+    @parameterized.parameters(["channels_first", "channels_last"])
+    def test_random_translation_left_numeric_reflect(self, data_format):
+        input_image = np.arange(0, 25)
         # Shifting by .2 * 5 = 1 pixel.
         expected_output = np.asarray(
             [
@@ -185,14 +260,22 @@ class RandomTranslationTest(testing.TestCase, parameterized.TestCase):
                 [21, 22, 23, 24, 24],
             ]
         )
-        expected_output = backend.convert_to_tensor(
-            np.reshape(expected_output, (1, 5, 5, 1))
-        )
+        if data_format == "channels_last":
+            input_image = np.reshape(input_image, (1, 5, 5, 1))
+            expected_output = backend.convert_to_tensor(
+                np.reshape(expected_output, (1, 5, 5, 1))
+            )
+        else:
+            input_image = np.reshape(input_image, (1, 1, 5, 5))
+            expected_output = backend.convert_to_tensor(
+                np.reshape(expected_output, (1, 1, 5, 5))
+            )
         self.run_layer_test(
             layers.RandomTranslation,
             init_kwargs={
                 "height_factor": 0.0,
                 "width_factor": (-0.2, -0.2),
+                "data_format": data_format,
             },
             input_shape=None,
             input_data=input_image,
@@ -201,8 +284,9 @@ class RandomTranslationTest(testing.TestCase, parameterized.TestCase):
             run_training_check=False,
         )
 
-    def test_random_translation_left_numeric_constant(self):
-        input_image = np.reshape(np.arange(0, 25), (1, 5, 5, 1))
+    @parameterized.parameters(["channels_first", "channels_last"])
+    def test_random_translation_left_numeric_constant(self, data_format):
+        input_image = np.arange(0, 25)
         # Shifting by .2 * 5 = 1 pixel.
         expected_output = np.asarray(
             [
@@ -213,9 +297,16 @@ class RandomTranslationTest(testing.TestCase, parameterized.TestCase):
                 [21, 22, 23, 24, 0],
             ]
         )
-        expected_output = backend.convert_to_tensor(
-            np.reshape(expected_output, (1, 5, 5, 1))
-        )
+        if data_format == "channels_last":
+            input_image = np.reshape(input_image, (1, 5, 5, 1))
+            expected_output = backend.convert_to_tensor(
+                np.reshape(expected_output, (1, 5, 5, 1))
+            )
+        else:
+            input_image = np.reshape(input_image, (1, 1, 5, 5))
+            expected_output = backend.convert_to_tensor(
+                np.reshape(expected_output, (1, 1, 5, 5))
+            )
         self.run_layer_test(
             layers.RandomTranslation,
             init_kwargs={
@@ -223,6 +314,7 @@ class RandomTranslationTest(testing.TestCase, parameterized.TestCase):
                 "width_factor": (-0.2, -0.2),
                 "fill_mode": "constant",
                 "fill_value": 0.0,
+                "data_format": data_format,
             },
             input_shape=None,
             input_data=input_image,
