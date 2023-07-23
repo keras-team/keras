1f7d0bc8b282d6f2b49259c75074524bdde36fff
Pranav Prajapati <94780581+pranavvp16@users.noreply.github.com>
2023-07-24 03:30:49 +0530

    docString for numpy.amin (#586)
    * doc: DocString for numpy.amax
    
    * doc:Add example for keepdims=True
    
    * doc:Added numpy.amin docstring, Update outputs of amax
    
    * doc: Updated outputs

--- keras_core/ops/numpy.py
+++ keras_core/ops/numpy.py
@@ -514,6 +514,37 @@ class Amin(Operation):
 
 @keras_core_export(["keras_core.ops.amin", "keras_core.ops.numpy.amin"])
 def amin(x, axis=None, keepdims=False):
+    """Returns the minimum of an array or minimum value along an axis.
+
+    Args:
+        x: Input tensor.
+        axis: Axis along which to compute the minimum.
+            By default (`axis=None`), find the minimum value in all the
+            dimensions of the input array.
+        keep_dims: If `True`, axes which are reduced are left in the result as
+            dimensions that are broadcast to the size of the original
+            input tensor. Defaults to `False`.
+
+    Returns:
+        An array with the minimum value. If `axis=None`, the result is a scalar
+        value representing the minimum element in the entire array. If `axis` is
+        given, the result is an array with the minimum values along
+        the specified axis.
+
+    Examples:
+
+    >>> x = keras_core.ops.convert_to_tensor([1, 3, 5, 2, 3, 6])
+    >>> keras_core.ops.amin(x)
+    array(1, dtype=int32)
+
+    >>> x = keras_core.ops.convert_to_tensor([[1, 6, 8], [7, 5, 3]])
+    >>> keras_core.ops.amin(x, axis=0)
+    array([1,5,3], dtype=int32)
+
+    >>> x = keras_core.ops.convert_to_tensor([[1, 6, 8], [7, 5, 3]])
+    >>> keras_core.ops.amin(x, axis=1, keepdims=True)
+    array([[1],[3]], dtype=int32)
+    """
     if any_symbolic_tensors((x,)):
         return Amin(axis=axis, keepdims=keepdims).symbolic_call(x)
     return backend.numpy.amin(x, axis=axis, keepdims=keepdims)
