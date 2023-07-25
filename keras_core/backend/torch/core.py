ff4bae8a70d1f263bacbc2f7b1ae737b8a0c4b77
Francois Chollet <francois.chollet@gmail.com>
2023-07-25 13:11:58 -0700

    Torch fix

--- keras_core/backend/torch/core.py
+++ keras_core/backend/torch/core.py
@@ -228,7 +228,7 @@ def compute_output_spec(fn, *args, **kwargs):
                 )
                 return fn(*eager_args, **eager_kwargs)
 
-    with StatelessScope():
+    with StatelessScope(), torch.no_grad():
         outputs = symbolic_call(fn, args, kwargs, fill_value=83)
 
         none_in_shape = any(map(has_none_shape, tree.flatten((args, kwargs))))
