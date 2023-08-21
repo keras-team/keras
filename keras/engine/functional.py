0fd79cc162613f97303dac3dd49fb49be52c5797
sushreebarsa <84765720+sushreebarsa@users.noreply.github.com>
2023-08-21 12:05:20 +0530

    Update functional.py

--- keras/engine/functional.py
+++ keras/engine/functional.py
@@ -344,7 +344,7 @@ class Functional(training_lib.Model):
         if hasattr(self, "_manual_input_spec"):
             return self._manual_input_spec
         if max([len(path) for path in nest.yield_flat_paths(
-        self._nested_inputs)]) > 1:
+            self._nested_inputs)]) > 1:
         ) != len(self.inputs):
             # Case where we have a nested structure.
             # In such a case we can't safely run any checks.
