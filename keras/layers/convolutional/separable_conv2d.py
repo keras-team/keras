9ec6cc54a115fccca023dc6c5dc80427cc315f4f
Sourabh singh <96938361+SouSingh@users.noreply.github.com>
2023-09-19 22:49:49 +0530

    Update separable_conv2d.py

--- keras/layers/convolutional/separable_conv2d.py
+++ keras/layers/convolutional/separable_conv2d.py
@@ -61,8 +61,8 @@ class SeparableConv2D(SeparableConv):
         any `dilation_rate` value != 1.
       padding: one of `"valid"` or `"same"` (case-insensitive).
         `"valid"` means no padding. `"same"` results in padding with zeros
-        evenly to the left/right or up/down of the input such that output has
-        the same height/width dimension as the input.
+        evenly to the left/right or up/down of the input.When `padding="same"` and
+       `strides=1`, the output has the same size as the input.
       data_format: A string,
         one of `channels_last` (default) or `channels_first`.
         The ordering of the dimensions in the inputs.
