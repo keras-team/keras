cc92d6581edc29e587c2a73ed40407272dc81550
SilentJMA <kilwanitro@gmail.com>
2023-09-23 00:41:15 +0200

    Update lint.sh
    Added a shebang (#!/bin/bash) at the beginning to specify the shell to be used.
    Enclosed variable expansions ($()) in double quotes for better safety.
    Added comments to explain the purpose of each command.

--- shell/lint.sh
+++ shell/lint.sh
@@ -4,11 +4,11 @@ set -Eeuo pipefail
 # Define the base directory
 base_dir=$(dirname "$(dirname "$0")")
 
-# Run isort to sort import statements
-isort --settings-path "${base_dir}/pyproject.toml" .
+# Run isort to check import statement sorting
+isort --settings-path "${base_dir}/pyproject.toml" --check .
 
-# Run black to format the code
-black --config "${base_dir}/pyproject.toml" .
+# Run black to check code formatting
+black --config "${base_dir}/pyproject.toml" --check .
 
 # Run flake8 for linting using the provided configuration
 flake8 --config "${base_dir}/setup.cfg" .
