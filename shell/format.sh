85713ffeba8db08db9292856a28e7bb59b87d320
SilentJMA <kilwanitro@gmail.com>
2023-09-23 00:38:10 +0200

    Update format.sh
    Added a shebang (#!/bin/bash) at the beginning to specify the shell to be used.
    Enclosed variable expansions ($()) in double quotes for better safety.
    Added comments to explain the purpose of each command.

--- shell/format.sh
+++ shell/format.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -Eeuo pipefail
 
-base_dir=$(dirname $(dirname $0))
+# Define the base directory
+base_dir=$(dirname "$(dirname "$0")")
 
-isort --sp "${base_dir}/pyproject.toml" .
+# Run isort to sort import statements
+isort --settings-path "${base_dir}/pyproject.toml" .
 
+# Run black to format the code
 black --config "${base_dir}/pyproject.toml" .
 
+# Run flake8 for linting using the provided configuration
 flake8 --config "${base_dir}/setup.cfg" .
-
