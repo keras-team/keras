a18380e7b24302f7c327d3a159b3ca3de8b69e50
Faisal Alsrheed <47912291+Faisal-Alsrheed@users.noreply.github.com>
2023-09-23 17:49:19 +0000

    Increase-test-coverage

--- keras/backend/common/keras_tensor_test.py
+++ keras/backend/common/keras_tensor_test.py
@@ -369,7 +369,7 @@ class KerasTensorTest(testing.TestCase):
         mock_symbolic_call.return_value = mock_tensor
         x = keras_tensor.KerasTensor(shape=(3, 4), dtype="float32")
         y = keras_tensor.KerasTensor(shape=(3, 4), dtype="float32")
-        # to ensure compatibility across Python versions
+        # to ensure compatibility across Python versions.
         result = x.__div__(y)
         mock_symbolic_call.assert_called_once_with(x, y)
         self.assertEqual(result, mock_tensor)
