00b9fe3ddb958b797e984bc6f87cfb326142843b
Faisal Alsrheed <47912291+Faisal-Alsrheed@users.noreply.github.com>
2023-09-24 14:03:57 +0000

    Add tests `variables_test`

--- keras/backend/common/variables_test.py
+++ keras/backend/common/variables_test.py
@@ -10,6 +10,7 @@ from keras.testing import test_case
 
 class VariablesTest(test_case.TestCase):
     def test_deferred_initialization(self):
+        """Tests deferred initialization of variables."""
         with backend.StatelessScope():
             v = backend.Variable(
                 initializer=initializers.RandomNormal(), shape=(2, 2)
@@ -24,6 +25,7 @@ class VariablesTest(test_case.TestCase):
                 v = backend.Variable(initializer=0)
 
     def test_deferred_assignment(self):
+        """Tests deferred assignment to variables."""
         with backend.StatelessScope() as scope:
             v = backend.Variable(
                 initializer=initializers.RandomNormal(), shape=(2, 2)
@@ -36,6 +38,7 @@ class VariablesTest(test_case.TestCase):
         self.assertAllClose(out, np.ones((2, 2)))
 
     def test_autocasting(self):
+        """Tests autocasting of float variables."""
         v = backend.Variable(
             initializer=initializers.RandomNormal(),
             shape=(2, 2),
@@ -43,7 +46,6 @@ class VariablesTest(test_case.TestCase):
         )
         self.assertEqual(v.dtype, "float32")
         self.assertEqual(backend.standardize_dtype(v.value.dtype), "float32")
-
         print("open scope")
         with AutocastScope("float16"):
             self.assertEqual(
@@ -65,43 +67,45 @@ class VariablesTest(test_case.TestCase):
             self.assertEqual(backend.standardize_dtype(v.value.dtype), "int32")
 
     def test_standardize_dtype_with_torch_dtype(self):
+        """Tests dtype standardization with PyTorch dtypes."""
         import torch
 
         x = torch.randn(4, 4)
         backend.standardize_dtype(x.dtype)
 
     def test_name_validation(self):
-        # Test when name is not a string
+        """Tests validation of variable names."""
         with self.assertRaisesRegex(
             ValueError, "Argument `name` must be a string"
         ):
             KerasVariable(initializer=initializers.RandomNormal(), name=12345)
 
-        # Test when name contains a '/'
         with self.assertRaisesRegex(ValueError, "cannot contain character `/`"):
             KerasVariable(
                 initializer=initializers.RandomNormal(), name="invalid/name"
             )
 
     def test_standardize_shape_with_none(self):
+        """Tests standardizing shape with None."""
         with self.assertRaisesRegex(
             ValueError, "Undefined shapes are not supported."
         ):
             standardize_shape(None)
 
     def test_standardize_shape_with_non_iterable(self):
+        """Tests shape standardization with non-iterables."""
         with self.assertRaisesRegex(
             ValueError, "Cannot convert '42' to a shape."
         ):
             standardize_shape(42)
 
     def test_standardize_shape_with_valid_input(self):
+        """Tests standardizing shape with valid input."""
         shape = [3, 4, 5]
         standardized_shape = standardize_shape(shape)
         self.assertEqual(standardized_shape, (3, 4, 5))
 
-    # TODO
-    # (3.9,torch) FAILED keras/backend/common/variables_test.py
+    # TODO (3.9,torch) FAILED keras/backend/common/variables_test.py
     # ::VariablesTest::test_standardize_shape_with_non_integer_entry:
     #  - AssertionError "Cannot convert '\(3, 4, 'a'\)' to a shape.
     # " does not match "invalid literal for int() with base 10: 'a'"
@@ -113,6 +117,7 @@ class VariablesTest(test_case.TestCase):
     #         standardize_shape([3, 4, "a"])
 
     def test_standardize_shape_with_negative_entry(self):
+        """Tests standardizing shape with negative entries."""
         with self.assertRaisesRegex(
             ValueError,
             "Cannot convert '\\(3, 4, -5\\)' to a shape. Negative dimensions",
@@ -120,8 +125,111 @@ class VariablesTest(test_case.TestCase):
             standardize_shape([3, 4, -5])
 
     def test_autocast_scope_with_non_float_dtype(self):
+        """Tests autocast scope with non-float dtype."""
         with self.assertRaisesRegex(
             ValueError,
             "`AutocastScope` can only be used with a floating-point",
         ):
             _ = AutocastScope("int32")
+
+    def test_variable_initialization_with_non_callable(self):
+        """Test variable init with non-callable initializer."""
+        v = backend.Variable(initializer=np.ones((2, 2)))
+        self.assertAllClose(v.value, np.ones((2, 2)))
+
+    def test_variable_path_creation(self):
+        """Test path creation for a variable."""
+        v = backend.Variable(initializer=np.ones((2, 2)), name="test_var")
+        self.assertEqual(v.path, "test_var")
+
+    def test_variable_initialization_with_non_trainable(self):
+        """Test variable initialization with non-trainable flag."""
+        v = backend.Variable(initializer=np.ones((2, 2)), trainable=False)
+        self.assertFalse(v.trainable)
+
+    def test_variable_initialization_without_shape(self):
+        """Test variable init without a shape."""
+        with self.assertRaisesRegex(
+            ValueError,
+            "When creating a Variable from an initializer, the `shape` ",
+        ):
+            backend.Variable(initializer=initializers.RandomNormal())
+
+    def test_deferred_initialize_already_initialized(self):
+        """Test deferred init on an already initialized variable."""
+        v = backend.Variable(initializer=np.ones((2, 2)))
+        with self.assertRaisesRegex(
+            ValueError, f"Variable {v.path} is already initialized."
+        ):
+            v._deferred_initialize()
+
+    def test_deferred_initialize_within_stateless_scope(self):
+        """Test deferred init within a stateless scope."""
+        with backend.StatelessScope():
+            v = backend.Variable(
+                initializer=initializers.RandomNormal(), shape=(2, 2)
+            )
+            with self.assertRaisesRegex(
+                ValueError,
+                "You are attempting to initialize a variable "
+                "while in a stateless scope. This is disallowed.",
+            ):
+                v._deferred_initialize()
+
+    def test_variable_as_boolean(self):
+        """Test converting a variable to boolean."""
+        v = backend.Variable(initializer=np.ones((2, 2)))
+        with self.assertRaises(TypeError):
+            bool(v)
+
+    def test_variable_negation(self):
+        """Test negating a variable."""
+        v = backend.Variable(initializer=np.array([-1, 2]))
+        neg_v = -v
+        self.assertAllClose(neg_v, np.array([1, -2]))
+
+    def test_variable_pos(self):
+        """Test positive value of a variable."""
+        v = backend.Variable(initializer=np.array([-1, 2]))
+        pos_v = v
+        self.assertAllClose(pos_v, np.array([-1, 2]))
+
+    def test_variable_abs(self):
+        """Test absolute value of a variable."""
+        v = backend.Variable(initializer=np.array([-1, 2]))
+        abs_v = abs(v)
+        self.assertAllClose(abs_v, np.array([1, 2]))
+
+    def test_variable_div_numpy_array(self):
+        """Test variable divided by numpy array."""
+        v = backend.Variable(initializer=np.array([2, 4, 8]))
+        arr = np.array([2, 8, 16])
+        div_result = arr / v
+        self.assertAllClose(div_result, np.array([1, 2, 2]))
+
+    def test_variable_rdiv_numpy_array(self):
+        """Test numpy array divided by variable."""
+        v = backend.Variable(initializer=np.array([2, 4, 8]))
+        arr = np.array([16, 32, 64])
+        rdiv_result = arr / v
+        self.assertAllClose(rdiv_result, np.array([8, 8, 8]))
+
+    def test_variable_rsub_numpy_array(self):
+        """Test numpy array minus variable."""
+        v = backend.Variable(initializer=np.array([1, 2, 3]))
+        arr = np.array([2, 2, 2])
+        rsub_result = arr - v
+        self.assertAllClose(rsub_result, np.array([1, 0, -1]))
+
+    def test_variable_rfloordiv(self):
+        """Test numpy array floordiv by variable."""
+        v = backend.Variable(initializer=np.array([3, 4, 6]))
+        result = np.array([9, 12, 18]) // v
+        self.assertAllClose(result, np.array([3, 3, 3]))
+
+    def test_variable_rmatmul(self):
+        """Test numpy array matmul with variable."""
+        v = backend.Variable(initializer=np.array([[2, 3], [4, 5]]))
+        other = np.array([[1, 2], [3, 4]])
+        rmatmul_result = other @ v
+        self.assertAllClose(rmatmul_result, np.array([[10, 13], [22, 29]]))
