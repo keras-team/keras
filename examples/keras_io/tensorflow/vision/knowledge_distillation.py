fae4e23b3ffb8a890864500a79f37fcbcdebf58e
Awsaf <awsaf49@gmail.com>
2023-09-26 08:36:18 +0600

    update: black format

--- examples/keras_io/tensorflow/vision/knowledge_distillation.py
+++ examples/keras_io/tensorflow/vision/knowledge_distillation.py
@@ -254,7 +254,9 @@ distiller = Distiller(student=student, teacher=teacher)
 distiller.compile(
     optimizer=keras.optimizers.Adam(),
     metrics=[keras.metrics.SparseCategoricalAccuracy()],
-    student_loss_fn=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    student_loss_fn=keras.losses.SparseCategoricalCrossentropy(
+        from_logits=True
+    ),
     distillation_loss_fn=keras.losses.KLDivergence(),
     alpha=0.1,
     temperature=10,
