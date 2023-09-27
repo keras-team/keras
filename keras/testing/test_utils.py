7f1c9f674bd50dd8a7f79987887e19e204869698
Faisal Alsrheed <47912291+Faisal-Alsrheed@users.noreply.github.com>
2023-09-27 15:28:08 +0000

    Fix `get_test_data` + Add `ClassDistributionTests`

--- keras/testing/test_utils.py
+++ keras/testing/test_utils.py
@@ -5,7 +5,8 @@ def get_test_data(
     train_samples, test_samples, input_shape, num_classes, random_seed=None
 ):
     """Generates synthetic test data for training a model,
-    ensuring balanced class distribution in train/test split.
+    ensuring balanced class distribution in train/test split
+    using stratified sampling.
 
     Args:
         train_samples (int): Number of training samples.
@@ -27,12 +28,15 @@ def get_test_data(
     # Ensure that we generate a balanced dataset
     samples_per_class = total_samples // num_classes
     y = np.array(
-        [i for i in range(num_classes) for _ in range(samples_per_class)]
+        [i for i in range(num_classes) for _ in range(samples_per_class)],
+        dtype=np.int64,  # Ensure integer datatype as np.int64
     )
 
-    # Generate extra samples if needed due to rounding
+    # Generate extra samples in a deterministic manner
     extra_samples = total_samples - len(y)
-    y_extra = np.random.randint(0, num_classes, size=(extra_samples,))
+    y_extra = np.array(
+        [i % num_classes for i in range(extra_samples)], dtype=np.int64
+    )
     y = np.concatenate([y, y_extra])
 
     # Generate data
@@ -43,11 +47,35 @@ def get_test_data(
             loc=0, scale=1.0, size=input_shape
         )
 
-    # Shuffle data and split
+    # Shuffle the entire dataset to ensure randomness based on seed
     indices = np.arange(total_samples)
     np.random.shuffle(indices)
     x, y = x[indices], y[indices]
-    x_train, x_test = x[:train_samples], x[train_samples:]
-    y_train, y_test = y[:train_samples], y[train_samples:]
+
+    # Stratified Shuffle Split
+    x_train, y_train, x_test, y_test = [], [], [], []
+    for cls in range(num_classes):
+        cls_indices = np.where(y == cls)[0]
+        np.random.shuffle(cls_indices)
+        train_count = int(train_samples / num_classes)
+
+        x_train.extend(x[cls_indices[:train_count]])
+        y_train.extend(y[cls_indices[:train_count]])
+
+        x_test.extend(x[cls_indices[train_count:]])
+        y_test.extend(y[cls_indices[train_count:]])
+
+    # Convert to numpy arrays
+    x_train, y_train = np.array(x_train), np.array(y_train)
+    x_test, y_test = np.array(x_test), np.array(y_test)
+
+    # Shuffle training and test sets after stratified split
+    train_indices = np.arange(len(x_train))
+    test_indices = np.arange(len(x_test))
+    np.random.shuffle(train_indices)
+    np.random.shuffle(test_indices)
+
+    x_train, y_train = x_train[train_indices], y_train[train_indices]
+    x_test, y_test = x_test[test_indices], y_test[test_indices]
 
     return x_train, y_train, x_test, y_test
--- keras/testing/test_utils_test.py
+++ keras/testing/test_utils_test.py
@@ -144,3 +144,92 @@ class GetTestDataTest(test_case.TestCase):
         )
         self.assertEqual(y_train.dtype, np.int64)
         self.assertEqual(y_test.dtype, np.int64)
+
+
+class ClassDistributionTests(test_case.TestCase):
+    def setUp(self):
+        self.train_samples = 100
+        self.test_samples = 50
+        self.input_shape = (28, 28)
+        self.num_classes = 10
+
+    def test_equal_class_distribution(self):
+        """Ensure each class has nearly equal
+        instances in train and test sets."""
+        _, y_train, _, y_test = test_utils.get_test_data(
+            self.train_samples,
+            self.test_samples,
+            self.input_shape,
+            self.num_classes,
+        )
+        _, counts_train = np.unique(y_train, return_counts=True)
+        _, counts_test = np.unique(y_test, return_counts=True)
+
+        self.assertTrue(
+            np.all(counts_train == self.train_samples // self.num_classes)
+        )
+        self.assertTrue(
+            np.all(counts_test == self.test_samples // self.num_classes)
+        )
+
+    def test_uneven_samples_class_distribution(self):
+        """Check class distribution when number of samples
+        isn't a multiple of number of classes."""
+        train_samples = 103
+        test_samples = 52
+        _, y_train, _, y_test = test_utils.get_test_data(
+            train_samples,
+            test_samples,
+            self.input_shape,
+            self.num_classes,
+        )
+        _, counts_train = np.unique(y_train, return_counts=True)
+        _, counts_test = np.unique(y_test, return_counts=True)
+
+        self.assertTrue(np.max(counts_train) - np.min(counts_train) <= 1)
+        self.assertTrue(np.max(counts_test) - np.min(counts_test) <= 1)
+
+    def test_randomness_in_class_distribution(self):
+        """Ensure class distribution isn't too deterministic."""
+        _, y_train_1, _, y_test_1 = test_utils.get_test_data(
+            self.train_samples,
+            self.test_samples,
+            self.input_shape,
+            self.num_classes,
+        )
+        _, y_train_2, _, y_test_2 = test_utils.get_test_data(
+            self.train_samples,
+            self.test_samples,
+            self.input_shape,
+            self.num_classes,
+        )
+        self.assertFalse(np.array_equal(y_train_1, y_train_2))
+        self.assertFalse(np.array_equal(y_test_1, y_test_2))
+
+    def test_large_number_of_classes(self):
+        """Validate function with a large number of classes."""
+        num_classes = 150
+        train_samples = (
+            num_classes * 10
+        )  # 10 samples for each class in training
+        test_samples = num_classes * 5  # 5 samples for each class in testing
+        _, y_train, _, y_test = test_utils.get_test_data(
+            train_samples,
+            test_samples,
+            self.input_shape,
+            num_classes,
+        )
+        self.assertEqual(len(np.unique(y_train)), num_classes)
+        self.assertEqual(len(np.unique(y_test)), num_classes)
+
+    def test_single_class(self):
+        """Test with a single class."""
+        num_classes = 1
+        _, y_train, _, y_test = test_utils.get_test_data(
+            self.train_samples,
+            self.test_samples,
+            self.input_shape,
+            num_classes,
+        )
+        self.assertTrue(np.all(y_train == 0))
+        self.assertTrue(np.all(y_test == 0))
