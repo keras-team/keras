1f931049754f437fb9aa717b030496c5b106b12d
Awsaf <awsaf49@gmail.com>
2023-10-15 09:05:36 +0600

    add: `query-key-value` & `causal` mask

--- keras/layers/attention/grouped_query_attention.py
+++ keras/layers/attention/grouped_query_attention.py
@@ -172,12 +172,27 @@ class GroupedQueryAttention(Layer):
         query,
         value,
         key=None,
+        query_mask=None,
+        value_mask=None,
+        key_mask=None,
         attention_mask=None,
         return_attention_scores=False,
         training=None,
+        use_causal_mask=False,
     ):
         if key is None:
             key = value
+
+        attention_mask = self._compute_attention_mask(
+            query,
+            value,
+            query_mask=query_mask,
+            value_mask=value_mask,
+            key_mask=key_mask,
+            attention_mask=attention_mask,
+            use_causal_mask=use_causal_mask,
+        )
+
         batch_dim, target_seq_len, _ = ops.shape(
             query
         )  # (batch_dim, target_seq_len, feature_dim)
@@ -251,6 +266,103 @@ class GroupedQueryAttention(Layer):
         if return_attention_scores:
             return output, scores
         return output
+
+    def _compute_attention_mask(
+        self,
+        query,
+        value,
+        query_mask=None,
+        value_mask=None,
+        key_mask=None,
+        attention_mask=None,
+        use_causal_mask=False,
+    ):
+        """Computes the attention mask, using the Keras masks of the inputs.
+
+        * The `query`'s mask is reshaped from [B, T] to [B, T, 1].
+        * The `value`'s mask is reshaped from [B, S] to [B, 1, S].
+        * The `key`'s mask is reshaped from [B, S] to [B, 1, S]. The `key`'s
+          mask is ignored if `key` is `None` or if `key is value`.
+        * If `use_causal_mask=True`, then the causal mask is computed. Its shape
+          is [1, T, S].
+
+        All defined masks are merged using a logical AND operation (`&`).
+
+        In general, if the `query` and `value` are masked, then there is no need
+        to define the `attention_mask`.
+
+        Args:
+            query: Projected query tensor of shape `(B, T, N, key_dim)`.
+            key: Projected key tensor of shape `(B, T, N, key_dim)`.
+            value: Projected value tensor of shape `(B, T, N, value_dim)`.
+            attention_mask: a boolean mask of shape `(B, T, S)`, that prevents
+                attention to certain positions.
+            use_causal_mask: A boolean to indicate whether to apply a causal
+                mask to prevent tokens from attending to future tokens (e.g.,
+                used in a decoder Transformer).
+
+        Returns:
+            attention_mask: a boolean mask of shape `(B, T, S)`, that prevents
+                attention to certain positions, based on the Keras masks of the
+                `query`, `key`, `value`, and `attention_mask` tensors, and the
+                causal mask if `use_causal_mask=True`.
+        """
+        auto_mask = None
+        if query_mask is not None:
+            query_mask = ops.cast(query_mask, "bool")  # defensive casting
+            # B = batch size, T = max query length
+            auto_mask = ops.expand_dims(query_mask, -1)  # shape is [B, T, 1]
+        if value_mask is not None:
+            value_mask = ops.cast(value_mask, "bool")  # defensive casting
+            # B = batch size, S == max value length
+            mask = ops.expand_dims(value_mask, -2)  # shape is [B, 1, S]
+            auto_mask = mask if auto_mask is None else auto_mask & mask
+        if key_mask is not None:
+            key_mask = ops.cast(key_mask, "bool")  # defensive casting
+            # B == batch size, S == max key length == max value length
+            mask = ops.expand_dims(key_mask, -2)  # shape is [B, 1, S]
+            auto_mask = mask if auto_mask is None else auto_mask & mask
+        if use_causal_mask:
+            # the shape of the causal mask is [1, T, S]
+            mask = self._compute_causal_mask(query, value)
+            auto_mask = mask if auto_mask is None else auto_mask & mask
+        if auto_mask is not None:
+            # merge attention_mask & automatic mask, to shape [B, T, S]
+            attention_mask = (
+                auto_mask
+                if attention_mask is None
+                else ops.cast(attention_mask, bool) & auto_mask
+            )
+        return attention_mask
+
+    def _compute_causal_mask(self, query, value=None):
+        """Computes a causal mask (e.g., for masked self-attention layers).
+
+        For example, if query and value both contain sequences of length 4,
+        this function returns a boolean tensor equal to:
+
+        ```
+        [[[True,  False, False, False],
+          [True,  True,  False, False],
+          [True,  True,  True,  False],
+          [True,  True,  True,  True]]]
+        ```
+
+        Args:
+            query: query tensor of shape `(B, T, ...)`.
+            value: value tensor of shape `(B, S, ...)` (optional, defaults to
+                query).
+
+        Returns:
+            mask: a boolean tensor of shape `(1, T, S)` containing a lower
+                triangular matrix of shape `(T, S)`.
+        """
+        q_seq_length = ops.shape(query)[1]
+        v_seq_length = q_seq_length if value is None else ops.shape(value)[1]
+        ones_mask = ops.ones((1, q_seq_length, v_seq_length), dtype="int32")
+        row_index = ops.cumsum(ones_mask, axis=-2)
+        col_index = ops.cumsum(ones_mask, axis=-1)
+        return ops.greater_equal(row_index, col_index)
 
     def _compute_attention(
         self, query, key, value, attention_mask=None, training=None
