321914de330855c78e25391bb3a95390c36e9053
Awsaf <awsaf49@gmail.com>
2023-10-15 20:33:51 +0600

    add: simple test

---
+++ keras/layers/attention/grouped_query_attention_test.py
@@ -0,0 +1,47 @@
+# import numpy as np
+# import pytest
+from absl.testing import parameterized
+
+# from keras import backend
+# from keras import initializers
+from keras import layers
+from keras import testing
+
+
+class GroupedQueryAttentionTest(testing.TestCase, parameterized.TestCase):
+    def test_basics(self):
+        self.run_layer_test(
+            layers.GroupedQueryAttention,
+            init_kwargs={
+                "num_query_heads": 2,
+                "num_key_value_heads": 2,
+                "head_dim": 2,
+            },
+            input_shape={"query_shape": (2, 8, 16), "value_shape": (2, 4, 16)},
+            expected_output_shape=(2, 8, 16),
+            expected_num_trainable_weights=8,
+            expected_num_non_trainable_weights=0,
+            expected_num_seed_generators=0,
+            expected_num_losses=0,
+            supports_masking=True,
+            run_training_check=False,
+        )
+
+        self.run_layer_test(
+            layers.GroupedQueryAttention,
+            init_kwargs={
+                "num_query_heads": 2,
+                "num_key_value_heads": 2,
+                "head_dim": 2,
+                "use_bias": False,
+                "dropout": 0.5,
+            },
+            input_shape={"query_shape": (2, 8, 16), "value_shape": (2, 4, 16)},
+            expected_output_shape=(2, 8, 16),
+            expected_num_trainable_weights=4,
+            expected_num_non_trainable_weights=0,
+            expected_num_seed_generators=0,
+            expected_num_losses=0,
+            supports_masking=True,
+            run_training_check=False,
+        )
