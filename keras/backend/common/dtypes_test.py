7d48ab3c260e9614c75c88492aacfc13abd050be
Faisal Alsrheed <47912291+Faisal-Alsrheed@users.noreply.github.com>
2023-10-16 06:53:04 +0000

    Add tests in `common/dtypes_test.py`

--- keras/backend/common/dtypes_test.py
+++ keras/backend/common/dtypes_test.py
@@ -5,6 +5,7 @@ from keras import ops
 from keras.backend.common.variables import ALLOWED_DTYPES
 from keras.backend.torch.core import to_torch_dtype
 from keras.testing import test_case
+from keras.backend.common import dtypes
 
 
 class DtypesTest(test_case.TestCase, parameterized.TestCase):
@@ -62,3 +63,45 @@ class DtypesTest(test_case.TestCase, parameterized.TestCase):
             ValueError, "Invalid `dtypes`. At least one dtype is required."
         ):
             backend.result_type()
+
+    def test_respect_weak_type_for_bool(self):
+        self.assertEqual(dtypes._respect_weak_type("bool", True), "bool")
+
+    def test_respect_weak_type_for_int(self):
+        self.assertEqual(dtypes._respect_weak_type("int32", True), "int")
+
+    def test_respect_weak_type_for_float(self):
+        self.assertEqual(dtypes._respect_weak_type("float32", True), "float")
+
+    def test_resolve_weak_type_for_bfloat16(self):
+        self.assertEqual(dtypes._resolve_weak_type("bfloat16"), "float32")
+
+    def test_resolve_weak_type_for_bfloat16_with_precision(self):
+        self.assertEqual(
+            dtypes._resolve_weak_type("bfloat16", precision="64"), "float64"
+        )
+
+    def test_invalid_dtype_for_keras_promotion(self):
+        with self.assertRaisesRegex(
+            ValueError, "is not a valid dtype for Keras type promotion."
+        ):
+            dtypes._least_upper_bound("invalid_dtype")
+
+    def test_resolve_weak_type_for_invalid_dtype(self):
+        with self.assertRaisesRegex(
+            ValueError, "Invalid value for argument `dtype`. Expected one of"
+        ):
+            dtypes._resolve_weak_type("invalid_dtype")
+
+    def test_resolve_weak_type_for_invalid_precision(self):
+        with self.assertRaisesRegex(
+            ValueError,
+            "Invalid value for argument `precision`. Expected one of",
+        ):
+            dtypes._resolve_weak_type("int32", precision="invalid_precision")
+
+    def test_respect_weak_type_for_invalid_dtype(self):
+        with self.assertRaisesRegex(
+            ValueError, "Invalid value for argument `dtype`. Expected one of"
+        ):
+            dtypes._respect_weak_type("invalid_dtype", True)
