c9b215cdb74d38ba4b6e937f96cccb468ca7bf6c
Hongyu Chiu <20734616+james77777778@users.noreply.github.com>
2023-10-21 21:58:09 +0800

    Update logic for torch

--- keras/backend/torch/numpy.py
+++ keras/backend/torch/numpy.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 import torch
 
@@ -685,15 +687,45 @@ def maximum(x1, x2):
 
 
 def median(x, axis=None, keepdims=False):
-    # use torch.median if possible
-    if axis is None and keepdims is False:
-        x = convert_to_tensor(x)
-        compute_dtype = dtypes.result_type(x.dtype, "float32")
-        result_dtype = dtypes.result_type(x.dtype, float)
-        x = cast(x, compute_dtype)
-        return cast(torch.median(x), result_dtype)
+    x = convert_to_tensor(x)
+    compute_dtype = dtypes.result_type(x.dtype, "float32")
+    result_dtype = dtypes.result_type(x.dtype, float)
+    x = cast(x, compute_dtype)
 
-    return quantile(x, 0.5, axis=axis, keepdims=keepdims)
+    if axis is None and keepdims is False:
+        return cast(torch.median(x), result_dtype)
+    elif isinstance(axis, int):
+        return cast(
+            torch.median(x, dim=axis, keepdim=keepdims)[0], result_dtype
+        )
+
+    # support multiple axes
+    if axis is None:
+        y = reshape(x, [-1])
+    else:
+        # transpose
+        axis = list(map(lambda a: a if a >= 0 else a + x.ndim, axis))
+        other_dims = sorted(set(range(x.ndim)).difference(axis))
+        perm = other_dims + list(axis)
+        x_permed = torch.permute(x, dims=perm)
+        # reshape
+        x_shape = list(x.shape)
+        other_shape = [x_shape[i] for i in other_dims]
+        end_shape = [math.prod([x_shape[i] for i in axis])]
+        full_shape = other_shape + end_shape
+        y = reshape(x_permed, full_shape)
+
+    y = torch.median(y, dim=-1)[0]
+
+    if keepdims:
+        if axis is None:
+            for _ in range(x.ndim):
+                y = expand_dims(y, axis=-1)
+        else:
+            for i in sorted(axis):
+                y = expand_dims(y, axis=i)
+
+    return cast(y, result_dtype)
 
 
 def meshgrid(*x, indexing="xy"):
@@ -848,15 +880,14 @@ def quantile(x, q, axis=None, method="linear", keepdims=False):
         y = reshape(x, [-1])
     else:
         # transpose
-        axis = np.array(axis, "int32")
-        axis = np.where(axis < 0, axis + x.ndim, axis)
+        axis = list(map(lambda a: a if a >= 0 else a + x.ndim, axis))
         other_dims = sorted(set(range(x.ndim)).difference(axis))
         perm = other_dims + list(axis)
         x_permed = torch.permute(x, dims=perm)
         # reshape
         x_shape = list(x.shape)
         other_shape = [x_shape[i] for i in other_dims]
-        end_shape = [np.prod([x_shape[i] for i in axis])]
+        end_shape = [math.prod([x_shape[i] for i in axis])]
         full_shape = other_shape + end_shape
         y = reshape(x_permed, full_shape)
 
