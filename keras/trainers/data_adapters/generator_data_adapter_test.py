b02a2072bd1778836590bbda1bda2ed6f77b2503
Fabien Hertschuh <1091026+hertschuh@users.noreply.github.com>
2023-11-01 15:08:13 -0700

    Add support for Tensorflow SparseTensors: data adapters.
    `GeneratorDataAdapter` now accepts `tf.SparseTensor`s and `scipy.sparse` matrices.
    
    `TFDatasetAdapter` now accepts `tf.SparseTensor`s.

--- keras/trainers/data_adapters/generator_data_adapter_test.py
+++ keras/trainers/data_adapters/generator_data_adapter_test.py
@@ -1,6 +1,7 @@
 import math
 
 import numpy as np
+import scipy
 import tensorflow as tf
 from absl.testing import parameterized
 
@@ -88,3 +89,49 @@ class GeneratorDataAdapterTest(testing.TestCase, parameterized.TestCase):
             for i in range(by.shape[0]):
                 sample_order.append(by[i, 0])
         self.assertAllClose(sample_order, list(range(64)))
+
+    def test_tf_sparse_tensors(self):
+        def generate_tf():
+            for i in range(4):
+                x = tf.SparseTensor(
+                    indices=[[0, 0], [1, 2]],
+                    values=[1.0, 2.0],
+                    dense_shape=(2, 4),
+                )
+                y = tf.SparseTensor(
+                    indices=[[0, 0], [1, 1]],
+                    values=[3.0, 4.0],
+                    dense_shape=(2, 2),
+                )
+                yield x, y
+
+        adapter = generator_data_adapter.GeneratorDataAdapter(generate_tf())
+        ds = adapter.get_tf_dataset()
+        for batch in ds:
+            self.assertEqual(len(batch), 2)
+            bx, by = batch
+            self.assertIsInstance(bx, tf.SparseTensor)
+            self.assertIsInstance(by, tf.SparseTensor)
+            self.assertEqual(bx.shape, (2, 4))
+            self.assertEqual(by.shape, (2, 2))
+
+    def test_scipy_sparse_tensors(self):
+        def generate_scipy():
+            for i in range(4):
+                x = scipy.sparse.coo_matrix(
+                    ([1.0, 2.0], ([0, 1], [0, 2])), shape=[2, 4]
+                )
+                y = scipy.sparse.coo_matrix(
+                    ([3.0, 4.0], ([0, 1], [0, 1])), shape=[2, 2]
+                )
+                yield x, y
+
+        adapter = generator_data_adapter.GeneratorDataAdapter(generate_scipy())
+        ds = adapter.get_tf_dataset()
+        for batch in ds:
+            self.assertEqual(len(batch), 2)
+            bx, by = batch
+            self.assertIsInstance(bx, tf.SparseTensor)
+            self.assertIsInstance(by, tf.SparseTensor)
+            self.assertEqual(bx.shape, (2, 4))
+            self.assertEqual(by.shape, (2, 2))
