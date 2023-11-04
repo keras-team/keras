7a6f4ac2ccc6f873a116df4632449fe188bd092f
Francois Chollet <francois.chollet@gmail.com>
2023-11-04 12:45:58 -0700

    Fix cct example

--- examples/keras_io/vision/cct.py
+++ examples/keras_io/vision/cct.py
@@ -248,12 +248,15 @@ class StochasticDepth(layers.Layer):
     def __init__(self, drop_prop, **kwargs):
         super().__init__(**kwargs)
         self.drop_prob = drop_prop
+        self.seed_generator = keras.random.SeedGenerator(1337)
 
     def call(self, x, training=None):
         if training:
             keep_prob = 1 - self.drop_prob
             shape = (keras.ops.shape(x)[0],) + (1,) * (len(x.shape) - 1)
-            random_tensor = keep_prob + keras.random.uniform(shape, 0, 1)
+            random_tensor = keep_prob + keras.random.uniform(
+                shape, 0, 1, seed=self.seed_generator
+            )
             random_tensor = keras.ops.floor(random_tensor)
             return (x / keep_prob) * random_tensor
         return x
