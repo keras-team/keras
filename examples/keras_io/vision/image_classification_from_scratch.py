00eccfb21f399dca3541960689c5877068951b94
Francois Chollet <francois.chollet@gmail.com>
2023-11-09 12:59:18 -0800

    Add back image filtering

--- examples/keras_io/vision/image_classification_from_scratch.py
+++ examples/keras_io/vision/image_classification_from_scratch.py
@@ -55,6 +55,32 @@ ls PetImages
 """
 
 """
+### Filter out corrupted images
+
+When working with lots of real-world image data, corrupted images are a common
+occurence. Let's filter out badly-encoded images that do not feature the string "JFIF"
+in their header.
+"""
+
+num_skipped = 0
+for folder_name in ("Cat", "Dog"):
+    folder_path = os.path.join("PetImages", folder_name)
+    for fname in os.listdir(folder_path):
+        fpath = os.path.join(folder_path, fname)
+        try:
+            fobj = open(fpath, "rb")
+            is_jfif = b"JFIF" in fobj.peek(10)
+        finally:
+            fobj.close()
+
+        if not is_jfif:
+            num_skipped += 1
+            # Delete corrupted image
+            os.remove(fpath)
+
+print(f"Deleted {num_skipped} images.")
+
+"""
 ## Generate a `Dataset`
 """
 
