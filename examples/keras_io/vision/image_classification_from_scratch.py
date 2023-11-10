8393193076558d5f1476a0d2fe7bc6709a3df9a5
Francois Chollet <francois.chollet@gmail.com>
2023-11-10 13:51:43 -0800

    Minor example fix

--- examples/keras_io/vision/image_classification_from_scratch.py
+++ examples/keras_io/vision/image_classification_from_scratch.py
@@ -319,5 +319,5 @@ img_array = keras.utils.img_to_array(img)
 img_array = keras.ops.expand_dims(img_array, 0)  # Create batch axis
 
 predictions = model.predict(img_array)
-score = float(predictions[0])
+score = float(keras.ops.sigmoid(predictions[0]))
 print(f"This image is {100 * (1 - score):.2f}% cat and {100 * score:.2f}% dog.")
