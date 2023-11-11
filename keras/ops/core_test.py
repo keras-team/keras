5999db29234e221740b39ff8487ad6c9a9ba6094
Gabriel Rasskin <43894452+grasskin@users.noreply.github.com>
2023-11-10 19:48:29 -0500

    Add ops.is_tensor to the public API (#18756)
    * Add is_tensor to ops
    
    * Format
    
    * Update core_test.py
    
    Co-authored-by: Tirth Patel <tirthasheshpatel@gmail.com>
    
    * Docstring formatting
    
    ---------
    
    Co-authored-by: Tirth Patel <tirthasheshpatel@gmail.com>

--- keras/ops/core_test.py
+++ keras/ops/core_test.py
@@ -411,3 +411,11 @@ class CoreOpsCorrectnessTest(testing.TestCase):
         self.assertAllClose(
             backend.convert_to_numpy(output), 2 * np.ones((2, 3))
         )
+
+    def test_is_tensor(self):
+        np_x = np.array([[1, 2, 3], [3, 2, 1]])
+        x = backend.convert_to_tensor(np_x)
+        if backend.backend() != "numpy":
+            self.assertFalse(ops.is_tensor(np_x))
+        self.assertTrue(ops.is_tensor(x))
+        self.assertFalse(ops.is_tensor([1, 2, 3]))
