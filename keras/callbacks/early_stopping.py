8c126e33fb915d400efee15c58c56dacf90f91ad
Francois Chollet <francois.chollet@gmail.com>
2023-11-23 14:14:53 -0800

    Update early stopping weight restoration behavior

--- keras/callbacks/early_stopping.py
+++ keras/callbacks/early_stopping.py
@@ -158,8 +158,10 @@ class EarlyStopping(Callback):
             # If no monitor value exists or still in initial warm-up stage.
             return
         if self.restore_best_weights and self.best_weights is None:
-            # Restore the weights after first epoch if no progress is ever made.
+            # If best weights were never set,
+            # then the current weights are the best.
             self.best_weights = self.model.get_weights()
+            self.best_epoch = epoch
 
         self.wait += 1
         if self._is_improvement(current, self.best):
@@ -175,24 +177,24 @@ class EarlyStopping(Callback):
                 self.wait = 0
             return
 
-        # Only check after the first epoch.
         if self.wait >= self.patience and epoch > 0:
+            # Patience has been exceeded: stop training
             self.stopped_epoch = epoch
             self.model.stop_training = True
-            if self.restore_best_weights and self.best_weights is not None:
-                if self.verbose > 0:
-                    io_utils.print_msg(
-                        "Restoring model weights from "
-                        "the end of the best epoch: "
-                        f"{self.best_epoch + 1}."
-                    )
-                self.model.set_weights(self.best_weights)
 
     def on_train_end(self, logs=None):
         if self.stopped_epoch > 0 and self.verbose > 0:
             io_utils.print_msg(
                 f"Epoch {self.stopped_epoch + 1}: early stopping"
             )
+        if self.restore_best_weights and self.best_weights is not None:
+            if self.verbose > 0:
+                io_utils.print_msg(
+                    "Restoring model weights from "
+                    "the end of the best epoch: "
+                    f"{self.best_epoch + 1}."
+                )
+            self.model.set_weights(self.best_weights)
 
     def get_monitor_value(self, logs):
         logs = logs or {}
--- keras/callbacks/early_stopping_test.py
+++ keras/callbacks/early_stopping_test.py
@@ -176,6 +176,7 @@ class EarlyStoppingTest(testing.TestCase):
             early_stop.on_epoch_end(epoch, logs={"val_loss": losses[epoch]})
             if early_stop.model.stop_training:
                 break
+        early_stop.on_train_end()
         # The best configuration is in epoch 2 (loss = 0.1000),
         # and while patience = 2, we're restoring the best weights,
         # so we end up at the epoch with the best weights, i.e. epoch 2
@@ -199,9 +200,35 @@ class EarlyStoppingTest(testing.TestCase):
             early_stop.on_epoch_end(epoch, logs={"val_loss": losses[epoch]})
             if early_stop.model.stop_training:
                 break
+        early_stop.on_train_end()
         # No epoch improves on the baseline, so we should train for only 5
         # epochs, and restore the second model.
         self.assertEqual(epochs_trained, 5)
+        self.assertEqual(early_stop.model.get_weights(), 2)
+
+        # Check weight restoration when another callback requests a stop.
+        early_stop = callbacks.EarlyStopping(
+            monitor="val_loss",
+            patience=5,
+            baseline=0.5,
+            restore_best_weights=True,
+        )
+        early_stop.set_model(DummyModel())
+        losses = [0.9, 0.8, 0.7, 0.71, 0.72, 0.73]
+        # The best configuration is in the epoch 2 (loss = 0.7000).
+        epochs_trained = 0
+        early_stop.on_train_begin()
+        for epoch in range(len(losses)):
+            epochs_trained += 1
+            early_stop.model.set_weight_to_epoch(epoch=epoch)
+            early_stop.on_epoch_end(epoch, logs={"val_loss": losses[epoch]})
+            if epoch == 3:
+                early_stop.model.stop_training = True
+            if early_stop.model.stop_training:
+                break
+        early_stop.on_train_end()
+        # We should restore the second model.
+        self.assertEqual(epochs_trained, 4)
         self.assertEqual(early_stop.model.get_weights(), 2)
 
     @pytest.mark.requires_trainable_backend
