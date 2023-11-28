9813d4a5d00d3aa7a6c4c620a083963afb905b26
Hongyu Chiu <20734616+james77777778@users.noreply.github.com>
2023-11-28 17:13:46 +0800

    Add `HardSwish` activation

--- keras/layers/activations/__init__.py
+++ keras/layers/activations/__init__.py
@@ -1,4 +1,5 @@
 from keras.layers.activations.elu import ELU
+from keras.layers.activations.hard_swish import HardSwish
 from keras.layers.activations.leaky_relu import LeakyReLU
 from keras.layers.activations.prelu import PReLU
 from keras.layers.activations.relu import ReLU
