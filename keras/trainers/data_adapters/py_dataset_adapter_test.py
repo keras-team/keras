a15268c8c27405e2bf156431bc58927cf541bcbc
Sam Levang <slevang@salientpredictions.com>
2023-11-29 16:05:25 -0500

    add test

--- keras/trainers/data_adapters/py_dataset_adapter_test.py
+++ keras/trainers/data_adapters/py_dataset_adapter_test.py
@@ -39,6 +39,29 @@ class ExamplePyDataset(py_dataset_adapter.PyDataset):
         return batch_x, batch_y
 
 
+class DictPyDataset(py_dataset_adapter.PyDataset):
+    def __init__(
+        self, inputs, batch_size=32, **kwargs
+    ):
+        super().__init__(**kwargs)
+        self.inputs = inputs
+        self.batch_size = batch_size
+
+    def __len__(self):
+        return math.ceil(len(self.inputs["x"]) / self.batch_size)
+
+    def __getitem__(self, idx):
+        # Return x, y for batch idx.
+        low = idx * self.batch_size
+        # Cap upper bound at array length; the last batch may be smaller
+        # if the total number of items is not a multiple of batch size.
+        high = min(low + self.batch_size, len(self.inputs["x"]))
+        batch_x = self.inputs["x"][low:high]
+        batch_y = self.inputs["y"][low:high]
+        batch = {"x": batch_x, "y": batch_y}
+        return batch
+
+
 class PyDatasetAdapterTest(testing.TestCase, parameterized.TestCase):
     @parameterized.parameters(
         [
@@ -146,3 +169,32 @@ class PyDatasetAdapterTest(testing.TestCase, parameterized.TestCase):
         speedup_time = time.time() - t0
 
         self.assertLess(speedup_time, no_speedup_time)
+
+    def test_dict_inputs(self):
+        inputs = {
+            "x": np.random.random((40, 4)),
+            "y": np.random.random((40, 2)),
+        }
+        py_dataset = DictPyDataset(inputs, batch_size=4)
+        adapter = py_dataset_adapter.PyDatasetAdapter(
+            py_dataset, shuffle=False
+        )
+        gen = adapter.get_numpy_iterator()
+        for batch in gen:
+            self.assertEqual(len(batch), 2)
+            bx, by = batch["x"], batch["y"]
+            self.assertIsInstance(bx, np.ndarray)
+            self.assertIsInstance(by, np.ndarray)
+            self.assertEqual(bx.dtype, by.dtype)
+            self.assertEqual(bx.shape, (4, 4))
+            self.assertEqual(by.shape, (4, 2))
+        
+        ds = adapter.get_tf_dataset()
+        for batch in ds:
+            self.assertEqual(len(batch), 2)
+            bx, by = batch["x"], batch["y"]
+            self.assertIsInstance(bx, tf.Tensor)
+            self.assertIsInstance(by, tf.Tensor)
+            self.assertEqual(bx.dtype, by.dtype)
+            self.assertEqual(tuple(bx.shape), (4, 4))
+            self.assertEqual(tuple(by.shape), (4, 2))
