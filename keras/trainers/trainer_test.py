199a34b811849a49ffd77aed885727617d5d6702
Albert Sund Aillet <albert@sundaillet.com>
2023-11-29 15:03:39 +0000

    added test_stop_loop

--- keras/trainers/trainer_test.py
+++ keras/trainers/trainer_test.py
@@ -1101,3 +1101,41 @@ class TestTrainer(testing.TestCase, parameterized.TestCase):
         )
         self.assertEqual(recorder.train_counter, 3)
         self.assertEqual(recorder.val_counter, 4)
+
+    @parameterized.named_parameters(
+        [
+            ("fit", "fit", "training", "train"),
+            ("evaluate", "evaluate", "evaluating", "test"),
+            ("predict", "predict", "predicting", "predict"),
+        ]
+    )
+    @pytest.mark.requires_trainable_backend
+    def test_stop_loop(self, method, method_gerund, on_end_name):
+        model = ExampleModel(3)
+        model.compile(optimizer="sgd", loss="mse", metrics=["mse"])
+
+        class Stopper(keras.callbacks.Callback):
+            def __init__(self, stop_count):
+                self.stop_count = stop_count
+                self.counter = 0
+                setattr(self, f"on_{on_end_name}_batch_end", self.batch_end)
+
+            def batch_end(self, *args, **kwargs):
+                self.counter += 1
+                if self.counter == self.stop_count:
+                    setattr(self.model, f"stop_{method_gerund}", True)
+
+        def infinite_gen():
+            while True:
+                x = np.ones((2, 2))
+                y = np.ones((2, 3))
+                yield (x,) if method == "predict" else (x, y)
+
+        for stop_count in (5, 7):
+            stopper = Stopper(stop_count)
+
+            getattr(model, method)(
+                infinite_gen(),
+                callbacks=[stopper],
+            )
+            self.assertEqual(stopper.counter, stop_count)
