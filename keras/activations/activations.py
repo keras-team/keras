a66ba3287e835e693f9d08991f5df637c53b8adf
Hongyu Chiu <20734616+james77777778@users.noreply.github.com>
2023-12-03 21:33:32 +0800

    Fix docstring

--- keras/activations/activations.py
+++ keras/activations/activations.py
@@ -376,15 +376,15 @@ def hard_sigmoid(x):
 
 @keras_export(["keras.activations.hard_silu", "keras.activations.hard_swish"])
 def hard_silu(x):
-    """Hard swish activation function.
+    """Hard SiLU activation function, also known as Hard Swish.
 
-    The hard swish activation is defined as:
+    It is defined as:
 
     - `0` if `if x < -3`
     - `x` if `x > 3`
     - `x * (x + 3) / 6` if `-3 <= x <= 3`
 
-    It's a faster, piecewise linear approximation of the swish activation.
+    It's a faster, piecewise linear approximation of the silu activation.
 
     Args:
         x: Input tensor.
