32da2a9c7262814f70f7aacccd128583b6a777f7
Yash Singh <yash9vardhan@gmail.com>
2023-12-17 18:57:51 +0530

    standard input_shape if include_top is false

--- keras/applications/nasnet.py
+++ keras/applications/nasnet.py
@@ -384,6 +384,8 @@ def NASNetMobile(
             "at this time due to an outstanding bug. "
             "If interested, please open a PR."
         )
+    if (include_top == False) and (input_shape == None):
+        input_shape = (224, 224, 3)
     return NASNet(
         input_shape,
         penultimate_filters=1056,
