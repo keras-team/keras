0d1ce4dcb27e005636e6eb0f875b1ec37dd693a5
Hongyu Chiu <20734616+james77777778@users.noreply.github.com>
2023-12-21 14:10:27 +0800

    Update

--- keras/callbacks/swap_ema_weights_test.py
+++ keras/callbacks/swap_ema_weights_test.py
@@ -21,14 +21,6 @@ class SwapEMAWeightsTest(testing.TestCase):
         )
         y_train = numerical_utils.to_categorical(y_train)
 
-        # use ema_momentum=0.1 to distinguish ema weights and model weights
-        optimizer = optimizers.SGD(use_ema=True, ema_momentum=0.9)
-        model = Sequential(
-            [layers.Dense(2, kernel_initializer="ones", use_bias=False)]
-        )
-        model.compile(loss="mse", optimizer=optimizer)
-
-        self.model = model
         self.x_train = x_train
         self.y_train = y_train
 
@@ -40,75 +32,57 @@ class SwapEMAWeightsTest(testing.TestCase):
 
     @pytest.mark.requires_trainable_backend
     def test_swap_ema_weights(self):
+        optimizer = optimizers.SGD(use_ema=True, ema_momentum=0.9)
+        model = Sequential(
+            [layers.Dense(2, kernel_initializer="ones", use_bias=False)]
+        )
+        model.compile(loss="mse", optimizer=optimizer)
         swap_ema_weights = callbacks.SwapEMAWeights()
-        swap_ema_weights.set_model(self.model)
+        swap_ema_weights.set_model(model)
 
         # train_on_batch to make ema work
         for _ in range(5):
-            self.model.train_on_batch(self.x_train, self.y_train)
-        model_variables = self._get_numpy_trainable_variables(
-            self.model.trainable_variables
-        )
+            model.train_on_batch(self.x_train, self.y_train)
+        model_variables = ops.convert_to_numpy(model.trainable_variables[0])
 
         # test stage
         swap_ema_weights.on_test_begin()
-        ema_variables = self._get_numpy_trainable_variables(
-            self.model.trainable_variables
-        )
-        for ema_variable, model_variable in zip(ema_variables, model_variables):
-            self.assertNotAllClose(ema_variable, model_variable)
+        ema_variables = ops.convert_to_numpy(model.trainable_variables[0])
+        self.assertNotAllClose(ema_variables, model_variables)
 
         swap_ema_weights.on_test_end()
-        restored_variables = self._get_numpy_trainable_variables(
-            self.model.trainable_variables
-        )
-        for restored_variable, model_variable in zip(
-            restored_variables, model_variables
-        ):
-            self.assertAllClose(restored_variable, model_variable)
+        restored_variables = ops.convert_to_numpy(model.trainable_variables[0])
+        self.assertAllClose(restored_variables, model_variables)
 
         # predict stage
         swap_ema_weights.on_predict_begin()
-        ema_variables = self._get_numpy_trainable_variables(
-            self.model.trainable_variables
-        )
-        for ema_variable, model_variable in zip(ema_variables, model_variables):
-            self.assertNotAllClose(ema_variable, model_variable)
+        ema_variables = ops.convert_to_numpy(model.trainable_variables[0])
+        self.assertNotAllClose(ema_variables, model_variables)
 
         swap_ema_weights.on_predict_end()
-        restored_variables = self._get_numpy_trainable_variables(
-            self.model.trainable_variables
-        )
-        for restored_variable, model_variable in zip(
-            restored_variables, model_variables
-        ):
-            self.assertAllClose(restored_variable, model_variable)
+        restored_variables = ops.convert_to_numpy(model.trainable_variables[0])
+        self.assertAllClose(restored_variables, model_variables)
 
     @pytest.mark.requires_trainable_backend
     def test_swap_ema_weights_on_epoch(self):
+        optimizer = optimizers.SGD(use_ema=True, ema_momentum=0.9)
+        model = Sequential(
+            [layers.Dense(2, kernel_initializer="ones", use_bias=False)]
+        )
+        model.compile(loss="mse", optimizer=optimizer)
         swap_ema_weights = callbacks.SwapEMAWeights(swap_on_epoch=True)
-        swap_ema_weights.set_model(self.model)
+        swap_ema_weights.set_model(model)
 
         # train_on_batch to make ema work
         for _ in range(5):
-            self.model.train_on_batch(self.x_train, self.y_train)
-        model_variables = self._get_numpy_trainable_variables(
-            self.model.trainable_variables
-        )
+            model.train_on_batch(self.x_train, self.y_train)
+        model_variables = ops.convert_to_numpy(model.trainable_variables[0])
 
         # epoch stage
         swap_ema_weights.on_epoch_begin()
-        ema_variables = self._get_numpy_trainable_variables(
-            self.model.trainable_variables
-        )
-        for ema_variable, model_variable in zip(ema_variables, model_variables):
-            self.assertNotAllClose(ema_variable, model_variable)
+        ema_variables = ops.convert_to_numpy(model.trainable_variables[0])
+        self.assertNotAllClose(ema_variables, model_variables)
 
         swap_ema_weights.on_epoch_end(epoch=1)
-        restored_variables = self._get_numpy_trainable_variables(
-            self.model.trainable_variables
-        )
-        for restored_variable, model_variable in zip(
-            restored_variables, model_variables
-        ):
-            self.assertAllClose(restored_variable, model_variable)
+        restored_variables = ops.convert_to_numpy(model.trainable_variables[0])
+        self.assertAllClose(restored_variables, model_variables)
