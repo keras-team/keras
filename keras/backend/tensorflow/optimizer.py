4258043d43f4732aeddf995f9c7cbcfc3fad5e82
Qianli Scott Zhu <scottzhu@google.com>
2023-12-31 16:06:37 -0800

    Fix format.

--- keras/backend/tensorflow/optimizer.py
+++ keras/backend/tensorflow/optimizer.py
@@ -33,7 +33,7 @@ class TFOptimizer(base_optimizer.BaseOptimizer):
             return super().add_variable_from_reference(
                 reference_variable, name=name, initializer=initializer
             )
-        
+
     def add_variable(self, shape, initializer="zeros", dtype=None, name=None):
         with tf.init_scope():
             return super().add_variable(shape, initializer, dtype, name)
