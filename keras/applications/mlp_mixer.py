8dac701d736e0be13060d8cc243d48effb610ea2
Muhammad Anas Raza <memanasraza@gmail.com>
2024-01-07 20:50:52 -0500

    chore: format

--- keras/applications/mlp_mixer.py
+++ keras/applications/mlp_mixer.py
@@ -4,50 +4,43 @@ from functools import partial
 from keras.api_export import keras_export
 from keras.applications import imagenet_utils
 
-BASE_WEIGHTS_PATH = (
-    "https://huggingface.co/anasrz/kimm/resolve/main"
-)
+BASE_WEIGHTS_PATH = "https://huggingface.co/anasrz/kimm/resolve/main"
 
 WEIGHTS_NAMES = {
-    "mixer_l16_224": (
-        "jx_mixer_l16_224-92f9adc4.weights.h5"
-    ),
-    "mixer_b16_224": (
-        "mixer_b16_224_miil-9229a591.weights.h5?download=true"
-    )
+    "mixer_l16_224": ("jx_mixer_l16_224-92f9adc4.weights.h5"),
+    "mixer_b16_224": ("mixer_b16_224_miil-9229a591.weights.h5?download=true"),
 }
-
 
 
 MODEL_CONFIGS = {
     "mixer_l16_224": {
-        "patch_size": 16, 
-        "num_blocks": 24, 
+        "patch_size": 16,
+        "num_blocks": 24,
         "embed_dim": 1024,
         "img_size": 224,
     },
     "mixer_b16_224": {
-        "patch_size":16, 
-        "num_blocks":12, 
-        "embed_dim":768,
+        "patch_size": 16,
+        "num_blocks": 12,
+        "embed_dim": 768,
         "img_size": 224,
     },
     "mixer_b32_224": {
-        "patch_size":32, 
-        "num_blocks":12, 
-        "embed_dim":768,
+        "patch_size": 32,
+        "num_blocks": 12,
+        "embed_dim": 768,
         "img_size": 224,
     },
     "mixer_s16_224": {
-        "patch_size":16, 
-        "num_blocks":8, 
-        "embed_dim":512,
-        "img_size":224,
+        "patch_size": 16,
+        "num_blocks": 8,
+        "embed_dim": 512,
+        "img_size": 224,
     },
     "mixer_s32_224": {
-        "patch_size":32, 
-        "num_blocks":8, 
-        "embed_dim":512,
+        "patch_size": 32,
+        "num_blocks": 8,
+        "embed_dim": 512,
     },
 }
 
@@ -104,6 +97,7 @@ Returns:
     A model instance.
 """
 
+
 def pair(t):
     return t if isinstance(t, tuple) else (t, t)
 
@@ -122,151 +116,198 @@ class DropPath(layers.Layer):
             batch_size = x.shape[0] or ops.shape(x)[0]
             drop_map_shape = (batch_size,) + (1,) * (len(x.shape) - 1)
             drop_map = ops.cast(
-                keras.random.uniform(drop_map_shape, seed=self.seed) > self.rate,
+                keras.random.uniform(drop_map_shape, seed=self.seed)
+                > self.rate,
                 x.dtype,
             )
             x = x / (1.0 - self.rate)
             x = x * drop_map
             return x
 
-def Mlp(hidden_features=None,
-            out_features=None,
-            act_layer=ops.gelu,
-            norm_layer=None,
-            bias=True,
-            drop=0.,
-            use_conv=False,
-        name='mlp'):
-    """ MLP as used in Vision Transformer, MLP-Mixer and related networks
-    """
+
+def Mlp(
+    hidden_features=None,
+    out_features=None,
+    act_layer=ops.gelu,
+    norm_layer=None,
+    bias=True,
+    drop=0.0,
+    use_conv=False,
+    name="mlp",
+):
+    """MLP as used in Vision Transformer, MLP-Mixer and related networks"""
     if not name:
-        name = ''
+        name = ""
     bias = pair(bias)
     drop_probs = pair(drop)
-    linear_layer = partial(layers.Conv2D, kernel_size=1) if use_conv else layers.Dense
-    norm = norm_layer(name=f'{name}.norm') if norm_layer is not None else layers.Identity()
-    
+    linear_layer = (
+        partial(layers.Conv2D, kernel_size=1) if use_conv else layers.Dense
+    )
+    norm = (
+        norm_layer(name=f"{name}.norm")
+        if norm_layer is not None
+        else layers.Identity()
+    )
+
     def _apply(x):
         nonlocal out_features, hidden_features
         in_features = ops.shape(x)[-1]
         out_features = out_features or in_features
         hidden_features = hidden_features or in_features
-        x = linear_layer(hidden_features, use_bias=bias[0], name=f'{name}.fc1')(x)
+        x = linear_layer(hidden_features, use_bias=bias[0], name=f"{name}.fc1")(
+            x
+        )
         x = act_layer(x)
         x = layers.Dropout(drop_probs[0])(x)
         x = norm(x)
-        x = linear_layer(out_features, use_bias=bias[1], name=f'{name}.fc2')(x)
+        x = linear_layer(out_features, use_bias=bias[1], name=f"{name}.fc2")(x)
         x = layers.Dropout(drop_probs[1])(x)
         return x
 
     return _apply
 
-def MixerBlock(dim,
-            seq_len,
-            mlp_ratio=(0.5, 4.0),
-            mlp_layer=Mlp,
-            norm_layer=partial(layers.LayerNormalization, epsilon=1e-6),
-            act_layer=ops.gelu,
-            drop=0.,
-            drop_path=0.,
-            name='block'):
+
+def MixerBlock(
+    dim,
+    seq_len,
+    mlp_ratio=(0.5, 4.0),
+    mlp_layer=Mlp,
+    norm_layer=partial(layers.LayerNormalization, epsilon=1e-6),
+    act_layer=ops.gelu,
+    drop=0.0,
+    drop_path=0.0,
+    name="block",
+):
     tokens_dim, channels_dim = [int(x * dim) for x in pair(mlp_ratio)]
-    drop_path = DropPath(drop_path) if drop_path > 0. else layers.Identity()
+    drop_path = DropPath(drop_path) if drop_path > 0.0 else layers.Identity()
 
     def _apply(x):
         x_skip = x
         x = norm_layer(name=f"{name}.norm1")(x)
         x = layers.Permute((2, 1))(x)
-        x = mlp_layer(tokens_dim, act_layer=act_layer, drop=drop, name=f'{name}.mlp_tokens')(x)
+        x = mlp_layer(
+            tokens_dim,
+            act_layer=act_layer,
+            drop=drop,
+            name=f"{name}.mlp_tokens",
+        )(x)
         x = layers.Permute((2, 1))(x)
         x = x_skip + drop_path(x)
         x_skip = x
         x = norm_layer(name=f"{name}.norm2")(x)
-        x = mlp_layer(channels_dim, act_layer=act_layer, drop=drop, name=f'{name}.mlp_channels')(x)
+        x = mlp_layer(
+            channels_dim,
+            act_layer=act_layer,
+            drop=drop,
+            name=f"{name}.mlp_channels",
+        )(x)
         x = x_skip + drop_path(x)
 
         return x
+
     return _apply
 
 
-def PatchEmbed(img_size = 224,
-            patch_size = 16,
-            in_chans = 3,
-            embed_dim = 768,
-            norm_layer = None,
-            flatten = True,
-            bias = True,
-            name=None):
+def PatchEmbed(
+    img_size=224,
+    patch_size=16,
+    in_chans=3,
+    embed_dim=768,
+    norm_layer=None,
+    flatten=True,
+    bias=True,
+    name=None,
+):
     patch_size = pair(patch_size)
     norm = norm_layer() if norm_layer else layers.Identity()
+
     def _apply(x):
-        x = layers.Conv2D(embed_dim, kernel_size=patch_size, strides=patch_size, use_bias=bias, name=f"{name}.proj")(x)
+        x = layers.Conv2D(
+            embed_dim,
+            kernel_size=patch_size,
+            strides=patch_size,
+            use_bias=bias,
+            name=f"{name}.proj",
+        )(x)
         x = layers.Reshape((-1, embed_dim))(x)
         x = norm(x)
         return x
+
     return _apply
 
-def MlpMixer(num_classes=1000,
-            img_size=224,
-            in_chans=3,
-            patch_size=16,
-            num_blocks=8,
-            embed_dim=512,
-            mlp_ratio=(0.5, 4.0),
-            block_layer=MixerBlock,
-            mlp_layer=Mlp,
-            norm_layer=partial(layers.LayerNormalization, epsilon=1e-6),
-            act_layer=ops.gelu,
-            drop_rate=0.,
-            proj_drop_rate=0.,
-            drop_path_rate=0.,
-            stem_norm=False,
-            global_pool='avg',
-             data_format='channels_last'):
-    assert keras.config.image_data_format() == 'channels_last', "Mixer only supports `channels_last` data_format"
+
+def MlpMixer(
+    num_classes=1000,
+    img_size=224,
+    in_chans=3,
+    patch_size=16,
+    num_blocks=8,
+    embed_dim=512,
+    mlp_ratio=(0.5, 4.0),
+    block_layer=MixerBlock,
+    mlp_layer=Mlp,
+    norm_layer=partial(layers.LayerNormalization, epsilon=1e-6),
+    act_layer=ops.gelu,
+    drop_rate=0.0,
+    proj_drop_rate=0.0,
+    drop_path_rate=0.0,
+    stem_norm=False,
+    global_pool="avg",
+    data_format="channels_last",
+):
+    assert (
+        keras.config.image_data_format() == "channels_last"
+    ), "Mixer only supports `channels_last` data_format"
     img_size = pair(img_size)
     input_shape = (img_size[0], img_size[1], in_chans)
     inputs = layers.Input(input_shape)
     x = PatchEmbed(
-            img_size=img_size,
-            patch_size=patch_size,
-            in_chans=in_chans,
-            embed_dim=embed_dim,
-            norm_layer=norm_layer if stem_norm else None,
-            name='stem'
-        )(inputs) # stem
+        img_size=img_size,
+        patch_size=patch_size,
+        in_chans=in_chans,
+        embed_dim=embed_dim,
+        norm_layer=norm_layer if stem_norm else None,
+        name="stem",
+    )(
+        inputs
+    )  # stem
     num_patches = ops.shape(x)[1]
     for i in range(num_blocks):
         x = block_layer(
-                embed_dim,
-                num_patches,
-                mlp_ratio,
-                mlp_layer=mlp_layer,
-                norm_layer=norm_layer,
-                act_layer=act_layer,
-                drop=proj_drop_rate,
-                drop_path=drop_path_rate,
-                name=f"blocks.{i}"
-            )(x)
-    x = norm_layer(name='norm')(x) 
-    
-    if global_pool == 'avg':
-            x = ops.mean(x, axis=1)
+            embed_dim,
+            num_patches,
+            mlp_ratio,
+            mlp_layer=mlp_layer,
+            norm_layer=norm_layer,
+            act_layer=act_layer,
+            drop=proj_drop_rate,
+            drop_path=drop_path_rate,
+            name=f"blocks.{i}",
+        )(x)
+    x = norm_layer(name="norm")(x)
+
+    if global_pool == "avg":
+        x = ops.mean(x, axis=1)
     x = layers.Dropout(drop_rate)(x)
     if num_classes > 0:
-        head = layers.Dense(num_classes, name='head') 
+        head = layers.Dense(num_classes, name="head")
     else:
-        head = layers.Identity() 
+        head = layers.Identity()
     out = head(x)
     return keras.Model(inputs=inputs, outputs=out)
 
+
 def get_weights_pretrained(model, model_name):
     print(WEIGHTS_NAMES.keys())
-    assert model_name in WEIGHTS_NAMES.keys(), f"Weights not available for model {model_name}"
+    assert (
+        model_name in WEIGHTS_NAMES.keys()
+    ), f"Weights not available for model {model_name}"
     weights_path = keras.utils.get_file(
-    origin=f"{BASE_WEIGHTS_PATH}/{WEIGHTS_NAMES[model_name]}",)
+        origin=f"{BASE_WEIGHTS_PATH}/{WEIGHTS_NAMES[model_name]}",
+    )
     model.load_weights(weights_path)
     return True
+
 
 @keras_export(
     [
@@ -275,10 +316,11 @@ def get_weights_pretrained(model, model_name):
     ]
 )
 def Mixer_L16_224(pretrained=False, **kwargs):
-    model = MlpMixer(**MODEL_CONFIGS['mixer_l16_224'], **kwargs)
+    model = MlpMixer(**MODEL_CONFIGS["mixer_l16_224"], **kwargs)
     if pretrained:
-        get_weights_pretrained(model, 'mixer_l16_224')
+        get_weights_pretrained(model, "mixer_l16_224")
     return model
+
 
 @keras_export(
     [
@@ -287,10 +329,11 @@ def Mixer_L16_224(pretrained=False, **kwargs):
     ]
 )
 def Mixer_B16_224(pretrained=False, **kwargs):
-    model = MlpMixer(**MODEL_CONFIGS['mixer_b16_224'], **kwargs)
+    model = MlpMixer(**MODEL_CONFIGS["mixer_b16_224"], **kwargs)
     if pretrained:
-        get_weights_pretrained(model, 'mixer_b16_224')
+        get_weights_pretrained(model, "mixer_b16_224")
     return model
+
 
 @keras_export(
     [
@@ -299,9 +342,9 @@ def Mixer_B16_224(pretrained=False, **kwargs):
     ]
 )
 def Mixer_B32_224(pretrained=False, **kwargs):
-    model = MlpMixer(**MODEL_CONFIGS['mixer_b32_224'], **kwargs)
+    model = MlpMixer(**MODEL_CONFIGS["mixer_b32_224"], **kwargs)
     if pretrained:
-        get_weights_pretrained(model, 'mixer_b32_224')
+        get_weights_pretrained(model, "mixer_b32_224")
     return model
 
 
@@ -312,10 +355,11 @@ def Mixer_B32_224(pretrained=False, **kwargs):
     ]
 )
 def Mixer_S16_224(pretrained=False, **kwargs):
-    model = MlpMixer(**MODEL_CONFIGS['mixer_s16_224'], **kwargs)
+    model = MlpMixer(**MODEL_CONFIGS["mixer_s16_224"], **kwargs)
     if pretrained:
-        get_weights_pretrained(model, 'mixer_s16_224')
+        get_weights_pretrained(model, "mixer_s16_224")
     return model
+
 
 @keras_export(
     [
@@ -324,10 +368,11 @@ def Mixer_S16_224(pretrained=False, **kwargs):
     ]
 )
 def Mixer_S32_224(pretrained=False, **kwargs):
-    model = MlpMixer(**MODEL_CONFIGS['mixer_s32_224'], **kwargs)
+    model = MlpMixer(**MODEL_CONFIGS["mixer_s32_224"], **kwargs)
     if pretrained:
-        get_weights_pretrained(model, 'mixer_s32_224')
+        get_weights_pretrained(model, "mixer_s32_224")
     return model
+
 
 Mixer_L16_224.__doc__ = BASE_DOCSTRING.format(name="Mixer_L16_224")
 Mixer_B16_224.__doc__ = BASE_DOCSTRING.format(name="Mixer_B16_224")
@@ -335,14 +380,16 @@ Mixer_B32_224.__doc__ = BASE_DOCSTRING.format(name="Mixer_B32_224")
 Mixer_S16_224.__doc__ = BASE_DOCSTRING.format(name="Mixer_S16_224")
 Mixer_S32_224.__doc__ = BASE_DOCSTRING.format(name="Mixer_S32_224")
 
+
 @keras_export("keras.applications.mlp_mixer.decode_predictions")
 def decode_predictions(preds, top=5):
     return imagenet_utils.decode_predictions(preds, top=top)
 
+
 decode_predictions.__doc__ = imagenet_utils.decode_predictions.__doc__
+
 
 @keras_export("keras.applications.convnext.preprocess_input")
 def preprocess_input(x, data_format=None):
-    """Preprocesses input image in 0-1 by dividing it by 255.0"
-    """        
+    """Preprocesses input image in 0-1 by dividing it by 255.0" """
     return x / 255.0
