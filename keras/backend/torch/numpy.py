d99cb03a8a8997d2a028533aa1883b4da7f68179
SuryanarayanaY <suryanarayanay@google.com>
2024-01-23 15:37:48 +0530

    Correct torch.numpy.take behaviour with axis=None

--- keras/backend/torch/numpy.py
+++ keras/backend/torch/numpy.py
@@ -1217,9 +1217,12 @@ def swapaxes(x, axis1, axis2):
 def take(x, indices, axis=None):
     x = convert_to_tensor(x)
     indices = convert_to_tensor(indices).long()
-    if x.ndim == 2 and (axis is None or axis == 0):
+    if x.ndim == 2 and axis == 0:
         # This case is equivalent to embedding lookup.
         return torch.nn.functional.embedding(indices, x)
+    if axis is None:
+        x = torch.reshape(x, (-1,))
+        axis = 0
     if axis is not None:
         # make sure axis is non-negative
         axis = len(x.shape) + axis if axis < 0 else axis
