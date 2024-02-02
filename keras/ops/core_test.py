8ae587a056fb9dc4a0856ce6e14d912c387cb2c6
Faisal Alsrheed <47912291+Faisal-Alsrheed@users.noreply.github.com>
2024-02-02 16:44:37 +0000

    fix Jax tests

--- keras/ops/core_test.py
+++ keras/ops/core_test.py
@@ -912,50 +912,6 @@ class CoreOpsCallsTests(testing.TestCase):
         with self.assertRaisesRegex(ValueError, expected_regex):
             cond_op(pred, true_fn, false_fn)
 
-    def test_cond_output_spec_mismatch_true_fn_tuple_false_fn_not_tuple(self):
-        cond_op = core.Cond()
-
-        def true_fn():
-            return (
-                KerasTensor(shape=(3, 3), dtype="float32"),
-                KerasTensor(shape=(4, 4), dtype="float32"),
-            )
-
-        def false_fn():
-            # not a tuple to match the true_fn
-            return {
-                "a": KerasTensor(shape=(3, 3), dtype="float32"),
-                "b": KerasTensor(shape=(4, 4), dtype="float32"),
-            }
-
-        pred = KerasTensor(shape=(), dtype="bool")
-        expected_regex = "should return outputs of the same kind"
-
-        with self.assertRaisesRegex(ValueError, expected_regex):
-            cond_op(pred, true_fn, false_fn)
-
-    def test_cond_output_spec_mismatch_in_tuple_elements(self):
-        cond_op = core.Cond()
-
-        def true_fn():
-            return (
-                KerasTensor(shape=(3, 3), dtype="float32"),
-                KerasTensor(shape=(4, 4), dtype="float32"),
-            )
-
-        def false_fn():
-            return (
-                KerasTensor(shape=(3, 3), dtype="float32"),
-                # Mismatched tuple
-                KerasTensor(shape=(2, 2), dtype="float32"),
-            )
-
-        pred = KerasTensor(shape=(), dtype="bool")
-        expected_regex = "should return outputs of the same kind"
-
-        with self.assertRaisesRegex(ValueError, expected_regex):
-            cond_op(pred, true_fn, false_fn)
-
     def test_cond_output_spec_dtype_mismatch(self):
         cond_op = core.Cond()
 
@@ -1008,6 +964,58 @@ class CoreOpsCallsTests(testing.TestCase):
                 "a": KerasTensor(shape=(3, 3), dtype="float32"),
                 # Key 'b' has a mismatch in dtype
                 "b": KerasTensor(shape=(4, 4), dtype="int32"),
+            }
+
+        pred = KerasTensor(shape=(), dtype="bool")
+        expected_regex = "should return outputs of the same kind"
+
+        with self.assertRaisesRegex(ValueError, expected_regex):
+            cond_op(pred, true_fn, false_fn)
+
+    @pytest.mark.skipif(
+        backend.backend() == "jax",
+        reason="Incompatibilities with JAX's symbolic tensors",
+    )
+    def test_cond_output_spec_mismatch_in_tuple_elements(self):
+        cond_op = core.Cond()
+
+        def true_fn():
+            return (
+                KerasTensor(shape=(3, 3), dtype="float32"),
+                KerasTensor(shape=(4, 4), dtype="float32"),
+            )
+
+        def false_fn():
+            return (
+                KerasTensor(shape=(3, 3), dtype="float32"),
+                # Mismatched tuple
+                KerasTensor(shape=(2, 2), dtype="float32"),
+            )
+
+        pred = KerasTensor(shape=(), dtype="bool")
+        expected_regex = "should return outputs of the same kind"
+
+        with self.assertRaisesRegex(ValueError, expected_regex):
+            cond_op(pred, true_fn, false_fn)
+
+    @pytest.mark.skipif(
+        backend.backend() == "jax",
+        reason="Incompatibilities with JAX's symbolic tensors",
+    )
+    def test_cond_output_spec_mismatch_true_fn_tuple_false_fn_not_tuple(self):
+        cond_op = core.Cond()
+
+        def true_fn():
+            return (
+                KerasTensor(shape=(3, 3), dtype="float32"),
+                KerasTensor(shape=(4, 4), dtype="float32"),
+            )
+
+        def false_fn():
+            # not a tuple to match the true_fn
+            return {
+                "a": KerasTensor(shape=(3, 3), dtype="float32"),
+                "b": KerasTensor(shape=(4, 4), dtype="float32"),
             }
 
         pred = KerasTensor(shape=(), dtype="bool")
