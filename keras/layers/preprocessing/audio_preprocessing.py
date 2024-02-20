00ceef83c62f4a7b9a0028e86646915fcf653413
Awsaf <awsaf49@gmail.com>
2024-02-21 04:08:04 +0600

    update: docstring

--- keras/layers/preprocessing/audio_preprocessing.py
+++ keras/layers/preprocessing/audio_preprocessing.py
@@ -49,19 +49,18 @@ def linear_to_mel_weight_matrix(
     upper_edge_hertz=3800.0,
     dtype="float32",
 ):
-    r"""Returns a matrix to warp linear scale spectrograms
-        to the [mel scale][mel].
+    """Returns a matrix to warp linear scale spectrograms to the mel scale.
 
     Returns a weight matrix that can be used to re-weight a tensor containing
     `num_spectrogram_bins` linearly sampled frequency information from
     `[0, sampling_rate / 2]` into `num_mel_bins` frequency information from
-    `[lower_edge_hertz, upper_edge_hertz]` on the [mel scale][mel].
+    `[lower_edge_hertz, upper_edge_hertz]` on the mel scale.
 
-    This function follows the [Hidden Markov Model Toolkit
-    (HTK)](http://htk.eng.cam.ac.uk/) convention, defining the mel scale in
+    This function follows the [Hidden Markov Model Toolkit (HTK)](
+    http://htk.eng.cam.ac.uk/) convention, defining the mel scale in
     terms of a frequency in hertz according to the following formula:
 
-    `mel(f) = 2595 * log10( 1 + f/700)`
+    ```mel(f) = 2595 * log10( 1 + f/700)```
 
     In the returned matrix, all the triangles (filterbanks) have a peak value
     of 1.0.
@@ -86,6 +85,9 @@ def linear_to_mel_weight_matrix(
     M = keras.ops.tensordot(S, A, 1)
     ```
 
+    References:
+    - [Mel scale (Wikipedia)](https://en.wikipedia.org/wiki/Mel_scale)
+
     Args:
         num_mel_bins: Python int. How many bands in the resulting mel spectrum.
         num_spectrogram_bins: An integer `Tensor`. How many bins there are
@@ -105,8 +107,6 @@ def linear_to_mel_weight_matrix(
 
     Returns:
         A tensor of shape `[num_spectrogram_bins, num_mel_bins]`.
-
-    [mel]: https://en.wikipedia.org/wiki/Mel_scale
     """
 
     # This function can be constant folded by graph optimization since there are
