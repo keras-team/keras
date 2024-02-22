c2583f4db285d1579234d866be4e4d2d67593a8e
Neel Kovelamudi <nkovela@google.com>
2024-02-22 00:35:08 +0000

    Modify comment

--- keras/backend/common/variables.py
+++ keras/backend/common/variables.py
@@ -455,7 +455,7 @@ def standardize_shape(shape):
         if e is None:
             continue
         if config.backend() == "jax" and "_DimExpr" in str(type(e)):
-            # JAX2TF tracing represents `None` dimensions as `b`
+            # JAX2TF tracing uses JAX-native dimension expressions
             continue
         if not is_int_dtype(type(e)):
             raise ValueError(
