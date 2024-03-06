43f09f78015a3f9a275f1b6ac146a61612261b24
Hongyu Chiu <20734616+james77777778@users.noreply.github.com>
2024-03-06 17:47:15 +0800

    Remove unused code

--- keras/layers/preprocessing/index_lookup.py
+++ keras/layers/preprocessing/index_lookup.py
@@ -188,7 +188,6 @@ class IndexLookup(Layer):
         )
         kwargs.pop("trainable", None)
         kwargs.pop("dtype", None)
-        kwargs.pop("quantization_mode", None)
         if kwargs:
             raise ValueError(f"Unrecognized keyword argument(s): {kwargs}")
 
