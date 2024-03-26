08a9af5e7e58745aff4999fb1c290d036bb36c23
MaanasArora <maanasarora23@gmail.com>
2024-03-26 01:25:17 -0400

    Add unit test for CTC beam search decoding

--- keras/src/ops/nn_test.py
+++ keras/src/ops/nn_test.py
@@ -1937,6 +1937,34 @@ class NNOpsCorrectnessTest(testing.TestCase, parameterized.TestCase):
         self.assertAllClose(decoded, labels)
         self.assertAllClose(scores, score_labels)
 
+        if backend.backend() == "tensorflow":
+            labels = [
+                np.array([[1, 0], [2, 0], [1, 0]]),
+                np.array([[2], [2], [2]]),
+            ]
+            score_labels = np.array(
+                [
+                    [-3.055509, -3.090198],
+                    [-2.361756, -2.739681],
+                    [-1.172442, -1.372442],
+                ]
+            )
+
+            beam_width = 3
+            top_paths = 2
+
+            decoded, scores = knn.ctc_decode(
+                inputs,
+                sequence_lengths=[3, 3, 1],
+                strategy="beam_search",
+                beam_width=beam_width,
+                top_paths=top_paths,
+            )
+
+            for i in range(top_paths):
+                self.assertAllClose(decoded[i], labels[i])
+                self.assertAllClose(scores, score_labels)
+
     def test_normalize(self):
         x = np.array([[1, 2, 3], [1, 2, 3]], dtype=np.float32)
         self.assertAllClose(
