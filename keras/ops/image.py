9b6a114aa7e3f89ddf30f1bd930028f3ff3d2e2a
Vision <gitesh.ch.0912@gmail.com>
2024-03-28 09:30:06 +0530

    KerasTensor

--- keras/ops/image.py
+++ keras/ops/image.py
@@ -13,11 +13,13 @@ class RGB_to_Grayscale(Operation):
         data_format="channels_last",
     ):
         self.data_format = data_format
+
     def call(self, image):
         return backend.image.rgb_to_grayscale(
             image,
             data_format=self.data_format,
         )
+
     def compute_output_spec(self, image):
         if len(image.shape) not in (3, 4):
             raise ValueError(
@@ -25,6 +27,28 @@ class RGB_to_Grayscale(Operation):
                 "or rank 4 (batch of images). Received input with shape: "
                 f"image.shape={image.shape}"
             )
+
+        if len(image.shape) == 3:
+            if self.data_format == "channels_last":
+                return KerasTensor(image.shape[:-1] + (1,), dtype=image.dtype)
+            else:
+                return KerasTensor((1,) + image.shape[1:], dtype=image.dtype)
+        elif len(image.shape) == 4:
+            if self.data_format == "channels_last":
+                return KerasTensor(
+                    (image.shape[0],) + image.shape[1:-1] + (1,),
+                    dtype=image.dtype,
+                )
+            else:
+                return KerasTensor(
+                    (
+                        image.shape[0],
+                        1,
+                    )
+                    + image.shape[2:],
+                    dtype=image.dtype,
+                )
+
 
 @keras_export("keras.ops.image.rgb_to_grayscale")
 def rgb_to_grayscale(
@@ -78,7 +102,7 @@ def rgb_to_grayscale(
         data_format=data_format,
     )
 
-    
+
 class Resize(Operation):
     def __init__(
         self,
