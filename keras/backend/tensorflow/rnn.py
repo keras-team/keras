2f90dab483745168e8049da9d0ae8b912e5da715
Francois Chollet <francois.chollet@gmail.com>
2024-04-04 15:04:54 -0700

    fix cudnn support

--- keras/backend/tensorflow/rnn.py
+++ keras/backend/tensorflow/rnn.py
@@ -877,20 +877,7 @@ def _cudnn_lstm(
     return_sequences,
 ):
     if mask is not None:
-        is_valid_mask = _is_sequence_right_padded(mask, time_major)
-        tf.Assert(
-            is_valid_mask,
-            message=(
-                "You are passing a RNN mask that does not correspond to "
-                "right-padded sequences, while using cuDNN, which is not "
-                "supported. With cuDNN, RNN masks can only be used for "
-                "right-padding, e.g. `[[True, True, False, False]]` would "
-                "be a valid mask, but any mask that isn't just contiguous "
-                "`True`'s on the left and contiguous `False`'s on the right "
-                "would be invalid. You can pass `use_cudnn=False` to your "
-                "RNN layer to stop using cuDNN (this may be slower)."
-            ),
-        )
+        _assert_valid_mask(mask)
         sequence_lengths = _compute_sequence_length_from_mask(mask, time_major)
     else:
         sequence_lengths = None
