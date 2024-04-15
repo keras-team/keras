774f54b42c5f27864a1a5afcef691134009ff54c
sampathweb <1437573+sampathweb@users.noreply.github.com>
2024-04-15 18:21:05 -0500

    API Generator for Keras

--- shell/format.sh
+++ shell/format.sh
@@ -3,6 +3,10 @@ set -Eeuo pipefail
 
 base_dir=$(dirname $(dirname $0))
 
+echo "Generating public APIs..."
+python3 "${base_dir}"/api_gen.py
+
+echo "Formatting code..."
 isort --sp "${base_dir}/pyproject.toml" .
 
 black --config "${base_dir}/pyproject.toml" .
