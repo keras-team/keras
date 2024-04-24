6b62e916a062bd731dda3bd6c35865574d0c9510
Francois Chollet <francois.chollet@gmail.com>
2024-04-24 16:42:47 -0700

    Implement custom layer insertion in clone_model.

--- keras/src/models/cloning.py
+++ keras/src/models/cloning.py
@@ -11,7 +11,9 @@ from keras.src.saving import serialization_lib
 
 
 @keras_export("keras.models.clone_model")
-def clone_model(model, input_tensors=None, clone_function=None):
+def clone_model(
+    model, input_tensors=None, clone_function=None, call_function=None
+):
     """Clone a Functional or Sequential `Model` instance.
 
     Model cloning is similar to calling a model on new inputs,
@@ -29,24 +31,35 @@ def clone_model(model, input_tensors=None, clone_function=None):
         input_tensors: optional list of input tensors or InputLayer objects
             to build the model upon. If not provided,
             new `Input` objects will be created.
-        clone_function: Callable to be used to clone each layer in the target
+        clone_function: Callable with signature `fn(layer)`
+            to be used to clone each layer in the target
             model (except `Input` instances). It takes as argument the
             layer instance to be cloned, and returns the corresponding layer
             instance to be used in the model copy. If unspecified, this callable
-            becomes the following serialization/deserialization function:
+            defaults to the following serialization/deserialization function:
             `lambda layer: layer.__class__.from_config(layer.get_config())`.
             By passing a custom callable, you can customize your copy of the
             model, e.g. by wrapping certain layers of interest (you might want
             to replace all `LSTM` instances with equivalent
             `Bidirectional(LSTM(...))` instances, for example).
             Defaults to `None`.
+        call_function: Callable with signature
+            `fn(layer, *args, **kwargs)` to be used to call each
+            cloned layer and a set of inputs. It takes the layer instance,
+            the call arguments and keyword arguments, and returns the
+            call outputs. If unspecified, this callable defaults to
+            the regular `__call__()` method:
+            `def fn(layer, *args, **kwargs): return layer(*args, **kwargs)`.
+            By passing a custom callable, you can insert new layers before or
+            after a given layer. Note: this argument can only be used with
+            Functional models.
 
     Returns:
         An instance of `Model` reproducing the behavior
         of the original model, on top of new inputs tensors,
         using newly instantiated weights. The cloned model may behave
         differently from the original model if a custom `clone_function`
-        modifies the layer.
+        or `call_function` modifies a layer or layer call.
 
     Example:
 
@@ -74,6 +87,23 @@ def clone_model(model, input_tensors=None, clone_function=None):
     new_model = clone_model(model)
     ```
 
+    Using a `call_function` to add a `Dropout` layer after each `Dense` layer
+    (without recreating new layers):
+
+    ```python
+    def call_function(layer, *args, **kwargs):
+        out = layer(*args, **kwargs)
+        if isinstance(layer, keras.layers.Dense):
+            out = keras.layers.Dropout(0.5)(out)
+        return out
+
+    new_model = clone_model(
+        model,
+        clone_function=lambda x: x,  # Reuse the same layers.
+        call_function=call_function,
+    )
+    ```
+
     Note that subclassed models cannot be cloned by default,
     since their internal layer structure is not known.
     To achieve equivalent functionality
@@ -89,8 +119,20 @@ def clone_model(model, input_tensors=None, clone_function=None):
     `clone_function`.
     """
     if isinstance(model, Sequential):
+        if call_function is not None:
+            raise ValueError(
+                "`call_function` argument is not supported with Sequential "
+                "models.  In a Sequential model, layers aren't called "
+                "at model-construction time (they're merely listed). "
+                "Use `call_function` with Functional models only. "
+                "Received model of "
+                f"type '{model.__class__.__name__}', with "
+                f"call_function={clone_function}"
+            )
         return _clone_sequential_model(
-            model, input_tensors=input_tensors, clone_function=clone_function
+            model,
+            input_tensors=input_tensors,
+            clone_function=clone_function,
         )
     if isinstance(model, Functional):
         # If the get_config() method is the same as a regular Functional
@@ -106,17 +148,25 @@ def clone_model(model, input_tensors=None, clone_function=None):
                 model,
                 input_tensors=input_tensors,
                 clone_function=clone_function,
+                call_function=call_function,
             )
 
     # Case of a custom model class
     if clone_function or input_tensors:
         raise ValueError(
-            "Arguments clone_function and input_tensors "
+            "Arguments `clone_function` and `input_tensors` "
             "are only supported for Sequential models "
             "or Functional models. Received model of "
             f"type '{model.__class__.__name__}', with "
             f"clone_function={clone_function} and "
             f"input_tensors={input_tensors}"
+        )
+    if call_function is not None:
+        raise ValueError(
+            "Argument `call_function` is only supported "
+            "for Functional models. Received model of "
+            f"type '{model.__class__.__name__}', with "
+            f"call_function={clone_function}"
         )
     config = serialization_lib.serialize_keras_object(model)
     return serialization_lib.deserialize_keras_object(
@@ -202,7 +252,9 @@ def _clone_sequential_model(model, input_tensors=None, clone_function=None):
     return Sequential(new_layers, name=model.name, trainable=model.trainable)
 
 
-def _clone_functional_model(model, input_tensors=None, clone_function=None):
+def _clone_functional_model(
+    model, input_tensors=None, clone_function=None, call_function=None
+):
     """Clone a `Functional` model instance.
 
     Model cloning is similar to calling a model on new inputs,
@@ -276,7 +328,9 @@ def _clone_functional_model(model, input_tensors=None, clone_function=None):
         return new_layer
 
     output_tensors = model._run_through_graph(
-        input_tensors, operation_fn=operation_fn
+        input_tensors,
+        operation_fn=operation_fn,
+        call_fn=call_function,
     )
 
     if functional_like_constructor(model.__class__):
