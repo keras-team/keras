c8bc01527b48d91f2437046675f1153b60d033d2
Kartheek L <lkarthee@users.noreply.github.com>
2024-05-27 01:51:58 +0530

    linalg ops fix (#19763)

--- keras/src/backend/mlx/linalg.py
+++ keras/src/backend/mlx/linalg.py
@@ -4,9 +4,52 @@ from keras.src.backend.common import standardize_dtype
 from keras.src.backend.mlx.core import convert_to_tensor
 
 
+def det(a):
+    raise NotImplementedError("det not yet implemented in mlx.")
+
+
+def eig(a):
+    raise NotImplementedError("eig not yet implemented in mlx.")
+
+
+def eigh(a):
+    raise NotImplementedError("eigh not yet implemented in mlx.")
+
+
+def lu_factor(a):
+    raise NotImplementedError("lu_factor not yet implemented in mlx.")
+
+
+def solve(a, b):
+    raise NotImplementedError("solve_triangular not yet implemented in mlx.")
+
+
+def solve_triangular(a, b, lower=False):
+    raise NotImplementedError("solve_triangular not yet implemented in mlx.")
+
+
+def qr(x, mode="reduced"):
+    return mx.linalg.qr(x)
+
+
+def svd(x, full_matrices=True, compute_uv=True):
+    with mx.stream(mx.cpu):
+        return mx.linalg.svd(x)
+
+
+def cholesky(a):
+    with mx.stream(mx.cpu):
+        return mx.linalg.cholesky(a)
+
+
 def norm(x, ord=None, axis=None, keepdims=False):
     dtype = standardize_dtype(x.dtype)
     if "int" in dtype or dtype == "bool":
         dtype = dtypes.result_type(x.dtype, "float32")
     x = convert_to_tensor(x, dtype=dtype)
     return mx.linalg.norm(x, ord=ord, axis=axis, keepdims=keepdims)
+
+
+def inv(a):
+    with mx.stream(mx.cpu):
+        return mx.linalg.inv(a)
