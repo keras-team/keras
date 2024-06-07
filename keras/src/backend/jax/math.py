Fix bug in rank computation.

keras/src/backend/jax/math.py
@@ -43,7 +43,7 @@ def in_top_k(targets, predictions, k):
     preds_at_label = jnp.take_along_axis(
         predictions, jnp.expand_dims(targets, axis=-1), axis=-1
     )
-    rank = jnp.sum(jnp.greater_equal(predictions, preds_at_label), axis=-1)
+    rank = 1 + jnp.sum(jnp.greater(predictions, preds_at_label), axis=-1)
     return jnp.less_equal(rank, k)
 
 