05029516f6cfe7fa85bec252ea7ea74654053b60
pandyah5 <pandyahetav1@gmail.com>
2024-06-11 09:55:36 -0700

    Added shape info in mask error message to help with degugging

--- keras/src/layers/normalization/batch_normalization.py
+++ keras/src/layers/normalization/batch_normalization.py
@@ -219,7 +219,8 @@ class BatchNormalization(Layer):
             if len(mask.shape) != len(inputs.shape) - 1:
                 # Raise a value error
                 raise ValueError(
-                    "The mask provided should be one dimension less than the inputs."
+                    "The mask provided should be one dimension less than the inputs. "
+                    f"Received: mask.shape={mask.shape}, inputs.shape={inputs.shape}"
                 )
 
         input_dtype = standardize_dtype(inputs.dtype)
