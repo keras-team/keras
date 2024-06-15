ec1e9bd10218aa42b2b7ad48f5dfb2e8969c8582
james77777778 <20734616+james77777778@users.noreply.github.com>
2024-06-16 02:32:19 +0800

    Add `rgb_to_hsv` and `hsv_to_rgb` (#19850)

--- keras/src/backend/tensorflow/image.py
+++ keras/src/backend/tensorflow/image.py
@@ -29,7 +29,7 @@ def rgb_to_grayscale(images, data_format=None):
     # Convert to floats
     original_dtype = images.dtype
     compute_dtype = backend.result_type(images.dtype, float)
-    images = backend.cast(images, compute_dtype)
+    images = tf.cast(images, compute_dtype)
 
     # Ref: tf.image.rgb_to_grayscale
     rgb_weights = convert_to_tensor(
@@ -38,6 +38,64 @@ def rgb_to_grayscale(images, data_format=None):
     images = tf.tensordot(images, rgb_weights, axes=(channels_axis, -1))
     images = tf.expand_dims(images, axis=channels_axis)
     return tf.cast(images, original_dtype)
+
+
+def rgb_to_hsv(images, data_format=None):
+    images = convert_to_tensor(images)
+    dtype = images.dtype
+    data_format = backend.standardize_data_format(data_format)
+    if len(images.shape) not in (3, 4):
+        raise ValueError(
+            "Invalid images rank: expected rank 3 (single image) "
+            "or rank 4 (batch of images). Received input with shape: "
+            f"images.shape={images.shape}"
+        )
+    if not backend.is_float_dtype(dtype):
+        raise ValueError(
+            "Invalid images dtype: expected float dtype. "
+            f"Received: images.dtype={backend.standardize_dtype(dtype)}"
+        )
+    if data_format == "channels_first":
+        if len(images.shape) == 4:
+            images = tf.transpose(images, (0, 2, 3, 1))
+        else:
+            images = tf.transpose(images, (1, 2, 0))
+    images = tf.image.rgb_to_hsv(images)
+    if data_format == "channels_first":
+        if len(images.shape) == 4:
+            images = tf.transpose(images, (0, 3, 1, 2))
+        elif len(images.shape) == 3:
+            images = tf.transpose(images, (2, 0, 1))
+    return images
+
+
+def hsv_to_rgb(images, data_format=None):
+    images = convert_to_tensor(images)
+    dtype = images.dtype
+    data_format = backend.standardize_data_format(data_format)
+    if len(images.shape) not in (3, 4):
+        raise ValueError(
+            "Invalid images rank: expected rank 3 (single image) "
+            "or rank 4 (batch of images). Received input with shape: "
+            f"images.shape={images.shape}"
+        )
+    if not backend.is_float_dtype(dtype):
+        raise ValueError(
+            "Invalid images dtype: expected float dtype. "
+            f"Received: images.dtype={backend.standardize_dtype(dtype)}"
+        )
+    if data_format == "channels_first":
+        if len(images.shape) == 4:
+            images = tf.transpose(images, (0, 2, 3, 1))
+        else:
+            images = tf.transpose(images, (1, 2, 0))
+    images = tf.image.hsv_to_rgb(images)
+    if data_format == "channels_first":
+        if len(images.shape) == 4:
+            images = tf.transpose(images, (0, 3, 1, 2))
+        elif len(images.shape) == 3:
+            images = tf.transpose(images, (2, 0, 1))
+    return images
 
 
 def resize(
