Fixing issue #20021: Unexpected result on keras.ops.nn.softmax when running with tuple axis

keras/src/ops/nn_test.py
@@ -1252,7 +1252,7 @@ class NNOpsCorrectnessTest(testing.TestCase, parameterized.TestCase):
 
     def test_softmax_correctness_with_axis_tuple(self):
         if backend.backend() == "torch":
-            pass
+            pytest.skip("unsupported tuple dim by torch")
 
         input = np.array([[[1.0, 2.0], [3.0, 4.0]], [[5.0, 6.0], [7.0, 8.0]]])
         combination = combinations(range(3), 2)