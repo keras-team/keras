Fixing issue #20021: Unexpected result on keras.ops.nn.softmax when running with tuple axis

keras/src/ops/nn_test.py
@@ -1,7 +1,8 @@
+from itertools import combinations
+
 import numpy as np
 import pytest
 from absl.testing import parameterized
-from itertools import combinations
 
 import keras
 from keras.src import backend
@@ -1429,9 +1430,9 @@ class NNOpsCorrectnessTest(testing.TestCase, parameterized.TestCase):
     @parameterized.product(strides=(1, 2), dilation_rate=(1, (2, 1)))
     def test_conv_2d_group_2(self, strides, dilation_rate):
         if (
-                backend.backend() == "tensorflow"
-                and strides == 2
-                and dilation_rate == (2, 1)
+            backend.backend() == "tensorflow"
+            and strides == 2
+            and dilation_rate == (2, 1)
         ):
             # This case is not supported by the TF backend.
             return
@@ -1508,9 +1509,9 @@ class NNOpsCorrectnessTest(testing.TestCase, parameterized.TestCase):
     )
     def test_depthwise_conv_2d(self, strides, padding, dilation_rate):
         if (
-                backend.backend() == "tensorflow"
-                and strides == (2, 2)
-                and dilation_rate == (2, 2)
+            backend.backend() == "tensorflow"
+            and strides == (2, 2)
+            and dilation_rate == (2, 2)
         ):
             # This case is not supported by the TF backend.
             return
@@ -1547,9 +1548,9 @@ class NNOpsCorrectnessTest(testing.TestCase, parameterized.TestCase):
     )
     def test_separable_conv_2d(self, strides, padding, dilation_rate):
         if (
-                backend.backend() == "tensorflow"
-                and strides == 2
-                and dilation_rate == (2, 2)
+            backend.backend() == "tensorflow"
+            and strides == 2
+            and dilation_rate == (2, 2)
         ):
             # This case is not supported by the TF backend.
             return
@@ -1868,7 +1869,7 @@ class NNOpsCorrectnessTest(testing.TestCase, parameterized.TestCase):
     @pytest.mark.skipif(
         backend.backend() != "tensorflow",
         reason="synchronized=True only implemented for TF backend",
-        )
+    )
     def test_moments_sync(self):
         # Test batch statistics for 4D moments (batch, height, width, channels)
         x = np.random.uniform(size=(2, 28, 28, 3)).astype(np.float32)
@@ -1898,7 +1899,7 @@ class NNOpsCorrectnessTest(testing.TestCase, parameterized.TestCase):
     @pytest.mark.skipif(
         backend.backend() != "tensorflow",
         reason="synchronized=True only implemented for TF backend",
-        )
+    )
     def test_moments_sync_with_distribution_strategy(self, dtype):
         from keras.src.utils.module_utils import tensorflow as tf
 
@@ -2509,25 +2510,25 @@ class NNOpsBehaviorTest(testing.TestCase, parameterized.TestCase):
     def test_normalize_order_validation(self):
         # Test with a non-integer order
         with self.assertRaisesRegex(
-                ValueError, "Argument `order` must be an int >= 1"
+            ValueError, "Argument `order` must be an int >= 1"
         ):
             knn.normalize(np.array([1, 2, 3]), order="a")
 
         # Test with a negative integer
         with self.assertRaisesRegex(
-                ValueError, "Argument `order` must be an int >= 1"
+            ValueError, "Argument `order` must be an int >= 1"
         ):
             knn.normalize(np.array([1, 2, 3]), order=-1)
 
         # Test with zero
         with self.assertRaisesRegex(
-                ValueError, "Argument `order` must be an int >= 1"
+            ValueError, "Argument `order` must be an int >= 1"
         ):
             knn.normalize(np.array([1, 2, 3]), order=0)
 
         # Test with a floating-point number
         with self.assertRaisesRegex(
-                ValueError, "Argument `order` must be an int >= 1"
+            ValueError, "Argument `order` must be an int >= 1"
         ):
             knn.normalize(np.array([1, 2, 3]), order=2.5)
 
@@ -2536,7 +2537,7 @@ class NNOpsBehaviorTest(testing.TestCase, parameterized.TestCase):
         name2, shape2 = "logits", (3, 4, 5)
         ctc_loss_instance = knn.CTCLoss(mask_index=-1)
         with self.assertRaisesRegex(
-                ValueError, "must have the same first dimension"
+            ValueError, "must have the same first dimension"
         ):
             ctc_loss_instance._check_shape_first_dim(
                 name1, shape1, name2, shape2