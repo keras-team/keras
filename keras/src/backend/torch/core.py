b85685bf89c0fcaee5112524cc9df54a76d7b200
Ali Boubezari <aboubezari@nuro.ai>
2024-07-24 14:16:44 -0700

    reformat

--- keras/src/backend/torch/core.py
+++ keras/src/backend/torch/core.py
@@ -240,7 +240,7 @@ def convert_to_numpy(x):
             if x.requires_grad:
                 x = x.detach()
             # Tensor has to be moved to CPU before converting to numpy.
-            if x.device != torch.device('cpu'):
+            if x.device != torch.device("cpu"):
                 x = x.cpu()
             if x.dtype == torch.bfloat16:
                 # Attempting to call .numpy() on a bfloat16 torch tensor leads
