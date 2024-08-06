ef78c5ed607442a88369a20a0d809a1a5580b215
ghsanti <125615505+ghsanti@users.noreply.github.com>
2024-08-06 05:28:39 +0000

    chore: replace os by path and rename function to read_name

--- setup.py
+++ setup.py
@@ -1,28 +1,26 @@
 """Setup script."""
 
-import os
-import pathlib
+from pathlib import Path
 
 from setuptools import find_packages
 from setuptools import setup
 
 
-def read(rel_path):
-    here = os.path.abspath(os.path.dirname(__file__))
-    with open(os.path.join(here, rel_path)) as fp:
+def read_text(rel_path):
+    here = Path(__file__).parent.absolute()
+    with open(here / rel_path) as fp:
         return fp.read()
 
 
 def get_version(rel_path):
-    for line in read(rel_path).splitlines():
+    for line in read_text(rel_path).splitlines():
         if line.startswith("__version__"):
             delim = '"' if '"' in line else "'"
             return line.split(delim)[1]
     raise RuntimeError("Unable to find version string.")
 
 
-HERE = pathlib.Path(__file__).parent
-README = (HERE / "README.md").read_text()
+README = read_text("README.md")
 VERSION = get_version("keras/src/version.py")
 
 setup(
