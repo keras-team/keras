7e61d6bc1b91f13f30df1202e3cd789df67783b7
Mansi Mehta <mehtamansi@google.com>
2024-08-09 22:31:27 +0530

    Updated nestedstructure testcase with keras.ops.convert_to_numpy

--- keras/src/backend/tensorflow/nestedstructure_test.py
+++ keras/src/backend/tensorflow/nestedstructure_test.py
@@ -1,16 +1,16 @@
-import os
-
 import numpy as np
-import tensorflow as tf
 import pytest
-from keras.src import testing
-from keras.src import backend
+import tensorflow as tf
+
 import keras
+from keras.src import backend
+from keras.src import testing
+
+
 @pytest.mark.skipif(
     backend.backend() != "tensorflow",
     reason="The nestedstructure test can only run with TF backend.",
 )
-
 def dict_input_fn(inputs):
     x = inputs["x"][:, 0]
     y = inputs["y"] + 1
@@ -41,9 +41,15 @@ class NestedTest(testing.TestCase):
     def test_dict_input_fn_outputs(self):
         ys = keras.ops.map(dict_input_fn, self.xs)
         self.assertEqual(ys["x"].shape, (4, 100))
-        self.assertTrue((ys["y"] == self.xs["y"] + 1).numpy().all())
+        self.assertEqual(
+            keras.ops.convert_to_numpy(ys["y"]).all(),
+            keras.ops.convert_to_numpy(self.xs["y"] + 1).all(),
+        )
 
     def test_list_input_fn_outputs(self):
         ys = keras.ops.map(list_input_fn, self.xs1)
         for i, (x, y) in enumerate(zip(self.xs1, ys)):
-            self.assertTrue((y.numpy() == x.numpy() ** 2).all())
+            self.assertEqual(
+                (keras.ops.convert_to_numpy(y)).all(),
+                (keras.ops.convert_to_numpy(x) ** 2).all(),
+            )
