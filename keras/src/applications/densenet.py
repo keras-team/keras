d00cbe5a3995e2f24afae696064e09610925c6fd
François Chollet <francois.chollet@gmail.com>
2024-08-30 03:57:32 -0700

    Update densenet.py

--- keras/src/applications/densenet.py
+++ keras/src/applications/densenet.py
@@ -470,7 +470,7 @@ Args:
             be applied.
     classes: optional number of classes to classify images
         into, only to be specified if `include_top` is `True`, and
-        if no `weights` argument is specified.Default to 1000
+        if no `weights` argument is specified. Defaults to 1000.
     classifier_activation: A `str` or callable.
         The activation function to use
         on the "top" layer. Ignored unless `include_top=True`. Set
