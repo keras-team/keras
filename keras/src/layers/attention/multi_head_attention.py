8cbbfcce674d18a2468f075bdff7439a4c8dd1ec
Sanskar Modi <sansyprog8@gmail.com>
2024-09-08 12:39:10 +0530

    Fixed issue #20220 to optimize einsum equation in multi-head attention

--- keras/src/layers/attention/multi_head_attention.py
+++ keras/src/layers/attention/multi_head_attention.py
@@ -734,20 +734,22 @@ def _build_proj_equation(free_dims, bound_dims, output_dims):
         output_str += char
 
     letter_offset += free_dims
-    for i in range(bound_dims):
-        char = _index_to_einsum_variable(i + letter_offset)
-        input_str += char
-        kernel_str += char
-
-    letter_offset += bound_dims
-    for i in range(output_dims):
+    for i in range(output_dims):  
         char = _index_to_einsum_variable(i + letter_offset)
         kernel_str += char
         output_str += char
         bias_axes += char
+
+    letter_offset += output_dims
+    for i in range(bound_dims):  
+        char = _index_to_einsum_variable(i + letter_offset)
+        input_str += char
+        kernel_str += char
+
     equation = f"{input_str},{kernel_str}->{output_str}"
 
     return equation, bias_axes, len(output_str)
+
 
 
 def _get_output_shape(output_rank, known_last_dims):
