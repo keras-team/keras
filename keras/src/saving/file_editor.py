36eabac69c3fc411a34365ffaed882777336fc8f
ugeunpark <pwg2004@gmail.com>
2024-09-14 22:13:03 +0900

    Add list_layer method for cli & remove matplotlib and implement list layer method for html by pure html

--- keras/src/saving/file_editor.py
+++ keras/src/saving/file_editor.py
@@ -1,26 +1,20 @@
-import base64
-import io
 import json
 import zipfile
 
 import h5py
 
+from keras.src.saving import deserialize_keras_object
+from keras.src.saving.saving_lib import H5IOStore
+
 try:
     import IPython as ipython
 except ImportError:
-    "You must install ipython (`pip install ipython`) for"
-    "KerasFileEditor to work."
     ipython = None
 
-try:
-    import matplotlib as mpl
-except ImportError:
-    "You must install matplotlib (`pip install matplotlib`) for"
-    "KerasFileEditor to work."
-    mpl = None
 
-from keras.src.saving import deserialize_keras_object
-from keras.src.saving.saving_lib import H5IOStore
+def check_ipython():
+    return ipython is not None
+
 
 _CONFIG_FILENAME = "config.json"
 _METADATA_FILENAME = "metadata.json"
@@ -60,7 +54,10 @@ class KerasFileEditor:
                 if isinstance(value, h5py.Group) and len(value) == 0:
                     continue
                 if hasattr(value, "keys"):
-                    result[key] = _extract_values(value)
+                    if "vars" in value.keys():
+                        result[key] = _extract_values(value["vars"])
+                    else:
+                        result[key] = _extract_values(value)
                 else:
                     result[key] = value
             return result
@@ -79,107 +76,142 @@ class KerasFileEditor:
             with zf.open(_METADATA_FILENAME, "r") as f:
                 self.metadata = json.loads(f.read())
 
-    def list_layers(self):
-        margin_left = 20
-        font_size = 20
-        print_prefix = (
-            f'<details style="margin-left: {margin_left}px;">'
-            + f'<summary style="font-size: {font_size}px;">'
-            + "{key}</summary>"
+    def _generate_filepath_info(self):
+        output = f"Keras model file '{self.filepath}'"
+        return output
+
+    def _generate_config_info(self):
+        output = (
+            f"Model: {self.config['class_name']} "
+            + f"'name='{self.config['config']['name']}'"
         )
-        print_suffix = "</details>"
+        return output
 
-        def _create_color_grid(weights):
-            fig, ax = mpl.pyplot.subplots(figsize=(5, 5))
-            if weights.ndim > 2:
-                weights = weights.reshape(-1, weights.shape[-1])
+    def _generate_metadata_info(self):
+        output = [
+            f"Saved with Keras {self.metadata['keras_version']}",
+            f"Date saved: {self.metadata['date_saved']}",
+        ]
+        return output
 
-            if weights.ndim == 1:
-                cax = ax.imshow(
-                    weights.reshape(1, -1), aspect="auto", cmap="viridis"
-                )
-            else:
-                cax = ax.matshow(
-                    weights, interpolation="nearest", cmap="viridis"
-                )
-                if (
-                    weights.shape[0] / weights.shape[1] > 10
-                    or weights.shape[1] / weights.shape[0] > 10
-                ):
-                    ax.set_aspect("auto", "datalim")
-                if weights.shape[0] == 1:
-                    ax.set_yticks([])
-                if weights.shape[1] == 1:
-                    ax.set_xticks([])
+    def list_layers_for_cli(self):
+        def _print_filepath():
+            print(self._generate_filepath_info())
 
-            fig.colorbar(cax)
-            ax.xaxis.set_label_position("top")
-            ax.xaxis.tick_top()
-            ax.set_xticks([])
-            ax.set_yticks([])
+        def _print_config():
+            print(self._generate_config_info())
 
-            buf = io.BytesIO()
-            mpl.pyplot.savefig(
-                buf, format="png", bbox_inches="tight", pad_inches=1
-            )
-            mpl.pyplot.close(fig)
-            buf.seek(0)
-            image_base64 = base64.b64encode(buf.read()).decode("utf-8")
-            return (
-                f'<img src="data:image/png;base64,{image_base64}" '
-                + 'style="display:block;margin:auto;" />'
-            )
+        def _print_metadata():
+            for meta_info in self._generate_metadata_info():
+                print(meta_info)
 
-        def _generate_html_config(config):
-            html_output = [
-                f"<p>Model: {config['class_name']} "
-                + f"'name='{config['config']['name']}'</p>"
-            ]
-            return "".join(html_output)
+        def _print_layer_structure(
+            dictionary, indent=0, is_last=True, prefix=""
+        ):
+            for idx, (key, value) in enumerate(dictionary.items()):
+                is_last_item = idx == len(dictionary) - 1
+                connector = "└─ " if is_last_item else "├─ "
 
-        def _generate_html_metadata(metadata):
-            html_output = [
-                f"<p>Saved with Keras {metadata['keras_version']}</p>",
-                f"<p>Date saved: {metadata['date_saved']}</p>",
-            ]
-            return "".join(html_output)
-
-        def _generate_html_weight(weight_dict):
-            html_output = []
-            for key, value in weight_dict.items():
-                html_output.append(print_prefix.format(key=key))
                 if isinstance(value, dict):
-                    if "vars" in value.keys():
-                        for weights_key, weights in value["vars"].items():
-                            html_output.append(
-                                print_prefix.format(
-                                    key=f"{weights_key} : "
-                                    + f"shape={weights.shape}, "
-                                    + f"dtype={weights.dtype}"
-                                )
-                            )
-
-                            html_output.append(_create_color_grid(weights[()]))
-
-                            html_output.append(print_suffix)
+                    print(f"{prefix}{connector}{key}")
+                    new_prefix = prefix + ("    " if is_last_item else "│   ")
+                    _print_layer_structure(
+                        value,
+                        indent + 1,
+                        is_last=is_last_item,
+                        prefix=new_prefix,
+                    )
+                else:
+                    if isinstance(value, h5py.Dataset):
+                        print(
+                            f"{prefix}{connector}{key}:"
+                            + f" shape={value.shape}, dtype={value.dtype}"
+                        )
                     else:
-                        html_output.append(_generate_html_weight(value))
-                html_output.append(print_suffix)
+                        print(f"{prefix}{connector}{key}: {value}")
 
-            return "".join(html_output)
+        def _print_layer():
+            print("Layers")
+            _print_layer_structure(self.nested_dict["layers"], prefix=" " * 2)
 
-        output = f"<h2>Keras model file '{self.filepath}'</h2>"
+        _print_filepath()
 
         if self.config is not None:
-            config_output = _generate_html_config(self.config)
-            output += config_output
+            _print_config()
 
         if self.metadata is not None:
-            metadata_output = _generate_html_metadata(self.metadata)
-            output += metadata_output
+            _print_metadata()
 
-        layer_output = "<h3>Weights file:</h3>"
-        layer_output += _generate_html_weight(self.nested_dict["layers"])
-        output += layer_output
+        _print_layer()
 
-        ipython.core.display.display(ipython.core.display.HTML(output))
+    def list_layers_for_html(self):
+        if not check_ipython():
+            message = (
+                "You must install ipython (`pip install ipython`) for"
+                + "KerasFileEditor to work."
+            )
+            raise ImportError(message)
+
+        def _generate_html_filepath():
+            output = f"<h2>{self._generate_filepath_info()}</h2>"
+            return output
+
+        def _generate_html_config():
+            output = f"<p>{self._generate_config_info()}</p>"
+            return output
+
+        def _generate_html_metadata():
+            output = [
+                f"<p>{meta_info}</p>"
+                for meta_info in self._generate_metadata_info()
+            ]
+            output = "".join(output)
+            return output
+
+        def _generate_html_weight(dictionary, margin_left=0, font_size=20):
+            html = ""
+            for key, value in dictionary.items():
+                if isinstance(value, dict) and value:
+                    html += (
+                        f'<details style="margin-left: {margin_left}px;">'
+                        + f'<summary style="font-size: {font_size}px;">'
+                        + f"{key}</summary>"
+                        + _generate_html_weight(
+                            value, margin_left + 20, font_size - 1
+                        )
+                        + "</details>"
+                    )
+                else:
+                    if isinstance(value, h5py.Dataset):
+                        html += (
+                            f'<details style="margin-left: {margin_left}px;">'
+                            + f'<summary style="font-size: {font_size}px;">'
+                            + f"{key} : shape={value.shape}"
+                            + f", dtype={value.dtype}</summary>"
+                            + "</details>"
+                        )
+                    else:
+                        html += (
+                            f'<details style="margin-left: {margin_left}px;">'
+                            + f'<summary style="font-size: {font_size}px;">'
+                            + f"{key} </summary>"
+                            + "</details>"
+                        )
+            return html
+
+        def _generate_html_layer():
+            output = "<p>Layers</p>"
+            output += _generate_html_weight(self.nested_dict["layers"])
+            return output
+
+        output = _generate_html_filepath()
+
+        if self.config is not None:
+            output += _generate_html_config()
+
+        if self.metadata is not None:
+            output += _generate_html_metadata()
+
+        output += _generate_html_layer()
+
+        ipython.display.display(ipython.display.HTML(output))
