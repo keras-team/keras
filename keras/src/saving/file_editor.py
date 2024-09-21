d47dc6b8fecef93f1f762e154710eb956385f82e
Francois Chollet <francois.chollet@gmail.com>
2024-09-21 13:12:55 -0700

    Improve file editor

--- keras/src/saving/file_editor.py
+++ keras/src/saving/file_editor.py
@@ -1,10 +1,13 @@
+import collections
 import json
+import pprint
 import zipfile
 
 import h5py
 
-from keras.src.saving import deserialize_keras_object
+from keras.src.saving import saving_lib
 from keras.src.saving.saving_lib import H5IOStore
+from keras.src.utils import io_utils
 
 try:
     import IPython as ipython
@@ -12,32 +15,60 @@ except ImportError:
     ipython = None
 
 
-def check_ipython():
-    return ipython is not None
+def is_ipython_notebook():
+    """Checks if the code is being executed in a notebook."""
+    try:
+        from IPython import get_ipython
 
-
-_CONFIG_FILENAME = "config.json"
-_METADATA_FILENAME = "metadata.json"
-_VARS_FNAME = "model.weights"
+        # Check if an active IPython shell exists.
+        if get_ipython() is not None:
+            return True
+        return False
+    except ImportError:
+        return False
 
 
 class KerasFileEditor:
-    def __init__(self, filepath, reference_model=None, custom_objects=None):
+    def __init__(
+        self,
+        filepath,
+        reference_model=None,
+        custom_objects=None,
+        safe_mode=True,
+    ):
         self.filepath = filepath
         self.custom_objects = custom_objects
         self.metadata = None
-        self.reference_model = None
         self.config = None
+        self.model = None
+        self.reference_model = reference_model
 
         if filepath.endswith(".keras"):
-            self._init_for_keras_format(
-                custom_objects, filepath, reference_model
-            )
+            zf = zipfile.ZipFile(filepath, "r")
             weights_store = H5IOStore(
-                _VARS_FNAME + ".h5",
-                archive=zipfile.ZipFile(filepath, "r"),
+                saving_lib._VARS_FNAME + ".h5",
+                archive=zf,
                 mode="r",
             )
+            with zf.open(saving_lib._CONFIG_FILENAME, "r") as f:
+                config_json = f.read()
+            with zf.open(saving_lib._METADATA_FILENAME, "r") as f:
+                metadata_json = f.read()
+            self.config = json.loads(config_json)
+            self.metadata = json.loads(metadata_json)
+
+            try:
+                self.model = saving_lib.load_model(
+                    filepath,
+                    custom_objects=custom_objects,
+                    compile=True,
+                    safe_mode=safe_mode,
+                )
+            except Exception as e:
+                io_utils.print_msg(
+                    f"Model file could not be loaded. Error: {e}"
+                )
+
         elif filepath.endswith(".weights.h5"):
             weights_store = H5IOStore(filepath, mode="r")
         else:
@@ -47,110 +78,78 @@ class KerasFileEditor:
                 f"Received: filepath={filepath}"
             )
 
-        def _extract_values(data):
-            result = {}
-            for key in data.keys():
-                value = data[key]
-                if isinstance(value, h5py.Group) and len(value) == 0:
-                    continue
-                if hasattr(value, "keys"):
-                    if "vars" in value.keys():
-                        result[key] = _extract_values(value["vars"])
-                    else:
-                        result[key] = _extract_values(value)
-                else:
-                    result[key] = value
-            return result
-
-        self.nested_dict = _extract_values(weights_store.h5_file)
-
-    def _init_for_keras_format(self, custom_objects, filepath, reference_model):
-        with zipfile.ZipFile(filepath, "r") as zf:
-            with zf.open(_CONFIG_FILENAME, "r") as f:
-                self.config = json.loads(f.read())
-            if reference_model is None:
-                self.reference_model = deserialize_keras_object(
-                    self.config, custom_objects=custom_objects
-                )
-
-            with zf.open(_METADATA_FILENAME, "r") as f:
-                self.metadata = json.loads(f.read())
-
-    def _generate_filepath_info(self):
-        output = f"Keras model file '{self.filepath}'"
-        return output
-
-    def _generate_config_info(self):
-        output = (
-            f"Model: {self.config['class_name']} "
-            + f"'name='{self.config['config']['name']}'"
+        self.weights_dict = self._extract_weights_from_store(
+            weights_store.h5_file
         )
-        return output
 
-    def _generate_metadata_info(self):
-        output = [
-            f"Saved with Keras {self.metadata['keras_version']}",
-            f"Date saved: {self.metadata['date_saved']}",
-        ]
-        return output
+    def _extract_weights_from_store(self, data):
+        result = collections.OrderedDict()
+        for key in data.keys():
+            value = data[key]
+            if isinstance(value, h5py.Group):
+                if len(value) == 0:
+                    continue
+                if "vars" in value.keys() and len(value["vars"]) == 0:
+                    continue
 
-    def list_layers_for_cli(self):
-        def _print_filepath():
-            print(self._generate_filepath_info())
-
-        def _print_config():
-            print(self._generate_config_info())
-
-        def _print_metadata():
-            for meta_info in self._generate_metadata_info():
-                print(meta_info)
-
-        def _print_layer_structure(
-            dictionary, indent=0, is_last=True, prefix=""
-        ):
-            for idx, (key, value) in enumerate(dictionary.items()):
-                is_last_item = idx == len(dictionary) - 1
-                connector = "└─ " if is_last_item else "├─ "
-
-                if isinstance(value, dict):
-                    print(f"{prefix}{connector}{key}")
-                    new_prefix = prefix + ("    " if is_last_item else "│   ")
-                    _print_layer_structure(
-                        value,
-                        indent + 1,
-                        is_last=is_last_item,
-                        prefix=new_prefix,
+            if hasattr(value, "keys"):
+                if "vars" in value.keys():
+                    result[key] = self._extract_weights_from_store(
+                        value["vars"]
                     )
                 else:
-                    if isinstance(value, h5py.Dataset):
-                        print(
-                            f"{prefix}{connector}{key}:"
-                            + f" shape={value.shape}, dtype={value.dtype}"
-                        )
-                    else:
-                        print(f"{prefix}{connector}{key}: {value}")
+                    result[key] = self._extract_weights_from_store(value)
+            else:
+                result[key] = value
+        return result
 
-        def _print_layer():
-            print("Layers")
-            _print_layer_structure(self.nested_dict["layers"], prefix=" " * 2)
+    def _generate_filepath_info(self):
+        return f"Keras model file '{self.filepath}'"
 
-        _print_filepath()
+    def _generate_config_info(self):
+        return pprint.pformat(self.config)
 
-        if self.config is not None:
-            _print_config()
+    def _generate_metadata_info(self):
+        return (
+            f"Saved with Keras {self.metadata['keras_version']} "
+            f"- date: {self.metadata['date_saved']}"
+        )
+
+    def _print_weights_structure(
+        self, weights_dict, indent=0, is_last=True, prefix=""
+    ):
+        for idx, (key, value) in enumerate(weights_dict.items()):
+            is_last_item = idx == len(weights_dict) - 1
+            connector = "└─ " if is_last_item else "├─ "
+
+            if isinstance(value, dict):
+                io_utils.print_msg(f"{prefix}{connector}{key}")
+                new_prefix = prefix + ("    " if is_last_item else "│   ")
+                self._print_weights_structure(
+                    value,
+                    indent + 1,
+                    is_last=is_last_item,
+                    prefix=new_prefix,
+                )
+            else:
+                if isinstance(value, h5py.Dataset):
+                    io_utils.print_msg(
+                        f"{prefix}{connector}{key}:"
+                        + f" shape={value.shape}, dtype={value.dtype}"
+                    )
+                else:
+                    io_utils.print_msg(f"{prefix}{connector}{key}: {value}")
+
+    def _list_layers_cli(self):
+        io_utils.print_msg(self._generate_filepath_info())
 
         if self.metadata is not None:
-            _print_metadata()
+            io_utils.print_msg(self._generate_metadata_info())
 
-        _print_layer()
+        io_utils.print_msg("Weights structure")
+        self._print_weights_structure(self.weights_dict, prefix=" " * 2)
 
-    def list_layers_for_html(self):
-        if not check_ipython():
-            message = (
-                "You must install ipython (`pip install ipython`) for"
-                + "KerasFileEditor to work."
-            )
-            raise ImportError(message)
+    def _list_layers_iteractive(self):
 
         def _generate_html_filepath():
             output = f"<h2>{self._generate_filepath_info()}</h2>"
@@ -168,7 +167,7 @@ class KerasFileEditor:
             output = "".join(output)
             return output
 
-        def _generate_html_weight(dictionary, margin_left=0, font_size=20):
+        def _generate_html_weights(dictionary, margin_left=0, font_size=20):
             html = ""
             for key, value in dictionary.items():
                 if isinstance(value, dict) and value:
@@ -176,7 +175,7 @@ class KerasFileEditor:
                         f'<details style="margin-left: {margin_left}px;">'
                         + f'<summary style="font-size: {font_size}px;">'
                         + f"{key}</summary>"
-                        + _generate_html_weight(
+                        + _generate_html_weights(
                             value, margin_left + 20, font_size - 1
                         )
                         + "</details>"
@@ -201,7 +200,7 @@ class KerasFileEditor:
 
         def _generate_html_layer():
             output = "<p>Layers</p>"
-            output += _generate_html_weight(self.nested_dict["layers"])
+            output += _generate_html_weights(self.weights_dict["layers"])
             return output
 
         output = _generate_html_filepath()
@@ -214,4 +213,5 @@ class KerasFileEditor:
 
         output += _generate_html_layer()
 
-        ipython.display.display(ipython.display.HTML(output))
+        if is_ipython_notebook():
+            ipython.display.display(ipython.display.HTML(output))
