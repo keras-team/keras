561a9a787e440ca01ff962add95ef491e40b8bc6
Hazem <hazemessam199@gmail.com>
2024-10-06 18:24:26 +0300

    added skip tests for every framework except torch

--- keras/src/ops/nn_test.py
+++ keras/src/ops/nn_test.py
@@ -2235,6 +2235,11 @@ class NNOpsCorrectnessTest(testing.TestCase):
                 mask_shape
             )
 
+        if flash_attention and backend.backend() in ["tensorflow", "numpy", "jax"]:
+            self.skipTest(
+                "flash attention is not supported in tensorflow and numpy."
+            )
+
         expected = _dot_product_attention(
             query,
             key,
