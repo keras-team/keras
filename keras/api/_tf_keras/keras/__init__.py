56eaab34aa45fa9a5d80a9de900492670b5c5120
Siva Sravana Kumar Neeli <113718461+sineeli@users.noreply.github.com>
2024-10-24 13:43:15 -0700

    [Visualization utils] Add visualization utils for plotting images(plain, with bounding boxes and segmentation masks) (#20401)
    * api gen
    
    * add plot image gallery function
    
    * add `plot_ bounding_box_gallery`
    
    * correct label key
    
    * add segmentation mask draw and plot functions
    
    * few arg corrections and docstrings
    
    * nit
    
    * add missing args for plotting segmenation masks use cols for each mask to make aspect ratio of each subplot correct
    
    * add missing argument for color

--- keras/api/_tf_keras/keras/__init__.py
+++ keras/api/_tf_keras/keras/__init__.py
@@ -24,6 +24,7 @@ from keras.api import random
 from keras.api import regularizers
 from keras.api import tree
 from keras.api import utils
+from keras.api import visualization
 from keras.api._tf_keras.keras import backend
 from keras.api._tf_keras.keras import layers
 from keras.api._tf_keras.keras import losses
