c802ca6421408b0f994a1497717ae3f2c89e1ff1
tilakrayal <81610181+tilakrayal@users.noreply.github.com>
2024-11-20 13:22:11 +0530

    Update losses.py (#20523)

--- keras/src/losses/losses.py
+++ keras/src/losses/losses.py
@@ -923,8 +923,8 @@ class CategoricalCrossentropy(LossFunctionWrapper):
 
     Standalone usage:
 
-    >>> y_true = [[0, 1, 0], [0, 0, 1]]
-    >>> y_pred = [[0.05, 0.95, 0], [0.1, 0.8, 0.1]]
+    >>> y_true = np.array([[0, 1, 0], [0, 0, 1]])
+    >>> y_pred = np.array([[0.05, 0.95, 0], [0.1, 0.8, 0.1]])
     >>> # Using 'auto'/'sum_over_batch_size' reduction type.
     >>> cce = keras.losses.CategoricalCrossentropy()
     >>> cce(y_true, y_pred)
