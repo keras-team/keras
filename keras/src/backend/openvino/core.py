796f50924c8d2f881efb04b042a7ec404dc55278
Kazantsev, Roman <roman.kazantsev@intel.com>
2024-11-28 21:32:04 +0400

    Fix sorted imports
    Signed-off-by: Kazantsev, Roman <roman.kazantsev@intel.com>

--- keras/src/backend/openvino/core.py
+++ keras/src/backend/openvino/core.py
@@ -3,8 +3,8 @@ import contextlib
 import numpy as np
 import openvino as ov
 import openvino.runtime.opset14 as ov_opset
-from openvino import Tensor
 from openvino import Model
+from openvino import Tensor
 from openvino import compile_model
 from openvino.runtime import Type
 
