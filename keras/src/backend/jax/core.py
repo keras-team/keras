192401298388fc0b1434122317aa4b1c94436357
Hongyu Chiu <20734616+james77777778@users.noreply.github.com>
2024-12-05 13:29:14 +0800

    Add rise

--- keras/src/backend/jax/core.py
+++ keras/src/backend/jax/core.py
@@ -26,6 +26,11 @@ class Variable(KerasVariable):
                 self._shape = self._validate_shape(value.shape)
             elif isinstance(value, (int, float)):
                 self._shape = ()
+            else:
+                raise AttributeError(
+                    "`value` must have a shape attr or be a Python number. "
+                    f"Received: value={value} of type ({type(value)})"
+                )
         # We can't import the keras/distribution/distribution_lib
         # due to circular dependency.
         distribution = global_state.get_global_attribute("distribution")
