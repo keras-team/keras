bc8b6ae9c0d28b3aecb4173eb25e8194c5034f59
ugeunpark <pwg2004@gmail.com>
2024-12-19 21:06:52 +0900

    Correct failed test cases

--- keras/src/layers/preprocessing/image_preprocessing/random_contrast_test.py
+++ keras/src/layers/preprocessing/image_preprocessing/random_contrast_test.py
@@ -37,8 +37,18 @@ class RandomContrastTest(testing.TestCase):
     def test_random_contrast_with_value_range_0_to_255(self):
         seed = 9809
         np.random.seed(seed)
-        inputs = np.random.random((12, 8, 16, 3))
-        inputs = backend.convert_to_tensor(inputs)
+
+        data_format = backend.config.image_data_format()
+        if data_format == "channels_last":
+            inputs = np.random.random((12, 8, 16, 3))
+            height_axis = -3
+            width_axis = -2
+        else:
+            inputs = np.random.random((12, 3, 8, 16))
+            height_axis = -2
+            width_axis = -1
+
+        inputs = backend.convert_to_tensor(inputs, dtype="float32")
         layer = layers.RandomContrast(
             factor=0.5, value_range=(0, 255), seed=seed
         )
@@ -49,8 +59,8 @@ class RandomContrastTest(testing.TestCase):
         np.random.seed(seed)
         factor = backend.convert_to_numpy(transformation["contrast_factor"])
         inputs = backend.convert_to_numpy(inputs)
-        inp_mean = np.mean(inputs, axis=-3, keepdims=True)
-        inp_mean = np.mean(inp_mean, axis=-2, keepdims=True)
+        inp_mean = np.mean(inputs, axis=height_axis, keepdims=True)
+        inp_mean = np.mean(inp_mean, axis=width_axis, keepdims=True)
         actual_outputs = (inputs - inp_mean) * factor + inp_mean
         outputs = backend.convert_to_numpy(outputs)
         actual_outputs = np.clip(actual_outputs, 0, 255)
@@ -60,8 +70,18 @@ class RandomContrastTest(testing.TestCase):
     def test_random_contrast_with_value_range_0_to_1(self):
         seed = 9809
         np.random.seed(seed)
-        inputs = np.random.random((12, 8, 16, 3))
-        inputs = backend.convert_to_tensor(inputs)
+
+        data_format = backend.config.image_data_format()
+        if data_format == "channels_last":
+            inputs = np.random.random((12, 8, 16, 3))
+            height_axis = -3
+            width_axis = -2
+        else:
+            inputs = np.random.random((12, 3, 8, 16))
+            height_axis = -2
+            width_axis = -1
+
+        inputs = backend.convert_to_tensor(inputs, dtype="float32")
         layer = layers.RandomContrast(factor=0.5, value_range=(0, 1), seed=seed)
         transformation = layer.get_random_transformation(inputs, training=True)
         outputs = layer.transform_images(inputs, transformation, training=True)
@@ -70,8 +90,8 @@ class RandomContrastTest(testing.TestCase):
         np.random.seed(seed)
         factor = backend.convert_to_numpy(transformation["contrast_factor"])
         inputs = backend.convert_to_numpy(inputs)
-        inp_mean = np.mean(inputs, axis=-3, keepdims=True)
-        inp_mean = np.mean(inp_mean, axis=-2, keepdims=True)
+        inp_mean = np.mean(inputs, axis=height_axis, keepdims=True)
+        inp_mean = np.mean(inp_mean, axis=width_axis, keepdims=True)
         actual_outputs = (inputs - inp_mean) * factor + inp_mean
         outputs = backend.convert_to_numpy(outputs)
         actual_outputs = np.clip(actual_outputs, 0, 1)
