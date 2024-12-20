511fb9121af441bd2424af433f715d45f649923c
ugeunpark <pwg2004@gmail.com>
2024-12-20 19:45:14 +0900

    Add random_color_jitter processing layer

--- keras/src/layers/preprocessing/image_preprocessing/random_color_jitter.py
+++ keras/src/layers/preprocessing/image_preprocessing/random_color_jitter.py
@@ -1,7 +1,7 @@
-import keras.src.layers.preprocessing.image_preprocessing.random_brightness as random_brightness
-import keras.src.layers.preprocessing.image_preprocessing.random_contrast as random_contrast
-import keras.src.layers.preprocessing.image_preprocessing.random_hue as random_hue
-import keras.src.layers.preprocessing.image_preprocessing.random_saturation as random_saturation
+import keras.src.layers.preprocessing.image_preprocessing.random_brightness as random_brightness  # noqa: E501
+import keras.src.layers.preprocessing.image_preprocessing.random_contrast as random_contrast  # noqa: E501
+import keras.src.layers.preprocessing.image_preprocessing.random_hue as random_hue  # noqa: E501
+import keras.src.layers.preprocessing.image_preprocessing.random_saturation as random_saturation  # noqa: E501
 from keras.src.api_export import keras_export
 from keras.src.layers.preprocessing.image_preprocessing.base_image_preprocessing_layer import (  # noqa: E501
     BaseImagePreprocessingLayer,
@@ -13,71 +13,58 @@ from keras.src.random.seed_generator import SeedGenerator
 class RandomColorJitter(BaseImagePreprocessingLayer):
     """RandomColorJitter class randomly apply brightness, contrast, saturation
     and hue image processing operation sequentially and randomly on the
-    input. It expects input as RGB image. The expected image should be
-    `(0-255)` pixel ranges.
+    input.
 
     Args:
         value_range: the range of values the incoming images will have.
             Represented as a two number tuple written [low, high].
             This is typically either `[0, 1]` or `[0, 255]` depending
             on how your preprocessing pipeline is set up.
-        brightness_factor: Float or a list/tuple of 2 floats between -1.0 and 1.0. The
-            factor is used to determine the lower bound and upper bound of the
-            brightness adjustment. A float value will be chosen randomly between
-            the limits. When -1.0 is chosen, the output image will be black, and
-            when 1.0 is chosen, the image will be fully white.
-            When only one float is provided, eg, 0.2,
-            then -0.2 will be used for lower bound and 0.2
-            will be used for upper bound.
-        contrast_factor: a positive float represented as fraction of value, or a tuple of
-            size 2 representing lower and upper bound.
-            When represented as a single float, lower = upper.
-            The contrast factor will be randomly picked between
-            `[1.0 - lower, 1.0 + upper]`. For any pixel x in the channel,
-            the output will be `(x - mean) * factor + mean`
-            where `mean` is the mean value of the channel.
-        saturation_factor: A tuple of two floats or a single float.
-            `factor` controls the extent to which the image saturation
-            is impacted. `factor=0.5` makes this layer perform a no-op
-            operation. `factor=0.0` makes the image fully grayscale.
-            `factor=1.0` makes the image fully saturated. Values should
-            be between `0.0` and `1.0`. If a tuple is used, a `factor`
-            is sampled between the two values for every image augmented.
-            If a single float is used, a value between `0.0` and the passed
-            float is sampled. To ensure the value is always the same,
-            pass a tuple with two identical floats: `(0.5, 0.5)`.
-        hue_factor: A single float or a tuple of two floats.
-            `factor` controls the extent to which the
-            image hue is impacted. `factor=0.0` makes this layer perform a
-            no-op operation, while a value of `1.0` performs the most aggressive
-            contrast adjustment available. If a tuple is used, a `factor` is
-            sampled between the two values for every image augmented. If a
-            single float is used, a value between `0.0` and the passed float is
-            sampled. In order to ensure the value is always the same, please
-            pass a tuple with two identical floats: `(0.5, 0.5)`.
+        brightness_factor: Float or a list/tuple of 2 floats between -1.0
+            and 1.0. The factor is used to determine the lower bound and
+            upper bound of the brightness adjustment. A float value will
+            be chosen randomly between the limits. When -1.0 is chosen,
+            the output image will be black, and when 1.0 is chosen, the
+            image will be fully white. When only one float is provided,
+            eg, 0.2, then -0.2 will be used for lower bound and 0.2 will
+            be used for upper bound.
+        contrast_factor: a positive float represented as fraction of value,
+            or a tuple of size 2 representing lower and upper bound. When
+            represented as a single float, lower = upper. The contrast
+            factor will be randomly picked between `[1.0 - lower, 1.0 +
+            upper]`. For any pixel x in the channel, the output will be
+            `(x - mean) * factor + mean` where `mean` is the mean value
+            of the channel.
+        saturation_factor: A tuple of two floats or a single float. `factor`
+            controls the extent to which the image saturation is impacted.
+            `factor=0.5` makes this layer perform a no-op operation.
+            `factor=0.0` makes the image fully grayscale. `factor=1.0`
+            makes the image fully saturated. Values should be between
+            `0.0` and `1.0`. If a tuple is used, a `factor` is sampled
+            between the two values for every image augmented. If a single
+            float is used, a value between `0.0` and the passed float is
+            sampled. To ensure the value is always the same, pass a tuple
+            with two identical floats: `(0.5, 0.5)`.
+        hue_factor: A single float or a tuple of two floats. `factor`
+            controls the extent to which the image hue is impacted.
+            `factor=0.0` makes this layer perform a no-op operation,
+            while a value of `1.0` performs the most aggressive contrast
+            adjustment available. If a tuple is used, a `factor` is
+            sampled between the two values for every image augmented.
+            If a single float is used, a value between `0.0` and the
+            passed float is sampled. In order to ensure the value is
+            always the same, please pass a tuple with two identical
+            floats: `(0.5, 0.5)`.
         seed: Integer. Used to create a random seed.
-
-    Example:
-    ```python
-    (images, labels), _ = keras.datasets.cifar10.load_data()
-    color_jitter = keras.layers.RandomColorJitter(
-            value_range=(0, 255),
-            brightness_factor=(-0.2, 0.5),
-            contrast_factor=(0.5, 0.9),
-            saturation_factor=(0.5, 0.9),
-            hue_factor=(0.5, 0.9),
-    )
-    augmented_images = color_jitter(images)
-    ```
     """
 
     def __init__(
         self,
-        value_range,
-        brightness_factor,
-        contrast_factor,
-        saturation_factor,
-        hue_factor,
+        value_range=(0, 255),
+        brightness_factor=None,
+        contrast_factor=None,
+        saturation_factor=None,
+        hue_factor=None,
         seed=None,
         data_format=None,
         **kwargs,
@@ -91,36 +78,50 @@ class RandomColorJitter(BaseImagePreprocessingLayer):
         self.seed = seed
         self.generator = SeedGenerator(seed)
 
-        self.random_brightness = random_brightness.RandomBrightness(
-            factor=self.brightness_factor, value_range=(0, 255), seed=self.seed
-        )
-        self.random_contrast = random_contrast.RandomContrast(
-            factor=self.contrast_factor, value_range=(0, 255), seed=self.seed
-        )
-        self.random_saturation = random_saturation.RandomSaturation(
-            factor=self.saturation_factor, seed=self.seed
-        )
-        self.random_hue = random_hue.RandomHue(
-            factor=self.hue_factor, value_range=(0, 255), seed=self.seed
-        )
+        self.random_brightness = None
+        self.random_contrast = None
+        self.random_saturation = None
+        self.random_hue = None
+
+        if self.brightness_factor is not None:
+            self.random_brightness = random_brightness.RandomBrightness(
+                factor=self.brightness_factor,
+                value_range=self.value_range,
+                seed=self.seed,
+            )
+
+        if self.contrast_factor is not None:
+            self.random_contrast = random_contrast.RandomContrast(
+                factor=self.contrast_factor,
+                value_range=self.value_range,
+                seed=self.seed,
+            )
+
+        if self.saturation_factor is not None:
+            self.random_saturation = random_saturation.RandomSaturation(
+                factor=self.saturation_factor,
+                value_range=self.value_range,
+                seed=self.seed,
+            )
+
+        if self.hue_factor is not None:
+            self.random_hue = random_hue.RandomHue(
+                factor=self.hue_factor,
+                value_range=self.value_range,
+                seed=self.seed,
+            )
 
     def transform_images(self, images, transformation, training=True):
-        images = self._transform_value_range(
-            images,
-            original_range=self.value_range,
-            target_range=(0, 255),
-            dtype=self.compute_dtype,
-        )
-        images = self.random_brightness(images)
-        images = self.random_contrast(images)
-        images = self.random_saturation(images)
-        images = self.random_hue(images)
-        images = self._transform_value_range(
-            images,
-            original_range=(0, 255),
-            target_range=self.value_range,
-            dtype=self.compute_dtype,
-        )
+        if training:
+            if self.brightness_factor is not None:
+                images = self.random_brightness(images)
+            if self.contrast_factor is not None:
+                images = self.random_contrast(images)
+            if self.saturation_factor is not None:
+                images = self.random_saturation(images)
+            if self.hue_factor is not None:
+                images = self.random_hue(images)
+            images = self.backend.cast(images, self.compute_dtype)
         return images
 
     def transform_labels(self, labels, transformation, training=True):
