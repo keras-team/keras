76911c7838e0c66757a02ca1e25cc91be43b1698
David Landup <david.landup.0@gmail.com>
2025-01-12 17:06:34 +0900

    Remove numpy from tests in lieu of keras.ops

--- keras/src/ops/einops_test.py
+++ keras/src/ops/einops_test.py
@@ -1,12 +1,9 @@
-import numpy as np
-
-from conftest import skip_if_backend
+from keras.src import ops
 from keras.src import testing
 from keras.src.backend.common import keras_tensor
 from keras.src.ops.einops import rearrange
 
 
-@skip_if_backend("openvino", "NumPy ops not supported with openvino backend")
 class RearrangeTest(testing.TestCase):
     def test_basic_rearrangement_symbolic(self):
         x = keras_tensor.KerasTensor((2, 3, 4))
@@ -15,17 +12,17 @@ class RearrangeTest(testing.TestCase):
         self.assertEqual(y.shape, (2, 4, 3))
 
     def test_basic_rearrangement(self):
-        x = np.random.rand(2, 3, 4)
+        x = ops.random.uniform((2, 3, 4))
         y = rearrange(x, "b c h -> b h c")
         self.assertEqual(y.shape, (2, 4, 3))
-        np.testing.assert_array_equal(y, x.transpose(0, 2, 1))
+        self.assertTrue(ops.all(ops.equal(y, ops.transpose(x, (0, 2, 1)))))
 
     def test_output_composition(self):
-        x = np.random.rand(2, 4, 4, 3)
+        x = ops.random.uniform((2, 4, 4, 3))
         y = rearrange(x, "b h w c -> (b h) w c")
         target_shape = (8, 4, 3)
         self.assertEqual(y.shape, target_shape)
-        np.testing.assert_array_equal(y, x.reshape(8, 4, 3))
+        self.assertTrue(ops.all(ops.equal(y, ops.reshape(x, (8, 4, 3)))))
 
     def test_basic_decomposition_and_rearrangement_symbolic(self):
         x = keras_tensor.KerasTensor((6, 8))
@@ -34,14 +31,14 @@ class RearrangeTest(testing.TestCase):
         self.assertEqual(y.shape, (2, 3, 8))
 
     def test_basic_decomposition_and_rearrangement(self):
-        x = np.random.rand(6, 8)
+        x = ops.random.uniform((6, 8))
         y = rearrange(x, "(h w) c -> h w c", h=2, w=3)
         self.assertEqual(y.shape, (2, 3, 8))
 
     def test_unchanged_shape(self):
-        x = np.ones([2, 3, 4])
+        x = ops.ones([2, 3, 4])
         y = rearrange(x, "b h c -> b h c")
-        np.testing.assert_array_equal(y, x)
+        self.assertTrue(ops.all(ops.equal(y, x)))
         self.assertTrue(x.shape, y.shape)
 
     def test_unchanged_shape_symbolic(self):
