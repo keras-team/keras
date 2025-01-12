90078cfac0938b4f4fc5ebc46ce535e1597e9d15
David Landup <david.landup.0@gmail.com>
2025-01-12 17:41:56 +0900

    Skip tests for openvino when the testing operation isn't supported

--- keras/src/ops/einops_test.py
+++ keras/src/ops/einops_test.py
@@ -1,3 +1,4 @@
+from conftest import skip_if_backend
 from keras.src import ops
 from keras.src import testing
 from keras.src.backend.common import keras_tensor
@@ -11,12 +12,14 @@ class RearrangeTest(testing.TestCase):
         self.assertIsInstance(y, keras_tensor.KerasTensor)
         self.assertEqual(y.shape, (2, 4, 3))
 
+    @skip_if_backend("openvino", "Test operation not supported by openvino")
     def test_basic_rearrangement(self):
         x = ops.random.uniform((2, 3, 4))
         y = rearrange(x, "b c h -> b h c")
         self.assertEqual(y.shape, (2, 4, 3))
         self.assertTrue(ops.all(ops.equal(y, ops.transpose(x, (0, 2, 1)))))
 
+    @skip_if_backend("openvino", "Test operation not supported by openvino")
     def test_output_composition(self):
         x = ops.random.uniform((2, 4, 4, 3))
         y = rearrange(x, "b h w c -> (b h) w c")
@@ -35,6 +38,7 @@ class RearrangeTest(testing.TestCase):
         y = rearrange(x, "(h w) c -> h w c", h=2, w=3)
         self.assertEqual(y.shape, (2, 3, 8))
 
+    @skip_if_backend("openvino", "Test operation not supported by openvino")
     def test_unchanged_shape(self):
         x = ops.ones([2, 3, 4])
         y = rearrange(x, "b h c -> b h c")
