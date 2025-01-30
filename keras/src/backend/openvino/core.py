adb27a9b1251a189f32843b7dded1ca8ecdfc4b2
divyashreepathihalli <divyashreepathihalli@gmail.com>
2025-01-30 23:43:12 +0000

    add warnings and ops to numpy and openvino backend

--- keras/src/backend/openvino/core.py
+++ keras/src/backend/openvino/core.py
@@ -615,3 +615,12 @@ def custom_gradient(fun):
     def __call__(self, *args, **kwargs):
         outputs, _ = self.fun(*args, **kwargs)
         return outputs
+
+
+def remat(func, *args, **kwargs):
+    warnings.warn(
+        "Rematerialization memory optimization is not supported by the "
+        "OpenVino backend. Please switch to JAX, TensorFlow, or PyTorch to "
+        "utilize this feature."
+    )
+    return func(*args, **kwargs)
