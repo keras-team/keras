b3d20d1507ec7d2d270920d459fb840edb32d8b8
Mohamed-Ashraf273 <117025882+Mohamed-Ashraf273@users.noreply.github.com>
2025-02-18 01:28:00 +0200

    Added np.average support

--- keras/src/backend/openvino/numpy.py
+++ keras/src/backend/openvino/numpy.py
@@ -278,9 +278,28 @@ def array(x, dtype=None):
 
 
 def average(x, axis=None, weights=None):
-    raise NotImplementedError(
-        "`average` is not supported with openvino backend"
-    )
+    x = get_ov_output(x)
+    weighted_sum = None
+    if axis is None:
+        flatten_shape = ov_opset.constant([-1], Type.i32).output(0)
+        x = ov_opset.reshape(x, flatten_shape, False).output(0)
+        axis = 0
+    axis_const = ov_opset.constant(axis, dtype=Type.i32).output(0)
+    if weights is not None:
+        weights = get_ov_output(weights)
+        element_type = None
+        if isinstance(x, OpenVINOKerasTensor):
+            element_type = x.output.get_element_type()
+        if isinstance(weights, OpenVINOKerasTensor):
+            element_type = weights.output.get_element_type()
+        x = get_ov_output(x, element_type)
+        weights = get_ov_output(weights, element_type)
+        x, weights = _align_operand_types(x, weights, "multiply()")
+        weighted_sum = ov_opset.multiply(x, weights)
+    else:
+        weighted_sum = x  
+    mean_ops = ov_opset.reduce_mean(weighted_sum, axis_const, False)  
+    return OpenVINOKerasTensor(mean_ops.output(0))
 
 
 def bincount(x, weights=None, minlength=0, sparse=False):
