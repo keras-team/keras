6f0800ae2c15d3b7ac813f9327229d7a6ef53e90
Mohamed-Ashraf273 <117025882+Mohamed-Ashraf273@users.noreply.github.com>
2025-02-22 19:43:25 +0200

    suppoer np.count_nonzero for ov BackEnd

--- keras/src/backend/openvino/numpy.py
+++ keras/src/backend/openvino/numpy.py
@@ -393,9 +393,24 @@ def cosh(x):
 
 
 def count_nonzero(x, axis=None):
-    raise NotImplementedError(
-        "`count_nonzero` is not supported with openvino backend"
-    )
+    x = get_ov_output(x)
+    if x.get_element_type() != Type.boolean:
+        x = ov_opset.ceil(x).output(0)
+        x = ov_opset.convert(x, Type.i32).output(0)
+        x = ov_opset.not_equal(
+            x, ov_opset.constant(0, dtype=Type.i32).output(0)
+        ).output(0)
+    x = ov_opset.convert(x, Type.i32).output(0)
+    if axis is None:
+        flatten_shape = ov_opset.constant([-1], Type.i32).output(0)
+        x = ov_opset.reshape(x, flatten_shape, False).output(0)
+        axis = 0
+    if isinstance(axis, tuple):
+        axis = list(axis)
+    if axis == []:
+        return OpenVINOKerasTensor(x)
+    axis = ov_opset.constant(axis, Type.i32).output(0)
+    return OpenVINOKerasTensor(ov_opset.reduce_sum(x, axis, False).output(0))
 
 
 def cross(x1, x2, axisa=-1, axisb=-1, axisc=-1, axis=None):
