7146c5765f2accd0cbda1582375174f371e9b94e
Mohamed-Ashraf273 <117025882+Mohamed-Ashraf273@users.noreply.github.com>
2025-02-24 03:27:09 +0200

    merge conflicts (#20934)
    Co-authored-by: Mohamed I. Hammad <ibraaaa@gmail.com>

--- keras/src/backend/openvino/numpy.py
+++ keras/src/backend/openvino/numpy.py
@@ -316,9 +316,35 @@ def array(x, dtype=None):
 
 
 def average(x, axis=None, weights=None):
-    raise NotImplementedError(
-        "`average` is not supported with openvino backend"
-    )
+    x = get_ov_output(x)
+    if weights is not None:
+        weights = get_ov_output(weights)
+    if axis is None:
+        flatten_shape = ov_opset.constant([-1], Type.i32).output(0)
+        x = ov_opset.reshape(x, flatten_shape, False).output(0)
+        if weights is not None:
+            weights = ov_opset.reshape(weights, flatten_shape, False).output(0)
+        axis = 0
+
+    if weights is not None:
+        x_type = x.get_element_type()
+        weights_type = weights.get_element_type()
+        if (weights_type.is_integral() or weights_type == Type.boolean) and (
+            x_type.is_integral() or x_type == Type.boolean
+        ):
+            x = ov_opset.convert(x, Type.f32).output(0)
+            weights = ov_opset.convert(weights, Type.f32).output(0)
+        x, weights = _align_operand_types(x, weights, "multiply()")
+        x = ov_opset.multiply(x, weights)
+
+    if isinstance(axis, tuple):
+        axis = list(axis)
+    if axis == []:
+        return OpenVINOKerasTensor(x)
+
+    axis_const = ov_opset.constant(axis, dtype=Type.i32).output(0)
+    mean_ops = ov_opset.reduce_mean(x, axis_const, False)
+    return OpenVINOKerasTensor(mean_ops.output(0))
 
 
 def bincount(x, weights=None, minlength=0, sparse=False):
