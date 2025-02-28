7efaff8e4699f6db2b7e692c7593e00e3a187e34
Surya <suryatechsavvy@gmail.com>
2025-02-28 11:15:43 +0530

    fix legacy model saving & reloading with axis argument in its layer (#20973)
    * fix legacy model saving & relaoding with axis arg in layer
    
    * fix formatting issue
    
    * add temp_file_path

--- keras/src/legacy/saving/saving_utils.py
+++ keras/src/legacy/saving/saving_utils.py
@@ -63,8 +63,11 @@ def model_from_config(config, custom_objects=None):
             config["config"]["input_shape"] = batch_input_shape
 
     axis = config["config"].pop("axis", None)
-    if axis is not None and isinstance(axis, list) and len(axis) == 1:
-        config["config"]["axis"] = int(axis[0])
+    if axis is not None:
+        if isinstance(axis, list) and len(axis) == 1:
+            config["config"]["axis"] = int(axis[0])
+        elif isinstance(axis, (int, float)):
+            config["config"]["axis"] = int(axis)
 
     # Handle backwards compatibility for Keras lambdas
     if config["class_name"] == "Lambda":
