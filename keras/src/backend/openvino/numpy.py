27a159e3c17d5ec964e8d077fb993c9be24fb8c3
Srinjoy <srinjoydutta03@gmail.com>
2025-02-28 11:31:24 +0530

    Update excluded tests file and fix code format

--- keras/src/backend/openvino/numpy.py
+++ keras/src/backend/openvino/numpy.py
@@ -767,10 +767,14 @@ def diag(x, k=0):
     if rank == 1:
         N_dim = x_shape[0]
         if not N_dim.is_static:
-            raise ValueError("diag requires input with static shape for 1D input.")
+            raise ValueError(
+                "diag requires input with static shape for 1D input."
+            )
         N = N_dim.get_length()
         output_size = N + np.abs(k)
-        out_shape = ov_opset.constant([output_size, output_size], dtype=Type.i32).output(0)
+        out_shape = ov_opset.constant(
+            [output_size, output_size], dtype=Type.i32
+        ).output(0)
         zeros_const = ov_opset.constant(0, x.get_element_type()).output(0)
         diag_matrix = ov_opset.broadcast(zeros_const, out_shape)
 
@@ -786,12 +790,14 @@ def diag(x, k=0):
         indices_const = ov_opset.constant(indices, dtype=Type.i32).output(0)
         updated = ov_opset.scatter_nd_update(diag_matrix, indices_const, x)
         return OpenVINOKerasTensor(updated.output(0))
-    
+
     elif rank == 2:
         M_dim = x_shape[0]
         N_dim = x_shape[1]
         if not M_dim.is_static or not N_dim.is_static:
-            raise ValueError("diag requires input with static shape for 2D input.")
+            raise ValueError(
+                "diag requires input with static shape for 2D input."
+            )
         M = M_dim.get_length()
         N = N_dim.get_length()
 
@@ -806,14 +812,16 @@ def diag(x, k=0):
             keras_dtype = ov_to_keras_type(x.get_element_type())
             np_dtype = np.dtype(keras_dtype)
             empty_np = np.empty((0,), dtype=np_dtype)
-            empty_const = ov_opset.constant(empty_np, x.get_element_type()).output(0)
+            empty_const = ov_opset.constant(
+                empty_np, x.get_element_type()
+            ).output(0)
             return OpenVINOKerasTensor(empty_const)
-        
+
         indices = np.array(indices, dtype=np.int32)
         indices_const = ov_opset.constant(indices, dtype=Type.i32).output(0)
         diag_vec = ov_opset.gather_nd(x, indices_const)
         return OpenVINOKerasTensor(diag_vec.output(0))
-    
+
     else:
         raise ValueError("diag supports only 1D or 2D tensors")
 
