67e5ee5ea2549e4f15d833aa59fe06fa8e5ccdc0
11happy <soni5happy@gmail.com>
2025-03-09 14:28:41 +0530

    fix: use reshape correctly
    Signed-off-by: 11happy <soni5happy@gmail.com>

--- keras/src/backend/openvino/numpy.py
+++ keras/src/backend/openvino/numpy.py
@@ -393,7 +393,7 @@ def bincount(x, weights=None, minlength=0, sparse=False):
     max_element = ov_opset.reduce_max(x, const_zero, keep_dims=False).output(0)
     depth = ov_opset.add(max_element, const_one).output(0)
     depth = ov_opset.maximum(depth, minlength).output(0)
-    depth = ov_opset.reshape(depth, []).output(0)
+    depth = ov_opset.reshape(depth, [], False).output(0)
     one_hot = ov_opset.one_hot(x, depth, const_one, const_zero, axis=-1).output(
         0
     )
