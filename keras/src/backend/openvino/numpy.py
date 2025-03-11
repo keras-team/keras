5b1ede4ddf2406d8d2c6e0027103e9633d1e977e
Imokutmfon Udoh <imokutofficial@gmail.com>
2025-03-11 14:11:43 +0100

    [OpenVINO backend] Support numpy.hstack

--- keras/src/backend/openvino/numpy.py
+++ keras/src/backend/openvino/numpy.py
@@ -11,7 +11,7 @@ from keras.src.backend.openvino.core import (
     align_operand_types as _align_operand_types,
 )
 from keras.src.backend.openvino.core import get_ov_output
-from keras.src.backend.openvino.core import ov_to_keras_type
+from keras.src.backend.openvino.core import ov_to_keras_type, convert_to_tensor
 
 
 def add(x1, x2):
@@ -680,7 +680,11 @@ def greater_equal(x1, x2):
 
 
 def hstack(xs):
-    raise NotImplementedError("`hstack` is not supported with openvino backend")
+    tensor_list = [convert_to_tensor(x) for x in xs]
+    is_1d = all(len(x.shape)==1 for x in tensor_list)
+    axis = 0 if is_1d else 1
+    concat_node = ov_opset.concat(tensor_list, axis)
+    return OpenVINOKerasTensor(concat_node.output(0))
 
 
 def identity(n, dtype=None):
