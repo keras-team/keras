e49b0ca1627e263c8c74b2ad02cba6cc57f4b907
Sonal kumari <165447633+Hmm-1224@users.noreply.github.com>
2025-03-16 10:32:11 +0530

    Update numpy.py

--- keras/src/backend/openvino/numpy.py
+++ keras/src/backend/openvino/numpy.py
@@ -770,12 +770,14 @@ def hstack(xs):
             element_type = x.output.get_element_type()
             break
     xs = [get_ov_output(x, element_type) for x in xs]
-    xs = [
-        _align_operand_types(xs[0], x, "hstack()")[0]
-        if i == 0
-        else _align_operand_types(xs[0], x, "hstack()")[1]
-        for i, x in enumerate(xs)
-    ]
+    aligned_xs = []
+    for i, x in enumerate(xs):
+        if i == 0:
+           aligned_xs.append(_align_operand_types(xs[0], x, "hstack()")[0])
+        else:
+           aligned_xs.append(_align_operand_types(xs[0], x, "hstack()")[1])
+    xs = aligned_xs
+    xs = [ov_opset.convert(x, element_type) for x in xs]
     rank = len(xs[0].get_partial_shape())
     axis = 1 if rank > 1 else 0
     return OpenVINOKerasTensor(ov_opset.concat(xs, axis=axis).output(0))
