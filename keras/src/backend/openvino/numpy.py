271fb9a35887e79484775038113bbb424beafe92
Deep Chordia <73215996+DeepC004@users.noreply.github.com>
2025-03-17 10:18:53 +0530

    Run format.sh to fix formatting error

--- keras/src/backend/openvino/numpy.py
+++ keras/src/backend/openvino/numpy.py
@@ -863,10 +863,12 @@ def logaddexp(x1, x2):
         x2 = ov_opset.convert(x2, ov_type)
     max_element = ov_opset.maximum(x1, x2).output(0)
     exp_k = ov_opset.exp(max_element).output(0)
-    exp_x1 = ov_opset.exp(ov_opset.subtract(x1, max_element).output(0)) \
-            .output(0)
-    exp_x2 = ov_opset.exp(ov_opset.subtract(x2, max_element).output(0)) \
-            .output(0)
+    exp_x1 = ov_opset.exp(ov_opset.subtract(x1, max_element).output(0)).output(
+        0
+    )
+    exp_x2 = ov_opset.exp(ov_opset.subtract(x2, max_element).output(0)).output(
+        0
+    )
     sum_exp = ov_opset.add(exp_x1, exp_x2).output(0)
     mul_exp = ov_opset.multiply(exp_k, sum_exp).output(0)
     return OpenVINOKerasTensor(ov_opset.log(mul_exp).output(0))
