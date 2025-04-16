8e4b4abcca0002c4435d54aa191d3e76a92e44d1
Bhuminjay Soni <Soni5Happy@gmail.com>
2025-04-17 00:47:58 +0530

    [OpenVINO Backend]: support numpy.ndim (#21176)
    * feat: support numpy.ndim
    
    Signed-off-by: 11happy <soni5happy@gmail.com>
    
    * use shapeof shapeof method
    
    Signed-off-by: 11happy <soni5happy@gmail.com>
    
    ---------
    
    Signed-off-by: 11happy <soni5happy@gmail.com>

--- keras/src/backend/openvino/numpy.py
+++ keras/src/backend/openvino/numpy.py
@@ -1036,7 +1036,10 @@ def nan_to_num(x, nan=0.0, posinf=None, neginf=None):
 
 
 def ndim(x):
-    raise NotImplementedError("`ndim` is not supported with openvino backend")
+    x = get_ov_output(x)
+    x_shape = ov_opset.shape_of(x).output(0)
+    x_dim = ov_opset.shape_of(x_shape, "i64")
+    return x_dim
 
 
 def nonzero(x):
