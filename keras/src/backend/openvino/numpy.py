a62795df08b9346c3de5fcb109d6b1cc1f40d6ac
Shruti Vishwakarma <shwvkrm@gmail.com>
2025-04-29 14:18:23 +0530

    Update numpy.py

--- keras/src/backend/openvino/numpy.py
+++ keras/src/backend/openvino/numpy.py
@@ -910,105 +910,130 @@ def less_equal(x1, x2):
 def linspace(
     start, stop, num=50, endpoint=True, retstep=False, dtype=None, axis=0
 ):
-    dtype = dtype or config.floatx()
-    ov_dtype = OPENVINO_DTYPES[dtype]
+    """
+    Return evenly spaced numbers over a specified interval.
 
-    start_node = get_ov_output(start, ov_dtype)
-    stop_node = get_ov_output(stop, ov_dtype)
-    start_node, stop_node = _align_operand_types(
-        start_node, stop_node, "linspace()"
-    )
+    Returns `num` evenly spaced samples, calculated over the
+    interval [`start`, `stop`].
+
+    The endpoint of the interval can optionally be excluded.
+
+    Parameters
+    ----------
+    start : array_like
+        The starting value of the sequence.
+    stop : array_like
+        The end value of the sequence, unless `endpoint` is set to False.
+        In that case, the sequence consists of all but the last of ``num + 1``
+        evenly spaced samples, so that `stop` is excluded.  Note that the step
+        size changes when `endpoint` is False.
+    num : int, optional
+        Number of samples to generate. Default is 50. Must be non-negative.
+    endpoint : bool, optional
+        If True, `stop` is the last sample. Otherwise, it is not included.
+        Default is True.
+    retstep : bool, optional
+        If True, return (`samples`, `step`), where `step` is the spacing
+        between samples.
+    dtype : dtype, optional
+        The type of the output array.  If `dtype` is not given, the data type
+        is inferred from `start` and `stop`. The inferred dtype will never be
+        an integer; `float` is chosen even if the arguments would produce an
+        array of integers.
+    axis : int, optional
+        The axis in the result to store the samples.  Relevant only if start
+        or stop are array-like.  By default (0), the samples will be along a
+        new axis inserted at the beginning. Use -1 to get an axis at the end.
+
+    Returns
+    -------
+    samples : ndarray
+        There are `num` equally spaced samples in the closed interval
+        ``[start, stop]`` or the half-open interval ``[start, stop)``
+        (depending on whether `endpoint` is True or False).
+    step : float, optional
+        Only returned if `retstep` is True
+
+        Size of spacing between samples.
+    """
+
+    dtype = dtype or config.floatx()
+    dtype = OPENVINO_DTYPES[dtype]
+
+    start = get_ov_output(start, dtype)
+    stop = get_ov_output(stop, dtype)
+    start, stop = _align_operand_types(start, stop, "linspace()")
 
     if isinstance(num, OpenVINOKerasTensor):
-        num_node = get_ov_output(num, Type.i32)
+        num = get_ov_output(num, Type.i32)
     elif isinstance(num, int):
-        num_node = ov_opset.constant(num, Type.i32).output(0)
+        num = ov_opset.constant(num, Type.i32).output(0)
     else:
         raise TypeError("`num` must be an int or OpenVINOKerasTensor.")
 
-    zero_const = ov_opset.constant(0, Type.i32).output(0)
-    one_const = ov_opset.constant(1, Type.i32).output(0)
+    zero_i = ov_opset.constant(0, Type.i32).output(0)
+    one_i = ov_opset.constant(1, Type.i32).output(0)
+    axis_i = ov_opset.constant(axis, Type.i32).output(0)
 
-    effective_num = num_node
-    if not endpoint:
-        effective_num = ov_opset.subtract(num_node, one_const).output(0)
+    div = ov_opset.subtract(num, one_i).output(0) if endpoint else num
 
-    step_node = ov_opset.divide(
-        ov_opset.convert(
-            ov_opset.subtract(stop_node, start_node), ov_dtype
-        ).output(0),
-        ov_opset.convert(effective_num, ov_dtype),
+    zero = ov_opset.convert(zero_i, dtype).output(0)
+    one = ov_opset.convert(one_i, dtype).output(0)
+    num_f = ov_opset.convert(num, dtype).output(0)
+
+    seq = ov_opset.range(zero, num_f, one).output(0)
+
+    delta = ov_opset.subtract(stop, start).output(0)
+    div_f = ov_opset.convert(div, dtype).output(0)
+
+    nan_const = ov_opset.constant(float("nan"), dtype).output(0)
+    cond = ov_opset.greater(div, zero_i).output(0)
+
+    step_val = ov_opset.divide(delta, div_f).output(0)
+    step = ov_opset.select(cond, step_val, nan_const).output(0)
+
+    eq_zero = ov_opset.equal(step, zero).output(0)
+    any_zero = ov_opset.reduce_logical_or(
+        eq_zero, ov_opset.constant([0], Type.i64), False
+    ).output(0)
+    y_norm = ov_opset.multiply(seq, step).output(0)
+    y_denorm = ov_opset.multiply(
+        ov_opset.divide(seq, div_f).output(0), delta
+    ).output(0)
+    y_pos = ov_opset.select(any_zero, y_denorm, y_norm).output(0)
+
+    y_zero = ov_opset.multiply(seq, delta).output(0)
+    y = ov_opset.add(
+        ov_opset.select(cond, y_pos, y_zero).output(0), start
     ).output(0)
 
-    range_node = ov_opset.range(
-        zero_const, num_node, one_const, Type.i32
-    ).output(0)
-    range_node = ov_opset.convert(range_node, ov_dtype).output(0)
-
-    def expand_along_axis(node, axis):
-        node_shape = ov_opset.shape_of(node, Type.i32).output(0)
-        input_rank = node.get_partial_shape().rank.get_length()
-
-        # Normalize axis
-        if axis < 0:
-            axis += input_rank
-
-        step = ov_opset.constant([1], Type.i32).output(0)
-        pre_shape = ov_opset.slice(
-            node_shape,
-            ov_opset.constant([0], Type.i32).output(0),
-            ov_opset.constant([axis], Type.i32).output(0),
-            step,
+    if endpoint:
+        idx = ov_opset.subtract(num, one_i).output(0)
+        idx64 = ov_opset.convert(idx, Type.i64).output(0)
+        idx_tensor = ov_opset.reshape(
+            idx64, ov_opset.constant([1], Type.i64)
+        ).output(0)
+        stop_tensor = ov_opset.reshape(
+            stop, ov_opset.constant([1], Type.i64)
+        ).output(0)
+        y = ov_opset.scatter_elements_update(
+            y, idx_tensor, stop_tensor, 0
         ).output(0)
 
-        post_shape = ov_opset.slice(
-            node_shape,
-            ov_opset.constant([axis], Type.i32).output(0),
-            ov_opset.constant([input_rank], Type.i32).output(0),
-            step,
+    if axis != 0:
+        rank = ov_opset.rank(y).output(0)
+        axis_p1 = ov_opset.add(axis_i, one_i).output(0)
+        pre = ov_opset.range(one_i, axis_p1, one_i).output(0)
+        post = ov_opset.range(axis_p1, rank, one_i).output(0)
+        zero_i = ov_opset.reshape(
+            zero_i, ov_opset.constant([1], Type.i32)
         ).output(0)
+        perm = ov_opset.concat([pre, zero_i, post], 0).output(0)
+        y = ov_opset.transpose(y, perm).output(0)
 
-        one_dim = ov_opset.constant([1], Type.i32).output(0)
-        new_shape = ov_opset.concat(
-            [pre_shape, one_dim, post_shape], axis=0
-        ).output(0)
+    y = ov_opset.convert(y, dtype).output(0)
 
-        return ov_opset.reshape(node, new_shape, special_zero=False).output(0)
-
-    input_rank = start_node.get_partial_shape().rank.get_length()
-    if input_rank is None or input_rank < 1:
-        input_rank = 1
-    normalized_axis = axis if axis >= 0 else axis + input_rank
-    if normalized_axis < 0:
-        raise ValueError(
-            f"`axis={axis}` is out of bounds for input rank {input_rank}"
-        )
-
-    range_node = expand_along_axis(range_node, normalized_axis)
-    step_node = expand_along_axis(step_node, normalized_axis)
-    start_node = expand_along_axis(start_node, normalized_axis)
-
-    broadcast_shape = ov_opset.shape_of(range_node, Type.i32).output(0)
-    step_broadcast = ov_opset.reshape(
-        step_node, broadcast_shape, special_zero=False
-    ).output(0)
-    start_broadcast = ov_opset.reshape(
-        start_node, broadcast_shape, special_zero=False
-    ).output(0)
-    # step_broadcast = ov_opset.broadcast(step_node, broadcast_shape).output(0)
-    # start_broadcast = ov_opset.broadcast(start_node, broadcast_shape).output(0)
-
-    linspace_result = ov_opset.add(
-        start_broadcast,
-        ov_opset.multiply(step_broadcast, range_node),
-    ).output(0)
-
-    if retstep:
-        return OpenVINOKerasTensor(linspace_result), OpenVINOKerasTensor(
-            step_node
-        )
-
-    return OpenVINOKerasTensor(linspace_result)
+    return (y, step) if retstep else y
 
 
 def log(x):
