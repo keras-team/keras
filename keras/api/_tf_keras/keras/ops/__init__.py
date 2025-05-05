6ddaefb73ceec2f62e6f1bc78acccc5d02b421a5
David Landup <60978046+DavidLandup0@users.noreply.github.com>
2025-05-06 02:26:44 +0900

    [Keras Ops] view_as_complex() and view_as_real() (#21221)
    * Add view_as_complex and view_as_real methods and tests
    
    * Make op jax compatible
    
    * Run linter
    
    * Remove outdated test
    
    * Remove is_complex() call
    
    * Skip tests for openvino
    
    * fix backend call
    
    * update tests
    
    * update tests
    
    * Use standardize_dtype for comparisons
    
    * Move impl to math.py
    
    * Remove math namespace

--- keras/api/_tf_keras/keras/ops/__init__.py
+++ keras/api/_tf_keras/keras/ops/__init__.py
@@ -60,6 +60,8 @@ from keras.src.ops.math import segment_max as segment_max
 from keras.src.ops.math import segment_sum as segment_sum
 from keras.src.ops.math import stft as stft
 from keras.src.ops.math import top_k as top_k
+from keras.src.ops.math import view_as_complex as view_as_complex
+from keras.src.ops.math import view_as_real as view_as_real
 from keras.src.ops.nn import average_pool as average_pool
 from keras.src.ops.nn import batch_normalization as batch_normalization
 from keras.src.ops.nn import binary_crossentropy as binary_crossentropy
