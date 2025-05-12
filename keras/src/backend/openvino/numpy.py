43067896abda54682dcc2df1d96818b3a5a8c29a
Shruti Vishwakarma <shwvkrm@gmail.com>
2025-05-12 14:49:28 +0530

    Update numpy.py

--- keras/src/backend/openvino/numpy.py
+++ keras/src/backend/openvino/numpy.py
@@ -1212,8 +1212,8 @@ def logspace(start, stop, num=50, endpoint=True, base=10, dtype=None, axis=0):
             start, stop, num=num, endpoint=endpoint, base=base, dtype=dtype
         )
 
-        np_dtype = y.dtype
-        np_dtype = np.float64 if dtype is None else standardize_dtype(np_dtype)
+        np_dtype = np.float64 if dtype is None else y.dtype
+        np_dtype = standardize_dtype(np_dtype)
         np_dtype = OPENVINO_DTYPES[np_dtype]
         # y = ov_opset.convert(get_ov_output(y), np_dtype).output(0)
         # return OpenVINOKerasTensor(y)
