3bedb9a970394879360fcb1c0264f3ffdc634a77
franklin5 <franklin5@users.noreply.github.com>
2025-05-19 13:59:36 -0700

    Update layer.py add_metric error message (#21290)
    * Update layer.py add_metric error message
    
    * Update layer.py
    
    ---------
    
    Co-authored-by: François Chollet <francois.chollet@gmail.com>

--- keras/src/layers/layer.py
+++ keras/src/layers/layer.py
@@ -1417,8 +1417,11 @@ class Layer(BackendLayer, Operation, KerasSaveable):
     def add_metric(self, *args, **kwargs):
         # Permanently disabled
         raise NotImplementedError(
-            "Layer `add_metric()` method is deprecated"
-            " add your metric in `Model.compile(metrics=[...]).`"
+            "Layer `add_metric()` method is deprecated. "
+            "Add your metric in `Model.compile(metrics=[...])`, "
+            "or create metric trackers in init() or build() "
+            "when subclassing the layer or model, then call "
+            "`metric.update_state()` whenever necessary."
         )
 
     def count_params(self):
