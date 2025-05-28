afcf5fd1b9df01d1eadb8fde95ba22fba06c3fc8
divyashreepathihalli <divyashreepathihalli@gmail.com>
2025-05-28 22:48:04 +0000

    clean up logic

--- keras/src/utils/progbar.py
+++ keras/src/utils/progbar.py
@@ -79,8 +79,6 @@ class Progbar:
 
         values = values or []
         for k, v in values:
-            if finalize and k not in self.stateful_metrics:
-                self._values[k] = [v, 1]
             if k not in self._values_order:
                 self._values_order.append(k)
             if k not in self.stateful_metrics:
@@ -89,12 +87,15 @@ class Progbar:
                 # called, which will cause 'current' and 'self._seen_so_far' to
                 # have the same value. Force the minimal value to 1 here,
                 # otherwise stateful_metric will be 0s.
-                value_base = max(current - self._seen_so_far, 1)
-                if k not in self._values:
-                    self._values[k] = [v * value_base, value_base]
+                if finalize:
+                    self._values[k] = [v, 1]
                 else:
-                    self._values[k][0] += v * value_base
-                    self._values[k][1] += value_base
+                    value_base = max(current - self._seen_so_far, 1)
+                    if k not in self._values:
+                        self._values[k] = [v * value_base, value_base]
+                    else:
+                        self._values[k][0] += v * value_base
+                        self._values[k][1] += value_base
             else:
                 # Stateful metrics output a numeric value. This representation
                 # means "take an average from a single value" but keeps the
