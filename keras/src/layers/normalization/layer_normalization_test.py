3738fdb62e563be437bafb56c46adc0059b34d17
Abheesht Sharma <abheesht@google.com>
2025-06-04 19:18:02 +0530

    Fix numerics UT (2)

--- keras/src/layers/normalization/layer_normalization_test.py
+++ keras/src/layers/normalization/layer_normalization_test.py
@@ -123,7 +123,9 @@ class LayerNormalizationTest(testing.TestCase):
         )
         inputs = np.arange(5).astype("float32")[None, :]
         out = layer(inputs)
-        self.assertAllClose(out, [[0.0, 0.40821, 0.81643, 1.22464, 1.63286]])
+        self.assertAllClose(
+            out, [[0.0, 0.408214, 0.816429, 1.224643, 1.632857]]
+        )
 
     def test_large_value_within_autocast_scope(self):
         layer = layers.LayerNormalization()
