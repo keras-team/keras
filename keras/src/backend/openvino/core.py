8213e9feb75acc22961dff101b87f558fdc8ec7a
Przemyslaw Wysocki <przemyslaw.wysocki@intel.com>
2025-07-14 09:03:44 +0200

    Update keras/src/backend/openvino/core.py

--- keras/src/backend/openvino/core.py
+++ keras/src/backend/openvino/core.py
@@ -6,7 +6,10 @@ import numpy as np
 import openvino as ov
 import openvino.opset14 as ov_opset
 
-from openvino import Model, Tensor, compile_model, Type
+from openvino import Model
+from openvino import Tensor
+from openvino import Type
+from openvino import compile_model
 
 from keras.src import tree
 from keras.src.backend.common import KerasVariable
