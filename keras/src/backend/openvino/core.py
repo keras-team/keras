4e1b250491627f871f6c82c0dcb577cc21093def
Przemyslaw Wysocki <przemyslaw.wysocki@intel.com>
2025-08-08 02:33:29 +0200

    [OpenVINO backend] Remove usage of deprecated `openvino.runtime` (#21418)
    * Remove runtime
    
    * Try to init ov first
    
    * Revert "Try to init ov first"
    
    This reverts commit de0fb678a8acdc74bdea0b1f1a86f9695e4515dc.
    
    * Restart CI
    
    * Update keras/src/backend/openvino/core.py
    
    * Update keras/src/backend/openvino/core.py
    
    * Update formatting
    
    * Restart CI

--- keras/src/backend/openvino/core.py
+++ keras/src/backend/openvino/core.py
@@ -4,11 +4,11 @@ import warnings
 
 import numpy as np
 import openvino as ov
-import openvino.runtime.opset14 as ov_opset
+import openvino.opset14 as ov_opset
 from openvino import Model
 from openvino import Tensor
+from openvino import Type
 from openvino import compile_model
-from openvino.runtime import Type
 
 from keras.src import tree
 from keras.src.backend.common import KerasVariable
