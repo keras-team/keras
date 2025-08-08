f9bc511c14063d2b916765f26cf483e631b50f29
ILCSFNO <138545608+ILCSFNO@users.noreply.github.com>
2025-08-08 18:06:26 +0800

    Fix the Doc of `alpha` in `mobilenet_v3`

--- keras/src/applications/mobilenet_v3.py
+++ keras/src/applications/mobilenet_v3.py
@@ -90,7 +90,9 @@ Args:
         E.g. `(160, 160, 3)` would be one valid value.
     alpha: controls the width of the network. This is known as the
         depth multiplier in the MobileNetV3 paper, but the name is kept for
-        consistency with MobileNetV1 in Keras.
+        consistency with MobileNetV1 in Keras. When `weights` is of default
+        value `"imagenet"`, `alpha` can be one of `0.75`, `1.0` for non
+        minimalistic or `1.0` for minimalistic only.
         - If `alpha < 1.0`, proportionally decreases the number
             of filters in each layer.
         - If `alpha > 1.0`, proportionally increases the number
