45c98ec8297f5a5b61c99a721c1801a11b7fec89
Hongyu, Chiu <20734616+james77777778@users.noreply.github.com>
2025-08-15 04:13:00 +0800

    Add `ops.image.scale_and_translate`. (#21577)
    * Add `ops.image.scale_and_translate`.
    
    * Fix op tests.
    
    * Fix np resize.

--- keras/api/ops/image/__init__.py
+++ keras/api/ops/image/__init__.py
@@ -16,3 +16,4 @@ from keras.src.ops.image import perspective_transform as perspective_transform
 from keras.src.ops.image import resize as resize
 from keras.src.ops.image import rgb_to_grayscale as rgb_to_grayscale
 from keras.src.ops.image import rgb_to_hsv as rgb_to_hsv
+from keras.src.ops.image import scale_and_translate as scale_and_translate
