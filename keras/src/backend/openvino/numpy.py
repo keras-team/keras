bf331c9410637bb6b0e5b63fd88fe91a524f9ac1
arunthakur009 <nk9599341@gmail.com>
2025-08-22 00:26:27 +0530

    Minor changes

--- keras/src/backend/openvino/numpy.py
+++ keras/src/backend/openvino/numpy.py
@@ -1105,13 +1105,15 @@ def meshgrid(*x, indexing="xy"):
     tensors = [get_ov_output(xi) for xi in x]
     n = len(tensors)
 
-    shapes = [ov_opset.shape_of(t, Type.i64).output(0) for t in tensors]   # each is [Ni]
+    shapes = [
+        ov_opset.shape_of(t, Type.i64).output(0) for t in tensors
+    ]  # each is [Ni]
     one = ov_opset.constant([1], Type.i64).output(0)
 
     if indexing == "xy":
         shape_list = [shapes[1], shapes[0]] + shapes[2:]
         out_shape = ov_opset.concat(shape_list, axis=0).output(0)
-    else:  
+    else:
         out_shape = ov_opset.concat(shapes, axis=0).output(0)
 
     outputs = []
@@ -1124,7 +1126,7 @@ def meshgrid(*x, indexing="xy"):
                 reshape_parts[0] = shapes[1]
             else:
                 reshape_parts[i] = shapes[i]
-        else:  
+        else:
             reshape_parts[i] = shapes[i]
 
         reshape_shape = ov_opset.concat(reshape_parts, axis=0).output(0)
