efa8cb405a552eb2413892793ff4fe6544981e8f
Axel Roebel <roebela@free.fr>
2025-08-24 20:42:16 +0200

    Implemented suggested changes:
    - removed explicit call to layer.build from the tests,
    - changed the new test to use Model.compile and Model.fit to cover the corresponding API in the tests.

--- keras/src/layers/reshaping/reshape_test.py
+++ keras/src/layers/reshaping/reshape_test.py
@@ -1,7 +1,7 @@
 import pytest
 from absl.testing import parameterized
 
-from keras.src import Model
+from keras.src import Sequential
 from keras.src import backend
 from keras.src import layers
 from keras.src import ops
@@ -98,40 +98,20 @@ class ReshapeTest(testing.TestCase):
     def test_reshape_with_dynamic_batch_size_and_minus_one(self):
         input = KerasTensor((None, 6, 4))
         layer = layers.Reshape((-1, 8))
-        layer.build(input.shape)
         reshaped = backend.compute_output_spec(layer.__call__, input)
         self.assertEqual(reshaped.shape, (None, 3, 8))
 
     def test_reshape_layer_with_varying_input_size_and_minus_one(self):
         input = KerasTensor((None, 6, 4))
         layer = layers.Reshape((-1, 8))
-        layer.build(input.shape)
         res = layer(ops.ones((1, 6, 4), dtype="float32"))
         self.assertEqual(res.shape, (1, 3, 8))
         res = layer(ops.ones((1, 10, 4), dtype="float32"))
         self.assertEqual(res.shape, (1, 5, 8))
 
-    def test_custom_reshape_model_with_varying_input_size_and_minus_one(self):
-        class MM(Model):
-            def __init__(self):
-                super().__init__()
-                self.conv = layers.Conv1D(4, 3, padding="same")
-                self.reshape = layers.Reshape((-1, 8))
-
-            def call(self, inputs):
-                x = self.conv(inputs)
-                return self.reshape(x)
-
-        m = MM()
-        res = m(ops.ones((1, 6, 2), dtype="float32"))
-        self.assertEqual(res.shape, (1, 3, 8))
-        res = m(ops.ones((1, 10, 2), dtype="float32"))
-        self.assertEqual(res.shape, (1, 5, 8))
-
     def test_reshape_with_dynamic_dim_and_minus_one(self):
         input = KerasTensor((4, 6, None, 3))
         layer = layers.Reshape((-1, 3))
-        layer.build(input.shape)
         reshaped = backend.compute_output_spec(layer.__call__, input)
         self.assertEqual(reshaped.shape, (4, None, 3))
 
@@ -140,3 +120,19 @@ class ReshapeTest(testing.TestCase):
         reshaped = layers.Reshape((3, 5))(input_layer)
         # Also make sure the batch dim is not lost after reshape.
         self.assertEqual(reshaped.shape, (2, 3, 5))
+
+    def test_reshape_model_fit_with_varying_input_size_and_minus_one(self):
+        def generator():
+            yield (
+                ops.ones((1, 12, 2), dtype="float32"),
+                ops.zeros((1, 3, 8), dtype="float32"),
+            )
+            yield (
+                ops.ones((1, 20, 2), dtype="float32"),
+                ops.zeros((1, 5, 8), dtype="float32"),
+            )
+
+        layer = layers.Reshape((-1, 8))
+        model = Sequential([layer])
+        model.compile(loss="mean_squared_error")
+        model.fit(generator(), steps_per_epoch=2, epochs=1)
