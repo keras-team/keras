d9129e358f67e79791a27cbf9a95344ec4074995
Miguel Gomes <miguel.teixeira.gomes@gmail.com>
2025-09-09 19:36:13 +0100

    fix: use canonicalize_axis function in reduce_shape function

--- keras/src/ops/operation_utils.py
+++ keras/src/ops/operation_utils.py
@@ -378,6 +378,8 @@ def reduce_shape(shape, axis=None, keepdims=False):
     elif isinstance(axis, int):
         axis = (axis,)
 
+    axis = [canonicalize_axis(a, len(shape)) for a in axis]
+
     if keepdims:
         for ax in axis:
             shape[ax] = 1
