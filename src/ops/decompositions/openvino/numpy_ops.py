9bd54eb0fc7c5bb935db0ab051d5405604ee70b2
TahaRh1 <taharh210@gmail.com>
2025-09-13 14:30:45 +0100

    Format code with black to pass CI

--- src/ops/decompositions/openvino/numpy_ops.py
+++ src/ops/decompositions/openvino/numpy_ops.py
@@ -2,20 +2,16 @@
 
 import numpy as np
 
+
 # logspace function that returns a numpy array (TODO: replace with actual OpenVINO nodes)
 def logspace(start, stop, num=50, dtype=np.float32):
     # TODO: Implement with OpenVINO operations instead of numpy
     values = np.logspace(start, stop, num=num, dtype=dtype)
     return values
 
+
 # Dummy evaluate function placeholder
 def _dummy_evaluate(node):
     # TODO: Implement a real evaluation function for OpenVINO graphs
     # If node is already a numpy array, just return it
     return node
-
-
-
-
-
-
