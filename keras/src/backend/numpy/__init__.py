f825cd385a2b5b143599eb7a5a12ef71f470bead
Suhana <suhanaaa@google.com>
2025-09-26 20:43:01 +0530

    Fixing test

--- keras/src/backend/numpy/__init__.py
+++ keras/src/backend/numpy/__init__.py
@@ -20,7 +20,6 @@ from keras.src.backend.numpy.core import is_tensor
 from keras.src.backend.numpy.core import random_seed_dtype
 from keras.src.backend.numpy.core import shape
 from keras.src.backend.numpy.core import vectorized_map
-from keras.src.backend.numpy.numpy import take
 from keras.src.backend.numpy.rnn import cudnn_ok
 from keras.src.backend.numpy.rnn import gru
 from keras.src.backend.numpy.rnn import lstm
