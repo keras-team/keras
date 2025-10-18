effc784f1ee06f2fdf29cf1094cc58260ccdb6ef
Utsab Dahal <utsabdahal34@gmail.com>
2025-10-18 13:35:02 +0545

    Move save_img tests to unit tests and convert to Keras TestCase

--- keras/src/utils/image_utils_test.py
+++ keras/src/utils/image_utils_test.py
@@ -1,16 +1,12 @@
 import os
-
 import numpy as np
 from absl.testing import parameterized
-
 from keras.src import testing
-
-from .image_utils import img_to_array
-from .image_utils import load_img
-from .image_utils import save_img
+from keras.utils import img_to_array, load_img, save_img
 
 
 class SaveImgJpgTest(testing.TestCase, parameterized.TestCase):
+
     @parameterized.parameters(
         ((50, 50, 3), "rgb.jpg"),
         ((50, 50, 4), "rgba.jpg"),
@@ -23,7 +19,8 @@ class SaveImgJpgTest(testing.TestCase, parameterized.TestCase):
         save_img(path, img, file_format="jpg")
         self.assertTrue(os.path.exists(path))
 
-        # Check that the image was saved correctly and converted to RGB if needed.
+        # Check that the image was saved correctly
+        # and converted to RGB if needed.
         loaded_img = load_img(path)
         loaded_array = img_to_array(loaded_img)
         self.assertEqual(loaded_array.shape, (50, 50, 3))
