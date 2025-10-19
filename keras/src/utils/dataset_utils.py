66bfc209a2dc9ad85d570f4d42933147107652f1
daniel.cahall <danielenricocahall@gmail.com>
2025-10-19 16:38:43 -0400

    fix docstring

--- keras/src/utils/dataset_utils.py
+++ keras/src/utils/dataset_utils.py
@@ -96,7 +96,7 @@ def split_dataset_tf(
 
     Args:
         dataset:
-            A `tf.data.Dataset`, a `torch.utils.data.Dataset` object,
+            A `tf.data.Dataset` object,
             or a list/tuple of arrays with the same length.
         left_size: If float (in the range `[0, 1]`), it signifies
             the fraction of the data to pack in the left dataset. If integer, it
@@ -115,15 +115,6 @@ def split_dataset_tf(
     Returns:
         A tuple of two `tf.data.Dataset` objects:
         the left and right splits.
-
-    Example:
-
-    >>> data = np.random.random(size=(1000, 4))
-    >>> left_ds, right_ds = keras.utils.split_dataset(data, left_size=0.8)
-    >>> int(left_ds.cardinality())
-    800
-    >>> int(right_ds.cardinality())
-    200
     """
     from keras.src.utils.module_utils import tensorflow as tf
 
@@ -185,6 +176,30 @@ def split_dataset_tf(
 def split_dataset_torch(
     dataset, left_size=None, right_size=None, shuffle=False, seed=None
 ):
+    """Splits a dataset into a left half and a right half (e.g. train / test).
+
+    Args:
+        dataset:
+            A `torch.utils.data.Dataset` object,
+            or a list/tuple of arrays with the same length.
+        left_size: If float (in the range `[0, 1]`), it signifies
+            the fraction of the data to pack in the left dataset. If integer, it
+            signifies the number of samples to pack in the left dataset. If
+            `None`, defaults to the complement to `right_size`.
+            Defaults to `None`.
+        right_size: If float (in the range `[0, 1]`), it signifies
+            the fraction of the data to pack in the right dataset.
+            If integer, it signifies the number of samples to pack
+            in the right dataset.
+            If `None`, defaults to the complement to `left_size`.
+            Defaults to `None`.
+        shuffle: Boolean, whether to shuffle the data before splitting it.
+        seed: A random seed for shuffling.
+
+    Returns:
+        A tuple of two `torch.utils.data.Dataset` objects:
+        the left and right splits.
+    """
     import torch
     from torch.utils.data import TensorDataset
     from torch.utils.data import random_split
