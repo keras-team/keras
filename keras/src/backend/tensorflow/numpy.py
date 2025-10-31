3973b153494d33f6289b38804f7b896b50748429
Wenyi Guo <41378453+wenyi-guo@users.noreply.github.com>
2025-10-30 19:32:47 -0700

    Support scalar view for tf backend. (#21802)
    * Support scalar view for tf backend.
    
    * update last dim

--- keras/src/backend/tensorflow/numpy.py
+++ keras/src/backend/tensorflow/numpy.py
@@ -1010,7 +1010,11 @@ def view(x, dtype=None):
     old_itemsize = old_dtype.size
     new_itemsize = new_dtype.size
 
-    if list(x.shape)[-1] * old_itemsize % new_itemsize != 0:
+    old_shape = list(shape_op(x))
+    last_dim_size = old_shape[-1] if len(old_shape) > 0 else -1
+    if (last_dim_size == -1 and old_itemsize != new_itemsize) or (
+        last_dim_size * old_itemsize % new_itemsize != 0
+    ):
         raise ValueError(
             f"Cannot view array of shape {x.shape} and dtype {old_dtype} "
             f"as dtype {new_dtype} because the total number of bytes "
@@ -1027,8 +1031,6 @@ def view(x, dtype=None):
         cast_tensor = tf.bitcast(flat_tensor, type=new_dtype)
         return tf.reshape(cast_tensor, new_shape)
     else:
-        old_shape = list(shape_op(x))
-        last_dim_size = old_shape[-1]
         ratio = new_itemsize // old_itemsize
         if isinstance(last_dim_size, int) and last_dim_size % ratio != 0:
             raise ValueError(
