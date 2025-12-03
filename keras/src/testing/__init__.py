0aafd055d02a477578488dab773a42b37470d2b0
wenyi-guo <wenyiguo@google.com>
2025-12-03 08:13:19 +0000

    fix import rename

--- keras/src/testing/__init__.py
+++ keras/src/testing/__init__.py
@@ -1,6 +1,6 @@
 from keras.src.testing.test_case import TestCase
 from keras.src.testing.test_case import jax_uses_gpu
-from keras.src.testing.test_case import jax_uses_tpu
+from keras.src.testing.test_case import uses_tpu
 from keras.src.testing.test_case import tensorflow_uses_gpu
 from keras.src.testing.test_case import torch_uses_gpu
 from keras.src.testing.test_case import uses_gpu
