7660ee3261e88c560a60af5eaa1b026dd54935b2
MCCbena <77214617+MCCbena@users.noreply.github.com>
2025-12-26 02:37:44 +0900

    Fixed a bug in _keras_mask (#21946)
    * Fixed a bug in _keras_mask
    
    * update Layer
    
    * Added a test for masking during autocast
    
    * Skip numpy checks
    
    * Test Fixes
    
    * remove files

--- keras/src/layers/layer.py
+++ keras/src/layers/layer.py
@@ -836,9 +836,14 @@ class Layer(BackendLayer, Operation):
         #############################################################
         # 1. Convert any array arguments to tensors of correct dtype.
         def maybe_convert(x):
-            return self.dtype_policy.convert_input(
+            # Prevent _keras_mask from disappearing
+            mask = backend.get_keras_mask(x)
+            y = self.dtype_policy.convert_input(
                 x, self.autocast, self.input_dtype
             )
+            if mask is not None:
+                backend.set_keras_mask(y, mask)
+            return y
 
         # Used to avoid expensive `tree` operations in the most common case.
         if (
