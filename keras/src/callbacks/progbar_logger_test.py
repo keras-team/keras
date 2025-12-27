9b44330b15ba63b82efc9c9b838b0958186b21ff
Nithurshen <nithurshen@gmail.com>
2025-12-27 09:37:12 +0530

    fix: tests

--- keras/src/callbacks/progbar_logger_test.py
+++ keras/src/callbacks/progbar_logger_test.py
@@ -1,3 +1,6 @@
+import io
+from unittest.mock import patch
+
 from keras.src import callbacks
 from keras.src import testing
 
@@ -27,7 +30,6 @@ class ProgbarLoggerTest(testing.TestCase):
 
         """
         logger = callbacks.ProgbarLogger(pinned=True)
-
         params = {
             "verbose": 1,
             "epochs": 1,
@@ -46,6 +48,7 @@ class ProgbarLoggerTest(testing.TestCase):
         logger.on_epoch_end(0)
 
     def test_progbar_logger_verbose_2(self):
+        """Verify that pinned=True has no effect when verbose=2."""
         logger = callbacks.ProgbarLogger(pinned=True)
         params = {
             "verbose": 2,
@@ -53,5 +56,13 @@ class ProgbarLoggerTest(testing.TestCase):
             "steps": 5,
         }
         logger.set_params(params)
-        logger.on_epoch_begin(0)
-        logger.on_epoch_end(0, logs={"loss": 0.1})
+
+        with patch("sys.stdout", new=io.StringIO()) as fake_out:
+            logger.on_epoch_begin(0)
+            logger.on_train_batch_end(0, logs={"loss": 0.1})
+            logger.on_epoch_end(0, logs={"loss": 0.1})
+
+            captured_output = fake_out.getvalue()
+
+            self.assertNotIn("\033[s", captured_output)
+            self.assertNotIn("\033[H", captured_output)
