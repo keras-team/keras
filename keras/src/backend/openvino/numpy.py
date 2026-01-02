34852173ea07718677ded6724e72cccf0e8c33e7
Kartikeya Srivastava <kartikeyasrivastava769@gmail.com>
2026-01-02 11:42:59 +0530

    fix code formatting

--- keras/src/backend/openvino/numpy.py
+++ keras/src/backend/openvino/numpy.py
@@ -895,23 +895,24 @@ def diag(x, k=0):
 def diagonal(x, offset=0, axis1=0, axis2=1):
     x = get_ov_output(x)
     shape = x.get_partial_shape()
-    rank = x.get_partial_shape().rank.get_length() 
+    rank = x.get_partial_shape().rank.get_length()
     if rank is None:
         raise ValueError("`diagonal` requires input tensor with static rank.")
     if rank < 2:
-        raise ValueError(f"diagonal requires input tensor with rank >= 2." 
-                         f"Given rank: {rank}")
+        raise ValueError(
+            f"diagonal requires input tensor with rank >= 2.Given rank: {rank}"
+        )
     axis1 = axis1 % rank
     axis2 = axis2 % rank
     if axis1 == axis2:
         raise ValueError("`axis1` and `axis2` cannot be the same.")
-    
-    perm_order = [axis1, axis2] + [i
-                        for i in range(rank) if i != axis1 and i != axis2
-                                   ]
+
+    perm_order = [axis1, axis2] + [
+        i for i in range(rank) if i != axis1 and i != axis2
+    ]
     perm_const = ov_opset.constant(np.array(perm_order, dtype=np.int32))
     x_transposed = ov_opset.transpose(x, perm_const)
-    
+
     N_dim = shape[axis1]
     M_dim = shape[axis2]
     if not N_dim.is_static or not M_dim.is_static:
@@ -927,16 +928,16 @@ def diagonal(x, offset=0, axis1=0, axis2=1):
     else:
         L = np.minimum(N + offset, M) if (N + offset) > 0 else 0
         indices = [[i - offset, i] for i in range(L)]
-        
+
     indices = np.array(indices, dtype=np.int32)
     indices_const = ov_opset.constant(indices, dtype=Type.i32).output(0)
-    
+
     diag_gathered = ov_opset.gather_nd(x_transposed, indices_const)
-    
+
     out_rank = rank - 1
     out_perm_order = list(range(1, out_rank)) + [0]
     out_perm_const = ov_opset.constant(np.array(out_perm_order, dtype=np.int32))
-    
+
     final_output = ov_opset.transpose(diag_gathered, out_perm_const)
     return OpenVINOKerasTensor(final_output.output(0))
 
