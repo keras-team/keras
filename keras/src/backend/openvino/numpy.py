6f58d6100239eb8471fb52cdfb87604792ed5ebf
Shipra <ships3040@gmail.com>
2026-01-18 10:37:05 +0000

    Implement  function for OpenVINO backend

--- keras/src/backend/openvino/numpy.py
+++ keras/src/backend/openvino/numpy.py
@@ -1779,9 +1779,45 @@ def logaddexp(x1, x2):
 
 
 def logaddexp2(x1, x2):
-    raise NotImplementedError(
-        "`logaddexp2` is not supported with openvino backend"
-    )
+    element_type = None
+    if isinstance(x1, OpenVINOKerasTensor):
+        element_type = x1.output.get_element_type()
+    if isinstance(x2, OpenVINOKerasTensor):
+        element_type = x2.output.get_element_type()
+    x1 = get_ov_output(x1, element_type)
+    x2 = get_ov_output(x2, element_type)
+    x1, x2 = _align_operand_types(x1, x2, "logaddexp2()")
+
+    if x1.element_type.is_integral() or x2.element_type.is_integral():
+        float_dtype = OPENVINO_DTYPES[config.floatx()]
+        if x1.get_element_type().is_integral():
+            x1 = ov_opset.convert(x1, float_dtype)
+        if x2.get_element_type().is_integral():
+            x2 = ov_opset.convert(x2, float_dtype)
+
+    max_val = ov_opset.maximum(x1, x2)
+
+    sub = ov_opset.subtract(x1, x2)
+    abs_diff = ov_opset.abs(sub)
+
+    neg_abs_diff = ov_opset.negative(abs_diff)
+
+    element_type = neg_abs_diff.get_element_type()
+    log_2 = ov_opset.log(ov_opset.constant(2, element_type))
+
+    scaled = ov_opset.multiply(neg_abs_diff, log_2)
+
+    exp_scaled = ov_opset.exp(scaled)
+
+    one_plus_exp = ov_opset.add(ov_opset.constant(1, element_type), exp_scaled)
+
+    log = ov_opset.log(one_plus_exp)
+
+    log2_term = ov_opset.divide(log, log_2)
+
+    result = ov_opset.add(max_val, log2_term).output(0)
+
+    return OpenVINOKerasTensor(result)
 
 
 def logical_and(x1, x2):
