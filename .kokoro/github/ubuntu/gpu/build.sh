f62315248f4ed7217178fde9820fcb0933b9323e
RAHUL KUMAR <hellorahul@google.com>
2026-01-20 11:45:37 +0530

    Remove invalid --resume-retries option from pip install commands

--- .kokoro/github/ubuntu/gpu/build.sh
+++ .kokoro/github/ubuntu/gpu/build.sh
@@ -27,7 +27,7 @@ pip install -U psutil
 if [ "$KERAS_BACKEND" == "tensorflow" ]
 then
    echo "TensorFlow backend detected."
-   pip install -r requirements-tensorflow-cuda.txt --progress-bar off --timeout 1800 --retries 10 --resume-retries 10 --no-cache-dir
+   pip install -r requirements-tensorflow-cuda.txt --progress-bar off --timeout 1800 --retries 10 --no-cache-dir
    pip uninstall -y keras keras-nightly
    echo "Check that TensorFlow uses GPU"
    python3 -c 'import tensorflow as tf;print(tf.__version__);print(tf.config.list_physical_devices("GPU"))'
@@ -44,7 +44,7 @@ fi
 if [ "$KERAS_BACKEND" == "jax" ]
 then
    echo "JAX backend detected."
-   pip install -r requirements-jax-cuda.txt --progress-bar off --timeout 1800 --retries 10 --resume-retries 10 --no-cache-dir
+   pip install -r requirements-jax-cuda.txt --progress-bar off --timeout 1800 --retries 10 --no-cache-dir
    pip uninstall -y keras keras-nightly
    python3 -c 'import jax;print(jax.__version__);print(jax.default_backend())'
    # Raise error if GPU is not detected.
@@ -67,7 +67,7 @@ fi
 if [ "$KERAS_BACKEND" == "torch" ]
 then
    echo "PyTorch backend detected."
-   pip install -r requirements-torch-cuda.txt --progress-bar off --timeout 1800 --retries 10 --resume-retries 10 --no-cache-dir
+   pip install -r requirements-torch-cuda.txt --progress-bar off --timeout 1800 --retries 10 --no-cache-dir
    pip uninstall -y keras keras-nightly
    python3 -c 'import torch;print(torch.__version__);print(torch.cuda.is_available())'
    # Raise error if GPU is not detected.
