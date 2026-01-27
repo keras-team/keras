dea98e332d240760fa694532cd1b36e5273c0ae5
Malyala Karthik <karthikmalyala728@gmail.com>
2026-01-27 18:59:13 +0530

    Fix arctan2 NaN propagation in OpenVINO backend

--- keras/src/ops/numpy_test.py
+++ keras/src/ops/numpy_test.py
@@ -3824,7 +3824,8 @@ class NumpyTwoInputOpsCorrectnessTest(testing.TestCase):
 
     def test_arctan2_nan_propagation(self):
         out = knp.arctan2(1.0, np.nan)
-        self.assertTrue(np.isnan(out.numpy()))
+        out_np = np.asarray(out)
+        self.assertTrue(np.isnan(out_np))
 
 
 class NumpyOneInputOpsCorrectnessTest(testing.TestCase):
