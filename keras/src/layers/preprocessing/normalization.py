0d0884ad54bcca6d7e349ab384c23f4355f62b82
Maitry Sinha <maitry@google.com>
2026-01-28 00:36:34 +0530

    fix: comment

--- keras/src/layers/preprocessing/normalization.py
+++ keras/src/layers/preprocessing/normalization.py
@@ -192,25 +192,24 @@ class Normalization(DataLayer):
             self.built = True
             self.finalize_state()
         else:
-             # In the no adapt case, make constant tensors for mean and variance
-             
-             mean = ops.convert_to_tensor(self.input_mean)
-             variance = ops.convert_to_tensor(self.input_variance)  
+            # In the no adapt case, make constant tensors for mean and variance
 
-             # proper broadcast for scalars 
-                 
-             if ops.ndim(mean) == 0:
+            mean = ops.convert_to_tensor(self.input_mean)
+            variance = ops.convert_to_tensor(self.input_variance)
+
+            # Broadcast scalar mean/variance to the target shape
+
+            if ops.ndim(mean) == 0:
                 mean = ops.broadcast_to(mean, self._broadcast_shape)
                 variance = ops.broadcast_to(variance, self._broadcast_shape)
-             else:
-
-                # proper reshape for multi-dim 
+            else:
+                # Reshape multi-dimensional mean/variance to match the broadcast
 
                 mean = ops.reshape(mean, self._broadcast_shape)
                 variance = ops.reshape(variance, self._broadcast_shape)
 
-             self.mean = ops.cast(mean, dtype=self.compute_dtype)
-             self.variance = ops.cast(variance, dtype=self.compute_dtype)
+            self.mean = ops.cast(mean, dtype=self.compute_dtype)
+            self.variance = ops.cast(variance, dtype=self.compute_dtype)
 
     def adapt(self, data):
         """Computes the mean and variance of values in a dataset.
