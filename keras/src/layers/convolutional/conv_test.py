49b1ccbc71b381fe48ae3fff734b59e5a7f51e9c
Malyala Karthik <karthikmalyala728@gmail.com>
2026-02-01 12:40:44 +0530

    Fix conv symbolic execution to fail on empty output shapes

--- keras/src/layers/convolutional/conv_test.py
+++ keras/src/layers/convolutional/conv_test.py
@@ -807,6 +807,48 @@ class ConvBasicTest(testing.TestCase):
             supports_masking=False,
         )
 
+    def test_conv1d_symbolic_invalid_configuration(self):
+        inputs = layers.Input(shape=(10, 3))
+        layer = layers.Conv1D(
+            filters=1,
+            kernel_size=2,
+            dilation_rate=10,
+        )
+
+        with self.assertRaisesRegex(
+            ValueError,
+            "Invalid convolution configuration",
+        ):
+            layer(inputs)
+
+    def test_conv2d_symbolic_invalid_configuration(self):
+        inputs = layers.Input(shape=(10, 10, 3))
+        layer = layers.Conv2D(
+            filters=1,
+            kernel_size=(2, 2),
+            dilation_rate=(10, 10),
+        )
+
+        with self.assertRaisesRegex(
+            ValueError,
+            "Invalid convolution configuration",
+        ):
+            layer(inputs)
+
+    def test_conv3d_symbolic_invalid_configuration(self):
+        inputs = layers.Input(shape=(10, 10, 10, 3))
+        layer = layers.Conv3D(
+            filters=1,
+            kernel_size=(2, 2, 2),
+            dilation_rate=(10, 10, 10),
+        )
+
+        with self.assertRaisesRegex(
+            ValueError,
+            "Invalid convolution configuration",
+        ):
+            layer(inputs)
+
 
 class ConvCorrectnessTest(testing.TestCase):
     @parameterized.parameters(
