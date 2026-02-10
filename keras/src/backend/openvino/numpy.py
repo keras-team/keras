f93b54029123f788ece8502c66d2e1b608d57a1a
Shipra <Shi-pra-19@users.noreply.github.com>
2026-02-10 16:43:08 +0000

    add bitwise operations (and, or, xor, invert) for OpenVINO backend

--- keras/src/backend/openvino/numpy.py
+++ keras/src/backend/openvino/numpy.py
@@ -677,6 +677,47 @@ def bincount(x, weights=None, minlength=0, sparse=False):
         return OpenVINOKerasTensor(final_output)
 
 
+def bitwise_and(x1, x2):
+    element_type = None
+    if isinstance(x1, OpenVINOKerasTensor):
+        element_type = x1.output.get_element_type()
+    if isinstance(x2, OpenVINOKerasTensor):
+        element_type = x2.output.get_element_type()
+    x1 = get_ov_output(x1, element_type)
+    x2 = get_ov_output(x2, element_type)
+    x1, x2 = _align_operand_types(x1, x2, "bitwise_and()")
+    return OpenVINOKerasTensor(ov_opset.bitwise_and(x1, x2).output(0))
+
+
+def bitwise_xor(x1, x2):
+    element_type = None
+    if isinstance(x1, OpenVINOKerasTensor):
+        element_type = x1.output.get_element_type()
+    if isinstance(x2, OpenVINOKerasTensor):
+        element_type = x2.output.get_element_type()
+    x1 = get_ov_output(x1, element_type)
+    x2 = get_ov_output(x2, element_type)
+    x1, x2 = _align_operand_types(x1, x2, "bitwise_xor()")
+    return OpenVINOKerasTensor(ov_opset.bitwise_xor(x1, x2).output(0))
+
+
+def bitwise_invert(x):
+    x = get_ov_output(x)
+    return OpenVINOKerasTensor(ov_opset.bitwise_not(x).output(0))
+
+
+def bitwise_or(x1, x2):
+    element_type = None
+    if isinstance(x1, OpenVINOKerasTensor):
+        element_type = x1.output.get_element_type()
+    if isinstance(x2, OpenVINOKerasTensor):
+        element_type = x2.output.get_element_type()
+    x1 = get_ov_output(x1, element_type)
+    x2 = get_ov_output(x2, element_type)
+    x1, x2 = _align_operand_types(x1, x2, "bitwise_or()")
+    return OpenVINOKerasTensor(ov_opset.bitwise_or(x1, x2).output(0))
+
+
 def blackman(x):
     x = get_ov_output(x)
     zero_const = ov_opset.constant(0, Type.i64)
