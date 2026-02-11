233417f88f0ab161e86d9724f2954a30acca4f42
hertschuh <1091026+hertschuh@users.noreply.github.com>
2026-02-11 15:56:43 -0800

    Add support for dynamic dimensions in `ops.slice.compute_output_spec`. (#22154)
    Also added verification that the shape of the inputs, the `shape` parameter and the `indices` have the same length.

--- keras/src/ops/core.py
+++ keras/src/ops/core.py
@@ -401,19 +401,31 @@ class Slice(Operation):
         return backend.core.slice(inputs, start_indices, self.shape)
 
     def compute_output_spec(self, inputs, start_indices):
-        if any(s == -1 for s in self.shape) and isinstance(
-            start_indices, KerasTensor
+        if len(self.shape) != len(inputs.shape):
+            raise ValueError(
+                "The number of dimensions in `inputs` must match the number of "
+                f"dimensions in `shape`. Received inputs.shape={inputs.shape} "
+                f"and shape={self.shape}"
+            )
+        if hasattr(start_indices, "__len__") and len(start_indices) != len(
+            inputs.shape
         ):
             raise ValueError(
-                "When using -1 in `shape`, `start_indices` should not be a "
-                "KerasTensor. "
+                "The number of dimensions in `start_indices` must match the "
+                "number of dimensions in `inputs`. Received "
+                f"start_indices={start_indices} and inputs.shape={inputs.shape}"
             )
-        # If self.shape[i] is -1, all remaining elements in dimension i are
-        # included in the slice.
-        final_shape = tuple(
-            inputs.shape[i] - start_indices[i] if s == -1 else s
-            for i, s in enumerate(self.shape)
-        )
+
+        final_shape = []
+        for i, (input_dim, slice_dim) in enumerate(
+            zip(inputs.shape, self.shape)
+        ):
+            if slice_dim != -1:
+                final_shape.append(slice_dim)
+            elif isinstance(start_indices, KerasTensor) or input_dim is None:
+                final_shape.append(None)
+            else:
+                final_shape.append(input_dim - start_indices[i])
         return KerasTensor(final_shape, dtype=inputs.dtype)
 
 
