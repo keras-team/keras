8b29add5292a9c4001b8eacfe6c52fbfda349718
Akshat-Raj <akshatraj287@gmail.com>
2026-02-14 03:25:03 +0530

    Wrap tree utils in pure python to fix dynamo disable decorator

--- keras/src/backend/torch/core.py
+++ keras/src/backend/torch/core.py
@@ -737,8 +737,41 @@ class CustomGradientFunction(torch.autograd.Function):
         return (None,) + grads
 
 if hasattr(torch, "compiler") and hasattr(torch.compiler, "disable"):
-    tree.flatten = torch.compiler.disable(tree.flatten)
-    tree.pack_sequence_as = torch.compiler.disable(tree.pack_sequence_as)
-    tree.map_structure = torch.compiler.disable(tree.map_structure)
-    tree.is_nested = torch.compiler.disable(tree.is_nested)
-    tree.assert_same_structure = torch.compiler.disable(tree.assert_same_structure)
+    _orig_flatten = tree.flatten
+    _orig_pack_sequence_as = tree.pack_sequence_as
+    _orig_map_structure = tree.map_structure
+    _orig_is_nested = tree.is_nested
+    _orig_assert_same_structure = tree.assert_same_structure
+
+    @torch.compiler.disable
+    def _flatten(*args, **kwargs):
+        return _orig_flatten(*args, **kwargs)
+
+    @torch.compiler.disable
+    def _pack_sequence_as(*args, **kwargs):
+        return _orig_pack_sequence_as(*args, **kwargs)
+
+    @torch.compiler.disable
+    def _map_structure(*args, **kwargs):
+        return _orig_map_structure(*args, **kwargs)
+
+    @torch.compiler.disable
+    def _is_nested(*args, **kwargs):
+        return _orig_is_nested(*args, **kwargs)
+
+    @torch.compiler.disable
+    def _assert_same_structure(*args, **kwargs):
+        return _orig_assert_same_structure(*args, **kwargs)
+
+    tree.flatten = _flatten
+    tree.pack_sequence_as = _pack_sequence_as
+    tree.map_structure = _map_structure
+    tree.is_nested = _is_nested
+    tree.assert_same_structure = _assert_same_structure
+
+    if hasattr(tree, "map_shape_structure"):
+        _orig_map_shape_structure = tree.map_shape_structure
+        @torch.compiler.disable
+        def _map_shape_structure(*args, **kwargs):
+            return _orig_map_shape_structure(*args, **kwargs)
+        tree.map_shape_structure = _map_shape_structure
