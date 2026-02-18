b5ee7a8bcd6e05b91d9a81d4e0f1053f7e6906dd
andersendsa <andersendsa631@gmail.com>
2026-02-18 23:59:08 +0530

    implement numpy.allclose in  Pytorch backend

--- keras/src/backend/torch/numpy.py
+++ keras/src/backend/torch/numpy.py
@@ -925,7 +925,7 @@ def isclose(x1, x2, rtol=1e-5, atol=1e-8, equal_nan=False):
     result_dtype = dtypes.result_type(x1.dtype, x2.dtype)
     x1 = cast(x1, result_dtype)
     x2 = cast(x2, result_dtype)
-    return torch.isclose(x1, x2, rtol, atol, equal_nan)
+    return torch.allclose(x1, x2, rtol=rtol, atol=atol, equal_nan=equal_nan)
 
 
 def isfinite(x):
