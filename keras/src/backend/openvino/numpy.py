9b072724f204d8a06635305fec93e1df2f7d1620
goyaladitya05 <goyaladitya2403@gmail.com>
2026-02-20 01:58:16 +0530

    fixed code style

--- keras/src/backend/openvino/numpy.py
+++ keras/src/backend/openvino/numpy.py
@@ -877,16 +877,12 @@ def clip(x, x_min, x_max):
 
 
 def concatenate(xs, axis=0):
-    
     elems = [get_ov_output(x) for x in xs]
-
-    
     if axis is None:
         flatten_shape = ov_opset.constant([-1], Type.i32).output(0)
         elems = [ov_opset.reshape(x, flatten_shape, False).output(0) 
                  for x in elems]
         axis = 0
-
     keras_types = [ov_to_keras_type(x.get_element_type()) for x in elems]
     if keras_types:
         target_type = dtypes.result_type(*keras_types)
@@ -896,7 +892,6 @@ def concatenate(xs, axis=0):
              if x.get_element_type() != ov_target_type else x
              for x in elems
         ]
-
     res = ov_opset.concat(elems, axis).output(0)
     return OpenVINOKerasTensor(res)
 
