6c3a881924ed4205b870028f9eac44f2a3cd2b0e
Aditya Goyal <107386933+goyaladitya05@users.noreply.github.com>
2026-02-21 08:38:57 +0530

    [OpenVINO backend] Improve numpy.concatenate implemenation (#22222)
    * Implemented concatenate op properly for OpenVINO backend
    
    * fixed code style
    
    * fixed code style
    
    ---------
    
    Co-authored-by: hertschuh <1091026+hertschuh@users.noreply.github.com>

--- keras/src/backend/openvino/numpy.py
+++ keras/src/backend/openvino/numpy.py
@@ -983,11 +983,23 @@ def clip(x, x_min, x_max):
 
 
 def concatenate(xs, axis=0):
-    assert isinstance(xs, list), "`concatenate` is supported only for `x` list"
-    elems = []
-    for elem in xs:
-        elem = get_ov_output(elem)
-        elems.append(elem)
+    elems = [get_ov_output(x) for x in xs]
+    if axis is None:
+        flatten_shape = ov_opset.constant([-1], Type.i32).output(0)
+        elems = [
+            ov_opset.reshape(x, flatten_shape, False).output(0) for x in elems
+        ]
+        axis = 0
+    keras_types = [ov_to_keras_type(x.get_element_type()) for x in elems]
+    if keras_types:
+        target_type = dtypes.result_type(*keras_types)
+        ov_target_type = OPENVINO_DTYPES[target_type]
+        elems = [
+            ov_opset.convert(x, ov_target_type).output(0)
+            if x.get_element_type() != ov_target_type
+            else x
+            for x in elems
+        ]
     res = ov_opset.concat(elems, axis).output(0)
     return OpenVINOKerasTensor(res)
 
