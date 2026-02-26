cc33224983e053bfc5f9c9947d659f8cf8b6b8d0
andersen dsa <andersendsa631@gmail.com>
2026-02-26 19:19:35 +0530

    formatting fix

--- keras/src/backend/openvino/rnn.py
+++ keras/src/backend/openvino/rnn.py
@@ -5,6 +5,8 @@ from openvino import Type
 from keras.src import tree
 from keras.src.backend.openvino.core import OpenVINOKerasTensor
 from keras.src.backend.openvino.core import get_ov_output
+
+
 def rnn(
     step_function,
     inputs,
@@ -26,6 +28,7 @@ def rnn(
         return OpenVINOKerasTensor(
             ov_opset.transpose(input_ov, perm_const).output(0)
         )
+
     if not time_major:
         inputs = tree.map_structure(swap_batch_timestep, inputs)
         if mask is not None:
@@ -51,14 +54,17 @@ def rnn(
                 ov_opset.constant(0, dtype=Type.i32).output(0),
                 time_steps,
                 ov_opset.constant(1, dtype=Type.i32).output(0),
-                output_type=Type.i32
+                output_type=Type.i32,
             ).output(0)
             indices = ov_opset.unsqueeze(
                 indices, ov_opset.constant([1], dtype=Type.i32).output(0)
             ).output(0)
-            input_len_casted = ov_opset.convert(input_len_ov, Type.i32).output(0)
+            input_len_casted = ov_opset.convert(input_len_ov, Type.i32).output(
+                0
+            )
             input_len_expanded = ov_opset.unsqueeze(
-                input_len_casted, ov_opset.constant([0], dtype=Type.i32).output(0)
+                input_len_casted,
+                ov_opset.constant([0], dtype=Type.i32).output(0),
             ).output(0)
             mask_bool = ov_opset.less(indices, input_len_expanded).output(0)
             mask = OpenVINOKerasTensor(mask_bool)
@@ -74,6 +80,7 @@ def rnn(
             ).output(0)
         mask = OpenVINOKerasTensor(mask_ov)
     if go_backwards:
+
         def reverse_time(x):
             x_ov = get_ov_output(x)
             start = ov_opset.constant([0], dtype=Type.i32).output(0)
@@ -86,14 +93,14 @@ def rnn(
                 output_type=Type.i32,
             ).output(0)
             return OpenVINOKerasTensor(
-                ov_opset.gather(
-                    x_ov, idx, start
-                ).output(0)
+                ov_opset.gather(x_ov, idx, start).output(0)
             )
+
         inputs = tree.map_structure(reverse_time, inputs)
         if mask is not None:
             mask = reverse_time(mask)
         flattened_inputs = tree.flatten(inputs)
+
     def _slice_at_0(x):
         x_ov = get_ov_output(x)
         slice_0 = ov_opset.gather(
@@ -102,6 +109,7 @@ def rnn(
             ov_opset.constant(0, dtype=Type.i32).output(0),
         ).output(0)
         return OpenVINOKerasTensor(slice_0)
+
     inputs_0 = tree.map_structure(_slice_at_0, inputs)
     output_0, _ = step_function(
         inputs_0, tuple(initial_states) + tuple(constants or [])
@@ -124,7 +132,11 @@ def rnn(
         if pshape.rank.is_static:
             new_shape = list(pshape)[1:]
         else:
-            new_shape = [-1] * (pshape.rank.get_length() - 1) if pshape.rank.is_static else None
+            new_shape = (
+                [-1] * (pshape.rank.get_length() - 1)
+                if pshape.rank.is_static
+                else None
+            )
         param = ov_opset.parameter(new_shape, inp_ov.get_element_type())
         sliced_inputs_params.append(param)
         params.append(param)
@@ -139,25 +151,35 @@ def rnn(
     merged_states_params = []
     for st in flattened_states:
         st_ov = get_ov_output(st)
-        param = ov_opset.parameter(st_ov.get_partial_shape(), st_ov.get_element_type())
+        param = ov_opset.parameter(
+            st_ov.get_partial_shape(), st_ov.get_element_type()
+        )
         merged_states_params.append(param)
         params.append(param)
     last_output_params = []
     for lo in last_output_states:
         lo_ov = get_ov_output(lo)
-        param = ov_opset.parameter(lo_ov.get_partial_shape(), lo_ov.get_element_type())
+        param = ov_opset.parameter(
+            lo_ov.get_partial_shape(), lo_ov.get_element_type()
+        )
         last_output_params.append(param)
         params.append(param)
     constants_params = []
     for c in flattened_constants:
         c_ov = get_ov_output(c)
-        param = ov_opset.parameter(c_ov.get_partial_shape(), c_ov.get_element_type())
+        param = ov_opset.parameter(
+            c_ov.get_partial_shape(), c_ov.get_element_type()
+        )
         constants_params.append(param)
         params.append(param)
-    sliced_inputs_t = [OpenVINOKerasTensor(p.output(0)) for p in sliced_inputs_params]
-    merged_states_t = [OpenVINOKerasTensor(p.output(0)) for p in merged_states_params]
+    sliced_inputs_t = [
+        OpenVINOKerasTensor(p.output(0)) for p in sliced_inputs_params
+    ]
+    merged_states_t = [
+        OpenVINOKerasTensor(p.output(0)) for p in merged_states_params
+    ]
     constants_t = [OpenVINOKerasTensor(p.output(0)) for p in constants_params]
-    
+
     packed_inputs = tree.pack_sequence_as(inputs, sliced_inputs_t)
     packed_states = tree.pack_sequence_as(initial_states, merged_states_t)
     step_output, step_new_states = step_function(
@@ -170,31 +192,46 @@ def rnn(
     final_last_output_list = []
     if mask is not None:
         mask_t = sliced_mask_params[0].output(0)
-        for i, (new_st, old_st) in enumerate(zip(flat_step_new_states, merged_states_t)):
+        for i, (new_st, old_st) in enumerate(
+            zip(flat_step_new_states, merged_states_t)
+        ):
             new_st_ov = get_ov_output(new_st)
             old_st_ov = get_ov_output(old_st)
             res = ov_opset.select(mask_t, new_st_ov, old_st_ov).output(0)
             final_states_list.append(res)
-        for i, (new_out, old_last_out) in enumerate(zip(flat_step_output, last_output_params)):
+        for i, (new_out, old_last_out) in enumerate(
+            zip(flat_step_output, last_output_params)
+        ):
             new_out_ov = get_ov_output(new_out)
             old_last_out_ov = old_last_out.output(0)
-            last_out_res = ov_opset.select(mask_t, new_out_ov, old_last_out_ov).output(0)
+            last_out_res = ov_opset.select(
+                mask_t, new_out_ov, old_last_out_ov
+            ).output(0)
             final_last_output_list.append(last_out_res)
             if zero_output_for_mask:
                 zero = ov_opset.broadcast(
-                    ov_opset.constant(0, new_out_ov.get_element_type()).output(0),
-                    ov_opset.shape_of(new_out_ov, Type.i32).output(0)
+                    ov_opset.constant(0, new_out_ov.get_element_type()).output(
+                        0
+                    ),
+                    ov_opset.shape_of(new_out_ov, Type.i32).output(0),
                 ).output(0)
-                seq_out_res = ov_opset.select(mask_t, new_out_ov, zero).output(0)
+                seq_out_res = ov_opset.select(mask_t, new_out_ov, zero).output(
+                    0
+                )
             else:
-                seq_out_res = last_out_res 
+                seq_out_res = last_out_res
             final_output_list.append(seq_out_res)
     else:
         final_states_list = [get_ov_output(x) for x in flat_step_new_states]
         final_output_list = [get_ov_output(x) for x in flat_step_output]
         final_last_output_list = [get_ov_output(x) for x in flat_step_output]
     cond_const = ov_opset.constant(True, Type.boolean).output(0)
-    results = [cond_const] + final_states_list + final_last_output_list + final_output_list
+    results = (
+        [cond_const]
+        + final_states_list
+        + final_last_output_list
+        + final_output_list
+    )
     body_model = Model(results, params)
     exec_cond_in = ov_opset.constant(True, Type.boolean).output(0)
     loop = ov_opset.loop(time_steps, exec_cond_in)
@@ -207,18 +244,24 @@ def rnn(
         loop.set_sliced_input(sliced_mask_params[0], mask_ov, 0, 1, 1, -1, 0)
     current_res_idx = 1
     for param, init in zip(merged_states_params, flattened_states):
-        loop.set_merged_input(param, get_ov_output(init), results[current_res_idx])
+        loop.set_merged_input(
+            param, get_ov_output(init), results[current_res_idx]
+        )
         current_res_idx += 1
     final_last_outputs_res = []
     for param, init in zip(last_output_params, last_output_states):
-        loop.set_merged_input(param, get_ov_output(init), results[current_res_idx])
+        loop.set_merged_input(
+            param, get_ov_output(init), results[current_res_idx]
+        )
         final_last_outputs_res.append(results[current_res_idx])
         current_res_idx += 1
     for param, val in zip(constants_params, flattened_constants):
         loop.set_invariant_input(param, get_ov_output(val))
     loop_outputs = []
     for _ in final_output_list:
-        out = loop.get_concatenated_slices(results[current_res_idx], 0, 1, 1, -1, 0)
+        out = loop.get_concatenated_slices(
+            results[current_res_idx], 0, 1, 1, -1, 0
+        )
         loop_outputs.append(OpenVINOKerasTensor(out))
         current_res_idx += 1
     loop_final_states = []
@@ -237,8 +280,10 @@ def rnn(
     new_states = tree.pack_sequence_as(initial_states, loop_final_states)
     last_output = tree.pack_sequence_as(output_0, loop_final_last_outputs)
     if not time_major:
-        outputs = tree.map_structure(swap_batch_timestep, outputs)  
+        outputs = tree.map_structure(swap_batch_timestep, outputs)
     return last_output, outputs, new_states
+
+
 def lstm(*args, **kwargs):
     raise NotImplementedError("`lstm` is not supported with openvino backend")
 
