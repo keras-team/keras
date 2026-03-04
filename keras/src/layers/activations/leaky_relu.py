cea6999836d19c9626b1be12a8f17b7feeb68abd
Shriram Thakare <shriram.220571105@vcet.edu.in>
2026-03-04 16:42:57 +0000

    resolve merge conflict, use concise is_continuous_axis

--- keras/src/layers/activations/leaky_relu.py
+++ keras/src/layers/activations/leaky_relu.py
@@ -43,7 +43,11 @@ class LeakyReLU(Layer):
                 "Argument `alpha` is deprecated. Use `negative_slope` instead."
             )
         super().__init__(**kwargs)
-        if negative_slope is None or math.isnan(negative_slope) or negative_slope < 0 :
+        if (
+            negative_slope is None
+            or math.isnan(negative_slope)
+            or negative_slope < 0
+        ):
             raise ValueError(
                 "The negative_slope value of a Leaky ReLU layer "
                 "cannot be None or negative value. Expected a float."
