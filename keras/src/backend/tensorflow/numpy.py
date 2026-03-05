e22458c69310ec104655996afa6c0dc830379206
rstar327 <rockstar00327@outlook.com>
2026-03-05 20:00:30 +0200

    feat(ops): add nancumprod to keras.ops.numpy

--- keras/src/backend/tensorflow/numpy.py
+++ keras/src/backend/tensorflow/numpy.py
@@ -2232,6 +2232,11 @@ def nancumsum(x, axis=None, dtype=None):
     return cumsum(x, axis=axis, dtype=dtype)
 
 
+def nancumprod(x, axis=None, dtype=None):
+    x = nan_to_num(x, nan=1.0)
+    return cumprod(x, axis=axis, dtype=dtype)
+
+
 def nanmax(x, axis=None, keepdims=False):
     x = convert_to_tensor(x)
 
