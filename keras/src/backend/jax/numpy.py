abfa3ae9e326a7ce87fff1cb1ac3595d3f807992
Ugeun Park <pwg2004@gmail.com>
2026-03-06 01:58:12 +0900

    Implement nanargmax function in keras.ops (#22345)
    * Add nanargmax for backends.
    
    * Add nanargmax for ops.
    
    * Add numpy test
    
    * Update openvino side

--- keras/src/backend/jax/numpy.py
+++ keras/src/backend/jax/numpy.py
@@ -1043,6 +1043,11 @@ def moveaxis(x, source, destination):
     return jnp.moveaxis(x, source=source, destination=destination)
 
 
+def nanargmax(x, axis=None, keepdims=False):
+    x = convert_to_tensor(x)
+    return jnp.nanargmax(x, axis=axis, keepdims=keepdims)
+
+
 def nanargmin(x, axis=None, keepdims=False):
     x = convert_to_tensor(x)
     return jnp.nanargmin(x, axis=axis, keepdims=keepdims)
