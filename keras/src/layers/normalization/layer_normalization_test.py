fix(normalization): use mean of squares instead of variance for rms_scaling in LayerNormalization

keras/src/layers/normalization/layer_normalization_test.py
@@ -123,7 +123,9 @@ class LayerNormalizationTest(testing.TestCase):
         )
         inputs = np.arange(5).astype("float32")[None, :]
         out = layer(inputs)
-        self.assertAllClose(out, [[0.0, 0.70693, 1.41386, 2.12079, 2.82772]])
+        self.assertAllClose(
+            out, [[0.0, 0.40821, 0.81643, 1.22464, 1.63286]], atol=1e-4
+        )
 
     def test_large_value_within_autocast_scope(self):
         layer = layers.LayerNormalization()