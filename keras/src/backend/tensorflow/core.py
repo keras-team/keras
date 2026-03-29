6843a4f4db149eb1adb064614470b5093e0d1b32
Chirag <wattamwarchiraggg@gmail.com>
2026-03-29 14:24:02 +0530

    fixed crash issue

--- keras/src/backend/tensorflow/core.py
+++ keras/src/backend/tensorflow/core.py
@@ -448,18 +448,29 @@ def associative_scan(f, elems, reverse=False, axis=0):
 
         def _interleave_with_b(a):
             a_shape = a.shape.as_list() if a.shape.rank is not None else None
-            if a_shape is not None and all(
-                d is not None for i, d in enumerate(a_shape) if i != axis
+            if isinstance(axis, int):
+                b_shape_at_axis = b.shape[axis]
+                num_elems_b_static = tf.get_static_value(
+                    b_shape_at_axis if b_shape_at_axis is not None else tf.shape(b)[axis]
+                )
+            else:
+                num_elems_b_static = None
+
+            if (
+                isinstance(axis, int)
+                and a_shape is not None
+                and num_elems_b_static is not None
+                and all(d is not None for i, d in enumerate(a_shape) if i != axis)
             ):
                 new_shape = (
-                    a_shape[:axis] + [2 * num_elems_b] + a_shape[axis + 1 :]
+                    a_shape[:axis] + [2 * num_elems_b_static] + a_shape[axis + 1:]
                 )
             else:
                 new_shape = tf.concat(
                     [
                         tf.shape(a)[:axis],
                         [2 * num_elems_b],
-                        tf.shape(a)[axis + 1 :],
+                        tf.shape(a)[axis + 1:],
                     ],
                     axis=0,
                 )
