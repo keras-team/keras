550c76228b5ed91cdbdb9c8b8cd2924b3b588214
daehyun99 <daehyun99@kakao.com>
2026-04-13 20:51:40 +0900

    Feat: OpenVINO Backend logic of `gaussian_blur`

--- keras/src/backend/openvino/image.py
+++ keras/src/backend/openvino/image.py
@@ -726,9 +726,119 @@ def map_coordinates(
 def gaussian_blur(
     images, kernel_size=(3, 3), sigma=(1.0, 1.0), data_format=None
 ):
-    raise NotImplementedError(
-        "`gaussian_blur` is not supported with openvino backend"
-    )
+    def _create_gaussian_kernel(kernel_size, sigma, ov_type):
+        def _get_gaussian_kernel1d(size, sigma):
+            x = (
+                ov_opset.range(0, size, 1, output_type=ov_type).output(0)
+                - (size - 1) / 2.0
+            )
+
+            exponent = ov_opset.divide(x, sigma).output(0)
+            exponent = ov_opset.multiply(exponent, exponent).output(0)
+            exponent = ov_opset.multiply(
+                exponent, ov_opset.constant(-0.5, Type.f32).output(0)
+            ).output(0)
+            kernel1d = ov_opset.exp(exponent).output(0)
+            kernel1d_sum = ov_opset.reduce_sum(
+                kernel1d, reduction_axes=0, keep_dims=False
+            ).output(0)
+
+            return ov_opset.divide(kernel1d, kernel1d_sum).output(0)
+
+        def _get_gaussian_kernel2d(size, sigma):
+            kernel1d_x = _get_gaussian_kernel1d(size[0], sigma[0])
+            kernel1d_y = _get_gaussian_kernel1d(size[1], sigma[1])
+
+            kernel1d_x = ov_opset.reshape(
+                kernel1d_x,
+                ov_opset.constant([1, int(size[1])], Type.i32).output(0),
+                False,
+            ).output(0)
+
+            kernel1d_y = ov_opset.reshape(
+                kernel1d_y,
+                ov_opset.constant([int(size[0]), 1], Type.i32).output(0),
+                False,
+            ).output(0)
+            return ov_opset.multiply(kernel1d_y, kernel1d_x).output(0)
+
+        kernel = _get_gaussian_kernel2d(kernel_size, sigma)
+        kernel = ov_opset.convert(kernel, ov_type).output(0)
+        return kernel
+
+    images = convert_to_tensor(images)
+    ov_type = Type(images.dtype)
+
+    kernel_size = convert_to_tensor(kernel_size)
+    sigma = convert_to_tensor(sigma)
+
+    if len(images.shape) not in (3, 4):
+        raise ValueError(
+            "Invalid images rank: expected rank 3 (single image) "
+            "or rank 4 (batch of images). Received input with shape: "
+            f"images.shape={images.shape}"
+        )
+
+    need_squeeze = False
+    if len(images.shape) == 3:
+        images = ov_opset.unsqueeze(images, axes=[0]).output(0)
+        need_squeeze = True
+
+    if data_format == "channels_last":
+        images = ov_opset.transpose(
+            images,
+            ov_opset.constant([0, 3, 1, 2], Type.i32).output(0),
+        ).output(0)
+
+    num_channels = ov_opset.gather(
+        ov_opset.shape_of(images, Type.i32).output(0),
+        ov_opset.constant([1], Type.i32).output(0),
+        ov_opset.constant(0, Type.i32).output(0),
+    ).output(0)
+    kernel = _create_gaussian_kernel(kernel_size, sigma, ov_type)
+
+    kernel = ov_opset.reshape(
+        kernel,
+        ov_opset.constant(
+            [1, 1, kernel_size[0], kernel_size[1]], Type.i32
+        ).output(0),
+        False,
+    ).output(0)
+
+    target_shape = ov_opset.concat(
+        [
+            num_channels,
+            ov_opset.constant(
+                [1, 1, kernel_size[0], kernel_size[1]], Type.i32
+            ).output(0),
+        ],
+        axis=0,
+    ).output(0)
+
+    kernel = ov_opset.broadcast(kernel, target_shape).output(0)
+
+    blurred_images = ov_opset.group_convolution(
+        images,
+        kernel,
+        [1, 1],
+        [(kernel_size[0] - 1) // 2, (kernel_size[1] - 1) // 2],
+        [
+            kernel_size[0] - 1 - (kernel_size[0] - 1) // 2,
+            kernel_size[1] - 1 - (kernel_size[1] - 1) // 2,
+        ],
+        [1, 1],
+    ).output(0)
+
+    if data_format == "channels_last":
+        blurred_images = ov_opset.transpose(
+            blurred_images,
+            ov_opset.constant([0, 2, 3, 1], Type.i32).output(0),
+        ).output(0)
+
+    if need_squeeze:
+        blurred_images = ov_opset.squeeze(blurred_images, axes=[0]).output(0)
+
+    return OpenVINOKerasTensor(blurred_images)
 
 
 def elastic_transform(
