e545deade06a38b0f78bb47ddcc6953c011a0452
daehyun99 <daehyun99@kakao.com>
2026-04-14 04:00:12 +0900

    Revert: revert dtypes

--- keras/src/backend/common/dtypes.py
+++ keras/src/backend/common/dtypes.py
@@ -51,7 +51,6 @@ PYTHON_DTYPES_MAP = {
     str: "string",
     # special case for string value
     "int": "int64" if config.backend() == "tensorflow" else "int32",
-    "bfloat16": "float32",
     complex: "complex128" if config.backend() == "tensorflow" else "complex64",
 }
 
