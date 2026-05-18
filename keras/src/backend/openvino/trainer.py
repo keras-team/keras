c8b27271dc8ee0067658c6b0675e38aa84eaf62c
Aditya Goyal <goyaladitya2403@gmail.com>
2026-05-19 05:17:21 +0530

    [OpenVINO]  Fix dynamic batch shape propagation through ops.slice (#22915)
    * Revert "[OpenVino] Change `_parameterize_data` for KerasHub. (#22681)"
    
    This reverts commit 5648142b79d272bb996c53524c7be1da8b3b2117.
    
    * [OpenVINO] Handle symbolic slice lengths in core.slice

--- keras/src/backend/openvino/trainer.py
+++ keras/src/backend/openvino/trainer.py
@@ -99,23 +99,23 @@ class OpenVINOTrainer(base_trainer.Trainer):
 
         self.test_function = test_step
 
-    def _parameterize_data(self, data, dynamic_batch_size=False):
+    def _parameterize_data(self, data):
         if isinstance(data, (list, tuple)):
             parametrize_data = []
             for elem in data:
-                param_elem = self._parameterize_data(elem, dynamic_batch_size)
+                param_elem = self._parameterize_data(elem)
                 parametrize_data.append(param_elem)
         elif isinstance(data, dict):
             parametrize_data = dict()
             for elem_name, elem in data.items():
-                param_elem = self._parameterize_data(elem, dynamic_batch_size)
+                param_elem = self._parameterize_data(elem)
                 parametrize_data[elem_name] = param_elem
         elif isinstance(data, OpenVINOKerasTensor):
             parametrize_data = data
         elif isinstance(data, np.ndarray) or np.isscalar(data):
             ov_type = OPENVINO_DTYPES[str(data.dtype)]
             ov_shape = list(data.shape)
-            if dynamic_batch_size and len(ov_shape) > 0:
+            if len(ov_shape) > 0:
                 ov_shape[0] = -1
             param = ov_opset.parameter(shape=ov_shape, dtype=ov_type)
             parametrize_data = OpenVINOKerasTensor(param.output(0))
@@ -152,9 +152,7 @@ class OpenVINOTrainer(base_trainer.Trainer):
         del self.ov_compiled_model
 
         # prepare parameterized input
-        self.struct_params = self._parameterize_data(
-            data, dynamic_batch_size=True
-        )
+        self.struct_params = self._parameterize_data(data)
         # construct OpenVINO graph during calling Keras Model
         if self._call_has_training_arg:
             self.struct_outputs = self(self.struct_params, training=False)
