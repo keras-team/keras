6b5b609254e0485068e30132b13af8ab45839e24
Vadym Doroshenko <53558779+dvadym@users.noreply.github.com>
2026-05-29 00:56:49 +0200

    Fix data distribution for model training in Jax Training (#23003)
    * fix
    
    * Addressed automatic comments
    
    * Added tst
    
    * Fixed test

--- keras/src/backend/jax/trainer.py
+++ keras/src/backend/jax/trainer.py
@@ -1068,10 +1068,13 @@ def _distribute_data(data, layouts=None):
 
     if distribution is not None:
         if layouts is None:
-            layouts = tree.map_structure(
-                lambda d: distribution.get_data_layout(d.shape),
-                data,
-            )
+
+            def get_layout(d):
+                if d is None:
+                    return None
+                return distribution.get_data_layout(d.shape)
+
+            layouts = tree.map_structure(get_layout, data)
         jax_dist_data_input = partial(
             jax_distribution_lib.distribute_data_input,
             batch_dim_name=distribution.batch_dim_name,
@@ -1101,12 +1104,13 @@ class JAXEpochIterator(EpochIterator):
         layouts = None
         for data in self.data_adapter.get_jax_iterator():
             if layouts is None:
-                layouts = tree.map_structure(
-                    lambda d: (
-                        distribution.get_data_layout(d.shape).backend_layout
-                    ),
-                    data,
-                )
+
+                def get_layout(d):
+                    if d is None:
+                        return None
+                    return distribution.get_data_layout(d.shape).backend_layout
+
+                layouts = tree.map_structure(get_layout, data)
             yield _distribute_data(data, layouts)
 
     def _one_batch_ahead_iterator(self, numpy_iterator):
