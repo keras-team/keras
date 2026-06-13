ec270b9c0f82b828830daffc517443d82a2adbad
pedro <3650280+peinguim@users.noreply.github.com>
2026-06-13 15:48:58 +0200

    enter tf.init_scope when adding optimizer variables with TF backend
    fixes #23085 tracing error in TF distributed training with stateful optimizers

--- keras/src/backend/tensorflow/optimizer.py
+++ keras/src/backend/tensorflow/optimizer.py
@@ -21,6 +21,10 @@ class TFOptimizer(KerasAutoTrackable, base_optimizer.BaseOptimizer):
         super().__init__(*args, **kwargs)
         self._distribution_strategy = tf.distribute.get_strategy()
 
+    def add_variable(self, *args, **kwargs):
+        with tf.init_scope():
+            return super().add_variable(*args, **kwargs)
+
     def add_variable_from_reference(
         self, reference_variable, name=None, initializer="zeros"
     ):
