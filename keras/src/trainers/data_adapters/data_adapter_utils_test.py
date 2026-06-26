fa308919b2de5d4f9aba538581b87e9eae356a65
Suhana <suhanaaa@google.com>
2026-06-27 00:01:04 +0530

    [Distribution] Implement native sharding support for Keras NumPy adapter (#23046)
    * [Distribution] Implement sharding support for Keras NumPy adapter
    
    * [Distribution] Fix JAX multi-process tests by using TFDatasetAdapter
    
    * [Distribution] Refactor tf_dataset_adapter_test to use real distribution objects
    
    * Implement sharding support for Keras NumPy adapter

--- keras/src/trainers/data_adapters/data_adapter_utils_test.py
+++ keras/src/trainers/data_adapters/data_adapter_utils_test.py
@@ -7,8 +7,89 @@ from absl.testing import parameterized
 from keras.src import backend
 from keras.src import testing
 from keras.src.trainers.data_adapters.data_adapter_utils import (
+    DistributedBatchSampler,
+)
+from keras.src.trainers.data_adapters.data_adapter_utils import (
     class_weight_to_sample_weights,
 )
+
+
+class TestDistributedBatchSampler(testing.TestCase):
+    @pytest.mark.skipif(backend.backend() != "torch", reason="torch only")
+    def test_basic_sharding(self):
+        # 10 batches total
+        batch_sampler = list(range(10))
+
+        # Test with 2 replicas
+        # Rank 0 should get [0, 2, 4, 6, 8]
+        sampler0 = DistributedBatchSampler(
+            batch_sampler, num_data_shards=2, data_shard_id=0
+        )
+        self.assertEqual(list(sampler0), [0, 2, 4, 6, 8])
+        self.assertEqual(len(sampler0), 5)
+
+        # Rank 1 should get [1, 3, 5, 7, 9]
+        sampler1 = DistributedBatchSampler(
+            batch_sampler, num_data_shards=2, data_shard_id=1
+        )
+        self.assertEqual(list(sampler1), [1, 3, 5, 7, 9])
+        self.assertEqual(len(sampler1), 5)
+
+    @pytest.mark.skipif(backend.backend() != "torch", reason="torch only")
+    def test_uneven_sharding(self):
+        # 11 batches total
+        batch_sampler = list(range(11))
+
+        # Test with 3 replicas
+        # Rank 0 should get [0, 3, 6, 9]
+        sampler0 = DistributedBatchSampler(
+            batch_sampler, num_data_shards=3, data_shard_id=0, drop_last=False
+        )
+        self.assertEqual(list(sampler0), [0, 3, 6, 9])
+        self.assertEqual(len(sampler0), 4)
+
+        # Rank 1 should get [1, 4, 7, 10]
+        sampler1 = DistributedBatchSampler(
+            batch_sampler, num_data_shards=3, data_shard_id=1, drop_last=False
+        )
+        self.assertEqual(list(sampler1), [1, 4, 7, 10])
+        self.assertEqual(len(sampler1), 4)
+
+        # Rank 2 should get [2, 5, 8]
+        sampler2 = DistributedBatchSampler(
+            batch_sampler, num_data_shards=3, data_shard_id=2, drop_last=False
+        )
+        self.assertEqual(list(sampler2), [2, 5, 8])
+        self.assertEqual(len(sampler2), 3)
+
+    @pytest.mark.skipif(backend.backend() != "torch", reason="torch only")
+    def test_drop_last(self):
+        # 11 batches total
+        batch_sampler = list(range(11))
+
+        # Test with 3 replicas and drop_last=True
+        # num_batches = 11 // 3 = 3 batches per replica
+        # Total used = 3 * 3 = 9 batches
+        # Rank 0: [0, 3, 6]
+        sampler0 = DistributedBatchSampler(
+            batch_sampler, num_data_shards=3, data_shard_id=0, drop_last=True
+        )
+        self.assertEqual(list(sampler0), [0, 3, 6])
+        self.assertEqual(len(sampler0), 3)
+
+        # Rank 1: [1, 4, 7]
+        sampler1 = DistributedBatchSampler(
+            batch_sampler, num_data_shards=3, data_shard_id=1, drop_last=True
+        )
+        self.assertEqual(list(sampler1), [1, 4, 7])
+        self.assertEqual(len(sampler1), 3)
+
+        # Rank 2: [2, 5, 8]
+        sampler2 = DistributedBatchSampler(
+            batch_sampler, num_data_shards=3, data_shard_id=2, drop_last=True
+        )
+        self.assertEqual(list(sampler2), [2, 5, 8])
+        self.assertEqual(len(sampler2), 3)
 
 
 class TestClassWeightToSampleWeights(testing.TestCase):
