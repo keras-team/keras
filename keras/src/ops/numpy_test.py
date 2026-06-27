dca216ed79f8136840f5e838c80edcdc48c70a84
Chirag Wattamwar <119049661+ChiragSW@users.noreply.github.com>
2026-06-27 10:58:57 +0530

    [Fix] keras.ops.nancumsum returns finite values instead of inf/nan for inputs containing NaN and infinities (#23135)
    * [Fix] keras.ops.nancumsum returns finite values instead of inf/nan for inputs containing NaN and infinities
    
    * did a custom fix for openvino

--- keras/src/ops/numpy_test.py
+++ keras/src/ops/numpy_test.py
@@ -7183,6 +7183,14 @@ class NumpyOneInputOpsCorrectnessTest(testing.TestCase):
             knp.nancumsum(x_all_nan, axis=1), np.nancumsum(x_all_nan, axis=1)
         )
 
+        x_with_inf = np.array(
+            [[np.nan, np.inf, 1.0], [np.nan, -np.inf, -1.0]], dtype=np.float32
+        )
+        self.assertAllClose(
+            knp.nancumsum(x_with_inf, axis=1),
+            np.nancumsum(x_with_inf, axis=1),
+        )
+
     def test_nancumprod(self):
         x = np.array([[1.0, np.nan, 3.0], [np.nan, 2.0, -1.0]])
 
