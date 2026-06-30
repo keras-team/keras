b9c7dcb212193fff622cb2ee76a86ab484d96fb4
Suhana <suhanaaa@google.com>
2026-06-30 17:48:20 +0530

    [Distribution] Support self-sharding for Torch DataLoader including IterableDataset

--- keras/src/trainers/data_adapters/torch_data_loader_adapter.py
+++ keras/src/trainers/data_adapters/torch_data_loader_adapter.py
@@ -3,6 +3,7 @@ import itertools
 import numpy as np
 
 from keras.src import tree
+from keras.src.distribution import distribution_lib
 from keras.src.trainers.data_adapters import data_adapter_utils
 from keras.src.trainers.data_adapters.data_adapter import DataAdapter
 
@@ -19,17 +20,108 @@ class TorchDataLoaderAdapter(DataAdapter):
                 f"`torch.utils.data.DataLoader`. Received: {dataloader}"
             )
 
+        dist = distribution_lib.distribution()
+        self._num_data_shards = 1
+        self._data_shard_id = 0
+        if dist is not None and getattr(dist, "auto_shard_dataset", False):
+            self._num_data_shards = min(
+                dist.num_model_replicas, dist.num_processes
+            )
+            self._data_shard_id = dist.data_shard_id
+
+        if (
+            dist is not None
+            and dist._is_multi_process
+            and getattr(dist, "auto_shard_dataset", False)
+        ):
+            # Detect shuffle from dataloader
+            shuffle = False
+            if hasattr(dataloader, "sampler") and isinstance(
+                dataloader.sampler, torch.utils.data.RandomSampler
+            ):
+                shuffle = True
+
+            dataset = dataloader.dataset
+            if isinstance(dataset, torch.utils.data.IterableDataset):
+
+                class ShardedIterableDataset(torch.utils.data.IterableDataset):
+                    def __init__(self, dataset, num_data_shards, data_shard_id):
+                        self.dataset = dataset
+                        self.num_data_shards = num_data_shards
+                        self.data_shard_id = data_shard_id
+
+                    def __iter__(self):
+                        for i, item in enumerate(self.dataset):
+                            if i % self.num_data_shards == self.data_shard_id:
+                                yield item
+
+                if hasattr(dataset, "__len__"):
+
+                    def __len__(self):
+                        return (
+                            len(self.dataset)
+                            - self.data_shard_id
+                            + self.num_data_shards
+                            - 1
+                        ) // self.num_data_shards
+
+                    ShardedIterableDataset.__len__ = __len__
+
+                dataloader = torch.utils.data.DataLoader(
+                    ShardedIterableDataset(
+                        dataset, self._num_data_shards, self._data_shard_id
+                    ),
+                    batch_size=dataloader.batch_size,
+                    num_workers=dataloader.num_workers,
+                    pin_memory=dataloader.pin_memory,
+                    drop_last=dataloader.drop_last,
+                    timeout=dataloader.timeout,
+                    worker_init_fn=dataloader.worker_init_fn,
+                    prefetch_factor=dataloader.prefetch_factor,
+                    persistent_workers=dataloader.persistent_workers,
+                )
+            else:
+                sampler = torch.utils.data.distributed.DistributedSampler(
+                    dataset,
+                    num_replicas=self._num_data_shards,
+                    rank=self._data_shard_id,
+                    shuffle=shuffle,
+                )
+                dataloader = torch.utils.data.DataLoader(
+                    dataset,
+                    batch_size=dataloader.batch_size,
+                    sampler=sampler,
+                    num_workers=dataloader.num_workers,
+                    pin_memory=dataloader.pin_memory,
+                    drop_last=dataloader.drop_last,
+                    timeout=dataloader.timeout,
+                    worker_init_fn=dataloader.worker_init_fn,
+                    prefetch_factor=dataloader.prefetch_factor,
+                    persistent_workers=dataloader.persistent_workers,
+                )
+
         self._dataloader = dataloader
-        self._output_signature = None
+        self._epoch = 0
         self._batch_size = dataloader.batch_size
+        self._output_signature = None
         self._num_batches = None
         self._partial_batch_size = None
-        if hasattr(dataloader.dataset, "__len__"):
-            self._num_batches = len(dataloader)
+        if hasattr(self._dataloader.dataset, "__len__"):
+            self._num_batches = len(self._dataloader)
             if self._batch_size is not None:
                 self._partial_batch_size = (
-                    len(dataloader.dataset) % self._batch_size
+                    len(self._dataloader.dataset) % self._batch_size
                 )
+
+    def on_epoch_begin(self):
+        if hasattr(self._dataloader, "sampler") and hasattr(
+            self._dataloader.sampler, "set_epoch"
+        ):
+            self._dataloader.sampler.set_epoch(self._epoch)
+
+    def on_epoch_end(self):
+        self._epoch += 1
+        self._output_signature = None
 
     def get_numpy_iterator(self):
         for batch in self._dataloader:
